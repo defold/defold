0e535ab6580e28f0c981d6d159179935321bada0
Christian Murray <chmu@CM-2.local>
2009-03-08 18:37:50 +0100

    Update script updates * Python egg support (requires setuptools to be installed locally) * Removed gzip -t check (doesn't work with egg-files) * Download files to $DYNAMO_EXT/cache instead of $DYNAMO_EXT

--- bin/update-ext.sh
+++ bin/update-ext.sh
@@ -1,8 +1,8 @@
 # Note: $USER must be set on windows!
 
 download() {
-    [ -f $DYNAMO_EXT/$1 ] && gzip -t $DYNAMO_EXT/$1 && return
-    scp "$REMOTE_PATH/$1" $DYNAMO_EXT
+    [ -f $DYNAMO_EXT/cache/$1 ] && return
+    scp "$REMOTE_PATH/$1" $DYNAMO_EXT/cache
 }
 
 tmp=`uname -s`
@@ -16,9 +16,12 @@ tmp=`uname -s`
 [ -z "$DYNAMO_EXT" ] && echo "ERROR: DYNAMO_EXT not set" && exit 1
 [ ! -d "$DYNAMO_EXT" ] && echo "ERROR: DYNAMO_EXT is not a directory" && exit 1
 
+mkdir -p $DYNAMO_EXT/cache
+
 REMOTE_PATH="$USER@overrated.dyndns.org:packages"
 PACKAGES_ALL="protobuf-2.0.3 waf-1.5.3"
 PACKAGES_HOST="protobuf-2.0.3"
+PACKAGES_EGGS="protobuf-2.0.3-py2.5.egg"
 
 for p in $PACKAGES_ALL; do
     download "$p-common.tar.gz"
@@ -28,13 +31,25 @@ for p in $PACKAGES_HOST; do
     download "$p-$HOST.tar.gz"
 done
 
+for p in $PACKAGES_EGGS; do
+    download "$p"
+done
+
 pushd $DYNAMO_EXT > /dev/null
-for p in `ls *.tar.gz`; do
+for p in `ls cache/*.tar.gz`; do
     echo "Extracting $p..."
     tar xfz $p
 done
 
-rm *.tar.gz
+mkdir -p $DYNAMO_EXT/lib/python
+export PYTHONPATH=$DYNAMO_EXT/lib/python
+
+for p in `ls cache/*.egg`; do
+    echo "Installing $p..."
+    easy_install -q -d $DYNAMO_EXT/lib/python -N $p
+done
+
+#rm *.tar.gz *.egg
 
 popd > /dev/null
 
