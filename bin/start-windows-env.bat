f4c355ea0e5555b40c45a747d6225fb91b5ac2ac
unknown <Administrator@.(none)>
2009-03-08 10:07:29 -0500

    Added start-window-env.bat

---
+++ bin/start-windows-env.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set PATH=%DYNAMO_EXT%\bin;%PATH%
+set PATH=%DYNAMO_EXT%\bin\win32;%PATH%
+
+call "%VS90COMNTOOLS%\vsvars32.bat"
+
+rem cmd.exe /c "C:\Program Files\Git\bin\sh.exe" --login -i
+cmd 
+
