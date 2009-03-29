10d8e87cf2ca553be6ea4df06362c2de1000a410
Christian Murray <chmu@CM-2.local>
2009-03-29 18:04:36 +0200

    Added pyglet to ext

--- bin/update-ext.sh
+++ bin/update-ext.sh
@@ -28,7 +28,7 @@ mkdir -p $DYNAMO_EXT/cache
 REMOTE_PATH="$USER_OVERRATED@overrated.dyndns.org:/repo/packages"
 PACKAGES_ALL="protobuf-2.0.3 waf-1.5.3 gtest-1.2.1 collada-blender-0.3.159.1"
 PACKAGES_HOST="protobuf-2.0.3 gtest-1.2.1"
-PACKAGES_EGGS="protobuf-2.0.3-py2.5.egg"
+PACKAGES_EGGS="protobuf-2.0.3-py2.5.egg pyglet-1.1.3-py2.5.egg"
 
 for p in $PACKAGES_ALL; do
     download "$p-common.tar.gz"
