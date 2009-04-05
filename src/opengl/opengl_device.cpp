1e167367cb9a77154bb64b98d9961ce603b1cf3b
Fredrik Egeberg <Freddan@fredrik-egebergs-macbook.local>
2009-04-05 13:58:25 +0200

    First version of graphics lib

---
+++ src/opengl/opengl_device.cpp
@@ -0,0 +1,129 @@
+#include <graphics_device.h>
+#include "opengl_device.h"
+#include "assert.h"
+
+
+GFXHDevice GFXCreateDevice(GFXSCreateDeviceParams *params )
+{
+    assert(params);
+
+    int argc = 0;
+    char** argv = NULL;
+
+    glutInitWindowSize(params->m_DisplayWidth, params->m_DisplayHeight);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInit(&argc, argv);
+
+    glutCreateWindow(params->m_AppTitle);
+
+
+    return 0;
+}
+
+
+void GFXClear(GFXHContext context, uint32_t flags, uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha, float depth, uint32_t stencil)
+{
+    (void)context;
+
+    float r = ((float)red)/255.0f;
+    float g = ((float)green)/255.0f;
+    float b = ((float)blue)/255.0f;
+    float a = ((float)alpha)/255.0f;
+    glClearColor(r, g, b, a);
+
+
+    glClearDepth(depth);
+    glClearStencil(stencil);
+    glClear(flags);
+}
+
+void GFXFlip()
+{
+    glutSwapBuffers();
+}
+
+void GFXDraw(GFXHContext context, GFXPrimitiveType primitive_type, int32_t first, int32_t count )
+{
+    (void)context;
+
+    glDrawArrays(primitive_type, first, count);
+}
+
+
+void GFXDrawTriangle2D(GFXHContext context, const float* vertices, const float* colours)
+{
+
+    (void)context;
+
+    assert(vertices);
+    assert(colours);
+
+    glBegin(GL_TRIANGLES);
+
+    glColor3f(colours[0], colours[1], colours[2]);
+    glVertex2f(vertices[0], vertices[1]);
+
+    glColor3f(colours[3], colours[4], colours[5]);
+    glVertex2f(vertices[2], vertices[3]);
+
+    glColor3f(colours[6], colours[7], colours[8]);
+    glVertex2f(vertices[4], vertices[5]);
+
+    glEnd();
+
+}
+
+
+void GFXDrawTriangle3D(GFXHContext context, const float* vertices, const float* colours)
+{
+
+    (void)context;
+
+    assert(vertices);
+    assert(colours);
+
+    glBegin(GL_TRIANGLES);
+
+    glColor3f(colours[0], colours[1], colours[2]);
+    glVertex3f(vertices[0], vertices[1], vertices[2]);
+
+    glColor3f(colours[3], colours[4], colours[5]);
+    glVertex3f(vertices[3], vertices[4], vertices[5]);
+
+    glColor3f(colours[6], colours[7], colours[8]);
+    glVertex3f(vertices[6], vertices[7], vertices[8]);
+
+    glEnd();
+
+}
+
+
+void GFXSetViewport(GFXHContext context, int width, int height, float field_of_view, float z_near, float z_far)
+{
+    (void)context;
+
+    float aspect_ratio = (float) width / (float) height;
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(field_of_view, aspect_ratio,
+      z_near,
+      z_far);
+
+    glViewport(0, 0, width, height);
+
+}
+
+void GFXEnableState(GFXHContext context, GFXRenderState state)
+{
+    (void)context;
+
+    glEnable(state);
+}
+
+void GFXDisableState(GFXHContext context, GFXRenderState state)
+{
+    (void)context;
+
+    glDisable(state);
+}
---
+++ wscript
@@ -0,0 +1,51 @@
+#! /usr/bin/env python
+
+VERSION = '0.1'
+APPNAME = 'graphics'
+
+srcdir = '.'
+blddir = 'build'
+
+import os, sys
+
+def init():
+    pass
+
+def set_options(opt):
+    opt.sub_options('src')
+    opt.tool_options('compiler_cxx')
+
+def configure(conf):
+    conf.check_tool('compiler_cxx')
+    conf.sub_config('src')
+
+    if sys.platform == "darwin":
+        platform = "darwin"
+    elif sys.platform == "linux2":
+        platform = "linux"
+    elif sys.platform == "win32":
+        platform = "win32"
+    else:
+        conf.fatal("Unable to determine platform")
+
+    if platform == "linux" or platform == "darwin":
+        conf.env['CXXFLAGS']='-g -D__STDC_LIMIT_MACROS -Wall'
+    else:
+        conf.env['CXXFLAGS']=['/Z7', '/MT', '/D__STDC_LIMIT_MACROS']
+        conf.env.append_value('CPPPATH', "../include/win32")
+
+    dynamo_home = os.getenv('DYNAMO_HOME')
+    if not dynamo_home:
+        conf.fatal("DYNAMO_HOME not set")
+    dynamo_ext = os.path.join(dynamo_home, "ext")
+
+    conf.env.append_value('CPPPATH', os.path.join(dynamo_ext, "include"))
+    conf.env.append_value('LIBPATH', os.path.join(dynamo_ext, "lib", platform))
+
+    conf.env['LIB_GTEST'] = 'gtest'
+
+def build(bld):
+    bld.add_subdirs('src')
+    bld.install_files('${PREFIX}/include/win32', 'include/win32/*.h')
+
+
