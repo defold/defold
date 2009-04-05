122c6eaedd89660a3a1c8a9def4f32f8a2f3369c
Christian Murray <chmu@CM-2.local>
2009-04-05 15:31:53 +0200

    Added Sonys vectormathlibrary.

--- bin/update-ext.sh
+++ bin/update-ext.sh
@@ -26,7 +26,7 @@ DYNAMO_EXT=$DYNAMO_HOME/ext
 mkdir -p $DYNAMO_EXT/cache
 
 REMOTE_PATH="$USER_OVERRATED@overrated.dyndns.org:/repo/packages"
-PACKAGES_ALL="protobuf-2.0.3 waf-1.5.3 gtest-1.2.1 collada-blender-0.3.159.1"
+PACKAGES_ALL="protobuf-2.0.3 waf-1.5.3 gtest-1.2.1 collada-blender-0.3.159.1 vectormathlibrary-r1649"
 PACKAGES_HOST="protobuf-2.0.3 gtest-1.2.1"
 PACKAGES_EGGS="protobuf-2.0.3-py2.5.egg pyglet-1.1.3-py2.5.egg"
 
