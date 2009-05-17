e13a27b53b220df8bbe7c4b4315406ed92ac18eb
Fredrik Egeberg <Freddan@fredrik-egebergs-macbook.local>
2009-05-17 19:31:27 +0200

    added error check

--- src/gamesys/script.cpp
+++ src/gamesys/script.cpp
@@ -3,28 +3,44 @@
 
 namespace Script
 {
+
+    void CheckErrors()
+    {
+        if ( PyErr_Occurred() )
+        {
+            PyErr_Print();
+            fflush(stderr);
+            fflush(stdout);
+        }
+    }
+
     bool Initialize()
     {
         Py_Initialize();
+        CheckErrors();
         return true;
     }
 
     bool Finalize()
     {
         Py_Finalize();
+        CheckErrors();
         return true;
     }
 
     HScript New(const void* memory)
     {
-        assert(memory);
-
         char* update_func = "Update";
 
         PyObject* obj = Py_CompileString((const char*)memory, "<script>", Py_file_input);
+        CheckErrors();
 
         PyObject* module = PyImport_ExecCodeModule(update_func, obj);
+        CheckErrors();
+
         PyObject* func = PyObject_GetAttrString(module, update_func);
+        CheckErrors();
+
         Py_DECREF(obj);
         Py_DECREF(module);
 
@@ -33,15 +49,11 @@ namespace Script
 
     void Delete(HScript script)
     {
-        assert(script);
-
         Py_DECREF((PyObject*) script);
     }
 
     bool Run(HScript script, PyObject* self, PyObject* args)
     {
-        assert(script);
-
         PyObject* func = (PyObject*)script;
 
         if (func && PyCallable_Check(func))
