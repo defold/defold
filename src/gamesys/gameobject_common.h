69341e42b98c5cec4f72d8cd3809fa17672b3f82
Christian Murray <chmu@CM-2.local>
2009-05-23 19:50:19 +0200

    Added missing file.

---
+++ src/gamesys/gameobject_common.h
@@ -0,0 +1,49 @@
+#ifndef GAMEOBJECT_COMMON_H
+#define GAMEOBJECT_COMMON_H
+
+#include <vectormath/cpp/vectormath_aos.h>
+using namespace Vectormath::Aos;
+
+namespace GameObject
+{
+    struct Prototype
+    {
+        const char*        m_Name;
+        HScript*           m_Script;
+
+        #if 0
+        HMesh              m_Mesh;
+        HMaterial          m_Material;
+        HScript            m_Script;
+        #endif
+    /*
+        TArray<SResourceDescriptor>  m_Resources;
+        // or
+        TArray<HEffect>    m_Effects;
+        TArray<HAnimation> m_Animations;
+        // ....
+        */
+    };
+
+    struct Instance
+    {
+        Instance(Prototype* prototype)
+        {
+            m_Position = Point3(0,0,0);
+            m_Prototype = prototype;
+            m_Self = PyObject_CallObject((PyObject*) &PythonInstanceType, 0);
+            ((PythonInstance*) m_Self)->m_Instance = this;
+        }
+
+        ~Instance()
+        {
+            Py_DECREF(m_Self);
+        }
+
+        Point3      m_Position;
+        Prototype*  m_Prototype;
+        PyObject*   m_Self;
+    };
+}
+
+#endif // GAMEOBJECT_COMMON_H
