6956c370744998f074623df56aae9edd855cec16
Fredrik Egeberg <Freddan@fredrik-egebergs-macbook.local>
2009-07-05 16:53:55 +0200

    Moved index and vertex data from .h to .cpp, doh

--- src/debug/debugrenderer.cpp
+++ src/debug/debugrenderer.cpp
@@ -6,6 +6,32 @@ using namespace Vectormath::Aos;
 
 namespace dmRenderDebug
 {
+    static const float CubeVertices[] = {
+            -1, -1, -1,
+            1, -1, -1,
+            1, 1, -1,
+            -1, 1, -1,
+            -1, -1, 1,
+            1, -1, 1,
+            1, 1, 1,
+            -1, 1, 1};
+
+    static const int CubeIndices[] =
+    {
+            0, 1, 2,
+            2, 3, 0,
+            0, 3, 7,
+            7, 4, 0,
+            4, 7, 6,
+            6, 5, 4,
+            1, 5, 6,
+            6, 2, 1,
+            0, 4, 5,
+            5, 1, 0,
+            3, 2, 6,
+            6, 7, 3
+    };
+
     void RenderCube(Point3 position, float size)
     {
 
