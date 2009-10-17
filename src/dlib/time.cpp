cbc0f449d4be09cf4b73814b76e0408808f336e2
Christian Murray <chmu@CM-2.local>
2009-10-17 14:52:03 +0200

    WIN32 support

--- src/dlib/time.cpp
+++ src/dlib/time.cpp
@@ -1,16 +1,19 @@
 #include "time.h"
 
-#if defined(__linux__) or defined(__MACH__)
+#if defined(__linux__) || defined(__MACH__)
 #include <unistd.h>
-
+#elif defined(_WIN32)
+#include <windows.h>
 #else
 #error "Unsupported platform"
 #endif
 
 void dmSleep(uint32_t useconds)
 {
-#if defined(__linux__) or defined(__MACH__)
+#if defined(__linux__) || defined(__MACH__)
     usleep(useconds);
+#elif defined(_WIN32)
+    Sleep(useconds / 1000);
 #else
 #error "Unsupported platform"
 #endif
