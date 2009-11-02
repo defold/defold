3f428981fed76a1c3db27a5526665cbb86377871
Christian Murray <chmu@CM-2.local>
2009-11-02 18:50:26 +0100

    Mega-n00b statement with side-effect in assert fix.

--- src/dlib/thread.cpp
+++ src/dlib/thread.cpp
@@ -7,27 +7,31 @@ namespace dmThread
     Thread New(ThreadStart thread_start, uint32_t stack_size, void* arg)
     {
         pthread_attr_t attr;
-        assert(pthread_attr_init(&attr) == 0);
+        int ret = pthread_attr_init(&attr);
+        assert(ret == 0);
 
         // NOTE: At least on OSX the stack-size must be a multiple of page size
         stack_size /= 4096;
         stack_size += 1;
         stack_size *= 4096;
 
-        assert(pthread_attr_setstacksize(&attr, stack_size) == 0);
+        ret = pthread_attr_setstacksize(&attr, stack_size);
+        assert(ret == 0);
         
         pthread_t thread;
         
-        int ret = pthread_create(&thread, &attr, (void* (*)(void*)) thread_start, arg);
+        ret = pthread_create(&thread, &attr, (void* (*)(void*)) thread_start, arg);
         assert(ret == 0);
-        assert(pthread_attr_destroy(&attr) == 0);
+        ret = pthread_attr_destroy(&attr);
+        assert(ret == 0);
         
         return thread;
     }
     
     void Join(Thread thread)
     {
-        assert(pthread_join(thread, 0) == 0);
+        int ret = pthread_join(thread, 0);
+        assert(ret == 0);
     }
 
 #elif defined(_WIN32)
