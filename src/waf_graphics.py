0f6bd5ce8ecb4a05c4c49146ee6cc9d373085af5
Christian Murray <chmu@CM-2.local>
2009-11-03 23:17:07 +0100

    Support for vertex_program and fragment_program in fontc.py

--- src/waf_graphics.py
+++ src/waf_graphics.py
@@ -53,7 +53,8 @@ def dds_file(self, node):
     # Hackish... configure?
     #self.env["DDSC"] = os.path.join(os.environ["DYNAMO_HOME"], "bin", "ddsc.py")
 
-Task.simple_task_type('ttf', 'python ${FONTC} -s ${size} -o ${TGT} ${SRC}',
+# TODO: Move ttf compilation to render?
+Task.simple_task_type('ttf', 'python ${FONTC} -s ${size} --vp ${vertex_program} --fp ${fragment_program} -o ${TGT} ${SRC}',
                       color='PINK',
                       after='proto_gen_py',
                       before='cc cxx',
@@ -64,6 +65,8 @@ def ttf_file(self, node):
     obj_ext = '.font'
     font = self.create_task('ttf')
     self.env['size'] = str(self.size)
+    self.env['vertex_program'] = str(self.vertex_program)
+    self.env['fragment_program'] = str(self.fragment_program)
     font.set_inputs(node)
     out = node.change_ext(obj_ext)
     font.set_outputs(out)
