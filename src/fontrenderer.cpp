07f5563d38975cb751756779ed61675fb4c7c0f6
Christian Murray <chmu@CM-2.local>
2009-11-05 20:26:50 +0100

    Added support for font separate from font renderer.

--- src/fontrenderer.cpp
+++ src/fontrenderer.cpp
@@ -10,45 +10,76 @@ using namespace Vectormath::Aos;
 
 namespace dmRender
 {
-
     struct SFontVertex
     {
         float m_Position[3];
         float m_UV[2];
     };
 
-    struct SFontRenderer
+    struct SFont
     {
-        Font*                          m_Font;
-        std::vector<SFontVertex>       m_Vertices;
-        Matrix4                        m_Projection;
-        dmGraphics::HTexture                  m_Texture;
-        uint32_t                       m_MaxCharacters;
+        ~SFont()
+        {
+            dmDDF::FreeMessage(m_Font);
+            dmGraphics::DestroyTexture(m_Texture);
+        }
+
+        Font*                m_Font;
+        dmGraphics::HTexture m_Texture;
     };
 
-    HFontRenderer FontRendererNew(const char* file_name,
-                                  uint32_t width, uint32_t height,
-                                  uint32_t max_characters)
+    struct SFontRenderer
+    {
+        ~SFontRenderer()
+        {
+        }
 
+        SFont*                    m_Font;
+        std::vector<SFontVertex>  m_Vertices;
+        Matrix4                   m_Projection;
+        uint32_t                  m_MaxCharacters;
+    };
+
+    HFont NewFont(const void* font,
+                  uint32_t font_size)
     {
         Font* f;
-        dmDDF::Result e = dmDDF::LoadMessageFromFile(file_name,
-                                                     &dmRender_Font_DESCRIPTOR, (void**) &f);
-        assert( e == dmDDF::RESULT_OK );
+        dmDDF::Result e = dmDDF::LoadMessage<Font>(font, font_size, &f);
+        if (e != dmDDF::RESULT_OK)
+        {
+            return 0;
+        }
 
+        SFont*ret = new SFont();
+        ret->m_Font = f;
+        ret->m_Texture = dmGraphics::CreateTexture(ret->m_Font->m_ImageWidth,
+                                                   ret->m_Font->m_ImageHeight,
+                                                   dmGraphics::TEXTURE_FORMAT_LUMINANCE);
+
+        dmGraphics::SetTextureData(ret->m_Texture, 0, ret->m_Font->m_ImageWidth, ret->m_Font->m_ImageHeight, 0,
+                                   dmGraphics::TEXTURE_FORMAT_LUMINANCE, &ret->m_Font->m_ImageData[0], ret->m_Font->m_ImageData.m_Count);
+
+        return ret;
+    }
+
+    void DeleteFont(HFont font)
+    {
+        delete font;
+    }
+
+    HFontRenderer NewFontRenderer(HFont font,
+                                  uint32_t width, uint32_t height,
+                                  uint32_t max_characters)
+    {
         SFontRenderer* fr = new SFontRenderer();
-        fr->m_Font = f;
+        fr->m_Font = font;
         fr->m_Projection = Matrix4::orthographic( 0, width, height, 0, 10, -10 );
         fr->m_Vertices.reserve(max_characters * 6);
-        fr->m_Texture = dmGraphics::CreateTexture(fr->m_Font->m_ImageWidth, fr->m_Font->m_ImageHeight, dmGraphics::TEXTURE_FORMAT_LUMINANCE);
         fr->m_MaxCharacters = max_characters;
-
-        dmGraphics::SetTextureData(fr->m_Texture, 0, fr->m_Font->m_ImageWidth, fr->m_Font->m_ImageHeight, 0,
-                          dmGraphics::TEXTURE_FORMAT_LUMINANCE, &fr->m_Font->m_ImageData[0], fr->m_Font->m_ImageData.m_Count);
         return fr;
     }
 
-    void FontRendererDelete(HFontRenderer renderer)
+    void DeleteFontRenderer(HFontRenderer renderer)
     {
         delete renderer;
     }
@@ -68,7 +99,7 @@ namespace dmRender
         {
             char c = string[i];
 
-            Font::Glyph g = renderer->m_Font->m_Glyphs[c];
+            Font::Glyph g = renderer->m_Font->m_Font->m_Glyphs[c];
 
             SFontVertex v1, v2, v3, v4;
 
@@ -88,8 +119,8 @@ namespace dmRender
             v4.m_Position[1] = y + g.m_Height;
             v4.m_Position[2] = 0;
 
-            float im_recip = 1.0f / renderer->m_Font->m_ImageWidth;
-            float ih_recip = 1.0f / renderer->m_Font->m_ImageHeight;
+            float im_recip = 1.0f / renderer->m_Font->m_Font->m_ImageWidth;
+            float ih_recip = 1.0f / renderer->m_Font->m_Font->m_ImageHeight;
 
             v1.m_UV[0] = (g.m_X) * im_recip;
             v1.m_UV[1] = (g.m_Y) * ih_recip;
@@ -132,7 +163,7 @@ namespace dmRender
                            sizeof(SFontVertex),
                            (void*) &renderer->m_Vertices[0].m_UV[0]);
 
-        dmGraphics::SetTexture(context, renderer->m_Texture);
+        dmGraphics::SetTexture(context, renderer->m_Font->m_Texture);
 
         dmGraphics::SetBlendFunc(context, dmGraphics::BLEND_FACTOR_SRC_ALPHA, dmGraphics::BLEND_FACTOR_ONE_MINUS_SRC_ALPHA);
         dmGraphics::DisableState(context, dmGraphics::DEPTH_TEST);
--- src/fontrenderer.h
+++ src/fontrenderer.h
@@ -6,17 +6,61 @@
 
 namespace dmRender
 {
+    /**
+     * Font handle
+     */
+    typedef struct SFont* HFont;
 
+    /**
+     * Create a new font
+     * @param font Font buffer (ddf format)
+     * @param font_size Font buffer size
+     * @return HFont on success. NULL on failure
+     */
+    HFont NewFont(const void* font, uint32_t font_size);
+
+    /**
+     * Delete a font
+     * @param font Font handle
+     */
+    void DeleteFont(HFont font);
+
+    /**
+     * Font render handle
+     */
     typedef struct SFontRenderer* HFontRenderer;
 
-    HFontRenderer FontRendererNew(const char* file_name,
+    /**
+     * Create a new font renderer
+     * @param font Font
+     * @param width Width
+     * @param height Height
+     * @param max_characters Max characters to render
+     * @return Font renderer handle
+     *
+     */
+    HFontRenderer NewFontRenderer(HFont font,
                                   uint32_t width, uint32_t height,
                                   uint32_t max_characters);
+    /**
+     * Deletes a font renderer
+     * @param renderer Font renderer handle
+     */
+    void DeleteFontRenderer(HFontRenderer renderer);
 
-    void FontRendererDelete(HFontRenderer renderer);
-
+    /**
+     * Draw string
+     * @param renderer Font renderer handle
+     * @param string String to render
+     * @param x0 X
+     * @param y0 Y
+     */
     void FontRendererDrawString(HFontRenderer renderer, const char* string, uint16_t x0, uint16_t y0);
 
+    /**
+     * Flush drawn characters
+     * @param renderer Font renderer handle
+     */
     void FontRendererFlush(HFontRenderer renderer);
 
 }
