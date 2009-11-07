44a251458db4619ec4bfa227ee5b3e40143d78f8
Christian Murray <chmu@CM-2.local>
2009-11-07 20:08:23 +0100

    Added ProtoCTask to waf_gamesys.py

--- src/waf_gamesys.py
+++ src/waf_gamesys.py
@@ -1,6 +1,23 @@
 import Task, TaskGen
 from TaskGen import extension
 
+def ProtoCTask(name, message_type, proto_file, input_ext, output_ext, append_to_all = False, include = '../proto'):
+    Task.simple_task_type(name, 'protoc --encode=%s -I %s %s < ${SRC} > ${TGT}' % (message_type, include, proto_file),
+                          color='PINK',
+                          after='proto_gen_py',
+                          before='cc cxx',
+                          shell=True)
+
+    @extension(input_ext)
+    def xfile(self, node):
+        obj_ext = output_ext
+        t = self.create_task(name)
+        t.set_inputs(node)
+        out = node.change_ext(obj_ext)
+        if append_to_all:
+            self.allnodes.append(out)
+        t.set_outputs(out)
+        
 Task.simple_task_type('gameobjectdesc', 'protoc --encode=dmGameObject.GameObjectPrototypeDesc -I ${DYNAMO_HOME}/share/proto ${DYNAMO_HOME}/share/proto/gameobject_ddf.proto < ${SRC} > ${TGT}',
                       color='PINK', 
                       before='cc cxx',
