07445a0c5e7261faa1b32762be7ce3c7450a5b54
Fredrik Egeberg <Freddan@fredrik-egebergs-macbook.local>
2010-01-11 19:54:02 +0100

    - Added code to render debug lines - Proper comments (doxygen) - Now uses own shaders (though they need to be set from main app)

--- src/debug/debugrenderer.cpp
+++ src/debug/debugrenderer.cpp
@@ -1,3 +1,4 @@
+#include <dlib/log.h>
 #include "debugrenderer.h"
 #include "graphics/graphics_device.h"
 
@@ -6,7 +7,24 @@ using namespace Vectormath::Aos;
 
 namespace dmRenderDebug
 {
-    static const float CubeVertices[] = {
+
+    struct State
+    {
+        State(): m_VertexProgram(0), m_FragmentProgram(0)
+        {
+
+        }
+        dmGraphics::HVertexProgram      m_VertexProgram;
+        dmGraphics::HFragmentProgram    m_FragmentProgram;
+
+        static const float              m_CubeVertices[];
+        static const int                m_CubeIndices[];
+    };
+
+
+
+    const float State::m_CubeVertices[] =
+    {
             -1, -1, -1,
             1, -1, -1,
             1, 1, -1,
@@ -14,9 +32,10 @@ namespace dmRenderDebug
             -1, -1, 1,
             1, -1, 1,
             1, 1, 1,
-            -1, 1, 1};
+            -1, 1, 1
+    };
 
-    static const int CubeIndices[] =
+    const int State::m_CubeIndices[] =
     {
             0, 1, 2,
             2, 3, 0,
@@ -32,26 +51,122 @@ namespace dmRenderDebug
             6, 7, 3
     };
 
-    void RenderCube(Point3 position, float size)
-    {
+    State   m_State;
 
+
+    static bool SetupPrograms(dmGraphics::HContext context)
+    {
+        if (m_State.m_VertexProgram == 0 || m_State.m_FragmentProgram == 0)
+        {
+            dmLogError("dmRenderDebug: No vertex/fragment program set, debug rendering disabled\n");
+            return false;
+        }
+
+        dmGraphics::SetVertexProgram(context, m_State.m_VertexProgram);
+        dmGraphics::SetFragmentProgram(context, m_State.m_FragmentProgram);
+
+        return true;
+    }
+
+    void SetupMatrices(dmGraphics::HContext context, Matrix4* view_proj, Matrix4* mat)
+    {
+        if (view_proj) dmGraphics::SetVertexConstantBlock(context, (const Vector4*)view_proj, 0, 4);
+        if (mat)       dmGraphics::SetVertexConstantBlock(context, (const Vector4*)mat, 4, 4);
+    }
+
+    void SetFragmentProgram(dmGraphics::HFragmentProgram program)
+    {
+        m_State.m_FragmentProgram = program;
+    }
+
+    void SetVertexProgram(dmGraphics::HVertexProgram program)
+    {
+        m_State.m_VertexProgram = program;
+    }
+
+
+    void Square(Matrix4 view_proj, Point3 position, Vector3 size, Vector4 color)
+    {
         dmGraphics::HContext context = dmGraphics::GetContext();
 
-        Vector4 v(1.0, 1.0, 1.0, 0.0);
-        dmGraphics::SetFragmentConstant(context, &v, 0);
+        if (!SetupPrograms(context)) return;
+
+        dmGraphics::SetFragmentConstant(context, &color, 0);
+
+        size.setZ(0.0f);
+        Matrix4 mat = Matrix4::scale(size*0.5f);
+        mat.setTranslation(Vector3(position) );
+
+        SetupMatrices(context, &view_proj, &mat);
+
+        dmGraphics::SetVertexStream(context, 0, 3, dmGraphics::TYPE_FLOAT, 0, (void*) &State::m_CubeVertices[0]);
+        dmGraphics::DisableVertexStream(context, 1);
+        dmGraphics::DisableVertexStream(context, 2);
+
+        dmGraphics::DrawElements(context, dmGraphics::PRIMITIVE_TRIANGLES, 3*12, dmGraphics::TYPE_UNSIGNED_INT, (void*) &State::m_CubeIndices[0]);
+    }
+
+    void Cube(Point3 position, float size, Vector4 color)
+    {
+        dmGraphics::HContext context = dmGraphics::GetContext();
+
+        if (!SetupPrograms(context)) return;
+
+        dmGraphics::SetFragmentConstant(context, &color, 0);
 
         Matrix4 mat = Matrix4::scale(Vector3(size));
         mat.setTranslation(Vector3(position) );
 
-        dmGraphics::SetVertexConstantBlock(context, (const Vector4*)&mat, 4, 4);
+        SetupMatrices(context, 0x0, &mat);
 
-
-        dmGraphics::SetVertexStream(context, 0, 3, dmGraphics::TYPE_FLOAT, 0, (void*) &CubeVertices[0]);
+        dmGraphics::SetVertexStream(context, 0, 3, dmGraphics::TYPE_FLOAT, 0, (void*) &State::m_CubeVertices[0]);
         dmGraphics::DisableVertexStream(context, 1);
         dmGraphics::DisableVertexStream(context, 2);
 
-        dmGraphics::DrawElements(context, dmGraphics::PRIMITIVE_TRIANGLES, 3*12, dmGraphics::TYPE_UNSIGNED_INT, (void*) &CubeIndices[0]);
+        dmGraphics::DrawElements(context, dmGraphics::PRIMITIVE_TRIANGLES, 3*12, dmGraphics::TYPE_UNSIGNED_INT, (void*) &State::m_CubeIndices[0]);
+    }
 
+
+    void Line(Matrix4 view_proj, Point3 start, Point3 end, Vector4 color)
+    {
+        dmGraphics::HContext context = dmGraphics::GetContext();
+
+        if (!SetupPrograms(context)) return;
+
+        dmGraphics::SetFragmentConstant(context, &color, 0);
+
+        Matrix4 mat = Matrix4::identity();
+
+        SetupMatrices(context, &view_proj, &mat);
+
+        Point3 list[2];
+        list[0] = start;
+        list[1] = end;
+
+        dmGraphics::SetVertexStream(context, 0, 4, dmGraphics::TYPE_FLOAT, 0, (void*)&list);
+        dmGraphics::DisableVertexStream(context, 1);
+        dmGraphics::DisableVertexStream(context, 2);
+
+        dmGraphics::Draw(context, dmGraphics::PRIMITIVE_LINES, 0, 2);
+    }
+
+    void Lines(Matrix4 view_proj, Point3* vertices, int vertex_count, Vector4 color)
+    {
+        dmGraphics::HContext context = dmGraphics::GetContext();
+
+        if (!SetupPrograms(context)) return;
+
+        dmGraphics::SetFragmentConstant(context, &color, 0);
+
+        Matrix4 mat = Matrix4::identity();
+
+        SetupMatrices(context, &view_proj, &mat);
+
+        dmGraphics::SetVertexStream(context, 0, 4, dmGraphics::TYPE_FLOAT, 0, (void*)vertices);
+        dmGraphics::DisableVertexStream(context, 1);
+        dmGraphics::DisableVertexStream(context, 2);
+
+        dmGraphics::Draw(context, dmGraphics::PRIMITIVE_LINE_STRIP, 0, vertex_count);
     }
 
 }
--- src/debug/debugrenderer.h
+++ src/debug/debugrenderer.h
@@ -1,14 +1,61 @@
-#ifndef __DEBUGRENDERER_H__
-#define __DEBUGRENDERER_H__
+#ifndef DEBUGRENDERER_H
+#define DEBUGRENDERER_H_
 
 #include <vectormath/cpp/vectormath_aos.h>
+#include <graphics/graphics_device.h>
 
 
+using namespace Vectormath::Aos;
 
 namespace dmRenderDebug
 {
 
-    void RenderCube(Vectormath::Aos::Point3 position, float size);
+    /**
+     * SetFragmentProgram Assign a fragment program to renderer
+     * @param program Fragment program
+     */
+    void SetFragmentProgram(dmGraphics::HFragmentProgram program);
+
+    /**
+     * SetVertexProgram Assign a vertex program to renderer
+     * @param program Vertex program
+     */
+    void SetVertexProgram(dmGraphics::HVertexProgram program);
+
+    /**
+     * Square Render debug square (2D)
+     * @param view_proj View-projection matrix
+     * @param position Position
+     * @param size Size
+     * @param color Color
+     */
+    void Square(Matrix4 view_proj, Point3 position, Vector3 size, Vector4 color);
+
+    /**
+     * Cube Render debug cube (3D)
+     * @param position Position
+     * @param size Size
+     */
+    void Cube(Point3 position, float size);
+
+    /**
+     * Line Render debug line
+     * @param view_proj View-projection matrix
+     * @param start Start point
+     * @param end End point
+     * @param color Color
+     */
+    void Line(Matrix4 view_proj, Point3 start, Point3 end, Vector4 color);
+
+    /**
+     * Lines Render debug lines (line strip)
+     * @param view_proj View-projection matrix
+     * @param vertices Points
+     * @param vertex_count Number of points (>1)
+     * @param color Color
+     */
+    void Lines(Matrix4 view_proj, Point3* vertices, int vertex_count, Vector4 color);
+
 }
 
-#endif // __DEBUGRENDERER_H__
+#endif // DEBUGRENDERER_H
