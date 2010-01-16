f35e21759c119f8aab5e85e5c91c0266a0ae0df8
Christian Murray <chmu@overrated.(none)>
2010-01-16 23:39:27 +0100

    Enable VALGRIND and compile fixes in hashlittle

--- src/dlib/lookup3.cpp
+++ src/dlib/lookup3.cpp
@@ -333,7 +333,7 @@ uint32_t hashlittle( const void *key, size_t length, uint32_t initval)
 
 #else /* make valgrind happy */
 
-    k8 = (const uint8_t *)k;
+    const uint8_t* k8 = (const uint8_t *)k;
     switch(length)
     {
     case 12: c+=k[2]; b+=k[1]; a+=k[0]; break;
@@ -517,7 +517,7 @@ void hashlittle2(
 
 #else /* make valgrind happy */
 
-    k8 = (const uint8_t *)k;
+    const uint8_t* k8 = (const uint8_t *)k;
     switch(length)
     {
     case 12: c+=k[2]; b+=k[1]; a+=k[0]; break;
@@ -693,7 +693,7 @@ uint32_t hashbig( const void *key, size_t length, uint32_t initval)
 
 #else  /* make valgrind happy */
 
-    k8 = (const uint8_t *)k;
+    const uint8_t* k8 = (const uint8_t *)k;
     switch(length)                   /* all the case statements fall through */
     {
     case 12: c+=k[2]; b+=k[1]; a+=k[0]; break;
