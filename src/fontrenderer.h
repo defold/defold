2f81f52e808003b94a1d4edcdca2ad0b585d01de
Fredrik Egeberg <Freddan@fredrik-egebergs-macbook.local>
2010-01-17 17:40:14 +0100

    Added colour setting for fontrenderer. Replaced stl::vector with dmArray

--- src/fontrenderer.h
+++ src/fontrenderer.h
@@ -109,8 +109,11 @@ namespace dmRender
      * @param string String to render
      * @param x0 X
      * @param y0 Y
+     * @param red Red color component
+     * @param green Green color component
+     * @param blue Blue color component
      */
-    void FontRendererDrawString(HFontRenderer renderer, const char* string, uint16_t x0, uint16_t y0);
+    void FontRendererDrawString(HFontRenderer renderer, const char* string, uint16_t x0, uint16_t y0, float red, float green, float blue);
 
     /**
      * Flush drawn characters
