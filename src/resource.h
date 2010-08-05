a7fd5b0a425c82403c3ad7b4f8be4f627b01c077
Christian Murray <cgmurray@gmail.com>
2010-08-05 21:23:17 +0200

    Initial import of resource (moved from gamesys)

---
+++ src/resource.h
@@ -0,0 +1,263 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#include <ddf/ddf.h>
+
+/*
+ * TODO:
+ * Add function to translate from factory to create result
+ */
+
+namespace dmResource
+{
+
+    /**
+     * Factory flags
+     */
+    #define RESOURCE_FACTORY_FLAGS_EMPTY            (0)    //!< FACTORY_FLAGS_EMPTY
+    #define RESOURCE_FACTORY_FLAGS_RELOAD_SUPPORT (1 << 0) //!< FACTORY_FLAGS_RELOAD_SUPPORT
+
+    /**
+     * Factory result
+     */
+    enum FactoryResult
+    {
+        FACTORY_RESULT_OK                        = 0,    //!< FACTORY_RESULT_OK
+        FACTORY_RESULT_INVALID_DATA              = -1,   //!< FACTORY_RESULT_INVALID_DATA
+        FACTORY_RESULT_DDF_ERROR                 = -2,   //!< FACTORY_RESULT_DDF_ERROR
+        FACTORY_RESULT_RESOURCE_NOT_FOUND        = -3,   //!< FACTORY_RESULT_RESOURCE_NOT_FOUND
+        FACTORY_RESULT_MISSING_FILE_EXTENSION    = -4,   //!< FACTORY_RESULT_MISSING_FILE_EXTENSION
+        FACTORY_RESULT_ALREADY_REGISTERED        = -5,   //!< FACTORY_RESULT_ALREADY_REGISTERED
+        FACTORY_RESULT_INVAL                     = -6,   //!< FACTORY_RESULT_INVAL
+        FACTORY_RESULT_UNKNOWN_RESOURCE_TYPE     = -7,   //!< FACTORY_RESULT_UNKNOWN_RESOURCE_TYPE
+        FACTORY_RESULT_OUT_OF_MEMORY             = -8,   //!< FACTORY_RESULT_OUT_OF_MEMORY
+        FACTORY_RESULT_IO_ERROR                  = -9,   //!< FACTORY_RESULT_IO_ERROR
+        FACTORY_RESULT_NOT_LOADED                = -10,  //!< FACTORY_RESULT_NOT_LOADED
+        FACTORY_RESULT_OUT_OF_RESOURCES          = -11,  //!< FACTORY_RESULT_OUT_OF_RESOURCES
+        FACTORY_RESULT_STREAMBUFFER_TOO_SMALL    = -12,  //!< FACTORY_RESULT_STREAMBUFFER_TOO_SMALL
+        FACTORY_RESULT_UNKNOWN                   = -1000,//!< FACTORY_RESULT_UNKNOWN
+    };
+
+    /**
+     * Resource kind
+     */
+    enum Kind
+    {
+        KIND_DDF_DATA,//!< KIND_DDF_DATA
+        KIND_POINTER, //!< KIND_POINTER
+    };
+
+    /// Resource descriptor
+    struct SResourceDescriptor
+    {
+        /// Hash of resource name
+        uint64_t m_NameHash;
+
+        /// Resource kind
+        Kind     m_ResourceKind;
+
+        /// Union of DDF descriptor and resource name
+        union
+        {
+            dmDDF::Descriptor* m_Descriptor;
+            const char*        m_ResourceTypeName;
+        };
+
+        /// Resource pointer. Must be unique and not NULL.
+        void*    m_Resource;
+
+        /// Reference count
+        uint32_t m_ReferenceCount;
+
+        /// For internal use only
+        void*    m_ResourceType;        // For internal use.
+        uint32_t m_ModificationTime;    // File modification time. For internal use.
+    };
+
+    /**
+     * Factory handle
+     */
+    typedef struct SResourceFactory* HFactory;
+
+    /**
+     * Create result
+     */
+    enum CreateResult
+    {
+        CREATE_RESULT_OK                = 0,    //!< CREATE_RESULT_OK
+        CREATE_RESULT_OUT_OF_MEMORY     = -1,   //!< CREATE_RESULT_OUT_OF_MEMORY
+        CREATE_RESULT_FORMAT_ERROR      = -2,   //!< CREATE_RESULT_FORMAT_ERROR
+        CREATE_RESULT_CONSTANT_ERROR    = -3,   //!< CREATE_RESULT_CONSTANT_ERROR
+        CREATE_RESULT_UNKNOWN           = -1000,//!< CREATE_RESULT_UNKNOWN
+    };
+
+    /**
+     * Resource create function
+     * @param factory Factory handle
+     * @param context User context
+     * @param buffer Buffer
+     * @param buffer_size Buffer size
+     * @param resource Resource descriptor
+     * @return CREATE_RESULT_OK on success
+     */
+    typedef CreateResult (*FResourceCreate)(HFactory factory,
+                                           void* context,
+                                           const void* buffer, uint32_t buffer_size,
+                                           SResourceDescriptor* resource,
+                                           const char* filename);
+
+    /**
+     * Resource destroy function
+     * @param factory Factory handle
+     * @param context User context
+     * @param resource Resource handle
+     * @return CREATE_RESULT_OK on success
+     */
+    typedef CreateResult (*FResourceDestroy)(HFactory factory,
+                                            void* context,
+                                            SResourceDescriptor* resource);
+
+    /**
+     * Resource recreate function. Recreate resource in-place.
+     * @param factory Factory handle
+     * @param context User context
+     * @param buffer Buffer
+     * @param buffer_size Buffer size
+     * @param resource Resource descriptor
+     * @return CREATE_RESULT_OK on success
+     */
+    typedef CreateResult (*FResourceRecreate)(HFactory factory,
+                                              void* context,
+                                              const void* buffer, uint32_t buffer_size,
+                                              SResourceDescriptor* resource,
+                                              const char* filename);
+
+    /**
+     * Set default NewFactoryParams params
+     * @param params
+     */
+    void SetDefaultNewFactoryParams(struct NewFactoryParams* params);
+
+    /**
+     * New factory parmetes
+     */
+    struct NewFactoryParams
+    {
+        /// Maximum number of resource in factory. Default is 1024
+        uint32_t m_MaxResources;
+
+        /// Factory flags. Default is RESOURCE_FACTORY_FLAGS_EMPTY
+        uint32_t m_Flags;
+
+        /// Stream buffer size. Must be equal or greater to the largest resource file to load.
+        /// Default is 4MB (4 * 1024 * 1024)
+        uint32_t m_StreamBufferSize;
+
+        uint32_t m_Reserved[7];
+
+        NewFactoryParams()
+        {
+            SetDefaultNewFactoryParams(this);
+        }
+    };
+
+    /**
+     * Create a new factory
+     * @param params New factory parmeters
+     * @param uri Resource root uri, eg http://locahost:5000 or build/default/content
+     * @return Factory handle. NULL if out of memory or invalid uri.
+     */
+    HFactory NewFactory(NewFactoryParams* params, const char* uri);
+
+    /**
+     * Delete a factory
+     * @param factory Factory handle
+     */
+    void DeleteFactory(HFactory factory);
+
+    /**
+     * Register a resource type
+     * @param factory Factory handle
+     * @param extension File extension for resource
+     * @param context User context
+     * @param create_function Create function pointer
+     * @param destroy_function Destroy function pointer
+     * @param recreate_function Recreate function pointer. Optional, 0 if recrete is not supported.
+     * @return FACTORY_RESULT_OK on success
+     */
+    FactoryResult RegisterType(HFactory factory,
+                               const char* extension,
+                               void* context,
+                               FResourceCreate create_function,
+                               FResourceDestroy destroy_function,
+                               FResourceRecreate recreate_function);
+
+    /**
+     * Get a resource from factory
+     * @param factory Factory handle
+     * @param name Resource name
+     * @param resource Created resource
+     * @return FACTORY_RESULT_OK on success
+     */
+    FactoryResult Get(HFactory factory, const char* name, void** resource);
+
+    /**
+     * Reload all resource of specified type
+     * @param factory Resource factory
+     * @param type Resource type
+     * @return FACTORY_RESULT_OK on success
+     */
+    FactoryResult ReloadType(HFactory factory, uint32_t type);
+
+    /**
+     * Get type for resource
+     * @param factory Factory handle
+     * @param resource Resource
+     * @param type Returned type
+     * @return FACTORY_RESULT_OK on success
+     */
+    FactoryResult GetType(HFactory factory, void* resource, uint32_t* type);
+
+    /**
+     * Get type from extension
+     * @param factory Factory handle
+     * @param extension File extension
+     * @param type Returned type
+     * @return FACTORY_RESULT_OK on success
+     */
+    FactoryResult GetTypeFromExtension(HFactory factory, const char* extension, uint32_t* type);
+
+    /**
+     * Get extension from type
+     * @param factory Factory handle
+     * @param type Resource type
+     * @param extension Returned extension
+     * @return FACTORY_RESULT_OK on success
+     */
+    FactoryResult GetExtensionFromType(HFactory factory, uint32_t type, const char** extension);
+
+    /**
+     * Get resource descriptor from resource (name)
+     * @param factory Factory handle
+     * @param name Resource name
+     * @param descriptor Returned resource descriptor
+     * @return FACTORY_RESULT_OK on success
+     */
+    FactoryResult GetDescriptor(HFactory factory, const char* name, SResourceDescriptor* descriptor);
+
+    /**
+     * Acquire resource, which basically increments the reference counting on the resource.
+     * @param factory Factory handle
+     * @param resource Resource
+     */
+    void Acquire(HFactory factory, void* resource);
+
+    /**
+     * Release resource
+     * @param factory Factory handle
+     * @param resource Resource
+     */
+    void Release(HFactory factory, void* resource);
+}
+
+#endif // RESOURCE_H
---
+++ wscript
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+
+VERSION='0.1'
+APPNAME='gamesys'
+
+srcdir = '.'
+blddir = 'build'
+
+import sys
+import waf_dynamo, waf_ddf
+
+def init():
+    pass
+
+def set_options(opt):
+    opt.tool_options('compiler_cc')
+    opt.tool_options('compiler_cxx')
+    opt.tool_options('waf_dynamo')
+
+def configure(conf):
+    conf.check_tool('compiler_cc')
+    conf.check_tool('compiler_cxx')
+    conf.check_tool('waf_dynamo')
+
+    waf_ddf.configure(conf)
+
+    conf.sub_config('src')
+
+    conf.env.append_value('CPPPATH', "default/src")
+    conf.env['LIB_GTEST'] = 'gtest'
+    conf.env['STATICLIB_DLIB'] = 'dlib'
+    conf.env['STATICLIB_DDF'] = 'ddf'
+
+    platform = conf.env['PLATFORM']
+
+    if platform == "linux":
+        conf.env['LIB_PLATFORM_SOCKET'] = ''
+    elif 'darwin' in platform:
+        conf.env['LIB_PLATFORM_SOCKET'] = ''
+    else:
+        conf.env['LIB_PLATFORM_SOCKET'] = 'WS2_32'
+
+def build(bld):
+    bld.add_subdirs('src')
+
+import Build, Options
+import os, subprocess
+def shutdown():
+    if not Options.commands['build']:
+        return
+
+    # TODO: Fix support for win32
+    from Logs import warn, error
+    import urllib2, time, atexit
+
+    if sys.platform != 'win32':
+        os.system('scripts/start_http_server.sh')
+        atexit.register(os.system, 'scripts/stop_http_server.sh')
+
+        start = time.time()
+        while True:
+            if time.time() - start > 5:
+                error('HTTP server failed to start within 5 seconds')
+                sys.exit(1)
+            try:
+                urllib2.urlopen('http://localhost:6123')
+                break
+            except urllib2.URLError:
+                print('Waiting for HTTP testserver to start...')
+                sys.stdout.flush()
+                time.sleep(0.5)
+    else:
+        warn('HTTP tests not supported on Win32 yet')
+
+    waf_dynamo.run_gtests(valgrind = True)
