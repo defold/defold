88eceb1feeb8f3f7e080b30acf90d4f05ece6356
Ragnar Svensson <ragnar.svensson@gmail.com>
2010-08-15 23:32:17 +0200

    The actual platform header.. :)

---
+++ src/dlib/platform.h
@@ -0,0 +1,18 @@
+#ifndef DM_PLATFORM_H
+#define DM_PLATFORM_H
+
+#define DM_PLATFORM_LINUX   "linux"
+#define DM_PLATFORM_OSX     "osx"
+#define DM_PLATFORM_WINDOWS "windows"
+
+#if defined(__linux__)
+#define DM_PLATFORM DM_PLATFORM_LINUX
+#elif defined(__MACH__)
+#define DM_PLATFORM DM_PLATFORM_OSX
+#elif defined(__WINNT__)
+#define DM_PLATFORM DM_PLATFORM_WINDOWS
+#else
+#error "Unsupported platform"
+#endif
+
+#endif
