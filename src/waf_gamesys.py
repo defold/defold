e969dcaed4bcf6cb004d1857febd7bd5ae59dba8
Ragnar Svensson <ragnar.svensson@gmail.com>
2010-09-19 22:32:42 +0200

    Removed old tasks and fixed some things.

--- src/waf_gamesys.py
+++ src/waf_gamesys.py
@@ -1,34 +1,6 @@
 import Task, TaskGen, Utils, re, os
 from TaskGen import extension
-import waf_ddf, waf_content
-
-def ProtoCTask(name, message_type, proto_file, input_ext, output_ext, append_to_all = False, include = '../proto'):
-    task = Task.simple_task_type(name, 'protoc --encode=%s -I ${DYNAMO_HOME}/share/proto -I %s -I ${DYNAMO_HOME}/ext/include %s < ${SRC} > ${TGT}' % (message_type, include, proto_file),
-                                 color='PINK',
-                                 after='proto_gen_py',
-                                 before='cc cxx',
-                                 shell=True)
-
-    def scan(self):
-        n = self.generator.path.find_resource(Utils.subst_vars(proto_file, self.env))
-        if n:
-            deps = waf_ddf.do_scan(self, [n], include)[0]
-            deps += [n]
-            return deps, []
-        else:
-            return [], []
-
-    task.scan = scan
-
-    @extension(input_ext)
-    def xfile(self, node):
-        obj_ext = output_ext
-        t = self.create_task(name)
-        t.set_inputs(node)
-        out = node.change_ext(obj_ext)
-        if append_to_all:
-            self.allnodes.append(out)
-        t.set_outputs(out)
+from waf_content import proto_compile_task
 
 def transform_gameobject(msg):
     for c in msg.Components:
@@ -50,28 +22,14 @@ def transform_emitter(msg):
     msg.Texture.Name = msg.Texture.Name.replace('.tga', '.texture')
     return msg
 
-Task.simple_task_type('gameobjectdesc', 'protoc --encode=dmGameObject.PrototypeDesc -I ${DYNAMO_HOME}/share/proto -I ${DYNAMO_HOME}/ext/include ${DYNAMO_HOME}/share/proto/gameobject_ddf.proto < ${SRC} > ${TGT}',
-                      color='PINK',
-                      before='cc cxx',
-                      shell=True)
-
-waf_content.proto_compile_task('collection', 'gameobject_ddf_pb2', 'CollectionDesc', '.collection', '.collectionc')
-waf_content.proto_compile_task('material', 'render.material_ddf_pb2', 'material_ddf_pb2.MaterialDesc', '.material', '.materialc')
-waf_content.proto_compile_task('emitter', 'particle.particle_ddf_pb2', 'particle_ddf_pb2.Emitter', '.emitter', '.emitterc', transform_emitter)
-waf_content.proto_compile_task('model', 'render.model_ddf_pb2', 'model_ddf_pb2.ModelDesc', '.model', '.modelc', transform_model)
-waf_content.proto_compile_task('gameobject',  'gameobject_ddf_pb2', 'PrototypeDesc', '.go', '.goc', transform_gameobject)
-waf_content.proto_compile_task('convexshape',  'physics_ddf_pb2', 'ConvexShape', '.convexshape_pb', '.convexshape')
-waf_content.proto_compile_task('collisionobject',  'physics_ddf_pb2', 'CollisionObjectDesc', '.collisionobject_pb', '.collisionobject')
-waf_content.proto_compile_task('gui',  'gui_ddf_pb2', 'SceneDesc', '.gui', '.guic')
-
-@extension('.go_pb')
-def gameobjectdesc_file(self, node):
-    obj_ext = '.go'
-    task = self.create_task('gameobjectdesc')
-    task.set_inputs(node)
-    out = node.change_ext(obj_ext)
-    task.set_outputs(out)
-
+proto_compile_task('collection', 'gameobject_ddf_pb2', 'CollectionDesc', '.collection', '.collectionc')
+proto_compile_task('material', 'render.material_ddf_pb2', 'material_ddf_pb2.MaterialDesc', '.material', '.materialc')
+proto_compile_task('emitter', 'particle.particle_ddf_pb2', 'particle_ddf_pb2.Emitter', '.emitter', '.emitterc', transform_emitter)
+proto_compile_task('model', 'render.model_ddf_pb2', 'model_ddf_pb2.ModelDesc', '.model', '.modelc', transform_model)
+proto_compile_task('gameobject',  'gameobject_ddf_pb2', 'PrototypeDesc', '.go', '.goc', transform_gameobject)
+proto_compile_task('convexshape',  'physics_ddf_pb2', 'ConvexShape', '.convexshape_pb', '.convexshape')
+proto_compile_task('collisionobject',  'physics_ddf_pb2', 'CollisionObjectDesc', '.collisionobject_pb', '.collisionobject')
+proto_compile_task('gui',  'gui_ddf_pb2', 'SceneDesc', '.gui', '.guic')
 
 Task.simple_task_type('luascript', 'cat < ${SRC} > ${TGT}',
                       color='PINK',
