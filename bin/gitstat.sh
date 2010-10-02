ef667a0454294f7fe37a505ced5f82f7c168a66c
Ragnar Svensson <ragnar.svensson@gmail.com>
2010-10-02 16:35:17 +0200

    Git status script.

---
+++ bin/gitstat.sh
@@ -0,0 +1,16 @@
+if [ -f .git ]; then
+  pushd ..
+else
+  pushd .
+fi
+for dir in com.dynamo.cr.common com.dynamo.cr.editor com.dynamo.cr.server ddf dlib dmedit dynamo gameobject gamesys graphics gui hid input jetbot lua particle physics render resource script sound tools
+do
+  echo -ne '### \E[33m'
+  echo -n "$dir"
+  echo -e '\E[0m ###'
+  cd $dir
+  git branch
+  git status -s
+  cd ..
+done
+popd
