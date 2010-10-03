8cc1df96023b07905adc86a1a78e9fb0af72639d
Fredrik Egeberg <Freddan@fredrik-egebergs-macbook.local>
2010-10-03 18:12:04 +0200

    text render aspect ration corrected, will be fixed when a proper renderpass is created.

--- src/rendertypes/rendertypetext.cpp
+++ src/rendertypes/rendertypetext.cpp
@@ -43,7 +43,7 @@ namespace dmRender
         dmGraphics::SetVertexProgram(context, GetVertexProgram(font));
         dmGraphics::SetFragmentProgram(context, GetFragmentProgram(font));
 
-        Matrix4 mat = Matrix4::orthographic( 0, 960, 480, 0, 10, -10 );
+        Matrix4 mat = Matrix4::orthographic( 0, 960, 540, 0, 10, -10 );
 
         dmGraphics::SetVertexConstantBlock(context, (const Vector4*)&mat, 0, 4);
         dmGraphics::SetVertexConstantBlock(context, (const Vector4*)&ident, 4, 4);
