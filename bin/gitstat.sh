50000f8d2f5c406a2d9a458add19070a7ab9f109
Ragnar Svensson <ragnar.svensson@gmail.com>
2010-10-03 14:07:18 +0200

    Added engine lib to script.

--- bin/gitstat.sh
+++ bin/gitstat.sh
@@ -3,7 +3,7 @@ if [ -f .git ]; then
 else
   pushd .
 fi
-for dir in com.dynamo.cr.common com.dynamo.cr.editor com.dynamo.cr.server ddf dlib dmedit dynamo gameobject gamesys graphics gui hid input jetbot lua particle physics render resource script sound tools
+for dir in com.dynamo.cr.common com.dynamo.cr.editor com.dynamo.cr.server ddf dlib dmedit dynamo engine gameobject gamesys graphics gui hid input jetbot lua particle physics render resource script sound tools
 do
   echo -ne '### \E[33m'
   echo -n "$dir"
