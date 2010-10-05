d59cda3ab3bffa5cba2d70a7ec22ddbf1b46ff85
Ragnar Svensson <ragnar.svensson@gmail.com>
2010-10-05 16:24:36 +0200

    Fixed resource extensions.

--- src/waf_render.py
+++ src/waf_render.py
@@ -6,7 +6,11 @@ from waf_content import proto_compile_task
 def configure(conf):
     conf.find_file('fontc.py', var='FONTC', mandatory = True)
 
-proto_compile_task('font', 'render.render_ddf_pb2', 'render_ddf_pb2.FontDesc', '.font_pb', '.font')
+def transform_font(msg):
+    msg.Font = msg.Font.replace('.ttf', '.imagefontc')
+    return msg
+
+proto_compile_task('font', 'render.render_ddf_pb2', 'render_ddf_pb2.FontDesc', '.font', '.fontc', transform_font)
 
 #Task.simple_task_type('ttf', 'python ${FONTC} -s ${size} -o ${TGT} ${SRC}',
 Task.simple_task_type('ttf', '${JAVA} -classpath ${CLASSPATH} com.dynamo.render.Fontc ${SRC} ${size} ${TGT}',
@@ -22,7 +26,7 @@ def ttf_file(self, node):
                  self.env['DYNAMO_HOME'] + '/share/java/ddf.jar',
                  self.env['DYNAMO_HOME'] + '/share/java/render.jar',
                  self.env['DYNAMO_HOME'] + '/share/java/fontc.jar']
-    obj_ext = '.imagefont'
+    obj_ext = '.imagefontc'
     font = self.create_task('ttf')
     font.env['CLASSPATH'] = os.pathsep.join(classpath)
     self.env['size'] = str(self.size)
