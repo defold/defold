4a1075f57711a1f41fd3a0cb020402af6ba99075
Fredrik Egeberg <Freddan@fredrik-egebergs-macbook.local>
2010-10-11 21:46:17 +0200

    No more rendercontext

--- src/physics_debug_render.h
+++ src/physics_debug_render.h
@@ -5,7 +5,7 @@
 
 namespace PhysicsDebugRender
 {
-    void RenderLine(void* ctx, Vectormath::Aos::Point3 p0, Vectormath::Aos::Point3 p1, Vectormath::Aos::Vector4 color);
+    void RenderLine(Vectormath::Aos::Point3 p0, Vectormath::Aos::Point3 p1, Vectormath::Aos::Vector4 color);
 }
 
 #endif
