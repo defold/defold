a572652266cd5bcc93860273b78b7a7495b1bc6f
Christian Murray <cgmurray@gmail.com>
2010-10-31 21:29:42 +0100

    Removed Position and Rotation from ComponentDesc. Feature removed

--- src/test/main.go
+++ src/test/main.go
@@ -1,6 +1,4 @@
 Components
 {
     Resource: "main.script"
-    Position: { X: 0 Y: 0 Z: 0 }
-    Rotation: { X: 0 Y: 0 Z: 0 W: 1 }
 }
--- src/test/target.go
+++ src/test/target.go
@@ -1,12 +1,8 @@
 Components
 {
     Resource: "target.script"
-    Position: { X: 0 Y: 0 Z: 0 }
-    Rotation: { X: 0 Y: 0 Z: 0 W: 1 }
 }
 Components
 {
     Resource: "target.collisionobject"
-    Position: { X: 0 Y: 0 Z: 0 }
-    Rotation: { X: 0 Y: 0 Z: 0 W: 1 }
 }
