fcfecfe81b0030e66ce625841ff50ed393a2205b
Ragnar Svensson <ragnar.svensson@gmail.com>
2010-11-07 14:48:20 +0100

    Fixed bug when readdressing strings.

--- src/render_script/render_script.cpp
+++ src/render_script/render_script.cpp
@@ -437,7 +437,7 @@ bail:
                         uint32_t field_type = field->m_Type;
                         if (field_type == dmDDF::TYPE_STRING)
                         {
-                            *((uintptr_t*)&data[field->m_Offset]) = (uintptr_t)data + (uintptr_t)data[field->m_Offset];
+                            *((uintptr_t*)&data[field->m_Offset]) = (uintptr_t)data + *((uintptr_t*)(data + field->m_Offset));
                         }
                     }
                     // TODO: setjmp/longjmp here... how to handle?!!! We are not running "from lua" here
