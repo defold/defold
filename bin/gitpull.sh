1b870becd1e1a212248958370c3fce1816ba8b9c
Christian Murray <cgmurray@gmail.com>
2010-11-21 18:56:32 +0100

    Changed ansi-escape to \033

--- bin/gitpull.sh
+++ bin/gitpull.sh
@@ -5,9 +5,9 @@ else
 fi
 for dir in bounce com.dynamo.cr.common com.dynamo.cr.editor com.dynamo.cr.server ddf dlib dmedit dynamo engine gameobject gamesys graphics gui hid input jetbot lua particle physics render resource script sound tools
 do
-  echo -ne '### \E[33m'
+  echo -ne '### \033[33m'
   echo -n "$dir"
-  echo -e '\E[0m ###'
+  echo -e '\033[0m ###'
   cd $dir
   git pull --rebase
   if [ $? -ne 0 ]; then
