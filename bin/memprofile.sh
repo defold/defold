df2054a0572ca51ba50e7ed85e14630670f601f5
Christian Murray <cgmurray@gmail.com>
2010-11-24 23:06:38 +0100

    Memprofile updates * Support for active allocations summary * Html report generation

---
+++ bin/memprofile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python -m memprofile $@
+
--- src/dlib/memprofile.py
+++ src/dlib/memprofile.py
@@ -1,8 +1,5 @@
 import sys, subprocess, hashlib
 
-def load_trace(f):
-    pass
-
 class Trace(object):
     def __init__(self, type, ptr, size, back_trace, back_trace_hash):
         self.type = type
@@ -17,15 +14,18 @@ class TraceSummary(object):
         self.nfree = 0
         self.malloc_total = 0
         self.free_total = 0
+        self.active_total = 0
         self.back_trace = None
 
         for t in lst:
             assert self.back_trace == None or self.back_trace == t.back_trace
             self.back_trace = t.back_trace
             if t.type == 'M':
+                assert self.nfree == 0
                 self.nmalloc += 1
                 self.malloc_total += t.size
             elif t.type == 'F':
+                assert self.nmalloc == 0
                 self.nfree += 1
                 self.free_total += t.size
             else:
@@ -65,6 +65,7 @@ def load_symbol_table(addresses, executable):
 def load(trace, executable):
     mem_profile = MemProfile()
     traces = []
+    active_allocations = {}
     for line in open(trace, 'rb'):
         type, ptr, size, trace = line.split(' ', 3)
         ptr, size = int(ptr, 16), int(size, 16)
@@ -78,6 +79,12 @@ def load(trace, executable):
         h = hashlib.md5(trace).digest()
         trace = Trace(type, ptr, size, lst, h)
         traces.append(trace)
+
+        if type == 'M':
+            active_allocations[ptr] = trace
+        elif type == 'F':
+            if ptr in active_allocations:
+                del(active_allocations[ptr])
 
     mem_profile.symbol_table = load_symbol_table(mem_profile.symbol_table.keys(), executable)
 
@@ -96,4 +103,43 @@ def load(trace, executable):
         assert not k in mem_profile.summary
         mem_profile.summary[k] = TraceSummary(lst)
 
+    for ptr, t in active_allocations.iteritems():
+        mem_profile.summary[t.back_trace_hash].active_total += t.size
+
+
     return mem_profile
+
+if __name__ == '__main__':
+    profile = load(sys.argv[2], sys.argv[1])
+
+    lst = profile.summary.values()
+    lst.sort(lambda x,y: cmp(y.malloc_total, x.malloc_total))
+    print '<html>'
+    print '<table border="1">'
+    print '<td><b>Backtrace</b></td><td><b>Allocations</b></td><td><b>Total</b></td><td><b>Active</b></td><tr/>'
+    for s in lst:
+        if s.nmalloc > 0:
+            bt = ''
+            for x in s.back_trace:
+                try:
+                    int(x, 16)
+                except ValueError:
+                    bt += x.split('(')[0] + '<br>\n'
+
+            tot = '%d B' % s.malloc_total
+            if s.malloc_total > 1024*1024:
+                tot = '%d M' % (s.malloc_total / (1024.0*1024.0))
+            elif s.malloc_total > 1024:
+                tot = '%d K' % (s.malloc_total / (1024.0))
+
+            active = '%d B' % s.active_total
+            if s.active_total > 1024*1024:
+                active = '%d M' % (s.active_total / (1024.0*1024.0))
+            elif s.active_total > 1024:
+                active = '%d K' % (s.active_total / (1024.0))
+
+            print '<td>%s</td><td>%d</td><td>%s</td><td>%s</td><tr/>' % (bt, s.nmalloc, tot, active)
+
+    print '</table>'
+    print '</html>'
+
