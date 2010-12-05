cb8fdf0723526455c7d52c93f1f51b29a99485f1
Christian Murray <cgmurray@gmail.com>
2010-12-05 23:01:52 +0100

    Endian boolean fix

--- src/dlib/endian.h
+++ src/dlib/endian.h
@@ -1,14 +1,14 @@
 #ifndef DM_ENDIAN
 #define DM_ENDIAN
 
-/// Endian. Defined to #DM_ENDIAN_LITTLE or #DM_ENDIAN_BIG
+/// Endian. Defined to #DM_ENDIAN_LITTLE || #DM_ENDIAN_BIG
 #define DM_ENDIAN
 #undef DM_ENDIAN
 
 #define DM_ENDIAN_LITTLE 0
 #define DM_ENDIAN_BIG 1
 
-#if defined(__x86_64) or defined(__x86_64__) or defined(_X86_) or defined(__i386__) or defined(_M_IX86) or defined(__LITTLE_ENDIAN__)
+#if defined(__x86_64) || defined(__x86_64__) || defined(_X86_) || defined(__i386__) || defined(_M_IX86) || defined(__LITTLE_ENDIAN__)
 #define DM_ENDIAN DM_ENDIAN_LITTLE
 #else
 #error "Unknown endian"
