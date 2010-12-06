0ddecc75e655b8245f8fb361dcc565594970bb8e
Ragnar Svensson <ragnar.svensson@gmail.com>
2010-12-07 00:15:03 +0100

    Changed profile rendering to use screen space.

--- src/profile_render.cpp
+++ src/profile_render.cpp
@@ -26,13 +26,12 @@ namespace dmProfileRender
 
     struct Context
     {
-        float                       m_Y;
-        uint32_t                    m_TextY;
-        float                       m_Barheight;
-        float                       m_Spacing;
+        uint32_t                    m_Y;
+        uint32_t                    m_BarHeight;
+        uint32_t                    m_Spacing;
         uint32_t                    m_Index;
         float                       m_TicksPerSecond;
-        float                       m_FrameX;
+        uint32_t                    m_FrameX;
         dmRender::HRenderContext    m_RenderContext;
         dmRender::HFont             m_Font;
         dmHashTable64<SampleStats>  m_SampleStats;
@@ -74,7 +73,7 @@ namespace dmProfileRender
     const int g_Sample_Time_x0 = g_Sample_x0 + 260;
     const int g_Sample_Count_x0 = g_Sample_Time_x0 + 60;
     const int g_Frame_x0 = g_Sample_Count_x0 + 65;
-    const int g_TextSpacing = 20;
+    const int g_Spacing = 20;
     const int g_Counter_x0 = 16;
     const int g_Counter_Amount_x0 = 16 + 160;
 
@@ -82,20 +81,18 @@ namespace dmProfileRender
     void ProfileSampleCallback(void* context, const dmProfile::Sample* sample)
     {
         Context* c = (Context*) context;
-        Matrix4 m = Matrix4::orthographic( -1, 1, 1, -1, 10, -10 );
 
-        const float scale = 30.0f;
+        float y = c->m_Y + sample->m_Scope->m_Index * (c->m_Spacing + c->m_BarHeight);
 
-        //float y = c->m_Y + sample->m_Depth * c->m_Spacing;
-        float y = c->m_Y + sample->m_Scope->m_Index * c->m_Spacing;
-
-        float x = c->m_FrameX + (1.0f * scale * sample->m_Start) / c->m_TicksPerSecond;
-        float w = (1.0f * scale * sample->m_Elapsed) / c->m_TicksPerSecond;
+        uint32_t frame_width = dmRender::GetDisplayWidth(c->m_RenderContext) - c->m_FrameX;
+        float frames_per_tick = 60.0f / c->m_TicksPerSecond;
+        float x = c->m_FrameX + frame_width * sample->m_Start * frames_per_tick;
+        float w = frame_width * sample->m_Elapsed * frames_per_tick;
 
         float col[3];
         HslToRgb2( (sample->m_Scope->m_Index % 16) / 16.0f, 1.0f, 0.65f, col);
 
-        dmRender::Square2d(c->m_RenderContext, x, y, x + w, y + c->m_Barheight, Vector4(col[0], col[1], col[2], 1));
+        dmRender::Square2d(c->m_RenderContext, x, y, x + w, y + c->m_BarHeight, Vector4(col[0], col[1], col[2], 1));
 
         uint64_t hash = dmHashString64(sample->m_Name);
         SampleStats* stats = c->m_SampleStats.Get(hash);
@@ -122,9 +119,8 @@ namespace dmProfileRender
     void ProfileScopeCallback(void* context, const dmProfile::Scope* scope)
     {
         Context* c = (Context*) context;
-        Matrix4 m = Matrix4::orthographic( -1, 1, 1, -1, 10, -10 );
 
-        int y = c->m_TextY + c->m_Index * g_TextSpacing;
+        int y = c->m_Y + c->m_Index * g_Spacing;
 
         float col[3];
         HslToRgb2( (scope->m_Index % 16) / 16.0f, 1.0f, 0.65f, col);
@@ -160,9 +156,8 @@ namespace dmProfileRender
             return;
 
         Context* c = (Context*) context;
-        Matrix4 m = Matrix4::orthographic( -1, 1, 1, -1, 10, -10 );
 
-        int y = c->m_TextY + c->m_Index * g_TextSpacing;
+        int y = c->m_Y + c->m_Index * g_Spacing;
 
         float col[3];
         HslToRgb2( 4/ 16.0f, 1.0f, 0.65f, col);
@@ -192,7 +187,7 @@ namespace dmProfileRender
         float e = stats->m_Elapsed / context->m_TicksPerSecond;
         if (e < 0.0001f)
             return;
-        int text_y = context->m_TextY + context->m_Index * g_TextSpacing;
+        int y = context->m_Y + context->m_Index * g_Spacing;
 
         float col[3];
         HslToRgb2( (stats->m_Sample->m_Scope->m_Index % 16) / 16.0f, 1.0f, 0.65f, col);
@@ -201,7 +196,7 @@ namespace dmProfileRender
 
         dmRender::DrawTextParams params;
         params.m_Text = buf;
-        params.m_Y = text_y;
+        params.m_Y = y;
         params.m_FaceColor = Vectormath::Aos::Vector4(col[0], col[1], col[2], 1.0f);
         params.m_ShadowColor = Vectormath::Aos::Vector4(0.0f, 0.0f, 0.0f, 1.0f);
 
@@ -222,41 +217,43 @@ namespace dmProfileRender
 
     void Draw(dmRender::HRenderContext render_context, dmRender::HFont font)
     {
-        Matrix4 m = Matrix4::orthographic(-1, 1, 1, -1, 1, -1);
+        uint32_t display_width = dmRender::GetDisplayWidth(render_context);
+        uint32_t display_height = dmRender::GetDisplayHeight(render_context);
+        dmRender::Square2d(render_context, 0.0f, 0.0f, display_width, display_height, Vector4(0.1f, 0.1f, 0.1f, 0.4f));
 
-        dmGraphics::HContext context = dmGraphics::GetContext();
-
-        dmGraphics::SetBlendFunc(context, dmGraphics::BLEND_FACTOR_SRC_ALPHA, dmGraphics::BLEND_FACTOR_ONE_MINUS_SRC_ALPHA);
-        dmGraphics::DisableState(context, dmGraphics::DEPTH_TEST);
-        dmGraphics::EnableState(context, dmGraphics::BLEND);
-
-        dmRender::Square2d(render_context, -1.0f, -1.0f, 1.0f, 1.0f, Vector4(0.1f, 0.1f, 0.1f, 0.4f));
-
-        int text_y0 = 50 - g_TextSpacing;
+        int y0 = 50 - g_Spacing;
 
         char buffer[256];
 
         dmRender::DrawTextParams params;
         params.m_Text = buffer;
-        params.m_Y = text_y0;
+        params.m_Y = y0;
         params.m_FaceColor = Vectormath::Aos::Vector4(1.0f, 1.0f, 1.0f, 1.0f);
         params.m_ShadowColor = Vectormath::Aos::Vector4(0.0f, 0.0f, 0.0f, 1.0f);
 
         bool profile_valid = true;
+        if (dmProfile::IsMaxDepthReached())
+        {
+            profile_valid = false;
+            params.m_X = g_Scope_x0;
+            params.m_Text = "Max scope depth reached!";
+            dmRender::DrawText(render_context, font, params);
+            y0 += g_Spacing;
+        }
         if (dmProfile::IsOutOfScopes())
         {
             profile_valid = false;
             params.m_X = g_Scope_x0;
-            params.m_Y = text_y0;
+            params.m_Y = y0;
             params.m_Text = "Out of scopes!";
             dmRender::DrawText(render_context, font, params);
-            text_y0 += g_TextSpacing;
+            y0 += g_Spacing;
         }
         if (dmProfile::IsOutOfSamples())
         {
             profile_valid = false;
             params.m_X = g_Scope_x0;
-            params.m_Y = text_y0;
+            params.m_Y = y0;
             params.m_Text = "Out of samples!";
             dmRender::DrawText(render_context, font, params);
         }
@@ -266,11 +263,10 @@ namespace dmProfileRender
             params.m_X = g_Scope_x0;
             dmRender::DrawText(render_context, font, params);
 
-            text_y0 += g_TextSpacing;
-            float frame_x0 = 2.0f * g_Frame_x0 / (float)dmRender::GetDisplayWidth(render_context) - 1.0f;
-            dmRender::Square2d(render_context, frame_x0, -0.85f, 1.0f, 0.15f, Vector4(0.1f, 0.1f, 0.15f, 0.4f));
+            y0 += g_Spacing;
+            dmRender::Square2d(render_context, g_Frame_x0, y0, display_width, display_height, Vector4(0.1f, 0.1f, 0.15f, 0.4f));
 
-            params.m_Y = text_y0;
+            params.m_Y = y0;
 
             params.m_Text = "Scopes:";
             params.m_X = g_Scope_x0;
@@ -295,13 +291,12 @@ namespace dmProfileRender
             dmRender::DrawText(render_context, font, params);
 
             Context ctx;
-            ctx.m_Y = -0.78f;
-            ctx.m_TextY = text_y0 + g_TextSpacing;
-            ctx.m_Barheight = 0.05f;
-            ctx.m_Spacing = 0.074f;
+            ctx.m_Y = y0 + g_Spacing;
+            ctx.m_BarHeight = 16.0f;
+            ctx.m_Spacing = 4.0f;
             ctx.m_Index = 0;
             ctx.m_TicksPerSecond = dmProfile::GetTicksPerSecond();
-            ctx.m_FrameX = frame_x0;
+            ctx.m_FrameX = g_Frame_x0;
             ctx.m_RenderContext = render_context;
             ctx.m_Font = font;
             ctx.m_SampleStats.SetCapacity(64, 256);
@@ -314,15 +309,15 @@ namespace dmProfileRender
             ctx.m_Index = 0;
             if (ctx.m_SampleStats.Size() > 0)
             {
-                ctx.m_TextY = text_y0 + g_TextSpacing;
+                ctx.m_Y = y0 + g_Spacing;
                 ctx.m_SampleStats.Iterate<Context>(ProfileSampleStatsCallback, &ctx);
             }
 
         }
         {
-            text_y0 = 300;
+            y0 = 300;
 
-            params.m_Y = text_y0;
+            params.m_Y = y0;
 
             params.m_Text = "Counters:";
             params.m_X = g_Counter_x0;
@@ -332,10 +327,9 @@ namespace dmProfileRender
             dmRender::DrawText(render_context, font, params);
 
             Context ctx;
-            ctx.m_Y = -0.78f;
-            ctx.m_TextY = text_y0 + g_TextSpacing;
-            ctx.m_Barheight = 0.05f;
-            ctx.m_Spacing = 0.074f;
+            ctx.m_Y = y0 + g_Spacing;
+            ctx.m_BarHeight = 16.0f;
+            ctx.m_Spacing = 4.0f;
             ctx.m_Index = 0;
             ctx.m_TicksPerSecond = dmProfile::GetTicksPerSecond();
             ctx.m_RenderContext = render_context;
