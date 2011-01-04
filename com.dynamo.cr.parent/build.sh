e6df7c8cd354efb2e2fcfba9ad84a9a9422c8465
Christian Murray <cgmurray@gmail.com>
2011-01-04 15:09:56 +0100

    Added creation of symbolic links to DYNAMO_HOME in build.sh

--- com.dynamo.cr.parent/build.sh
+++ com.dynamo.cr.parent/build.sh
@@ -2,4 +2,8 @@
 
 # Wrapper script for maven
 [ -z $DYNAMO_HOME ] && echo "DYNAMO_HOME not set" && exit 1
+
+ln -sfn $DYNAMO_HOME ../com.dynamo.cr.common/DYNAMO_HOME
+ln -sfn $DYNAMO_HOME ../com.dynamo.cr.contenteditor/DYNAMO_HOME
+
 $DYNAMO_HOME/ext/share/maven/bin/mvn $@
