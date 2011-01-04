f02748d3d6716c76549d236e23e32f28930e4005
Christian Murray <cgmurray@gmail.com>
2011-01-04 15:22:35 +0100

    Added some required environment variables in build.sh

--- com.dynamo.cr.parent/build.sh
+++ com.dynamo.cr.parent/build.sh
@@ -3,6 +3,13 @@
 # Wrapper script for maven
 [ -z $DYNAMO_HOME ] && echo "DYNAMO_HOME not set" && exit 1
 
+os=`uname | awk '{ print tolower($0) }'`
+
+export PATH=$DYNAMO_HOME/bin:$PATH
+export PATH=$DYNAMO_HOME/ext/bin:$DYNAMO_HOME/ext/bin/${os}:$PATH
+export LD_LIBRARY_PATH=$DYNAMO_HOME/lib:$DYNAMO_HOME/ext/lib/${os}
+export PYTHONPATH=$DYNAMO_HOME/lib/python:$DYNAMO_HOME/ext/lib/python
+
 ln -sfn $DYNAMO_HOME ../com.dynamo.cr.common/DYNAMO_HOME
 ln -sfn $DYNAMO_HOME ../com.dynamo.cr.contenteditor/DYNAMO_HOME
 
