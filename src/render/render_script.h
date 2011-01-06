d11ac18b90c0eba939d778c7eab4f75ce9c37736
Ragnar Svensson <ragnar.svensson@gmail.com>
2011-01-06 17:57:42 +0100

    Moved render script to render lib.

---
+++ src/render/render_script.h
@@ -0,0 +1,59 @@
+#ifndef DMRENDER_RENDER_SCRIPT_H
+#define DMRENDER_RENDER_SCRIPT_H
+
+#include <dlib/array.h>
+#include <render/render.h>
+
+namespace dmRender
+{
+    enum RenderScriptResult
+    {
+        RENDER_SCRIPT_RESULT_FAILED = -1,
+        RENDER_SCRIPT_RESULT_NO_FUNCTION = 0,
+        RENDER_SCRIPT_RESULT_OK = 1
+    };
+
+    enum RenderScriptFunction
+    {
+        RENDER_SCRIPT_FUNCTION_INIT,
+        RENDER_SCRIPT_FUNCTION_UPDATE,
+        RENDER_SCRIPT_FUNCTION_ONMESSAGE,
+        MAX_RENDER_SCRIPT_FUNCTION_COUNT
+    };
+
+    extern const char* RENDER_SCRIPT_FUNCTION_NAMES[MAX_RENDER_SCRIPT_FUNCTION_COUNT];
+
+    struct RenderScript
+    {
+        int m_FunctionReferences[MAX_RENDER_SCRIPT_FUNCTION_COUNT];
+    };
+    typedef RenderScript* HRenderScript;
+
+    static const uint32_t MAX_PREDICATE_COUNT = 64;
+    struct RenderScriptInstance
+    {
+        dmRender::Predicate* m_Predicates[MAX_PREDICATE_COUNT];
+        uint32_t            m_PredicateCount;
+        HRenderScript       m_RenderScript;
+        // TODO: This is needed since we are doing everything immediate atm, can probably be removed when that changes
+        dmRender::RenderContext* m_RenderContext;
+        int                 m_InstanceReference;
+        int                 m_RenderScriptDataReference;
+    };
+    typedef RenderScriptInstance* HRenderScriptInstance;
+
+
+    void    InitializeRenderScript();
+    void    FinalizeRenderScript();
+
+    HRenderScript NewRenderScript(const void* buffer, uint32_t buffer_size, const char* filename);
+    bool    ReloadRenderScript(HRenderScript render_script, const void* buffer, uint32_t buffer_size, const char* filename);
+    void    DeleteRenderScript(HRenderScript render_script);
+
+    HRenderScriptInstance   NewRenderScriptInstance(HRenderScript render_script, dmRender::HRenderContext render_context);
+    void                    DeleteRenderScriptInstance(HRenderScriptInstance render_script_instance);
+    RenderScriptResult      InitRenderScriptInstance(HRenderScriptInstance render_script_instance);
+    RenderScriptResult      UpdateRenderScriptInstance(HRenderScriptInstance render_script_instance);
+}
+
+#endif // DMRENDER_RENDER_SCRIPT_H
