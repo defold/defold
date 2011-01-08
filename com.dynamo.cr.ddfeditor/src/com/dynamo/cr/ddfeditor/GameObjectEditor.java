6ff91e511b2f9322db6f3a1385aeb2f2ee8e8ee4
Ragnar Svensson <ragnar.svensson@gmail.com>
2011-01-08 23:26:54 +0100

    Support for go-editing in property views.

--- com.dynamo.cr.ddfeditor/src/com/dynamo/cr/ddfeditor/GameObjectEditor.java
+++ com.dynamo.cr.ddfeditor/src/com/dynamo/cr/ddfeditor/GameObjectEditor.java
@@ -1,5 +1,14 @@
 package com.dynamo.cr.ddfeditor;
 
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.ListDialog;
+import org.eclipse.ui.dialogs.ResourceListSelectionDialog;
+
+import com.dynamo.gameobject.proto.GameObject.ComponentDesc;
 import com.dynamo.gameobject.proto.GameObject.PrototypeDesc;
 
 public class GameObjectEditor extends DdfEditor {
@@ -7,5 +16,38 @@ public class GameObjectEditor extends DdfEditor {
     public GameObjectEditor() {
         super(PrototypeDesc.newBuilder().buildPartial());
     }
+
+    @Override
+    public boolean hasNewValueForPath(String path) {
+        return path.equalsIgnoreCase("/components");
+    }
+
+    @Override
+    public Object getNewValueForPath(String path) {
+        if (path.equalsIgnoreCase("/components")) {
+
+            IFileEditorInput fi = (IFileEditorInput) getEditorInput();
+            IContainer contentRoot = findContentRoot(fi.getFile());
+            if (contentRoot == null) {
+                return null;
+            }
+
+            ResourceListSelectionDialog dialog = new ResourceListSelectionDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), fi.getFile().getProject(), IResource.FILE | IResource.DEPTH_INFINITE);
+            int ret = dialog.open();
+
+            if (ret == ListDialog.OK)
+            {
+                IResource r = (IResource) dialog.getResult()[0];
+                IPath fullPath = r.getFullPath();
+                String relPath = fullPath.makeRelativeTo(contentRoot.getFullPath()).toPortableString();
+                return ComponentDesc.newBuilder().setResource(relPath).build();
+            }
+
+            return null;
+        }
+        return null;
+
+    }
+
 }
 
