6a19e534de1adb8c7a5c3eb0dd00cea16c4627c2
Ragnar Svensson <ragnar.svensson@gmail.com>
2011-01-08 19:33:12 +0100

    Added demos repo.

--- bin/gitpull.sh
+++ bin/gitpull.sh
@@ -3,7 +3,7 @@ if [ -f .git -o -d .git ]; then
 else
   pushd .
 fi
-for dir in bounce com.dynamo.cr ddf dlib dmedit dynamo engine gameobject gamesys graphics gui hid input jetbot lua particle physics render resource script sound tools
+for dir in bounce com.dynamo.cr ddf demos dlib dmedit dynamo engine gameobject gamesys graphics gui hid input jetbot lua particle physics render resource script sound tools
 do
   echo -ne '### \033[33m'
   echo -n "$dir"
