d7ef0bb01af005f978d4016acf77d8a0eb05a3b2
Christian Murray <cgmurray@gmail.com>
2011-01-15 21:52:21 +0100

    Embedding of resources in .go-files (#288)

--- src/waf_gamesys.py
+++ src/waf_gamesys.py
@@ -64,10 +64,94 @@ def transform_render(msg):
         m.Material = m.Material.replace('.material', '.materialc')
     return msg
 
+def write_embedded(task):
+    try:
+        import google.protobuf.text_format
+        import gameobject_ddf_pb2
+        msg = gameobject_ddf_pb2.PrototypeDesc()
+        with open(task.inputs[0].srcpath(task.env), 'rb') as in_f:
+            google.protobuf.text_format.Merge(in_f.read(), msg)
+
+        msg = transform_gameobject(msg)
+
+        for i, c in enumerate(msg.EmbeddedComponents):
+            with open(task.outputs[i].bldpath(task.env), 'wb') as out_f:
+                out_f.write(msg.SerializeToString())
+
+        return 0
+    except google.protobuf.text_format.ParseError,e:
+        print >>sys.stderr, '%s:%s' % (task.inputs[0].srcpath(task.env), str(e))
+        return 1
+
+task = Task.task_type_from_func('write_embedded',
+                                func    = write_embedded,
+                                color   = 'RED',
+                                after='proto_gen_py',
+                                before='cc cxx')
+
+def compile_go(task):
+    try:
+        import google.protobuf.text_format
+        import gameobject_ddf_pb2
+        msg = gameobject_ddf_pb2.PrototypeDesc()
+        with open(task.inputs[0].srcpath(task.env), 'rb') as in_f:
+            google.protobuf.text_format.Merge(in_f.read(), msg)
+
+        for i, c in enumerate(msg.EmbeddedComponents):
+            with open(task.outputs[i+1].bldpath(task.env), 'wb') as out_f:
+                out_f.write(c.Data)
+
+            desc = msg.Components.add()
+            rel_path_dir = os.path.relpath(task.inputs[0].abspath(), task.generator.content_root)
+            rel_path_dir = os.path.dirname(rel_path_dir)
+            desc.Resource = rel_path_dir + '/' + task.outputs[i+1].name
+
+        msg = transform_gameobject(msg)
+        while len(msg.EmbeddedComponents) > 0:
+            del(msg.EmbeddedComponents[0])
+
+        with open(task.outputs[0].bldpath(task.env), 'wb') as out_f:
+            out_f.write(msg.SerializeToString())
+
+        return 0
+    except google.protobuf.text_format.ParseError,e:
+        print >>sys.stderr, '%s:%s' % (task.inputs[0].srcpath(task.env), str(e))
+        return 1
+
+task = Task.task_type_from_func('gameobject',
+                                func    = compile_go,
+                                color   = 'RED',
+                                after='proto_gen_py',
+                                before='cc cxx')
+
+@extension('.go')
+def gofile(self, node):
+    import google.protobuf.text_format
+    import gameobject_ddf_pb2
+    msg = gameobject_ddf_pb2.PrototypeDesc()
+    with open(node.abspath(self.env), 'rb') as in_f:
+        google.protobuf.text_format.Merge(in_f.read(), msg)
+
+    task = self.create_task('gameobject')
+    task.set_inputs(node)
+
+    embed_output_nodes = []
+    for i, c in enumerate(msg.EmbeddedComponents):
+        name = '%s_generated_%d.%s' % (node.name.split('.')[0], i, c.Type)
+        embed_node = node.parent.exclusive_build_node(name)
+        embed_output_nodes.append(embed_node)
+
+        sub_task = self.create_task(c.Type)
+        sub_task.set_inputs(embed_node)
+        out = embed_node.change_ext('.' + c.Type + 'c')
+        sub_task.set_outputs(out)
+        sub_task.set_run_after(task)
+    out = node.change_ext('.goc')
+    task.set_outputs([out] + embed_output_nodes)
+
 proto_compile_task('collection', 'gameobject_ddf_pb2', 'CollectionDesc', '.collection', '.collectionc', transform_collection)
 proto_compile_task('emitter', 'particle.particle_ddf_pb2', 'particle_ddf_pb2.Emitter', '.emitter', '.emitterc', transform_emitter)
 proto_compile_task('model', 'model_ddf_pb2', 'ModelDesc', '.model', '.modelc', transform_model)
-proto_compile_task('gameobject',  'gameobject_ddf_pb2', 'PrototypeDesc', '.go', '.goc', transform_gameobject)
 proto_compile_task('convexshape',  'physics_ddf_pb2', 'ConvexShape', '.convexshape', '.convexshapec')
 proto_compile_task('collisionobject',  'physics_ddf_pb2', 'CollisionObjectDesc', '.collisionobject', '.collisionobjectc', transform_collisionobject)
 proto_compile_task('gui',  'gui_ddf_pb2', 'SceneDesc', '.gui', '.guic', transform_gui)
