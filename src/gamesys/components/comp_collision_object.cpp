e61a8146f8d8578b40d7739cc1d77e7915f95f91
Ragnar Svensson <ragnar.svensson@gmail.com>
2011-01-15 15:39:35 +0100

    Collision callback cap.

--- src/gamesys/components/comp_collision_object.cpp
+++ src/gamesys/components/comp_collision_object.cpp
@@ -2,6 +2,7 @@
 
 #include <dlib/dstrings.h>
 #include <dlib/hash.h>
+#include <dlib/log.h>
 #include <dlib/math.h>
 
 #include <physics/physics.h>
@@ -14,6 +15,9 @@
 namespace dmGameSystem
 {
     using namespace Vectormath::Aos;
+
+    static const uint32_t MAX_COLLISION_COUNT = 64;
+    static const uint32_t MAX_CONTACT_COUNT = 128;
 
     void GetWorldTransform(void* user_data, Vectormath::Aos::Point3& position, Vectormath::Aos::Quat& rotation)
     {
@@ -104,55 +108,82 @@ namespace dmGameSystem
         return dmGameObject::CREATE_RESULT_OK;
     }
 
-    void CollisionCallback(void* user_data_a, uint16_t group_a, void* user_data_b, uint16_t group_b, void* user_data)
+    bool CollisionCallback(void* user_data_a, uint16_t group_a, void* user_data_b, uint16_t group_b, void* user_data)
     {
-        dmGameObject::HInstance instance_a = (dmGameObject::HInstance)user_data_a;
-        dmGameObject::HInstance instance_b = (dmGameObject::HInstance)user_data_b;
-        dmPhysicsDDF::CollisionMessage ddf;
-        ddf.m_Group = group_b;
-        ddf.m_OtherGameObjectId = dmGameObject::GetIdentifier(instance_b);
-        dmGameObject::PostDDFMessageTo(instance_a, 0x0, dmPhysicsDDF::CollisionMessage::m_DDFDescriptor, &ddf);
+        uint32_t* count = (uint32_t*)user_data;
+        if (*count < MAX_COLLISION_COUNT)
+        {
+            *count += 1;
 
-        // Broadcast to B components
-        ddf.m_Group = group_a;
-        ddf.m_OtherGameObjectId = dmGameObject::GetIdentifier(instance_a);
-        dmGameObject::PostDDFMessageTo(instance_b, 0x0, dmPhysicsDDF::CollisionMessage::m_DDFDescriptor, &ddf);
+            dmGameObject::HInstance instance_a = (dmGameObject::HInstance)user_data_a;
+            dmGameObject::HInstance instance_b = (dmGameObject::HInstance)user_data_b;
+            dmPhysicsDDF::CollisionMessage ddf;
+            ddf.m_Group = group_b;
+            ddf.m_OtherGameObjectId = dmGameObject::GetIdentifier(instance_b);
+            dmGameObject::PostDDFMessageTo(instance_a, 0x0, dmPhysicsDDF::CollisionMessage::m_DDFDescriptor, &ddf);
+
+            // Broadcast to B components
+            ddf.m_Group = group_a;
+            ddf.m_OtherGameObjectId = dmGameObject::GetIdentifier(instance_a);
+            dmGameObject::PostDDFMessageTo(instance_b, 0x0, dmPhysicsDDF::CollisionMessage::m_DDFDescriptor, &ddf);
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
-    void ContactPointCallback(const dmPhysics::ContactPoint& contact_point, void* user_data)
+    bool ContactPointCallback(const dmPhysics::ContactPoint& contact_point, void* user_data)
     {
-        dmGameObject::HInstance instance_a = (dmGameObject::HInstance)contact_point.m_UserDataA;
-        dmGameObject::HInstance instance_b = (dmGameObject::HInstance)contact_point.m_UserDataB;
+        uint32_t* count = (uint32_t*)user_data;
+        if (*count < MAX_CONTACT_COUNT)
+        {
+            *count += 1;
 
-        dmPhysicsDDF::ContactPointMessage ddf;
-        float mass_a = dmMath::Select(-contact_point.m_InvMassA, 0.0f, 1.0f / contact_point.m_InvMassA);
-        float mass_b = dmMath::Select(-contact_point.m_InvMassB, 0.0f, 1.0f / contact_point.m_InvMassB);
+            dmGameObject::HInstance instance_a = (dmGameObject::HInstance)contact_point.m_UserDataA;
+            dmGameObject::HInstance instance_b = (dmGameObject::HInstance)contact_point.m_UserDataB;
 
-        // Broadcast to A components
-        ddf.m_Position = contact_point.m_PositionA;
-        ddf.m_Normal = -contact_point.m_Normal;
-        ddf.m_RelativeVelocity = -contact_point.m_RelativeVelocity;
-        ddf.m_Distance = contact_point.m_Distance;
-        ddf.m_AppliedImpulse = contact_point.m_AppliedImpulse;
-        ddf.m_Mass = mass_a;
-        ddf.m_OtherMass = mass_b;
-        ddf.m_OtherGameObjectId = dmGameObject::GetIdentifier(instance_b);
-        ddf.m_Group = contact_point.m_GroupB;
-        dmGameObject::PostDDFMessageTo(instance_a, 0x0, dmPhysicsDDF::ContactPointMessage::m_DDFDescriptor, &ddf);
+            dmPhysicsDDF::ContactPointMessage ddf;
+            float mass_a = dmMath::Select(-contact_point.m_InvMassA, 0.0f, 1.0f / contact_point.m_InvMassA);
+            float mass_b = dmMath::Select(-contact_point.m_InvMassB, 0.0f, 1.0f / contact_point.m_InvMassB);
 
-        // Broadcast to B components
-        ddf.m_Position = contact_point.m_PositionB;
-        ddf.m_Normal = contact_point.m_Normal;
-        ddf.m_RelativeVelocity = contact_point.m_RelativeVelocity;
-        ddf.m_Distance = contact_point.m_Distance;
-        ddf.m_AppliedImpulse = contact_point.m_AppliedImpulse;
-        ddf.m_Mass = mass_b;
-        ddf.m_OtherMass = mass_a;
-        ddf.m_OtherGameObjectId = dmGameObject::GetIdentifier(instance_a);
-        ddf.m_Group = contact_point.m_GroupA;
-        ddf.m_LifeTime = contact_point.m_LifeTime;
-        dmGameObject::PostDDFMessageTo(instance_b, 0x0, dmPhysicsDDF::ContactPointMessage::m_DDFDescriptor, &ddf);
+            // Broadcast to A components
+            ddf.m_Position = contact_point.m_PositionA;
+            ddf.m_Normal = -contact_point.m_Normal;
+            ddf.m_RelativeVelocity = -contact_point.m_RelativeVelocity;
+            ddf.m_Distance = contact_point.m_Distance;
+            ddf.m_AppliedImpulse = contact_point.m_AppliedImpulse;
+            ddf.m_Mass = mass_a;
+            ddf.m_OtherMass = mass_b;
+            ddf.m_OtherGameObjectId = dmGameObject::GetIdentifier(instance_b);
+            ddf.m_Group = contact_point.m_GroupB;
+            dmGameObject::PostDDFMessageTo(instance_a, 0x0, dmPhysicsDDF::ContactPointMessage::m_DDFDescriptor, &ddf);
+
+            // Broadcast to B components
+            ddf.m_Position = contact_point.m_PositionB;
+            ddf.m_Normal = contact_point.m_Normal;
+            ddf.m_RelativeVelocity = contact_point.m_RelativeVelocity;
+            ddf.m_Distance = contact_point.m_Distance;
+            ddf.m_AppliedImpulse = contact_point.m_AppliedImpulse;
+            ddf.m_Mass = mass_b;
+            ddf.m_OtherMass = mass_a;
+            ddf.m_OtherGameObjectId = dmGameObject::GetIdentifier(instance_a);
+            ddf.m_Group = contact_point.m_GroupA;
+            ddf.m_LifeTime = contact_point.m_LifeTime;
+            dmGameObject::PostDDFMessageTo(instance_b, 0x0, dmPhysicsDDF::ContactPointMessage::m_DDFDescriptor, &ddf);
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
+
+    static bool g_CollisionOverflowWarning = false;
+    static bool g_ContactOverflowWarning = false;
 
     dmGameObject::UpdateResult CompCollisionObjectUpdate(dmGameObject::HCollection collection,
                          const dmGameObject::UpdateContext* update_context,
@@ -163,7 +194,33 @@ namespace dmGameSystem
             return dmGameObject::UPDATE_RESULT_OK;
         dmPhysics::HWorld physics_world = (dmPhysics::HWorld) world;
         dmPhysics::StepWorld(physics_world, update_context->m_DT);
-        dmPhysics::ForEachCollision(physics_world, CollisionCallback, 0x0, ContactPointCallback, 0x0);
+        uint32_t collision_count = 0;
+        uint32_t contact_count = 0;
+        dmPhysics::ForEachCollision(physics_world, CollisionCallback, &collision_count, ContactPointCallback, &contact_count);
+        if (collision_count >= 128)
+        {
+            if (!g_CollisionOverflowWarning)
+            {
+                dmLogWarning("Maximum number of collisions (%d) reached, messages have been lost.", MAX_COLLISION_COUNT);
+                g_CollisionOverflowWarning = true;
+            }
+        }
+        else
+        {
+            g_CollisionOverflowWarning = false;
+        }
+        if (contact_count >= 128)
+        {
+            if (!g_ContactOverflowWarning)
+            {
+                dmLogWarning("Maximum number of contacts (%d) reached, messages have been lost.", MAX_CONTACT_COUNT);
+                g_ContactOverflowWarning = true;
+            }
+        }
+        else
+        {
+            g_ContactOverflowWarning = false;
+        }
         PhysicsContext* physics_context = (PhysicsContext*)context;
         if (physics_context->m_Debug)
             dmPhysics::DebugRender(physics_world);
