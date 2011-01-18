bbb3cf314e2a368845267dac99a433fedaa5ad05
Christian Murray <cgmurray@gmail.com>
2011-01-18 23:48:39 +0100

    Added InputBinding editing support

---
+++ com.dynamo.cr.ddfeditor/src/com/dynamo/cr/ddfeditor/resourcetypes/InputBindingEditSupport.java
@@ -0,0 +1,32 @@
+package com.dynamo.cr.ddfeditor.resourcetypes;
+
+import com.dynamo.cr.editor.core.IResourceTypeEditSupport;
+import com.dynamo.input.proto.Input.Gamepad;
+import com.dynamo.input.proto.Input.GamepadTrigger;
+import com.dynamo.input.proto.Input.Key;
+import com.dynamo.input.proto.Input.KeyTrigger;
+import com.dynamo.input.proto.Input.Mouse;
+import com.dynamo.input.proto.Input.MouseTrigger;
+import com.google.protobuf.Descriptors.Descriptor;
+import com.google.protobuf.Message;
+
+public class InputBindingEditSupport implements IResourceTypeEditSupport {
+
+    public InputBindingEditSupport() {
+    }
+
+    @Override
+    public Message getTemplateMessageFor(Descriptor descriptor) {
+        if (descriptor.getFullName().equals(GamepadTrigger.getDescriptor().getFullName())) {
+            return GamepadTrigger.newBuilder().setAction("unnamed").setInput(Gamepad.GAMEPAD_RPAD_DOWN).build();
+        }
+        else if (descriptor.getFullName().equals(KeyTrigger.getDescriptor().getFullName())) {
+            return KeyTrigger.newBuilder().setAction("unnamed").setInput(Key.KEY_ENTER).build();
+        }
+        else if (descriptor.getFullName().equals(MouseTrigger.getDescriptor().getFullName())) {
+            return MouseTrigger.newBuilder().setAction("unnamed").setInput(Mouse.MOUSE_BUTTON_1).build();
+        }
+        return null;
+    }
+
+}
