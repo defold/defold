5b3e6973cdbd0a0ee156376250bc623a9326935b
Christian Murray <cgmurray@gmail.com>
2011-01-18 22:24:56 +0100

    Added javadocs for resource type classes

--- com.dynamo.cr.editor.core/src/com/dynamo/cr/editor/core/IResourceType.java
+++ com.dynamo.cr.editor.core/src/com/dynamo/cr/editor/core/IResourceType.java
@@ -4,21 +4,58 @@ import com.google.protobuf.Descriptors.Descriptor;
 import com.google.protobuf.GeneratedMessage;
 import com.google.protobuf.Message;
 
+/**
+ * Resource type description interface
+ * @author chmu
+ *
+ */
 public interface IResourceType {
 
+    /**
+     * Get resource type identifier
+     * @return identifier
+     */
     String getId();
 
+    /**
+     * Get resource type name
+     * @return name
+     */
     String getName();
 
+    /**
+     * Get resource type file extension
+     * @return file extension
+     */
     String getFileExtension();
 
+    /**
+     * Get resource type template data.
+     * @return template data. null if no template data is defined for this type.
+     */
     byte[] getTemplateData();
 
+    /**
+     * Create google protobuf message from template data.
+     * @return message. null if this resource type isn't a protobuf type or if no template data exists for this type.
+     */
     Message createTemplateMessage();
 
+    /**
+     * Get google protobuf message class
+     * @return message class. null of not defined
+     */
     Class<GeneratedMessage> getMessageClass();
 
+    /**
+     * Get google protobuf message descriptor
+     * @return descriptor. null if not defined
+     */
     Descriptor getMessageDescriptor();
 
+    /**
+     * Is the resource type embeddable in a Game Object prototype etc.
+     * @return true if embeddable
+     */
     boolean isEmbeddable();
 }
--- com.dynamo.cr.editor.core/src/com/dynamo/cr/editor/core/IResourceTypeRegistry.java
+++ com.dynamo.cr.editor.core/src/com/dynamo/cr/editor/core/IResourceTypeRegistry.java
@@ -1,9 +1,23 @@
 package com.dynamo.cr.editor.core;
 
+/**
+ * Registry of resource types
+ * @author chmu
+ *
+ */
 public interface IResourceTypeRegistry {
 
+    /**
+     * Get all resource types.
+     * @return array of all resource types
+     */
     IResourceType[] getResourceTypes();
 
-    IResourceType getResourceTypeFromExtension(String type);
+    /**
+     * Get resource type from file extension.
+     * @param extension file extension
+     * @return resource type. null if no mapping exists
+     */
+    IResourceType getResourceTypeFromExtension(String extension);
 
 }
