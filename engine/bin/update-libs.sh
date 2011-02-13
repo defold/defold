58aa54595fb74ce1c9b2ced99446c0a0d4d35b69
Christian Murray <cgmurray@gmail.com>
2011-02-13 16:48:08 +0100

    Updated update-libs.sh to work with new single repo layout

--- engine/bin/update-libs.sh
+++ engine/bin/update-libs.sh
@@ -5,33 +5,9 @@ set -e
 [ -z $DYNAMO_HOME ] && echo "DYNAMO_HOME not set" && exit 1
 
 for lib in $LIBS; do
-    echo "Checking '$lib'..."
-    if [ ! -e ../$lib ]; then
-        cd ..
-        echo $lib
-        repo="overrated.dyndns.org:/repo/$lib"
-        if [ -n "$USER_OVERRATED" ]; then
-            repo="$USER_OVERRATED@$repo"
-        fi
-        git clone $repo
-        cd $lib
-    else
-        cd ../$lib
-
-        git fetch
-        LOG=`git log origin/master..`
-        if [ ! -z "$LOG" ]; then
-            echo "You have local commits in '$lib' that needs to be pushed."
-            exit 1
-        fi
-    fi
-done
-
-for lib in $LIBS; do
-    echo "Updating and building $lib"
+    echo "Building $lib"
     cd ../$lib
 
-    git pull --rebase
     waf configure --prefix=$DYNAMO_HOME
     waf clean
 
