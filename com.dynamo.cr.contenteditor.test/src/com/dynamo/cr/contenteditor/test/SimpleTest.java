41328b59489f0ffa56bea8c193fe2dbb06987c44
Ragnar Svensson <ragnar.svensson@gmail.com>
2011-02-22 18:50:06 +0100

    Content Editor Test

---
+++ com.dynamo.cr.contenteditor.test/src/com/dynamo/cr/contenteditor/test/SimpleTest.java
@@ -0,0 +1,14 @@
+package com.dynamo.cr.contenteditor.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void test() throws Throwable {
+
+        assertTrue(true);
+    }
+}
