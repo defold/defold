bce78a5644485d6abae26fa532100413e17b35c8
Christian Murray <cgmurray@gmail.com>
2011-06-06 22:15:10 +0200

    Most basic functionality apart from invoke is implemented. ref #534

--- vscript/src/interpreter.py
+++ vscript/src/interpreter.py
@@ -1,52 +1,123 @@
 import sys
-from vscript_ddf_pb2 import VisualScript, STATEMENT_TYPE_PRINT, EXPRESSION_TYPE_ADD, EXPRESSION_TYPE_CONSTANT
+import vscript_ddf_pb2
+from vscript_ddf_pb2 import VisualScript
 from google.protobuf import text_format
-
 
 class Interpreter(object):
 
     def __init__(self, source):
         self.script = VisualScript()
         text_format.Merge(source, self.script)
+        self.script.SerializeToString()
         self.stack = []
+        self.globals = {}
+
+    def get_global(self, name):
+        return self.globals[name]
+
+    def set_global(self, name, value):
+        self.globals[name] = value
 
     def push(self, value):
         self.stack.insert(0, value)
 
     def pop(self):
-        return self.stack[0]
+        return self.stack.pop(0)
 
-    def visit_Node(self, node):
+    def visit_node(self, node):
         for stmt in node.statements:
-            self.visit_Statement(stmt)
+            self.visit_statement(stmt)
 
-    def visit_StmtPrint(self, stmt):
-        self.visit_Expression(stmt.expression)
+    # Generic statement dispatch
+    def visit_statement(self, stmt):
+        name = vscript_ddf_pb2._STATEMENTTYPE.values_by_number[stmt.type].name
+        name = name.lower()
+        method = getattr(self, 'visit_%s' % name)
+        method(stmt)
+
+    # Statements
+    def visit_if(self, stmt):
+        self.visit_expression(stmt.expression)
+        cond = self.pop()
+        if cond:
+            for stmt in stmt.statements:
+                self.visit_statement(stmt)
+
+    # Statements
+    def visit_if_else(self, stmt):
+        self.visit_expression(stmt.expression)
+        cond = self.pop()
+        if cond:
+            for stmt in stmt.statements:
+                self.visit_statement(stmt)
+        else:
+            for stmt in stmt.else_statements:
+                self.visit_statement(stmt)
+
+    def visit_set_variable(self, stmt):
+        self.visit_expression(stmt.expression)
+        self.globals[stmt.variable] = self.pop()
+
+    def visit_print(self, stmt):
+        self.visit_expression(stmt.expression)
         print self.pop()
 
-    def visit_Statement(self, stmt):
-        self.statement_methods[stmt.type](self, stmt)
+    # Generic expression dispatch
+    def visit_expression(self, expr):
+        name = vscript_ddf_pb2._EXPRESSIONTYPE.values_by_number[expr.type].name
+        name = name.lower()
+        method = getattr(self, 'visit_%s' % name)
+        method(expr)
 
-    def visit_Expression(self, expr):
-        self.expression_methods[expr.type](self, expr)
-
-    def visit_ExpressionAdd(self, expr):
-        self.visit_Expression(expr.expressions[0])
-        self.visit_Expression(expr.expressions[1])
+    # Expressions
+    def do_binary(self, expr, func):
+        self.visit_expression(expr.expressions[0])
+        self.visit_expression(expr.expressions[1])
         v2, v1 = self.pop(), self.pop()
-        self.push(v1 + v2)
+        self.push(func(v1, v2))
 
-    def visit_ExpressionConstant(self, expr):
-        self.push(expr.constant.float_value)
+    def visit_add(self, expr):
+        self.do_binary(expr, lambda x, y: x + y)
+
+    def visit_sub(self, expr):
+        self.do_binary(expr, lambda x, y: x - y)
+
+    def visit_mul(self, expr):
+        self.do_binary(expr, lambda x, y: x * y)
+
+    def visit_div(self, expr):
+        self.do_binary(expr, lambda x, y: x / y)
+
+    def visit_lt(self, expr):
+        self.do_binary(expr, lambda x, y: x < y)
+
+    def visit_le(self, expr):
+        self.do_binary(expr, lambda x, y: x <= y)
+
+    def visit_eq(self, expr):
+        self.do_binary(expr, lambda x, y: x == y)
+
+    def visit_ge(self, expr):
+        self.do_binary(expr, lambda x, y: x >= y)
+
+    def visit_gt(self, expr):
+        self.do_binary(expr, lambda x, y: x > y)
+
+    def visit_constant(self, expr):
+        if expr.constant.type.basic_type == vscript_ddf_pb2.NUMBER:
+            self.push(expr.constant.float_value)
+        elif expr.constant.type.basic_type == vscript_ddf_pb2.STRING:
+            self.push(expr.constant.string_value)
+        else:
+            assert False
+
+    def visit_load_variable(self, expr):
+        value = self.globals[expr.variable]
+        self.push(value)
 
     def run(self):
         for node in self.script.nodes:
-            self.visit_Node(node)
-
-    statement_methods = { STATEMENT_TYPE_PRINT : visit_StmtPrint }
-    expression_methods = { EXPRESSION_TYPE_ADD : visit_ExpressionAdd,
-                           EXPRESSION_TYPE_CONSTANT : visit_ExpressionConstant }
-
+            self.visit_node(node)
 
 if __name__ == '__main__':
     source = open(sys.argv[1], 'r').read()
--- vscript/src/test/test_vscript.py
+++ vscript/src/test/test_vscript.py
@@ -7,11 +7,41 @@ import sys, os
 
 class TestVScript(unittest.TestCase):
 
-    def test01(self):
-        script_source = open('src/test/data/test01.vscript', 'r').read()
+    def parse(self, name):
+        script_source = open('src/test/data/%s.vscript' % name, 'r').read()
+        return Interpreter(script_source)
 
-        interpreter = Interpreter(script_source)
+    def test01(self):
+        interpreter = self.parse('test01')
         interpreter.run()
+
+    def test_set_variable(self):
+        interpreter = self.parse('test_set_variable')
+        interpreter.run()
+        self.assertEquals(interpreter.get_global('x'), 123)
+
+    def test_get_variable(self):
+        interpreter = self.parse('test_get_variable')
+        interpreter.set_global('a_constant', 10000)
+        interpreter.run()
+        self.assertEquals(interpreter.get_global('x'), 10000)
+
+
+    def test_constants(self):
+        interpreter = self.parse('test_constants')
+        interpreter.run()
+        self.assertEquals(interpreter.get_global('number'), 123)
+        self.assertEquals(interpreter.get_global('string'), 'hello')
+
+    def test_if1(self):
+        interpreter = self.parse('test_if1')
+        interpreter.run()
+        self.assertEquals(interpreter.get_global('x'), 1010)
+
+    def test_if_else1(self):
+        interpreter = self.parse('test_if_else1')
+        interpreter.run()
+        self.assertEquals(interpreter.get_global('x'), 2)
 
 if __name__ == '__main__':
     unittest.main()
