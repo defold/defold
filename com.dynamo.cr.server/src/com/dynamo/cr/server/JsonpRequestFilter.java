aa6a88329173283a5bb7206b36ba34340bc1bea3
Christian Murray <cgmurray@gmail.com>
2011-06-30 00:01:24 +0200

    JsonpRequestFilter fix. Clear old media types and set application/json instead of text/javascript ref #571

--- com.dynamo.cr.server/src/com/dynamo/cr/server/JsonpRequestFilter.java
+++ com.dynamo.cr.server/src/com/dynamo/cr/server/JsonpRequestFilter.java
@@ -10,7 +10,9 @@ public class JsonpRequestFilter implements ContainerRequestFilter {
     @Override
     public ContainerRequest filter(ContainerRequest request) {
         if (request.getQueryParameters().get("callback") != null) {
-            request.getAcceptableMediaTypes().add(new MediaType("text", "javascript"));
+            // Force application/json for jsonp requests
+            request.getAcceptableMediaTypes().clear();
+            request.getAcceptableMediaTypes().add(MediaType.APPLICATION_JSON_TYPE);
         }
         return request;
     }
