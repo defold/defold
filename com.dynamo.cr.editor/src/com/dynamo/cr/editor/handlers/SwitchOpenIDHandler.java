2c3dc801d73d19f4be1dcda5201db3e1b25e5b09
Christian Murray <cgmurray@gmail.com>
2011-07-29 15:07:56 +0200

    Really logout from google when switching openid. ref #595

--- com.dynamo.cr.editor/src/com/dynamo/cr/editor/handlers/SwitchOpenIDHandler.java
+++ com.dynamo.cr.editor/src/com/dynamo/cr/editor/handlers/SwitchOpenIDHandler.java
@@ -3,23 +3,68 @@ package com.dynamo.cr.editor.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.handlers.HandlerUtil;
 
-import com.dynamo.cr.editor.Activator;
-import com.dynamo.cr.editor.preferences.PreferenceConstants;
+class LoginDialog extends TitleAreaDialog {
+
+    public LoginDialog(Shell parentShell) {
+        super(parentShell);
+    }
+
+    @Override
+    protected void configureShell(Shell newShell) {
+        super.configureShell(newShell);
+        newShell.setText("Sign in to your Google account");
+    }
+
+    @Override
+    protected void createButtonsForButtonBar(Composite parent) {
+        createButton(parent, IDialogConstants.OK_ID,
+                IDialogConstants.OK_LABEL, false);
+    }
+
+    @Override
+    protected Control createContents(Composite parent) {
+        Control ret = super.createContents(parent);
+        setTitle("Sign in");
+        setMessage("Sign in to your Google account. Press Ok when signed in.");
+        return ret;
+    }
+
+    @Override
+    protected boolean isResizable() {
+        return true;
+    }
+
+    @Override
+    protected Control createDialogArea(Composite parent) {
+        Browser browser = new Browser(parent, SWT.NONE);
+        browser.setUrl("https://www.google.com/accounts/Logout");
+        browser.setLayoutData(new GridData(GridData.FILL_BOTH));
+        return parent;
+    }
+
+    @Override
+    protected Point getInitialSize() {
+        return new Point(700, 800);
+    }
+}
 
 public class SwitchOpenIDHandler extends AbstractHandler {
 
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
-        IPreferenceStore store = Activator.getDefault().getPreferenceStore();
-        String authCookie = store.getString(PreferenceConstants.P_AUTH_COOKIE);
-        store.setValue(PreferenceConstants.P_AUTH_COOKIE, "");
-        boolean ret = Activator.getDefault().connectProjectClient();
-        if (!ret) {
-            // Set back auth-cookie if connectProjectClient fails
-            store.setValue(PreferenceConstants.P_AUTH_COOKIE, authCookie);
-        }
+        LoginDialog loginDialog = new LoginDialog(HandlerUtil.getActiveShell(event));
+        loginDialog.open();
         return null;
     }
 
