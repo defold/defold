bd214e353ad51a40f8053f83ace398ad3385663f
Christian Murray <cgmurray@gmail.com>
2011-07-29 15:42:42 +0200

    Added IOExceptionMapper ref #595

--- com.dynamo.cr.server/src/com/dynamo/cr/server/ResourceConfig.java
+++ com.dynamo.cr.server/src/com/dynamo/cr/server/ResourceConfig.java
@@ -26,6 +26,7 @@ public class ResourceConfig extends DefaultResourceConfig {
             classes.add(cl.loadClass("com.dynamo.cr.server.resources.ServerProvider"));
             classes.add(cl.loadClass("com.dynamo.cr.server.resources.EntityManagerFactoryProvider"));
             classes.add(cl.loadClass("com.dynamo.cr.server.resources.GitExceptionMapper"));
+            classes.add(cl.loadClass("com.dynamo.cr.server.resources.IOExceptionMapper"));
             classes.add(cl.loadClass("com.dynamo.cr.server.resources.RollbackExceptionMapper"));
             classes.add(cl.loadClass("com.dynamo.cr.server.resources.WebApplicationExceptionMapper"));
             classes.add(cl.loadClass("com.dynamo.cr.server.auth.AuthenticationExceptionMapper"));
