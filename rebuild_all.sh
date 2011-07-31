6e6833b104b45276b1f65535306c26da1711cb55
Ragnar Svensson <ragnar.svensson@gmail.com>
2011-08-01 00:11:13 +0200

    Added arg-support to the rebuild_all-script.

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -3,7 +3,7 @@ set -e
 
 for lib in "dlib" "ddf" "lua" "graphics" "sound" "physics" "hid" "resource" "script" "gameobject" "gui" "input" "render" "particle" "gamesys" "engine" ; do
 	cd $lib
-	waf distclean configure --prefix=$DYNAMO_HOME build install
+	waf distclean configure --prefix=$DYNAMO_HOME build "$@" install
 	cd ..
 	if [ $? -ne 0 ] ; then echo $? ; fi
 done
