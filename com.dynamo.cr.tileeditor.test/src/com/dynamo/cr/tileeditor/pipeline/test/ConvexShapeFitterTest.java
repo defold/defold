d6a030f281bfb45bd3bbc95b3fea759988fadd5c
Christian Murray <cgmurray@gmail.com>
2011-08-28 13:59:31 +0200

    Tile editor tests fixes ref #664 * Removed ham crest plugin. Hamcrest conflicts with junit etc * Use JUnitMatchers.hasItem instead

--- com.dynamo.cr.tileeditor.test/src/com/dynamo/cr/tileeditor/pipeline/test/ConvexShapeFitterTest.java
+++ com.dynamo.cr.tileeditor.test/src/com/dynamo/cr/tileeditor/pipeline/test/ConvexShapeFitterTest.java
@@ -1,7 +1,8 @@
 package com.dynamo.cr.tileeditor.pipeline.test;
 
-import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.hasItem;
 
 import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
@@ -11,12 +12,18 @@ import java.util.HashSet;
 
 import javax.imageio.ImageIO;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.dynamo.cr.tileeditor.pipeline.ConvexHull2D;
 import com.dynamo.cr.tileeditor.pipeline.ConvexHull2D.Point;
 
 public class ConvexShapeFitterTest {
+
+    @Before
+    public void setUp() {
+        System.setProperty("java.awt.headless", "true");
+    }
 
     static HashSet<Point> calc(String fileName) throws IOException {
         BufferedImage image = ImageIO.read(new FileInputStream(fileName));
@@ -31,7 +38,7 @@ public class ConvexShapeFitterTest {
     @Test
     public void test45() throws Exception {
         HashSet<Point> points = calc("test/test_45.png");
-        assertThat(points.size(), is(3));
+        assertEquals(3, points.size());
         assertThat(points, hasItem(new Point(0, 0)));
         assertThat(points, hasItem(new Point(7, 7)));
         assertThat(points, hasItem(new Point(7, 0)));
@@ -40,7 +47,7 @@ public class ConvexShapeFitterTest {
     @Test
     public void testCenterBox() throws Exception {
         HashSet<Point> points = calc("test/test_center_box.png");
-        assertThat(points.size(), is(4));
+        assertEquals(4, points.size());
         assertThat(points, hasItem(new Point(1, 1)));
         assertThat(points, hasItem(new Point(1, 6)));
         assertThat(points, hasItem(new Point(6, 1)));
@@ -50,7 +57,7 @@ public class ConvexShapeFitterTest {
     @Test
     public void testGround() throws Exception {
         HashSet<Point> points = calc("test/test_ground.png");
-        assertThat(points.size(), is(4));
+        assertEquals(4, points.size());
         assertThat(points, hasItem(new Point(0, 0)));
         assertThat(points, hasItem(new Point(0, 3)));
         assertThat(points, hasItem(new Point(7, 0)));
@@ -60,8 +67,7 @@ public class ConvexShapeFitterTest {
     @Test
     public void testGroundPrim() throws Exception {
         HashSet<Point> points = calc("test/test_ground_prim.png");
-        System.out.println(points);
-        assertThat(points.size(), is(6));
+        assertEquals(6, points.size());
         assertThat(points, hasItem(new Point(0, 0)));
         assertThat(points, hasItem(new Point(0, 3)));
         assertThat(points, hasItem(new Point(7, 0)));
@@ -75,8 +81,7 @@ public class ConvexShapeFitterTest {
     @Test
     public void testPoints() throws Exception {
         HashSet<Point> points = calc("test/test_points.png");
-        assertThat(points.size(), is(6));
-
+        assertEquals(6, points.size());
         assertThat(points, hasItem(new Point(1, 1)));
         assertThat(points, hasItem(new Point(2, 0)));
         assertThat(points, hasItem(new Point(1, 4)));
