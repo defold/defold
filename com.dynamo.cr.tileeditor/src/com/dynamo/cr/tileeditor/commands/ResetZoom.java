9d2d1169ce52f4844634f1190b0bdfafe4c0e179
Ragnar Svensson <ragnar.svensson@gmail.com>
2011-10-20 20:00:54 +0200

    Exposed reset zoom in grid editor. fix 180

--- com.dynamo.cr.tileeditor/src/com/dynamo/cr/tileeditor/commands/ResetZoom.java
+++ com.dynamo.cr.tileeditor/src/com/dynamo/cr/tileeditor/commands/ResetZoom.java
@@ -6,6 +6,7 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 
+import com.dynamo.cr.tileeditor.GridEditor;
 import com.dynamo.cr.tileeditor.TileSetEditor;
 
 public class ResetZoom extends AbstractHandler {
@@ -16,6 +17,9 @@ public class ResetZoom extends AbstractHandler {
         if (activeEditor instanceof TileSetEditor) {
             TileSetEditor editor = (TileSetEditor)activeEditor;
             editor.resetZoom();
+        } else if (activeEditor instanceof GridEditor) {
+            GridEditor editor = (GridEditor)activeEditor;
+            editor.getPresenter().onPreviewResetZoom();
         }
         return null;
     }
