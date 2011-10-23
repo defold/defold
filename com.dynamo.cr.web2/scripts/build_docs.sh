2b1e7c13507d7ffae8cec7bc053e3ffff5af73d1
Ragnar Svensson <ragnar.svensson@gmail.com>
2011-10-23 22:24:17 +0200

    Removed old copy-images build-step.

--- com.dynamo.cr.web2/scripts/build_docs.sh
+++ com.dynamo.cr.web2/scripts/build_docs.sh
@@ -4,5 +4,3 @@ mkdir -p war/site
 asciidoc -a data-uri -o war/site/tutorial_falling_box.html site/tutorial_falling_box.txt
 asciidoc -a data-uri -o war/site/tutorial02.html site/tutorial02.txt
 asciidoc -a data-uri -o war/site/blog.html site/blog.txt
-
-cp -r site/images/* war/images
