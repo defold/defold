788fd7cb1876e848333f7e84abc8b5869528e658
Christian Murray <cgmurray@gmail.com>
2011-10-30 17:26:47 +0100

    Implement scan function and example. case: 223

--- com.dynamo.cr.bob/src/bob.py
+++ com.dynamo.cr.bob/src/bob.py
@@ -1,5 +1,6 @@
 from glob import glob
-from os.path import splitext, join
+from os.path import splitext, join, exists, dirname, normpath
+import re, logging
 
 def exec_script(s):
     l = {}
@@ -15,10 +16,14 @@ def set_default(d, **kwargs):
 def project(**kwargs):
     return set_default(dict(kwargs),
                        globs = [],
-                       task_gens = {'.c' : c_lang})
+                       task_gens = {'.c' : c_lang},
+                       includes = [])
+
+def null_scanner(p, t): return []
 
 def task(**kwargs):
-    return set_default(dict(kwargs))
+    return set_default(dict(kwargs), dependencies = [],
+                                     scanner = null_scanner)
 
 def change_ext(p, input, ext):
     return join(p['bld_dir'], splitext(input)[0] + ext)
@@ -26,6 +31,7 @@ def change_ext(p, input, ext):
 def build(p):
     collect_inputs(p)
     create_tasks(p)
+    scan(p)
 
 def collect_inputs(p):
     '''Collection input files for project. Input files are collected
@@ -45,12 +51,55 @@ def create_tasks(p):
         except KeyError: pass
     p['tasks'] = tasks
 
+def scan(p):
+    deps = set()
+    for t in p['tasks']:
+        s = t['scanner']
+        d = s(p, t)
+        deps = deps.union(set(d))
+    t['dependencies'] = list(deps)
+
 # Builtin tasks
 
 def c_lang_file(input, output, transform):
     pass
 
+header_pattern = re.compile('.*?#include.*?[<"](.+?)[>"].*')
+def c_scanner(p, t):
+    def find_include(fname, include_paths):
+        for i in include_paths:
+            full = join(i, fname)
+            if exists(full): return normpath(full), i
+        return None, None
+
+    def scan(fname, include_paths, scanned):
+        if fname in scanned:
+            return set()
+        scanned.add(fname)
+        this_paths = [dirname(fname)] + include_paths
+
+        ret = set()
+        f = open(fname)
+        for line in f:
+            if header_pattern.match(line):
+                i = header_pattern.match(line).groups()[0]
+                qual, path = find_include(i, this_paths)
+                if qual:
+                    ret.add(qual)
+                    ret = ret.union(scan(qual, include_paths, scanned))
+                else:
+                    logging.warn('include "%s" not found in %s', i, this_paths)
+        f.close()
+        return ret
+
+    deps = set()
+    includes = list(p['includes'])
+    for i in t['inputs']:
+        deps = deps.union(scan(i, includes, set()))
+    return list(deps)
+
 def c_lang(p, input):
     return task(function = c_lang_file,
                 inputs = [input],
-                outputs = [change_ext(p, input, ".o")])
+                outputs = [change_ext(p, input, ".o")],
+                scanner = c_scanner)
--- com.dynamo.cr.bob/src/test/test_bob.py
+++ com.dynamo.cr.bob/src/test/test_bob.py
@@ -11,7 +11,8 @@ class TestBob(unittest.TestCase):
     def test_collect_inputs(self):
         script = '''
 p = project(bld_dir = "build",
-            globs = ['test_data/*.c'])
+            globs = ['test_data/*.c'],
+            includes = ['test_data/include'])
 build(p)
 '''
         l = bob.exec_script(script)
@@ -23,7 +24,8 @@ build(p)
     def test_create_tasks(self):
         script = '''
 p = project(bld_dir = "build",
-            globs = ['test_data/main.c'])
+            globs = ['test_data/main.c'],
+            includes = ['test_data/include'])
 build(p)
 '''
         l = bob.exec_script(script)
@@ -33,6 +35,27 @@ build(p)
 
         self.assertSetEquals(['test_data/main.c'], t['inputs'])
         self.assertSetEquals(['build/test_data/main.o'], t['outputs'])
+
+    def test_scan(self):
+        script = '''
+p = project(bld_dir = "build",
+            globs = ['test_data/util.c'],
+            includes = ['test_data/include'])
+build(p)
+'''
+        l = bob.exec_script(script)
+        t = l['p']['tasks'][0]
+
+        self.assertSetEquals(['test_data/util.h', 'test_data/include/misc.h'],
+                             t['dependencies'])
+
+        if False:
+            tasks = l['p']['tasks']
+            self.assertEqual(1, len(tasks))
+            t = tasks[0]
+
+            self.assertSetEquals(['test_data/main.c'], t['inputs'])
+            self.assertSetEquals(['build/test_data/main.o'], t['outputs'])
 
 if __name__ == '__main__':
     unittest.main()
