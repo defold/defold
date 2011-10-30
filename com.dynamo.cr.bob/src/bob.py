6b55e73cfacca5589ac7847e807ea1431a3bc2e2
Christian Murray <cgmurray@gmail.com>
2011-10-30 16:34:55 +0100

    Implement create-tasks. case: 222

--- com.dynamo.cr.bob/src/bob.py
+++ com.dynamo.cr.bob/src/bob.py
@@ -1,4 +1,5 @@
 from glob import glob
+from os.path import splitext, join
 
 def exec_script(s):
     l = {}
@@ -12,10 +13,19 @@ def set_default(d, **kwargs):
     return d
 
 def project(**kwargs):
-    return set_default(dict(kwargs), globs = [])
+    return set_default(dict(kwargs),
+                       globs = [],
+                       task_gens = {'.c' : c_lang})
+
+def task(**kwargs):
+    return set_default(dict(kwargs))
+
+def change_ext(p, input, ext):
+    return join(p['bld_dir'], splitext(input)[0] + ext)
 
 def build(p):
     collect_inputs(p)
+    create_tasks(p)
 
 def collect_inputs(p):
     '''Collection input files for project. Input files are collected
@@ -25,3 +35,22 @@ def collect_inputs(p):
     for g in p["globs"]:
         inputs = inputs.union(set(glob(g)))
     p['inputs'] = list(inputs)
+
+def create_tasks(p):
+    tasks = []
+    for input in p['inputs']:
+        try:
+            tg = p['task_gens'][splitext(input)[1]]
+            tasks.append(tg(p, input))
+        except KeyError: pass
+    p['tasks'] = tasks
+
+# Builtin tasks
+
+def c_lang_file(input, output, transform):
+    pass
+
+def c_lang(p, input):
+    return task(function = c_lang_file,
+                inputs = [input],
+                outputs = [change_ext(p, input, ".o")])
--- com.dynamo.cr.bob/src/test/test_bob.py
+++ com.dynamo.cr.bob/src/test/test_bob.py
@@ -5,6 +5,9 @@ import bob
 
 class TestBob(unittest.TestCase):
 
+    def assertSetEquals(self, l1, l2):
+        self.assertEquals(set(l1), set(l2))
+
     def test_collect_inputs(self):
         script = '''
 p = project(bld_dir = "build",
@@ -15,6 +18,21 @@ build(p)
         p = l['p']
         self.assertEqual(set(['test_data/main.c', 'test_data/util.c']),
                          set(p['inputs']))
+
+
+    def test_create_tasks(self):
+        script = '''
+p = project(bld_dir = "build",
+            globs = ['test_data/main.c'])
+build(p)
+'''
+        l = bob.exec_script(script)
+        tasks = l['p']['tasks']
+        self.assertEqual(1, len(tasks))
+        t = tasks[0]
+
+        self.assertSetEquals(['test_data/main.c'], t['inputs'])
+        self.assertSetEquals(['build/test_data/main.o'], t['outputs'])
 
 if __name__ == '__main__':
     unittest.main()
