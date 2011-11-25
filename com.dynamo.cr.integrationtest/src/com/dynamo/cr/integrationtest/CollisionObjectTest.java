8c5d2801f7ca61f83121d2a4a5d9e8ff987c6837
Ragnar Svensson <ragnar.svensson@gmail.com>
2011-11-25 01:07:29 +0100

    More refactoring for integration tests. case 331

--- com.dynamo.cr.integrationtest/src/com/dynamo/cr/integrationtest/CollisionObjectTest.java
+++ com.dynamo.cr.integrationtest/src/com/dynamo/cr/integrationtest/CollisionObjectTest.java
@@ -6,17 +6,14 @@ import org.eclipse.core.resources.IFile;
 import org.junit.Test;
 
 import com.dynamo.cr.ddfeditor.scene.CollisionObjectNode;
-import com.dynamo.cr.sceneed.core.ISceneView.INodeLoader;
 import com.dynamo.cr.sceneed.core.Node;
 
 public class CollisionObjectTest extends AbstractNodeTest {
 
     @Test
     public void testCollisionObject() throws Exception {
-        INodeLoader loader = nodeTypeRegistry.getLoader("collisionobject");
-
         IFile coFile = project.getFile("collision_object/kinematic_box.collisionobject");
-        CollisionObjectNode co = (CollisionObjectNode) loader.load(loaderContext, "collisionobject", coFile.getContents());
+        CollisionObjectNode co = (CollisionObjectNode) this.loaderContext.loadNode("collisionobject", coFile.getContents());
         assertNotNull(co);
         Node shape = co.getCollisionShapeNode();
         assertNotNull(shape);
