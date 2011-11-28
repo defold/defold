0e55dc76f69d5e21e69d5ce0be8680ad8f8020ab
Ragnar Svensson <ragnar.svensson@gmail.com>
2011-11-28 20:34:05 +0100

    Changed Sprite2Test into a proper unit test. case 371

--- com.dynamo.cr.tileeditor.test/src/com/dynamo/cr/tileeditor/test/Sprite2Test.java
+++ com.dynamo.cr.tileeditor.test/src/com/dynamo/cr/tileeditor/test/Sprite2Test.java
@@ -2,109 +2,36 @@ package com.dynamo.cr.tileeditor.test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 
-import com.dynamo.cr.sceneed.core.IImageProvider;
-import com.dynamo.cr.sceneed.core.IModelListener;
-import com.dynamo.cr.sceneed.core.ISceneModel;
 import com.dynamo.cr.sceneed.core.ISceneView;
-import com.dynamo.cr.sceneed.core.ISceneView.ILoaderContext;
-import com.dynamo.cr.sceneed.core.ISceneView.IPresenterContext;
-import com.dynamo.cr.sceneed.core.Node;
-import com.dynamo.cr.sceneed.core.SceneModel;
-import com.dynamo.cr.sceneed.core.ScenePresenter;
-import com.dynamo.cr.sceneed.core.test.AbstractSceneTest;
-import com.dynamo.cr.sceneed.ui.LoaderContext;
+import com.dynamo.cr.tileeditor.scene.Sprite2Loader;
 import com.dynamo.cr.tileeditor.scene.Sprite2Node;
-import com.google.inject.Module;
-import com.google.inject.Singleton;
 
-public class Sprite2Test extends AbstractSceneTest {
+public class Sprite2Test {
 
-    private ILoaderContext loaderContext;
-    private IPresenterContext presenterContext;
-    private IImageProvider imageProvider;
+    private ISceneView.ILoaderContext loaderContext;
+    private Sprite2Loader loader;
 
-    class TestModule extends GenericTestModule {
-        @Override
-        protected void configure() {
-            super.configure();
-            bind(ISceneModel.class).to(SceneModel.class).in(Singleton.class);
-            bind(ISceneView.class).toInstance(view);
-            bind(ISceneView.IPresenter.class).to(ScenePresenter.class).in(Singleton.class);
-            bind(IModelListener.class).to(ScenePresenter.class).in(Singleton.class);
-            bind(ISceneView.ILoaderContext.class).to(LoaderContext.class).in(Singleton.class);
-            bind(ISceneView.IPresenterContext.class).toInstance(presenterContext);
-            bind(IImageProvider.class).toInstance(imageProvider);
-        }
-    }
-
-    @Override
     @Before
     public void setup() throws CoreException, IOException {
-        this.view = mock(ISceneView.class);
-        this.presenterContext = mock(IPresenterContext.class);
-        this.imageProvider = mock(IImageProvider.class);
-
-        super.setup();
-        this.model = this.injector.getInstance(ISceneModel.class);
-        this.presenter = this.injector.getInstance(ISceneView.IPresenter.class);
-        this.loaderContext = this.injector.getInstance(ISceneView.ILoaderContext.class);
-
-        when(this.presenterContext.getSelection()).thenAnswer(new Answer<IStructuredSelection>() {
-            @Override
-            public IStructuredSelection answer(InvocationOnMock invocation) throws Throwable {
-                return model.getSelection();
-            }
-        });
-        when(this.presenterContext.getView()).thenReturn(this.view);
-    }
-
-    // Tests
-
-    @Test
-    public void testLoadDefault() throws Exception {
-        Sprite2Node sprite = (Sprite2Node)this.loaderContext.loadNodeFromTemplate(Sprite2Node.class);
-        assertThat(sprite.getTileSet(), is(""));
-        assertThat(sprite.getDefaultAnimation(), is(""));
+        this.loader = new Sprite2Loader();
+        this.loaderContext = mock(ISceneView.ILoaderContext.class);
     }
 
     @Test
     public void testLoad() throws Exception {
-        IFile sprite = this.contentRoot.getFile(new Path("test.sprite2"));
-        this.presenter.onLoad("sprite2", sprite.getContents());
-        Node root = this.model.getRoot();
-        assertTrue(root instanceof Sprite2Node);
-        assertTrue(this.model.getSelection().toList().contains(root));
-        verify(this.view, times(1)).setRoot(root);
-        verifySelection();
-        verifyNoClean();
-        verifyNoDirty();
-    }
-
-    @Override
-    protected Module getModule() {
-        return new TestModule();
-    }
-
-    @Override
-    protected String getBundleName() {
-        return "com.dynamo.cr.tileeditor.test";
+        String ddf = "tile_set: \"test.tileset\" default_animation: \"test\"";
+        Sprite2Node sprite = this.loader.load(this.loaderContext, new ByteArrayInputStream(ddf.getBytes()));
+        assertThat(sprite.getTileSet(), is("test.tileset"));
+        assertThat(sprite.getDefaultAnimation(), is("test"));
     }
 
 }
--- com.dynamo.cr.tileeditor/src/com/dynamo/cr/tileeditor/scene/Sprite2Loader.java
+++ com.dynamo.cr.tileeditor/src/com/dynamo/cr/tileeditor/scene/Sprite2Loader.java
@@ -2,20 +2,31 @@ package com.dynamo.cr.tileeditor.scene;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 import com.dynamo.cr.sceneed.core.ISceneView.ILoaderContext;
 import com.dynamo.cr.sceneed.core.ISceneView.INodeLoader;
+import com.dynamo.sprite2.proto.Sprite2.Sprite2Desc;
+import com.dynamo.sprite2.proto.Sprite2.Sprite2Desc.Builder;
 import com.google.protobuf.Message;
+import com.google.protobuf.TextFormat;
 
 public class Sprite2Loader implements INodeLoader<Sprite2Node> {
 
     @Override
     public Sprite2Node load(ILoaderContext context, InputStream contents)
             throws IOException, CoreException {
-        return new Sprite2Node();
+        InputStreamReader reader = new InputStreamReader(contents);
+        Builder builder = Sprite2Desc.newBuilder();
+        TextFormat.merge(reader, builder);
+        Sprite2Desc ddf = builder.build();
+        Sprite2Node node = new Sprite2Node();
+        node.setTileSet(ddf.getTileSet());
+        node.setDefaultAnimation(ddf.getDefaultAnimation());
+        return node;
     }
 
     @Override
