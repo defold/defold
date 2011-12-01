8952aa8a08d8d762391d11632e46677bb9ee6f30
Ragnar Svensson <ragnar.svensson@gmail.com>
2011-12-01 14:37:33 +0100

    Fixed sprite saving. case 333

--- com.dynamo.cr.tileeditor.test/src/com/dynamo/cr/tileeditor/test/Sprite2Test.java
+++ com.dynamo.cr.tileeditor.test/src/com/dynamo/cr/tileeditor/test/Sprite2Test.java
@@ -3,6 +3,7 @@ package com.dynamo.cr.tileeditor.test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -11,27 +12,54 @@ import org.eclipse.core.runtime.CoreException;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.dynamo.cr.sceneed.core.INodeType;
 import com.dynamo.cr.sceneed.core.ISceneView;
+import com.dynamo.cr.sceneed.core.Node;
+import com.dynamo.cr.sceneed.core.test.AbstractNodeTest;
 import com.dynamo.cr.tileeditor.scene.Sprite2Loader;
 import com.dynamo.cr.tileeditor.scene.Sprite2Node;
+import com.dynamo.sprite2.proto.Sprite2.Sprite2Desc;
 
-public class Sprite2Test {
+public class Sprite2Test extends AbstractNodeTest {
 
-    private ISceneView.ILoaderContext loaderContext;
     private Sprite2Loader loader;
+    private Sprite2Node spriteNode;
 
+    @Override
     @Before
     public void setup() throws CoreException, IOException {
         this.loader = new Sprite2Loader();
-        this.loaderContext = mock(ISceneView.ILoaderContext.class);
+        super.setup();
+
+        when(getModel().getExtension(Sprite2Node.class)).thenReturn("sprite2");
+
+        String ddf = "tile_set: \"test.tileset\" default_animation: \"test\"";
+        this.spriteNode = this.loader.load(getLoaderContext(), new ByteArrayInputStream(ddf.getBytes()));
+        this.spriteNode.setModel(getModel());
     }
 
     @Test
     public void testLoad() throws Exception {
-        String ddf = "tile_set: \"test.tileset\" default_animation: \"test\"";
-        Sprite2Node sprite = this.loader.load(this.loaderContext, new ByteArrayInputStream(ddf.getBytes()));
-        assertThat(sprite.getTileSet(), is("test.tileset"));
-        assertThat(sprite.getDefaultAnimation(), is("test"));
+        assertThat(this.spriteNode.getTileSet(), is("test.tileset"));
+        assertThat(this.spriteNode.getDefaultAnimation(), is("test"));
     }
 
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testBuildMessage() throws Exception {
+        INodeType spriteType = mock(INodeType.class);
+        // Not a nice cast, but ok since this is merely a test
+        when(spriteType.getLoader()).thenReturn((ISceneView.INodeLoader<Node>)(Object)new Sprite2Loader());
+        when(spriteType.getExtension()).thenReturn("sprite2");
+
+        when(getNodeTypeRegistry().getNodeType(Sprite2Node.class)).thenReturn(spriteType);
+
+        setNodeProperty(this.spriteNode, "tileSet", "test2.tileset");
+        setNodeProperty(this.spriteNode, "defaultAnimation", "test2");
+
+        Sprite2Desc ddf = (Sprite2Desc)this.loader.buildMessage(getLoaderContext(), this.spriteNode, null);
+
+        assertThat(ddf.getTileSet(), is("test2.tileset"));
+        assertThat(ddf.getDefaultAnimation(), is("test2"));
+    }
 }
