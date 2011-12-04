8618a236d637873e9afc9911ac35ddc979436135
Ragnar Svensson <ragnar.svensson@gmail.com>
2011-12-04 18:48:06 +0100

    Removed getMessages from background node. case 393

--- com.dynamo.cr.sceneed.ui/src/com/dynamo/cr/sceneed/ui/BackgroundNode.java
+++ com.dynamo.cr.sceneed.ui/src/com/dynamo/cr/sceneed/ui/BackgroundNode.java
@@ -1,17 +1,10 @@
 package com.dynamo.cr.sceneed.ui;
 
-import org.eclipse.osgi.util.NLS;
-
 import com.dynamo.cr.sceneed.core.Node;
 
 public class BackgroundNode extends Node {
 
     public BackgroundNode() {
-    }
-
-    @Override
-    protected Class<? extends NLS> getMessages() {
-        return null;
     }
 
 }
