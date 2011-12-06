3976d3554686d064be8c3db0cb431e605b087120
Ragnar Svensson <ragnar.svensson@gmail.com>
2011-12-07 00:49:06 +0100

    Changed go.core, go.ui => go and go.core.test => go.test

---
+++ com.dynamo.cr.go/src/com/dynamo/cr/go/core/ComponentNode.java
@@ -0,0 +1,66 @@
+package com.dynamo.cr.go.core;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.graphics.Image;
+
+import com.dynamo.cr.go.Constants;
+import com.dynamo.cr.properties.NotEmpty;
+import com.dynamo.cr.properties.Property;
+import com.dynamo.cr.sceneed.core.Node;
+
+public class ComponentNode extends Node {
+
+    @Property
+    @NotEmpty(severity = IStatus.ERROR)
+    private String id;
+
+    public ComponentNode() {
+    }
+
+    public ComponentNode(ComponentTypeNode type) {
+        addChild(type);
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        if (this.id != null ? !this.id.equals(id) : id != null) {
+            this.id = id;
+            notifyChange();
+            if (getParent() != null) {
+                ((GameObjectNode)getParent()).sortComponents();
+            }
+        }
+    }
+
+    public IStatus validateId() {
+        if (getParent() != null) {
+            for (Node sibling : getParent().getChildren()) {
+                if (sibling != this) {
+                    if (this.id.equals(((ComponentNode)sibling).getId())) {
+                        return new Status(IStatus.ERROR, Constants.PLUGIN_ID, NLS.bind(Messages.ComponentNode_id_DUPLICATED, this.id));
+                    }
+                }
+            }
+        }
+        return Status.OK_STATUS;
+    }
+
+    @Override
+    public String toString() {
+        return this.id;
+    }
+
+    @Override
+    public Image getIcon() {
+        if (hasChildren()) {
+            return this.getChildren().get(0).getIcon();
+        }
+        return super.getIcon();
+    }
+
+}
