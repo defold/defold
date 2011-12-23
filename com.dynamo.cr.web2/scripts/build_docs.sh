44049862b4eae1617ef3e848f4d82dd8d67081ef
Christian Murray <cgmurray@gmail.com>
2011-12-23 01:54:18 +0100

    Copy mp4 to war

--- com.dynamo.cr.web2/scripts/build_docs.sh
+++ com.dynamo.cr.web2/scripts/build_docs.sh
@@ -4,3 +4,10 @@ mkdir -p war/site
 asciidoc -a data-uri -o war/site/tutorial_falling_box.html site/tutorial_falling_box.txt
 asciidoc -a data-uri -o war/site/tutorial02.html site/tutorial02.txt
 asciidoc -a data-uri -o war/site/blog.html site/blog.txt
+
+files=`find site -name '*.mp4'`
+for f in $files; do
+    d=`dirname $f`
+    mkdir -p war/$d
+    cp -v $f war/$d
+done
