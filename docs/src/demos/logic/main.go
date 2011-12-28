a290afa70507649455d975d5a4b03467a69d5665
Christian Murray <cgmurray@gmail.com>
2011-12-28 22:42:36 +0100

    Demo Test Environment with one sample. case 471

---
+++ docs/src/demos/logic/main.go
@@ -0,0 +1,13 @@
+components {
+  id: "script"
+  component: "/logic/main.script"
+}
+components {
+  id: "title"
+  component: "/logic/title.gui"
+}
+embedded_components {
+  id: "demo01"
+  type: "collectionproxy"
+  data: "collection: \"/demo01/demo01.collection\"\n"
+}
