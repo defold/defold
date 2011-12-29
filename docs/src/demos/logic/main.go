068466e054fab84672320630db29cd77ba43c7cb
Christian Murray <cgmurray@gmail.com>
2011-12-29 21:54:54 +0100

    Main menu demo. case 474

--- docs/src/demos/logic/main.go
+++ docs/src/demos/logic/main.go
@@ -11,3 +11,8 @@ embedded_components {
   type: "collectionproxy"
   data: "collection: \"/demo01/demo01.collection\"\n"
 }
+embedded_components {
+  id: "main_menu"
+  type: "collectionproxy"
+  data: "collection: \"/main_menu/main.collection\"\n"
+}
---
+++ docs/src/demos/main_menu/main.go
@@ -0,0 +1,8 @@
+components {
+  id: "gui"
+  component: "/main_menu/menu.gui"
+}
+components {
+  id: "script"
+  component: "/main_menu/main.script"
+}
