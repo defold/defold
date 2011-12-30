bad8c19cd66e9f09dc3613fbe973b91c72f37af3
Christian Murray <cgmurray@gmail.com>
2011-12-30 16:41:52 +0100

    Added script to record all videos

---
+++ docs/scripts/record_all.sh
@@ -0,0 +1,9 @@
+pushd src/demos
+
+waf
+dmengine --config=demo.length=1000 --config=demo.demo=parallax "--config=demo.title=Parallax"
+dmengine --config=demo.length=700 --config=demo.demo=main_menu "--config=demo.title=Main Menu Animation"
+dmengine --config=demo.length=700 --config=demo.demo=hud "--config=demo.title=Hud"
+
+popd
+
