9a63cb9d0650c0c0484348a44d222acdf498a36f
Ragnar Svensson <ragnar.svensson@gmail.com>
2012-01-25 23:55:03 +0100

    Fixed some issues with the tutorial. case 575

--- docs/src/tutorials/side_scroller/spaceship/spaceship.go
+++ docs/src/tutorials/side_scroller/spaceship/spaceship.go
@@ -16,7 +16,7 @@ components {
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
-  data: "collision_shape: \"\"\ntype: COLLISION_OBJECT_TYPE_KINEMATIC\nmass: 0.0\nfriction: 0.1\nrestitution: 0.5\ngroup: \"player\"\nmask: \"star\"\nembedded_collision_shape {\n  shapes {\n    shape_type: TYPE_SPHERE\n    position {\n      x: 0.0\n      y: -4.5075126\n      z: 0.0\n    }\n    rotation {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n      w: 1.0\n    }\n    index: 0\n    count: 1\n  }\n  data: 25.626043\n}\n"
+  data: "collision_shape: \"\"\ntype: COLLISION_OBJECT_TYPE_KINEMATIC\nmass: 0.0\nfriction: 0.1\nrestitution: 0.5\ngroup: \"default\"\nmask: \"default\"\nembedded_collision_shape {\n  shapes {\n    shape_type: TYPE_SPHERE\n    position {\n      x: 0.0\n      y: -4.5075126\n      z: 0.0\n    }\n    rotation {\n      x: 0.0\n      y: 0.0\n      z: 0.0\n      w: 1.0\n    }\n    index: 0\n    count: 1\n  }\n  data: 25.626043\n}\n"
   position {
     x: 0.0
     y: 0.0
