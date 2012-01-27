eb7fd5dd5eaa3a1414c12427e15d7908a3b65666
Ragnar Svensson <ragnar.svensson@gmail.com>
2012-01-28 00:52:59 +0100

    Updated hero to sprite2. case 572

--- docs/src/tutorials/platformer/logic/hero/hero.go
+++ docs/src/tutorials/platformer/logic/hero/hero.go
@@ -1,14 +1,47 @@
 components {
   id: "script"
   component: "/logic/hero/hero.script"
-}
-embedded_components {
-  id: "sprite"
-  type: "sprite"
-  data: "texture: \"/graphics/hero_sheet.png\"\nwidth: 64.0\nheight: 64.0\ntile_width: 64\ntile_height: 64\ntiles_per_row: 12\ntile_count: 48\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
 }
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
   data: "collision_shape: \"/logic/hero/hero.convexshape\"\ntype: COLLISION_OBJECT_TYPE_KINEMATIC\nmass: 0.0\nfriction: 0.1\nrestitution: 0.5\ngroup: \"hero\"\nmask: \"platform\"\nmask: \"bridge\"\nmask: \"ladder\"\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite2"
+  type: "sprite2"
+  data: "tile_set: \"/logic/hero/hero.tileset\"\ndefault_animation: \"idle_right\"\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
 }
