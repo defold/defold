79ece589ff6291095f1e050e0b301bf9d7afa06f
Christian Murray <cgmurray@gmail.com>
2012-02-06 21:42:02 +0100

    Created built-ins bundle. case 627

---
+++ com.dynamo.cr.builtins/src/com/dynamo/cr/builtins/Builtins.java
@@ -0,0 +1,41 @@
+package com.dynamo.cr.builtins;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+
+public class Builtins extends Plugin {
+
+    private static Builtins plugin;
+
+    public static Builtins getDefault() {
+        return plugin;
+    }
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        super.stop(context);
+        plugin = null;
+    }
+
+    public String getBuiltins() {
+        URL bundleUrl = getBundle().getEntry("/DYNAMO_HOME/content/builtins");
+        URL fileUrl;
+        try {
+            fileUrl = FileLocator.toFileURL(bundleUrl);
+            return fileUrl.getPath();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
