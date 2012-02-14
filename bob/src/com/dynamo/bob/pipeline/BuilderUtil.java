253acc6f9bfb28c4662d328b355bf3b542001858
Christian Murray <cgmurray@gmail.com>
2012-02-14 18:20:47 +0100

    Moved bob to cr. case 640

--- bob/src/com/dynamo/bob/pipeline/BuilderUtil.java
+++
@@ -1,9 +0,0 @@
-package com.dynamo.bob.pipeline;
-
-public class BuilderUtil {
-
-    static String replaceExt(String str, String from, String to) {
-        return str.replace(from, to);
-    }
-
-}
