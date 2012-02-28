addbad0e80e99e0d8248da7b4269360ef7a2153e
Ragnar Svensson <ragnar.svensson@gmail.com>
2012-02-28 21:43:46 +0100

    Moved blog to engine/docs.

--- com.dynamo.cr.web2/scripts/build_docs.sh
+++ com.dynamo.cr.web2/scripts/build_docs.sh
@@ -3,7 +3,6 @@ export PATH=~/local/bin:$PATH
 mkdir -p war/site
 asciidoc -a data-uri -o war/site/tutorial_falling_box.html site/tutorial_falling_box.txt
 asciidoc -a data-uri -o war/site/tutorial02.html site/tutorial02.txt
-asciidoc -a data-uri -o war/site/blog.html site/blog.txt
 
 files=`find site -name '*.mp4'`
 for f in $files; do
