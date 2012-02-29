a2f708d0567a567eb6cd62ebaa6a2113b850d36a
Ragnar Svensson <ragnar.svensson@gmail.com>
2012-02-29 19:42:22 +0100

    Testing dummy authenticator.

--- com.dynamo.cr.editor/src/com/dynamo/cr/editor/Activator.java
+++ com.dynamo.cr.editor/src/com/dynamo/cr/editor/Activator.java
@@ -3,6 +3,8 @@ package com.dynamo.cr.editor;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
 import java.net.URI;
 import java.util.HashSet;
 import java.util.Set;
@@ -259,7 +261,12 @@ public class Activator extends AbstractUIPlugin implements IPropertyChangeListen
          * HTTP Basic Auth is a hack due to limitations in JGit. No support for custom HTTP
          * headers. We should probably patch JGit and remove the line below at some point.
          */
-        //Authenticator.setDefault(null);
+        Authenticator.setDefault(new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("apa", new char[] {'t', 'e', 's', 't'});
+            }
+        });
 
         /*
          * NOTE: We can't invoke getWorkbench() in start. The workbench is not started yet.
