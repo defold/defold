0cecc43a94e4ad1923b7c90de8b716361d39fd3b
Christian Murray <cgmurray@gmail.com>
2012-03-04 16:25:59 +0100

    Check for update at startup. case 698

--- com.dynamo.cr.editor/src/com/dynamo/cr/editor/ApplicationWorkbenchWindowAdvisor.java
+++ com.dynamo.cr.editor/src/com/dynamo/cr/editor/ApplicationWorkbenchWindowAdvisor.java
@@ -1,6 +1,20 @@
 package com.dynamo.cr.editor;
 
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.equinox.internal.p2.core.helpers.LogHelper;
+import org.eclipse.equinox.internal.p2.core.helpers.ServiceHelper;
+import org.eclipse.equinox.p2.core.IProvisioningAgent;
+import org.eclipse.equinox.p2.operations.UpdateOperation;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
@@ -30,5 +44,66 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
         configurer.configureEditorAreaDropListener(
                 new EditorAreaDropAdapter(
                   configurer.getWindow()));
+    }
+
+    private static final String JUSTUPDATED = "justUpdated";
+
+    private void checkForUpdates() {
+        if (System.getProperty("osgi.dev") != null) {
+            // Do not run update when running from eclipse
+            return;
+        }
+
+        final IProvisioningAgent agent = (IProvisioningAgent) ServiceHelper
+                .getService(Activator.getContext(),
+                        IProvisioningAgent.SERVICE_NAME);
+        if (agent == null) {
+            LogHelper.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID,
+                    "No provisioning agent found.  This application is not set up for updates."));
+        }
+
+        final IPreferenceStore prefStore = Activator.getDefault().getPreferenceStore();
+        if (prefStore.getBoolean(JUSTUPDATED)) {
+            prefStore.setValue(JUSTUPDATED, false);
+            return;
+        }
+
+        final IRunnableWithProgress runnable = new IRunnableWithProgress() {
+            public void run(IProgressMonitor monitor)
+                    throws InvocationTargetException, InterruptedException {
+                IProgressMonitor safeMonitor = new UIDelegateProgressMonitor(monitor, Display.getDefault());
+                IStatus updateStatus = P2Util.checkForUpdates(agent, safeMonitor);
+                if (updateStatus.getCode() == UpdateOperation.STATUS_NOTHING_TO_UPDATE) {
+                    // We do nothing, ie no dialog or similar :-)
+                } else if (updateStatus.getSeverity() != IStatus.ERROR
+                        && updateStatus.getSeverity() != IStatus.CANCEL) {
+                    prefStore.setValue(JUSTUPDATED, true);
+                    PlatformUI.getWorkbench().restart();
+                } else {
+                    LogHelper.log(updateStatus);
+                }
+            }
+        };
+
+        final Display display = Display.getCurrent();
+        display.asyncExec(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    new ProgressMonitorDialog(display.getActiveShell()).run(false, true, runnable);
+                } catch (InvocationTargetException e) {
+                    Activator.logException(e);
+                } catch (InterruptedException e) {
+                    Activator.logException(e);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void postWindowOpen() {
+        super.postWindowOpen();
+        checkForUpdates();
     }
 }
