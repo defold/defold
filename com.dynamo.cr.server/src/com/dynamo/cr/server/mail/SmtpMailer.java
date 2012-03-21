3675fc9d81cc88f5fb1824830698c9f680e53c6b
Ragnar Svensson <ragnar.svensson@gmail.com>
2012-03-21 20:29:35 +0100

    Fixed mailing through gmail. case 780

--- com.dynamo.cr.server/src/com/dynamo/cr/server/mail/SmtpMailer.java
+++ com.dynamo.cr.server/src/com/dynamo/cr/server/mail/SmtpMailer.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import javax.inject.Inject;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -14,21 +15,25 @@ import com.dynamo.cr.proto.Config.Configuration;
 
 public class SmtpMailer implements IMailer {
 
-    private String smtpHost;
-    private String userName;
-    private String password;
     private Session session;
 
     @Inject
     public SmtpMailer(Configuration configuration) {
-        smtpHost = configuration.getSmtpHost();
-        userName = configuration.getSmtpUsername();
-        password = configuration.getSmtpPassword();
+        final String smtpHost = configuration.getSmtpHost();
+        final String userName = configuration.getSmtpUsername();
+        final String password = configuration.getSmtpPassword();
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        session = Session.getInstance(props);
+        props.put("mail.smtp.host", smtpHost);
+        props.put("mail.smtp.port", "587");
+        session = Session.getInstance(props, new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(userName, password);
+            }
+        });
     }
 
     @Override
@@ -40,8 +45,6 @@ public class SmtpMailer implements IMailer {
         message.setSubject(email.getSubject());
         message.setText(email.getMessage());
 
-        Transport transport = session.getTransport("smtp");
-        transport.connect(smtpHost, 587, userName, password);
         Transport.send(message);
     }
 
