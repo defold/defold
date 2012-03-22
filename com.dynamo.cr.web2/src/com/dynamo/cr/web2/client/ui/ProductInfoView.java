275006af512bb76148c7c170e7e3f24be545ff32
Ragnar Svensson <ragnar.svensson@gmail.com>
2012-03-22 11:12:27 +0100

    Frontend for prospect registration. case 783

--- com.dynamo.cr.web2/src/com/dynamo/cr/web2/client/ui/ProductInfoView.java
+++ com.dynamo.cr.web2/src/com/dynamo/cr/web2/client/ui/ProductInfoView.java
@@ -2,8 +2,15 @@ package com.dynamo.cr.web2.client.ui;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.BlurEvent;
+import com.google.gwt.event.dom.client.BlurHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.FocusEvent;
+import com.google.gwt.event.dom.client.FocusHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -13,12 +20,19 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DeckPanel;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
-public class ProductInfoView extends Composite implements ClickHandler {
+public class ProductInfoView extends Composite implements ClickHandler, KeyPressHandler {
+
+    private static final String REGISTER_BOX_BLUR = "enter your email";
 
     private static final Binder binder = GWT.create(Binder.class);
     private static final Resources resources = GWT.create(Resources.class);
+
+    public interface Presenter {
+        void registerProspect(String email);
+    }
 
     interface Binder extends UiBinder<Widget, ProductInfoView> {
     }
@@ -36,12 +50,22 @@ public class ProductInfoView extends Composite implements ClickHandler {
 
     @UiField DeckPanel deck;
     @UiField FlowPanel buttonPanel;
+    @UiField DeckPanel registerDeck;
+    @UiField(provided = true) TextBox registerEmail;
 
+    private Presenter listener;
     private Image[] images;
     private Timer timer;
     private int activeDeck;
 
     public ProductInfoView() {
+        registerEmail = new TextBox();
+        /*
+         * NOTE: Workaround for the following bug:
+         * http://code.google.com/p/google-web-toolkit/issues/detail?id=3533 We
+         * add handler to the textbox
+         */
+        registerEmail.addKeyPressHandler(this);
         initWidget(binder.createAndBindUi(this));
 
         int n = this.deck.getWidgetCount();
@@ -60,6 +84,19 @@ public class ProductInfoView extends Composite implements ClickHandler {
                 setActiveDeck(activeDeck + 1);
             }
         };
+        registerEmail.addBlurHandler(new BlurHandler() {
+            @Override
+            public void onBlur(BlurEvent event) {
+                registerEmail.setText(REGISTER_BOX_BLUR);
+            }
+        });
+
+        registerEmail.addFocusHandler(new FocusHandler() {
+            @Override
+            public void onFocus(FocusEvent event) {
+                registerEmail.setText("");
+            }
+        });
         reset();
     }
 
@@ -68,6 +105,12 @@ public class ProductInfoView extends Composite implements ClickHandler {
         timer.cancel();
         // Schedule the timer to run once every 5 seconds.
         timer.scheduleRepeating(5000);
+        registerDeck.showWidget(0);
+        registerEmail.setText(REGISTER_BOX_BLUR);
+    }
+
+    public void setPresenter(Presenter listener) {
+        this.listener = listener;
     }
 
     public void setActiveDeck(int activeDeck) {
@@ -77,12 +120,26 @@ public class ProductInfoView extends Composite implements ClickHandler {
         deck.showWidget(this.activeDeck);
     }
 
+    public void showRegisterConfirmation() {
+        registerDeck.showWidget(1);
+    }
+
     @Override
     public void onClick(ClickEvent event) {
         for (int i = 0; i < this.images.length; ++i) {
             if (images[i] == event.getSource()) {
                 timer.cancel();
                 setActiveDeck(i);
+            }
+        }
+    }
+
+    @Override
+    public void onKeyPress(KeyPressEvent event) {
+        if (KeyCodes.KEY_ENTER == event.getNativeEvent().getKeyCode()) {
+            String prospect = this.registerEmail.getText();
+            if (!prospect.isEmpty()) {
+                listener.registerProspect(prospect);
             }
         }
     }
