b7591661d3ce3cd7ae6d023c88b8b74b5c3e49cf
Christian Murray <cgmurray@gmail.com>
2012-05-06 21:24:39 +0200

    Explicit bundling of jre on win32. case 966 * Update of jre no longer possible on win32 * See comment about bug in p2 in code

--- build-editor.sh
+++ build-editor.sh
@@ -7,7 +7,16 @@ prebuild
 gitclone
 echo 'root.linux.gtk.x86=absolute:${buildDirectory}/plugins/com.dynamo.cr.editor/jre_linux/' > build/root.properties
 echo 'root.linux.gtk.x86.permissions.755=jre/'  >> build/root.properties
-echo 'root.win32.win32.x86=absolute:${buildDirectory}/plugins/com.dynamo.cr.editor/jre_win32/' >> build/root.properties
+# NOTE:
+# Due to bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=300812
+# we cannot add jre to p2 on win32
+# The jre is explicitly bundled instead. See below
+# echo 'root.win32.win32.x86=absolute:${buildDirectory}/plugins/com.dynamo.cr.editor/jre_win32/' >> build/root.properties
 build build_editor.xml editor.properties editor
 postbuild
 
+# Explicit bundling of jre on win32. See comment above about bug
+export CR_EDITOR_PATH_WIN32=`pwd`/build/I.Defold/Defold-win32.win32.x86.zip
+pushd build/plugins/com.dynamo.cr.editor/jre_win32 >/dev/null
+zip -r $CR_EDITOR_PATH_WIN32 jre
+popd >/dev/null
