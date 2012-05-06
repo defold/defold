6f00404f21ce580edac4de5c9a31665e8e233c9e
Ragnar Svensson <ragnar.svensson@gmail.com>
2012-05-06 18:28:50 +0200

    Fixing windows update by renaming jre-dir. case 873

--- build-editor.sh
+++ build-editor.sh
@@ -7,7 +7,7 @@ prebuild
 gitclone
 echo 'root.linux.gtk.x86=absolute:${buildDirectory}/plugins/com.dynamo.cr.editor/jre_linux/' > build/root.properties
 echo 'root.linux.gtk.x86.permissions.755=jre/'  >> build/root.properties
-echo 'root.win32.win32.x86=absolute:${buildDirectory}/plugins/com.dynamo.cr.editor/jre_win32/' >> build/root.properties
+echo 'root.win32.win32.x86.folder.jre_win32=absolute:${buildDirectory}/plugins/com.dynamo.cr.editor/jre_win32/jre' >> build/root.properties
 build build_editor.xml editor.properties editor
 postbuild
 
