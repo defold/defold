c404b4138efaa1f86c15003920df9ec78ab75d3e
Ragnar Svensson <ragnar.svensson@gmail.com>
2012-05-08 23:19:58 +0200

    Bumped to 1.1.7

--- com.dynamo.cr.editor/src/com/dynamo/cr/editor/dialogs/AboutDialog.java
+++ com.dynamo.cr.editor/src/com/dynamo/cr/editor/dialogs/AboutDialog.java
@@ -20,7 +20,7 @@ public class AboutDialog extends Shell {
 
     // NOTE: Currently hard-coded
     // Non-trivial to find the product number :-(
-    private static final String VERSION = "1.1.6";
+    private static final String VERSION = "1.1.7";
     private static final String NEW_LINE = System.getProperty("line.separator");
 
     public AboutDialog(Shell shell) {
