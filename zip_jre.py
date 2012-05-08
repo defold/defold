54d9d208a0b6fb94a9e9af30759ffc5977ebf664
Ragnar Svensson <ragnar.svensson@gmail.com>
2012-05-08 13:41:00 +0200

    Changed zip script to work on python 2.6

--- zip_jre.py
+++ zip_jre.py
@@ -6,14 +6,16 @@ from os.path import join, relpath
 PREFIX='Defold'
 ZIP=sys.argv[1]
 JRE_ROOT=sys.argv[2]
-with zipfile.ZipFile(ZIP, 'a') as zip:
+zip = zipfile.ZipFile(ZIP, 'a')
 
-    for root, dirs, files in os.walk(JRE_ROOT):
-        for f in files:
-            full = join(root, f)
-            rel = relpath(full, JRE_ROOT)
+for root, dirs, files in os.walk(JRE_ROOT):
+    for f in files:
+        full = join(root, f)
+        rel = relpath(full, JRE_ROOT)
 
-            with open(full, 'rb') as file:
-                data = file.read()
-                path = join(PREFIX, rel)
-                zip.writestr(path, data)
+        with open(full, 'rb') as file:
+            data = file.read()
+            path = join(PREFIX, rel)
+            zip.writestr(path, data)
+
+zip.close()
