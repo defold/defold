884c1313122ec5774eeb681ef185e25b6649bbb9
Christian Murray <cgmurray@gmail.com>
2012-06-14 22:36:47 +0200

    Alignent macros in dlib. case 1076

---
+++ engine/dlib/src/test/test_align.cpp
@@ -0,0 +1,32 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <gtest/gtest.h>
+#include "../dlib/align.h"
+
+struct AlignStruct
+{
+    char DM_ALIGNED(128) x;
+};
+
+char DM_ALIGNED(256) g_Array[] = "foobar";
+
+TEST(dmAlign, Alignment)
+{
+    ASSERT_EQ(128U, sizeof(AlignStruct));
+    ASSERT_EQ(0U, ((uintptr_t) &g_Array[0]) & 255U);
+}
+
+TEST(dmAlign, Align)
+{
+    void* p = (void*) 0xaabb7;
+
+    p = (void*) DM_ALIGN(p, 16);
+
+    ASSERT_EQ(0xaabc0U, (uintptr_t) p);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
