54bcd11a80af6acf1367a097ed4fead2af6d3817
Christian Murray <cgmurray@gmail.com>
2012-07-07 22:06:19 +0200

    Images cleanup. case 1139 * Removed obsolete files * File for the old gwt-only site is not copied * Updated .gitignore to reflect this

--- com.dynamo.cr/com.dynamo.cr.web2/scripts/copy_docs.sh
+++ com.dynamo.cr/com.dynamo.cr.web2/scripts/copy_docs.sh
@@ -5,8 +5,5 @@ mkdir -p war/blog/images
 mkdir -p war/site
 cp -v $DYNAMO_HOME/share/doc/*doc*.json war/doc
 cp -v $DYNAMO_HOME/share/doc/*.html war/site
-# TODO: Remove this. Only here for backwards compatibility with old dynamic site
-# The new static is mostly static and the documentation resides under /doc
-cp -v -r $DYNAMO_HOME/share/doc/images war
 cp -v -r $DYNAMO_HOME/share/doc/images war/doc
 cp -v -r $DYNAMO_HOME/share/doc/images/blog war/blog/images
