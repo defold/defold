b8e05253339315d5e29b8bb0dce4b699f0bbe357
Christian Murray <cgmurray@gmail.com>
2012-07-18 09:28:44 +0200

    Initial profiler plugin-in and parser. case 1199
    * Parser of dlibs profiler format
    * Initial view
    * Console dump of profiler output
    * The plugin-in is not added to cr.product or
      com.dynamo.cr.editor.

---
+++ com.dynamo.cr/com.dynamo.cr.profiler/src/com/dynamo/cr/profiler/core/ProfilerException.java
@@ -0,0 +1,11 @@
+package com.dynamo.cr.profiler.core;
+
+public class ProfilerException extends Exception {
+
+    public ProfilerException(String message) {
+        super(message);
+    }
+
+    private static final long serialVersionUID = 1L;
+
+}
