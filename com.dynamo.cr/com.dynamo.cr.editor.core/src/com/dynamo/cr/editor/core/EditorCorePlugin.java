894980e384f915f58e1b96ddb58f1809db798a03
Ragnar Svensson <ragnar.svensson@gmail.com>
2012-08-17 12:26:42 +0200

    Release 1.2.2 case 1379
    * Blog
    * Version bump

--- com.dynamo.cr/com.dynamo.cr.editor.core/src/com/dynamo/cr/editor/core/EditorCorePlugin.java
+++ com.dynamo.cr/com.dynamo.cr.editor.core/src/com/dynamo/cr/editor/core/EditorCorePlugin.java
@@ -22,8 +22,8 @@ public class EditorCorePlugin extends Plugin implements IResourceTypeRegistry {
     // NOTE: Currently hard-coded. Non-trivial to find the product number :-(
 	// The version-number is updated
 	// with build-script (bump command)
-    public static final String VERSION = "1.2.1";
-    public static final String VERSION_SHA1 = "1cdf5c1";
+    public static final String VERSION = "1.2.2";
+    public static final String VERSION_SHA1 = "2940ae6";
 
     private static BundleContext context;
     private static EditorCorePlugin plugin;
--- engine/engine/src/engine_version.h
+++ engine/engine/src/engine_version.h
@@ -3,8 +3,8 @@
 
 namespace dmEngineVersion
 {
-    static const char* VERSION = "1.2.1";
-    static const char* VERSION_SHA1 = "1cdf5c1";
+    static const char* VERSION = "1.2.2";
+    static const char* VERSION_SHA1 = "2940ae6";
 }
 
 #endif // DM_ENGINE_VERSION
