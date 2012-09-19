e23da628ae19422af848e2e7494b0008583fccb3
Ragnar Svensson <ragnar.svensson@gmail.com>
2012-09-19 11:26:26 +0200

    Fixed emitter ddf editor. case 1454

--- com.dynamo.cr/com.dynamo.cr.ddfeditor/src/com/dynamo/cr/ddfeditor/resourcetypes/EmitterEditSupport.java
+++ com.dynamo.cr/com.dynamo.cr.ddfeditor/src/com/dynamo/cr/ddfeditor/resourcetypes/EmitterEditSupport.java
@@ -1,15 +1,11 @@
 package com.dynamo.cr.ddfeditor.resourcetypes;
 
 import com.dynamo.cr.editor.core.IResourceTypeEditSupport;
-import com.dynamo.particle.proto.Particle.Emitter.Modifier;
-import com.dynamo.particle.proto.Particle.Emitter.Modifier.InputType;
-import com.dynamo.particle.proto.Particle.Emitter.ParticleModifier;
+import com.dynamo.particle.proto.Particle.Emitter.ParticleProperty;
 import com.dynamo.particle.proto.Particle.Emitter.Property;
 import com.dynamo.particle.proto.Particle.EmitterKey;
-import com.dynamo.particle.proto.Particle.ModifierKey;
-import com.dynamo.particle.proto.Particle.ModifierProperty;
-import com.dynamo.particle.proto.Particle.ModifierType;
 import com.dynamo.particle.proto.Particle.ParticleKey;
+import com.dynamo.particle.proto.Particle.PropertySample;
 import com.google.protobuf.Descriptors.Descriptor;
 import com.google.protobuf.Message;
 
@@ -20,28 +16,20 @@ public class EmitterEditSupport implements IResourceTypeEditSupport {
 
     @Override
     public Message getTemplateMessageFor(Descriptor descriptor) {
-        if (descriptor.getFullName().equals(Modifier.getDescriptor().getFullName())) {
-            return Modifier.newBuilder()
-                .setType(ModifierType.MODIFIER_TYPE_LINEAR)
-                .setInput(InputType.INPUT_TYPE_GAIN)
-                .build();
-        }
-        else if (descriptor.getFullName().equals(Property.getDescriptor().getFullName())) {
+        if (descriptor.getFullName().equals(Property.getDescriptor().getFullName())) {
             return Property.newBuilder()
-                .setKey(EmitterKey.EMITTER_KEY_PARTICLE_ALPHA)
-                .setValue(1)
+                    .setKey(EmitterKey.EMITTER_KEY_PARTICLE_LIFE_TIME)
                 .build();
         }
-        else if (descriptor.getFullName().equals(ParticleModifier.getDescriptor().getFullName())) {
-            return ParticleModifier.newBuilder()
-                .setType(ModifierType.MODIFIER_TYPE_LINEAR)
-                .setModify(ParticleKey.PARTICLE_KEY_ALPHA)
+        else if (descriptor.getFullName().equals(ParticleProperty.getDescriptor().getFullName())) {
+            return ParticleProperty.newBuilder()
+                    .setKey(ParticleKey.PARTICLE_KEY_SCALE)
                 .build();
         }
-        else if (descriptor.getFullName().equals(ModifierProperty.getDescriptor().getFullName())) {
-            return ModifierProperty.newBuilder()
-                .setKey(ModifierKey.MODIFIER_KEY_MIN)
-                .setValue(1)
+        else if (descriptor.getFullName().equals(PropertySample.getDescriptor().getFullName())) {
+            return PropertySample.newBuilder()
+                    .setX(0.0f)
+                    .setY(0.0f)
                 .build();
         }
         return null;
