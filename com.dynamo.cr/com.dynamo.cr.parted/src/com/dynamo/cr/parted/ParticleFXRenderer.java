3703ee5b70111076a6f11f7ecef91e3566d90dba
Christian Murray <cgmurray@gmail.com>
2012-10-01 10:45:37 +0200

    Simple time elapsed rendering overlay. case 1514

--- com.dynamo.cr/com.dynamo.cr.parted/src/com/dynamo/cr/parted/ParticleFXRenderer.java
+++ com.dynamo.cr/com.dynamo.cr.parted/src/com/dynamo/cr/parted/ParticleFXRenderer.java
@@ -1,5 +1,6 @@
 package com.dynamo.cr.parted;
 
+import java.awt.Font;
 import java.nio.ByteBuffer;
 import java.util.EnumSet;
 
@@ -12,16 +13,19 @@ import com.dynamo.cr.sceneed.core.RenderContext;
 import com.dynamo.cr.sceneed.core.RenderContext.Pass;
 import com.dynamo.cr.sceneed.core.RenderData;
 import com.sun.jna.Pointer;
+import com.sun.opengl.util.j2d.TextRenderer;
 
 public class ParticleFXRenderer implements INodeRenderer<ParticleFXNode> {
 
-    private static final EnumSet<Pass> passes = EnumSet.of(Pass.TRANSPARENT);
+    private static final EnumSet<Pass> passes = EnumSet.of(Pass.TRANSPARENT, Pass.OVERLAY);
     private static final int MAX_PARTICLE_COUNT = 1024;
     private static final int MAX_EMITTER_COUNT = 128;
 
     private ByteBuffer vertexBuffer;
     private Pointer context;
     private Callback callBack = new Callback();
+    private TextRenderer textRenderer;
+    private float timeElapsed = 0;
 
     private class Callback implements RenderInstanceCallback {
         GL gl;
@@ -44,6 +48,9 @@ public class ParticleFXRenderer implements INodeRenderer<ParticleFXNode> {
         if (context != null) {
             ParticleLibrary.Particle_DestroyContext(context);
         }
+        if (textRenderer != null) {
+            textRenderer.dispose();
+        }
     }
 
     @Override
@@ -56,19 +63,45 @@ public class ParticleFXRenderer implements INodeRenderer<ParticleFXNode> {
     @Override
     public void render(RenderContext renderContext, ParticleFXNode node,
             RenderData<ParticleFXNode> renderData) {
-        double dt = renderContext.getDt();
+
         GL gl = renderContext.getGL();
-        node.simulate(context, vertexBuffer, dt);
+        double dt = renderContext.getDt();
+        if (dt == 0) {
+            timeElapsed = 0;
+        }
 
-        gl.glEnableClientState(GL.GL_VERTEX_ARRAY);
-        gl.glVertexPointer(3, GL.GL_FLOAT, 6 * 4, vertexBuffer);
+        if (renderData.getPass() == Pass.OVERLAY) {
+            // Special case for background pass. Render simulation time feedback
 
-        callBack.gl = gl;
-        // TODO: Creating a new callback here instead of the allocated one above
-        // jna will crash in com.sun.jna.Native.freeNativeCallback when finalizers are run (during gc)
-        // WHY!? Bug in JNA?
-        ParticleLibrary.Particle_Render(context, new Pointer(0), callBack);
-        gl.glDisableClientState(GL.GL_VERTEX_ARRAY);
+            if (textRenderer == null) {
+                String fontName = Font.SANS_SERIF;
+                Font debugFont = new Font(fontName, Font.BOLD, 20);
+                textRenderer = new TextRenderer(debugFont, true, true);
+            }
 
+            gl.glPushMatrix();
+            gl.glScaled(1, -1, 1);
+            textRenderer.setColor(1, 1, 1, 1);
+            textRenderer.begin3DRendering();
+            String text = String.format("%.1f", timeElapsed);
+            textRenderer.draw3D(text, (float) 10, (float) -26, 1, 1);
+            textRenderer.end3DRendering();
+            gl.glPopMatrix();
+
+        } else {
+            // General particle rendering
+            node.simulate(context, vertexBuffer, dt);
+            gl.glEnableClientState(GL.GL_VERTEX_ARRAY);
+            gl.glVertexPointer(3, GL.GL_FLOAT, 6 * 4, vertexBuffer);
+
+            callBack.gl = gl;
+            // TODO: Creating a new callback here instead of the allocated one above
+            // jna will crash in com.sun.jna.Native.freeNativeCallback when finalizers are run (during gc)
+            // WHY!? Bug in JNA?
+            ParticleLibrary.Particle_Render(context, new Pointer(0), callBack);
+            gl.glDisableClientState(GL.GL_VERTEX_ARRAY);
+        }
+
+        timeElapsed += dt;
     }
 }
