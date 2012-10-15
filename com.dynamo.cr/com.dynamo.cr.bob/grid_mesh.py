ceb6177b41d8e0659d7497f15065fa75af042c83
Christian Murray <cgmurray@gmail.com>
2012-10-15 12:00:54 +0200

    New grid (lines) generation script

--- com.dynamo.cr/com.dynamo.cr.bob/grid_mesh.py
+++ com.dynamo.cr/com.dynamo.cr.bob/grid_mesh.py
@@ -2,15 +2,42 @@ import maya.OpenMaya as OpenMaya
 
 # Utility script to create a grid in Maya
 
-def create():
-    mesh = OpenMaya.MFnMesh()
-    face_index = xutil.createFromInt(0)
+def float_point_array(lst):
+    a = OpenMaya.MPointArray()
+    for tmp in lst:
+        if len(tmp) == 2:
+            x,y = tmp
+            z = 0
+        else:
+            x, y, z = tmp
 
-    NX = 18 * 2
-    NY = 10 * 2
-    width = 60 * NX / 2
-    height = 60 * NY / 2
-    t = thickness = 3
+        a.append(x, y, z)
+    return a
+
+UVS = [(0, 1), (0, 0), (1, 0), (1, 1), (0.5, 0.5)]
+
+def add_tri(mesh, lst, uv):
+    mesh.addPolygon(float_point_array(lst), False)
+
+    i = mesh.numPolygons() - 1
+    a = OpenMaya.MIntArray()
+    mesh.getPolygonVertices(i, a)
+
+    for j in range(3):
+        mesh.setUV(i * 3 + j, UVS[uv[j]][0], UVS[uv[j]][1])
+
+    for j in range(3):
+        mesh.assignUV(i, j, i * 3 + j)
+
+def create_grid():
+    mesh = OpenMaya.MFnMesh()
+
+    NX = 18
+    NY = 10
+    width = 60 * NX
+    height = 60 * NY
+    NSPLIT = 16
+    t = thickness = 1.5
 
     for j in range(NY):
         for i in range(NX):
@@ -63,25 +90,91 @@ def create():
 
             if i < (NX-1):
                 # connect horizontally
-                add_tri(mesh,
-                        [(x + t, y - t), (xp - t, y - t), (xp - t, y + t)],
-                        [1, 2, 3])
+                for k in range(NSPLIT):
+                    startx = x + t
+                    endx = xp - t
 
-                add_tri(mesh,
-                        [(x + t, y - t), (xp - t, y + t), (x + t, y + t)],
-                        [0, 1, 2])
+                    x0 = startx + (endx - startx) * float(k) / float(NSPLIT)
+                    x1 = startx + (endx - startx) * float(k + 1) / float(NSPLIT)
+                    add_tri(mesh,
+                            [(x0, y - t), (x1, y - t), (x1, y + t)],
+                            [1, 2, 3])
+
+                    add_tri(mesh,
+                            [(x0, y - t), (x1, y + t), (x0, y + t)],
+                            [0, 1, 2])
 
             # connect vertically
             if j < (NY-1):
-                add_tri(mesh,
-                        [(x - t, y - t), (x - t, yp + t), (x + t, yp + t)],
-                        [1, 2, 3])
+                for k in range(NSPLIT):
+                    starty = y - t
+                    endy = yp + t
 
-                add_tri(mesh,
-                        [(x - t, y - t), (x + t, yp + t), (x + t, y - t)],
-                        [0, 1, 2])
+                    y0 = starty + (endy - starty) * float(k) / float(NSPLIT)
+                    y1 = starty + (endy - starty) * float(k + 1) / float(NSPLIT)
+
+                    add_tri(mesh,
+                            [(x - t, y0), (x - t, y1), (x + t, y1)],
+                            [1, 2, 3])
+
+                    add_tri(mesh,
+                            [(x - t, y0), (x + t, y1), (x + t, y0)],
+                            [0, 1, 2])
 
     for i in range(mesh.numEdges()):
         mesh.setEdgeSmoothing(i, False)
 
-create()
+def create_lines(x_y):
+    mesh = OpenMaya.MFnMesh()
+    NX = 18
+    NY = 10
+    width = 60 * NX
+    height = 60 * NY
+    NSPLIT = 128
+    t = thickness = 1.5
+
+    if not x_y:
+        i = 0
+        for j in range(NY):
+            x0 = 0
+            y = -height * j / (NY - 1.0) + height
+            print y
+            for k in range(NSPLIT):
+                startx = 0
+                endx = width
+
+                x0 = startx + (endx - startx) * float(k) / float(NSPLIT)
+                x1 = startx + (endx - startx) * float(k + 1) / float(NSPLIT)
+                add_tri(mesh,
+                        [(x0, y - t, y), (x1, y - t, y), (x1, y + t, y)],
+                        [1, 2, 3])
+
+                add_tri(mesh,
+                        [(x0, y - t, y), (x1, y + t, y), (x0, y + t, y)],
+                        [0, 1, 2])
+    else:
+        j = 0
+        for i in range(NX):
+            y0 = 0
+            x = width * i / (NX - 1.0)
+            print x
+            for k in range(NSPLIT):
+                starty = 0
+                endy = height
+
+                y0 = starty + (endy - starty) * float(k) / float(NSPLIT)
+                y1 = starty + (endy - starty) * float(k + 1) / float(NSPLIT)
+
+                add_tri(mesh,
+                        [(x - t, y0, x), (x - t, y1, x), (x + t, y1, x)],
+                        [1, 2, 3])
+
+                add_tri(mesh,
+                        [(x - t, y0, x), (x + t, y1, x), (x + t, y0, x)],
+                        [0, 1, 2])
+
+        print height
+
+#create_grid()
+create_lines(True)
+create_lines(False)
