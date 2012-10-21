c3ed80b8b2f2fb16e597a2afc1d06d473196caa9
Christian Murray <cgmurray@gmail.com>
2012-10-21 20:10:39 +0200

    Copy 64-bit version of libparticle. case 1642

--- com.dynamo.cr/com.dynamo.cr.parted/scripts/copy_libparticle.sh
+++ com.dynamo.cr/com.dynamo.cr.parted/scripts/copy_libparticle.sh
@@ -7,5 +7,5 @@ mkdir -p lib/ios
 SHA1=`git log --oneline | head -1 | awk '{ print $1 }'`
 
 scp builder@ci-master.defold.com:builds/linux/libparticle_shared.so.${SHA1} lib/linux/libparticle_shared.so
-scp builder@ci-master.defold.com:builds/darwin/libparticle_shared.dylib.${SHA1} lib/x86_64-darwin/libparticle_shared.dylib
+scp builder@ci-master.defold.com:builds/x86_64-darwin/libparticle_shared.dylib.${SHA1} lib/x86_64-darwin/libparticle_shared.dylib
 scp builder@ci-master.defold.com:builds/win32/particle_shared.dll.${SHA1} lib/win32/particle_shared.dll
