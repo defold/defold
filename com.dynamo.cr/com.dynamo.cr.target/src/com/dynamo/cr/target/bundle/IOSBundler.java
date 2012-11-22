8093ca31e7964e01a4e977969d3b77b5a9bb0de7
Christian Murray <cgmurray@gmail.com>
2012-11-22 14:05:26 +0100

    Improved iOS bundling. case 1803
    * Launch image support
    * Creation of .ipa
    * armv7 and OpenGL ES 2.0 now required

--- com.dynamo.cr/com.dynamo.cr.target/src/com/dynamo/cr/target/bundle/IOSBundler.java
+++ com.dynamo.cr/com.dynamo.cr.target/src/com/dynamo/cr/target/bundle/IOSBundler.java
@@ -2,11 +2,16 @@ package com.dynamo.cr.target.bundle;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.SubnodeConfiguration;
@@ -17,6 +22,7 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.dynamo.cr.common.util.Exec;
 import com.dynamo.cr.editor.core.ProjectProperties;
 import com.dynamo.cr.target.core.TargetPlugin;
 
@@ -30,6 +36,7 @@ public class IOSBundler {
     private File appDir;
     private String identity;
     private String provisioningProfile;
+    private File packageDir;
 
     private static List<PropertyAlias> propertyAliases = new ArrayList<IOSBundler.PropertyAlias>();
 
@@ -60,14 +67,13 @@ public class IOSBundler {
         addProperty("CFBundleShortVersionString", "project", "version", "1.0");
     }
 
-    private void copyIcon(String name, String outName, List<String> iconNames)
+    private void copyIcon(String name, String outName)
             throws IOException {
         String resource = projectProperties.getStringValue("ios", name);
         if (resource != null && resource.length() > 0) {
             File inFile = new File(projectRoot, resource);
             File outFile = new File(appDir, outName);
             FileUtils.copyFile(inFile, outFile);
-            iconNames.add(outFile.getName());
         }
     }
 
@@ -94,7 +100,7 @@ public class IOSBundler {
         this.projectRoot = projectRoot;
         this.contentRoot = contentRoot;
 
-        File packageDir = new File(outputDir);
+        packageDir = new File(outputDir);
         this.title = projectProperties.getStringValue("project", "title",
                 "Unnamed");
         appDir = new File(packageDir, title + ".app");
@@ -132,11 +138,19 @@ public class IOSBundler {
         resourceRulesIn.close();
 
         // Copy icons
-        List<String> iconNames = new ArrayList<String>();
-        copyIcon("app_icon_57x57", "ios_icon_57.png", iconNames);
-        copyIcon("app_icon_114x114", "ios_icon_114.png", iconNames);
-        copyIcon("app_icon_72x72", "ios_icon_72.png", iconNames);
-        copyIcon("app_icon_144x144", "ios_icon_144.png", iconNames);
+        copyIcon("app_icon_57x57", "ios_icon_57.png");
+        copyIcon("app_icon_114x114", "ios_icon_114.png");
+        copyIcon("app_icon_72x72", "ios_icon_72.png");
+        copyIcon("app_icon_144x144", "ios_icon_144.png");
+
+        // Copy launch images
+        copyIcon("launch_image_320x480", "Default.png");
+        copyIcon("launch_image_640x960", "Default@2x.png");
+        copyIcon("launch_image_640x1136", "Default-568h@2x.png");
+        copyIcon("launch_image_768x1004", "Default-Portrait~ipad.png");
+        copyIcon("launch_image_1536x2008", "Default-Portrait@2x~ipad.png");
+        copyIcon("launch_image_1024x748", "Default-Landscape~ipad.png");
+        copyIcon("launch_image_2048x1496", "Default-Landscape@2x~ipad.png");
 
         SubnodeConfiguration primaryIcon = info.configurationAt(
                 "CFBundleIcons", true).configurationAt("CFBundlePrimaryIcon",
@@ -150,7 +164,8 @@ public class IOSBundler {
                 .getBooleanValue("ios", "pre_renderered_icons", false));
 
         // Save updated Info.plist
-        info.save(new File(appDir, "Info.plist"));
+        File infoFile = new File(appDir, "Info.plist");
+        info.save(infoFile);
 
         // Copy Provisioning Profile
         FileUtils.copyFile(new File(provisioningProfile), new File(appDir,
@@ -187,8 +202,28 @@ public class IOSBundler {
             } catch (InterruptedException e1) {
                 throw new RuntimeException(e1);
             }
-
         }
+
+        File zipFile = new File(this.packageDir, this.title + ".ipa");
+        ZipOutputStream zipStream = new ZipOutputStream(new FileOutputStream(zipFile));
+
+        Collection<File> files = FileUtils.listFiles(appDir, null, true);
+        String root = FilenameUtils.normalize(packageDir.getPath(), true);
+        for (File f : files) {
+            String p = FilenameUtils.normalize(f.getPath(), true);
+            String rel = p.substring(root.length());
+
+            // NOTE: The path to Payload is relative, i.e. not /Payload
+            // If rooted iTunes complains about invalid package
+            zipStream.putNextEntry(new ZipEntry("Payload" + rel));
+
+            FileInputStream input = new FileInputStream(f);
+            IOUtils.copy(input, zipStream);
+            input.close();
+            zipStream.closeEntry();
+        }
+
+        zipStream.close();
     }
 
 }
