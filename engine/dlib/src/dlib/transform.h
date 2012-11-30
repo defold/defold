8075a82f27fe767909cfb7879bcc5c959476a3b6
Ragnar Svensson <ragnar.svensson@gmail.com>
2012-11-30 09:55:08 +0100

    Transform with uniform scale. case 1825

---
+++ engine/dlib/src/dlib/transform.h
@@ -0,0 +1,80 @@
+#ifndef DM_TRANSFORM_H
+#define DM_TRANSFORM_H
+
+#include <assert.h>
+#include <math.h>
+#include <vectormath/cpp/vectormath_aos.h>
+
+namespace dmTransform
+{
+    using namespace Vectormath::Aos;
+
+    /**
+     * Transform with uniform scale.
+     * Transform applied as:
+     * T(p) = translate(rotate(scale(p))) = p'
+     */
+    struct TransformS1
+    {
+        // tx, ty, tz, scale
+        Vector4 m_TranslationScale;
+        Quat    m_Rotation;
+
+        inline void SetIdentity()
+        {
+            m_TranslationScale = Vector4(0.0f, 0.0f, 0.0f, 1.0f);
+            m_Rotation = Quat::identity();
+        }
+
+        inline Vector3 GetTranslation() const
+        {
+            return m_TranslationScale.getXYZ();
+        }
+
+        inline void SetTranslation(Vector3 translation)
+        {
+            m_TranslationScale.setXYZ(translation);
+        }
+
+        inline float GetScale() const
+        {
+            return m_TranslationScale.getW();
+        }
+
+        inline void SetScale(float scale)
+        {
+            m_TranslationScale.setW(scale);
+        }
+
+        inline Quat GetRotation() const
+        {
+            return m_Rotation;
+        }
+
+        inline void SetRotation(Quat rotation)
+        {
+            m_Rotation = rotation;
+        }
+    };
+
+    inline TransformS1 Mul(const TransformS1& lhs, const TransformS1& rhs)
+    {
+        TransformS1 res;
+        res.SetRotation(lhs.GetRotation() * rhs.GetRotation());
+        res.SetTranslation(rotate(lhs.GetRotation(), lhs.GetScale() * rhs.GetTranslation()) + lhs.GetTranslation());
+        res.SetScale(lhs.GetScale() * rhs.GetScale());
+        return res;
+    }
+
+    inline TransformS1 Inv(const TransformS1& t)
+    {
+        assert(t.GetScale() != 0.0f && "Transform can not be inverted (0 scale).");
+        TransformS1 res;
+        res.SetRotation(conj(t.GetRotation()));
+        res.SetScale(1.0f / t.GetScale());
+        res.SetTranslation(rotate(res.GetRotation(), -t.GetTranslation()) * res.GetScale());
+        return res;
+    }
+}
+
+#endif // DM_TRANSFORM_H
---
+++ engine/dlib/src/test/test_transform.cpp
@@ -0,0 +1,65 @@
+#include <gtest/gtest.h>
+#include "dlib/transform.h"
+
+using namespace Vectormath::Aos;
+using namespace dmTransform;
+
+#define EPSILON 0.001f
+#define ASSERT_V3_NEAR(expected, actual)\
+    ASSERT_NEAR(expected.getX(), actual.getX(), EPSILON);\
+    ASSERT_NEAR(expected.getY(), actual.getY(), EPSILON);\
+    ASSERT_NEAR(expected.getZ(), actual.getZ(), EPSILON);
+#define ASSERT_V4_NEAR(expected, actual)\
+    ASSERT_NEAR(expected.getX(), actual.getX(), EPSILON);\
+    ASSERT_NEAR(expected.getY(), actual.getY(), EPSILON);\
+    ASSERT_NEAR(expected.getZ(), actual.getZ(), EPSILON);\
+    ASSERT_NEAR(expected.getW(), actual.getW(), EPSILON);
+#define ASSERT_TRANSFORMS1_NEAR(expected, actual)\
+    ASSERT_V3_NEAR(expected.GetTranslation(), actual.GetTranslation());\
+    ASSERT_V4_NEAR(expected.GetRotation(), actual.GetRotation());\
+    ASSERT_NEAR(expected.GetScale(), actual.GetScale(), EPSILON);
+
+TEST(dmTransform, Multiply)
+{
+    TransformS1 t0;
+    t0.SetTranslation(Vector3(1.0f, 0.0f, 0.0f));
+    t0.SetRotation(Quat::rotationZ(M_PI_2));
+    t0.SetScale(2.0f);
+
+    TransformS1 res = Mul(t0, t0);
+    ASSERT_V3_NEAR(Vector3(1.0f, 2.0f, 0.0f), res.GetTranslation());
+    ASSERT_V4_NEAR(Quat::rotationZ(M_PI), res.GetRotation());
+    ASSERT_EQ(4.0f, res.GetScale());
+}
+
+TEST(dmTransform, Inverse)
+{
+    TransformS1 i;
+    i.SetIdentity();
+
+    TransformS1 t0;
+    t0.SetTranslation(Vector3(1.0f, 0.0f, 0.0f));
+    t0.SetRotation(Quat::rotationZ(M_PI_2));
+    t0.SetScale(2.0f);
+
+    // Identity inverse is identity
+    ASSERT_TRANSFORMS1_NEAR(i, Inv(i));
+
+    // Inverted inverse is original
+    ASSERT_TRANSFORMS1_NEAR(t0, Inv(Inv(t0)));
+
+    // Inverted product is identity
+    ASSERT_TRANSFORMS1_NEAR(i, Mul(t0, Inv(t0)));
+    ASSERT_TRANSFORMS1_NEAR(i, Mul(Inv(t0), t0));
+}
+
+#undef EPSILON
+#undef ASSERT_V3_NEAR
+#undef ASSERT_V4_NEAR
+#undef ASSERT_TRANSFORMS1_NEAR
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
