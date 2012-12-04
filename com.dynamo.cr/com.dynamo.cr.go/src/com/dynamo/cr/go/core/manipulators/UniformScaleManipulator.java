7846f547cf7a47e3f508d107f1f3bcc92b168af9
Ragnar Svensson <ragnar.svensson@gmail.com>
2012-12-04 16:39:13 +0100

    Fixed scale manipulator to work for multi-selection. case 1868

--- com.dynamo.cr/com.dynamo.cr.go/src/com/dynamo/cr/go/core/manipulators/UniformScaleManipulator.java
+++ com.dynamo.cr/com.dynamo.cr.go/src/com/dynamo/cr/go/core/manipulators/UniformScaleManipulator.java
@@ -1,23 +1,20 @@
 package com.dynamo.cr.go.core.manipulators;
 
-import org.eclipse.core.commands.operations.IUndoableOperation;
-import org.eclipse.swt.events.MouseEvent;
+import java.util.List;
+
+import javax.vecmath.Matrix3d;
+import javax.vecmath.Matrix4d;
 
 import com.dynamo.cr.go.core.InstanceNode;
-import com.dynamo.cr.properties.IPropertyModel;
-import com.dynamo.cr.sceneed.core.ISceneModel;
 import com.dynamo.cr.sceneed.core.Manipulator;
 import com.dynamo.cr.sceneed.core.Node;
-import com.dynamo.cr.sceneed.ui.RootManipulator;
 import com.dynamo.cr.sceneed.ui.ScaleAxisManipulator;
+import com.dynamo.cr.sceneed.ui.TransformManipulator;
 
 @SuppressWarnings("serial")
-public class UniformScaleManipulator extends RootManipulator {
+public class UniformScaleManipulator extends TransformManipulator {
 
     private ScaleAxisManipulator scaleManipulator;
-    private double originalScale;
-    private boolean scaleChanged = false;
-    private double scale;
 
     public UniformScaleManipulator() {
         scaleManipulator = new ScaleAxisManipulator(this, new float[] {1, 0, 0, 1});
@@ -25,62 +22,42 @@ public class UniformScaleManipulator extends RootManipulator {
     }
 
     @Override
-    public boolean match(Object[] selection) {
-        if (selection.length > 0 && selection[0] instanceof InstanceNode) {
-            return true;
-        }
-        return false;
+    protected String getOperation() {
+        return "Scale";
     }
 
     @Override
-    protected void transformChanged() {
+    public boolean match(Object[] selection) {
+        for (Object object : selection) {
+            if (!(object instanceof InstanceNode)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private double getScale(Matrix4d transform, Matrix3d tmpRotationScale) {
+        transform.getRotationScale(tmpRotationScale);
+        return tmpRotationScale.getScale();
+    }
+
+    @Override
+    protected void applyTransform(List<Node> selection, List<Matrix4d> originalLocalTransforms) {
+        Matrix3d tmp = new Matrix3d();
+        double distance = this.scaleManipulator.getDistance();
+        double factor = 0.002; // * ManipulatorRendererUtil.getScaleFactor(this.scaleManipulator, getController().getRenderView());
+        int n = selection.size();
+        for (int i = 0; i < n; ++i) {
+            InstanceNode instance = (InstanceNode)selection.get(i);
+            double scale = getScale(originalLocalTransforms.get(i), tmp);
+            scale += factor * distance;
+            instance.setScale(scale);
+        }
     }
 
     @Override
     public void manipulatorChanged(Manipulator manipulator) {
-        scaleChanged = true;
-        double factor = 0.001;
-        for (Node node : getSelection()) {
-            if (node instanceof InstanceNode) {
-                InstanceNode instance = (InstanceNode) node;
-                scale = this.originalScale + scaleManipulator.getDistance() * factor;
-                if (scale == 0.0) {
-                    scale = 0.0001;
-                }
-                instance.setScale(scale);
-            }
-        }
-    }
-
-    @Override
-    protected void selectionChanged() {
-        Node node = getSelection().get(0);
-        originalScale = ((InstanceNode) node).getScale();
-        setTranslation(node.getTranslation());
-        setRotation(node.getRotation());
-    }
-
-    @Override
-    public void refresh() {
-    }
-
-    @Override
-    public void mouseUp(MouseEvent e) {
-        if (scaleChanged) {
-            Node node = getSelection().get(0);
-            InstanceNode instance = (InstanceNode) node;
-            // We must reset scale. Otherwise setPropertyValue will detect no change and return null operation
-            instance.setScale(originalScale);
-            // Set original scale to current in order to "continue" scaling
-            originalScale = scale;
-
-            @SuppressWarnings("unchecked")
-            IPropertyModel<Node, ISceneModel> propertyModel = (IPropertyModel<Node, ISceneModel>) node.getAdapter(IPropertyModel.class);
-            IUndoableOperation operation = propertyModel.setPropertyValue("scale", scale);
-            if (operation != null) {
-                getController().executeOperation(operation);
-            }
-        }
+        transformChanged();
     }
 
 }
