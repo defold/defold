2d01ac907d0ed17d0e5d6fd815835e12fb6a9a95
Ragnar Svensson <ragnar.svensson@gmail.com>
2012-12-04 22:43:41 +0100

    Added missing scale manip renderer
    * Empty implementation

---
+++ com.dynamo.cr/com.dynamo.cr.sceneed/src/com/dynamo/cr/sceneed/ui/ScaleManipulatorRenderer.java
@@ -0,0 +1,24 @@
+package com.dynamo.cr.sceneed.ui;
+
+import com.dynamo.cr.sceneed.core.INodeRenderer;
+import com.dynamo.cr.sceneed.core.RenderContext;
+import com.dynamo.cr.sceneed.core.RenderData;
+
+public class ScaleManipulatorRenderer implements INodeRenderer<ScaleManipulator> {
+
+    public ScaleManipulatorRenderer() {
+    }
+
+    @Override
+    public void dispose() { }
+
+    @Override
+    public void setup(RenderContext renderContext, ScaleManipulator node) {
+    }
+
+    @Override
+    public void render(RenderContext renderContext, ScaleManipulator node,
+            RenderData<ScaleManipulator> renderData) {
+    }
+
+}
