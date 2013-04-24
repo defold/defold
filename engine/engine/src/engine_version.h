1b6c64c38d4ef2e513d8c18cf4f12ff9db715662
Ragnar Svensson <ragnar.svensson@gmail.com>
2013-04-24 23:16:39 +0200

    Bump to 1.2.19

--- engine/engine/src/engine_version.h
+++ engine/engine/src/engine_version.h
@@ -3,8 +3,8 @@
 
 namespace dmEngineVersion
 {
-    static const char* VERSION = "1.2.18";
-    static const char* VERSION_SHA1 = "a029465";
+    static const char* VERSION = "1.2.19";
+    static const char* VERSION_SHA1 = "f3500b0";
 }
 
 #endif // DM_ENGINE_VERSION
