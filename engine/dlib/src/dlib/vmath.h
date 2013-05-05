bdcb56e38e4c927fc5767e84eb745c1e00c586db
Ragnar Svensson <ragnar.svensson@gmail.com>
2013-05-05 19:55:16 +0200

    Quat to euler conversion. case 2099

--- engine/dlib/src/dlib/vmath.h
+++ engine/dlib/src/dlib/vmath.h
@@ -27,6 +27,50 @@ namespace dmVMath
         return q;
     }
 
+    /**
+     * Converts a quaternion into euler angles (r0, r1, r2), based on the specified rotation order.
+     * Order XYZ is specified as (x, y, z, w)
+     * To handle gimbal lock (singularity at r1 ~ +/- 90 degrees), the cut off is at r0 = +/- 88.85 degrees, which snaps to +/- 90.
+     * The provided quaternion is expected to be normalized.
+     * @param q0 first imaginary axis
+     * @param q1 second imaginary axis
+     * @param q2 third imaginary axis
+     * @param q3 real part
+     * @result Euler angles in the same order as the specified rotation order
+     */
+    inline Vectormath::Aos::Vector3 QuatToEuler(float q0, float q1, float q2, float q3)
+    {
+        // Implementation based on:
+        // * http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+        // * http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToEuler/
+        // * http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToEuler/Quaternions.pdf
+        const float limit = 0.4999f; // gimbal lock limit, corresponds to 88.85 degrees
+        float test = q3 * q0 + q1 * q2;
+        float r0, r1, r2;
+        if (test > limit)
+        {
+            r0 = 2.0f * atan2(q0, q3);
+            r1 = M_PI_2;
+            r2 = 0.0f;
+        }
+        else if (test < -limit)
+        {
+            r0 = -2.0f * atan2(q0, q3);
+            r1 = -M_PI_2;
+            r2 = 0.0f;
+        }
+        else
+        {
+            float sq0 = q0 * q0;
+            float sq1 = q1 * q1;
+            float sq2 = q2 * q2;
+            r0 = atan2(2.0f * (q3 * q0 - q1 * q2), 1.0f - 2.0f * (sq0 + sq1));
+            r1 = asin(2.0f * (q3 * q1 + q0 * q2));
+            r2 = atan2(2.0f * (q3 * q2 - q0 * q1), 1.0f - 2.0f * (sq1 + sq2));
+        }
+        const float deg_factor = 180.0f / M_PI;
+        return Vectormath::Aos::Vector3(r0, r1, r2) * deg_factor;
+    }
 }
 
 #endif
--- engine/dlib/src/test/test_vmath.cpp
+++ engine/dlib/src/test/test_vmath.cpp
@@ -14,6 +14,38 @@ TEST(dmVMath, QuatFromAngle)
     ASSERT_NEAR(0, dot(q - q1, q - q1), epsilon);
 
 }
+#include <stdio.h>
+TEST(dmMath, TestQuatToEuler)
+{
+    float epsilon = 0.02f;
+
+    Vector3 euler;
+
+    float rad_factor = M_PI / 360.0f; // contains half angle factor too
+    // different degrees around different single axis
+    for (uint32_t i = 0; i < 3; ++i)
+    {
+        for (float a = 0.0f; a < 105.0f; a += 10.0f)
+        {
+            float ap = dmMath::Min(a, 90.0f);
+            float sa2 = sin(ap * rad_factor);
+            float ca2 = cos(ap * rad_factor);
+            float v[] = {0.0f, 0.0f, 0.0f};
+            v[i] = sa2;
+            euler = dmVMath::QuatToEuler(v[0], v[1], v[2], ca2);
+            float expected_i = ap;
+            float expected_i_1 = 0.0f;
+            float expected_i_n_1 = 0.0f;
+            if (i == 0 and ap >= 90.0f)
+            {
+                expected_i_1 = 90.0f;
+            }
+            ASSERT_NEAR(expected_i, euler.getElem(i), epsilon);
+            ASSERT_NEAR(expected_i_1, euler.getElem((i + 1) % 3), epsilon);
+            ASSERT_NEAR(expected_i_n_1, euler.getElem((i + 2) % 3), epsilon);
+        }
+    }
+}
 
 int main(int argc, char **argv)
 {
