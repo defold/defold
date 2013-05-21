f421a7f801515768e6bffb81ba262b0f2b0e283d
Christian Murray <cgmurray@gmail.com>
2013-05-21 23:04:05 +0200

    Compile fix. case 2112

--- engine/script/src/script_http.h
+++ engine/script/src/script_http.h
@@ -1,12 +1,6 @@
 #ifndef DM_SCRIPT_HTTP_H
 #define DM_SCRIPT_HTTP_H
 
-#include <dlib/configfile.h>
-
-#include <gameobject/gameobject.h>
-
-#include <render/render.h>
-
 namespace dmScript
 {
     void InitializeHttp(lua_State* L);
