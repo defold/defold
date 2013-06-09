51d5062ad3c07612865bfcf71ac5ccdb0ae191bb
Christian Murray <cgmurray@gmail.com>
2013-06-09 19:51:20 +0200

    Initial git-server in go. case 2158
    * Support only for the "smart" protocol
    * Pluggable authorization
    * Tests

---
+++ go/cmd/gitsrv/gitsrv.go
@@ -0,0 +1,74 @@
+/*
+	Git server in go
+*/
+package main
+
+import (
+	"defold/gitsrv"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+var (
+	port         = 0
+	repoRoot     = ""
+	gitRoot      = ""
+	logDir       = ""
+	readTimeOut  = 0
+	writeTimeOut = 0
+)
+
+func init() {
+	flag.IntVar(&port, "port", 8080, "http port")
+	flag.StringVar(&repoRoot, "repo-root", "", "repository root")
+	flag.StringVar(&gitRoot, "git-root", "/usr", "git install root directory")
+	flag.StringVar(&logDir, "log-dir", ".", "log directory")
+	flag.IntVar(&readTimeOut, "read-timeout", 100, "http read timeout in seconds")
+	flag.IntVar(&writeTimeOut, "write-timeout", 100, "http write timeout in seconds")
+	flag.Parse()
+}
+
+// TODO: Temporary solution until proper authorization is implemented.
+// See https://defold.fogbugz.com/default.asp?2159
+type AccessAll int
+
+func (a *AccessAll) Authorize(header http.Header, repo string) error {
+	return nil
+}
+
+func main() {
+	if fi, _ := os.Stat(filepath.Join(gitRoot, "libexec/git-core")); fi == nil {
+		log.Fatalf("%s not found. Invalid git installation?", filepath.Join(gitRoot, "libexec/git-core"))
+	}
+
+	accessFile, err := os.OpenFile(filepath.Join(logDir, "access.log"), os.O_CREATE|os.O_APPEND|os.O_RDWR, 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	errorFile, err := os.OpenFile(filepath.Join(logDir, "error.log"), os.O_CREATE|os.O_APPEND|os.O_RDWR, 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	accessLog := log.New(accessFile, "", log.LstdFlags)
+	errorLog := log.New(errorFile, "", log.LstdFlags)
+
+	s := gitsrv.NewServer(repoRoot, gitRoot, new(AccessAll), accessLog, errorLog)
+	httpServer := &http.Server{
+		Addr:         fmt.Sprintf(":%d", port),
+		Handler:      s,
+		ReadTimeout:  time.Duration(readTimeOut) * time.Second,
+		WriteTimeout: time.Duration(writeTimeOut) * time.Second,
+	}
+
+	err = httpServer.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go/src/defold/gitsrv/gitsrv_test.go
@@ -0,0 +1,210 @@
+package gitsrv
+
+import (
+	"crypto/md5"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+var (
+	smallFileContent = "some data"
+	largeFileContent = []byte{}
+	largeFileSize    = 1024 * 1024 * 2
+)
+
+func init() {
+	largeFileContent = make([]byte, largeFileSize)
+	for i := 0; i < largeFileSize; i++ {
+		largeFileContent[i] = byte(rand.Int() & 0xff)
+	}
+}
+
+type AccessAll struct {
+}
+
+func (a *AccessAll) Authorize(header http.Header, repo string) error {
+	return nil
+}
+
+type ForbidAll struct {
+}
+
+func (a *ForbidAll) Authorize(header http.Header, repo string) error {
+	return fmt.Errorf("Permission denied")
+}
+
+type HttpAuth struct {
+}
+
+func parseBasicAuth(authHeader string) (string, string, error) {
+	auth, _ := base64.StdEncoding.DecodeString(authHeader[6:])
+	userPass := strings.Split(string(auth), ":")
+	if len(userPass) == 2 {
+		return userPass[0], userPass[1], nil
+	}
+	return "", "", fmt.Errorf("malformed auth header")
+}
+
+func (a *HttpAuth) Authorize(header http.Header, repo string) error {
+
+	authHeader := header.Get("Authorization")
+	if authHeader != "" {
+		if user, pass, err := parseBasicAuth(authHeader); err == nil {
+			if user == "foo" && pass == "bar" {
+				return nil
+			}
+		} else {
+			return err
+		}
+	}
+
+	return fmt.Errorf("Permission denied")
+}
+
+func newTemplate() string {
+	d, _ := ioutil.TempDir(os.TempDir(), "gitsrv")
+	gd := filepath.Join(d, ".git")
+	if e := exec.Command("git", "init", d).Run(); e != nil {
+		panic(e)
+	}
+
+	small := filepath.Join(d, "small")
+	ioutil.WriteFile(small, []byte(smallFileContent), 0777)
+
+	large := filepath.Join(d, "large")
+	ioutil.WriteFile(large, largeFileContent, 0777)
+
+	if out, e := exec.Command("git", "--work-tree="+d, "--git-dir="+gd, "add", ".").CombinedOutput(); e != nil {
+		panic(string(out))
+	}
+	if out, e := exec.Command("git", "--work-tree="+d, "--git-dir="+gd, "commit", "-a", "-minitial").CombinedOutput(); e != nil {
+		panic(string(out))
+	}
+
+	return d
+}
+
+func cloneRepoBare(repo string) string {
+	d, _ := ioutil.TempDir(os.TempDir(), "gitsrv")
+	if out, e := exec.Command("git", "clone", "--bare", repo, d).CombinedOutput(); e != nil {
+		panic(string(out))
+	}
+
+	if out, e := exec.Command("git", "--git-dir="+d, "config", "http.receivepack", "true").CombinedOutput(); e != nil {
+		panic(string(out))
+	}
+
+	return d
+}
+
+type gitTest struct {
+	template   string
+	repo       string
+	listener   net.Listener
+	port       int
+	httpServer *http.Server
+}
+
+func setup(auth Authorizor) *gitTest {
+	template := newTemplate()
+	repo := cloneRepoBare(template)
+
+	// TODO: Use bundled git
+	s := NewServer(repo, "/usr", auth, nil, nil)
+
+	l, e := net.Listen("tcp", ":0")
+	if e != nil {
+		panic(e)
+	}
+
+	port := l.Addr().(*net.TCPAddr).Port
+
+	httpServer := &http.Server{Addr: fmt.Sprintf("%d", port), Handler: s}
+	go httpServer.Serve(l)
+
+	return &gitTest{template: template,
+		repo:       repo,
+		listener:   l,
+		port:       port,
+		httpServer: httpServer,
+	}
+}
+
+func shutdown(gt *gitTest) {
+	gt.listener.Close()
+	os.RemoveAll(gt.template)
+	os.RemoveAll(gt.repo)
+}
+
+func TestClone(t *testing.T) {
+	gt := setup(new(AccessAll))
+	defer shutdown(gt)
+
+	clone, _ := ioutil.TempDir(os.TempDir(), "gitsrv")
+	if out, e := exec.Command("git", "clone", fmt.Sprintf("http://localhost:%d", gt.port), clone).CombinedOutput(); e != nil {
+		panic(string(out))
+	}
+
+	defer os.RemoveAll(clone)
+	small, err := ioutil.ReadFile(filepath.Join(clone, "small"))
+	if err != nil {
+		panic(err)
+	}
+
+	if string(small) != smallFileContent {
+		t.Errorf("expected %v, found %v", smallFileContent, string(small))
+	}
+
+	large, err := ioutil.ReadFile(filepath.Join(clone, "large"))
+	if err != nil {
+		panic(err)
+	}
+
+	md5repo := md5.New()
+	md5repo.Write(large)
+	md5ref := md5.New()
+	md5ref.Write(largeFileContent)
+
+	md5repos := fmt.Sprintf("%v", md5repo.Sum(nil))
+	md5refs := fmt.Sprintf("%v", md5ref.Sum(nil))
+
+	if md5repos != md5refs {
+		t.Errorf("expected md5 for large file %v, found %v", md5refs, md5repos)
+
+	}
+}
+
+func TestAuth(t *testing.T) {
+	gt := setup(new(ForbidAll))
+	defer shutdown(gt)
+
+	clone, _ := ioutil.TempDir(os.TempDir(), "gitsrv")
+	defer os.RemoveAll(clone)
+	if _, e := exec.Command("git", "clone", fmt.Sprintf("http://foo:bar@localhost:%d", gt.port), clone).CombinedOutput(); e == nil {
+		t.Error("expected permission denied error")
+	}
+}
+
+func TestHttpAuth(t *testing.T) {
+	gt := setup(new(HttpAuth))
+	defer shutdown(gt)
+
+	clone, _ := ioutil.TempDir(os.TempDir(), "gitsrv")
+	defer os.RemoveAll(clone)
+	if _, e := exec.Command("git", "clone", fmt.Sprintf("http://foo:bar_@localhost:%d", gt.port), clone).CombinedOutput(); e == nil {
+		t.Error("expected permission denied error")
+	}
+
+	if out, e := exec.Command("git", "clone", fmt.Sprintf("http://foo:bar@localhost:%d", gt.port), clone).CombinedOutput(); e != nil {
+		t.Error(string(out))
+	}
+}
