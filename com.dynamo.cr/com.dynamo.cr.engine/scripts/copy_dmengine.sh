42ad1fadcdffd81f2088ab22b84385a6099a0628
Christian Murray <cgmurray@gmail.com>
2013-07-03 15:53:13 +0200

    Android archiving support. case 1765

--- com.dynamo.cr/com.dynamo.cr.engine/scripts/copy_dmengine.sh
+++ com.dynamo.cr/com.dynamo.cr.engine/scripts/copy_dmengine.sh
@@ -3,6 +3,7 @@ mkdir -p engine/linux
 mkdir -p engine/darwin
 mkdir -p engine/win32
 mkdir -p engine/ios
+mkdir -p engine/android
 
 SHA1=`git log --oneline | head -1 | awk '{ print $1 }'`
 
@@ -14,3 +15,5 @@ scp builder@ci-master.defold.com:builds/win32/dmengine.exe.${SHA1} engine/win32/
 scp builder@ci-master.defold.com:builds/win32/dmengine_release.exe.${SHA1} engine/win32/dmengine_release.exe
 scp builder@ci-master.defold.com:builds/armv7-darwin/dmengine.${SHA1} engine/ios/dmengine
 scp builder@ci-master.defold.com:builds/armv7-darwin/dmengine_release.${SHA1} engine/ios/dmengine_release
+scp builder@ci-master.defold.com:builds/armv7-android/libdmengine.so.${SHA1} engine/android/dmengine
+scp builder@ci-master.defold.com:builds/armv7-android/libdmengine_release.so.${SHA1} engine/android/dmengine_release
