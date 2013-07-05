460dd062c7e01f7c8be99b27bb28f87b94db40bf
Ragnar Svensson <ragnar.svensson@gmail.com>
2013-07-05 12:43:03 +0200

    Temp disabled apkc copy

--- com.dynamo.cr/com.dynamo.cr.target/scripts/copy_apkc.sh
+++ com.dynamo.cr/com.dynamo.cr.target/scripts/copy_apkc.sh
@@ -7,4 +7,4 @@ SHA1=`git log --oneline | head -1 | awk '{ print $1 }'`
 
 scp builder@ci-master.defold.com:/archive/go/darwin/apkc.${SHA1} lib/darwin/apkc
 scp builder@ci-master.defold.com:/archive/go/linux/apkc.${SHA1} lib/linux/apkc
-scp builder@ci-master.defold.com:/archive/go/win32/apkc.${SHA1} lib/win32/apkc
+#scp builder@ci-master.defold.com:/archive/go/win32/apkc.${SHA1} lib/win32/apkc
