e69c866b9325ce38184729a905249d9c6c8e9551
Christian Murray <cgmurray@gmail.com>
2013-08-07 15:06:59 +0200

    Fixed glsl compilation .bat-scripts. case 2260

--- engine/graphics/bin/glslfc.bat
+++ engine/graphics/bin/glslfc.bat
@@ -1,6 +1,11 @@
-cgc -q -ogles -profile arbfp1 %1 >NUL
-if %errorlevel% GTR 0 exit %errorlevel%
 echo #ifdef GL_ES > %2
 echo precision mediump float; >> %2
 echo #endif >> %2
+echo #ifndef GL_ES >> $2
+echo #define lowp >> $2
+echo #define mediump >> $2
+echo #define highp >> $2
+echo #endif >> $2
 type %1 >> %2
+cgc -q -oglsl -profile arbfp1 %2 >NUL
+if %errorlevel% GTR 0 exit %errorlevel%
