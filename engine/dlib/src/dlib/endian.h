356d7eb8012fe70a24e0745634314eeafeb7fdff
Christian Murray <cgmurray@gmail.com>
2013-08-08 09:53:09 +0200

    Android endian check support. case 2263

--- engine/dlib/src/dlib/endian.h
+++ engine/dlib/src/dlib/endian.h
@@ -8,7 +8,7 @@
 #define DM_ENDIAN_LITTLE 0
 #define DM_ENDIAN_BIG 1
 
-#if defined(__x86_64) || defined(__x86_64__) || defined(_X86_) || defined(__i386__) || defined(_M_IX86) || defined(__LITTLE_ENDIAN__)
+#if defined(__x86_64) || defined(__x86_64__) || defined(_X86_) || defined(__i386__) || defined(_M_IX86) || defined(__LITTLE_ENDIAN__) || (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
 #define DM_ENDIAN DM_ENDIAN_LITTLE
 #else
 #error "Unknown endian"
