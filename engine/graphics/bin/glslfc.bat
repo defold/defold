d97ff572a25ad4bfa56d54d6bffdd0e9a800c47f
Christian Murray <cgmurray@gmail.com>
2013-08-12 22:52:40 +0200

    Fixed glsl*.bat script
    * Type: $ instead of %
    * Enabled shader validation again

--- engine/graphics/bin/glslfc.bat
+++ engine/graphics/bin/glslfc.bat
@@ -1,11 +1,11 @@
 echo #ifdef GL_ES > %2
 echo precision mediump float; >> %2
 echo #endif >> %2
-echo #ifndef GL_ES >> $2
-echo #define lowp >> $2
-echo #define mediump >> $2
-echo #define highp >> $2
-echo #endif >> $2
+echo #ifndef GL_ES >> %2
+echo #define lowp >> %2
+echo #define mediump >> %2
+echo #define highp >> %2
+echo #endif >> %2
 type %1 >> %2
-rem cgc -q -oglsl -profile arbfp1 %2 >NUL
+cgc -q -oglsl -profile arbfp1 %2 >NUL
 if %errorlevel% GTR 0 exit %errorlevel%
