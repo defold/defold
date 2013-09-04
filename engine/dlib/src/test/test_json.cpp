0129aa88779982500cb2a78c339156859c511373
chmu <chmu@ip-10-39-13-107.eu-west-1.compute.internal>
2013-09-04 12:01:52 +0000

    Fix for invalid string replacement. case 2310

--- engine/dlib/src/test/test_json.cpp
+++ engine/dlib/src/test/test_json.cpp
@@ -179,7 +179,13 @@ TEST_F(dmJsonTest, Flickr)
     // Flickr escape ' with \'
     // This is invalid json so we replace \' with ' before parsing
     std::string json((const char*) FLICKR_JSON, (size_t) FLICKR_JSON_SIZE);
-    json = json.replace(json.begin(), json.end(), "\\'", "'");
+    size_t pos = 0;
+    std::string search = "\\'";
+    std::string replace = "'";
+    while ((pos = json.find(search, pos)) != std::string::npos) {
+        json.replace(pos, search.length(), replace);
+        pos += replace.length();
+    }
     dmJson::Result r = dmJson::Parse(json.c_str(), &doc);
     ASSERT_EQ(dmJson::RESULT_OK, r);
 }
