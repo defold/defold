ef9085bb45a1b66f8a3a1cb8f9c278c515bc62c8
Ragnar Svensson <ragnar.svensson@gmail.com>
2013-09-25 22:25:14 +0200

    Bumped to 1.2.24 and flipped appengine version.

--- com.dynamo.cr/com.dynamo.cr.editor.core/src/com/dynamo/cr/editor/core/EditorCorePlugin.java
+++ com.dynamo.cr/com.dynamo.cr.editor.core/src/com/dynamo/cr/editor/core/EditorCorePlugin.java
@@ -22,8 +22,8 @@ public class EditorCorePlugin extends Plugin implements IResourceTypeRegistry {
     // NOTE: Currently hard-coded. Non-trivial to find the product number :-(
 	// The version-number is updated
 	// with build-script (bump command)
-    public static final String VERSION = "1.2.23";
-    public static final String VERSION_SHA1 = "cb27dc7";
+    public static final String VERSION = "1.2.24";
+    public static final String VERSION_SHA1 = "df1c2d4";
 
     private static BundleContext context;
     private static EditorCorePlugin plugin;
--- engine/engine/src/engine_version.h
+++ engine/engine/src/engine_version.h
@@ -3,8 +3,8 @@
 
 namespace dmEngineVersion
 {
-    static const char* VERSION = "1.2.23";
-    static const char* VERSION_SHA1 = "cb27dc7";
+    static const char* VERSION = "1.2.24";
+    static const char* VERSION_SHA1 = "df1c2d4";
 }
 
 #endif // DM_ENGINE_VERSION
