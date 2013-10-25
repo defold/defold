a84b1f1c8c3d9379931869cb86a5c00d91bafa84
Christian Murray <cgmurray@gmail.com>
2013-10-25 11:51:07 +0200

    iOS symbolicate script

---
+++ scripts/symbolicate.py
@@ -0,0 +1,61 @@
+import re, sys, os, subprocess
+
+# Script to "symbolicate" an iOS crash log.
+# Not particularly pretty script but it works...
+
+def run_cmd(cmd):
+    p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+    return p.stdout.read()
+
+def parse_stacktrace(cd, slide, load_addr):
+    lst = []
+    for x in re.findall(r'^Thread \d+( Crashed)?:(.*?)^[ ]*$', cd, re.MULTILINE | re.DOTALL):
+        x = x[1]
+        tmp = [ y.strip() for y in x.split('\n') if y.strip() != '' ]
+        st = []
+        for y in tmp:
+            lst2 = filter(len, re.split(r'[ \t]', y))
+            stack = int(lst2[2].strip(), 16)
+            address = slide + stack - load_addr
+            st.append((lst2[1], address))
+        lst.append(st)
+    return lst
+
+def get_address_map(stack_trace):
+    addresses = reduce(lambda x,y: x + y,  [ [ y[1] for y in x ] for x in stack_trace ], [])
+    out = run_cmd('atos -arch armv7 -o %s %s' % (sys.argv[2], ['0x%x' % a for a in addresses]))[1:-2]
+    lst = [ s.strip().replace(',', '') for s in  out.split('\n') ]
+    address_map = {}
+    for i,s in enumerate(lst):
+        address_map[addresses[i]] = s
+    return address_map
+
+def symbolicate(cd):
+    id = re.search(r'^Identifier:[ ]*(.*?)$', cd, re.MULTILINE | re.DOTALL).groups()[0]
+    load_addr = int(re.search(r'^Binary Images:.*?0x(.*?)[ ]', cd, re.MULTILINE | re.DOTALL).groups()[0], 16)
+    uuid = re.search(r'^Binary Images:.*?<(.*?)>[ ]', cd, re.MULTILINE | re.DOTALL).groups()[0].lower()
+    slide = 0x1000
+
+    load_cmds = run_cmd('otool -l %s' % sys.argv[2])
+    exe_uuid = re.search(r'^\s*?uuid\s*?(.*?)$', load_cmds, re.MULTILINE | re.DOTALL).groups()[0].strip().replace('-', '').lower()
+
+    if uuid != exe_uuid:
+        print 'FATAL: uuid mismatch. %s != %s' % (uuid, exe_uuid)
+        sys.exit(5)
+
+    stack_trace = parse_stacktrace(cd, slide, load_addr)
+    address_map = get_address_map(stack_trace)
+
+    for i, t in enumerate(stack_trace):
+        print "thread %d:" % (i)
+        for j, s in enumerate(t):
+            address = s[1]
+            print '%2s:    %30s  %s (0x%x)' % (j, s[0], str(address_map[address]), address)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print 'usage: symbolicate <crashdump> <executable>'
+        print ''
+        sys.exit(5)
+    cd = open(sys.argv[1], 'rb').read()
+    symbolicate(cd)
