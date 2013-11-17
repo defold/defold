57d5cd124a7ac052263023ed3df23be3d66344dd
Christian Murray <cgmurray@gmail.com>
2013-11-17 15:24:15 +0100

    Use DYLD_FORCE_FLAG_NAMESPACE instead of -flat_namespace. case 2409
    * Workaround for random link errors with clang on OSX

--- engine/dlib/scripts/run_test_memprofile.sh
+++ engine/dlib/scripts/run_test_memprofile.sh
@@ -3,6 +3,8 @@ set -e
 platform=`uname -s`
 
 if [ "Darwin" == $platform ]; then
+    # NOTE: We used to link with -flat_namespace but go spurious link errors
+    export DYLD_FORCE_FLAT_NAMESPACE=yes
     export DYLD_INSERT_LIBRARIES=./build/default/src/libdlib_memprofile.dylib
 fi
 
