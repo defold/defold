2300c6959b2538f633589cea18384cd97f326bd5
Ragnar Svensson <ragnar.svensson@gmail.com>
2013-12-17 22:30:24 +0100

    Refactored dmArray to conform to code style. case 2435
    * Also improved docs

--- engine/dlib/src/dlib/array.cpp
+++ engine/dlib/src/dlib/array.cpp
@@ -1,35 +1,43 @@
-
-#include <string.h>
 #include "array.h"
 
+#include <assert.h>
+#include <string.h>
 
-void dmArrayHelper::SetCapacity(uint32_t new_capacity, uint32_t type_size, uintptr_t* ptr_first, uintptr_t* ptr_last, uintptr_t* ptr_end)
+namespace dmArrayUtil
 {
-    uintptr_t old_capacity = (*ptr_last - *ptr_first)/type_size;
-    if(new_capacity == old_capacity)
-        return;
-
-    uint8_t *new_block;
-    if(new_capacity)
+    void SetCapacity(uint32_t capacity, uint32_t type_size, uintptr_t* first, uintptr_t* last, uintptr_t* end)
     {
-        new_block = new uint8_t[new_capacity*type_size];
-        assert (new_block != 0 && "SetCapacity failed allocating memory");
+        uintptr_t old_capacity = (*last - *first) / type_size;
+        // early-out for now change
+        if (capacity == old_capacity)
+            return;
+
+        // new backing storage
+        uint8_t *new_storage;
+        if (capacity)
+        {
+            new_storage = new uint8_t[capacity * type_size];
+            assert (new_storage != 0 && "SetCapacity could not allocate memory");
+        }
+        else
+        {
+            new_storage = 0;
+        }
+
+        uintptr_t old_size = (*end - *first) / type_size;
+        // truncate size if the new capacity is less
+        uintptr_t new_size = old_size < capacity ? old_size : capacity;
+
+        // copy content if any
+        if (old_capacity)
+        {
+            memcpy(new_storage, (void *)*first, (size_t)(type_size * (size_t)new_size));
+            delete[] (uint8_t*) *first;
+        }
+
+        *first = (uintptr_t)new_storage;
+        *end   = (uintptr_t)(new_storage + (new_size * type_size));
+        *last  = (uintptr_t)(new_storage + (capacity * type_size));
     }
-    else
-        new_block = 0;
-
-    uintptr_t old_size = (*ptr_end - *ptr_first)/type_size;
-    uintptr_t new_size = old_size;
-    new_size = new_size < new_capacity ? new_size : new_capacity;
-
-    if(old_capacity)
-    {
-        memcpy(new_block, (void *)*ptr_first, (size_t)((size_t)type_size*(old_size < new_capacity ? old_size : new_capacity)));
-        delete[] (uint8_t*) *ptr_first;
-    }
-
-    *ptr_first = (uintptr_t) new_block;
-    *ptr_end   = (uintptr_t) (new_block + (new_size*type_size));
-    *ptr_last  = (uintptr_t) (new_block + (new_capacity*type_size));
 }
 
