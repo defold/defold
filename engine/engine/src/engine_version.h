52078a621d06268923a3594f501195b683519b83
Ragnar Svensson <ragnar.svensson@gmail.com>
2014-01-09 21:08:54 +0100

    Bumped to 1.2.27

--- engine/engine/src/engine_version.h
+++ engine/engine/src/engine_version.h
@@ -3,8 +3,8 @@
 
 namespace dmEngineVersion
 {
-    static const char* VERSION = "1.2.26";
-    static const char* VERSION_SHA1 = "4c036ac";
+    static const char* VERSION = "1.2.27";
+    static const char* VERSION_SHA1 = "1f68773";
 }
 
 #endif // DM_ENGINE_VERSION
