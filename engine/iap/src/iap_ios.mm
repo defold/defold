f55cbbcbef419f3c24282db9d210eee3e006e165
Ragnar Svensson <ragnar.svensson@gmail.com>
2014-02-25 11:45:02 +0100

    Added "currency_code" to product listing from IAP iOS. case 2491

--- engine/iap/src/iap_ios.mm
+++ engine/iap/src/iap_ios.mm
@@ -120,6 +120,10 @@ static void PushError(lua_State*L, NSError* error)
         lua_pushstring(L, [price_string UTF8String]);
         lua_rawset(L, -3);
 
+        lua_pushstring(L, "currency_code");
+        lua_pushstring(L, [[product.priceLocale objectForKey:NSLocaleCurrencyCode] UTF8String]);
+        lua_rawset(L, -3);
+
         lua_rawset(L, -3);
     }
     lua_pushnil(L);
