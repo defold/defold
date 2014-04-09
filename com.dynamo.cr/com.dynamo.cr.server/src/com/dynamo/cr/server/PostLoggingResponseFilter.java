defold/defold

commit 8eb19452ecc72b9a78e72ef8ac9e71e4389b6b0f

Added more logging to find reason of failing testProjectInfo. case 2369

com.dynamo.cr/com.dynamo.cr.server/src/com/dynamo/cr/server/PostLoggingResponseFilter.java
@@ -15,7 +15,6 @@ public class PostLoggingResponseFilter implements ContainerResponseFilter {
     @Override
     public ContainerResponse filter(ContainerRequest request,
             ContainerResponse response) {
-        logger.info("");
         MDC.remove("remoteAddr");
         MDC.remove("userId");
         MDC.remove("method");

com.dynamo.cr/com.dynamo.cr.server/src/logback.xml
@@ -19,7 +19,7 @@
 
   <appender name="STDERR" class="ch.qos.logback.core.ConsoleAppender">
     <filter class="ch.qos.logback.classic.filter.ThresholdFilter">
-      <level>WARN</level>
+      <level>INFO</level>
     </filter>
 
     <encoder>

com.dynamo.cr/com.dynamo.cr.server.test/src/com/dynamo/cr/server/resources/test/ProjectsResourceTest.java
@@ -19,7 +19,13 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.ObjectNode;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.dynamo.cr.common.providers.JsonProviders;
 import com.dynamo.cr.common.providers.ProtobufProviders;
@@ -60,6 +66,8 @@ public class ProjectsResourceTest extends AbstractResourceTest {
             this.projectsResource = projectsResource;
         }
     }
+
+    protected static Logger logger = LoggerFactory.getLogger(ProjectsResourceTest.class);
 
     static int port = 6500;
     String joeEmail = "joe@foo.com";
@@ -182,11 +190,22 @@ public class ProjectsResourceTest extends AbstractResourceTest {
         bobClient.destroy();
     }
 
+    @Rule
+    public TestRule watcher = new TestWatcher() {
+        protected void starting(Description description) {
+            logger.info("Starting test: " + description.getMethodName());
+        }
+
+        @Override
+        protected void finished(Description description) {
+            logger.info("Finished test: " + description.getMethodName());
+        }
+    };
+
     @Test
     public void testProjectInfo() throws Exception {
-        NewProject newProject = NewProject.newBuilder()
-                .setName("test project")
-                .setDescription("New test project").build();
+        NewProject newProject = NewProject.newBuilder().setName("test project").setDescription("New test project")
+                .build();
 
         ProjectInfo projectInfo = joeProjectsWebResource
             .path(joeUser.getId().toString())

commit 5314f037febcb1b14077e6c98c31b2f2297f27d0

Fixed server access logging. case 2514
* Broken in commit 8eb19452ecc72b9a78e72ef8ac9e71e4389b6b0f
* Also reverted invalid log-level set. From INFO
  to WARN in default logback.xml

com.dynamo.cr/com.dynamo.cr.server/src/com/dynamo/cr/server/PostLoggingResponseFilter.java
@@ -15,6 +15,8 @@ public class PostLoggingResponseFilter implements ContainerResponseFilter {
     @Override
     public ContainerResponse filter(ContainerRequest request,
             ContainerResponse response) {
+        // NOTE: Do *NOT* remove this statement. It's used for access logging.
+        logger.info("");
         MDC.remove("remoteAddr");
         MDC.remove("userId");
         MDC.remove("method");

com.dynamo.cr/com.dynamo.cr.server/src/logback.xml
@@ -19,7 +19,7 @@
 
   <appender name="STDERR" class="ch.qos.logback.core.ConsoleAppender">
     <filter class="ch.qos.logback.classic.filter.ThresholdFilter">
-      <level>INFO</level>
+      <level>WARN</level>
     </filter>
 
     <encoder>