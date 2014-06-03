03c5899109faeccd8413e62c25d09857a1272195
Michael Nygard <mtnygard@gmail.com>
2014-06-03 16:40:48 -0500

    Stabilize classpaths and library locations.

--- com.dynamo.cr/com.dynamo.cr.clojure-eclipse/src/clj/eclipse/handlers.clj
+++ com.dynamo.cr/com.dynamo.cr.clojure-eclipse/src/clj/eclipse/handlers.clj
@@ -1,6 +1,29 @@
 (ns eclipse.handlers
-  (:require [eclipse.tracer :as log]))
+  (:require [eclipse.tracer :as log])
+  (:import [org.eclipse.core.expressions IEvaluationContext]
+           [org.eclipse.ui ISources]))
+
+(def ^:private variable-names
+  )
+
+(defn- variables [event]
+  (when (instance? IEvaluationContext (.getApplicationContext event))
+    (map (fn [n] [n (.getVariable event n)]) (variable-names))))
+
+(defn- log-variables [event]
+  (doseq [[n v] (variables event)]
+    (log/info n " = " v)))
 
 (defn lookup [event]
   (log/info "Event " event " received in clojure lookup function")
+  (log-variables event)
   println)
+
+(comment
+  		if (event.getApplicationContext() instanceof IEvaluationContext) {
+			Object var = ((IEvaluationContext) event.getApplicationContext())
+					.getVariable(name);
+			return var == IEvaluationContext.UNDEFINED_VARIABLE ? null : var;
+		}
+		return null;
+  )
