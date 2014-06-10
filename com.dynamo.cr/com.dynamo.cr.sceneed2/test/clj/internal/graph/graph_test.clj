e05896af33df2f9c0d6fb9dfd440fdc96ca250a8
Michael Nygard <mtnygard@gmail.com>
2014-06-10 16:00:20 -0500

    72874222: writing test specs to define graph behavior

---
+++ com.dynamo.cr/com.dynamo.cr.sceneed2/test/clj/internal/graph/graph_test.clj
@@ -0,0 +1,71 @@
+(ns internal.graph.graph-test
+  (:use clojure.test))
+
+;- Instantiate node
+
+;; Given any graph
+;;  when I create a node
+;;  then I can discover the node's unique identifier
+
+;; Given an empty graph
+;;  when I create a node with a name and type
+;;  then I can look up the node by its name
+;;   and I can look up the node by its type
+
+;; Given an existing graph
+;;  when I create a node with a name and type
+;;  then I can look up the node by its name
+;;   and I can look up the node by its type
+
+;; Given an graph that has a named node
+;;  when I try to create a node with the same name
+;;  then I can discover the node's unique identifier
+;;   and I can look up the node by its name
+;;   and I can look up the node by its identifier
+
+;- Query for nodes matching criteria (satisfies a protocol or set of protocols, matches properties)
+
+;; Given a graph with multiple nodes having the same name
+;;  when I look up the nodes' name
+;;  then I get a collection of nodes with that name
+
+;; Given a populated graph
+;;  when I query for nodes that match some criteria 
+;;  then I get a possibly empty collection of all nodes matching those criteria
+
+;; Given a graph with a node A with an output label "x"
+;;   and a node B with an input label "y"
+;;   and "y" can receive input from "x"
+;;  when I ask to connect "x" to "y"
+;;  then I can ask what the target of A's "x" output is
+;;   and I can ask what the source of B's "y" input is
+
+;; Given a graph with some nodes
+;;  when I connect two nodes' outputs to one target node
+;;  then each source node will have the same output
+;;   and the target node will have both sources on the same input.
+
+;; Given a graph with some nodes
+;;   and I connect multiple nodes to the same input
+;;  then each source node will have the same output
+;;   and the target node will have them all on the same input
+
+;; Given a graph with a node A with an output label "x"
+;;   and a node B with an input label "y"
+;;   and a connection from x to y
+;;  when I disconnect A from B
+;;  then x no longer appears in B's "y" input
+;;   and A's "x" output will be empty
+
+;; Given a graph with a node A with an output label "x"
+;;   and a node B with an input label "y"
+;;   and a connection from x to y
+;;  when I remove A from the graph
+;;  then x no longer appears in B's "y" input
+
+
+;; Cases still needed for the following actions:
+;- Delete node
+;- Apply a function to a node
+;- Traverse arcs by label
+;- Transactions
