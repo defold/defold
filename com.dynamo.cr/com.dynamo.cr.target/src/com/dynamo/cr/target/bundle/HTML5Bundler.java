33edf03ab125c608f2467c1fbbaec73ccaa08fba
Peter Hodges <peter.hodges@king.com>
2014-06-30 16:21:45 +0200

    Using IDBFS, rather than memory based file system. Application entry deferred until filesystem is mounted. Template html and related resources moved into resource bundle.

--- com.dynamo.cr/com.dynamo.cr.target/src/com/dynamo/cr/target/bundle/HTML5Bundler.java
+++ com.dynamo.cr/com.dynamo.cr.target/src/com/dynamo/cr/target/bundle/HTML5Bundler.java
@@ -2,15 +2,16 @@ package com.dynamo.cr.target.bundle;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.regex.Pattern;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 
 import com.dynamo.cr.editor.core.ProjectProperties;
@@ -18,7 +19,7 @@ import com.dynamo.cr.editor.core.ProjectProperties;
 public class HTML5Bundler {
     private String contentRoot;
     private File appDir;
-    private String html;
+    private String projectHtml;
     private String js;
     private String title;
 
@@ -32,9 +33,12 @@ public class HTML5Bundler {
      */
     public HTML5Bundler(ProjectProperties projectProperties, String js, String projectRoot, String contentRoot, String outputDir) {
         this.js = js;
-        String defaultHtml = FilenameUtils.getFullPath(js) + File.separator + "engine_template.html";
-        String projectHtml = projectProperties.getStringValue("project", "htmlfile", "").trim();
-        this.html = (projectHtml != null && projectHtml.length() > 0 ? projectHtml : defaultHtml);        	
+
+        this.projectHtml = projectProperties.getStringValue("project", "htmlfile", null);
+        if (this.projectHtml != null) {
+        	this.projectHtml = this.projectHtml.trim();
+        }
+
         this.contentRoot = contentRoot;
 
         File packageDir = new File(outputDir);
@@ -51,16 +55,41 @@ public class HTML5Bundler {
             FileUtils.copyFile(new File(contentRoot, name), new File(appDir, name));
         }
 
-        // Copy engine        
+        // Copy engine
         File jsOut = new File(appDir, String.format("%s.js", title));
-        FileUtils.copyFile(new File(js), jsOut);        
+        FileUtils.copyFile(new File(js), jsOut);
 
         // Copy html (and replace placeholders)
         // TODO: More efficient way to do the placeholder replacing needed...
         Charset cs = Charset.forName("UTF-8");
         File htmlOut = new File(appDir, String.format("%s.html", title));
-        String htmlText = IOUtils.toString(new FileInputStream(new File(html)), cs);
+        String htmlText = GetHtmlText();
         htmlText = htmlText.replaceAll(Pattern.quote("${DMENGINE_JS}$"), title + ".js");
         IOUtils.write(htmlText, new FileOutputStream(htmlOut), cs);
+
+        CopyResource(appDir, "modernizr.custom.js");
+        CopyResource(appDir, "IndexedDBShim.min.js");
+    }
+
+    private String GetHtmlText() throws FileNotFoundException, IOException {
+    	String data = null;
+    	if (this.projectHtml != null && this.projectHtml.length() > 0) {
+    		data = IOUtils.toString(new FileInputStream(new File(this.projectHtml)), Charset.forName("UTF-8"));
+    	} else {
+    		InputStream resource = getClass().getResourceAsStream("resources/jsweb/engine_template.html");
+            data = IOUtils.toString(resource);
+            resource.close();
+    	}
+    	return data;
+    }
+
+    private void CopyResource(File targetDir, String sourceFile) throws FileNotFoundException, IOException {
+    	String sourcePath = "resources/jsweb/" + sourceFile;
+
+    	File file = new File(this.appDir, sourceFile);
+    	FileOutputStream output = new FileOutputStream(file);
+
+    	InputStream resource = getClass().getResourceAsStream(sourcePath);
+    	IOUtils.copy(resource, output);
     }
 }
