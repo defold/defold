4c3768195a897a475f19d0240e8cc40bb8b958b0
Michael Nygard <mtnygard@gmail.com>
2014-08-10 21:18:47 -0500

    Record some additional thoughts about tool code syntax and representation.

--- com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/basement.clj
+++ com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/basement.clj
@@ -1,42 +1,118 @@
 (ns basement)
 
 (comment
+  ;; This file explores alternate syntax for the "above the waterline" code.
 
   ;; in atlas.node?
-
   (Node Atlas <: OutlineNode, Renderable
-      inputs:
-      OutlineItem assets
-      ImageSource images
-      Animation animations
+        inputs:
+        OutlineItem assets
+        ImageSource images
+        Animation animations
 
-      properties:
-      Int margin = 0
-      Int extrude-borders = 0
+        properties:
+        Int margin = 0
+        Int extrude-borders = 0
 
-      outputs:
-      [cached] textureset = fn (pack-textures .margin .extrude-borders (consolidate .images .animations)))
+        outputs:
+        [cached] textureset = fn (pack-textures .margin .extrude-borders (consolidate .images .animations)))
 
-;; in atlas.clj
+  ;; in atlas.clj
+  (node Atlas :extends Outline Renderable
+        (inputs
+         assets :many OutlineItem
+         images :many ImageSource
+         foo    :one Foo)
 
-(node Atlas :extends Outline Renderable
-      (inputs
-        assets :many OutlineItem
-        images :many ImageSource
-        foo    :one Foo)
+        (properties
+         [margin :one Int :default 0 :must-be #(< 0 %)])
 
-      (properties
-        [margin :one Int :default 0 :must-be #(< 0 %)])
+        (outputs
+         [textureset :one TextureSet :using (pack-textures .margin .extrude-borders (consolidate .images .animations)) :cached])
 
-      (outputs
-        [textureset :one TextureSet :using (pack-textures .margin .extrude-borders (consolidate .images .animations)) :cached])
+        (produce textureset :one TextureSet :using (pack-textures .margin .extrude-borders (consolidate .images .animations)) :cached)
 
-      (produce textureset :one TextureSet :using (pack-textures .margin .extrude-borders (consolidate .images .animations)) :cached)
+        (on-load
+         (fn [this]
+           (connect this :textureset (CompilerNode.) :texturesetc)))
+        )
 
-      (on-load
-        (fn [this]
-          (connect this :textureset (CompilerNode.) :texturesetc)))
-      )
+  ;; in this version, each node type gets its own go-loop for
+  ;; processing incoming events. (Possible event types are shown
+  ;; below.)
+  ;;
+  ;; here, defnode is a macro that "compiles" this description into
+  ;; the faceted behaviors we use today, plus an event-handling
+  ;; function that runs a stateful go loop for each node.
+  ;;
+  ;; we can handle a large number of nodes with go-loops because an
+  ;; idle loop does not consume a thread. it only requires some memory
+  ;; for the automatically generated state machine.
+  (defnode Atlas
+    (output 'textureset TextureSet
+            [arglist]
+            fn-tail)
+
+    (input 'images [Image])
+    (input 'animations [Animation])
+
+    (property 'filename Path)
+    (property 'margin Integer)
+    (property 'extrude-borders Integer)
+
+    (command 'add-animation ...)
+
+    (command 'add-image
+             (when-let [new-image (pickr ...)]
+               (let [img (new ImageNode new-image)]
+                 (attach img 'image self 'images)
+                 (send self 'images-changed))))
+
+    (on 'post-create
+        (let [compiler (new AtlasCompiler)]
+          (attach self 'textureset compiler 'textureset)
+          (attach self 'filename compiler 'filename)))
+
+    (on 'select ...)
+
+    (on 'images-changed
+        (invalidate 'textureset)
+        (invalidate 'texturesetc)))
 
 
+  ;; node events
+  ;; selection
+  :select
+  :deselect
+
+  ;; lifecycle
+  :create
+  :init
+  :post-create
+  :pre-destroy
+  :destroy
+
+  ;; tree manipulation
+  :attach-to-parent
+  :post-attach-to-parent
+  :attach-child
+  :post-attach-child
+  :detach-child
+  :post-detach-child
+  :detach-from-parent
+  :post-detach-from-parent
+
+  ;; value changes
+  :property-change
+  :cache-invalidate
+
+  ;; file interaction
+  :load-source
+  :save-source
+
+  ;; clipboard (no need for specific copy or begin-drag ops, because everything is a value.)
+  :can-paste?
+  :accept-paste
+  :can-drop?
+  :accept-drop
   )
