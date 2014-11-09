607f39ff6f5db85e4b972ac381137eec9bc544e1
Christian Murray <christian.murray@king.com>
2014-11-09 18:44:05 +0100

    Extract luajit directly from packages. DEF-621
    * We need luajit for all platforms
    * Updated .gitignore

--- com.dynamo.cr/com.dynamo.cr.bob/scripts/copy_luajit.sh
+++ com.dynamo.cr/com.dynamo.cr.bob/scripts/copy_luajit.sh
@@ -9,7 +9,12 @@ mkdir -p share
 
 SHA1=`git log --pretty=%H -n1`
 
-cp -v $DYNAMO_HOME/archive/${SHA1}/ext/bin/linux/luajit libexec/linux/luajit
-cp -v $DYNAMO_HOME/archive/${SHA1}/ext/bin/darwin/luajit libexec/darwin/luajit
-cp -v $DYNAMO_HOME/archive/${SHA1}/ext/bin/win32/luajit.exe libexec/win32/luajit.exe
+mkdir -p tmp
+tar xf ../../packages/luajit-2.0.3-win32.tar.gz -C tmp
+tar xf ../../packages/luajit-2.0.3-linux.tar.gz -C tmp
+tar xf ../../packages/luajit-2.0.3-darwin.tar.gz -C tmp
+
+cp -v tmp/bin/linux/luajit libexec/linux/luajit
+cp -v tmp/bin/darwin/luajit libexec/darwin/luajit
+cp -v tmp/bin/win32/luajit.exe libexec/win32/luajit.exe
 cp -v -r $DYNAMO_HOME/ext/share/luajit share
