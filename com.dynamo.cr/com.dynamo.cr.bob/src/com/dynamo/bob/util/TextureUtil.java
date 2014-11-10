fecb37c5b938a54a561098bbd543c91703bcfff7
Peter Hodges <peter.hodges@king.com>
2014-11-10 12:52:33 +0100

    DEF-854: convert palettised images before extruding borders.

--- com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/util/TextureUtil.java
+++ com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/util/TextureUtil.java
@@ -1,6 +1,7 @@
 package com.dynamo.bob.util;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,7 +24,7 @@ public class TextureUtil {
 
     /**
      * Util method to get the image type from an image, or best guess when it's lacking (0).
-     * 
+     *
      * The current java version fails for new BufferedImage(..., 0), which is 0 as image type
      */
     public static int getImageType(BufferedImage image) {
@@ -60,11 +61,14 @@ public class TextureUtil {
 
     public static BufferedImage extrudeBorders(BufferedImage src, int extrudeBorders) {
         if (extrudeBorders > 0) {
+            src = depalettiseImage(src);
+
             int origWidth = src.getWidth();
             int origHeight = src.getHeight();
             int newWidth = origWidth + extrudeBorders * 2;
             int newHeight = origHeight + extrudeBorders * 2;
             int type = getImageType(src);
+
             BufferedImage tgt = new BufferedImage(newWidth, newHeight, type);
             int numComponents = src.getColorModel().getNumComponents();
             int[] srcPixels = new int[origWidth * origHeight * numComponents];
@@ -86,5 +90,34 @@ public class TextureUtil {
         } else {
             return src;
         }
+    }
+
+    public static BufferedImage depalettiseImage(BufferedImage src) {
+        BufferedImage result = null;
+        if (BufferedImage.TYPE_BYTE_INDEXED == src.getType()) {
+            ColorModel indexedCM = src.getColorModel();
+
+            int width = src.getWidth();
+            int height = src.getHeight();
+
+            result = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
+            int[] srcPixels = new int[width * height];
+            int[] tgtPixels = new int[width * height];
+
+            src.getRaster().getPixels(0, 0, width, height, srcPixels);
+
+            for (int y=0; y<height; ++y) {
+                for (int x=0; x<width; ++x) {
+                    int offset = x + width * y;
+                    int index = srcPixels[offset];
+                    int colour = indexedCM.getRGB(index);
+                    tgtPixels[offset] = colour;
+                }
+            }
+            result.setRGB(0, 0, width, height, tgtPixels, 0, width);
+        } else {
+            result = src;
+        }
+        return result;
     }
 }
