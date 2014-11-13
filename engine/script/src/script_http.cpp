8866211da65e5f9c80328b8f8fdd29a5e009c3a5
Mikael Säker <mikael@sicher.org>
2014-11-13 13:51:54 +0100

    Various API doc fixes.
    - Some missing descriptions.
    - Some parameter and return values missing.
    - Typos and style fixes.

--- engine/script/src/script_http.cpp
+++ engine/script/src/script_http.cpp
@@ -30,7 +30,8 @@ namespace dmScript
     int g_ServiceRefCount = 0;
     uint64_t g_Timeout = 0;
 
-    /*# perform http request
+    /*# perform a HTTP request
+     * Perform a HTTP request.
      *
      * @name http.request
      * @param url target url
@@ -40,7 +41,8 @@ namespace dmScript
      * @param [post_data] option data to send
      * @examples
      * <p>
-     * Basic HTTP-GET request:
+     * Basic HTTP-GET request. The callback receives an object with the response
+     * in the fields status, payload (the data) and headers.
      * </p>
      * <pre>
      * local function http_result(response)
