defold/defold

commit 7a874bbe4a10effc1b07fcdad99629de74925431

When loading nodes by filename, reuse an existing node.

com.dynamo.cr/com.dynamo.cr.builtins/builtins/tools/cubemap/core.clj
@@ -169,9 +169,8 @@
     cubemap))
 
 (defn on-edit
-  [project-node editor-site file]
-  (let [cubemap (p/node-by-filename project-node file)
-        editor  (ise/make-scene-editor :name "editor")]
+  [project-node editor-site cubemap]
+  (let [editor (ise/make-scene-editor :name "editor")]
     (ds/transactional
       (ds/in (ds/add editor)
         (let [background (ds/add (background/make-background))

com.dynamo.cr/com.dynamo.cr.builtins/builtins/tools/atlas/core.clj
@@ -497,9 +497,8 @@
     atlas))
 
 (defn on-edit
-  [project-node editor-site file]
-  (let [atlas-node (p/node-by-filename project-node file)
-        editor (make-scene-editor :name "editor")]
+  [project-node editor-site atlas-node]
+  (let [editor (make-scene-editor :name "editor")]
     (transactional
       (in (add editor)
         (let [background (add (make-background))

com.dynamo.cr/com.dynamo.cr.sceneed2.test/src/clj/internal/loader_test.clj
@@ -1,0 +1,30 @@
+(ns internal.loader-test
+  (:require [clojure.test :refer :all]
+            [dynamo.node :as n]
+            [dynamo.project :as p]
+            [dynamo.system :as ds]
+            [dynamo.system.test-support :refer :all]
+            [internal.query :as iq]
+            [schema.core :as s]))
+
+(n/defnode DummyNode
+  (property project s/Any)
+  (property filename String))
+
+(defn- dummy
+  [proj file]
+  (ds/transactional
+   (ds/in proj
+     (ds/add
+      (make-dummy-node :project proj :filename file)))))
+
+(defn- bomb
+  [& _]
+  (throw (ex-info "Should not have been called" {})))
+
+(deftest reuse-same-node
+  (with-clean-world
+    (let [project (ds/transactional (ds/add (p/make-project)))
+          node1   (p/node-by-filename world-ref project "test-resource.txt" dummy)
+          node2   (p/node-by-filename world-ref project "test-resource.txt" bomb)]
+      (is (= (:_id node1) (:_id node2))))))

com.dynamo.cr/com.dynamo.cr.sceneed2.test/src/clj/suite.clj
@@ -1,22 +1,24 @@
 (ns suite
  (:require [clojure.test :as test]))
 
-(def test-namespaces ['internal.node-test
+(def test-namespaces ['internal.injection-test
+                      'internal.node-test
                       'internal.scope-test
-                      'internal.graph.graph-test
-                      'internal.value-test
                       'internal.system-test
                       'internal.type-test
-                      'internal.injection-test
                       'internal.either-test
-                      'dynamo.transaction-test
-                      'dynamo.texture-test
-                      'dynamo.image-test
-                      'dynamo.geom-test
-                      'dynamo.gl.vertex-test
-                      'dynamo.gl.translate-test
+                      'internal.value-test
+                      'internal.graph.graph-test
+                      'internal.render.pass-test
                       'dynamo.camera-test
+                      'dynamo.condition-test
+                      'dynamo.geom-test
+                      'dynamo.image-test
                       'dynamo.protobuf-test
+                      'dynamo.texture-test
+                      'dynamo.transaction-test
+                      'dynamo.gl.translate-test
+                      'dynamo.gl.vertex-test
                       'docs])
 
 (def test-namespaces-for-junit

com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/dynamo/project.clj
@@ -56,15 +56,17 @@
 (defn loader-for [project-scope ext] (handler project-scope :loader ext))
 (defn editor-for [project-scope ext] (handler project-scope :editor ext))
 
-(defn- load-resource
-  [project-scope path]
+(defn load-resource
+  [project-scope file]
   (ds/transactional
     (ds/in project-scope
-      ((loader-for project-scope (file/extension path)) path (io/reader path)))))
+      ((loader-for project-scope (file/extension file)) file (io/reader file)))))
 
 (defn node-by-filename
-  [project-scope filename]
-  (load-resource project-scope (file/project-path project-scope filename)))
+  [world-ref project-scope file factory]
+  (if-let [node (first (iq/query world-ref [[:filename file]]))]
+    node
+    (factory project-scope file)))
 
 (defn- send-project-scope-message
   [graph self txn]

com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/internal/ui/editors.clj
@@ -1,16 +1,17 @@
 (ns internal.ui.editors
-  (:require [dynamo.project :as p]
+  (:require [dynamo.file :as file]
+            [dynamo.project :as p]
             [dynamo.system :as ds]
             [dynamo.types :refer [MessageTarget]]
             [internal.query :as iq]
             [internal.system :as is]
             [service.log :as log])
   (:import [org.eclipse.core.resources IFile IProject]
-           [org.eclipse.ui PlatformUI]
-           [org.eclipse.ui.internal.registry FileEditorMapping EditorRegistry]
            [org.eclipse.e4.core.contexts IEclipseContext]
            [org.eclipse.e4.ui.model.application.ui.basic MBasicFactory MPart]
-           [org.eclipse.e4.ui.workbench.modeling EPartService EPartService$PartState]))
+           [org.eclipse.e4.ui.workbench.modeling EPartService EPartService$PartState]
+           [org.eclipse.ui PlatformUI]
+           [org.eclipse.ui.internal.registry FileEditorMapping EditorRegistry]))
 
 (set! *warn-on-reflection* true)
 
@@ -43,7 +44,9 @@
       ((p/editor-for
          proj
          (.. file getFullPath getFileExtension))
-        proj site file))))
+       proj site (p/node-by-filename world-ref proj
+                                     (file/project-path proj file)
+                                     p/load-resource)))))
 
 (defn- dynamic-part
   [{:keys [id label closeable] :or {id "sceneed.view" label "Default part" closeable true}}]

commit 074469e88044e3ce74e4a151a7eb088c364994a8

Split CubemapNode from (now view-scoped) CubemapRender
- CubemapRender outputs should be disposed when view scope is disposed.
- This fixes an issue introduced by 7a874bbe (though true cause was introduced by 830d6005 and b09ba448).
- Atlas was fixed in a similar manner by 03ea239e.

com.dynamo.cr/com.dynamo.cr.builtins/builtins/tools/cubemap/core.clj
@@ -43,21 +43,6 @@
             [org.eclipse.core.commands ExecutionEvent]
             [dynamo.types AABB Camera]))
 
-(n/defnode CubemapProperties
-  (input image-right  Image)
-  (input image-left   Image)
-  (input image-top    Image)
-  (input image-bottom Image)
-  (input image-front  Image)
-  (input image-back   Image)
-
-  (property right  String)
-  (property left   String)
-  (property top    String)
-  (property bottom String)
-  (property front  String)
-  (property back   String))
-
 (vtx/defvertex normal-vtx
   (vec3 position)
   (vec3 normal))
@@ -132,17 +117,48 @@
     (g/aabb-incorporate  1  1  1)
     (g/aabb-incorporate -1 -1 -1)))
 
+#_(defmacro for-each-side [bindings ])
+
 (n/defnode CubemapRender
   (input  camera        Camera)
+  (input  image-right   Image)
+  (input  image-left    Image)
+  (input  image-top     Image)
+  (input  image-bottom  Image)
+  (input  image-front   Image)
+  (input  image-back    Image)
   (output shader        s/Any      :cached produce-shader)
   (output vertex-buffer s/Any      :cached produce-renderable-vertex-buffer)
   (output gpu-texture   s/Any      :cached produce-gpu-texture)
   (output renderable    RenderData :cached produce-renderable)
   (output aabb          AABB               unit-bounding-box))
 
+(defnk produce-image-right  [image-right]  image-right)
+(defnk produce-image-left   [image-left]   image-left)
+(defnk produce-image-top    [image-top]    image-top)
+(defnk produce-image-bottom [image-bottom] image-bottom)
+(defnk produce-image-front  [image-front]  image-front)
+(defnk produce-image-back   [image-back]   image-back)
+
 (n/defnode CubemapNode
-  (inherits CubemapProperties)
-  (inherits CubemapRender))
+  (property right        String)
+  (property left         String)
+  (property top          String)
+  (property bottom       String)
+  (property front        String)
+  (property back         String)
+  (input    image-right  Image)
+  (input    image-left   Image)
+  (input    image-top    Image)
+  (input    image-bottom Image)
+  (input    image-front  Image)
+  (input    image-back   Image)
+  (output   image-right  Image produce-image-right)
+  (output   image-left   Image produce-image-left)
+  (output   image-top    Image produce-image-top)
+  (output   image-bottom Image produce-image-bottom)
+  (output   image-front  Image produce-image-front)
+  (output   image-back   Image produce-image-back))
 
 (protocol-buffer-converters
  Graphics$Cubemap
@@ -173,17 +189,24 @@
   (let [editor (ise/make-scene-editor :name "editor")]
     (ds/transactional
       (ds/in (ds/add editor)
-        (let [background (ds/add (background/make-background))
-              grid       (ds/add (grid/make-grid))
-              camera     (ds/add (c/make-camera-controller :camera (c/make-camera :orthographic)))]
-          (ds/connect camera     :camera     grid       :camera)
-          (ds/connect camera     :camera     editor     :view-camera)
-          (ds/connect camera     :self       editor     :controller)
-          (ds/connect background :renderable editor     :renderables)
-          (ds/connect cubemap    :renderable editor     :renderables)
-          (ds/connect camera     :camera     cubemap    :camera)
-          (ds/connect grid       :renderable editor     :renderables)
-          (ds/connect cubemap    :aabb       editor     :aabb))
+        (let [cubemap-render (ds/add (make-cubemap-render))
+              background     (ds/add (background/make-background))
+              grid           (ds/add (grid/make-grid))
+              camera         (ds/add (c/make-camera-controller :camera (c/make-camera :orthographic)))]
+          (ds/connect cubemap        :image-right  cubemap-render :image-right)
+          (ds/connect cubemap        :image-left   cubemap-render :image-left)
+          (ds/connect cubemap        :image-top    cubemap-render :image-top)
+          (ds/connect cubemap        :image-bottom cubemap-render :image-bottom)
+          (ds/connect cubemap        :image-front  cubemap-render :image-front)
+          (ds/connect cubemap        :image-back   cubemap-render :image-back)
+          (ds/connect camera         :camera       grid           :camera)
+          (ds/connect camera         :camera       editor         :view-camera)
+          (ds/connect camera         :self         editor         :controller)
+          (ds/connect background     :renderable   editor         :renderables)
+          (ds/connect cubemap-render :renderable   editor         :renderables)
+          (ds/connect camera         :camera       cubemap-render :camera)
+          (ds/connect grid           :renderable   editor         :renderables)
+          (ds/connect cubemap-render :aabb         editor         :aabb))
         editor))))
 
 (p/register-editor "cubemap" #'on-edit)