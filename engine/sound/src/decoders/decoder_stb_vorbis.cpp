30ba0e92180438d7e4362b6d319c3a4291781dec
Dan Nilsson <dan.nilsson@king.com>
2014-12-01 17:33:33 +0100

    - Refactoring of sound_codec to split out decoding functionaliy and making it modular. - Split out wav/stb_vorbis into their own modules and also add a new tremolo decoder that will be picked by default, except win32 and js-web - Fix calculation error in decode rate comparison. Add special test_sound_vorbis that forces the tremolo decoder to run - Add tremolo build script, patch & built packages

---
+++ engine/sound/src/decoders/decoder_stb_vorbis.cpp
@@ -0,0 +1,94 @@
+#include <stdint.h>
+#include <dlib/index_pool.h>
+#include <dlib/log.h>
+#include <dlib/math.h>
+#include <dlib/profile.h>
+
+#include "stb_vorbis/stb_vorbis.h"
+#include "sound_codec.h"
+#include "sound_decoder.h"
+
+namespace dmSoundCodec
+{
+    namespace
+    {
+        struct DecodeStreamInfo {
+            Info m_Info;
+            stb_vorbis *m_StbVorbis;
+        };
+    }
+
+    static Result StbVorbisOpenStream(const void* buffer, uint32_t buffer_size, HDecodeStream* stream)
+    {
+        int error;
+        stb_vorbis* vorbis = stb_vorbis_open_memory((unsigned char*) buffer, buffer_size, &error, NULL);
+
+        if (vorbis) {
+            stb_vorbis_info info = stb_vorbis_get_info(vorbis);
+
+            DecodeStreamInfo *streamInfo = new DecodeStreamInfo;
+            streamInfo->m_Info.m_Rate = info.sample_rate;
+            streamInfo->m_Info.m_Size = 0;
+            streamInfo->m_Info.m_Channels = info.channels;
+            streamInfo->m_Info.m_BitsPerSample = 16;
+            streamInfo->m_StbVorbis = vorbis;
+
+            *stream = streamInfo;
+            return RESULT_OK;
+        } else {
+            return RESULT_INVALID_FORMAT;
+        }
+    }
+
+    static Result StbVorbisDecode(HDecodeStream stream, char* buffer, uint32_t buffer_size, uint32_t* decoded)
+    {
+        DecodeStreamInfo *streamInfo = (DecodeStreamInfo *) stream;
+
+        DM_PROFILE(SoundCodec, "StbVorbis")
+
+        int ret = 0;
+        if (streamInfo->m_Info.m_Channels == 1) {
+            ret = stb_vorbis_get_samples_short_interleaved(streamInfo->m_StbVorbis, 1, (short*) buffer, buffer_size / 2);
+        } else if (streamInfo->m_Info.m_Channels == 2) {
+            ret = stb_vorbis_get_samples_short_interleaved(streamInfo->m_StbVorbis, 2, (short*) buffer, buffer_size / 2);
+        } else {
+            assert(0);
+        }
+
+        if (ret < 0) {
+            return RESULT_DECODE_ERROR;
+        } else {
+            if (streamInfo->m_Info.m_Channels == 1) {
+                *decoded = ret * 2;
+            } else if (streamInfo->m_Info.m_Channels == 2) {
+                *decoded = ret * 4;
+            } else {
+                assert(0);
+            }
+        }
+
+        return RESULT_OK;
+    }
+
+    Result StbVorbisResetStream(HDecodeStream stream)
+    {
+        stb_vorbis_seek_start(((DecodeStreamInfo*)stream)->m_StbVorbis);
+        return RESULT_OK;
+    }
+
+    void StbVorbisCloseStream(HDecodeStream stream)
+    {
+        DecodeStreamInfo *streamInfo = (DecodeStreamInfo*) stream;
+        stb_vorbis_close(streamInfo->m_StbVorbis);
+        delete streamInfo;
+    }
+
+    void StbVorbisGetInfo(HDecodeStream stream, struct Info* out)
+    {
+        *out = ((DecodeStreamInfo *)stream)->m_Info;
+    }
+
+    DM_DECLARE_SOUND_DECODER(AudioDecoderStbVorbis, "VorbisDecoderStb", FORMAT_VORBIS,
+                             5, // baseline score (1-10)
+                             StbVorbisOpenStream, StbVorbisCloseStream, StbVorbisDecode, StbVorbisResetStream, StbVorbisGetInfo);
+}
