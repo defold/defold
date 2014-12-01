9870522736a3af2ccbb214bf7096f1a90e779988
Dan Nilsson <dan.nilsson@king.com>
2014-12-01 17:33:33 +0100

    - Refactoring of sound_codec to split out decoding functionaliy and making it modular. - Split out wav/stb_vorbis into their own modules and also add a new tremolo decoder that will be picked by default, except win32 and js-web - Fix calculation error in decode rate comparison. Add special test_sound_vorbis that forces the tremolo decoder to run - Add tremolo build script, patch & built packages

---
+++ engine/sound/src/decoders/decoder_wav.cpp
@@ -0,0 +1,203 @@
+#include <stdint.h>
+#include <dlib/index_pool.h>
+#include <dlib/log.h>
+#include <dlib/math.h>
+#include <dlib/profile.h>
+
+#include "sound.h"
+#include "sound2.h"
+#include "sound_decoder.h"
+
+#if DM_ENDIAN == DM_ENDIAN_LITTLE
+#define FOUR_CC(a,b,c,d) (((uint32_t)d << 24) | ((uint32_t)c << 16) | ((uint32_t)b << 8) | ((uint32_t)a))
+#else
+#define FOUR_CC(a,b,c,d) (((uint32_t)a << 24) | ((uint32_t)b << 16) | ((uint32_t)c << 8) | ((uint32_t)d))
+#endif
+
+namespace dmSoundCodec
+{
+    namespace
+    {
+    #if DM_ENDIAN == DM_ENDIAN_LITTLE
+        static uint16_t Swap16(uint16_t x)
+        {
+          return x;
+        }
+
+        static uint32_t Swap32(uint32_t x)
+        {
+          return x;
+        }
+    #else
+        static uint16_t Swap16(uint16_t x)
+        {
+          return ((x & 0xff00) >> 8) |
+               ((x & 0x00ff) << 8);
+        }
+
+        static uint32_t Swap32(uint32_t x)
+        {
+          return ((x & 0xff000000) >> 24) |
+               ((x & 0x00ff0000) >>  8) |
+               ((x & 0x0000ff00) <<  8) |
+               ((x & 0x000000ff) << 24);
+        }
+    #endif
+
+        struct CommonHeader
+        {
+            uint32_t m_ChunkID;
+            uint32_t m_ChunkSize;
+
+            void SwapHeader() {
+                m_ChunkID = Swap32(m_ChunkID);
+                m_ChunkSize = Swap32(m_ChunkSize);
+            }
+        };
+
+        struct RiffHeader : CommonHeader
+        {
+            uint32_t m_Format;
+            void Swap()
+            {
+                SwapHeader();
+                m_Format = Swap32(m_Format);
+            }
+        };
+
+        struct FmtChunk : CommonHeader
+        {
+            uint16_t m_AudioFormat;
+            uint16_t m_NumChannels;
+            uint32_t m_SampleRate;
+            uint32_t m_ByteRate;
+            uint16_t m_BlockAlign;
+            uint16_t m_BitsPerSample;
+
+            void Swap()
+            {
+                SwapHeader();
+                m_AudioFormat = Swap16(m_AudioFormat);
+                m_NumChannels = Swap16(m_NumChannels);
+                m_SampleRate = Swap32(m_SampleRate);
+                m_ByteRate = Swap32(m_ByteRate);
+                m_BlockAlign = Swap16(m_BlockAlign);
+                m_BitsPerSample = Swap16(m_BitsPerSample);
+            }
+
+        };
+
+        struct DataChunk : CommonHeader
+        {
+            char m_Data[0];
+            void Swap() {
+                SwapHeader();
+            }
+        };
+
+        struct DecodeStreamInfo {
+            Info m_Info;
+            uint32_t m_Cursor;
+            const void* m_Buffer;
+        };
+    }
+
+    static Result WavOpenStream(const void* buffer, uint32_t buffer_size, HDecodeStream* stream)
+    {
+        RiffHeader* header = (RiffHeader*) buffer;
+        DecodeStreamInfo streamTemp;
+
+        bool fmt_found = false;
+        bool data_found = false;
+
+        if (header->m_ChunkID == FOUR_CC('R', 'I', 'F', 'F') &&
+            header->m_Format == FOUR_CC('W', 'A', 'V', 'E')) {
+
+            const char* current = (const char*) buffer;
+            const char* end = (const char*) buffer + buffer_size;
+            current += sizeof(RiffHeader);
+            do {
+                CommonHeader header;
+                memcpy(&header, current, sizeof(header));
+                header.SwapHeader();
+                if (header.m_ChunkID == FOUR_CC('f', 'm', 't', ' ')) {
+                    FmtChunk fmt;
+                    memcpy(&fmt, current, sizeof(fmt));
+                    fmt.Swap();
+                    fmt_found = true;
+
+                    if (fmt.m_AudioFormat != 1) {
+                        dmLogWarning("Only wav-files with PCM format supported");
+                        return RESULT_INVALID_FORMAT;
+                    }
+
+                    streamTemp.m_Info.m_Rate = fmt.m_SampleRate;
+                    streamTemp.m_Info.m_Channels = fmt.m_NumChannels;
+                    streamTemp.m_Info.m_BitsPerSample = fmt.m_BitsPerSample;
+
+                } else if (header.m_ChunkID == FOUR_CC('d', 'a', 't', 'a')) {
+                    // NOTE: We don't byte-swap PCM-data and a potential problem on big-endian architectures
+                    DataChunk data;
+                    memcpy(&data, current, sizeof(data));
+                    data.Swap();
+                    streamTemp.m_Buffer = (void*) (current + sizeof(DataChunk));
+                    streamTemp.m_Info.m_Size = data.m_ChunkSize;
+                    data_found = true;
+                }
+                current += header.m_ChunkSize + sizeof(CommonHeader);
+            } while (current < end);
+
+            if (fmt_found && data_found) {
+                // Allocate stream output and copy temporary data over there.
+                // Doing this last-minute avoids having to worry about deallocating
+                // on failure. NOTE: Maybe pool allocate here.
+                streamTemp.m_Cursor = 0;
+                DecodeStreamInfo *streamOut = new DecodeStreamInfo;
+                *streamOut = streamTemp;
+                *stream = streamOut;
+                return RESULT_OK;
+            } else {
+                return RESULT_INVALID_FORMAT;
+            }
+        } else {
+            return RESULT_INVALID_FORMAT;
+        }
+    }
+
+    void WavCloseStream(HDecodeStream stream)
+    {
+        assert(stream);
+        DecodeStreamInfo *streamInfo = (DecodeStreamInfo *) stream;
+        delete streamInfo;
+    }
+
+    Result WavResetStream(HDecodeStream stream)
+    {
+        DecodeStreamInfo *streamInfo = (DecodeStreamInfo *) stream;
+        streamInfo->m_Cursor = 0;
+        return RESULT_OK;
+    }
+
+    Result WavDecodeStream(HDecodeStream stream, char* buffer, uint32_t buffer_size, uint32_t* decoded)
+    {
+        DecodeStreamInfo *streamInfo = (DecodeStreamInfo *) stream;
+
+        DM_PROFILE(SoundCodec, "Wav")
+
+        assert(streamInfo->m_Cursor <= streamInfo->m_Info.m_Size);
+        uint32_t n = dmMath::Min(buffer_size, streamInfo->m_Info.m_Size - streamInfo->m_Cursor);
+        *decoded = n;
+        memcpy(buffer, (const char*) streamInfo->m_Buffer + streamInfo->m_Cursor, n);
+        streamInfo->m_Cursor += n;
+        return RESULT_OK;
+    }
+
+    void WavGetInfo(HDecodeStream stream, struct Info* out)
+    {
+        *out = ((DecodeStreamInfo *)stream)->m_Info;
+    }
+
+    DM_DECLARE_SOUND_DECODER(AudioDecoderWav, "WavDecoder", FORMAT_WAV,
+                             0,
+                             WavOpenStream, WavCloseStream, WavDecodeStream, WavResetStream, WavGetInfo);
+}
