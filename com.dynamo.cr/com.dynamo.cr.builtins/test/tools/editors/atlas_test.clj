19d37e7557de22029016ecf11b9fa595934148c2
Michael Nygard and Sam Umbach <pair+mn+su@cognitect.com>
2014-12-22 14:41:01 -0600

    Use property-based test for Atlas round-trip.
    - Loads an Atlas from a randomly generated protobuf string.
    - Writes it back out
    - Reads it again
    - Writes it _again_
    - Verifies that the write->read->write cycle generates the same output.

--- com.dynamo.cr/com.dynamo.cr.builtins/test/tools/editors/atlas_test.clj
+++ com.dynamo.cr/com.dynamo.cr.builtins/test/tools/editors/atlas_test.clj
@@ -1,6 +1,66 @@
 (ns editors.atlas-test
-  (:require [clojure.test :refer :all]
-            [editors.atlas :as atlas]))
+  (:require [clojure.string :as str]
+            [clojure.test :refer :all]
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [dynamo.file :as file]
+            [dynamo.node :as n]
+            [dynamo.system :as ds]
+            [dynamo.system.test-support :refer [with-clean-world]]
+            [dynamo.types :as t]
+            [schema.test]
+            [editors.atlas :as atlas])
+  (:import [com.dynamo.atlas.proto AtlasProto AtlasProto$Atlas AtlasProto$AtlasAnimation AtlasProto$AtlasImage]
+           [java.io StringReader]))
 
-(deftest bad-test
-  (is (not (nil? (atlas/make-atlas-node)))))
+(use-fixtures :once schema.test/validate-schemas)
+
+(def ident (gen/not-empty gen/string-alpha-numeric))
+
+(def image-name (gen/fmap (fn [ns] (str (str/join \/ ns) ".png")) (gen/not-empty (gen/vector ident))))
+
+(def image (gen/fmap #(format "images{\nimage: \"%s\"\n}"  %) image-name))
+
+(def animation-name ident)
+(def fps (gen/such-that #(< 0 % 60) gen/pos-int))
+(def flip (gen/frequency [[9 (gen/return 0)] [1 (gen/return 1)]]))
+(def playback (gen/elements ["PLAYBACK_NONE"
+                             "PLAYBACK_ONCE_FORWARD"
+                             "PLAYBACK_ONCE_BACKWARD"
+                             "PLAYBACK_ONCE_PINGPONG"
+                             "PLAYBACK_LOOP_FORWARD"
+                             "PLAYBACK_LOOP_BACKWARD"
+                             "PLAYBACK_LOOP_PINGPONG"]))
+
+(def animation (gen/fmap (fn [[id imgs fps flip-horiz flip-vert playback]]
+                           (format "animations {\nid: \"%s\"\n%s\nfps: %d\nflip_horizontal: %d\nflip_vertical: %d\nplayback: %s\n}"
+                             id (str/join \newline imgs) fps flip-horiz flip-vert playback))
+                 (gen/tuple animation-name (gen/not-empty (gen/vector image)) fps flip flip playback )))
+
+(def atlas (gen/fmap (fn [[margin borders animations images]]
+                       (format "%s\n%s\nmargin: %d\nextrude_borders: %d\n"
+                         (str/join \newline images) (str/join \newline animations) margin borders))
+             (gen/tuple gen/pos-int gen/pos-int (gen/vector animation) (gen/vector image))))
+
+(defn <-text
+  [loader text-format]
+  (ds/transactional (loader (file/native-path "/var/tmp/dummy-path.atlas") (StringReader. text-format))))
+
+(defn ->text
+  [atlas]
+  (n/get-node-value atlas :text-format))
+
+(def loader (file/protocol-buffer-loader AtlasProto$Atlas atlas/on-load))
+
+(defn round-trip
+  [random-atlas]
+  (with-clean-world
+    (let [first-gen  (->text (<-text loader random-atlas))
+         second-gen (->text (<-text loader first-gen))]
+     (= first-gen second-gen))))
+
+(defspec round-trip-preserves-fidelity
+  10
+  (prop/for-all* [atlas] round-trip))
