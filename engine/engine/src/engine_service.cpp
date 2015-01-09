7e309c6d36aac25f262783b0b0f97aa734f9fbb1
Ragnar Svensson <ragnar.svensson@king.com>
2015-01-09 17:42:31 +0100

    Tweaked SSDP notify intervals. DEF-961
    * Notify first update, instead of after 1 sec
    * Notify each 3 secs instead of each 5

--- engine/engine/src/engine_service.cpp
+++ engine/engine/src/engine_service.cpp
@@ -280,7 +280,7 @@ namespace dmEngineService
             m_DeviceDesc.m_DeviceDescription = m_DeviceDescXml;
 
             dmSSDP::NewParams ssdp_params;
-            ssdp_params.m_MaxAge = 5;
+            ssdp_params.m_MaxAge = 3;
             dmSSDP::HSSDP ssdp;
             dmSSDP::Result sr = dmSSDP::New(&ssdp_params, &ssdp);
             if (sr != dmSSDP::RESULT_OK)
