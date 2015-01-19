c6deb745d23e60eeb4dc8cb23328f06d35f222bb
Michael Nygard <pair+mn@cognitect.com>
2015-01-19 14:45:35 -0600

    Moving away from tail-doc style.

--- com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/dynamo/editors.clj
+++ com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/dynamo/editors.clj
@@ -1,4 +1,5 @@
 (ns dynamo.editors
+  "This namespace provides the built in editor parts, along with support for building your own editors."
   (:require [clojure.core.async :refer [chan dropping-buffer]]
             [internal.ui.editors :as ie]
             [internal.ui.handlers :refer [active-editor]])
@@ -8,29 +9,20 @@
 (set! *warn-on-reflection* true)
 
 (defn event->active-editor
+  "Returns the Clojure implementation of the active editor from the event's application context."
   [^ExecutionEvent evt]
   (let [editor (active-editor (.getApplicationContext evt))]
     (if (instance? GenericEditor editor)
       (.getBehavior ^GenericEditor editor))))
 
 (defn open-part
-  [behavior & {:as opts}]
-  (ie/open-part behavior opts))
-
-(doseq [[v doc]
-        {*ns*
-         "This namespace allows you to create new editors."
-
-         #'event->active-editor
-         "Returns the Clojure implementation of the active editor from the event's application context."
-
-         #'open-part
-         "Creates an Eclipse view for the given behavior. The
+  "Creates an Eclipse view for the given behavior. The
 behavior must support dynamo.types/MessageTarget.
 
 Allowed options are:
 
 :label - the visible label for the part
 :closeable - if true, the part can be closed (defaults to true)
-:id - the part ID by which Eclipse will know it"}]
-  (alter-meta! v assoc :doc doc))
+:id - the part ID by which Eclipse will know it"
+  [behavior & {:as opts}]
+  (ie/open-part behavior opts))
