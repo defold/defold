db143468ae95b9f3de0861b66aab435556f5b730
Michael Nygard <pair+mn@cognitect.com>
2015-01-20 10:00:11 -0600

    Decruft editors.image-node

--- com.dynamo.cr/com.dynamo.cr.builtins/builtins/tools/editors/image_node.clj
+++ com.dynamo.cr/com.dynamo.cr.builtins/builtins/tools/editors/image_node.clj
@@ -1,17 +1,11 @@
 (ns editors.image-node
-  (:require [plumbing.core :refer [defnk]]
-            [dynamo.node :as n :refer [defnode]]
+  (:require [dynamo.node :as n]
             [dynamo.image :as i]
-            [dynamo.system :as ds]
             [dynamo.project :as p]))
 
-(defnode ImageResourceNode
+(n/defnode ImageResourceNode
   (inherits n/OutlineNode)
   (inherits i/ImageSource))
-
-(defn on-load
-  [project-node path _]
-  (ds/add (n/construct ImageResourceNode :filename path)))
 
 (when (ds/in-transaction?)
   (p/register-node-type "png" ImageResourceNode)
