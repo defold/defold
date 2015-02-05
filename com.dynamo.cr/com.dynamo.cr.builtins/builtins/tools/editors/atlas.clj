defold/defold

commit 2f9c6374cc21bb8ef7b052e09206e66eed798786

Added safety for resource autowiring.
- Avoid infinite recursion
- Make testable
- Introduce *tx-debug* dynamic var, default false

[Finishes #87045318]

com.dynamo.cr/com.dynamo.cr.builtins/builtins/tools/editors/atlas.clj
@@ -84,7 +84,7 @@
 (sm/defn build-atlas-image :- AtlasProto$AtlasImage
   [image :- Image]
   (.build (doto (AtlasProto$AtlasImage/newBuilder)
-            (.setImage (str (.path image))))))
+            (protobuf/set-if-present :image image (comp str :path)))))
 
 (sm/defn build-atlas-animation :- AtlasProto$AtlasAnimation
   [animation :- Animation]
@@ -178,7 +178,7 @@
   [this textureset selection]
   (let [project-root (p/project-root-node this)
         selected (set @selection)]
-    (vec 
+    (vec
       (keep
         (fn [rect]
           (let [node (t/lookup project-root (:path rect))]

com.dynamo.cr/com.dynamo.cr.builtins/test/tools/editors/atlas_test.clj
@@ -9,7 +9,7 @@
             [dynamo.node :as n]
             [dynamo.project :as p]
             [dynamo.system :as ds]
-            [dynamo.system.test-support :refer [with-clean-world tempfile]]
+            [dynamo.system.test-support :refer [with-clean-world tempfile mock-iproject]]
             [dynamo.types :as t]
             [schema.test]
             [editors.atlas :as atlas]
@@ -45,22 +45,35 @@
              (gen/tuple gen/pos-int gen/pos-int (gen/vector animation) (gen/vector image))))
 
 (defn <-text
-  [text-format]
+  [project-node text-format]
   (ds/transactional
-    (let [locator (reify t/NamingContext (lookup [this name] (ds/add (n/construct image/ImageResourceNode :filename name))))
-          atlas   (ds/add (n/construct atlas/AtlasNode))]
-      (atlas/construct-ancillary-nodes atlas locator (StringReader. text-format))
-      atlas)))
+    (ds/in project-node
+      (let [atlas (ds/add (n/construct atlas/AtlasNode))]
+        (atlas/construct-ancillary-nodes atlas project-node (StringReader. text-format))
+        atlas))))
 
 (defn ->text
   [atlas]
   (n/get-node-value atlas :text-format))
 
+(defn test-project
+  []
+  (ds/transactional
+    (let [eproj (mock-iproject {})
+          project-node (ds/add (n/construct p/Project :eclipse-project eproj))]
+      (ds/in project-node
+        (p/register-editor "atlas" #'editors.atlas/on-edit)
+        (p/register-node-type "atlas" editors.atlas/AtlasNode)
+        (p/register-node-type "png" editors.image-node/ImageResourceNode)
+        (p/register-node-type "jpg" editors.image-node/ImageResourceNode)
+        project-node))))
+
 (defn round-trip
   [random-atlas]
   (with-clean-world
-    (let [first-gen (->text (<-text random-atlas))
-          second-gen (->text (<-text first-gen))]
+    (let [project-node (test-project)
+          first-gen    (->text (<-text project-node random-atlas))
+          second-gen   (->text (<-text project-node first-gen))]
       (= first-gen second-gen))))
 
 (defspec round-trip-preserves-fidelity
@@ -70,16 +83,17 @@
 (deftest compilation-to-binary
   (testing "Doesn't throw an exception"
     (with-clean-world
-      (let [atlas       (<-text (first (gen/sample (gen/resize 10 atlas) 1)))
+      (let [project-node (test-project)
+            atlas        (<-text project-node (first (gen/sample (gen/resize 5 atlas) 1)))
             txname       "random-mcnally"
-            texturesetc (tempfile txname "texturesetc" true)
-            texturec    (tempfile txname "texturec" true)
-            compiler    (ds/transactional
-                          (ds/add
-                            (n/construct atlas/TextureSave
-                              :texture-name        txname
-                              :texture-filename    (file/native-path (.getPath texturec))
-                              :textureset-filename (file/native-path (.getPath texturesetc)))))]
+            texturesetc  (tempfile txname "texturesetc" true)
+            texturec     (tempfile txname "texturec" true)
+            compiler     (ds/transactional
+                           (ds/add
+                             (n/construct atlas/TextureSave
+                               :texture-name        txname
+                               :texture-filename    (file/native-path (.getPath texturec))
+                               :textureset-filename (file/native-path (.getPath texturesetc)))))]
         (ds/transactional (ds/connect atlas :textureset compiler :textureset))
         (is (= :ok (n/get-node-value compiler :texturec)))
         (is (= :ok (n/get-node-value compiler :texturesetc)))))))

com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/dynamo/editors.clj
@@ -155,7 +155,7 @@ Messages:
 
   (on :destroy
     (when (:context self)
-      (texture/unload-all (.. (:context self) getGL)))
+      (texture/unload-all (.. ^GLContext (:context self) getGL)))
 
     (ds/delete self))
 

com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/dynamo/gl.clj
@@ -72,7 +72,7 @@
 (defmacro gl-cull-face [gl mode]                                         `(.glCullFace ~gl ~mode))
 
 (defn gl-get-integer-v
-  [^GL2 gl param sz]
+  [^GL2 gl ^Integer param sz]
   (let [buff (int-array sz)]
     (.glGetIntegerv gl param buff 0)
     buff))

com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/dynamo/image.clj
@@ -51,8 +51,7 @@
     img))
 
 (defn load-image [src reference]
-  (if-let [img (ImageIO/read (io/input-stream src))]
-    (make-image reference img)))
+  (make-image reference (ImageIO/read (io/input-stream src))))
 
 ;; Use "Hollywood Cerise" for the placeholder image color.
 (def placeholder-image (make-image "placeholder" (flood (blank-image 64 64) 0.9568 0.0 0.6313)))

com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/dynamo/node.clj
@@ -227,8 +227,8 @@ When a Scope is deleted, all nodes within that scope will also be deleted."
   (property tag      s/Keyword)
   (property parent   (s/protocol NamingContext))
 
-  (trigger dependency-injection :modified inject-new-nodes)
-  (trigger garbage-collection   :deleted  dispose-nodes)
+  (trigger dependency-injection :modified #'inject-new-nodes)
+  (trigger garbage-collection   :deleted  #'dispose-nodes)
 
   (output dictionary s/Any in/scope-dictionary)
 
@@ -261,7 +261,7 @@ Inheritors are required to supply a production function for the :save output."
 (defnode AutowireResources
   "Mixin. Nodes with this behavior automatically keep their graph connections
 up to date with their resource properties."
-  (trigger autowire-resources :modified in/connect-resource))
+  (trigger autowire-resources :modified #'in/connect-resource))
 
 (defnode OutlineNode
   "Mixin. Any OutlineNode can be shown in an outline view.

com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/dynamo/project.clj
@@ -179,13 +179,11 @@ There is no guaranteed ordering of the sequence."
   t/NamingContext
   (lookup [this name]
     (let [path (if (instance? dynamo.file.ProjectPath name) name (file/make-project-path this name))]
-      (if-let [node (first (ds/query (:world-ref this) [[:_id (:_id this)] '(input :nodes) [:filename path]]))]
-        node
-        (when (ds/in-transaction?)
-          (println "Auto-creating node for " path)
-            (ds/in this
-              (ds/add
-                (new-node-for-path this path Placeholder)))))))
+      (first (ds/query (:world-ref this) [[:_id (:_id this)] '(input :nodes) [:filename path]]))))
+
+  t/FileContainer
+  (node-for-path [this path]
+    (new-node-for-path this (file/make-project-path this path) Placeholder))
 
   (on :destroy
     (ds/delete self)))
@@ -267,7 +265,7 @@ There is no guaranteed ordering of the sequence."
   (let [with-placeholders (group-by #(add-or-replace? project-node %) added)
         replacements      (mapcat #(nodes-with-filename project-node (file/make-project-path project-node %)) (:replace-existing with-placeholders))]
     (unload-nodes replacements)
-    (replace-nodes project-node replacements #(new-node-for-path project-node (:filename %) Placeholder))
+    (replace-nodes project-node replacements #(t/node-for-path project-node (:filename %)))
     (apply post-load "Compiling"          (load-resource-nodes (ds/refresh project-node) (:load-clojure with-placeholders) nil))
     (apply post-load "Loading asset from" (load-resource-nodes (ds/refresh project-node) (:load-other   with-placeholders) nil))
     project-node))

com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/dynamo/system.clj
@@ -239,18 +239,11 @@ inherits from dynamo.node/Scope."
       (partial enclosing-scope graph)
       n)))
 
-(defn- transaction-added-nodes
+(defn transaction-added-nodes
   [transaction]
   (let [g (:graph transaction)]
     (map #(dg/node g %) (:nodes-added transaction))))
 
-(defn lookup-in-transaction
-  "Attempt to find the node-name, as it exists at the present
-state of the transaction. This first looks at the newly created nodes,
-then moves to the enclosing scope of the origin."
-  [transaction origin node-name]
-  (let [parents (drop 1 (path-to-root (:graph transaction) origin))
-        path    (if (instance? dynamo.file.ProjectPath node-name) node-name (file/make-project-path (first parents) node-name))]
-    (if-let [added-this-txn (first (filter #(= path (:filename %)) (transaction-added-nodes transaction)))]
-      added-this-txn
-      (first (map #(t/lookup % node-name) parents)))))
+(defn parent
+  [graph n]
+  (first (drop 1 (path-to-root graph n))))

commit 027b5d83f7fddfed2977b416b55bac7b4137be77

Fix .atlas file output (broken since 2f9c637)
- `protobuf/set-if-present` can only be used if the protobuf attribute
  name matches the map/record key. In this case the protobuf attribute
  name `image` does not match the Image record key `path`.
  Additionally, the `image` attribute is required, so there is no need
  for the conditional logic.
- Also fixed the `round-trip-preserves-fidelity` spec by providing a
  special ResourceNode implementation. `WildcardImageResourceNode`
  always produces a placeholder image with a `:path` matching the
  initial filename, allowing tests to behave as though any
  `/random/image-file.png` file exists in the project.

com.dynamo.cr/com.dynamo.cr.builtins/builtins/tools/editors/atlas.clj
@@ -128,7 +128,7 @@
 (sm/defn build-atlas-image :- AtlasProto$AtlasImage
   [image :- Image]
   (.build (doto (AtlasProto$AtlasImage/newBuilder)
-            (protobuf/set-if-present :image image (comp str :path)))))
+            (.setImage (str (.path image))))))
 
 (sm/defn build-atlas-animation :- AtlasProto$AtlasAnimation
   [animation :- Animation]

com.dynamo.cr/com.dynamo.cr.builtins/test/tools/editors/atlas_test.clj
@@ -16,8 +16,7 @@
             [dynamo.system.test-support :refer [with-clean-world tempfile mock-iproject fixture]]
             [dynamo.types :as t]
             [schema.test]
-            [editors.atlas :as atlas]
-            [editors.image-node :as image-node])
+            [editors.atlas :as atlas])
   (:import [com.dynamo.atlas.proto AtlasProto AtlasProto$Atlas AtlasProto$AtlasAnimation AtlasProto$AtlasImage]
            [java.io StringReader]
            [dynamo.types Image]
@@ -62,6 +61,10 @@
   [atlas]
   (n/get-node-value atlas :text-format))
 
+(n/defnode WildcardImageResourceNode
+  (property filename (s/protocol t/PathManipulation) (visible false))
+  (output content Image :cached (fnk [filename] (assoc image/placeholder-image :path (t/local-path filename)))))
+
 (defn test-project
   []
   (ds/transactional
@@ -70,8 +73,7 @@
       (ds/in project-node
         (p/register-editor "atlas" #'editors.atlas/on-edit)
         (p/register-node-type "atlas" editors.atlas/AtlasNode)
-        (p/register-node-type "png" editors.image-node/ImageResourceNode)
-        (p/register-node-type "jpg" editors.image-node/ImageResourceNode)
+        (p/register-node-type "png" WildcardImageResourceNode)
         project-node))))
 
 (defn round-trip
@@ -82,13 +84,7 @@
           second-gen   (->text (<-text project-node first-gen))]
       (= first-gen second-gen))))
 
-;; MTN - Removed on 2014-02-02
-;;
-;; This is failing due to the autowiring work. We now need to have legit image references.
-;;
-;; Sam has a test fixture on its way that we can use to resurrect this test.
-;;
-#_(defspec round-trip-preserves-fidelity
+(defspec round-trip-preserves-fidelity
   10
   (prop/for-all* [atlas] round-trip))
 