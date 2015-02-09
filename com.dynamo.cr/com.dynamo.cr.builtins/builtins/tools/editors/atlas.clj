8d7127972cd0c0fe2d409226cb79bbd4b4e28e9e
Stuart Sierra <stuart.sierra@cognitect.com>
2015-02-09 16:27:58 -0500

    Set *warn-on-reflection* in Atlas editor
    [#87802376]

--- com.dynamo.cr/com.dynamo.cr.builtins/builtins/tools/editors/atlas.clj
+++ com.dynamo.cr/com.dynamo.cr.builtins/builtins/tools/editors/atlas.clj
@@ -41,6 +41,8 @@
             [org.eclipse.swt SWT]
             [org.eclipse.ui IEditorSite]))
 
+(set! *warn-on-reflection* true)
+
 (def integers (iterate (comp int inc) (int 0)))
 
 (def min-drag-move
