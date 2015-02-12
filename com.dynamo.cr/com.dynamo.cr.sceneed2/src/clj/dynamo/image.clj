0c9c11ea2425ca9521208d92e8ab9c9b7f45c2cb
Sam Umbach <samumbach@gmail.com>
2015-02-12 14:29:22 -0500

    [Fixes #87942684] Convert image to TYPE_4BYTE_ABGR before extruding borders
    - `extrude-borders` assumes it is operating on an array of color
      values; it did not work correctly for indexed/palettized images.
    - Before manipulating the array of pixels, we now convert to a
      suitable format.
    - See also `com.dynamo.bob.util.TextureUtil/depalettiseImage`.

--- com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/dynamo/image.clj
+++ com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/dynamo/image.clj
@@ -119,6 +119,7 @@ region will be identical to the nearest pixel of the source image."
   (if-not (< 0 extrusion)
     src
     (let [src-img        (t/contents src)
+          src-img        (image-convert-type src-img BufferedImage/TYPE_4BYTE_ABGR)
           orig-width     (.width src)
           orig-height    (.height src)
           new-width      (+ orig-width (* 2 extrusion))
