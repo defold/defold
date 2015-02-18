0d77872de7674ac22530ad172ec17c7dd7b72a51
Mikael Säker <mikael@sicher.org>
2015-02-18 22:47:50 +0100

    Updated API docs for collectionfactory and delete_all.
    - Some more descriptive text.
    - A bit more elaborate examples provided.

--- engine/gamesys/src/gamesys/scripts/script_collection_factory.cpp
+++ engine/gamesys/src/gamesys/scripts/script_collection_factory.cpp
@@ -45,38 +45,67 @@ namespace dmGameSystem
     }
 
     /*# Spawn a new instance of a collection into the existing collection.
-     * The URL identifies which collection factory should be used.
+     * The URL identifies the collectionfactory component that should do the spawning.
      *
-     * Spawning happens instantly, but spawned game objects get their first update calls the following frame. An extra game object is added,
-     * which will be the parent of all root objects in the collection being spawned. The supplied parameters for position, rotation and scale
-     * will be applied to this new object.
+     * Spawning is instant, but spawned game objects get their first update calls the following frame. The supplied parameters for position, rotation and scale
+     * will be applied to the whole collection when spawned.
      *
-     * Properties defined in scripts in the created game object can be overridden through the properties-parameter below.
+     * Script properties in the created game objects can be overridden through 
+     * a properties-parameter table. The table should contain game object ids
+     * (hash) as keys and property tables as values to be used when initiating each
+     * spawned game object.
+     *
      * See go.property for more information on script properties.
      *
+     * The function returns a table that contains a key for each game object 
+     * id (hash), as addressed if the collection file was top level, and the
+     * corresponding spawned instance id (hash) as value with a unique path
+     * prefix added to each instance.
+     *
      * @name collectionfactory.create
-     * @param url the collection factory to be used (url)
-     * @param [position] position to assign to the root of the newly spawned collection (vector3)
-     * @param [rotation] rotation to assign to the root of the newly spawned collection (quat)
-     * @param [properties] table of script properties to propagate to the new instances (table)
-     * @param [scale] scale to assign to the root of the newly spawned collection (must be greater than 0). (number)
-     * @return a table mapping the ids from the collection to the new instance ids 
+     * @param url the collection factory component to be used (url)
+     * @param [position] position to assign to the newly spawned collection (vector3)
+     * @param [rotation] rotation to assign to the newly spawned collection (quat)
+     * @param [properties] table of script properties to propagate to any new game object instances (table)
+     * @param [scale] uniform scaling to apply to the newly spawned collection (must be greater than 0). (number)
+     * @return a table mapping the id:s from the collection to the new instance id:s 
      * @examples
      * <p>
-     * How to spawn spawn in a new collection:
+     * How to spawn a collection of game objects:
      * </p>
      * <pre>
      * function init(self)
-     *     self.my_new_tree = collectionfactory.create("#collectionfactory", nil, nil, { [hash("button")] = { button_prop = 1 } })
-     *     -- send message to one of the new objects
-     *     msg.post(msg.url(nil, self.my_new_tree["titlebar"], "hello")
+     *   -- Spawn a small group of enemies.
+     *   local pos = vmath.vector3(100, 12.5, 0)
+     *   local rot = vmath.quat_rotation_z(math.pi / 2)
+     *   local scale = 0.5
+     *   local props = {}
+     *   props[hash("/enemy_leader")] = { health = 1000.0 }
+     *   props[hash("/enemy_1")] = { health = 200.0 }
+     *   props[hash("/enemy_2")] = { health = 400.0, color = hash("green") } 
+     *
+     *   local self.enemy_ids = collectionfactory.create("#enemyfactory", pos, rot, props, scale)
+     *   -- enemy_ids now map to the spawned instance ids:
+     *   --
+     *   -- pprint(self.enemy_ids)
+     *   --
+     *   -- DEBUG:SCRIPT: 
+     *   -- {
+     *   --   hash: [/enemy_leader] = hash: [/collection0/enemy_leader],
+     *   --   hash: [/enemy_1] = hash: [/collection0/enemy_1],
+     *   --   hash: [/enemy_2] = hash: [/collection0/enemy_2]
+     *   -- }
+     *   
+     *   -- Send "attack" message to the leader. First look up its instance id. 
+     *   local leader_id = self.enemy_ids[hash("/enemy_leader")]
+     *   msg.post(leader_id, "attack")
      * end
      * </pre>
      * <p>
-     * How to unspawn the new collection
+     * How to delete a spawned collection:
      * </p>
      * <pre>
-     * go.delete_all(self.my_new_tree)
+     * go.delete_all(self.enemy_ids)
      * </pre>
      */
 
