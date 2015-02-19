73665822507e86a6f25d1241b95429bdf213fc5e
Sam Umbach <samumbach@gmail.com>
2015-02-19 15:59:21 -0500

    [#88435114] Add command info to OutlineItem
    - At the moment, commands are simply strings.
    - Change `outline-children` to output to enable node definitions to
      alter child items (e.g., to inject additional commands).
    - Remove outline-related functionality from `dynamo.protobuf-test`--it
      is a distraction from the purpose of this test and is likely to
      break as outline implementation changes.

--- com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/internal/outline.clj
+++ com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/internal/outline.clj
@@ -3,8 +3,9 @@
             [plumbing.core :refer [defnk]]))
 
 (defnk outline-tree-producer :- t/OutlineItem
-  [this outline-label outline-children :- [t/OutlineItem]]
+  [this outline-label outline-commands outline-children :- [t/OutlineItem]]
   {:label outline-label
    ;:icon "my type of icon"
    :node-ref (t/node-ref this)
+   :commands outline-commands
    :children outline-children})
