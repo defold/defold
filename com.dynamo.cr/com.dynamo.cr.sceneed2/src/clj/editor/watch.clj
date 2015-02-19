e61b0faf30a6f4120442fd0ae79a7ed13aaf60fa
Christian Murray <cgmurray@gmail.com>
2015-02-19 20:30:19 +0100

    Minor cleanup

--- com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/editor/watch.clj
+++ com.dynamo.cr/com.dynamo.cr.sceneed2/src/clj/editor/watch.clj
@@ -1,7 +1,5 @@
 (ns editor.watch
-  (:require [clojure.java.io :as io]
-            [clojure.pprint :refer [pprint]]
-            [clojure.core.async :as a])
+  (:require [clojure.java.io :as io])
   (:import [java.io File]))
 
 (defrecord Watcher [root state changes])
@@ -19,19 +17,19 @@
     (mapv first)))
 
 (defn- state-changes
-  "Compare two states. Returns a map of with :added :removed :changed as sets"
+  "Compare two states. Returns a map with keys :added, :removed and :changed as sets"
   [current-state new-state]
   { :added (watch-filter (fn [[f t]] (not (current-state f))) new-state)
     :removed (watch-filter (fn [[f t]] (not (new-state f))) current-state)
     :changed (watch-filter (fn [[f t]] (and (new-state f) (not= (new-state f) t))) current-state)})
 
 (defn make-watcher
-  "create a new watcher. changes are stored in :changes. see update"
+  "Create a new watcher. changes are stored in :changes. see update"
   [root]
   (Watcher. root (file-map-seq (io/file root)) {} ))
 
 (defn update
-  "update watcher. on file-system changes :changes contain sets for :added, :removed and :changed files"
+  "Update watcher. On file-system changes :changes contains sets for :added, :removed and :changed files"
   [watcher]
   (let [new-state (file-map-seq (io/file (:root watcher)))
         changes (state-changes (:state watcher) new-state)]
