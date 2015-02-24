286a7783b0e748fcf11ccac9d74410c06d9e8884
Michael Nygard <pair+mn@cognitect.com>
2015-02-24 14:15:05 -0600

    Remove leftover from migration.

--- editor/src/java/internal/ui/JavaMath.java
+++
@@ -1,8 +0,0 @@
-package internal.ui;
-
-public class JavaMath {
-    public static short toShortUV(float fuv) {
-        int uv = (int) (fuv * 65535.0f);
-        return (short) (uv & 0xffff);
-    }
-}
