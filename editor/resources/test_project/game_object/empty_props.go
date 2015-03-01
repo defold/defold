69d23afae26b92efa5c6fa28d7029e9d3bb3a594
Ragnar Svensson <ragnar.svensson@king.com>
2015-03-01 23:52:59 +0100

    Integration tests for undo. [Fixes 89368888]
    * The tests do not yet pass until the graph changes have been implemented
    * Open the editor of a node should not affect undo history
    * Undoing the deletion of a node opened in an editor, should reconnect it to the editor

---
+++ editor/resources/test_project/game_object/empty_props.go
@@ -0,0 +1,4 @@
+components {
+    id: "script"
+    component: "/script/props.script"
+}
---
+++ editor/resources/test_project/logic/session/pow.go
@@ -0,0 +1,13 @@
+components {
+  id: "sprite"
+  component: "/logic/session/pow.sprite"
+}
+components {
+  id: "script"
+  component: "/logic/session/pow.script"
+}
+embedded_components {
+  id: "co"
+  type: "collisionobject"
+  data: "collision_shape: \"/logic/session/pow.convexshape\"\ntype: COLLISION_OBJECT_TYPE_TRIGGER\nmass: 0.0\nfriction: 0.0\nrestitution: 1.0\ngroup: \"16\"\nmask: \"2\"\n"
+}
