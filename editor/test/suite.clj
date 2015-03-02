0a7647656491c4e92e8b419645e32f4a1cb3ee06
Michael Nygard <pair+mn@cognitect.com>
2015-03-02 12:40:52 -0600

    Test in place for dependency tracing function.

--- editor/test/suite.clj
+++ editor/test/suite.clj
@@ -5,6 +5,7 @@
  (:import [clojure.lang Compiler]))
 
 (def test-namespaces ['internal.cache-test
+                      'internal.dependency-test
                       'internal.either-test
                       'internal.injection-test
                       'internal.math-test
