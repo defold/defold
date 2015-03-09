defold/defold

commit 5b9656f279e40ce8c2d35a4f2aac8ed3ace1c58c

Cache as top-level component
* During transactions, cache invalidation is collected
* Applied atomically at end of transaction.
* world-ref no longer has :cache or :cache-keys
* The whole cache-keys mechanism is no longer needed
* Cache is indexed by [node-id output] in all cases.

editor/dev/tools.clj
@@ -11,6 +11,7 @@
             [dynamo.system :as ds]
             [dynamo.types :as t]
             [dynamo.ui :as ui]
+            [dynamo.cache :as cache]
             [internal.graph.dgraph :as dg]
             [internal.graph.lgraph :as lg]
             [internal.java :as j]
@@ -35,7 +36,7 @@
 
 (defn the-world       [] (-> is/the-system deref :world))
 (defn the-world-state [] (-> (the-world) :state deref))
-(defn the-cache       [] (-> (the-world-state) :cache))
+(defn the-cache       [] (-> is/the-system deref :cache))
 (defn the-graph       [] (-> (the-world-state) :graph))
 (defn nodes           [] (-> (the-graph) :nodes vals))
 
@@ -84,21 +85,14 @@
 
 (defn cache-peek
   [id label]
-  (if-let [cache-key (some-> (the-world-state) :cache-keys (get-in [id label]))]
-    (if-let [x (get (the-cache) cache-key)]
-      x
-      :value-not-cached)
-    :output-not-cacheable))
+  (if-let [x (get cache/cache-snapshot [id label])]
+    x
+    :value-not-cached))
 
 (defn decache
   [id label]
-  (if-let [cache-key (some-> (the-world-state) :cache-keys (get-in [id label]))]
-    (if (get (the-cache) cache-key)
-      (dosync
-        (alter (:state (the-world)) update-in [:cache] clojure.core.cache/evict cache-key)
-        :ok)
-      :value-not-cached)
-    :output-not-cacheable))
+  (cache/cache-invalidate [[id label]])
+  :ok)
 
 (defn projects-in-memory
   []

editor/src/clj/dynamo/cache.clj
@@ -11,7 +11,7 @@
   [pairs]
   (c/cache-invalidate (is/system-cache) pairs))
 
-(defn cache-miss
+(defn cache-encache
   "Uses the system’s Cache component.
 
   Atomic action to record one or more items in cache.

editor/src/clj/dynamo/system/test_support.clj
@@ -14,6 +14,10 @@
         world       (is/world report-ch (ref #{}) disposal-ch)]
     (component/start world)))
 
+(defn clean-system
+  []
+  (component/start-system (is/system)))
+
 (defmacro with-clean-world
   [& forms]
   `(let [~'world     (clean-world)
@@ -22,6 +26,16 @@
      (binding [it/*transaction* (it/->TransactionSeed ~'world-ref)]
        (ds/in ~'root
            ~@forms))))
+
+(defmacro with-clean-system
+  [& forms]
+  `(let [~'system    (clean-system)
+         ~'world     (:world ~'system)
+         ~'world-ref (:state ~'world)
+         ~'root      (ds/node ~'world-ref 1)]
+     (binding [it/*transaction* (it/->TransactionSeed ~'world-ref)]
+       (ds/in ~'root
+              ~@forms))))
 
 (defn tx-nodes [& resources]
   (ds/transactional

editor/src/clj/internal/cache.clj
@@ -84,22 +84,6 @@
   []
   (cc/lru-cache-factory {} :threshold 1000))
 
-(defrecord CacheLifecycle [limit dispose-ch state]
-  component/Lifecycle
-  (start [this]
-    (if state
-      this
-      (assoc this :state (atom (lru-cache-factory {} :threshold limit :dispose-ch dispose-ch)))))
-
-  (stop [this]
-    (if state
-      (assoc this :state nil)
-      this))
-
-  clojure.lang.IDeref
-  (deref [this]
-    (when state @state)))
-
 (defn- encache
   [c kvs]
   (reduce
@@ -114,7 +98,37 @@
      (cc/hit c k))
    c ks))
 
-(defn- decache [cache ks] (reduce cc/evict cache ks))
+(defn- evict [cache ks] (reduce cc/evict cache ks))
+
+(defn- evict! [storage ks]
+  (swap! storage evict ks))
+
+(defn- evict-obsoletes
+  [storage _ world-state _ new-world-state]
+  (let [obsoletes (-> new-world-state :last-tx :outputs-modified)]
+    (evict! storage obsoletes)))
+
+(defrecord CacheLifecycle [limit dispose-ch storage]
+  component/Lifecycle
+  (start [this]
+    (if storage
+      this
+      (do
+        (assert (:world this) "A world component is required.")
+        (let [storage (atom (lru-cache-factory {} :threshold limit :dispose-ch dispose-ch))]
+          (add-watch (-> this :world :state) ::decache (partial evict-obsoletes storage))
+          (assoc this :storage storage)))))
+
+  (stop [this]
+    (if storage
+      (do
+        (remove-watch (-> this :world :state) ::decache)
+        (assoc this :storage nil))
+      this))
+
+  clojure.lang.IDeref
+  (deref [this]
+    (when storage @storage)))
 
 ;; ----------------------------------------
 ;; Interface
@@ -122,7 +136,7 @@
 
 (defn cache-subsystem
   [limit dispose-ch]
-  (CacheLifecycle. limit dispose-ch nil))
+  (map->CacheLifecycle {:limit limit :dispose-ch dispose-ch}))
 
 (defn cache-snapshot
   [ccomp]
@@ -130,12 +144,12 @@
 
 (defn cache-hit
   [ccomp ks]
-  (swap! (:state ccomp) hits ks))
+  (swap! (:storage ccomp) hits ks))
 
-(defn cache-miss
+(defn cache-encache
   [ccomp kvs]
-  (swap! (:state ccomp) encache kvs))
+  (swap! (:storage ccomp) encache kvs))
 
 (defn cache-invalidate
   [ccomp ks]
-  (swap! (:state ccomp) decache ks))
+  (evict! (:storage ccomp) ks))

editor/src/clj/internal/system.clj
@@ -20,6 +20,7 @@
 (def ^:private maximum-disposal-backlog 2500)
 
 (prefer-method print-method java.util.Map clojure.lang.IDeref)
+(prefer-method print-method clojure.lang.IPersistentMap clojure.lang.IDeref)
 
 (defn graph [world-ref]
   (-> world-ref deref :graph))
@@ -31,8 +32,6 @@
 (defn new-world-state
   [state root repaint-needed disposal-queue]
   {:graph               (attach-root (dg/empty-graph) root)
-   :cache               (c/make-cache) ;; TODO - remove once the new component is in use
-   :cache-keys          {}
    :world-time          0
    :message-bus         (bus/make-bus)
    :disposal-queue      disposal-queue
@@ -169,7 +168,7 @@
     {:refresh   (refresh-subsystem (shred-tx-reports tx-report-chan) 1)
      :world     (world tx-report-chan repaint-needed disposal-queue)
      :repaint   (component/using (repaint/repaint-subsystem repaint-needed) [:world])
-     :cache     (c/cache-subsystem maximum-cached-items disposal-queue)})))
+     :cache     (component/using (c/cache-subsystem maximum-cached-items disposal-queue) [:world])})))
 
 (def the-system (atom (system)))
 

editor/src/clj/internal/transaction.clj
@@ -1,6 +1,5 @@
 (ns internal.transaction
   (:require [clojure.core.async :as a]
-            [clojure.core.cache :as cache]
             [clojure.set :as set]
             [dynamo.types :as t]
             [dynamo.util :refer :all]
@@ -23,9 +22,6 @@
 ; ---------------------------------------------------------------------------
 (def ^:dynamic *tx-debug* nil)
 (def ^:dynamic *scope* nil)
-
-(def ^:private ^java.util.concurrent.atomic.AtomicInteger nextkey (java.util.concurrent.atomic.AtomicInteger. 1000000))
-(defn- new-cache-key [] (.getAndIncrement nextkey))
 
 (def ^:private ^java.util.concurrent.atomic.AtomicInteger next-txid (java.util.concurrent.atomic.AtomicInteger. 1))
 (defn- new-txid [] (.getAndIncrement next-txid))
@@ -145,9 +141,6 @@
 (defn has-tempid? [n] (and (:_id n) (tempid? (:_id n))))
 (defn resolve-tempid [ctx x] (when x (if (pos? x) x (get (:tempids ctx) x))))
 
-(defn node->cache-keys [n]
-  (zipmap (t/cached-outputs n) (repeatedly new-cache-key)))
-
 ; ---------------------------------------------------------------------------
 ; Executing transactions
 ; ---------------------------------------------------------------------------
@@ -171,7 +164,7 @@
 (defmulti perform (fn [ctx m] (:type m)))
 
 (defmethod perform :create-node
-  [{:keys [graph tempids cache-keys triggers-to-fire nodes-affected world-ref new-event-loops nodes-added] :as ctx}
+  [{:keys [graph tempids triggers-to-fire nodes-affected world-ref new-event-loops nodes-added] :as ctx}
    {:keys [node]}]
   (let [next-id     (dg/next-node graph)
         full-node   (assoc node :_id next-id :world-ref world-ref) ;; TODO: can we remove :world-ref from nodes?
@@ -181,7 +174,6 @@
       :graph               graph-after
       :nodes-added         (conj nodes-added next-id)
       :tempids             (assoc tempids (:_id node) next-id)
-      :cache-keys          (assoc cache-keys next-id (node->cache-keys full-node))
       :new-event-loops     (if (satisfies? t/MessageTarget full-node) (conj new-event-loops next-id) new-event-loops)
       :triggers-to-fire    (update-in triggers-to-fire [next-id :added] concat [])
       :nodes-affected      (merge-with set/union nodes-affected {next-id (t/outputs full-node)}))))
@@ -211,7 +203,7 @@
     (lg/targets (:graph ctx) (:_id source-node) source-label)))
 
 (defmethod perform :become
-  [{:keys [graph tempids cache-keys nodes-affected world-ref new-event-loops obsolete-cache-keys old-event-loops] :as ctx}
+  [{:keys [graph tempids nodes-affected world-ref new-event-loops old-event-loops] :as ctx}
    {:keys [node-id to-node]}]
   (let [old-node         (dg/node graph node-id)
         to-node-id       (:_id to-node)
@@ -234,11 +226,10 @@
       :tempids             (if (tempid? to-node-id) (assoc tempids to-node-id node-id) tempids)
       :new-event-loops     (if start-loop (conj new-event-loops node-id)  new-event-loops)
       :old-event-loops     (if end-loop   (conj old-event-loops old-node) old-event-loops)
-      :obsolete-cache-keys (concat obsolete-cache-keys (vals (get cache-keys node-id)))
-      :cache-keys          (assoc cache-keys node-id (node->cache-keys to-node)))))
+)))
 
 (defmethod perform :delete-node
-  [{:keys [graph nodes-deleted old-event-loops nodes-added triggers-to-fire obsolete-cache-keys cache-keys] :as ctx} {:keys [node-id]}]
+  [{:keys [graph nodes-deleted old-event-loops nodes-added triggers-to-fire] :as ctx} {:keys [node-id]}]
   (when-not (dg/node graph (resolve-tempid ctx node-id))
     (prn :delete-node "Can't locate node for ID " node-id))
   (let [node-id     (resolve-tempid ctx node-id)
@@ -249,9 +240,7 @@
       :old-event-loops     (if (satisfies? t/MessageTarget node) (conj old-event-loops node) old-event-loops)
       :nodes-deleted       (assoc nodes-deleted node-id node)
       :nodes-added         (disj nodes-added node-id)
-      :obsolete-cache-keys (concat obsolete-cache-keys (vals (get cache-keys node-id)))
-      :triggers-to-fire    (update-in triggers-to-fire [node-id :deleted] concat [])
-      :cache-keys          (dissoc cache-keys node-id))))
+      :triggers-to-fire    (update-in triggers-to-fire [node-id :deleted] concat []))))
 
 (defmethod perform :update-property
   [{:keys [graph triggers-to-fire properties-modified] :as ctx} {:keys [node-id property fn args]}]
@@ -379,19 +368,16 @@
       ctx
       (recur (one-transaction-pass (assoc ctx :pending pending-actions) current-action) (dec retrigger-count)))))
 
-(def tx-report-keys [:status :obsolete-cache-keys :expired-outputs :values-to-dispose :new-event-loops :tempids :graph :nodes-added :nodes-deleted :outputs-modified :properties-modified :label])
+(def tx-report-keys [:status :expired-outputs :values-to-dispose :new-event-loops :tempids :graph :nodes-added :nodes-deleted :outputs-modified :properties-modified :label])
 
 (defn- finalize-update
-  "Makes the transacted graph the new value of the world-state graph.
-   Likewise for cache and cache-keys."
-  [{:keys [graph cache cache-keys world-time] :as ctx}]
+  "Makes the transacted graph the new value of the world-state graph."
+  [{:keys [graph world-time] :as ctx}]
   (let [empty-tx?  (empty? (:completed ctx))
         status     (if empty-tx? :empty :ok)
         world-time (if empty-tx? world-time (inc world-time))]
     (update-in ctx [:world] assoc
       :graph      graph
-      :cache      cache
-      :cache-keys cache-keys
       :world-time world-time
       :last-tx    (assoc (select-keys ctx tx-report-keys) :status status))))
 
@@ -401,8 +387,6 @@
     {:world-ref           world-ref
      :world               current-world
      :graph               (:graph current-world)
-     :cache               (:cache current-world)
-     :cache-keys          (:cache-keys current-world)
      :world-time          (:world-time current-world)
      :expired-outputs     []
      :tempids             {}
@@ -422,25 +406,6 @@
   (update-in ctx [:outputs-modified]
              #(gt/trace-dependencies graph %)))
 
-(defn- determine-obsoletes
-  [{:keys [graph obsolete-cache-keys outputs-modified cache-keys] :as ctx}]
-  (assoc ctx :obsolete-cache-keys
-    (concat obsolete-cache-keys
-            (keep identity (map #(get-in cache-keys %) outputs-modified)))))
-
-(defn- dispose-obsoletes
-  [{:keys [cache obsolete-cache-keys nodes-deleted] :as ctx}]
-  (let [candidates (concat
-                     (keep #(when (and % (e/exists? %)) (e/result %)) (map #(get cache %) obsolete-cache-keys))
-                     (filter t/disposable? (vals nodes-deleted)))]
-    (assoc ctx :values-to-dispose (keep identity candidates))))
-
-(defn- evict-obsolete-caches
-  [{:keys [obsolete-cache-keys] :as ctx}]
-  (when *tx-debug*
-    (println (txerrstr ctx "Evicting " (pr-str obsolete-cache-keys))))
-  (update-in ctx [:cache] (fn [c] (reduce cache/evict c obsolete-cache-keys))))
-
 (defn- determine-autoupdates
   [{:keys [graph outputs-modified] :as ctx}]
   (update-in ctx [:expired-outputs] concat
@@ -456,9 +421,6 @@
    (let [txr (-> ctx
                  exhaust-actions-and-triggers
                  trace-dependencies
-                 determine-obsoletes
-                 dispose-obsoletes
-                 evict-obsolete-caches
                  determine-autoupdates
                  finalize-update)]
      (ref-set world-ref (:world txr))

editor/test/dynamo/transaction_test.clj
@@ -1,12 +1,11 @@
 (ns dynamo.transaction-test
   (:require [clojure.core.async :as a]
             [clojure.test.check :as tc]
+            [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
-            [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test :refer :all]
-            [schema.core :as s]
-            [plumbing.core :refer [defnk fnk]]
+            [dynamo.cache :as cache]
             [dynamo.node :as n :refer [Scope]]
             [dynamo.project :as p]
             [dynamo.system :as ds]
@@ -18,7 +17,9 @@
             [internal.graph.dgraph :as dg]
             [internal.graph.lgraph :as lg]
             [internal.node :as in]
-            [internal.transaction :as it]))
+            [internal.transaction :as it]
+            [plumbing.core :refer [defnk fnk]]
+            [schema.core :as s]))
 
 (defn dummy-output [& _] :ok)
 
@@ -369,24 +370,19 @@
   (output cached-output s/Str :cached (fnk [] "an-output-value")))
 
 (defn cache-peek
-  [world-ref cache-key]
-  (some-> world-ref deref :cache (get cache-key)))
+  [system node-id output]
+  (some-> system :cache deref (get [node-id output])))
 
-(defn cache-locate-key
-  [world-ref node-id output]
-  (some-> world-ref deref :cache-keys (get-in [node-id output])))
-
+;; TODO - move this to an integration test group
 (deftest deleted-nodes-values-removed-from-cache
-  (with-clean-world
+  (with-clean-system
     (let [node    (ds/transactional (ds/add (n/construct CachedValueNode)))
           node-id (:_id node)]
       (is (= "an-output-value" (n/get-node-value node :cached-output)))
-      (let [cache-key    (cache-locate-key world-ref node-id :cached-output)
-            cached-value (cache-peek world-ref cache-key)]
+      (let [cached-value (cache-peek system node-id :cached-output)]
         (is (= "an-output-value" (e/result cached-value)))
         (ds/transactional (ds/delete node))
-        (is (nil? (cache-peek world-ref cache-key)))
-        (is (nil? (cache-locate-key world-ref node-id :cached-output)))))))
+        (is (nil? (cache-peek system node-id :cached-output)))))))
 
 (defrecord DisposableValue [disposed?]
   t/IDisposable
@@ -562,5 +558,3 @@
         (is (nil? (cache-peek world-ref cache-key)))
         (is (= {:x 42 :sum 42 :cached-sum 42} (in/collect-inputs world-before node {:x ::unused :sum ::unused :cached-sum ::unused})))
         (is (= 42 (some-> (cache-peek world-ref cache-key) e/result)))))))
-
-(run-tests)

editor/test/internal/cache_test.clj
@@ -8,22 +8,29 @@
 
 (def null-chan (a/chan (a/sliding-buffer 1)))
 
+(defn- mock-system [cache-size ch]
+  {:world {:state (ref {})}
+   :cache (component/using (cache-subsystem cache-size ch) [:world])})
+
 (defn- as-map [c] (select-keys c (keys c)))
 
 (defn cache-with-stuff
   []
-  (let [cache-component (component/start (cache-subsystem 1000 null-chan))]
-    (cache-hit cache-component [[:a 1] [:b 2] [:c 3]])
+  (let [system          (component/start-system (mock-system 1000 null-chan))
+        cache-component (:cache system)]
+    (cache-encache cache-component [[:a 1] [:b 2] [:c 3]])
     cache-component))
 
 (deftest cache-lifecycle-tests
   (testing "has an embedded cache"
-    (let [cache-component (component/start (cache-subsystem 1000 nil))]
+    (let [system          (component/start-system (mock-system 1000 nil))
+          cache-component (:cache system)]
       (is (not (nil? (cache-snapshot cache-component))))))
 
   (testing "things put into cache appear in snapshot"
-    (let [cache-component (component/start (cache-subsystem 1000 nil))]
-      (cache-hit cache-component [[:a 1] [:b 2] [:c 3]])
+    (let [system          (component/start-system (mock-system 1000 nil))
+          cache-component (:cache system)]
+      (cache-encache cache-component [[:a 1] [:b 2] [:c 3]])
       (let [snapshot (cache-snapshot cache-component)]
         (are [k v] (= v (get snapshot k))
              :a 1
@@ -31,21 +38,24 @@
              :c 3))))
 
   (testing "multiple starts are idempotent"
-    (let [cache-component (component/start (cache-subsystem 1000 nil))]
-      (cache-hit cache-component [[:a 1] [:b 2] [:c 3]])
+    (let [system          (component/start-system (mock-system 1000 nil))
+          cache-component (:cache system)]
+      (cache-encache cache-component [[:a 1] [:b 2] [:c 3]])
       (let [snapshot (cache-snapshot cache-component)
             cc2      (component/start cache-component)]
         (is (= snapshot (cache-snapshot cc2))))))
 
   (testing "after stop, is empty"
-    (let [cache-component (component/start (cache-subsystem 1000 nil))]
-      (cache-hit cache-component [[:a 1] [:b 2] [:c 3]])
+    (let [system          (component/start-system (mock-system 1000 nil))
+          cache-component (:cache system)]
+      (cache-encache cache-component [[:a 1] [:b 2] [:c 3]])
       (let [cc2 (component/stop cache-component)]
         (is (empty? (cache-snapshot cc2))))))
 
   (testing "multiple stops are idempotent"
-    (let [cache-component (component/start (cache-subsystem 1000 nil))]
-      (cache-hit cache-component [[:a 1] [:b 2] [:c 3]])
+    (let [system          (component/start-system (mock-system 1000 nil))
+          cache-component (:cache system)]
+      (cache-encache cache-component [[:a 1] [:b 2] [:c 3]])
       (let [cc2 (component/stop cache-component)
             cc3 (component/stop cc2)]
         (is (= cc2 cc3))))))
@@ -76,8 +86,9 @@
 (deftest value-disposal
   (testing "one value disposed when decached"
     (let [dispose-ch (a/chan 10)
-          ccomp      (component/start (cache-subsystem 1000 dispose-ch))]
-      (cache-hit ccomp [[:a (thing 1)] [:b (thing 2)] [:c (thing 3)]])
+          system     (component/start-system (mock-system 1000 dispose-ch))
+          ccomp      (:cache system)]
+      (cache-encache ccomp [[:a (thing 1)] [:b (thing 2)] [:c (thing 3)]])
       (cache-invalidate ccomp [:a])
       (yield)
       (let [waiting-to-dispose (ia/take-all dispose-ch)]
@@ -86,8 +97,9 @@
 
   (testing "multiple values disposed when decached"
     (let [dispose-ch (a/chan 10)
-          ccomp      (component/start (cache-subsystem 1000 dispose-ch))]
-      (cache-hit ccomp [[:a (thing 1)] [:b (thing 2)] [:c (thing 3)]])
+          system     (component/start-system (mock-system 1000 dispose-ch))
+          ccomp      (:cache system)]
+      (cache-encache ccomp [[:a (thing 1)] [:b (thing 2)] [:c (thing 3)]])
       (cache-invalidate ccomp [:b :c])
       (yield)
       (let [waiting-to-dispose (ia/take-all dispose-ch)]
@@ -96,10 +108,11 @@
 
   (testing "values that are pushed out also get disposed"
     (let [dispose-ch (a/chan 10)
-          ccomp      (component/start (cache-subsystem 1 dispose-ch))]
-      (cache-hit ccomp [[:a (thing 1)]])
+          system     (component/start-system (mock-system 1 dispose-ch))
+          ccomp      (:cache system)]
+      (cache-encache ccomp [[:a (thing 1)]])
       (yield)
-      (cache-hit ccomp [[:b (thing 2)] [:c (thing 3)]])
+      (cache-encache ccomp [[:b (thing 2)] [:c (thing 3)]])
       (yield)
       (let [waiting-to-dispose (ia/take-all dispose-ch)]
         (is (= 2 (count waiting-to-dispose)))

commit 5ca996273f83c888b279c6cefa494961d1d34901

Fix bug in cache-peek introduced in 5b9656f

editor/dev/tools.clj
@@ -85,7 +85,7 @@
 
 (defn cache-peek
   [id label]
-  (if-let [x (get cache/cache-snapshot [id label])]
+  (if-let [x (get (cache/cache-snapshot) [id label])]
     x
     :value-not-cached))
 