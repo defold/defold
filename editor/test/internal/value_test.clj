ea5d8011662bfa3701408b68beeab2f3632cc142
Michael Nygard <pair+mn@cognitect.com>
2015-04-13 14:46:28 -0500

    Simplify graph API
    * Consolidate `attach-graph`, `attach-graph-with-history`, and `make-graph` into just `make-graph!`

--- editor/test/internal/value_test.clj
+++ editor/test/internal/value_test.clj
@@ -215,8 +215,8 @@
 
 (deftest invalidation-across-graphs
   (with-clean-system
-    (let [project-graph (g/attach-graph-with-history (g/make-graph))
-          view-graph    (g/attach-graph (g/make-graph :volatility 100))
+    (let [project-graph (g/make-graph! :history true)
+          view-graph    (g/make-graph! :volatility 100)
           [content-node aux-node] (tx-nodes (g/make-node project-graph CacheTestNode :scalar "Snake")
                                             (g/make-node project-graph CacheTestNode :scalar "Plissken"))
           [view-node]    (tx-nodes (g/make-node view-graph CacheTestNode))]
