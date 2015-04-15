7e6cc10abe20e4f77fe9bd52a9f1cad9f439d614
Ragnar Svensson <ragnar.svensson@king.com>
2015-04-15 15:22:46 +0200

    [Delivers #92479474] Save all functionality in editor.
    * Only implemented for GO and Sprites, but generic.
    * Requires save-data outputs for anything saveable.
    * No incremental save, everything that can be saved is saved.

--- editor/resources/test_project/logic/atlas_sprite.go
+++ editor/resources/test_project/logic/atlas_sprite.go
@@ -1,5 +1,5 @@
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/switcher/switcher.atlas\"\ndefault_animation: \"blue_candy\"\n"
+  data: "tile_set: \"/switcher/switcher.atlas\"\ndefault_animation: \"blue_candy\"\nblend_mode: BLEND_MODE_ALPHA\n"
 }
--- editor/src/clj/editor/game_object.clj
+++ editor/src/clj/editor/game_object.clj
@@ -9,7 +9,7 @@
             [editor.project :as project]
             [editor.scene :as scene]
             [editor.workspace :as workspace])
-  (:import [com.dynamo.gameobject.proto GameObject GameObject$PrototypeDesc]
+  (:import [com.dynamo.gameobject.proto GameObject GameObject$PrototypeDesc GameObject$ComponentDesc GameObject$EmbeddedComponentDesc]
            [com.dynamo.graphics.proto Graphics$Cubemap Graphics$TextureImage Graphics$TextureImage$Image Graphics$TextureImage$Type]
            [com.jogamp.opengl.util.awt TextRenderer]
            [dynamo.types Region Animation Camera Image TexturePacking Rect EngineFormatTexture AABB TextureSetAnimationFrame TextureSetAnimation TextureSet]
@@ -21,12 +21,25 @@
 
 (def game-object-icon "icons/brick.png")
 
+(defn- produce-ref-ddf [id save-data]
+  (.build (doto (GameObject$ComponentDesc/newBuilder)
+            (.setId id)
+            ; TODO - fix global path hack
+            (.setComponent (str "/" (workspace/path (:resource save-data)))))))
+
+(defn- produce-embed-ddf [id save-data]
+  (.build (doto (GameObject$EmbeddedComponentDesc/newBuilder)
+            (.setId id)
+            (.setType (:ext (workspace/resource-type (:resource save-data))))
+            (.setData (:content save-data)))))
+
 (g/defnode ComponentNode
   (property id t/Str)
   (property embedded t/Bool (visible false))
 
   (input source t/Any)
   (input outline t/Any)
+  (input save-data t/Any)
 
   (output context-menu t/Any (g/fnk [self embedded] [{:label "Delete"
                                                       :handler-fn (fn [event] (g/transact
@@ -37,7 +50,18 @@
   (output render-setup t/Any (g/fnk [source]
                                     (when-let [resource-type (project/get-resource-type source)]
                                       (let [view-fns (:scene (:view-fns resource-type))]
-                                        {:self source :setup-rendering-fn (:setup-rendering-fn view-fns)})))))
+                                        {:self source :setup-rendering-fn (:setup-rendering-fn view-fns)}))))
+  (output ddf-message t/Any :cached (g/fnk [id embedded save-data] (if embedded (produce-embed-ddf id save-data) (produce-ref-ddf id save-data)))))
+
+(g/defnk produce-save-data [resource ref-ddf embed-ddf]
+  {:resource resource
+   :content (protobuf/pb->str
+              (let [builder (GameObject$PrototypeDesc/newBuilder)]
+                (doseq [ddf ref-ddf]
+                  (.addComponents builder ddf))
+                (doseq [ddf embed-ddf]
+                  (.addEmbeddedComponents builder ddf))
+                (.build builder)))})
 
 (g/defnode GameObjectNode
   (inherits project/ResourceNode)
@@ -45,9 +69,17 @@
   (input aabbs [AABB])
   (input outline [t/Any])
   (input render-setups [t/Any])
+  (input ref-ddf [t/Any])
+  (input embed-ddf [t/Any])
 
   (output outline t/Any (g/fnk [self outline] {:self self :label "Game Object" :icon game-object-icon :children outline}))
-  (output aabb AABB (g/fnk [aabbs] (reduce (fn [a b] (geom/aabb-union a b)) (geom/null-aabb) aabbs))))
+  (output aabb AABB (g/fnk [aabbs] (reduce (fn [a b] (geom/aabb-union a b)) (geom/null-aabb) aabbs)))
+  (output save-data t/Any :cached produce-save-data))
+
+(defn- connect-if-output [out-node out-label in-node in-label]
+  (if ((g/outputs out-node) out-label)
+    (g/connect out-node out-label in-node in-label)
+    []))
 
 (defn load-game-object [project self input]
   (let [project-graph (g/node->graph-id self)
@@ -60,10 +92,10 @@
                       (if-let [source-node (project/resolve-resource-node self (:component component))]
                         (concat
                           (g/connect comp-node :render-setup self :render-setups)
+                          (g/connect comp-node :ddf-message self :ref-ddf)
                           (g/connect source-node :self comp-node :source)
-                          (if (:outline (g/outputs source-node))
-                            (g/connect source-node :outline comp-node :outline)
-                            []))
+                          (connect-if-output source-node :outline comp-node :outline)
+                          (connect-if-output source-node :save-data comp-node :save-data))
                         [])))
       (for [embedded (:embedded-components prototype)]
         (let [resource (project/make-embedded-resource project (:type embedded) (:data embedded))]
@@ -73,8 +105,10 @@
                            source-node [(:node-type resource-type) :resource resource :parent project :resource-type resource-type]]
                           (g/connect source-node :self         comp-node :source)
                           (g/connect source-node :outline      comp-node :outline)
+                          (g/connect source-node :save-data comp-node :save-data)
                           (g/connect comp-node   :outline      self      :outline)
-                          (g/connect comp-node   :render-setup self      :render-setups))
+                          (g/connect comp-node   :render-setup self      :render-setups)
+                          (g/connect comp-node   :ddf-message  self      :embed-ddf))
             (g/make-nodes project-graph
                           [comp-node [ComponentNode :id (:id embedded) :embedded true]]
                           (g/connect comp-node   :outline      self      :outline))))))))
