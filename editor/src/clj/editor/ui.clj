fe09df19b94cc20e2600d567171096481d901147
Christian Murray <christian.murray@king.com>
2015-04-22 19:44:22 +0200

    Toolbar support and misc updates

--- editor/src/clj/editor/ui.clj
+++ editor/src/clj/editor/ui.clj
@@ -3,14 +3,48 @@
             [editor.jfx :as jfx]
             [editor.handler :as handler]
             [service.log :as log])
-  (:import [javafx.scene.control ButtonBase Control ContextMenu SeparatorMenuItem ToggleButton TreeView TreeItem MenuItem]
+  (:import [javafx.scene Node Scene]
+           [javafx.scene.control ButtonBase Control ContextMenu SeparatorMenuItem ToggleButton TreeView TreeItem MenuItem]
            [javafx.application Platform]
            [javafx.event ActionEvent EventHandler]
            [javafx.scene.input KeyCombination ContextMenuEvent]))
 
 (set! *warn-on-reflection* true)
 
-(def ^:dynamic *menus* (atom {}))
+(defonce ^:dynamic *menus* (atom {}))
+
+(defn init []
+  (reset! *menus* {}))
+
+(defn do-run-now [f]
+  (let [p (promise)]
+    (Platform/runLater
+      (fn []
+        (try
+         (deliver p (f))
+         (catch Throwable e
+           (deliver p e)))))
+    (let [val @p]
+      (if (instance? Throwable val)
+        (throw val)
+        val))))
+
+(defmacro run-now
+  [& body]
+  `(do-run-now
+    (fn [] ~@body)))
+
+(defmacro run-later
+  [& body]
+  `(Platform/runLater
+    (fn [] ~@body)))
+
+(defn- set-user-data [^Node node key val]
+  (let [ud (or (.getUserData node) {})]
+    (.setUserData node (assoc ud key val))))
+
+(defn- get-user-data [^Node node key]
+  (get (.getUserData node) key))
 
 (defmacro event-handler [event & body]
   `(reify EventHandler
@@ -35,30 +69,32 @@
     (reduce (fn [acc x] (update-in acc [(:location x)] concat (:menu x))) {})))
 
 (defn- realize-menu [id]
-  (let [exts (collect-menu-extensions)]
-    (->>
-      (get @*menus* id)
-      (:menu)
-      (mapcat (fn [x] (concat [x] (get exts (:id x))))))))
+  (let [exts (collect-menu-extensions)
+        menu (->>
+               (get @*menus* id)
+               (:menu)
+               (mapcat (fn [x] (concat [x] (get exts (:id x))))))]
+    (concat menu (get exts id))))
 
-(defn- create-menu-item [item arg-map]
+(defn- create-menu-item [item command-context]
   (let [menu-item (MenuItem. (:label item))
         command (:command item)]
     (when (:acc item)
       (.setAccelerator menu-item (KeyCombination/keyCombination (:acc item))))
     (when (:icon item) (.setGraphic menu-item (jfx/get-image-view (:icon item))))
-    (.setDisable menu-item (not (handler/enabled? command arg-map)))
-    (.setOnAction menu-item (event-handler event (handler/run command arg-map) ))
+    (.setDisable menu-item (not (handler/enabled? command command-context)))
+    (.setOnAction menu-item (event-handler event (handler/run command command-context) ))
     menu-item))
 
 (defn- populate-context-menu [^ContextMenu context-menu selection-provider menu]
-  (let [instances (workspace/selection selection-provider)
-        arg-map {:instances instances}]
+  (let [selection (workspace/selection selection-provider)
+        command-context {:selection selection}]
     (doseq [item menu]
-      (let [menu-item (if (= :separator (:label item))
-                        (SeparatorMenuItem.)
-                        (create-menu-item item arg-map) )]
-        (.add (.getItems context-menu) menu-item)))))
+      (when (handler/visible? (:command item) command-context)
+        (let [menu-item (if (= :separator (:label item))
+                          (SeparatorMenuItem.)
+                          (create-menu-item item command-context) )]
+          (.add (.getItems context-menu) menu-item))))))
 
 (defn register-context-menu [^Control control selection-provider menu-id]
   (.addEventHandler control ContextMenuEvent/CONTEXT_MENU_REQUESTED
@@ -72,30 +108,46 @@
                        (.show cm control (.getScreenX ^ContextMenuEvent event) (.getScreenY ^ContextMenuEvent event))
                        (.consume event))))))
 
-(defn make-ui-binder [scene]
-  (atom {:scene scene
-         :toolbars {}}))
+(defn register-tool-bar [^Scene scene command-context selection-provider toolbar-id menu-id ]
+  (let [root (.getRoot scene)]
+    (if-let [toolbar (.lookup root toolbar-id)]
+      (let [toolbar-desc {:control toolbar
+                          :menu-id menu-id
+                          :command-context command-context
+                          :selection-provider selection-provider}]
+        (set-user-data root ::toolbars (assoc-in (get-user-data root ::toolbars) [toolbar-id] toolbar-desc)))
+      (log/warn :message (format "toolbar %s not found" toolbar-id)))))
 
-(defn bind-toolbar [binder toolbar-id arg-map]
-  (if-let [toolbar (.lookup (:scene @binder) toolbar-id)]
-    (do
-      (swap! binder assoc-in [:toolbars toolbar-id] toolbar)
-      (doseq [n (.getChildren toolbar)]
-        (when (instance? ButtonBase n)
-          (.setOnAction n (event-handler event (handler/run (keyword (.getId n)) arg-map))))))
-    (log/warn :message (format "toolbar %s not found" toolbar-id))))
+(defn- refresh-toolbar [td]
+ (let [menu (realize-menu (:menu-id td))
+       control (:control td)]
+   (when-not (= menu (get-user-data control ::menu))
+     (.clear (.getChildren control))
+     (set-user-data control ::menu menu)
+     (doseq [menu-item menu]
+       (let [button (ToggleButton. (:label menu-item))
+             icon (:icon menu-item)
+             selection-provider (:selection-provider td)]
+         (when icon
+           (.setGraphic button (jfx/get-image-view icon)))
+         (.setId button (name (:command menu-item)))
+         (.setOnAction button (event-handler event (handler/run (:command menu-item)
+                                                                (assoc (:command-context td) :selection (selection-provider)))))
+         (.add (.getChildren control) button))))))
 
-(defn- refresh-toolbar [toolbar arg-map]
+(defn- refresh-toolbar-state [toolbar command-context]
   (let [nodes (.getChildren toolbar)
         ids (map #(.getId %) nodes)]
     (doseq [n nodes]
-      (.setDisable n (not (handler/enabled? (keyword (.getId n)) arg-map)))
+      (.setDisable n (not (handler/enabled? (keyword (.getId n)) command-context)))
       (when (instance? ToggleButton n)
-        (if (handler/state (keyword (.getId n)) arg-map)
-          (.setSelected n true)
-          (.setSelected n false))))))
+        (if (handler/state (keyword (.getId n)) command-context)
+         (.setSelected n true)
+         (.setSelected n false))))))
 
-(defn refresh [binder arg-map]
-  (doseq [[_ t] (:toolbars @binder)]
-    (refresh-toolbar t arg-map)))
-
+(defn refresh [^Scene scene]
+ (let [root (.getRoot scene)
+       toolbar-descs (vals (get-user-data root ::toolbars))]
+   (doseq [td toolbar-descs]
+     (refresh-toolbar td)
+     (refresh-toolbar-state (:control td) (:command-context td)))))
---
+++ editor/test/editor/ui_test.clj
@@ -0,0 +1,51 @@
+(ns editor.ui-test
+  (:require [clojure.test :refer :all]
+            [editor.ui :as ui])
+  (:import [javafx.scene Scene]
+           [javafx.scene.layout Pane]))
+
+(defn fixture [f]
+  (ui/init)
+  (f))
+
+(use-fixtures :each fixture)
+
+(not (= ({:label "Open", :id :editor.ui-test/open} {:label "Save"} [:label "Quit"])
+        [{:label "Open", :id :editor.ui-test/open} {:label "Save"} {:label "Quit"}]))
+
+(deftest extend-menu-test
+  (ui/extend-menu ::root-menu nil
+                  [{:label "Open"
+                    :id ::open}])
+  (ui/extend-menu ::save-menu ::open
+                  [{:label "Save"}])
+  (ui/extend-menu ::quit-menu ::root-menu
+                  [{:label "Quit"}])
+  (is (= (#'ui/realize-menu ::root-menu) [{:label "Open"
+                                           :id ::open}
+                                          {:label "Save"}
+                                          {:label "Quit"}])))
+
+(deftest toolbar-test
+  (ui/extend-menu ::my-menu nil
+                  [{:label "Open"
+                    :id ::open}])
+
+  (let [root (Pane.)
+        scene (ui/run-now (Scene. root))
+        selection-provider nil
+        command-context {}]
+    (.setId root "toolbar")
+    (ui/register-tool-bar scene command-context selection-provider "#toolbar" ::my-menu)
+    (ui/run-now (ui/refresh scene))
+    (let [c1 (ui/run-now (ui/refresh scene) (.getChildren root))
+          c2 (ui/run-now (ui/refresh scene) (.getChildren root))]
+      (is (= 1 (count c1) (count c2)))
+      (is (= (.get c1 0) (.get c2 0))))
+
+    (ui/extend-menu ::extra ::open
+                    [{:label "Save"}])
+    (ui/run-now (ui/refresh scene))
+    (is (= 2 (count (.getChildren root))))))
+
+(run-tests)
