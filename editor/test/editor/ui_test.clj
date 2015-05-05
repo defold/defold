6f9a88c944035a13a53aa8776efae79b29cc4ef5
Christian Murray <christian.murray@king.com>
2015-05-05 15:13:13 +0200

    Various UI improvements
    * Splash and modal progress bar support
    * Project loaded (mostly) in separate thread from UI
    * Replaced old menu system with new one

--- editor/test/editor/ui_test.clj
+++ editor/test/editor/ui_test.clj
@@ -1,7 +1,10 @@
 (ns editor.ui-test
   (:require [clojure.test :refer :all]
-            [editor.ui :as ui])
+            [editor.ui :as ui]
+            [editor.workspace :as workspace]
+            [editor.handler :as handler])
   (:import [javafx.scene Scene]
+           [javafx.scene.control Menu MenuBar MenuItem]
            [javafx.scene.layout Pane]))
 
 (defn fixture [f]
@@ -10,21 +13,48 @@
 
 (use-fixtures :each fixture)
 
-(not (= ({:label "Open", :id :editor.ui-test/open} {:label "Save"} [:label "Quit"])
-        [{:label "Open", :id :editor.ui-test/open} {:label "Save"} {:label "Quit"}]))
-
 (deftest extend-menu-test
-  (ui/extend-menu ::root-menu nil
-                  [{:label "Open"
-                    :id ::open}])
-  (ui/extend-menu ::save-menu ::open
+  (ui/extend-menu ::menubar nil
+                  [{:label "File"
+                    :children [ {:label "New"
+                                 :id ::new} ]}])
+  (ui/extend-menu ::save-menu ::new
                   [{:label "Save"}])
-  (ui/extend-menu ::quit-menu ::root-menu
+  (ui/extend-menu ::quit-menu ::new
                   [{:label "Quit"}])
-  (is (= (#'ui/realize-menu ::root-menu) [{:label "Open"
-                                           :id ::open}
-                                          {:label "Save"}
-                                          {:label "Quit"}])))
+  (is (= (#'ui/realize-menu ::menubar) [{:label "File"
+                                         :children [{:label "New"
+                                                    :id ::new}
+                                                    {:label "Save"}
+                                                    {:label "Quit"}]}])))
+
+(defrecord TestSelectionProvider [selection]
+  workspace/SelectionProvider
+  (selection [this] selection))
+
+(deftest menu-test
+  (ui/extend-menu ::my-menu nil
+                  [{:label "File"
+                    :children [{:label "Open"
+                                :id ::open
+                                :command :open}
+                               {:label "Save"
+                                :command :save}]}])
+
+  (handler/defhandler my :open
+      (visible? [instances] true)
+      (enabled? [instances] true)
+      (run [instances] 123))
+
+  (let [root (Pane.)
+        scene (ui/run-now (Scene. root))
+        selection-provider (TestSelectionProvider. [])
+        command-context {}]
+    (let [menus (#'ui/make-menu {}  selection-provider (#'ui/realize-menu ::my-menu))]
+      (is (= 1 (count menus)))
+      (is (instance? Menu (first menus)))
+      (is (= 1 (count (.getItems (first menus)))))
+      (is (instance? MenuItem (first (.getItems (first menus))))))))
 
 (deftest toolbar-test
   (ui/extend-menu ::my-menu nil
@@ -33,10 +63,10 @@
 
   (let [root (Pane.)
         scene (ui/run-now (Scene. root))
-        selection-provider nil
+        selection-provider (TestSelectionProvider. [])
         command-context {}]
     (.setId root "toolbar")
-    (ui/register-tool-bar scene command-context selection-provider "#toolbar" ::my-menu)
+    (ui/register-toolbar scene command-context selection-provider "#toolbar" ::my-menu)
     (ui/run-now (ui/refresh scene))
     (let [c1 (ui/run-now (ui/refresh scene) (.getChildren root))
           c2 (ui/run-now (ui/refresh scene) (.getChildren root))]
@@ -47,4 +77,26 @@
                     [{:label "Save"}])
     (ui/run-now (ui/refresh scene))
     (is (= 2 (count (.getChildren root))))))
+
+(deftest menubar-test
+  (ui/extend-menu ::my-menu nil
+                  [{:label "Open"
+                    :id ::open}])
+
+  (let [root (Pane.)
+        scene (ui/run-now (Scene. root))
+        selection-provider (TestSelectionProvider. [])
+        command-context {}
+        menubar (MenuBar.)]
+    (ui/run-now (.add (.getChildren root) menubar))
+    (.setId menubar "menubar")
+    (ui/register-menubar scene command-context selection-provider "#menubar" ::my-menu)
+    (ui/run-now (ui/refresh scene))
+    (let [c1 (ui/run-now (ui/refresh scene) (.getChildren root))
+          c2 (ui/run-now (ui/refresh scene) (.getChildren root))]
+      (is (= 1 (count c1) (count c2)))
+      (is (= (.get c1 0) (.get c2 0))))
+    (ui/extend-menu ::extra ::open
+                    [{:label "Save"}])
+    (ui/run-now (ui/refresh scene))))
 
