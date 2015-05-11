6645f5e6f75897b804fb68d35001a02c27fbe8ab
Carin Meier and Michael Nygard <pair+cm+mn@cognitect.com>
2015-05-11 14:33:04 -0400

    add in a random reproducible graph

--- editor/test/benchmark/graph_benchmark.clj
+++ editor/test/benchmark/graph_benchmark.clj
@@ -6,11 +6,16 @@
             [internal.graph.generator :as ggen]
             [internal.system :as isys]
             [dynamo.graph.test-support :refer :all]
-            [dynamo.types :as t]))
+            [dynamo.types :as t]
+            [clojure.test.check.generators :as gen]))
 
 
-;;;Want to use non-random graphs do that we have a non-moving target
-;; for subsequent benchmarks
+(def build-network
+  (let [seed 21323424
+        seeded-random (fn [] (java.util.Random. seed))]
+    (with-redefs [gen/random seeded-random]
+      (with-clean-system
+        (ggen/make-random-graph-builder)))))
 
 (defmacro do-benchmark [benchmark-name f]
 ;;; For more rigorous use benchmark instead of quick-benchmark
@@ -27,51 +32,37 @@
          (cc/report-result bresults# :os)
          bresults#))))
 
+
 (g/defnode AThing
   (property a-property t/Str))
 
 (g/defnode Container
   (input nodes t/Any))
 
-(defn build-network
-  [world]
-  (let [nodes-num          10000
-        tx-result          (g/transact (repeatedly nodes-num  #(g/make-node world AThing)))
-        named-nodes        (g/tx-nodes-added tx-result)
-        named-node-ids     (map g/node-id named-nodes)
-        tx-result          (g/transact (repeatedly nodes-num  #(g/make-node world Container)))
-        container-nodes    (g/tx-nodes-added tx-result)
-        container-node-ids (map g/node-id container-nodes)
-        tx-result          (g/transact (map #(g/connect %1 :a-property %2 :nodes) named-nodes container-nodes))]
-   {:tx-result tx-result
-    :named-node-ids named-node-ids
-    :container-node-ids container-node-ids}))
-
-
 (defn network-creation []
   (do-benchmark "Whole Graph Network Creation"
                 (with-clean-system
-                  (build-network world))))
+                  (build-network))))
 
 (defn add-one-node []
   (println "Benching: Adding one node")
   (with-clean-system
-    (build-network world)
+    (build-network)
     (do-benchmark "Add One Node"
                   (g/transact (g/make-node world AThing)))))
 
 (defn add-one-node-delete-one-node []
   (with-clean-system
-    (build-network world)
+    (build-network)
     (do-benchmark "Add One Node and Delete One Node"
                   (let [[new-node] (g/tx-nodes-added (g/transact (g/make-node world AThing)))]
                     (g/transact (g/delete-node new-node))))))
 
 (defn set-property-some-nodes []
   (with-clean-system
-    (let [r              (build-network world)
+    (let [r              (build-network)
           affected-num    100
-          chosen-node-ids (repeatedly affected-num (partial rand-nth (:named-node-ids r)))
+          chosen-node-ids (repeatedly affected-num (partial rand-nth (ig/node-ids r)))
           chosen-nodes    (mapv #(g/node-by-id (g/now) %) chosen-node-ids)]
       (do-benchmark  (str "Set Property on " affected-num " Nodes")
                      (doseq [chosen-node chosen-nodes]
@@ -79,7 +70,7 @@
 
 (defn add-two-nodes-and-connect-them []
   (with-clean-system
-    (build-network world)
+    (build-network)
     (do-benchmark "Add Two Nodes and Connect Them"
                   (let [txn-results (g/transact [(g/make-node world AThing)
                                                  (g/make-node world Container)])
@@ -88,7 +79,7 @@
 
 (defn add-two-nodes-and-connect-and-disconnect-them []
   (with-clean-system
-    (build-network world)
+    (build-network)
     (do-benchmark "Add Two Nodes Connect and Disconnect Them"
      (let [txn-results (g/transact [(g/make-node world AThing)
                                     (g/make-node world Container)])
@@ -118,8 +109,7 @@
   (set-property-some-nodes)
   (add-two-nodes-and-connect-them)
   (add-two-nodes-and-connect-and-disconnect-them)
-  (run-many-transactions)
-  )
+  (run-many-transactions))
 
 (defn -main [& args]
   (println "Running benchmarks and outputing results to ./test/benchmark/bench-results.txt")
