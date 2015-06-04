c86107e5624f3c8e2873fa1e8e405089127e9365
Sven Andersson <sven.andersson@king.com>
2015-06-04 19:50:11 +0200

    Fixes for push API on Android.
    - Added push.get_scheduled and push.get_all_scheduled.
    - Fixed various bugs with local notifications specific for Android.

--- engine/push/src/java/com/defold/push/LocalPushDispatchActivity.java
+++ engine/push/src/java/com/defold/push/LocalPushDispatchActivity.java
@@ -13,17 +13,30 @@ public class LocalPushDispatchActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(Push.TAG, "LocalPushDispatchActivity.onCreate");
 
         PrintStream os = null;
         try {
             Bundle extras = getIntent().getExtras();
             if (extras != null) {
-                os = new PrintStream(openFileOutput(
-                        Push.SAVED_PUSH_MESSAGE_NAME, MODE_PRIVATE));
-                os.print(extras.getString("userdata"));
 
-                Push.getInstance().onLocalPush(extras.getString("userdata"));
+                if (Push.getInstance().hasListener())
+                {
+                    Push.getInstance().onLocalPush(extras.getString("payload"), extras.getInt("uid"));
+                } else {
+
+                    // need to save this to disk until a listener is set
+                    os = new PrintStream(openFileOutput(
+                            Push.SAVED_LOCAL_MESSAGE_NAME, MODE_PRIVATE));
+                    os.println(extras.getInt("uid"));
+                    os.print(extras.getString("payload"));
+
+                    Intent intent = getPackageManager().getLaunchIntentForPackage(
+                            getPackageName());
+                    intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                    startActivity(intent);
+                }
+
+
             } else {
                 Log.e(Push.TAG, "Unable to queue message. extras is null");
             }
@@ -35,9 +48,6 @@ public class LocalPushDispatchActivity extends Activity {
             }
         }
 
-        Intent intent = getPackageManager().getLaunchIntentForPackage(
-                getPackageName());
-        startActivity(intent);
         finish();
     }
 }
