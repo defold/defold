ef119c77053bc9deb8a152acad3c115485775db5
Dan Nilsson <dan.nilsson@king.com>
2015-06-15 14:17:58 +0200

    DEF-1155 Tracking
    - Review fixes

--- engine/tracking/src/tracking.h
+++ engine/tracking/src/tracking.h
@@ -19,15 +19,15 @@ namespace dmTracking
      * Start up and configure the tracking script. In case it fails it will start up
      * in a disabled mode.
      * @param context context
-     * @param saves_app_name App named passed to sys.get_save_file
-     * @param defold_version Version to supply to the backend
+     * @param saves_app_name app named passed to sys.get_save_file
+     * @param defold_version version to supply to the backend
      */
     void Start(HContext context, const char* saves_app_name, const char* defold_version);
 
     /**
      * Update tracking context. This triggers sends and internal handling of posted
      * events.
-     * @param context contetx
+     * @param context context
      * @param dt time delta since last call
      */
     void Update(HContext context, float dt);
@@ -35,26 +35,26 @@ namespace dmTracking
     /**
      * Finalize the system. Not necessary but gives the tracking system a chance to
      * flush out any pending saves. No more Update calls are allowed after this.
-     * @param context contetx
+     * @param context context
      */
     void Finalize(HContext context);
 
     /**
      * Destroy and free all resources.
-     * @param context contetx
+     * @param context context
      */
     void Delete(HContext context);
 
     /**
-     * Helper to register a simple event. Internally it posts a message to the socket
+     * Helper to post a simple event to the tracking socket.
      * @param context context
      * @param type type
      */
-    void SimpleEvent(HContext context, const char *type);
+    void PostSimpleEvent(HContext context, const char *type);
 
     /**
      * Get socket associated with a context.
-     * @param context contetx
+     * @param context context
      */
     dmMessage::HSocket GetSocket(HContext context);
 }
