990f3347e4bd8e380b37b960a4078c9b55605b88
Sven Andersson <sven.andersson@king.com>
2015-06-16 19:10:02 +0200

    Added error check in swap buffers for Android.
    - Added specific error check for swap buffers based on how it
      is done in the Android/Java implementation of GLSurfaceView.
    
    - Added new hasSurface flag to keep track when there is a
      rendering surface. This flag is included in the iconified check.

--- engine/glfw/lib/android/android_window.c
+++ engine/glfw/lib/android/android_window.c
@@ -144,8 +144,27 @@ void _glfwPlatformSwapBuffers( void )
         return;
     }
 
-    eglSwapBuffers(_glfwWin.display, _glfwWin.surface);
-    CHECK_EGL_ERROR
+    if (!eglSwapBuffers(_glfwWin.display, _glfwWin.surface))
+    {
+        // Error checking inspired by Android implementation of GLSurfaceView:
+        // http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.1.0_r1/android/opengl/GLSurfaceView.java?av=f
+        EGLint error = eglGetError();
+        if (error != EGL_SUCCESS) {
+
+            if (error == EGL_CONTEXT_LOST) {
+                LOGE("eglSwapBuffers failed due to EGL_CONTEXT_LOST!");
+                assert(0);
+                return;
+            } else {
+                // Other errors typically mean that the current surface is bad,
+                // probably because the SurfaceView surface has been destroyed,
+                // but we haven't been notified yet.
+                // Ignore error, but log for debugging purpose.
+                LOGW("eglSwapBuffers failed, eglGetError: %X", error);
+                return;
+            }
+        }
+    }
 
     /*
      The preferred way of handling orientation changes is probably
