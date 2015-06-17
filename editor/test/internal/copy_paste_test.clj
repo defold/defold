ab109f66abe8d3118e006520cbc5ca75637f14a4
Carin Meier <pair+cm@cognitect.com>
2015-06-17 11:26:21 -0400

    diamond paste working

--- editor/test/internal/copy_paste_test.clj
+++ editor/test/internal/copy_paste_test.clj
@@ -1,5 +1,6 @@
 (ns internal.copy-paste-test
   (:require [clojure.set :as set]
+            [clojure.string :as str]
             [clojure.test :refer :all]
             [dynamo.graph :as g]
             [dynamo.graph.test-support :as ts]
@@ -16,7 +17,9 @@
 
 (g/defnode ConsumeAndProduceNode
   (inherits ConsumerNode)
-  (inherits ProducerNode))
+  (inherits ProducerNode)
+
+  (output produces-value t/Str (g/fnk [consumes-value] (str/join " " consumes-value))))
 
 ;; (comment
 ;;   {:roots #{id-of-the-most-important-copied-node}
@@ -71,27 +74,60 @@
       (is (= (g/node-id node1) (first (:root-node-ids paste-data))))
       (is (g/connected? (g/now) (g/node-id node2) :produces-value (g/node-id node1) :consumes-value)))))
 
+(deftest poste-and-clone
+  (ts/with-clean-system
+    (let [fragment           (simple-copy-fragment world)
+          paste-once         (g/paste world fragment)
+          paste-once-result  (g/transact (:tx-data paste-once))
+          [node1-once node2-once] (g/tx-nodes-added paste-once-result)
+          paste-twice        (g/paste world fragment)
+          paste-twice-result (g/transact (:tx-data paste-twice))
+          [node1-twice node2-twice] (g/tx-nodes-added paste-twice-result)]
+      (is (not= (g/node-id node1-once) (g/node-id node1-twice)))
+      (is (not= (g/node-id node2-once) (g/node-id node2-twice)))
+      (is (g/connected? (g/now) (g/node-id node2-once)  :produces-value (g/node-id node1-once)  :consumes-value))
+      (is (g/connected? (g/now) (g/node-id node2-twice) :produces-value (g/node-id node1-twice) :consumes-value))
+      (is (not (g/connected? (g/now) (g/node-id node2-once)  :produces-value (g/node-id node1-twice) :consumes-value)))
+      (is (not (g/connected? (g/now) (g/node-id node2-twice) :produces-value (g/node-id node1-once)  :consumes-value))))))
+
+(defn diamond-copy-fragment [world]
+  (let [[node1 node2 node3 node4] (ts/tx-nodes (g/make-node world ConsumeAndProduceNode)
+                                               (g/make-node world ConsumeAndProduceNode)
+                                               (g/make-node world ConsumeAndProduceNode)
+                                               (g/make-node world ProducerNode) )]
+    (g/transact
+     [(g/connect node2 :produces-value node1 :consumes-value)
+      (g/connect node3 :produces-value node1 :consumes-value)
+      (g/connect node4 :produces-value node3 :consumes-value)
+      (g/connect node4 :produces-value node2 :consumes-value)])
+    (g/copy [(g/node-id node1)] (constantly true))))
 
 (deftest diamond-copy
   (ts/with-clean-system
-    (let [[node1 node2 node3 node4] (ts/tx-nodes (g/make-node world ConsumerNode)
-                                                 (g/make-node world ConsumeAndProduceNode)
-                                                 (g/make-node world ConsumeAndProduceNode)
-                                                 (g/make-node world ProducerNode) )
-          node1-id                  (g/node-id node1)]
-      (g/transact
-       [(g/connect node2 :produces-value node1 :consumes-value)
-        (g/connect node3 :produces-value node1 :consumes-value)
-        (g/connect node4 :produces-value node3 :consumes-value)
-        (g/connect node4 :produces-value node2 :consumes-value)])
-      (let [fragment            (g/copy [node1-id] (constantly true))
-            fragment-nodes      (:nodes fragment)
-            serial-ids          (map :serial-id fragment-nodes)
-            arc-node-references (into #{} (concat (map #(:node-id (first %))  (:arcs fragment))
-                                                  (map #(:node-id (second %)) (:arcs fragment))))]
-        (is (= 4 (count (:arcs fragment))))
-        (is (= 4 (count fragment-nodes)))
-        (is (empty? (set/difference (set arc-node-references) (set serial-ids))))))))
+    (let [fragment            (diamond-copy-fragment world)
+          fragment-nodes      (:nodes fragment)
+          serial-ids          (map :serial-id fragment-nodes)
+          arc-node-references (into #{} (concat (map #(:node-id (first %))  (:arcs fragment))
+                                                (map #(:node-id (second %)) (:arcs fragment))))]
+      (is (= 4 (count (:arcs fragment))))
+      (is (= 4 (count fragment-nodes)))
+      (is (empty? (set/difference (set arc-node-references) (set serial-ids)))))))
+
+(deftest diamond-paste
+  (ts/with-clean-system
+    (let [fragment        (diamond-copy-fragment world)
+          paste-data      (g/paste world fragment)
+          paste-tx-data   (:tx-data paste-data)
+          paste-tx-result (g/transact paste-tx-data)
+          new-nodes-added (g/tx-nodes-added paste-tx-result)
+          [node1 node2 node3 node4]   (:nodes paste-data)]
+      (is (= 1 (count (:root-node-ids paste-data))))
+      (is (= 4 (count (:nodes paste-data))))
+      (is (= [:create-node :create-node :create-node :create-node
+              :connect     :connect     :connect     :connect]  (map :type paste-tx-data)))
+      (is (= 4 (count new-nodes-added)))
+      (is (= (g/node-id node1) (first (:root-node-ids paste-data))))
+      (is (= (g/node-value node1 :produces-value) "A string A string")))))
 
 
 (deftest short-circut
