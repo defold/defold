43a21233cd4c86125b163487a2046695c2e23ce0
Ragnar Svensson <ragnar.svensson@king.com>
2015-06-18 00:13:35 +0200

    Pipeline proof of concept. DEFEDIT-112
    * Fixes also DEFEDIT-93
    
    Squashed commit of the following:
    
    commit ad005e7fec4030972dbf084abc6740923645923b
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Thu Jun 18 00:05:51 2015 +0200
    
        Fixed broken tests.
    
        * Sloppy merges
        * Sloppy coding
    
    commit 1d5a314c059e597775f51b799641c86b80f7a41a
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Wed Jun 17 23:22:59 2015 +0200
    
        Build caches are now pruned to avoid mem leaks.
    
    commit 2984144661049ceee7dddfce3d734d55f4754570
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Wed Jun 17 18:16:05 2015 +0200
    
        FS cache for the pipeline.
    
    commit b8f5aa5314af275d02dc0b8092eb535e5880ad77
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Wed Jun 17 17:09:42 2015 +0200
    
        Build targets are merged before being built.
    
        Squashed commit of the following:
    
        commit daf0231946f82d24b8da26672e5f2877cbd73699
        Author: Ragnar Svensson <ragnar.svensson@king.com>
        Date:   Wed Jun 17 17:08:40 2015 +0200
    
            Merge complete with user-data in the key.
    
            * Massive hack to pass proto-msgs with equality semantics (seq of bytearray)
    
        commit 1ef4bbc9d72df76b32a24432dd4a1f8339315d29
        Author: Ragnar Svensson <ragnar.svensson@king.com>
        Date:   Wed Jun 17 14:44:46 2015 +0200
    
            Build functions are custom-memoized. WIP
    
            * Simple cache for build targets
    
        commit 6ab57c17a57307e5fa5d5675a8fa884fafec03fc
        Author: Ragnar Svensson <ragnar.svensson@king.com>
        Date:   Wed Jun 17 12:57:02 2015 +0200
    
            Moved back to order-less build. WIP
    
        commit d304f4919d7a456fd0e2add0a4213c2a9c54f516
        Author: Ragnar Svensson <ragnar.svensson@king.com>
        Date:   Wed Jun 17 09:11:51 2015 +0200
    
            Merging pipeline WIP
    
    commit f23d4d45b6d6aef15985e7539fcf50e29c0df99b
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Tue Jun 16 10:11:42 2015 +0200
    
        Readded build test project (previously incorrectly added as a submodule)
    
    commit a8db98ec6d60569e6e259d68c1bd1273f6fee62a
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Tue Jun 16 10:10:59 2015 +0200
    
        Removing git submodule
    
    commit 06a5d656da5c19f9b279e6339f122f08b8848703
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Tue Jun 16 10:10:20 2015 +0200
    
        Fixed build handler to match updated API.
    
    commit b875384235fb5a36b631303ba59e0b6bc6c2a896
    Merge: 5b7c5c0 b3a140c
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Tue Jun 16 09:29:26 2015 +0200
    
        Merge branch 'javafx-project' into pipeline
    
    commit 5b7c5c02f6094900964972adf539501cc382f495
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Mon Jun 15 20:03:20 2015 +0200
    
        Pipeline PoC. WIP
    
        * Collection compiling
        * Game object compiling
        * Game project compiling
    
    commit 8a94970cdb4b6e3595f4c80450493149f6f7c155
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Thu Jun 11 15:32:25 2015 +0200
    
        Initial pipeline. WIP
    
        * Build-targets for game.project file
        * Simple build in project.clj, writes to disk

---
+++ editor/resources/build_project/SideScroller/background/large_clouds.go
@@ -0,0 +1,115 @@
+components {
+  id: "script"
+  component: "/background/large_clouds.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/background/background.atlas\"\n"
+  "default_animation: \"earthlike_planet\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 367.68527
+    y: 271.58572
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite1"
+  type: "sprite"
+  data: "tile_set: \"/background/background.atlas\"\n"
+  "default_animation: \"gas_planet\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 1090.5211
+    y: 655.98395
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite2"
+  type: "sprite"
+  data: "tile_set: \"/background/background.atlas\"\n"
+  "default_animation: \"ice_planet\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 1203.3336
+    y: 125.34725
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite3"
+  type: "sprite"
+  data: "tile_set: \"/background/background.atlas\"\n"
+  "default_animation: \"ring_planet\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 2047.3385
+    y: 614.20154
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite4"
+  type: "sprite"
+  data: "tile_set: \"/background/background.atlas\"\n"
+  "default_animation: \"earthlike_planet\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 2444.2715
+    y: 116.99077
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ editor/src/clj/editor/game_project.clj
@@ -0,0 +1,110 @@
+(ns editor.game-project
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]
+            [dynamo.buffers :refer :all]
+            [dynamo.camera :refer :all]
+            [dynamo.file.protobuf :as protobuf]
+            [dynamo.geom :as geom]
+            [dynamo.graph :as g]
+            [dynamo.types :as t :refer :all]
+            [dynamo.ui :refer :all]
+            [editor.math :as math]
+            [editor.project :as project]
+            [editor.scene :as scene]
+            [editor.workspace :as workspace]
+            [editor.core :as core]
+            [editor.ui :as ui]
+            [editor.handler :as handler]
+            [editor.dialogs :as dialogs]
+            [editor.outline-view :as outline-view])
+  (:import [com.dynamo.gameobject.proto GameObject GameObject$PrototypeDesc  GameObject$ComponentDesc GameObject$EmbeddedComponentDesc GameObject$PrototypeDesc$Builder]
+           [com.dynamo.graphics.proto Graphics$Cubemap Graphics$TextureImage Graphics$TextureImage$Image Graphics$TextureImage$Type]
+           [com.dynamo.proto DdfMath$Point3 DdfMath$Quat]
+           [com.jogamp.opengl.util.awt TextRenderer]
+           [dynamo.types Region Animation Camera Image TexturePacking Rect EngineFormatTexture AABB TextureSetAnimationFrame TextureSetAnimation TextureSet]
+           [java.awt.image BufferedImage]
+           [java.io PushbackReader StringReader BufferedReader]
+           [javax.media.opengl GL GL2 GLContext GLDrawableFactory]
+           [javax.media.opengl.glu GLU]
+           [javax.vecmath Matrix4d Point3d Quat4d Vector3d]))
+
+(def game-object-icon "icons/brick.png")
+
+(g/defnk produce-save-data [resource content]
+  {:resource resource
+   :content content})
+
+(defn- build-game-project [self basis resource dep-resources user-data]
+  {:resource resource :content (.getBytes (:content user-data))})
+
+(g/defnode GameProjectNode
+  (inherits project/ResourceNode)
+
+  (property content t/Str)
+
+  (input dep-build-targets t/Any :array)
+
+  (output outline t/Any :cached (g/fnk [node-id] {:node-id node-id :label "Game Project" :icon game-object-icon}))
+  (output save-data t/Any :cached produce-save-data)
+  (output build-targets t/Any :cached (g/fnk [node-id resource content dep-build-targets]
+                                             [{:node-id node-id
+                                               :resource (workspace/make-build-resource resource)
+                                               :build-fn build-game-project
+                                               :user-data {:content content}
+                                               :deps (vec (flatten dep-build-targets))}])))
+
+(defn- line! [reader]
+  (let [line (.readLine reader)]
+    (when line
+      (s/trim line))))
+
+(defn- parse-properties [reader]
+  (loop [line (line! reader)
+         properties (transient {})]
+    (if line
+      (if (.startsWith line "[")
+        (let [cat-name (subs line 1 (dec (count line)))
+              [line cat-props] (loop [line (line! reader)
+                                      cat-props (transient (get properties name {}))]
+                                 (if (or (nil? line) (.startsWith line "["))
+                                   [line cat-props]
+                                   (let [sep (.indexOf line "=")]
+                                     (if (= sep -1)
+                                       (recur (line! reader) cat-props)
+                                       (let [key (s/trim (subs line 0 sep))
+                                             val (s/trim (subs line (inc sep)))]
+                                         (recur (line! reader) (assoc! cat-props key val)))))))]
+          (recur line (assoc! properties cat-name (persistent! cat-props))))
+        (recur (line! reader) properties))
+      (persistent! properties))))
+
+(def meta-properties (parse-properties (io/reader (io/resource "meta.properties"))))
+
+(defn- property [properties category key]
+  (get-in properties [category key] (get-in meta-properties [category (str key ".default")])))
+
+(defn- root-node [self properties category key]
+  (let [path (property properties category key)
+        ; TODO - hack for compiled files in game.project
+        path (subs path 0 (dec (count path)))
+        ; TODO - hack for inconsistencies in game.project paths
+        path (if (.startsWith path "/")
+               path
+               (str "/" path))]
+    (project/resolve-resource-node self path)))
+
+(defn load-game-project [project self input]
+  (let [content (slurp input)
+        properties (parse-properties (BufferedReader. (StringReader. content)))
+        main-collection (root-node self properties "bootstrap" "main_collection")]
+    (concat
+      (g/set-property self :content content)
+      (g/connect main-collection :build-targets self :dep-build-targets))))
+
+(defn register-resource-types [workspace]
+  (workspace/register-resource-type workspace
+                                    :ext "project"
+                                    :node-type GameProjectNode
+                                    :load-fn load-game-project
+                                    :icon game-object-icon
+                                    :view-types [:text]))
