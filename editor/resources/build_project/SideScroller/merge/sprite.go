938dda816ab158a074a51ebee1034de730d85976
Ragnar Svensson <ragnar.svensson@king.com>
2015-06-20 10:40:42 +0200

    Sprite compiler WIP

---
+++ editor/resources/build_project/SideScroller/merge/sprite.go
@@ -0,0 +1,15 @@
+components {
+  id: "sprite"
+  component: "/merge/empty.sprite"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
