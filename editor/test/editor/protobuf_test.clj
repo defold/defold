78c87400d11e09588b1fae6d6ff5230d30edda73
Ragnar Svensson <ragnar.svensson@king.com>
2015-06-22 15:27:53 +0200

    Cleaned up protobuf API.
    * Added test for repeated primitives too

--- editor/test/editor/protobuf_test.clj
+++ editor/test/editor/protobuf_test.clj
@@ -1,22 +1,26 @@
 (ns editor.protobuf-test
   (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
             [editor.protobuf :as protobuf])
-  (:import [TestScript TestDdf TestDdf$Msg TestDdf$SubMsg TestDdf$Transform TestDdf$DefaultValue
-            TestDdf$OptionalNoDefaultValue TestDdf$EmptyMsg TestDdf$Uint64Msg]
+  (:import [com.defold.editor.test TestDdf TestDdf$Msg TestDdf$SubMsg TestDdf$Transform TestDdf$DefaultValue
+            TestDdf$OptionalNoDefaultValue TestDdf$EmptyMsg TestDdf$Uint64Msg TestDdf$RepeatedUints
+            TestAtlasProto$AtlasAnimation TestAtlasProto$AtlasImage]
            [javax.vecmath Point3d Vector3d]))
+
+(defn- round-trip [^java.lang.Class cls m]
+  (with-in-str (protobuf/map->str cls m)
+    (protobuf/read-text cls (io/reader *in*))))
 
 (deftest simple
   (let [m {:uint-value 1}
-        msg (protobuf/map->pb TestDdf$SubMsg m)
-        new-m (protobuf/pb->map msg)]
-    (is (= 1 (.getUintValue msg)))
+        new-m (round-trip TestDdf$SubMsg m)]
+    (is (= 1 (:uint-value new-m)))
     (is (= m new-m))))
 
 (deftest transform
   (let [m {:position [0.0 1.0 2.0]
            :rotation [0.0 1.0 2.0 3.0]}
-        msg (protobuf/map->pb TestDdf$Transform m)
-        new-m (protobuf/pb->map msg)]
+        new-m (round-trip TestDdf$Transform m)]
     (is (= m new-m))))
 
 (deftest types
@@ -30,11 +34,10 @@
            :sub-msg-value {:uint-value 1}
            :enum-value :enum-val0
            :bool-value true}
-        msg (protobuf/map->pb TestDdf$Msg m)
-        new-m (protobuf/pb->map msg)]
-    (is (= 1 (.getUintValue msg)))
-    (is (= 2 (.getIntValue msg)))
-    (is (= "three" (.getStringValue msg)))
+        new-m (round-trip TestDdf$Msg m)]
+    (is (= 1 (:uint-value new-m)))
+    (is (= 2 (:int-value new-m)))
+    (is (= "three" (:string-value new-m)))
     (is (= m new-m))))
 
 (deftest default-vals
@@ -43,28 +46,38 @@
                   :quat-value [0.0 0.0 0.0 1.0]
                   :enum-value :enum-val1
                   :bool-value true}
-        msg (protobuf/map->pb TestDdf$DefaultValue {})
-        new-m (protobuf/pb->map msg)]
+        new-m (round-trip TestDdf$DefaultValue {})]
     (is (= defaults new-m))))
 
 (deftest optional-no-defaults
   (let [defaults {:uint-value 0
                   :string-value ""
                   :enum-value :enum-val0}
-        msg (protobuf/map->pb TestDdf$OptionalNoDefaultValue {})
-        new-m (protobuf/pb->map msg)]
+        new-m (round-trip TestDdf$OptionalNoDefaultValue {})]
     (is (= defaults new-m))))
 
 (deftest empty-msg
   (let [m {}
-        msg (protobuf/map->pb TestDdf$EmptyMsg m)
-        new-m (protobuf/pb->map msg)]
+        new-m (round-trip TestDdf$EmptyMsg m)]
     (is (= m new-m))))
 
 (deftest uint64-msg
   (let [m {:uint64-value 1}
-        msg (protobuf/map->pb TestDdf$Uint64Msg m)
-        new-m (protobuf/pb->map msg)]
+        new-m (round-trip TestDdf$Uint64Msg m)]
     (is (= m new-m))))
 
-(uint64-msg)
+(deftest repeated-msgs
+  (let [m {:id "my_anim"
+           :playback :playback-once-forward
+           :fps 30
+           :flip-horizontal 0
+           :flip-vertical 0
+           :images [{:image "/path/1.png"}
+                    {:image "/path/2.png"}]}
+        new-m (round-trip TestAtlasProto$AtlasAnimation m)]
+    (is (= m new-m))))
+
+(deftest repeated-uints
+  (let [m {:uint-values (into [] (range 10))}
+        new-m (round-trip TestDdf$RepeatedUints m)]
+    (is (= m new-m))))
