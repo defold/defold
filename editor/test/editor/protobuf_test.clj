cebdfd4600aaea10b4b5a0a006c0d010ece073d2
Ragnar Svensson <ragnar.svensson@king.com>
2015-06-22 12:09:20 +0200

    Added more tests, support for long/double. WIP
    * Repeated remaining

--- editor/test/editor/protobuf_test.clj
+++ editor/test/editor/protobuf_test.clj
@@ -1,7 +1,8 @@
 (ns editor.protobuf-test
   (:require [clojure.test :refer :all]
             [editor.protobuf :as protobuf])
-  (:import [TestScript TestDdf TestDdf$Msg TestDdf$SubMsg TestDdf$Transform]
+  (:import [TestScript TestDdf TestDdf$Msg TestDdf$SubMsg TestDdf$Transform TestDdf$DefaultValue
+            TestDdf$OptionalNoDefaultValue TestDdf$EmptyMsg TestDdf$Uint64Msg]
            [javax.vecmath Point3d Vector3d]))
 
 (deftest simple
@@ -35,3 +36,35 @@
     (is (= 2 (.getIntValue msg)))
     (is (= "three" (.getStringValue msg)))
     (is (= m new-m))))
+
+(deftest default-vals
+  (let [defaults {:uint-value 10
+                  :string-value "test"
+                  :quat-value [0.0 0.0 0.0 1.0]
+                  :enum-value :enum-val1
+                  :bool-value true}
+        msg (protobuf/map->pb TestDdf$DefaultValue {})
+        new-m (protobuf/pb->map msg)]
+    (is (= defaults new-m))))
+
+(deftest optional-no-defaults
+  (let [defaults {:uint-value 0
+                  :string-value ""
+                  :enum-value :enum-val0}
+        msg (protobuf/map->pb TestDdf$OptionalNoDefaultValue {})
+        new-m (protobuf/pb->map msg)]
+    (is (= defaults new-m))))
+
+(deftest empty-msg
+  (let [m {}
+        msg (protobuf/map->pb TestDdf$EmptyMsg m)
+        new-m (protobuf/pb->map msg)]
+    (is (= m new-m))))
+
+(deftest uint64-msg
+  (let [m {:uint64-value 1}
+        msg (protobuf/map->pb TestDdf$Uint64Msg m)
+        new-m (protobuf/pb->map msg)]
+    (is (= m new-m))))
+
+(uint64-msg)
