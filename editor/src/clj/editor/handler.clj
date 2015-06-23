6e377f4dcfe4100ea45823ae952831a5f6d68f7b
Erik Angelin <erik.angelin@king.com>
2015-06-23 17:15:25 +0200

    Changes to handler/defhandler and related functions
    * active?-function responds whether this handler is relevant in this context (typically: selection is of correct type)
    * only commands with an active? handler are visible in menus
    * label-function for customizing menu item label

--- editor/src/clj/editor/handler.clj
+++ editor/src/clj/editor/handler.clj
@@ -15,26 +15,40 @@
                                      :context ~context
                                      :fns ~fns})))
 
-(defn- invoke [handler fn command-vars]
-  (when-let [f (get-in handler [:fns fn])]
-    (f command-vars)))
+(defn- get-fnk [handler fsym]
+  (get-in handler [:fns fsym]))
 
-(defn- do-get-enabled [command command-context]
+(defn- invoke-fnk [f command-context]
+  (f (:env command-context)))
+
+(defn- do-get-active [command command-context]
   (->> (vals @*handlers*)
     (filter #(when (and (= command (:command %)) (= (:name command-context) (:context %))) %))
-    (some (fn [h] (when (invoke h :enabled? (:env command-context)) [h command-context])))))
+    (some (fn [handler]
+            (let [f (get-fnk handler :active?)]
+              (when (or (nil? f) (invoke-fnk f command-context))
+                [handler command-context]))))))
 
-(defn- get-enabled [command command-contexts]
-  (some identity (map (fn [ctx] (do-get-enabled command ctx)) command-contexts)))
+(defn- get-active [command command-contexts]
+  (some (fn [command-context] (do-get-active command command-context)) command-contexts))
 
 (defn run [command command-contexts]
-  (when-let [[h c] (get-enabled command command-contexts)]
-    (invoke h :run (:env c))))
+  (let [[handler command-context] (get-active command command-contexts)]
+    (invoke-fnk (get-fnk handler :run) command-context)))
 
 (defn state [command command-contexts]
-  (let [[h c] (get-enabled command command-contexts)]
-    (invoke h :state (:env c))))
+  (when-let [[handler command-context] (get-active command command-contexts)]
+    (invoke-fnk (get-fnk handler :state) command-context)))
 
 (defn enabled? [command command-contexts]
-  (let [[h c] (get-enabled command command-contexts)]
-    (boolean (invoke h :enabled? (:env c)))))
+  (when-let [[handler command-context] (get-active command command-contexts)]
+    (let [f (get-fnk handler :enabled?)]
+      (boolean (or (nil? f) (invoke-fnk f command-context))))))
+
+(defn label [command command-contexts]
+  (when-let [[handler command-context] (get-active command command-contexts)]
+    (when-let [f (get-fnk handler :label)]
+      (invoke-fnk f command-context))))
+
+(defn active? [command command-contexts]
+  (boolean (get-active command command-contexts)))
