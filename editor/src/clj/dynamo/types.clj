9193b14f5b25c9bd2c87ff7bdd03387878e0a8e7
Carin Meier <pair+cm@cognitect.com>
2015-06-23 16:23:45 -0400

    move dynamo.ui to editor and more cleaning

--- editor/src/clj/dynamo/types.clj
+++
@@ -1,9 +0,0 @@
-(ns dynamo.types
-  "Schema and type definitions. Refer to Prismatic's schema.core for s/* definitions."
-  (:require [potemkin.namespaces :refer [import-vars]]
-            [schema.core :as s])
-  (:import [com.dynamo.graphics.proto Graphics$TextureImage$TextureFormat]
-           [com.dynamo.tile.proto Tile$Playback]
-           [java.awt.image BufferedImage]
-           [java.nio ByteBuffer]
-           [javax.vecmath Matrix4d Point3d Quat4d Vector3d Vector4d]))
