c68adaf8f701adb5df69c3a9e4af091209782477
Christian Murray <cgmurray@gmail.com>
2015-06-24 20:35:30 +0200

    Native library loading cleanup and support for loading libraries from $DYNAMO_HOME
    TODO
    * bundle defold libraries for all platforms by 
      first downloading all artifacts from s3
      (_sync_archive in scripts/build.py)
    * Add texc_shared to nativeLibs in Libs.java

--- editor/src/java/com/defold/libs/Libs.java
+++ editor/src/java/com/defold/libs/Libs.java
@@ -1,175 +1,81 @@
 package com.defold.libs;
 
-import java.io.Closeable;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.security.CodeSource;
-import java.security.ProtectionDomain;
-import java.util.UUID;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipException;
-import java.util.zip.ZipFile;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.defold.editor.Editor;
+import com.defold.editor.Platform;
 
 public class Libs {
-    private static String texcLibDir = null;
 
-    public static String getTexcLibDir() {
-        if (texcLibDir == null) {
-            try {
-                setTexcLibDir();
-            } catch (ZipException e) {
-                throw new RuntimeException(e);
-            } catch (URISyntaxException e) {
-                throw new RuntimeException(e);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return texcLibDir;
+    private static Logger logger = LoggerFactory.getLogger(Libs.class);
+    private static File libPath;
+    private static boolean isInitialized = false;
+
+    private static Map<com.defold.editor.Platform, String[]> nativeLibs = new HashMap<>();
+    static {
+        nativeLibs.put(Platform.X86_64Darwin, new String[] {
+                "libjogl_desktop.jnilib", "libjogl_mobile.jnilib",
+                "libnativewindow_awt.jnilib", "libnativewindow_macosx.jnilib",
+                "libnewt.jnilib", "libgluegen-rt.jnilib" });
+
+        nativeLibs.put(Platform.X86Win32, new String[] { "jogl_desktop.dll",
+                "jogl_mobile.dll", "nativewindow_awt.dll",
+                "nativewindow_win32.dll", "newt.dll", "gluegen-rt.dll" });
+
+        nativeLibs.put(Platform.X86Linux, new String[] { "libjogl_desktop.so",
+                "libjogl_mobile.so", "libnativewindow_awt.so",
+                "libnativewindow_x11.so", "libnewt.so", "libgluegen-rt.so" });
+
+        nativeLibs.put(Platform.X86_64Linux, nativeLibs.get(Platform.X86Linux));
+        nativeLibs.put(Platform.X86_64Win32, nativeLibs.get(Platform.X86Win32));
     }
 
-    private enum PlatformType {
-        Windows,
-        Darwin,
-        Linux
-    }
-
-    private static PlatformType getPlatform() {
-        String os_name = System.getProperty("os.name").toLowerCase();
-
-        if (os_name.indexOf("win") != -1) {
-            return PlatformType.Windows;
-        } else if (os_name.indexOf("mac") != -1) {
-            return PlatformType.Darwin;
-        } else if (os_name.indexOf("linux") != -1) {
-            return PlatformType.Linux;
+    private static void extractNativeLib(Platform platform, String name) throws IOException {
+        String path = String.format("/lib/%s/%s", platform.getPair(), name);
+        URL resource = logger.getClass().getResource(path);
+        if (resource != null) {
+            logger.debug("extracting lib {}", path);
+            FileUtils.copyURLToFile(resource,
+                    new File(libPath, new File(path).getName()));
         } else {
-            throw new RuntimeException(String.format("Could not identify OS: '%s'", os_name));
+            logger.warn("can't find library {}", path);
         }
     }
 
-    private static void setTexcLibDir() throws URISyntaxException, ZipException, IOException {
-        URI uri = getJarURI();
-        String libSubPath = null;
-        PlatformType platform = getPlatform();
-        switch (platform) {
-        case Windows:
-            libSubPath = "lib/win32/texc_shared.dll";
-            break;
-        case Darwin:
-            libSubPath = "lib/x86_64-darwin/libtexc_shared.dylib";
-            break;
-        case Linux:
-            libSubPath = "lib/linux/libtexc_shared.so";
-            break;
+    public static void extractNativeLibs() throws IOException {
+        if (isInitialized) {
+            return;
         }
 
-        URL url = getFile(uri, libSubPath).toURL();
-        File file = FileUtils.toFile(url);
-        if (file.exists()) {
-            texcLibDir = file.getParentFile().getAbsolutePath();
+        isInitialized = true;
+
+        if (Editor.isDev()) {
+            String dynamo_home = System.getenv("DYNAMO_HOME");
+            libPath = new File(String.format("%s/lib/%s", dynamo_home, Platform.getJavaPlatform().getPair()));
         } else {
-            throw new IOException(String.format("Could not locate '%s' (%s)", libSubPath, url));
+            libPath = Files.createTempDirectory(null).toFile();
         }
-    }
 
-    private static URI getJarURI() throws URISyntaxException {
-        final ProtectionDomain domain = Libs.class.getProtectionDomain();
-        final CodeSource source = domain.getCodeSource();
-        final URL url = source.getLocation();
-        final URI uri;
-        try {
-            uri = url.toURI();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            throw e;
-        }
-        return uri;
-    }
+        logger.debug("java.library.path={}", libPath);
 
-    private static URI getFile(final URI where, final String filePath) throws ZipException, IOException {
-        final File location;
-        final URI fileURI;
-
-        location = new File(where);
-
-        // not in a JAR, just return the path on disk
-        if(location.isDirectory()) {
-            fileURI = URI.create(where.toString() + filePath);
-        } else {
-            final ZipFile zipFile = new ZipFile(location);
-            try {
-                fileURI = extract(zipFile, filePath);
-            } finally {
-                zipFile.close();
+        if (!Editor.isDev()) {
+            Platform platform = Platform.getJavaPlatform();
+            for (String name : nativeLibs.get(platform)) {
+                extractNativeLib(platform, name);
             }
+            System.setProperty("jogamp.gluegen.UseTempJarCache", "false");
         }
-
-        return (fileURI);
+        System.setProperty("java.library.path", libPath.getAbsolutePath());
+        System.setProperty("jna.library.path", libPath.getAbsolutePath());
     }
 
-    private static String uniqueTmpDir() {
-        final int length = 8;
-        String tmp = FileUtils.getTempDirectory().getAbsolutePath();
-        String uniquePath = FilenameUtils.concat(tmp, UUID.randomUUID().toString().substring(0, length));
-        File f = new File(uniquePath);
-        while (f.exists()) {
-            uniquePath = FilenameUtils.concat(tmp, UUID.randomUUID().toString().substring(length));
-            f = new File(uniquePath);
-        }
-        f.mkdir();
-        f.deleteOnExit();
-        return uniquePath;
-    }
-
-    private static URI extract(final ZipFile zipFile, final String filePath) throws IOException {
-        String fileName = FilenameUtils.getName(filePath);
-        String dstPath = FilenameUtils.concat(uniqueTmpDir(), fileName);
-        final ZipEntry entry = zipFile.getEntry(filePath);
-
-        if(entry == null) {
-            throw new FileNotFoundException("cannot find file: " + filePath + " in archive: " + zipFile.getName());
-        }
-
-        final InputStream zipStream  = zipFile.getInputStream(entry);
-        OutputStream fileStream = null;
-
-        try {
-            final byte[] buf;
-            int i;
-
-            fileStream = new FileOutputStream(dstPath);
-            buf = new byte[1024];
-            i = 0;
-
-            while((i = zipStream.read(buf)) != -1) {
-                fileStream.write(buf, 0, i);
-            }
-        } finally {
-            close(zipStream);
-            close(fileStream);
-        }
-
-        return (new File(dstPath).toURI());
-    }
-
-    private static void close(final Closeable stream) {
-        if(stream != null) {
-            try {
-                stream.close();
-            } catch(final IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
 }
