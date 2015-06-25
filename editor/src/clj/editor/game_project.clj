519d0ad38c2a4f7c08c3c228350c8d8aa7c7784d
Ragnar Svensson <ragnar.svensson@king.com>
2015-06-25 23:29:17 +0200

    Gamepads support.

--- editor/src/clj/editor/game_project.clj
+++ editor/src/clj/editor/game_project.clj
@@ -83,14 +83,13 @@
 (defn load-game-project [project self input]
   (let [content (slurp input)
         properties (parse-properties (BufferedReader. (StringReader. content)))
-        main-collection (root-node self properties "bootstrap" "main_collection")
-        input-binding (root-node self properties "input" "game_binding")
-        render (root-node self properties "bootstrap" "render")]
+        roots (map (fn [[category field]] (root-node self properties category field))
+                   [["bootstrap" "main_collection"] ["input" "game_binding"] ["input" "gamepads"]
+                    ["bootstrap" "render"]])]
     (concat
       (g/set-property self :content content)
-      (g/connect main-collection :build-targets self :dep-build-targets)
-      (g/connect input-binding :build-targets self :dep-build-targets)
-      (g/connect render :build-targets self :dep-build-targets))))
+      (for [root roots]
+        (g/connect root :build-targets self :dep-build-targets)))))
 
 (defn register-resource-types [workspace]
   (workspace/register-resource-type workspace
