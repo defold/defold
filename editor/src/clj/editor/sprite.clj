82bc58106694d59fc5bc0566e780d2ea04533aab
Erik Angelin <erik.angelin@king.com>
2015-06-30 13:24:47 +0200

    Replace use of tags for meta-data in node properties DEFEDIT-151
    * dynamic edit-type returns a map {:type <control-dispatch-value> ...}
    * { :type :choicebox :options {val str}} will create a ChoiceBox with supplied options
    * SpriteNode - default-animation and blend-mode uses this

--- editor/src/clj/editor/sprite.clj
+++ editor/src/clj/editor/sprite.clj
@@ -241,9 +241,17 @@
   (inherits project/ResourceNode)
 
   (property image (g/maybe (g/protocol workspace/Resource)))
-  (property default-animation g/Str)
+  (property default-animation g/Str
+            (dynamic edit-type (g/fnk [anim-data] {:type :choicebox
+                                                   :options (or (and anim-data (zipmap (keys anim-data) (keys anim-data))) {})})))
+                     
   (property material (g/protocol workspace/Resource))
-  (property blend-mode g/Any (default :BLEND_MODE_ALPHA) #_(tag Sprite$SpriteDesc$BlendMode))
+  (property blend-mode g/Any (default :BLEND_MODE_ALPHA)
+            (dynamic edit-type (g/fnk []
+                                      (let [options (protobuf/enum-values Sprite$SpriteDesc$BlendMode)]
+                                        {:type :choicebox
+                                         :options (zipmap (map first options)
+                                                          (map (comp :display-name second) options))}))))
 
   (trigger reconnect :property-touched #'reconnect)
 
