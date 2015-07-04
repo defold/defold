5859ada1b46995adbb223fddb5165918f0ef5202
Michael Nygard <pair+mn@cognitect.com>
2015-07-04 12:31:41 -0500

    Minify imports for editor.text

--- editor/src/clj/editor/text.clj
+++ editor/src/clj/editor/text.clj
@@ -3,35 +3,14 @@
             [editor.core :as core]
             [editor.ui :as ui]
             [editor.workspace :as workspace])
-  (:import [com.defold.editor Start]
-           [com.jogamp.opengl.util.awt Screenshot]
-           [java.awt Desktop]
-           [javafx.application Platform]
-           [javafx.collections FXCollections ObservableList]
-           [javafx.embed.swing SwingFXUtils]
-           [javafx.event ActionEvent EventHandler]
-           [javafx.fxml FXMLLoader]
-           [javafx.geometry Insets]
-           [javafx.scene Scene Node Parent]
-           [javafx.scene.control Button ColorPicker Label TextField TitledPane TextArea TreeItem TreeCell Menu MenuItem MenuBar Tab ProgressBar]
-           [javafx.scene.image Image ImageView WritableImage PixelWriter]
-           [javafx.scene.input MouseEvent]
-           [javafx.scene.layout AnchorPane GridPane StackPane HBox Priority Pane]
-           [javafx.scene.paint Color]
-           [javafx.stage Stage FileChooser]
-           [javafx.util Callback]
-           [java.io File]
-           [java.nio.file Paths]
-           [java.util.prefs Preferences]
-           [javax.media.opengl GL GL2 GLContext GLProfile GLDrawableFactory GLCapabilities]))
+  (:import [javafx.scene Parent]
+           [javafx.scene.layout Pane]
+           [javafx.scene.control TextArea]))
 
 (g/defnode TextView
   (inherits core/ResourceNode)
 
-  (property text-area TextArea)
-
-  )
-
+  (property text-area TextArea))
 
 (defn make-view [graph ^Parent parent resource-node opts]
   (let [text-area (TextArea.)]
