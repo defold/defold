8ae977a1ed8ef30ef50c379e82d6e5bb528a2b29
Dan Nilsson <dan.nilsson@king.com>
2015-07-06 17:26:21 +0200

    DEF-1220 - [QRT] Functionality: App crashes when performing a lock button interrupt
    - Move get ad id to its own thread in the startup flow

--- engine/glfw/java/com/dynamo/android/DefoldActivity.java
+++ engine/glfw/java/com/dynamo/android/DefoldActivity.java
@@ -10,6 +10,8 @@ import android.os.IBinder;
 import android.view.KeyEvent;
 import android.view.inputmethod.InputMethodManager;
 import android.util.Log;
+import com.google.android.gms.ads.identifier.AdvertisingIdClient;
+import com.google.android.gms.common.*;
 
 public class DefoldActivity extends NativeActivity {
 
@@ -41,6 +43,65 @@ public class DefoldActivity extends NativeActivity {
             }
         } catch (PackageManager.NameNotFoundException e) {
             throw new RuntimeException("Error getting activity info", e);
+        }
+
+        final DefoldActivity self = this;
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    AdvertisingIdClient.Info info = AdvertisingIdClient.getAdvertisingIdInfo(self);
+                    self.setAdInfo(info.getId(), info.isLimitAdTrackingEnabled());
+                } catch (java.io.IOException e) {
+                    self.setAdInfo("", false);
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    self.setAdInfo("", false);
+                } catch (GooglePlayServicesRepairableException e) {
+                    self.setAdInfo("", false);
+                }
+            }
+        }).start();
+    }
+
+    private Object m_AdGuard = new Object();
+    private boolean m_GotAdInfo = false;
+    private String m_AdId;
+    private boolean m_LimitAdTracking;
+
+
+    public void setAdInfo(String adId, boolean limitAdTracking)
+    {
+        synchronized(m_AdGuard)
+        {
+            m_AdId = adId;
+            m_LimitAdTracking = limitAdTracking;
+            m_GotAdInfo = true;
+        }
+    }
+
+    /**
+     * Return null for does not exist yet, blank string if completed but unable to get id.
+     */
+    public String getAdId()
+    {
+        synchronized(m_AdGuard)
+        {
+            return m_AdId;
+        }
+    }
+
+    public boolean getLimitAdTracking()
+    {
+        synchronized(m_AdGuard)
+        {
+            return m_LimitAdTracking;
+        }
+    }
+
+    public boolean isStartupDone()
+    {
+        synchronized(m_AdGuard)
+        {
+            return m_GotAdInfo;
         }
     }
 
