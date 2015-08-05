f10b23e7b08bdc5789977034f6dc99e089b735e9
Sven Andersson <sven.andersson@king.com>
2015-08-05 17:24:38 +0200

    Aligned show_dialog params with JS field names.

--- engine/facebook/src/facebook.h
+++ engine/facebook/src/facebook.h
@@ -38,48 +38,50 @@ namespace dmFacebook {
     Notes on facebook.show_dialog in regards to FB SDK 4.
 
     Dialog parameters have been updated to align with field names in the
-    current SDK version. Names have been chosen with a preference of iOS
-    field names (imageURL vs imageUrl for example).
+    current SDK version. Names have been chosen with a preference of JS
+    field names (object_id vs objectID for example).
 
     Some fields that have changed names in the SDK still exist, but are
-    considered deprecated (to -> recipients).
+    considered deprecated (title -> caption).
+
+    Results are forwarded as-is from each platform.
 
     facebook.show_dialog( dialog_type, param_table, callback_func ):
 
-    dialog_type == "apprequests" or "gamerequest":
-                           arg     type                    iOS / Android
-        -                title : string [                title / setTitle       ]
-        -              message : string [              message / setMessage     ]
-        -           actionType :    int [           actionType / setActionType  ]
-        -              filters :    int [              filters / setFilters     ]
-        -                 data : string [                 data / setData        ]
-        -             objectID : string [             objectID / setObjectId    ]
-        - recipientSuggestions :  array [ recipientSuggestions / setSuggestions ]
-        -           recipients :  array [           recipients / setTo          ]
+    dialog_type == "apprequests":
+        Details for each parameter: https://developers.facebook.com/docs/games/requests/v2.4#params
+
+                           arg     type            JS                   iOS                Android
+        -                title : string [       title,                title,              setTitle ]
+        -              message : string [     message,              message,            setMessage ]
+        -          action_type :    int [ action_type,           actionType,         setActionType ]
+        -              filters :    int [     filters,              filters,            setFilters ]
+        -                 data : string [        data,                 data,               setData ]
+        -            object_id : string [   object_id,             objectID,           setObjectId ]
+        -          suggestions :  array [ suggestions, recipientSuggestions,        setSuggestions ]
+        -                   to : string [          to,           recipients,                 setTo ]
+
+    dialog_type == "feed":
+        Details for each parameter: https://developers.facebook.com/docs/sharing/reference/feed-dialog/v2.4#params
+
+                           arg     type            JS                   iOS                Android
+        -              caption : string [     caption,         contentTitle,       setContentTitle ]
+        -          description : string [ description,   contentDescription, setContentDescription ]
+        -              picture : string [     picture,             imageURL,           setImageUrl ]
+        -                 link : string [        link,           contentURL,         setContentUrl ]
+        -           people_ids :  array [           -,            peopleIDs,          setPeopleIds ]
+        -             place_id : string [           -,              placeID,            setPlaceId ]
+        -                  ref : string [         ref,                  ref,                setRef ]
 
         Deprecated fields:
-        -          suggestions : string, use recipientSuggestions instead
-        -                   to : string, use recipients instead
+        -                title : string, use caption instead
 
-    dialog_type == "feed" or "link":
-                           arg     type                  iOS / Android
-        -         contentTitle : string [       contentTitle / setContentTitle       ]
-        -   contentDescription : string [ contentDescription / setContentDescription ]
-        -             imageURL : string [           imageURL / setImageUrl           ]
-        -           contentURL : string [         contentURL / setContentUrl         ]
-        -            peopleIDs :  array [          peopleIDs / setPeopleIds          ]
-        -              placeID : string [            placeID / setPlaceId            ]
-        -                  ref : string [                ref / setRef                ]
+    dialog_type == "appinvite": (Only available under iOS and Android)
+        Details for each parameter: https://developers.facebook.com/docs/reference/ios/current/class/FBSDKAppInviteContent/
 
-        Deprecated fields:
-        -                title : string, use contentTitle instead
-        -          description : string, use contentDescription instead
-        -                 link : string, use contentURL instead
-
-    dialog_type == "appinvite":
-                               arg     type                        iOS / Android
-        -               appLinkURL : string [               appLinkURL / setApplinkUrl      ]
-        - appInvitePreviewImageURL : string [ appInvitePreviewImageURL / setPreviewImageUrl ]
+                           arg     type                        iOS / Android
+        -                  url : string [               appLinkURL / setApplinkUrl      ]
+        -    preview_image_url : string [ appInvitePreviewImageURL / setPreviewImageUrl ]
 
 */
 
