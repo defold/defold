bab5bfb4393bc8500c7f63cbe46005171ce07fc4
Erik Angelin <erik.angelin@king.com>
2015-08-24 11:52:11 +0200

    cruft

--- editor/src/clj/editor/form.clj
+++ editor/src/clj/editor/form.clj
@@ -1,48 +1,6 @@
 (ns editor.form
   (:require [dynamo.graph :as g]))
 
-;;; sample form data
-#_{
- :ctxt [node-id [property & path-within-property]]
- :sections
- [
-  {
-   :section "project"
-   :help "..."
-   :title "..."
-   :fields
-   [
-    {
-     :field "name"
-     :help "..."
-     :type String
-     :default "<my name>"
-     }
-    ]
-   }
-  {
-   :section "sound"
-   :help "..."
-   :title "..."
-   :fields
-   [
-    {
-     :field "f"
-     :help "freq"
-     :type g/Int
-     :default 22050
-     }
-    ]
-   }
-  ]
- :values
- {
-  "project" {"name" "my fps"}
-  "sound" {"f" 44100}
-  }
-}
-
-
 (defn set-value! [{:keys [user-data set]} path value]
   (set user-data path value))
 
