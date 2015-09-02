65c56de770c622180fca2110846ee7e39fe3d321
Michael Nygard <pair+mn@cognitect.com>
2015-09-02 11:21:13 -0500

    Eliminate tricky reflection warnings
    [Finishes #102454486]

---
+++ editor/src/java/com/defold/control/TreeCell.java
@@ -0,0 +1,7 @@
+package com.defold.control;
+
+public class TreeCell extends javafx.scene.control.TreeCell<Object> {
+    public void updateItem(Object o, boolean empty) {
+        super.updateItem(o, empty);
+    }
+}
