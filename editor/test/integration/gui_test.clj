2ed722d44ba594faa9f61a69cc81733dfc393251
Ragnar Svensson <ragnar.svensson@king.com>
2015-09-18 16:50:26 +0200

    Box rendering

--- editor/test/integration/gui_test.clj
+++ editor/test/integration/gui_test.clj
@@ -9,9 +9,37 @@
            [java.nio.file Files attribute.FileAttribute]
            [org.apache.commons.io FilenameUtils FileUtils]))
 
+(defn- prop [node-id label]
+  (get-in (g/node-value node-id :_properties) [:properties label :value]))
+
+(defn- prop! [node-id label val]
+  (g/transact (g/set-property node-id label val)))
+
 (deftest load-gui
   (with-clean-system
     (let [workspace (test-util/setup-workspace! world)
           project   (test-util/setup-project! workspace)
           node-id   (test-util/resource-node project "/logic/main.gui")
           gui-node (ffirst (g/sources-of node-id :node-outlines))])))
+
+(deftest gui-scene-generation
+ (with-clean-system
+   (let [workspace (test-util/setup-workspace! world)
+         project   (test-util/setup-project! workspace)
+         node-id   (test-util/resource-node project "/logic/main.gui")]
+     #_(prn "scene" (g/node-value node-id :scene)))))
+
+(deftest gui-textures
+  (with-clean-system
+   (let [workspace (test-util/setup-workspace! world)
+         project   (test-util/setup-project! workspace)
+         node-id   (test-util/resource-node project "/logic/main.gui")
+         outline (g/node-value node-id :outline)
+         png-node (get-in outline [:children 0 :children 1 :node-id])
+         png-tex (get-in outline [:children 1 :children 0 :node-id])]
+     (is (some? png-tex))
+     (is (= "png_texture" (prop png-node :texture)))
+     (prop! png-tex :name "new-name")
+     (is (= "new-name" (prop png-node :texture))))))
+
+(gui-textures)
