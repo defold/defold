6098c71824fffa5082ef0ff8dbfc203bf6599649
Ragnar Svensson <ragnar.svensson@king.com>
2015-10-16 18:11:34 +0200

    DEFEDIT-162 Font editor using the scene view
    * Rendering of all font types
    * Property visibility dep. on type
    
    Squashed commit of the following:
    
    commit 2a671982a7aa619f280e006d1805149e1d82a869
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Fri Oct 16 17:29:30 2015 +0200
    
        Font rendering using custom materials.

---
+++ editor/test/integration/font_test.clj
@@ -0,0 +1,24 @@
+(ns integration.font-test
+  (:require [clojure.test :refer :all]
+            [dynamo.graph :as g]
+            [support.test-support :refer [with-clean-system]]
+            [integration.test-util :as test-util]
+            [editor.workspace :as workspace]
+            [editor.project :as project])
+  (:import [java.io File]
+           [java.nio.file Files attribute.FileAttribute]
+           [org.apache.commons.io FilenameUtils FileUtils]))
+
+(defn- prop [node-id label]
+  (get-in (g/node-value node-id :_properties) [:properties label :value]))
+
+(defn- prop! [node-id label val]
+  (g/transact (g/set-property node-id label val)))
+
+(deftest load-material-render-data
+  (with-clean-system
+    (let [workspace (test-util/setup-workspace! world)
+          project   (test-util/setup-project! workspace)
+          node-id   (test-util/resource-node project "/fonts/score.font")
+          scene (g/node-value node-id :scene)]
+      (is (not (nil? scene))))))
