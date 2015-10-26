bd0eb80b270a8aa16dbdfcb552ad14a0935a343e
Mathias Westerdahl <mathias.westerdahl@king.com>
2015-10-26 17:13:18 +0100

    DEF-1330 Better socket error code handling

--- engine/script/src/test/test_http_timeout.lua
+++ engine/script/src/test/test_http_timeout.lua
@@ -1,7 +1,7 @@
 function callback(response)
 end
 
-requests_left = 1
+requests_left = 2
 
 -- NOTE: We test timeout in a separate file as the test-server is single threaded
 -- and could potentially timeout other requests
@@ -16,6 +16,15 @@ function test_http_timeout()
             requests_left = requests_left - 1
         end,
     headers, '', options)
+
+    -- The config file also specifies a timeout value, let's test that too
+    http.request("http://localhost:" .. PORT .. "/sleep", "GET",
+        function(response)
+            assert(response.status == 0)
+            requests_left = requests_left - 1
+        end,
+    headers)
+
 end
 
 functions = { test_http_timeout = test_http_timeout }
