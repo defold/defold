b1c26311bf9fb3282097f2ba0762a2bc51422123
Ragnar Svensson <ragnar.svensson@king.com>
2015-10-26 20:40:37 +0100

    DEFEDIT-180 Gui text node support.
    Squashed commit of the following:
    
    commit 3aecc46a6b9bde728b9aaa133861fa6dbc361272
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Mon Oct 26 20:33:01 2015 +0100
    
        Gui fonts are properly added.
    
    commit 49ff07b9b90c5f474918123b95e468a9251f1a57
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Mon Oct 26 20:23:47 2015 +0100
    
        Correct blending of gui nodes.
    
    commit a5e658dc51bde45b4083372941647b14e72d7769
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Mon Oct 26 11:53:08 2015 +0100
    
        Fixed issue with protobuf bools and clojures way of dealing with java.lang.Boolean
    
    commit 13fd337d219bcecb278f2ae4ed23a14251e2cd33
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Mon Oct 26 09:25:37 2015 +0100
    
        Fixing broken font editor
    
    commit a2fbfe000d506fd6a019e6f6601d69bcca3fcf07
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Mon Oct 26 08:57:43 2015 +0100
    
        Gui text node rendering.
    
        Some remaining issues left before this is complete.
    
    commit 428fe3e3e89b5cf41bc8640d5e7ca52319238a5e
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Thu Oct 22 10:12:39 2015 +0200
    
        Font rendering WIP
    
    commit 828cf1f66ccc61703815a07e5aa9b5e85df87c8f
    Merge: b58f613 d1fb5ab
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Wed Oct 21 15:54:27 2015 +0200
    
        Merge branch 'ed2-become-reload-fix' into ed2-gui
    
    commit 4a6bd3a994147a3f06e53570a0fc6ebc8b5313b3
    Merge: b292d52 4665f98
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Wed Oct 21 10:21:23 2015 +0200
    
        Merge branch 'dev' into ed2-gui
    
    commit 34a1664ff07e16d77c99c57887965d9f0b03ceab
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Wed Oct 21 09:25:09 2015 +0200
    
        DEFEDIT-239 Gui and collection namespaces can now be reloaded.
    
        * Did not manage to find the root cause, this only removed the symptom.
    
    commit 47b6fa537284a26311291df8e70c7e800f084031
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Mon Oct 19 16:12:43 2015 +0200
    
        Text nodes WIP
    
    commit e0dccc18bcedcea665e28e8dedc741abc8ff7b60
    Merge: 9ac2dc3 212067b
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Mon Oct 19 16:11:48 2015 +0200
    
        Merge branch 'dev' into ed2-gui
    
    commit f8633ec106659ee5261862ae4f3bc6b18c9117ed
    Merge: 32d1117 64472b1
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Sat Oct 17 11:57:18 2015 +0200
    
        Merge branch 'ed2-font-pr' into ed2-font
    
    commit 2a671982a7aa619f280e006d1805149e1d82a869
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Fri Oct 16 17:29:30 2015 +0200
    
        Font rendering using custom materials.

--- editor/src/clj/editor/pipeline/font_gen.clj
+++ editor/src/clj/editor/pipeline/font_gen.clj
@@ -2,7 +2,8 @@
   (:require [clojure.java.io :as io]
             [editor.protobuf :as protobuf])
   (:import [com.defold.editor.pipeline Fontc Fontc$FontResourceResolver]
-           [com.dynamo.render.proto Font$FontDesc Font$FontMap]))
+           [com.dynamo.render.proto Font$FontDesc Font$FontMap]
+           [java.awt.image BufferedImage]))
 
 (defn generate [font-desc font-path resolver]
   (let [^Font$FontDesc font-desc (protobuf/map->pb Font$FontDesc font-desc)
@@ -11,7 +12,9 @@
                             (getResource [this resource-name]
                               (io/input-stream (resolver (str "/" resource-name)))))]
     (with-open [font-stream (io/input-stream font-path)]
-      (let [image (-> (Fontc.)
-                    (.compile font-stream font-desc font-map-builder font-res-resolver))]
+      (let [^BufferedImage image (-> (Fontc.)
+                                   (.compile font-stream font-desc font-map-builder font-res-resolver))]
         {:image image
-         :font-map (protobuf/pb->map (.build font-map-builder))}))))
+         :font-map (-> (protobuf/pb->map (.build font-map-builder))
+                     (assoc :width (.getWidth image)
+                            :height (.getHeight image)))}))))
