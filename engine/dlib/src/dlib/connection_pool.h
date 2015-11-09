282f608b8924e2325c363ed65bf5840b544b826a
Mathias Westerdahl <mathias.westerdahl@king.com>
2015-11-09 09:27:47 +0100

    DEF-1470 Fixed SSL handshake issue for http requests

--- engine/dlib/src/dlib/connection_pool.h
+++ engine/dlib/src/dlib/connection_pool.h
@@ -94,11 +94,12 @@ namespace dmConnectionPool
      * @param host host
      * @param port port
      * @param ssl true for ssl connection
+     * @param ssl_handshake_timeout The timeout (micro seconds) for the ssl handshake. Only used if ssl is true.
      * @param connection connection (out)
      * @param sock_res socket-result code on failure
      * @return RESULT_OK on success
      */
-    Result Dial(HPool pool, const char* host, uint16_t port, bool ssl, HConnection* connection, dmSocket::Result* sock_res);
+    Result Dial(HPool pool, const char* host, uint16_t port, bool ssl, int ssl_handshake_timeout, HConnection* connection, dmSocket::Result* sock_res);
 
     /**
      * Return connection to pool
