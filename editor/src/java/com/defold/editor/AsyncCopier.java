f4cc33457c5774da9946c07460f6cf888a7462f4
Christian Murray <christian.murray@king.com>
2015-11-11 16:38:15 +0100

    Improved profiler etc

--- editor/src/java/com/defold/editor/AsyncCopier.java
+++ editor/src/java/com/defold/editor/AsyncCopier.java
@@ -1,22 +1,22 @@
 package com.defold.editor;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javax.media.opengl.GL2;
 
+import com.defold.editor.Profiler.Sample;
+
 import javafx.application.Platform;
 import javafx.concurrent.Task;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelFormat;
 import javafx.scene.image.WritableImage;
@@ -24,20 +24,6 @@ import javafx.scene.image.WritableImage;
 public class AsyncCopier {
 
     private static final int N_BUFFERS = 2;
-
-    private static class Sample {
-        String name;
-        long start;
-        long end;
-        int index;
-
-        public Sample(String name, long start, int index) {
-            this.name = name;
-            this.start = start;
-            this.index = index;
-        }
-    }
-
     private List<Buffer> buffers = new ArrayList<>();
     private int pboIndex = 0;
     private int width;
@@ -45,13 +31,13 @@ public class AsyncCopier {
     private ExecutorService threadPool;
     private ImageView imageView;
 
-    private ArrayList<Sample> samples = new ArrayList<>();
+    private BlockingQueue<WritableImage> freeImages = new ArrayBlockingQueue<>(N_BUFFERS);
     private Task<Void> task;
     private Sample frame;
 
     private static class Buffer {
         int pbo;
-        WritableImage image;
+        //WritableImage image;
 
         Buffer(int pbo) {
             this.pbo = pbo;
@@ -62,7 +48,7 @@ public class AsyncCopier {
             gl.glBindBuffer(GL2.GL_PIXEL_PACK_BUFFER, pbo);
             gl.glBufferData(GL2.GL_PIXEL_PACK_BUFFER, data_size, null, GL2.GL_STREAM_READ);
             gl.glBindBuffer(GL2.GL_PIXEL_PACK_BUFFER, 0);
-            image = new WritableImage(width, height);
+          //  image = new WritableImage(width, height);
         }
     }
 
@@ -77,60 +63,25 @@ public class AsyncCopier {
         for (int i = 0; i < N_BUFFERS; ++i) {
             Buffer b = new Buffer(pbos[i]);
             buffers.add(b);
+            freeImages.add(new WritableImage(width, height));
         }
         setSize(gl, width, height);
     }
 
-    Sample begin(String name, int index) {
-        return new Sample(name, System.currentTimeMillis(), index);
-    }
-
-    synchronized void end(Sample sample) {
-        sample.end = System.currentTimeMillis();
-        samples.add(sample);
-    }
-
-    public synchronized void dumpSamples(String filename, int removeFirst) throws IOException {
-        Collections.sort(samples, new Comparator<Sample>() {
-
-            @Override
-            public int compare(Sample o1, Sample o2) {
-                return Long.compare(o1.start, o2.start);
-            }
-
-        });
-
-        for (int i = 0; i < removeFirst; i++) {
-            samples.remove(0);
-        }
-
-        try (PrintStream out = new PrintStream(new FileOutputStream(filename))) {
-            out.println("start,end,type,index");
-            long min = Long.MAX_VALUE;
-            for (Sample s : samples) {
-                min = Math.min(min, s.start);
-            }
-            for (Sample s : samples) {
-                out.format("%d,%d,%s,%d,%n", s.start - min, s.end - min, s.name, s.index);
-            }
-        } catch (FileNotFoundException e) {
-            throw new IOException(e);
-        }
-    }
-
     public void beginFrame(GL2 gl)  {
-
-        Sample begin = begin("begin", -1);
+        Profiler.beginFrame();
+        Sample begin = Profiler.begin("begin", -1);
         if (frame != null) {
-            end(frame);
+            Profiler.add(frame);
         }
-        frame = begin("frame", -1);
+        frame = Profiler.begin("frame", -1);
 
         if (task != null) {
             try {
-                Sample wait = begin("wait", -1);
+                Sample wait = Profiler.begin("wait", -1);
                 task.get();
-                end(wait);
+                task = null;
+                Profiler.add(wait);
             } catch (ExecutionException e) {
                 throw new RuntimeException(e);
             } catch (InterruptedException e) {
@@ -140,58 +91,116 @@ public class AsyncCopier {
 
         gl.getContext().makeCurrent();
 
-        end(begin);
+        Profiler.add(begin);
     }
 
     public void endFrame(GL2 gl) {
-
-        Sample end = begin("end", -1);
+        Sample end = Profiler.begin("end", -1);
 
         pboIndex = (pboIndex + 1) % N_BUFFERS;
         Buffer readTo = buffers.get(pboIndex);
         gl.glBindBuffer(GL2.GL_PIXEL_PACK_BUFFER, readTo.pbo);
         int type = GL2.GL_UNSIGNED_INT_8_8_8_8_REV;
-        Sample readPixels = begin("readPixels", readTo.pbo);
+        Sample readPixels = Profiler.begin("readPixels", readTo.pbo);
         gl.glReadPixels(0, 0, width, height, GL2.GL_BGRA, type, 0);
-        end(readPixels);
+        Profiler.add(readPixels);
 
         gl.getContext().release();
+
+        if (task != null) {
+            throw new RuntimeException("task in flight!");
+        }
 
         task = new Task<Void>() {
 
             @Override
             protected Void call() throws Exception {
                 gl.getContext().makeCurrent();
-                gl.glBindBuffer(GL2.GL_PIXEL_PACK_BUFFER, readTo.pbo);
-                Sample mapBuffer = begin("mapBuffer", readTo.pbo);
-                ByteBuffer buffer = gl.glMapBuffer(GL2.GL_PIXEL_PACK_BUFFER, GL2.GL_READ_ONLY);
-                end(mapBuffer);
 
-                PixelFormat<IntBuffer> pf = PixelFormat.getIntArgbPreInstance();
+                try {
+                    gl.glBindBuffer(GL2.GL_PIXEL_PACK_BUFFER, readTo.pbo);
+                    Sample mapBuffer = Profiler.begin("mapBuffer", readTo.pbo);
+                    ByteBuffer buffer = gl.glMapBuffer(GL2.GL_PIXEL_PACK_BUFFER, GL2.GL_READ_ONLY);
+                    Profiler.add(mapBuffer);
 
-                Sample setPixels = begin("setPixels", readTo.pbo);
-                readTo.image.getPixelWriter().setPixels(0, 0, width, height, pf, buffer.asIntBuffer(), width);
-                end(setPixels);
+                    PixelFormat<IntBuffer> pf = PixelFormat.getIntArgbPreInstance();
 
-                gl.glBindBuffer(GL2.GL_PIXEL_PACK_BUFFER, readTo.pbo);
-                gl.glUnmapBuffer(GL2.GL_PIXEL_PACK_BUFFER);
-                gl.glBindBuffer(GL2.GL_PIXEL_PACK_BUFFER, 0);
-                gl.getContext().release();
-
-                Platform.runLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        Sample setImage = begin("setImage", readTo.pbo);
-                        imageView.setImage(readTo.image);
-                        end(setImage);
+                    /*
+                    if (readTo.image == imageView.getImage()) {
+                        System.out.println("Image in use...");
+                        gl.getContext().release();
+                        return null;
                     }
-                });
+
+                    if (buffer == null) {
+                        System.out.println("Buffer is null...");
+                        gl.getContext().release();
+                        return null;
+                    }
+    */
+
+                    Sample setPixels = Profiler.begin("setPixels", readTo.pbo);
+                    //readTo.image = new WritableImage(width, height);
+
+                    WritableImage tmpImage = freeImages.poll(1, TimeUnit.MILLISECONDS);
+                    if (tmpImage != null) {
+                        if (tmpImage.getWidth() != width || tmpImage.getHeight() != height) {
+                            tmpImage = new WritableImage(width, height);
+                        }
+                        tmpImage.getPixelWriter().setPixels(0, 0, width, height, pf, buffer.asIntBuffer(), width);
+                    } else {
+                        System.out.println("no image available");
+                    }
+
+                    WritableImage image = tmpImage;
+
+                    /*
+                    synchronized (AsyncCopier.this) {
+                        if (readTo.image != imageView.getImage()) {
+                            readTo.image.getPixelWriter().setPixels(0, 0, width, height, pf, buffer.asIntBuffer(), width);
+                        } else {
+                            System.out.println("image in use");
+                        }
+                    }*/
+                    Profiler.add(setPixels);
+
+                    gl.glBindBuffer(GL2.GL_PIXEL_PACK_BUFFER, readTo.pbo);
+                    gl.glUnmapBuffer(GL2.GL_PIXEL_PACK_BUFFER);
+                    gl.glBindBuffer(GL2.GL_PIXEL_PACK_BUFFER, 0);
+
+                    if (image != null) {
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                Sample setImage = Profiler.begin("setImage", readTo.pbo);
+                                //readTo.image.set
+                                Image oldImage = imageView.getImage();
+                                if (oldImage != null) {
+                                    try {
+                                        freeImages.put((WritableImage) oldImage);
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                imageView.setImage(image);
+
+                                /*synchronized (AsyncCopier.this) {
+                                    imageView.setImage(readTo.image);
+                                }*/
+                                Profiler.add(setImage);
+                            }
+                        });
+                    }
+
+                } finally {
+                    gl.getContext().release();
+                }
 
                 return null;
             }
         };
         threadPool.submit(task);
-        end(end);
+        Profiler.add(end);
     }
 
     public void setSize(GL2 gl, int width, int height) {
