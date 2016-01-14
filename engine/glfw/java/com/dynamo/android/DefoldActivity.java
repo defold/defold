987a48a7817f2f19e733179581c64c67c9c58cfe
Sven Andersson <sven.andersson@king.com>
2016-01-14 15:05:24 +0100

    DEF-1461 - Added new input handling for Android.

--- engine/glfw/java/com/dynamo/android/DefoldActivity.java
+++ engine/glfw/java/com/dynamo/android/DefoldActivity.java
@@ -11,6 +11,24 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.view.KeyEvent;
 import android.view.inputmethod.InputMethodManager;
+
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.EditText;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.PopupWindow;
+import android.text.TextWatcher;
+import android.text.Editable;
+import android.widget.FrameLayout;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnectionWrapper;
+import android.view.inputmethod.InputConnection;
+import android.text.InputType;
+import android.view.inputmethod.CompletionInfo;
+import java.lang.CharSequence;
+
 import android.util.Log;
 import com.google.android.gms.ads.identifier.AdvertisingIdClient;
 import com.google.android.gms.common.*;
@@ -28,6 +46,8 @@ public class DefoldActivity extends NativeActivity {
     }
 
     private InputMethodManager imm = null;
+    private TextWatcher mTextWatcher = null;
+    private EditText mTextEdit = null;
 
     private static final String TAG = "DefoldActivity";
 
@@ -36,11 +56,114 @@ public class DefoldActivity extends NativeActivity {
      * not copied over to the corresponding native AInputEvent.
      * Therefore it is implemented in android_window.c so that the characters can be sent to glfw.
      */
+    public native void FakeBackspace();
     public native void glfwInputCharNative(int unicode);
+    public native void glfwSetMarkedTextNative(String text);
+
+    private class DefoldInputWrapper extends InputConnectionWrapper {
+        private DefoldActivity _ctx;
+        private String m_ComposingText = "";
+
+        // String markedText;
+        public DefoldInputWrapper(DefoldActivity ctx, InputConnection target, boolean mutable) {
+            super(target, mutable);
+            _ctx = ctx;
+            m_ComposingText = "";
+        }
+
+        @Override
+        public CharSequence getTextBeforeCursor(int n, int flags)
+        {
+            // Hacky...
+            return " ";
+        }
+
+        @Override
+        public boolean setComposingText(CharSequence text, int newCursorPosition)
+        {
+            m_ComposingText = text.toString();
+            Log.d(TAG, "setComposingText '" + m_ComposingText.toString() + "'");
+            _ctx.sendMarkedText(m_ComposingText.toString());
+
+            return super.setComposingText(text, newCursorPosition);
+        }
+
+        @Override
+        public boolean commitCompletion (CompletionInfo text)
+        {
+            Log.d(TAG, "commitCompletion: " + text.toString());
+            return super.commitCompletion(text);
+        }
+
+        @Override
+        public boolean finishComposingText ()
+        {
+            Log.d(TAG, "finishComposingText: '" + m_ComposingText + "'");
+            _ctx.sendInputText(new String(m_ComposingText));
+            _ctx.sendMarkedText("");
+
+            // If we are finished composing, clear the composing text
+            if (m_ComposingText.length() > 0) {
+                this.setComposingText("", 0);
+            }
+            m_ComposingText = "";
+
+            return super.finishComposingText();
+        }
+
+        @Override
+        public boolean commitText(CharSequence text, int newCursorPosition)
+        {
+            Log.d(TAG, "commitText '" + text.toString() + "'");
+            _ctx.sendInputText(text.toString());
+
+            // When committing new text, we assume the composing text should be cleared.
+            m_ComposingText = "";
+            _ctx.clearComposingText();
+
+            return super.commitText(text, newCursorPosition);
+        }
+
+        @Override
+        public boolean sendKeyEvent(KeyEvent event) {
+            return super.sendKeyEvent(event);
+        }
+
+
+        @Override
+        public boolean deleteSurroundingText(int beforeLength, int afterLength) {
+            if (beforeLength == 1 && afterLength == 0) {
+                Log.d(TAG, "DELETE!");
+
+                _ctx.clearText();
+                _ctx.clearComposingText();
+                _ctx.FakeBackspace();
+            }
+
+            return super.deleteSurroundingText(beforeLength, afterLength);
+        }
+
+        }
+
+    public synchronized void sendInputText(final String text) {
+        int charCount = text.length();
+        for (int i = 0; i < charCount; ++i) {
+            int c = text.codePointAt(i);
+            glfwInputCharNative(c);
+        }
+
+        sendMarkedText("");
+    }
+
+    public synchronized void sendMarkedText(final String text) {
+        glfwSetMarkedTextNative(text);
+    }
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final DefoldActivity self = this;
 
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         imm = (InputMethodManager)DefoldActivity.this.getSystemService(Context.INPUT_METHOD_SERVICE);
 
         try {
@@ -57,7 +180,25 @@ public class DefoldActivity extends NativeActivity {
             throw new RuntimeException("Error getting activity info", e);
         }
 
-        final DefoldActivity self = this;
+        // TODO: Remove text watcher before commit, not used...
+        mTextWatcher = new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                System.out.println("SVEN: time to send: '" + s.toString() + "'");
+                System.out.println("SVEN: changed: '" + s.toString().substring(start, start + count) + "'");
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                System.out.println("SVEN: beforeTextChanged");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                System.out.println("SVEN: afterTextChanged");
+            }
+        };
+
         new Thread(new Runnable() {
             public void run() {
                 try {
@@ -139,6 +280,7 @@ public class DefoldActivity extends NativeActivity {
      */
     @Override
     public boolean dispatchKeyEvent(KeyEvent event) {
+        System.out.println("SVEN: dispatchKeyEvent triggered.");
         if (event.getAction() == KeyEvent.ACTION_MULTIPLE && event.getKeyCode() == KeyEvent.KEYCODE_UNKNOWN) {
             String chars = event.getCharacters();
             int charCount = chars.length();
@@ -154,10 +296,98 @@ public class DefoldActivity extends NativeActivity {
      * Implemented here to ensure that calls from native code are delayed and run on the UI thread.
      */
     public void showSoftInput() {
+        final DefoldActivity self = this;
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                imm.showSoftInput(getWindow().getDecorView(), InputMethodManager.SHOW_FORCED);
+
+                if (mTextEdit != null)
+                    mTextEdit.setVisibility(View.GONE);
+                mTextEdit = new EditText(self) {
+                    @Override
+                    public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
+
+                        // We set manual input connection to catch all keyboard interaction
+                        InputConnection inpc = super.onCreateInputConnection(outAttrs);
+                        outAttrs.inputType = InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS;
+                        return new DefoldInputWrapper(self, inpc, true);
+
+                    }
+
+                    @Override
+                    public boolean onCheckIsTextEditor() {
+                        return true;
+                    }
+                };
+
+
+                // Disable the fullscreen keyboard mode (present in landscape)
+                // If we don't do this, we get a large grey inpt box above the keyboard
+                // blocking the game view.
+                mTextEdit.setImeOptions(EditorInfo.IME_FLAG_NO_EXTRACT_UI);
+
+                FrameLayout.LayoutParams mEditTextLayoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+                mEditTextLayoutParams.gravity = Gravity.TOP;
+                mEditTextLayoutParams.setMargins(0, 0, 0, 0);
+                mTextEdit.setLayoutParams(mEditTextLayoutParams);
+                mTextEdit.setVisibility(View.VISIBLE);
+                mTextEdit.addTextChangedListener(self.mTextWatcher); // TODO remove this?
+
+                // mTextEdit.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS & (~InputType.TYPE_TEXT_FLAG_AUTO_CORRECT));
+                // mTextEdit.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_FILTER | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS & (~InputType.TYPE_TEXT_FLAG_AUTO_CORRECT));
+                mTextEdit.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+                // mTextEdit.setRawInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+                // self.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+
+                // Start on an empty slate each time we bring up the keyboard
+                mTextEdit.setText("");
+
+                getWindow().addContentView(mTextEdit, mEditTextLayoutParams);
+
+                mTextEdit.bringToFront();
+                mTextEdit.setSelection(0);
+                mTextEdit.requestFocus();
+
+                imm.showSoftInput(mTextEdit, InputMethodManager.SHOW_FORCED);
+            }
+        });
+    }
+
+    public void clearText() {
+        final DefoldActivity self = this;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mTextEdit != null) {
+                    mTextEdit.setText("");
+                }
+            }
+        });
+    }
+
+    public void clearComposingText() {
+        final DefoldActivity self = this;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mTextEdit != null) {
+                    mTextEdit.clearComposingText();
+                }
+            }
+        });
+    }
+
+    public void resetSoftInput() {
+        final DefoldActivity self = this;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "resetSoftInput");
+                if (mTextEdit != null) {
+                    Log.d(TAG, "mTextEdit exist, clearing text");
+                    mTextEdit.setText("");
+                    mTextEdit.clearComposingText();
+                }
             }
         });
     }
--- engine/glfw/lib/android/android_init.c
+++ engine/glfw/lib/android/android_init.c
@@ -532,7 +532,15 @@ static int LooperCallback(int fd, int events, void* data)
     struct Command cmd;
     if (read(_glfwWin.m_Pipefd[0], &cmd, sizeof(cmd)) == sizeof(cmd)) {
         if (cmd.m_Command == CMD_INPUT_CHAR) {
+            _glfwInputChar( (int)cmd.m_Data, GLFW_RELEASE );
             _glfwInputChar( (int)cmd.m_Data, GLFW_PRESS );
+
+        } else if (cmd.m_Command == CMD_INPUT_MARKED_TEXT) {
+            _glfwSetMarkedText( (char*)cmd.m_Data );
+
+            // Need to free marked text string thas was
+            // allocated in android_window.c
+            free(cmd.m_Data);
         }
     } else {
         LOGF("read error in looper callback");
