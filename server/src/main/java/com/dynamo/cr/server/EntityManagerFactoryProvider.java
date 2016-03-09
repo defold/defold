03050803524df395f77177f32f0ff97e39c677df
Christian Murray <christian.murray@king.com>
2016-03-09 22:42:47 +0100

    Support for overriding persistence.xml properties

--- server/src/main/java/com/dynamo/cr/server/EntityManagerFactoryProvider.java
+++ server/src/main/java/com/dynamo/cr/server/EntityManagerFactoryProvider.java
@@ -1,6 +1,7 @@
 package com.dynamo.cr.server;
 
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManagerFactory;
@@ -22,6 +23,10 @@ public class EntityManagerFactoryProvider implements Provider<EntityManagerFacto
     @Override
     public EntityManagerFactory get() {
         HashMap<String, Object> props = new HashMap<String, Object>();
+        // Get all system properties in order to support overriding persistence.xml properties with -Dx=..
+        for (Entry<Object, Object> e : System.getProperties().entrySet()) {
+            props.put((String) e.getKey(), e.getValue());
+        }
         props.put(PersistenceUnitProperties.CLASSLOADER, this.getClass().getClassLoader());
         // NOTE: JPA-PersistenceUnits: in plug-in MANIFEST.MF has to be set. Otherwise the persistence unit is not found.
         EntityManagerFactory emf = new PersistenceProvider().createEntityManagerFactory(configuration.getPersistenceUnitName(), props);
