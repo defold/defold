6c7d034a6476dd8af88f2737c409571f4f7497ea
Jakob Pogulis <jakob.pogulis@king.com>
2016-03-16 18:20:49 +0100

    Rewrote package_editor.sh script to include specific requirements for Linux and Linux x64

--- com.dynamo.cr/com.dynamo.cr.editor-product/scripts/package_editor.sh
+++ com.dynamo.cr/com.dynamo.cr.editor-product/scripts/package_editor.sh
@@ -5,45 +5,57 @@ SCRIPT_NAME="$(basename "${0}")"
 SCRIPT_PATH="$(cd "$(dirname "${0}")"; pwd)"
 
 package () {
-	_PLATFORM="${1}"  # Platform
-	_WND="${2}"       # Window Manager
-	_ARCH="${3}"      # Architecture
-	_JRE="${4}"       # Java Runtime Environment
+  _PLATFORM="${1}"  # Platform
+  _WND="${2}"       # Window Manager
+  _ARCH="${3}"      # Architecture
+  _JRE="${4}"       # Java Runtime Environment
 
-	(
-		_PRODUCT_DIR="${SCRIPT_PATH}/../target/products"
-		_PACKAGE="Defold-${1}.${2}.${3}.zip"
+  (
+    _PRODUCT_DIR="${SCRIPT_PATH}/../target/products"
+    _PACKAGE="Defold-${1}.${2}.${3}.zip"
 
-		cd "${_PRODUCT_DIR}/com.dynamo.cr.editor.product/${_PLATFORM}/${_WND}/${_ARCH}"
+    cd "${_PRODUCT_DIR}/com.dynamo.cr.editor.product/${_PLATFORM}/${_WND}/${_ARCH}"
 
-		# Clean up previous artefacts
-		[ -f "eclipsec.exe" ] && rm -f "eclipsec.exe"
-		[ -f "Defold" ] && [ "${_PLATFORM}" == "macosx" ] && rm -f "Defold"
-		[ -d "jre" ] && rm -rf "jre"
-		[ -f "${_PRODUCT_DIR}/${_PACKAGE}" ] && rm -f "${_PRODUCT_DIR}/${_PACKAGE}"
+    # Clean up previous artefacts
+    [ -f "eclipsec.exe" ] && rm -f "eclipsec.exe"
+    [ -f "Defold" ] && [ "${_PLATFORM}" == "macosx" ] && rm -f "Defold"
+    [ -d "jre" ] && rm -rf "jre"
+    [ -d "xulrunner" ] && rm -rf "xulrunner"
+    [ -f "bootstrap.sh" ] && rm -f "bootstrap.sh"
+    [ -f "${_PRODUCT_DIR}/${_PACKAGE}" ] && rm -f "${_PRODUCT_DIR}/${_PACKAGE}"
 
-		clear; ls; sleep 10
+    # Construct package
+    unzip -q "${SCRIPT_PATH}/../target/${_JRE}"
+    if [ "${_PLATFORM}" == "linux" ]; then
+      # Include the bootstrap script in the package for Linux
+      cp "${SCRIPT_PATH}/../xulrunner/bootstrap.sh" .
 
-		# Construct package
-		unzip -q "${SCRIPT_PATH}/../target/${_JRE}"
-		clear; ls; sleep 10
-		if [ "${_PLATFORM}" == "linux" ]; then
-			if [ "${_ARCH}" == "x86_64" ]; then
-				__XULRUNNER="xulrunner-1.9.2.13.en-US.linux-x86_64.tar.bz2"
-				tar -xj "${SCRIPT_PATH}/../xulrunner/${__XULRUNNER}"
-				clear; ls; sleep 10
-				# Patch Defold.ini
-			fi
-		fi
+      if [ "${_ARCH}" == "x86_64" ]; then
+      	# Include XULRunner for 64 bit Linux
+        __XULRUNNER="xulrunner-1.9.2.13.en-US.linux-x86_64.tar.bz2"
+        tar -xjf "${SCRIPT_PATH}/../xulrunner/${__XULRUNNER}"
 
-		zip -r -y -q "${_PRODUCT_DIR}/${_PACKAGE}"
-	)
+        if ! grep "XULRunnerPath" "Defold.ini" > /dev/null 2>&1; then
+          _ARG_KEY="-Dorg.eclipse.swt.browser.XULRunnerPath"
+          _ARG_VAL="xulrunner"
+          echo "${_ARG_KEY}=${_ARG_VAL}" >> "Defold.ini"
+        fi
+      fi
+    fi
+
+    zip -r -y -q "${_PRODUCT_DIR}/${_PACKAGE}" .
+
+    # Clean up current artefacts
+    if [ -f "Defold.ini" ]; then
+      sed -i '/XULRunnerPath/d' "Defold.ini"
+    fi
+  )
 }
 
-#package macosx cocoa x86_64 jre-8u5-macosx-x64.zip &
-#package linux gtk x86 jre-8u5-linux-i586.zip &
+package macosx cocoa x86_64 jre-8u5-macosx-x64.zip &
+package linux gtk x86 jre-8u5-linux-i586.zip &
 package linux gtk x86_64 jre-8u5-linux-x64.zip &
-#package win32 win32 x86 jre-8u5-windows-i586.zip &
+package win32 win32 x86 jre-8u5-windows-i586.zip &
 
 # wait for background jobs
 wait
