9d74efb26ce7d3cb58388c7c7f23dbc538c84358
gigasquid <cmeier@gigasquidsoftware.com>
2016-03-28 13:48:56 -0400

    Bump core.cache with performance fixes https://github.com/clojure/core.cache

--- editor/src/clj/internal/cache.clj
+++ editor/src/clj/internal/cache.clj
@@ -8,82 +8,17 @@
 ;; ----------------------------------------
 ;; Null cache for testing
 ;; ----------------------------------------
-(cc/defcache NullCache []
+(cc/defcache NullCache [cache]
   cc/CacheProtocol
-  (lookup [_ item] nil)
+  (lookup [_ item])
   (lookup [_ item not-found] not-found)
-  (has?   [_ item] false)
-  (hit    [this item] this)
-  (miss   [this item result] this)
-  (evict  [this key] this)
-  (seed   [_ base] base))
+  (has? [_ item] false)
+  (hit [this item] this)
+  (miss [this item result] this)
+  (evict [this key] this)
+  (seed [this base] base))
 
-(defn- null-cache [] (NullCache.))
-
-;; ----------------------------------------
-;; Faster LRU cache, (contains? cache key) rather than (= v ::miss) nonsense
-;; ----------------------------------------
-
-(defn- build-leastness-queue
-  [base limit start-at]
-  (into (clojure.data.priority-map/priority-map)
-        (concat (take (- limit (count base)) (for [k (range (- limit) 0)] [k k]))
-                (for [[k _] base] [k start-at]))))
-
-(cc/defcache LRUCache [cache lru tick limit]
-  cc/CacheProtocol
-  (lookup [_ item]
-    (get cache item))
-  (lookup [_ item not-found]
-    (get cache item not-found))
-  (has? [_ item]
-    (contains? cache item))
-  (hit [_ item]
-    (let [tick+ (inc tick)]
-      (LRUCache. cache
-                 (if (contains? cache item)
-                   (assoc lru item tick+)
-                   lru)
-                 tick+
-                 limit)))
-  (miss [_ item result]
-    (let [tick+ (inc tick)]
-      (if (>= (count lru) limit)
-        (let [k (if (contains? lru item)
-                  item
-                  (first (peek lru))) ;; minimum-key, maybe evict case
-              c (-> cache (dissoc k) (assoc item result))
-              l (-> lru (dissoc k) (assoc item tick+))]
-          (LRUCache. c l tick+ limit))
-        (LRUCache. (assoc cache item result)  ;; no change case
-                   (assoc lru item tick+)
-                   tick+
-                   limit))))
-  (evict [this key]
-    (if (contains? cache key)
-      (LRUCache. (dissoc cache key)
-                 (dissoc lru key)
-                 (inc tick)
-                 limit)
-      this))
-  (seed [_ base]
-    (LRUCache. base
-               (build-leastness-queue base limit 0)
-               0
-               limit))
-  Object
-  (toString [_]
-    (str cache \, \space lru \, \space tick \, \space limit)))
-
-(defn lru-cache-factory
-  "Returns an LRU cache with the cache and usage-table initialied to `base` --
-   each entry is initialized with the same usage value.
-   This function takes an optional `:threshold` argument that defines the maximum number
-   of elements in the cache before the LRU semantics apply (default is 32)."
-  [base & {threshold :threshold :or {threshold 32}}]
-  {:pre [(number? threshold) (< 0 threshold)
-         (map? base)]}
-  (clojure.core.cache/seed (LRUCache. {} (clojure.data.priority-map/priority-map) 0 threshold) base))
+(defn- null-cache [] (NullCache. {}))
 
 ;; ----------------------------------------
 ;; Mutators
@@ -118,7 +53,7 @@
    (atom
     (if (zero? limit)
       (null-cache)
-      (lru-cache-factory {} :threshold limit)))))
+      (cc/lru-cache-factory {} :threshold limit)))))
 
 (defn cache-snapshot
   [cache]
