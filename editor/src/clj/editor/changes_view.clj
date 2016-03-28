467ea4c3a33e177ca1897670b2a1b7ab3ca0cc29
gigasquid <cmeier@gigasquidsoftware.com>
2016-03-28 17:21:52 -0400

    Replace post-create protocol with regular functions

--- editor/src/clj/editor/changes_view.clj
+++ editor/src/clj/editor/changes_view.clj
@@ -52,24 +52,23 @@
              new (slurp (io/file work-tree new-name))]
          (diff-view/make-diff-viewer old-name old new-name new))))
 
+(defn changes-view-post-create
+  [this basis event]
+  (let [{:keys [^Parent parent git workspace]} event
+        refresh                      (.lookup parent "#changes-refresh")
+        ^ListView list-view          (.lookup parent "#changes")]
+    (.setSelectionMode (.getSelectionModel list-view) SelectionMode/MULTIPLE)
+                                        ; TODO: Should we really include both git and list-view in the context. Have to think about this
+    (ui/context! list-view :asset-browser {:git git :list-view list-view :workspace workspace} list-view)
+    (ui/register-context-menu list-view ::changes-menu)
+    (ui/cell-factory! list-view status-render)
+    (ui/on-action! refresh (fn [_] (refresh! git list-view)))
+    (refresh! git list-view)))
+
 (g/defnode ChangesView
   (inherits core/Scope)
   (property parent-view g/Any)
-  (property git g/Any)
-
-  core/ICreate
-  (post-create
-   [this basis event]
-   (let [{:keys [^Parent parent git workspace]} event
-         refresh                      (.lookup parent "#changes-refresh")
-         ^ListView list-view          (.lookup parent "#changes")]
-     (.setSelectionMode (.getSelectionModel list-view) SelectionMode/MULTIPLE)
-     ; TODO: Should we really include both git and list-view in the context. Have to think about this
-     (ui/context! list-view :asset-browser {:git git :list-view list-view :workspace workspace} list-view)
-     (ui/register-context-menu list-view ::changes-menu)
-     (ui/cell-factory! list-view status-render)
-     (ui/on-action! refresh (fn [_] (refresh! git list-view)))
-     (refresh! git list-view))))
+  (property git g/Any))
 
 (defn make-changes-view [view-graph workspace parent]
   (let [view-id (g/make-node! view-graph ChangesView :parent-view parent)
@@ -77,7 +76,7 @@
     ; TODO: try/catch to protect against project without git setup
     ; Show warning/error etc?
     (try
-      (core/post-create view (g/now) {:parent parent :git (Git/open (io/file (g/node-value workspace :root))) :workspace workspace})
+      (changes-view-post-create view (g/now) {:parent parent :git (Git/open (io/file (g/node-value workspace :root))) :workspace workspace})
       view-id
       (catch Exception e
         (log/error :exception e)))))
