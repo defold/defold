e566ed84e9dc633167fb51d22efa55f0c521ddef
Sven Andersson <sven.andersson@king.com>
2016-03-30 14:38:19 +0200

    Fixed so that library zip files can include game.project files inside subfolders.
    In turn this will enable projects to depend on zip/libraries generated from GitHub releases for example.

--- com.dynamo.cr/com.dynamo.cr.bob.test/src/com/dynamo/bob/test/TestLibrariesRule.java
+++ com.dynamo.cr/com.dynamo.cr.bob.test/src/com/dynamo/bob/test/TestLibrariesRule.java
@@ -13,17 +13,17 @@ public class TestLibrariesRule extends ExternalResource {
 
     private File serverLocation;
 
-    void createLib(String root, String name, String sha1) throws IOException {
+    void createLib(String root, String subdir, String name, String sha1) throws IOException {
         File file = new File(String.format("%s/test_lib%s.zip", root, name));
         ZipOutputStream out = new ZipOutputStream(new FileOutputStream(file));
         out.setComment(sha1);
         ZipEntry ze;
 
-        ze = new ZipEntry("game.project");
+        ze = new ZipEntry(String.format("%sgame.project", subdir));
         out.putNextEntry(ze);
         out.write(String.format("[library]\ninclude_dirs=test_lib%s", name).getBytes());
 
-        ze = new ZipEntry(String.format("test_lib%s/file%s.in", name, name));
+        ze = new ZipEntry(String.format("%stest_lib%s/file%s.in", subdir, name, name));
         out.putNextEntry(ze);
         out.write(String.format("file%s", name).getBytes());
 
@@ -34,8 +34,10 @@ public class TestLibrariesRule extends ExternalResource {
     protected void before() throws Throwable {
         serverLocation = new File("server_root");
         serverLocation.mkdirs();
-        createLib(serverLocation.getAbsolutePath(), "1", "111");
-        createLib(serverLocation.getAbsolutePath(), "2", "222");
+        createLib(serverLocation.getAbsolutePath(), "", "1", "111");
+        createLib(serverLocation.getAbsolutePath(), "", "2", "222");
+        createLib(serverLocation.getAbsolutePath(), "subdir/", "3", "333");
+        createLib(serverLocation.getAbsolutePath(), "subdir/second/", "4", "444");
     }
 
     @Override
