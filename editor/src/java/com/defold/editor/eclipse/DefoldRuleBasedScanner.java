b0fb06147043f9f53f187285992f08b9311081ae
gigasquid <cmeier@gigasquidsoftware.com>
2016-04-13 10:18:59 -0400

    Create Custom RuleBasedScanner subclass that contains a method to directly get a substring on match move forward the offset by the length of the match

---
+++ editor/src/java/com/defold/editor/eclipse/DefoldRuleBasedScanner.java
@@ -0,0 +1,27 @@
+package com.defold.editor.eclipse;
+
+import org.eclipse.jface.text.rules.RuleBasedScanner;
+import org.eclipse.jface.text.BadLocationException;
+
+public class DefoldRuleBasedScanner extends RuleBasedScanner {
+
+    //Reads from offset all at once to the end of the doc as a peek
+    public String toString() {
+        try {
+            if (fOffset < fDocument.getLength()){
+                 int len = fDocument.getLength() - fOffset;
+                 return fDocument.get(fOffset, len);
+            }else{
+                return "";
+            }
+        } catch (BadLocationException e) {
+            System.out.println("Bad Location in Document" + "offset:" + fOffset +  "length:" + fDocument.getLength());
+            return "";
+        }
+    }
+
+    // increments the offset by a given amount - this is from a match found
+    public void moveForward(int offset){
+        fOffset = fOffset + offset;
+    }
+}
