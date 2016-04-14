5a798ca3425ba46dc4cf4976a0c65fc3e15a0b48
gigasquid <cmeier@gigasquidsoftware.com>
2016-04-14 12:11:04 -0400

    tests passing

--- editor/test/editor/code_view_test.clj
+++ editor/test/editor/code_view_test.clj
@@ -4,6 +4,7 @@
             [editor.code-view :as cv]
             [editor.lua :as lua]
             [editor.script :as script]
+            [editor.gl.shader :as shader]
             [support.test-support :refer [with-clean-system tx-nodes]])
   (:import [org.eclipse.jface.text IDocument DocumentEvent TextUtilities]
            [org.eclipse.fx.text.ui TextPresentation]))
@@ -37,50 +38,90 @@
        (let [new-code "x="]
          (g/transact (g/set-property code-node :code new-code))
          (g/node-value viewer-node :new-content)
-         (is (= [{:start 0 :length 2 :stylename "default"}] (viewer-node-style-maps viewer-node)))
-         ))
-     (testing "keyword style"
-       (let [new-code "break"]
-         (g/transact (g/set-property code-node :code new-code))
-         (g/node-value viewer-node :new-content)
-         (is (= [{:start 0 :length 5 :stylename "keyword"}] (viewer-node-style-maps viewer-node)))
-         ))
-     (testing "string style"
-       (let [new-code "\"cat\""]
-         (g/transact (g/set-property code-node :code new-code))
-         (g/node-value viewer-node :new-content)
-         (is (= [{:start 0 :length 5 :stylename "string"}] (viewer-node-style-maps viewer-node)))
-         ))
+         (is (= [{:start 0 :length 2 :stylename "default"}] (viewer-node-style-maps viewer-node)))))
      (testing "number style"
        (let [new-code "22"]
          (g/transact (g/set-property code-node :code new-code))
          (g/node-value viewer-node :new-content)
-         (is (= [{:start 0 :length 2 :stylename "number"}] (viewer-node-style-maps viewer-node)))
-         ))
+         (is (= [{:start 0 :length 2 :stylename "number"}] (viewer-node-style-maps viewer-node)))))
+     (testing "keyword style"
+       (let [new-code "break"]
+         (g/transact (g/set-property code-node :code new-code))
+         (g/node-value viewer-node :new-content)
+         (is (= [{:start 0 :length 5 :stylename "keyword"}] (viewer-node-style-maps viewer-node)))))
+     (testing "string style"
+       (let [new-code "\"foo\""]
+         (g/transact (g/set-property code-node :code new-code))
+         (g/node-value viewer-node :new-content)
+         (is (= [{:start 0 :length 5 :stylename "string"}] (viewer-node-style-maps viewer-node)))))
      (testing "single line comment style"
        (let [new-code "--foo"]
          (g/transact (g/set-property code-node :code new-code))
          (g/node-value viewer-node :new-content)
-         (is (= [{:start 0 :length 5 :stylename "comment"}] (viewer-node-style-maps viewer-node)))
-         ))
+         (is (= [{:start 0 :length 5 :stylename "comment"}] (viewer-node-style-maps viewer-node)))))
      (testing "single line comment style stays to one line"
        (let [new-code "--foo\n bar"]
          (g/transact (g/set-property code-node :code new-code))
          (g/node-value viewer-node :new-content)
-         (is (= {:start 0 :length 5 :stylename "comment"} (first (viewer-node-style-maps viewer-node))))
-         ))
+         (is (= {:start 0 :length 5 :stylename "comment"} (first (viewer-node-style-maps viewer-node))))))
      (testing "multi line comment style"
        (let [new-code "--[[\nmultilinecomment\n]]"]
          (g/transact (g/set-property code-node :code new-code))
          (g/node-value viewer-node :new-content)
-         ;;; The length on this doesn't seem to be right
-         (is (= [{:start 0 :stylename "comment"}] (viewer-node-style-maps viewer-node)))
-         ))
+         (is (= [{:start 0 :length 24 :stylename "comment2"}] (viewer-node-style-maps viewer-node)))))
      (testing "multi line comment style terminates"
-       (let [new-code "--[[\nmultilinecomment\n]] foo"]
+       (let [new-code "--[[\nmultilinecomment\n]] morecode"]
          (g/transact (g/set-property code-node :code new-code))
          (g/node-value viewer-node :new-content)
-         ;;; The length on this doesn't seem to be right maybe
-         ;;; somehting when the partitioning gets fixed
-         (is (= [{:start 0 :stylename "comment"}] (first (viewer-node-style-maps viewer-node))))
-         )))))
+         (is (= {:start 0 :length 24 :stylename "comment2"} (first (viewer-node-style-maps viewer-node)))))))))
+
+(deftest shader-glsl-vertex-program
+  (with-clean-system
+   (let [code "nothings"
+         opts (:code shader/glsl-opts)
+         source-viewer (cv/setup-source-viewer opts)
+         [code-node viewer-node] (tx-nodes (g/make-node world shader/ShaderNode)
+                                           (g/make-node world cv/CodeView :source-viewer source-viewer))]
+     (g/transact (g/set-property code-node :code code))
+     (cv/setup-code-view viewer-node code-node 0)
+     (testing "default style"
+       (let [new-code "x="]
+         (g/transact (g/set-property code-node :code new-code))
+         (g/node-value viewer-node :new-content)
+         (viewer-node-style-maps viewer-node)
+         (is (= [{:start 0 :length 2 :stylename "default"}] (viewer-node-style-maps viewer-node)))))
+    (testing "number style"
+       (let [new-code "22"]
+         (g/transact (g/set-property code-node :code new-code))
+         (g/node-value viewer-node :new-content)
+         (is (= [{:start 0 :length 2 :stylename "number"}] (viewer-node-style-maps viewer-node)))))
+    (testing "keyword style"
+      (let [new-code "template"]
+        (g/transact (g/set-property code-node :code new-code))
+        (g/node-value viewer-node :new-content)
+        (is (= [{:start 0 :length 8 :stylename "keyword"}] (viewer-node-style-maps viewer-node)))))
+    (testing "string style"
+      (let [new-code "\"foo\""]
+        (g/transact (g/set-property code-node :code new-code))
+        (g/node-value viewer-node :new-content)
+        (is (= [{:start 0 :length 5 :stylename "string"}] (viewer-node-style-maps viewer-node)))))
+     (testing "single line comment style"
+       (let [new-code "//foo"]
+         (g/transact (g/set-property code-node :code new-code))
+         (g/node-value viewer-node :new-content)
+         (is (= [{:start 0 :length 5 :stylename "comment"}] (viewer-node-style-maps viewer-node)))))
+     (testing "single line comment style stays to one line"
+       (let [new-code "//foo\n bar"]
+         (g/transact (g/set-property code-node :code new-code))
+         (g/node-value viewer-node :new-content)
+         (is (= {:start 0 :length 5 :stylename "comment"} (first (viewer-node-style-maps viewer-node))))))
+     (testing "multi line comment style"
+       (let [new-code "/**multilinecomment\n**/"]
+         (g/transact (g/set-property code-node :code new-code))
+         (g/node-value viewer-node :new-content)
+         (is (= [{:start 0 :length 23 :stylename "comment2"}] (viewer-node-style-maps viewer-node)))))
+     (testing "multi line comment style terminates"
+       (let [new-code "/**multilinecomment\n**/ more stuff"]
+         (g/transact (g/set-property code-node :code new-code))
+         (g/node-value viewer-node :new-content)
+         (is (= {:start 0 :length 23 :stylename "comment2"} (first (viewer-node-style-maps viewer-node)))))))))
