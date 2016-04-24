9fad6288705d58d9b3ecc15f214ca038fc746482
Ragnar Svensson <ragnar.svensson@king.com>
2016-04-24 14:04:44 +0200

    DEFEDIT-333 Profiler improvements
    * Profiler can dump json data
    * perf.html => profiler_template.html
    ** command-handler produces tmp/profiler.html from template + json data
    * Web profiler is located in web-profiler
    * Simple web server at util.http-server
    * Outline view was updated twice per tick
    * Web server is a part of the handler context for easy access (to the url)
    * Properties view is created and updated like the other views
    * Scopes for asset-browser, outline-view, properties-view

---
+++ editor/src/clj/editor/web_profiler.clj
@@ -0,0 +1,38 @@
+(ns editor.web-profiler
+  (:require [editor.handler :as handler]
+            [util.http-server :as http-server])
+  (:import  [com.defold.util Profiler]
+            [java.io File]
+            [java.awt Desktop]
+            [java.net URI]))
+
+(def ^:private template-path "resources/profiler_template.html")
+(def ^:private target-path "tmp/profiler.html")
+
+(defn- dump-profiler []
+  (let [data (Profiler/dumpJson)
+        html (-> (slurp template-path)
+               (clojure.string/replace "$PROFILER_DATA" data))]
+    (-> (File. target-path)
+      .getParentFile
+      .mkdirs)
+    (spit target-path html)))
+
+(handler/defhandler :profile :global
+  (enabled? [] true)
+  (run [] (dump-profiler)))
+
+(handler/defhandler :profile-show :global
+  (enabled? [] true)
+  (run [web-server]
+       (dump-profiler)
+       (.browse (Desktop/getDesktop) (URI. (format "%s/profiler" (http-server/local-url web-server))))))
+
+(defn handler [req]
+  {:code 200
+   :headers {"Content-Type" "text/html"}
+   :body (try
+           (slurp target-path)
+           (catch Throwable e
+             (dump-profiler)
+             (slurp target-path)))})
