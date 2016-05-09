cafce27c652d590dbc187630c3983010a010a84f
Ragnar Svensson <ragnar.svensson@king.com>
2016-05-09 17:21:52 +0200

    DEFEDIT-332 Various perf fixes when dragging gui template nodes.
    Squashed commit of the following:
    
    commit a95b681c0f5efbb97755bd8f1d3e7f3f7bed3361
    Merge: 4f2be73 35ad9b8
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Sun May 8 13:54:33 2016 +0200
    
        Merge branch 'dev' into ed2-outline-perf
    
    commit 4f2be73636ac6f7d1c8a09a85040faa9d05e1864
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Sun May 8 13:54:15 2016 +0200
    
        Fixed benchmark test
    
    commit fec872125b2d90dd7c1402d333ed34c08c1cb3c6
    Merge: a5bc7eb 37a00dc
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Sat Apr 30 10:10:31 2016 +0200
    
        Merge branch 'dev' into ed2-outline-perf
    
    commit a5bc7eb6ef0418b5a7e59327964af296a872e884
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Sat Apr 30 10:09:26 2016 +0200
    
        Removed old code and test code that did not work.
    
    commit a8ba439e6323104e0bd4644f2d46832a4b3dd13d
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Fri Apr 29 08:34:08 2016 +0200
    
        Various perf fixes
    
    commit 848fa37f26142415c40e9b2dc19a32d91cc9a39b
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Wed Apr 27 11:45:21 2016 +0200
    
        Faster graph traversal.
    
        * Changed graph data structure so that sarcs and tarcs are also keyed by label
    
    commit ac1ed2ef889d86bdb69a9a57e8b58f9a570f0696
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Tue Apr 26 15:36:34 2016 +0200
    
        Fixed local caching in value production
    
    commit da42dbbadb8bcec5cfa809729911a22446465e46
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Tue Apr 26 14:52:21 2016 +0200
    
        Fixed cycle detection
    
    commit b6d7786a2b0d77d5089cfa2849a751ed157e8c25
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Tue Apr 26 12:51:29 2016 +0200
    
        Removed redundant eval in value production for overrides
    
    commit 5640f54656f727525c21340cea6ade007e042267
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Tue Apr 26 12:50:46 2016 +0200
    
        Fixed bug that made falsey values uncachable
    
    commit 9ee9e97b13ad7b4ce4eaa57b8b21596ae222ed0f
    Author: Ragnar Svensson <ragnar.svensson@king.com>
    Date:   Tue Apr 26 12:49:45 2016 +0200
    
        Cycle detection is much faster (set instead of vec)

--- editor/test/internal/graph/graph_test.clj
+++ editor/test/internal/graph/graph_test.clj
@@ -28,8 +28,8 @@
     (is (nil? (ig/node g id)))
     (is (empty? (filter #(= "Any ig/node value" %) (ig/node-values g))))))
 
-(defn targets [g n l] (map gt/tail (filter #(= (.sourceLabel %) l) (get-in g [:sarcs n]))))
-(defn sources [g n l] (map gt/head (filter #(= (.targetLabel %) l) (get-in g [:tarcs n]))))
+(defn targets [g n l] (map gt/tail (get-in g [:sarcs n l])))
+(defn sources [g n l] (map gt/head (get-in g [:tarcs n l])))
 
 (defn- source-arcs-without-targets
   [g]
