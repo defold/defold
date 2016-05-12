21a4f750b41084af1ab37d08a2e88b9649e1816d
Mathias Westerdahl <mathias.westerdahl@king.com>
2016-05-12 15:46:35 +0200

    Making sure the 64 bit tests run when building on darwin

--- scripts/build.py
+++ scripts/build.py
@@ -438,7 +438,10 @@ class Configuration(object):
             self.upload_file(lib_path, '%s/%s%s_shared%s' % (full_archive_path, lib_prefix, lib, lib_ext))
 
     def build_engine(self):
-        skip_tests = '--skip-tests' if self.skip_tests or self.target_platform != self.host else ''
+        supported_tests = {}
+        supported_tests['darwin'] = ['darwin', 'x86_64-darwin']
+        supports_tests = self.target_platform in supported_tests.get(self.host, []) or self.host == self.target_platform
+        skip_tests = '--skip-tests' if self.skip_tests or not supports_tests else ''
         skip_codesign = '--skip-codesign' if self.skip_codesign else ''
         disable_ccache = '--disable-ccache' if self.disable_ccache else ''
 
