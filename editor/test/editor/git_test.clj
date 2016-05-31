611a6cdb9e468819eb063c10ebbe76dc8abb2823
Martin Trojer <martin.trojer@gmail.com>
2016-05-31 09:40:38 +0100

    clean and tests

--- editor/test/editor/git_test.clj
+++ editor/test/editor/git_test.clj
@@ -1,13 +1,15 @@
 (ns editor.git-test
-  (:require [clojure.java.io :as io]
-            [clojure.test :refer :all]
+  (:require [clojure
+             [set :as set]
+             [test :refer :all]]
+            [clojure.java.io :as io]
             [editor.git :as git])
-  (:import [java.nio.file Files]
-           [java.nio.file.attribute FileAttribute]
-           [org.eclipse.jgit.api Git MergeResult$MergeStatus]
-           [org.apache.commons.io FileUtils]))
+  (:import java.nio.file.attribute.FileAttribute
+           java.nio.file.Files
+           org.apache.commons.io.FileUtils
+           [org.eclipse.jgit.api Git MergeResult MergeResult$MergeStatus]))
 
-(defn- create-file [git path content]
+(defn create-file [git path content]
   (let [f (io/file (str (.getWorkTree (.getRepository git)) "/" path))]
     (io/make-parents f)
     (spit f content)))
@@ -15,7 +17,7 @@
 (defn- temp-dir []
   (.toFile (Files/createTempDirectory "foo" (into-array FileAttribute []))))
 
-(defn- new-git []
+(defn new-git []
   (let [f (temp-dir)
         git (-> (Git/init) (.setDirectory f) (.call))]
     ; We must have a HEAD in general
@@ -24,13 +26,13 @@
     (-> git (.commit) (.setMessage "message") (.call))
     git))
 
-(defn- clone [git]
+(defn clone [git]
   (-> (Git/cloneRepository)
       (.setURI (.getAbsolutePath (.getWorkTree (.getRepository git))))
       (.setDirectory (io/file (temp-dir)))
       (.call)))
 
-(defn- delete-git [git]
+(defn delete-git [git]
   (FileUtils/deleteDirectory (.getWorkTree (.getRepository git))))
 
 (defn delete-file [git file]
@@ -42,125 +44,138 @@
 (defn- add-src [git]
   (-> git (.add) (.addFilepattern "src") (.call)))
 
-(defn- commit-src [git]
+(defn commit-src [git]
   (add-src git)
   (-> git (.commit) (.setMessage "message") (.call)))
 
 (defn- all-files [status]
   (reduce (fn [acc [k v]] (clojure.set/union acc v)) #{} (dissoc status :untracked-folders)))
 
+(defn- autostage [^Git git]
+  (let [s (git/status git)]
+    (doseq [f (:modified s)]
+      (-> git (.add) (.addFilepattern f) (.call)))
+
+    (doseq [f (:untracked s)]
+      (-> git (.add) (.addFilepattern f) (.call)))
+
+    (doseq [f (:missing s)]
+      (-> git (.rm) (.addFilepattern f) (.call)))))
+
+;; ============================================================================================
+
 (deftest autostage-test
   (let [git (new-git)]
-    ; Create files
-    (create-file git "/src/main.cpp" "void main() {}")
-    (create-file git "/src/util.cpp" "")
+    (testing "create files"
+      (create-file git "/src/main.cpp" "void main() {}")
+      (create-file git "/src/util.cpp" ""))
 
-    ; Add and commit initial files
-    (commit-src git)
+    (testing "Add and commit initial files"
+      (commit-src git))
 
-    ; Change util.cpp
-    (is (= #{} (:untracked (git/status git))))
-    (create-file git "/src/util.cpp" "// stuff")
-    (is (= #{"src/util.cpp"} (:modified (git/status git))))
-    (is (= #{"src/util.cpp"} (:uncommited-changes (git/status git))))
-    (git/autostage git)
-    (is (= #{} (:modified (git/status git))))
-    (is (= #{"src/util.cpp"} (:uncommited-changes (git/status git))))
+    (testing "Change util.cpp"
+      (is (= #{} (:untracked (git/status git))))
+      (create-file git "/src/util.cpp" "// stuff")
+      (is (= #{"src/util.cpp"} (:modified (git/status git))))
+      (is (= #{"src/util.cpp"} (:uncommited-changes (git/status git))))
+      (autostage git)
+      (is (= #{} (:modified (git/status git))))
+      (is (= #{"src/util.cpp"} (:uncommited-changes (git/status git)))))
 
-    ; Create new.cpp
-    (create-file git "/src/new.cpp" "")
-    (is (= #{"src/new.cpp"} (:untracked (git/status git))))
-    (git/autostage git)
-    (is (= #{} (:untracked (git/status git))))
+    (testing "Create new.cpp"
+      (create-file git "/src/new.cpp" "")
+      (is (= #{"src/new.cpp"} (:untracked (git/status git))))
+      (autostage git)
+      (is (= #{} (:untracked (git/status git)))))
 
-    ; Delete main.cpp
-    (delete-file git "src/main.cpp")
-    (is (= #{"src/main.cpp"} (:missing (git/status git))))
-    (git/autostage git)
-    (is (= #{} (:missing (git/status git))))
-    (is (= #{"src/main.cpp"} (:removed (git/status git))))
+    (testing "Delete main.cpp"
+      (delete-file git "src/main.cpp")
+      (is (= #{"src/main.cpp"} (:missing (git/status git))))
+      (autostage git)
+      (is (= #{} (:missing (git/status git))))
+      (is (= #{"src/main.cpp"} (:removed (git/status git)))))
 
     (delete-git git)))
 
-(deftest simple-status-test
+(deftest worktree-test
   (let [git (new-git)]
-    (create-file git "/src/main.cpp" "void main() {}")
-    (is (= {"src/main.cpp" :added} (git/simple-status git)))
-    (git/autostage git)
-    (is (= {"src/main.cpp" :added} (git/simple-status git)))
-    (delete-git git))
-
-  (let [git (new-git)]
-    (create-file git "/src/main.cpp" "void main() {}")
-    (commit-src git)
-    (is (= #{} (all-files (git/status git))))
-
-    (delete-file git "src/main.cpp")
-    (is (= {"src/main.cpp" :deleted} (git/simple-status git)))
-    (git/autostage git)
-    (is (= {"src/main.cpp" :deleted} (git/simple-status git)))
-    (delete-git git))
-
-  (let [git (new-git)]
-    (create-file git "/src/main.cpp" "void main() {}")
-    (commit-src git)
-    (create-file git "/src/main.cpp" "void main2() {}")
-
-    (is (= {"src/main.cpp" :modified} (git/simple-status git)))
-    (git/autostage git)
-    (is (= {"src/main.cpp" :modified} (git/simple-status git)))
+    (is (instance? java.io.File (git/worktree git)))
     (delete-git git)))
 
+(deftest get-current-commit-ref-test
+  (let [git (new-git)]
+    (is (instance? org.eclipse.jgit.revwalk.RevCommit (git/get-current-commit-ref git)))
+    (delete-git git)))
+
+(deftest status-test
+  (let [git (new-git)]
+    (testing "untracked"
+      (create-file git "/src/main.cpp" "void main() {}")
+      (is (= #{"/src/main.cpp" (:untracked (git/status git))}))
+      (is (= #{"src" (:untracked-folders (git/status git))})))
+    (testing "added"
+      (add-src git)
+      (is (= #{"/src/main.cpp" (:added (git/status git))}))
+      (is (= #{"src" (:uncommnited-changes (git/status git))})))
+
+    (testing "conflicts"
+      (let [local-git (clone git)]
+        (create-file git "/src/main.cpp" "void main() {BAR}")
+        (create-file local-git "/src/main.cpp" "void main() {FOO}")
+        (commit-src git)
+        (commit-src local-git)
+        (-> local-git .pull .call)
+        (is (= #{"src/main.cpp"} (:conflicting (git/status local-git))))
+        (is (= {"src/main.cpp" :both-added} (:conflicting-stage-state (git/status local-git))))))
+    (delete-git git)))
 
 (deftest unified-status-test
-  ; new file
-  ; unstaged and staged
-  (let [git (new-git)]
-    (create-file git "/src/main.cpp" "void main() {}")
-    (is (= [{:score 0, :change-type :add, :old-path nil, :new-path "src/main.cpp"}] (git/unified-status git)))
-    (add-src git)
-    (is (= [{:score 0, :change-type :add, :old-path nil, :new-path "src/main.cpp"}] (git/unified-status git)))
-    (delete-git git))
+  (testing "new file; unstaged and staged"
+    (let [git (new-git)]
+      (create-file git "/src/main.cpp" "void main() {}")
+      (is (= [{:score 0, :change-type :add, :old-path nil, :new-path "src/main.cpp"}] (git/unified-status git)))
+      (add-src git)
+      (is (= [{:score 0, :change-type :add, :old-path nil, :new-path "src/main.cpp"}] (git/unified-status git)))
+      (delete-git git)))
 
-  ; changed file
-  ; unstaged and staged and partially staged
-  (let [git (new-git)
-        expect {:score 0, :change-type :modify, :old-path "src/main.cpp", :new-path "src/main.cpp"}]
+  (testing "changed file; unstaged and staged and partially staged"
+    (let [git (new-git)
+          expect {:score 0, :change-type :modify, :old-path "src/main.cpp", :new-path "src/main.cpp"}]
 
-    (create-file git "/src/main.cpp" "void main() {}")
-    (commit-src git)
-    (is (= [] (git/unified-status git)))
+      (create-file git "/src/main.cpp" "void main() {}")
+      (commit-src git)
+      (is (= [] (git/unified-status git)))
 
-    (create-file git "/src/main.cpp" "void main2() {}")
-    (is (= [expect] (git/unified-status git)))
-    (add-src git)
-    (is (= [expect] (git/unified-status git)))
+      (create-file git "/src/main.cpp" "void main2() {}")
+      (is (= [expect] (git/unified-status git)))
+      (add-src git)
+      (is (= [expect] (git/unified-status git)))
 
-    (create-file git "/src/main.cpp" "void main2() {}\nint x;")
-    (add-src git)
-    (is (= [expect] (git/unified-status git)))
+      (create-file git "/src/main.cpp" "void main2() {}\nint x;")
+      (add-src git)
+      (is (= [expect] (git/unified-status git)))
 
-    (delete-git git))
+      (delete-git git)))
 
-  ; delete and related
-  (let [git (new-git)]
+  (testing "delete and related"
+    (let [git (new-git)]
 
-    (create-file git "/src/main.cpp" "void main() {}")
-    (commit-src git)
-    (is (= [] (git/unified-status git)))
+      (create-file git "/src/main.cpp" "void main() {}")
+      (commit-src git)
+      (is (= [] (git/unified-status git)))
 
-    ; stage delete for src/main.cpp
-    (-> git (.rm) (.addFilepattern "src/main.cpp") (.call))
-    (is (= [{:score 0, :change-type :delete, :old-path "src/main.cpp", :new-path nil}] (git/unified-status git)))
+                                        ; stage delete for src/main.cpp
+      (-> git (.rm) (.addFilepattern "src/main.cpp") (.call))
+      (is (= [{:score 0, :change-type :delete, :old-path "src/main.cpp", :new-path nil}] (git/unified-status git)))
 
-    ; recreate src/main.cpp and expect in modified state
-    (create-file git "/src/main.cpp" "void main2() {}")
-    (is (= [{:score 0, :change-type :modify, :old-path "src/main.cpp", :new-path "src/main.cpp"}] (git/unified-status git)))
+                                        ; recreate src/main.cpp and expect in modified state
+      (create-file git "/src/main.cpp" "void main2() {}")
+      (is (= [{:score 0, :change-type :modify, :old-path "src/main.cpp", :new-path "src/main.cpp"}] (git/unified-status git)))
 
-    (delete-git git))
+      (delete-git git)))
 
-  ; rename in work tree
-  (let [git (new-git)]
+  (testing "rename in work tree"
+    (let [git (new-git)]
       (create-file git "/src/main.cpp" "void main() {}")
       (commit-src git)
       (is (= [] (git/unified-status git)))
@@ -170,10 +185,10 @@
 
       (is (= [{:score 100, :change-type :rename, :old-path "src/main.cpp", :new-path "src/foo.cpp"}] (git/unified-status git)))
 
-      (delete-git git))
+      (delete-git git)))
 
-  ; rename deleted and staged file
-  (let [git (new-git)]
+  (testing "rename deleted and staged file"
+    (let [git (new-git)]
       (create-file git "/src/main.cpp" "void main() {}")
       (commit-src git)
       (is (= [] (git/unified-status git)))
@@ -183,66 +198,144 @@
 
       (is (= [{:score 100, :change-type :rename, :old-path "src/main.cpp", :new-path "src/foo.cpp"}] (git/unified-status git)))
 
-      (delete-git git))
+      (delete-git git)))
 
-  ; honor .gitignore
-  (let [git (new-git)
-        expect {:score 0, :change-type :modify, :old-path "src/main.cpp", :new-path "src/main.cpp"}]
+  (testing "honor .gitignore"
+    (let [git (new-git)
+          expect {:score 0, :change-type :modify, :old-path "src/main.cpp", :new-path "src/main.cpp"}]
 
-    (create-file git "/.gitignore" "*.cpp")
-    (-> git (.add) (.addFilepattern ".gitignore") (.call))
-    (-> git (.commit) (.setMessage "message") (.call))
+      (create-file git "/.gitignore" "*.cpp")
+      (-> git (.add) (.addFilepattern ".gitignore") (.call))
+      (-> git (.commit) (.setMessage "message") (.call))
 
-    (is (= [] (git/unified-status git)))
+      (is (= [] (git/unified-status git)))
 
-    (create-file git "/src/main.cpp" "void main() {}")
-    (is (= [] (git/unified-status git)))
+      (create-file git "/src/main.cpp" "void main() {}")
+      (is (= [] (git/unified-status git)))
 
+      (delete-git git))))
+
+(deftest show-file-test
+  (let [git (new-git)]
+    (create-file git "/src/main.cpp" "void main() {}")
+    (let [ref (commit-src git)]
+      (is (= "void main() {}" (String. (git/show-file git "src/main.cpp"))))
+      (is (= "void main() {}" (String. (git/show-file git "src/main.cpp" (.name ref))))))
+    (delete-git git)))
+
+(deftest stage-file-test
+  (let [git (new-git)]
+    (create-file git "src/main.cpp" "void main() {}")
+    (git/stage-file git "src/main.cpp")
+    (is (= #{"src/main.cpp"} (:added (git/status git))))
+    (is (= #{} (:untracked (git/status git))))
+    (delete-git git)))
+
+(deftest unstage-file-test
+  (let [git (new-git)]
+    (create-file git "src/main.cpp" "void main() {}")
+    (git/stage-file git "src/main.cpp")
+    (is (= #{"src/main.cpp"} (:added (git/status git))))
+    (git/unstage-file git "src/main.cpp")
+    (is (= #{} (:added (git/status git))))
+    (is (= #{"src/main.cpp"} (:untracked (git/status git))))
+    (delete-git git)))
+
+(deftest hard-reset-test
+  (let [git (new-git)]
+    (create-file git "src/main.cpp" "void main() {}")
+    (let [ref (commit-src git)]
+      (create-file git "src/main.cpp" "void main() {FOO}")
+      (is (= #{"src/main.cpp"} (:modified (git/status git))))
+      (git/hard-reset git ref)
+      (is (= #{} (:modified (git/status git)))))
+    (delete-git git)))
+
+(deftest stash-test
+  (let [git (new-git)]
+    (create-file git "src/main.cpp" "void main() {}")
+    (is (= #{"src/main.cpp"} (:untracked (git/status git))))
+    (git/stash git)
+    (is (= #{} (:untracked (git/status git))))
+    (delete-git git)))
+
+(deftest stash-apply-test
+  (let [git (new-git)]
+    (create-file git "src/main.cpp" "void main() {}")
+    (is (= #{"src/main.cpp"} (:untracked (git/status git))))
+    (let [ref (git/stash git)]
+      (is (= #{} (:untracked (git/status git))))
+      (git/stash-apply git ref)
+      (is (= #{"src/main.cpp"} (:untracked (git/status git)))))
+    (delete-git git)))
+
+(deftest stash-drop-test
+  (let [git (new-git)]
+    (create-file git "src/main.cpp" "void main() {}")
+    (let [ref (git/stash git)]
+      (git/stash-drop git ref)
+      (git/stash-apply git ref)
+      ;; ref still is reflog
+      (is (= #{"src/main.cpp"} (:untracked (git/status git)))))
     (delete-git git)))
 
 (deftest revert-test
-  ; untracked
-  (let [git (new-git)]
-    (create-file git "/src/main.cpp" "void main() {}")
-    (is (= #{"src/main.cpp"} (:untracked (git/status git))))
-    (git/revert git ["src/main.cpp"])
-    (is (= #{} (all-files (git/status git))))
-    (delete-git git))
+  (testing "untracked"
+    (let [git (new-git)]
+      (create-file git "/src/main.cpp" "void main() {}")
+      (is (= #{"src/main.cpp"} (:untracked (git/status git))))
+      (git/revert git ["src/main.cpp"])
+      (is (= #{} (all-files (git/status git))))
+      (delete-git git)))
 
-  ; new and staged
-  (let [git (new-git)]
-    (create-file git "/src/main.cpp" "void main() {}")
+  (testing "new and staged"
+    (let [git (new-git)]
+      (create-file git "/src/main.cpp" "void main() {}")
 
-    (git/autostage git)
-    (is (= #{"src/main.cpp"} (:added (git/status git))))
-    (git/revert git ["src/main.cpp"])
-    (is (= #{} (all-files (git/status git))))
-    (delete-git git))
+      (autostage git)
+      (is (= #{"src/main.cpp"} (:added (git/status git))))
+      (git/revert git ["src/main.cpp"])
+      (is (= #{} (all-files (git/status git))))
+      (delete-git git)))
 
-  ; changed
-  (let [git (new-git)]
-    (create-file git "/src/main.cpp" "void main() {}")
-    (commit-src git)
-    (create-file git "/src/main.cpp" "void main2() {}")
+  (testing "changed"
+    (let [git (new-git)]
+      (create-file git "/src/main.cpp" "void main() {}")
+      (commit-src git)
+      (create-file git "/src/main.cpp" "void main2() {}")
 
-    (is (= #{"src/main.cpp"} (:modified (git/status git))))
-    (git/revert git ["src/main.cpp"])
-    (is (= #{} (all-files (git/status git))))
-    (delete-git git))
+      (is (= #{"src/main.cpp"} (:modified (git/status git))))
+      (git/revert git ["src/main.cpp"])
+      (is (= #{} (all-files (git/status git))))
+      (delete-git git)))
 
-  ; changed and staged
-  (let [git (new-git)]
-    (create-file git "/src/main.cpp" "void main() {}")
-    (commit-src git)
-    (create-file git "/src/main.cpp" "void main2() {}")
+  (testing "changed and staged"
+    (let [git (new-git)]
+      (create-file git "/src/main.cpp" "void main() {}")
+      (commit-src git)
+      (create-file git "/src/main.cpp" "void main2() {}")
 
-    (is (= #{"src/main.cpp"} (:modified (git/status git))))
-    (git/autostage git)
-    (git/revert git ["src/main.cpp"])
-    (is (= #{} (all-files (git/status git))))
-    (delete-git git))
+      (is (= #{"src/main.cpp"} (:modified (git/status git))))
+      (autostage git)
+      (git/revert git ["src/main.cpp"])
+      (is (= #{} (all-files (git/status git))))
+      (delete-git git)))
 
-  ; renamed and staged
+  (testing "renamed and staged"
+    (let [git (new-git)]
+      (create-file git "/src/main.cpp" "void main() {}")
+      (commit-src git)
+
+      (create-file git "/src/foo.cpp" "void main() {}")
+      (delete-file git "src/main.cpp")
+
+      (is (= #{"src/main.cpp"} (:missing (git/status git))))
+      (is (= #{"src/foo.cpp"} (:untracked (git/status git))))
+                                        ;(git/autostage git)
+      (git/revert git ["src/foo.cpp"])
+      (is (= #{} (all-files (git/status git))))
+      (delete-git git)))
+
   (let [git (new-git)]
     (create-file git "/src/main.cpp" "void main() {}")
     (commit-src git)
@@ -252,51 +345,7 @@
 
     (is (= #{"src/main.cpp"} (:missing (git/status git))))
     (is (= #{"src/foo.cpp"} (:untracked (git/status git))))
-    ;(git/autostage git)
-    (git/revert git ["src/foo.cpp"])
-    (is (= #{} (all-files (git/status git))))
-    (delete-git git))
-
-  (let [git (new-git)]
-    (create-file git "/src/main.cpp" "void main() {}")
-    (commit-src git)
-
-    (create-file git "/src/foo.cpp" "void main() {}")
-    (delete-file git "src/main.cpp")
-
-    (is (= #{"src/main.cpp"} (:missing (git/status git))))
-    (is (= #{"src/foo.cpp"} (:untracked (git/status git))))
-    (git/autostage git)
+    (autostage git)
     (git/revert git ["src/foo.cpp"])
     (is (= #{} (all-files (git/status git))))
     (delete-git git)))
-
-(deftest merge-test
-  ; merge conflict
-  (let [remote-git (new-git)
-        base-src "void main() {}"]
-    (create-file remote-git "/src/main.cpp" base-src)
-    (commit-src remote-git)
-    (let [git (clone remote-git)
-          their-src "void main1() {}"
-          our-src "void main2() {}"]
-      (create-file remote-git "/src/main.cpp" their-src)
-      (commit-src remote-git)
-      (create-file git "/src/main.cpp" our-src)
-      (commit-src git)
-      (let [status (-> git
-                     .pull
-                     .call
-                     .getMergeResult
-                     .getMergeStatus)]
-        (is (= status MergeResult$MergeStatus/CONFLICTING)))
-
-      (-> (.checkout git)
-          (.addPath "src/main.cpp")
-          (.setStage org.eclipse.jgit.api.CheckoutCommand$Stage/OURS)
-          (.call))
-
-      (is (= our-src (slurp-file git "src/main.cpp")))
-
-      (delete-git git))
-    (delete-git remote-git)))
