773a1920a857781ae79bdad0d7d4201e022f696b
Martin Trojer <martin.trojer@gmail.com>
2016-05-31 09:40:38 +0100

    clean and tests

--- editor/test/editor/sync_test.clj
+++ editor/test/editor/sync_test.clj
@@ -1,172 +1,49 @@
 (ns editor.sync-test
   (:require [clojure.java.io :as io]
             [clojure.test :refer :all]
+            [editor.git-test :refer :all]
+            [editor.prefs :as prefs]
             [editor.git :as git]
-            [editor.sync :as sync])
+            [editor.sync :as sync]
+            [editor.progress :as progress])
   (:import [java.nio.file Files]
            [java.nio.file.attribute FileAttribute]
            [org.eclipse.jgit.api Git]
            [org.apache.commons.io FileUtils]))
 
-(defn- ws-file [git path]
-  (let [f (io/file (str (.getWorkTree (.getRepository git)) "/" path))]
-    f))
+(defn- new-flow [git]
+  (let [p (prefs/make-prefs "unit-test")]
+    (prefs/set-prefs p "email" "foo@bar.com")
+    (prefs/set-prefs p "token" "TOKEN")
+    (sync/make-flow git p)))
 
-(defn- create-file [git path content]
-  (let [f (ws-file git path)]
-    (io/make-parents f)
-    (spit f content)))
+(deftest make-flow-test
+  (let [git  (new-git)
+        flow (new-flow git)]
+    (is (= :pull/start (:state flow)))
+    (is (= git (:git flow)))
+    (is (instance? org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider (:creds flow)))
+    (is (instance? org.eclipse.jgit.revwalk.RevCommit (:start-ref flow)))
+    (is (nil? (:stash-ref flow)))
+    (delete-git git)))
 
-(defn slurp-file [git path]
-  (slurp (ws-file git path)))
-
-(defn- temp-dir []
-  (.toFile (Files/createTempDirectory "foo" (into-array FileAttribute []))))
-
-(defn- new-git []
-  (let [f (temp-dir)
-        git (-> (Git/init) (.setDirectory f) (.call))]
-    ; We must have a HEAD in general
-    (create-file git "/dummy" "")
-    (-> git (.add) (.addFilepattern "dummy") (.call))
-    (-> git (.commit) (.setMessage "initial") (.call))
-    git))
-
-(defn- add-src [git]
-  (-> git (.add) (.addFilepattern "src") (.call)))
-
-(defn- commit-src [git]
-  (add-src git)
-  (-> git (.commit) (.setMessage "message") (.call)))
-
-(defn- clone [git]
-  (-> (Git/cloneRepository)
-      (.setURI (.getAbsolutePath (.getWorkTree (.getRepository git))))
-      (.setDirectory (io/file (temp-dir)))
-      (.call)))
-
-(defn- setup-repos []
+(deftest cancel-flow-test
   (let [git (new-git)]
     (create-file git "/src/main.cpp" "void main() {}")
     (commit-src git)
-    [(clone git) git]))
+    (let [flow (new-flow git)]
+      (create-file git "/src/main.cpp" "void main() {FOO}")
+      (sync/cancel-flow flow)
+      (is (= "void main() {}" (slurp-file git "/src/main.cpp"))))
+    (delete-git git)))
 
-(defn- all-files [status]
-  (reduce (fn [acc [k v]] (clojure.set/union acc v)) #{} (dissoc status :untracked-folders)))
+(deftest advance-flow-test
+  (testing ":pull/start"
+    (let [git       (new-git)
+          local-git (clone git)
+          flow      (new-flow local-git)
+          res       (sync/advance-flow flow progress/null-render-progress!)]
+      (is (= :pull/done (:state res)))))
 
-(deftest sync-test
-  (testing "cancel"
-    (let [[git remote] (setup-repos)]
-      (create-file git "foobar" "hej")
-      (let [flow (sync/make-flow git)]
-        (sync/cancel-flow flow)
-        (is (= "master" (git/branch-name git)))
-        (is (= #{"foobar"} (:untracked (git/status git))))
-        (is (= "hej" (slurp-file git "foobar"))))))
-
-  (testing "new file"
-    (let [[git remote] (setup-repos)]
-      (create-file git "foobar" "hej")
-      (let [flow (sync/make-flow git)
-            flow (sync/push-flow flow ["foobar"])]
-        (is (= :success (:status flow)))
-        (is (= #{} (:untracked (git/status git))))
-        (is (= "hej" (slurp-file git "foobar"))))))
-
-  (testing "alter file"
-    (let [[git remote] (setup-repos)]
-      (create-file git "/src/main.cpp" "void main2() {}")
-      (let [flow (sync/make-flow git)
-            flow (sync/push-flow flow ["src/main.cpp"])]
-        (is (= :success (:status flow)))
-        (is (= #{} (:untracked (git/status git))))
-        (is (= "void main2() {}" (slurp-file git "/src/main.cpp"))))))
-
-  (testing "uncommited conflicts"
-    (let [[git remote] (setup-repos)]
-      (create-file git "/src/main.cpp" "void main1() {}")
-      (create-file remote "/src/main.cpp" "void main2() {}")
-      (commit-src remote)
-      (let [flow (sync/make-flow git)
-            flow (sync/push-flow flow [])
-            status (git/status git)]
-        (is (= :checkout-conflict (:status flow)))
-        (is (= "void main1() {}" (slurp-file git "/src/main.cpp")))
-        (is (= #{"src/main.cpp"} (:modified (git/status git)))))))
-
-  (testing "both modified"
-    (doseq [resolution [:ours :theirs]]
-      (let [[git remote] (setup-repos)]
-        (create-file git "/src/main.cpp" "void main1() {}")
-        (create-file remote "/src/main.cpp" "void main2() {}")
-        (commit-src remote)
-        (let [flow (sync/make-flow git)
-              flow (sync/push-flow flow ["src/main.cpp"])
-              status (git/status git)]
-          (is (= {"src/main.cpp" :both-modified} (:conflicting-stage-state status)))
-          (is (= :merging (:status flow)))
-          (is (= #{} (:untracked (git/status git))))
-          (is (= #{"src/main.cpp"} (:conflicting (git/status git))))
-          (sync/resolve-file flow "src/main.cpp" resolution)
-          (sync/commit-merge flow)
-          (is (= #{} (all-files (git/status git))))
-          (if (= resolution :ours)
-            (is (= "void main1() {}" (slurp-file git "src/main.cpp")))
-            (is (= "void main2() {}" (slurp-file git "src/main.cpp"))))))))
-
-  (testing "deleted by us"
-    (doseq [resolution [:ours :theirs]]
-      (let [[git remote] (setup-repos)]
-       (io/delete-file (ws-file git "/src/main.cpp"))
-       (create-file remote "/src/main.cpp" "void main2() {}")
-       (commit-src remote)
-       (let [flow (sync/make-flow git)
-             flow (sync/push-flow flow ["src/main.cpp"])
-             status (git/status git)]
-         (is (= {"src/main.cpp" :deleted-by-us} (:conflicting-stage-state status)))
-         (is (= :merging (:status flow)))
-         (is (= #{"src/main.cpp"} (:conflicting (git/status git))))
-         (sync/resolve-file flow "src/main.cpp" resolution)
-         (sync/commit-merge flow)
-         (is (= #{} (all-files (git/status git))))
-         (if (= resolution :ours)
-           (is (= false (.exists (ws-file git "src/main.cpp"))))
-           (is (= "void main2() {}" (slurp-file git "src/main.cpp"))))))))
-
-  (testing "deleted by them"
-    (doseq [resolution [:ours :theirs]]
-      (let [[git remote] (setup-repos)]
-        (-> (.rm remote) (.addFilepattern "src/main.cpp") (.call))
-        (commit-src remote)
-        (create-file git "/src/main.cpp" "void main1() {}")
-        (let [flow (sync/make-flow git)
-              flow (sync/push-flow flow ["src/main.cpp"])
-              status (git/status git)]
-          (is (= {"src/main.cpp" :deleted-by-them} (:conflicting-stage-state status)))
-          (is (= :merging (:status flow)))
-          (is (= #{"src/main.cpp"} (:conflicting (git/status git))))
-          (sync/resolve-file flow "src/main.cpp" resolution)
-          (sync/commit-merge flow)
-          (is (= #{} (all-files (git/status git))))
-          (if (= resolution :ours)
-            (is (= "void main1() {}" (slurp-file git "src/main.cpp")))
-            (is (= false (.exists (ws-file git "src/main.cpp")))))))))
-
-  (testing "both added"
-    (doseq [resolution [:ours :theirs]]
-      (let [[git remote] (setup-repos)]
-        (create-file git "/src/new.cpp" "a")
-        (create-file remote "/src/new.cpp" "b")
-        (commit-src remote)
-        (let [flow (sync/make-flow git)
-              flow (sync/push-flow flow ["src/new.cpp"])
-              status (git/status git)]
-          (is (= {"src/new.cpp" :both-added} (:conflicting-stage-state status)))
-          (is (= :merging (:status flow)))
-          (is (= #{"src/new.cpp"} (:conflicting (git/status git))))
-          (sync/resolve-file flow "src/new.cpp" resolution)
-          (sync/commit-merge flow)
-          (is (= #{} (all-files (git/status git))))
-          (if (= resolution :ours)
-            (is (= "a" (slurp-file git "src/new.cpp")))
-            (is (= "b" (slurp-file git "src/new.cpp")))))))))
+  ;; TODO -- filling in
+  )
