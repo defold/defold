d771bf2be6964fdefd08485513088f91fa2a9dcf
gigasquid <cmeier@gigasquidsoftware.com>
2016-06-08 17:11:48 -0400

    more tests with failure on local vs non-local variables & functions

--- editor/test/editor/lua_parser_test.clj
+++ editor/test/editor/lua_parser_test.clj
@@ -1,8 +1,12 @@
 (ns editor.lua-parser-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer :all]
+            [clojure.java.io :as io]
             [editor.lua-parser :refer :all]))
 
 (deftest test-variables
+  (testing "global variable"
+    (is (= ["foo"] (:locals (lua-info "foo")))))
   (testing "one local with no assignment"
     (is (= ["foo"] (:locals (lua-info "local foo")))))
   (testing "multiple locals with no assignment"
@@ -33,3 +37,18 @@
     (let [code "function oadd(num1, num2) return num1 end \n function tadd(foo) return num1 end"]
       (is (= {"oadd" {:params ["num1" "num2"]}, "tadd" {:params ["foo"]}} (:functions (lua-info code)))))))
 
+
+(deftest test-lua-info-with-files
+  (testing "variable test file"
+    (let [result (lua-info (slurp (io/resource "lua/variable_test.lua")))]
+      (is (= {:locals ["a" "b" "c" "d"]
+              :functions {}
+              :requires {}} result))))
+(testing "function test file"
+    (let [result (lua-info (slurp (io/resource "lua/function_test.lua")))]
+      (is (= {:locals []
+              :functions {"add" {:params ["num1" "num2"]}
+                          "ladd" {:params ["num1" "num2"]}
+                          "oadd" {:params ["num1" "num2"]}}
+              :requires {}} result))))
+  )
