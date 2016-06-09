2b2c798d6fd2ae24b9561071d04a5972f5010897
gigasquid <cmeier@gigasquidsoftware.com>
2016-06-09 11:31:24 -0400

    local and globals for functions and vars

--- editor/src/clj/editor/lua_parser.clj
+++ editor/src/clj/editor/lua_parser.clj
@@ -32,18 +32,31 @@
         {(first parsed-names) (require-name zexpr)}))
     {}))
 
-(defmulti xform-node (fn [node] (keyword (second node))))
+(defn collect-vars [varlist]
+  (remove nil? (mapv second (rest varlist))))
 
-(defmethod xform-node :default [node]
+(defmulti xform-node (fn [k node] k))
+
+(defmethod xform-node :default [k node]
   nil)
 
-(defmethod xform-node :local [node]
-  (let [[_ _ namelist _ exprlist] node
-        parsed-names (parse-namelist namelist)
-        require-info (collect-require parsed-names exprlist)]
-    {:locals parsed-names :requires require-info}))
+(defmethod xform-node :local [k node]
+  (if (and (> (count node) 2) (string? (nth node 2)) (= "function") (nth node 2))
+   (let [[_ _ _ funcname funcbody] node
+         [_ _  [_  namelist]] funcbody]
+     {:functions {funcname {:params (parse-namelist namelist)}}})
+   (let [[_ _ namelist _ exprlist] node
+         parsed-names (parse-namelist namelist)
+         require-info (collect-require parsed-names exprlist)]
+     {:vars parsed-names :requires require-info})))
 
-(defmethod xform-node :function [node]
+(defmethod xform-node :varlist [k node]
+  (let [[_ varlist _ exprlist] node
+        parsed-names (collect-vars varlist)
+        require-info (collect-require parsed-names exprlist)]
+    {:vars parsed-names :requires require-info}))
+
+(defmethod xform-node :function [k node]
   (let [[_ _ [_ funcname] funcbody] node
         [_ _  [_  namelist]] funcbody]
     {:functions {funcname {:params (parse-namelist namelist)}}}))
@@ -55,16 +68,20 @@
      result
      (let [node (zip/node loc)]
        (if (and (seq? node) (= :stat (first node)))
-         (recur (zip/next loc) (conj result (xform-node node)))
+         (let [k (second node)]
+           (if (seq? k)
+             (recur (zip/next loc) (conj result (xform-node (first k) node)))
+             (recur (zip/next loc) (conj result (xform-node (keyword k) node)))
+             ))
          (recur (zip/next loc) result))))))
 
 
 (defn lua-info [code]
   (let [info (-> code lua-parser zip/seq-zip collect-info)
-        locals-info (vec (apply concat (map :locals info)))
+        locals-info (vec (apply concat (map :vars info)))
         functions-info (or (apply merge (map :functions info)) {})
         requires-info (or (apply merge (map :requires info)) {})]
-    {:locals locals-info
+    {:vars locals-info
      :functions functions-info
      :requires requires-info}))
 
@@ -73,7 +90,27 @@
 
   (lua-info "local d \n local e")
 
-  (collect-info  (zip/seq-zip (lua-parser "local d \n local e")))
+  (lua-info  "foo=1")
+
+  (lua-parser "foo=1")
+  (:chunk (:block (:stat (:varlist (:var "foo")) "=" (:explist (:exp (:number "1"))))) "<EOF>")
+  (lua-parser "local foo=1")
+  (:chunk (:block (:stat "local" (:namelist "foo") "=" (:explist (:exp (:number "1"))))) "<EOF>")
+
+  (lua-parser "foo,bar=1,2")
+(:chunk (:block (:stat (:varlist (:var "foo") "," (:var "bar")) "=" (:explist (:exp (:number "1")) "," (:exp (:number "2"))))) "<EOF>")
+
+  (lua-info "function oadd(num1, num2) return num1 end")
+  (lua-parser "function oadd(num1, num2) return num1 end")
+  (:chunk (:block (:stat "function" (:funcname "oadd") (:funcbody "(" (:parlist (:namelist "num1" "," "num2")) ")" (:block (:retstat "return" (:explist (:exp (:prefixexp (:varOrExp (:var "num1"))))))) "end"))) "<EOF>")
+
+  (lua-info "local function oadd(num1, num2) return num1 end")
+  (lua-parser "local function oadd(num1, num2) return num1 end")
+  (:chunk (:block (:stat "local" "function" "oadd" (:funcbody "(" (:parlist (:namelist "num1" "," "num2")) ")" (:block (:retstat "return" (:explist (:exp (:prefixexp (:varOrExp (:var "num1"))))))) "end"))) "<EOF>")
+
+
+
+  (collect-info  (zip/seq-zip (lua-parser "a,v=1,2")))
   (apply merge-with concat  [{:locals ["d"]} {:locals ["e"]}])
 
 
