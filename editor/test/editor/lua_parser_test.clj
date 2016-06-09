10c07f3cbc40765fbbe45f60c046ab6e0c2c8133
gigasquid <cmeier@gigasquidsoftware.com>
2016-06-09 11:37:55 -0400

    tests pass

--- editor/test/editor/lua_parser_test.clj
+++ editor/test/editor/lua_parser_test.clj
@@ -6,36 +6,36 @@
 
 (deftest test-variables
   (testing "global variable with assignment"
-    (is (= ["foo"] (:vars (lua-info "foo=1")))))
+    (is (= #{"foo"} (:vars (lua-info "foo=1")))))
   (testing "mulitiple global variable with assignment"
-    (is (= ["foo" "bar"] (:vars (lua-info "foo,bar=1,2")))))
+    (is (= #{"foo" "bar"} (:vars (lua-info "foo,bar=1,2")))))
   (testing "one local with no assignment"
-    (is (= ["foo"] (:vars (lua-info "local foo")))))
+    (is (= #{"foo"} (:vars (lua-info "local foo")))))
   (testing "multiple locals with no assignment"
-    (is (= ["foo" "bar"] (:vars (lua-info "local foo,bar")))))
+    (is (= #{"foo" "bar"} (:vars (lua-info "local foo,bar")))))
   (testing "one local with assignment"
-    (is (= ["foo"] (:vars (lua-info "local foo=1")))))
+    (is (= #{"foo"} (:vars (lua-info "local foo=1")))))
   (testing "multiple locals with assignment"
-    (is (= ["foo" "bar"] (:vars (lua-info "local foo,bar = 1,3")))))
+    (is (= #{"foo" "bar"} (:vars (lua-info "local foo,bar = 1,3")))))
   (testing "local with a require assignment"
     (let [code "local mymathmodule = require(\"mymath\")"
           result (select-keys (lua-info code) [:vars :requires])]
-     (is (= {:vars ["mymathmodule"]
-             :requires {"mymathmodule" "mymath"}} result))))
+      (is (= {:vars #{"mymathmodule"}
+              :requires {"mymathmodule" "mymath"}} result))))
   (testing "global with a require assignment"
     (let [code "mymathmodule = require(\"mymath\")"
           result (select-keys (lua-info code) [:vars :requires])]
-     (is (= {:vars ["mymathmodule"]
-             :requires {"mymathmodule" "mymath"}} result))))
+      (is (= {:vars #{"mymathmodule"}
+              :requires {"mymathmodule" "mymath"}} result))))
   (testing "local with multiple require assignments"
     (let [code "local x = require(\"myx\") \n local y = require(\"myy\")"
           result (select-keys (lua-info code) [:vars :requires])]
-     (is (= {:vars ["x" "y"]
+      (is (= {:vars #{"x" "y"}
              :requires {"x" "myx" "y" "myy"}} result))))
   (testing "global with multiple require assignments"
     (let [code "x = require(\"myx\") \n y = require(\"myy\")"
           result (select-keys (lua-info code) [:vars :requires])]
-     (is (= {:vars ["x" "y"]
+      (is (= {:vars #{"x" "y"}
              :requires {"x" "myx" "y" "myy"}} result)))))
 
 (deftest test-functions
@@ -53,12 +53,12 @@
 (deftest test-lua-info-with-files
   (testing "variable test file"
     (let [result (lua-info (slurp (io/resource "lua/variable_test.lua")))]
-      (is (= {:locals ["a" "b" "c" "d"]
+      (is (= {:vars #{"a" "b" "c" "d"}
               :functions {}
               :requires {}} result))))
 (testing "function test file"
     (let [result (lua-info (slurp (io/resource "lua/function_test.lua")))]
-      (is (= {:locals []
+      (is (= {:vars #{}
               :functions {"add" {:params ["num1" "num2"]}
                           "ladd" {:params ["num1" "num2"]}
                           "oadd" {:params ["num1" "num2"]}}
