67c4cd3b3b17b2c43b96f9520b0ed7a6b26074c7
gigasquid <cmeier@gigasquidsoftware.com>
2016-06-28 11:42:28 -0400

    take out debug

--- editor/src/java/com/defold/editor/eclipse/DefoldSourceViewer.java
+++ editor/src/java/com/defold/editor/eclipse/DefoldSourceViewer.java
@@ -4,11 +4,9 @@ import org.eclipse.fx.text.ui.source.SourceViewer;
 import org.eclipse.fx.ui.controls.styledtext.StyledTextArea;
 
 public class DefoldSourceViewer extends SourceViewer {
-	
-	
+
 	@Override
 	protected StyledTextArea createTextWidget() {
-		System.err.println ("Carin creating widget!");
 		StyledTextArea styledText= new DefoldStyledTextArea();
 		styledText.setLineRulerVisible(true);
 //		styledText.setLeftMargin(Math.max(styledText.getLeftMargin(), 2));
