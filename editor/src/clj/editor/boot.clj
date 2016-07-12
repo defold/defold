efed083a255bdb3bdf1d65453840afe7edd9c786
Michael Nygard <pair+mn@cognitect.com>
2016-07-12 11:29:05 -0500

    Fix hang on open project
    Synopsis of the problem: Loading namespaces was tied to displaying the
    welcome dialog. On a project open command, the welcome dialog is not
    shown, so nothing triggered loading of namespaces. The progress dialog
    was waiting for namespaces to finish loading, so it would hang and wait
    forever.
    
    Solution: Always trigger loading of namespaces, whether we show the
    welcome dialog or not. Previously, main with no args would start loading
    namespaces and show the welcome dialog. On completion, the welcome
    dialog would call _back_ into main.
    
    Now the welcome dialog uses continuation passing style. `main` always
    starts loading namespaces. If no arguments, then show the welcome dialog
    with a continuation function that opens the project. (Main is not called
    recursively any more.) If there are arguments to main, then call the
    function to open the project directly.
    
    In both cases, the new function `open-project-with-progress-dialog`
    ensures that namespace loading completes before attempting to use
    anything.

--- editor/src/clj/editor/boot.clj
+++ editor/src/clj/editor/boot.clj
@@ -18,8 +18,6 @@
 
 (set! *warn-on-reflection* true)
 
-(declare main)
-
 (defmacro deferred
    "Loads and runs a function dynamically to defer loading the namespace.
     Usage: \"(deferred clojure.core/+ 1 2 3)\" returns 6.  There's no issue
@@ -53,7 +51,7 @@
     (.addAll (.getChildren vbox) controls)
     vbox))
 
-(defn open-welcome [prefs]
+(defn open-welcome [prefs cont]
   (let [^VBox root (ui/load-fxml "welcome.fxml")
         stage (Stage.)
         scene (Scene. root)
@@ -67,18 +65,18 @@
                                           ; In other words, we can't reuse the welcome page and it has to be closed.
                                           ; We should potentially changed this when we have uberjar support and hence
                                           ; faster loading.
-                                          (main [file-name]))))
+                                          (cont file-name))))
 
     (ui/on-action! import-project (fn [_] (when-let [file-name (import/open-import-dialog prefs)]
                                             (ui/close! stage)
                                             ; See comment above about main and class-loaders
-                                            (main [file-name]))))
+                                            (cont file-name))))
 
     (.setOnMouseClicked recent-projects (ui/event-handler e (when (= 2 (.getClickCount ^MouseEvent e))
                                                               (when-let [file (-> recent-projects (.getSelectionModel) (.getSelectedItem))]
                                                                 (ui/close! stage)
                                                                 ; See comment above about main and class-loaders
-                                                                (main [(.getAbsolutePath ^File file)])))))
+                                                                (cont (.getAbsolutePath ^File file))))))
     (.setCellFactory recent-projects (reify Callback (call ^ListCell [this view]
                                                        (proxy [ListCell] []
                                                          (updateItem [file empty]
@@ -97,37 +95,56 @@
     (.setResizable stage false)
     (ui/show! stage)))
 
-(def namespaces-loaded (promise))
+(defn- load-namespaces-in-background
+  []
+  ;; load the namespaces of the project with all the defnode
+  ;; creation in the background
+  (future
+    (println "Running editor.boot-open-project/load-namespaces")
+    (deferred editor.boot-open-project/load-namespaces)
+    (println "Finished editor.boot-open-project/load-namespaces")))
+
+(defn- open-project-with-progress-dialog
+  [namespace-loader prefs project]
+  (ui/modal-progress
+   "Loading project" 100
+   (fn [render-progress!]
+     (let [progress (atom (progress/make "Loading project" 1))]
+       (render-progress! (swap! progress progress/message "Initializing project"))
+       ;; ensure the the namespaces have been loaded
+       (println "Waiting for namespaces to finish loading")
+       @namespace-loader
+       (deferred editor.boot-open-project/initialize-project)
+       (add-to-recent-projects prefs project)
+       (deferred editor.boot-open-project/open-project (io/file project) prefs render-progress!)))))
+
+(defn- select-project-from-welcome
+  [namespace-loader prefs]
+  (ui/run-later
+   (open-welcome prefs
+                 (fn [project]
+                   (open-project-with-progress-dialog namespace-loader prefs project)))))
 
 (defn main [args]
+  ;; note - the default exception handler gets reset each time a new
+  ;; project is opened. this _probably_ doesn't cause any issues, just
+  ;; don't rely on the identity of the handler.
   (Thread/setDefaultUncaughtExceptionHandler
    (reify Thread$UncaughtExceptionHandler
      (uncaughtException [_ thread exception]
        (log/error :exception exception :msg "uncaught exception"))))
-  (let [prefs (prefs/make-prefs "defold")]
-    (if (= (count args) 0)
-      (do
-        ;; load the namespaces of the project with all the defnode
-        ;; creation in the background while the open-welcome is coming
-        ;; up and the user is browsing for a project
-        (future ((fn [p]
-                   (deferred editor.boot-open-project/load-namespaces)
-                   (deliver p true)) namespaces-loaded))
-        (ui/run-later (open-welcome prefs)))
-      (try
-        (ui/modal-progress "Loading project" 100
-                           (fn [render-progress!]
-                             (do
-                               (let [progress  (atom (progress/make "Loading project" 1))]
-                                 (render-progress! (swap! progress progress/message "Initializing project"))
-                                 ;; ensure the the namespaces have been loaded
-                                 @namespaces-loaded
-                                 (deferred editor.boot-open-project/initialize-project)
-                                 (let [project-file (first args)]
-                                   (add-to-recent-projects prefs project-file)
-                                   (deferred editor.boot-open-project/open-project (io/file project-file) prefs render-progress!))))))
-        (catch Throwable t
-          (log/error :exception t)
-          (stack/print-stack-trace t)
-          (.flush *out*)
-          (System/exit -1))))))
+  (let [namespace-loader (load-namespaces-in-background)
+        prefs            (prefs/make-prefs "defold")]
+    (try
+      (if (= (count args) 0)
+        (select-project-from-welcome namespace-loader prefs)
+        (open-project-with-progress-dialog namespace-loader prefs (first args)))
+      (catch Throwable t
+        (log/error :exception t)
+        (stack/print-stack-trace t)
+        (.flush *out*)
+        ;; note - i'm not sure System/exit is a good idea here. it
+        ;; means that failing to open one project causes the whole
+        ;; editor to quit, maybe losing unsaved work in other open
+        ;; projects.
+        (System/exit -1)))))
