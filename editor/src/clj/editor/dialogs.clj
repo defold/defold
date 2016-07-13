3ff7f401203bd98328fdb18ffd958881da151276
Ragnar Dahlén <ragnar.dahlen@king.com>
2016-07-13 15:38:18 +0200

    DEFEDIT-361 Improve application focus/unfocus detection
    JavaFX nor Java provides a reliable way to detect when the application
    as a whole loses focus.
    
    This commit provides a (questionable) solution that detects when the
    time no application window has had focus goes above a threshold (500ms).

--- editor/src/clj/editor/dialogs.clj
+++ editor/src/clj/editor/dialogs.clj
@@ -58,17 +58,30 @@
         (finally
           (ui/run-later (ui/disable! (:root this) false)))))))
 
+(defonce focus-state (atom nil))
+
+(defn record-focus-change!
+  [focused?]
+  (reset! focus-state {:focused? focused?
+                       :t (System/currentTimeMillis)}))
+
+(defn observe-focus
+  [^Stage stage]
+  (ui/observe (.focusedProperty stage)
+              (fn [property old-val new-val]
+                (record-focus-change! new-val))))
+
 (defn make-alert-dialog [message]
   (let [root     ^Parent (ui/load-fxml "alert.fxml")
         stage    (Stage.)
         scene    (Scene. root)
         controls (ui/collect-controls root ["message" "ok"])]
+    (observe-focus stage)
     (ui/title! stage "Alert")
     (ui/text! (:message controls) message)
     (ui/on-action! (:ok controls) (fn [_] (.close stage)))
 
     (.initModality stage Modality/APPLICATION_MODAL)
-    (.setScene stage scene)
     (ui/show-and-wait! stage)))
 
 (defn make-confirm-dialog [message]
@@ -77,6 +90,7 @@
         scene    (Scene. root)
         controls (ui/collect-controls root ["message" "ok" "cancel"])
         result   (atom false)]
+    (observe-focus stage)
     (ui/title! stage "Please confirm")
     (ui/text! (:message controls) message)
     (ui/on-action! (:ok controls) (fn [_]
@@ -105,7 +119,7 @@
                         (ui/managed! (:error-group controls) visible)
                         (ui/visible! (:error-group controls) visible)
                         (.sizeToScene stage))))]
-
+    (observe-focus stage)
     (ui/text! (:ok controls) (or (:ok-label options) "Ok"))
     (ui/title! stage (or (:title options) ""))
     (ui/children! (:dialog-area controls) [dialog-root])
@@ -152,7 +166,7 @@
         items        (filter #(and (= :file (resource/source-type %)) (accepted-ext (:ext (resource/resource-type %))))
                              (g/node-value workspace :resource-list))
         close        (fn [] (reset! return (ui/selection (:resources controls))) (.close stage))]
-
+    (observe-focus stage)    
     (.initOwner stage (ui/main-stage))
     (ui/title! stage (or (:title options) "Select Resource"))
     (ui/items! (:resources controls) items)
@@ -281,7 +295,7 @@
         exts      (atom nil)
         close     (fn [] (reset! return (ui/selection (:resources-tree controls))) (.close stage))
         tree-view ^TreeView (:resources-tree controls)]
-
+    (observe-focus stage)
     (.initOwner stage (ui/main-stage))
     (ui/title! stage "Search in files")
 
@@ -336,6 +350,7 @@
         controls (ui/collect-controls root ["name" "ok"])
         return (atom nil)
         close (fn [] (reset! return (ui/text (:name controls))) (.close stage))]
+    (observe-focus stage)    
     (.initOwner stage (ui/main-stage))
     (ui/title! stage "New Folder")
 
@@ -368,6 +383,7 @@
                         (str/replace #"/" "")
                         (str/replace #"\\" "")
                         (str (when typ (str "." typ)))))]
+    (observe-focus stage)
     (.initOwner stage (ui/main-stage))
 
     (ui/title! stage title)
@@ -422,6 +438,7 @@
                   (reset! return (File. base-dir (ui/text (:path controls)))))
                 (.close stage))
         set-location (fn [location] (ui/text! (:location controls) (relativize base-dir location)))]
+    (observe-focus stage)    
     (.initOwner stage (ui/main-stage))
     (ui/title! stage (str "New " type))
     (set-location location)
@@ -457,6 +474,7 @@
         scene (Scene. root)
         controls (ui/collect-controls root ["line"])
         close (fn [v] (do (deliver result v) (.close stage)))]
+    (observe-focus stage)    
     (.initOwner stage (ui/main-stage))
     (ui/title! stage "Go to line")
     (.setOnKeyPressed scene
@@ -477,6 +495,7 @@
         scene (Scene. root)
         controls (ui/collect-controls root ["text"])
         close (fn [v] (do (deliver result v) (.close stage)))]
+    (observe-focus stage)    
     (.initOwner stage (ui/main-stage))
     (ui/title! stage "Find Text")
     (.setOnKeyPressed scene
@@ -497,6 +516,7 @@
         scene (Scene. root)
         controls (ui/collect-controls root ["find-text" "replace-text"])
         close (fn [v] (do (deliver result v) (.close stage)))]
+    (observe-focus stage)    
     (.initOwner stage (ui/main-stage))
     (ui/title! stage "Find/Replace Text")
     (.setOnKeyPressed scene
@@ -531,6 +551,7 @@
                                   (do
                                     (println "Proposal filter bad filter pattern " @filter-text)
                                     (swap! filter-text #(apply str (drop-last %)))))))]
+    (observe-focus stage)    
     (.setFill scene nil)
     (.initStyle stage StageStyle/UNDECORATED)
     (.initStyle stage StageStyle/TRANSPARENT)
