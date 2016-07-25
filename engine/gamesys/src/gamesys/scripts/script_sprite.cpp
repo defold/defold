48a7ad13a639a4b3b9865697b199b1d759e8e8e5
Mathias Westerdahl <mathias.westerdahl@king.com>
2016-07-25 16:51:49 +0200

    DEF-2011 Fixed alignment issue for some messages (#747)
    * DEF-2011 Fixed alignment issue for some messages
    Also replaced some copy-paste code with standardized dmScript::CheckHashOrString()
    
    * DEF-2011 Review fix: better type name

--- engine/gamesys/src/gamesys/scripts/script_sprite.cpp
+++ engine/gamesys/src/gamesys/scripts/script_sprite.cpp
@@ -5,6 +5,7 @@
 #include <dlib/hash.h>
 #include <dlib/log.h>
 #include <dlib/math.h>
+#include <script/script.h>
 
 #include "gamesys.h"
 #include "gamesys_ddf.h"
@@ -89,19 +90,14 @@ namespace dmGameSystem
 
         dmGameObject::HInstance instance = CheckGoInstance(L);
 
-        const uint32_t buffer_size = 256;
-        uint8_t buffer[buffer_size];
-        dmGameSystemDDF::SetFlipHorizontal* request = (dmGameSystemDDF::SetFlipHorizontal*)buffer;
-
-        uint32_t msg_size = sizeof(dmGameSystemDDF::SetFlipHorizontal);
-
-        request->m_Flip = (uint32_t)lua_toboolean(L, 2);
+        dmGameSystemDDF::SetFlipHorizontal msg;
+        msg.m_Flip = (uint32_t)lua_toboolean(L, 2);
 
         dmMessage::URL receiver;
         dmMessage::URL sender;
         dmScript::ResolveURL(L, 1, &receiver, &sender);
 
-        dmMessage::Post(&sender, &receiver, dmGameSystemDDF::SetFlipHorizontal::m_DDFDescriptor->m_NameHash, (uintptr_t)instance, (uintptr_t)dmGameSystemDDF::SetFlipHorizontal::m_DDFDescriptor, buffer, msg_size);
+        dmMessage::Post(&sender, &receiver, dmGameSystemDDF::SetFlipHorizontal::m_DDFDescriptor->m_NameHash, (uintptr_t)instance, (uintptr_t)dmGameSystemDDF::SetFlipHorizontal::m_DDFDescriptor, &msg, sizeof(msg));
         assert(top == lua_gettop(L));
         return 0;
     }
@@ -131,19 +127,14 @@ namespace dmGameSystem
 
         dmGameObject::HInstance instance = CheckGoInstance(L);
 
-        const uint32_t buffer_size = 256;
-        uint8_t buffer[buffer_size];
-        dmGameSystemDDF::SetFlipVertical* request = (dmGameSystemDDF::SetFlipVertical*)buffer;
-
-        uint32_t msg_size = sizeof(dmGameSystemDDF::SetFlipVertical);
-
-        request->m_Flip = (uint32_t)lua_toboolean(L, 2);
+        dmGameSystemDDF::SetFlipVertical msg;
+        msg.m_Flip = (uint32_t)lua_toboolean(L, 2);
 
         dmMessage::URL receiver;
         dmMessage::URL sender;
         dmScript::ResolveURL(L, 1, &receiver, &sender);
 
-        dmMessage::Post(&sender, &receiver, dmGameSystemDDF::SetFlipVertical::m_DDFDescriptor->m_NameHash, (uintptr_t)instance, (uintptr_t)dmGameSystemDDF::SetFlipVertical::m_DDFDescriptor, buffer, msg_size);
+        dmMessage::Post(&sender, &receiver, dmGameSystemDDF::SetFlipVertical::m_DDFDescriptor->m_NameHash, (uintptr_t)instance, (uintptr_t)dmGameSystemDDF::SetFlipVertical::m_DDFDescriptor, &msg, sizeof(msg));
         assert(top == lua_gettop(L));
         return 0;
     }
@@ -177,36 +168,18 @@ namespace dmGameSystem
         int top = lua_gettop(L);
 
         dmGameObject::HInstance instance = CheckGoInstance(L);
-
-        dmhash_t name_hash;
-        if (lua_isstring(L, 2))
-        {
-            name_hash = dmHashString64(lua_tostring(L, 2));
-        }
-        else if (dmScript::IsHash(L, 2))
-        {
-            name_hash = dmScript::CheckHash(L, 2);
-        }
-        else
-        {
-            return luaL_error(L, "name must be either a hash or a string");
-        }
+        dmhash_t name_hash = dmScript::CheckHashOrString(L, 2);
         Vectormath::Aos::Vector4* value = dmScript::CheckVector4(L, 3);
 
-        const uint32_t buffer_size = 256;
-        DM_ALIGNED(16) uint8_t buffer[buffer_size];
-        dmGameSystemDDF::SetConstant* request = (dmGameSystemDDF::SetConstant*)buffer;
-
-        uint32_t msg_size = sizeof(dmGameSystemDDF::SetConstant);
-
-        request->m_NameHash = name_hash;
-        request->m_Value = *value;
+        dmGameSystemDDF::SetConstant msg;
+        msg.m_NameHash = name_hash;
+        msg.m_Value = *value;
 
         dmMessage::URL receiver;
         dmMessage::URL sender;
         dmScript::ResolveURL(L, 1, &receiver, &sender);
 
-        dmMessage::Post(&sender, &receiver, dmGameSystemDDF::SetConstant::m_DDFDescriptor->m_NameHash, (uintptr_t)instance, (uintptr_t)dmGameSystemDDF::SetConstant::m_DDFDescriptor, buffer, msg_size);
+        dmMessage::Post(&sender, &receiver, dmGameSystemDDF::SetConstant::m_DDFDescriptor->m_NameHash, (uintptr_t)instance, (uintptr_t)dmGameSystemDDF::SetConstant::m_DDFDescriptor, &msg, sizeof(msg));
         assert(top == lua_gettop(L));
         return 0;
     }
@@ -238,34 +211,16 @@ namespace dmGameSystem
         int top = lua_gettop(L);
 
         dmGameObject::HInstance instance = CheckGoInstance(L);
+        dmhash_t name_hash = dmScript::CheckHashOrString(L, 2);
 
-        dmhash_t name_hash;
-        if (lua_isstring(L, 2))
-        {
-            name_hash = dmHashString64(lua_tostring(L, 2));
-        }
-        else if (dmScript::IsHash(L, 2))
-        {
-            name_hash = dmScript::CheckHash(L, 2);
-        }
-        else
-        {
-            return luaL_error(L, "name must be either a hash or a string");
-        }
-
-        const uint32_t buffer_size = 256;
-        uint8_t buffer[buffer_size];
-        dmGameSystemDDF::ResetConstant* request = (dmGameSystemDDF::ResetConstant*)buffer;
-
-        uint32_t msg_size = sizeof(dmGameSystemDDF::ResetConstant);
-
-        request->m_NameHash = name_hash;
+        dmGameSystemDDF::ResetConstant msg;
+        msg.m_NameHash = name_hash;
 
         dmMessage::URL receiver;
         dmMessage::URL sender;
         dmScript::ResolveURL(L, 1, &receiver, &sender);
 
-        dmMessage::Post(&sender, &receiver, dmGameSystemDDF::ResetConstant::m_DDFDescriptor->m_NameHash, (uintptr_t)instance, (uintptr_t)dmGameSystemDDF::ResetConstant::m_DDFDescriptor, buffer, msg_size);
+        dmMessage::Post(&sender, &receiver, dmGameSystemDDF::ResetConstant::m_DDFDescriptor->m_NameHash, (uintptr_t)instance, (uintptr_t)dmGameSystemDDF::ResetConstant::m_DDFDescriptor, &msg, sizeof(msg));
         assert(top == lua_gettop(L));
         return 0;
     }
@@ -279,19 +234,14 @@ namespace dmGameSystem
 
         Vectormath::Aos::Vector3* scale = dmScript::CheckVector3(L, 2);
 
-        const uint32_t buffer_size = 256;
-        uint8_t buffer[buffer_size];
-        dmGameSystemDDF::SetScale* request = (dmGameSystemDDF::SetScale*)buffer;
-
-        uint32_t msg_size = sizeof(dmGameSystemDDF::SetScale);
-
-        request->m_Scale = *scale;
+        dmGameSystemDDF::SetScale msg;
+        msg.m_Scale = *scale;
 
         dmMessage::URL receiver;
         dmMessage::URL sender;
         dmScript::ResolveURL(L, 1, &receiver, &sender);
 
-        dmMessage::Post(&sender, &receiver, dmGameSystemDDF::SetScale::m_DDFDescriptor->m_NameHash, (uintptr_t)instance, (uintptr_t)dmGameSystemDDF::SetScale::m_DDFDescriptor, buffer, msg_size);
+        dmMessage::Post(&sender, &receiver, dmGameSystemDDF::SetScale::m_DDFDescriptor->m_NameHash, (uintptr_t)instance, (uintptr_t)dmGameSystemDDF::SetScale::m_DDFDescriptor, &msg, sizeof(msg));
         assert(top == lua_gettop(L));
         return 0;
     }
