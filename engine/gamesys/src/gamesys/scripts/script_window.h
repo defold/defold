212c38b7533fb127572f0d2e039f1a67b531934d
Jakob Pogulis <jakob.pogulis@king.com>
2016-07-27 10:29:36 +0200

    Moved implementation to Window instead of System.

--- engine/gamesys/src/gamesys/scripts/script_window.h
+++ engine/gamesys/src/gamesys/scripts/script_window.h
@@ -5,10 +5,19 @@ namespace dmGameSystem
 {
     struct ScriptLibContext;
 
+    enum SleepMode
+    {
+        SLEEP_MODE_NORMAL = 0,
+        SLEEP_MODE_AWAKE  = 1
+    };
+
     void ScriptWindowRegister(const ScriptLibContext& context);
 
     void ScriptWindowOnWindowFocus(bool focus);
     void ScriptWindowOnWindowResized(int width, int height);
+
+    void SetSleepMode(SleepMode normal_sleep);
+    SleepMode GetSleepMode();
 }
 
 #endif // DM_GAMESYS_SCRIPT_WINDOW_H
