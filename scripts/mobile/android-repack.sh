2d18445c5ddc7fd2da28bed149c96ffc859c7ef5
Jakob Pogulis <jakob.pogulis@king.com>
2016-08-09 13:19:14 +0200

    More utility scripts for mobile development.

---
+++ scripts/mobile/android-repack.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+# Mathias Westerdahl, Jakob Pogulis
+set -eu
+
+SCRIPT_NAME="$(basename "${0}")"
+SCRIPT_PATH="$(cd "$(dirname "${0}")"; pwd)"
+
+
+# ----------------------------------------------------------------------------
+# Script functions
+# ----------------------------------------------------------------------------
+function terminate() {
+    echo "TERM - ${1:-}" && exit ${2:-1}
+}
+
+function terminate_usage() {
+    echo "Usage: ${SCRIPT_NAME} <source>"
+    echo "  source   - absolute filepath to the source apk to repack"
+    exit 1
+}
+
+function terminate_trap() {
+    trap - SIGHUP SIGINT SIGTERM
+    find "${ROOT}"
+    [ ! -z "${ROOT:-}" ] && rm -rf "${ROOT}"
+    terminate "An unexpected error occurred."
+}
+
+
+# ----------------------------------------------------------------------------
+# Script environment
+# ----------------------------------------------------------------------------
+[ ! -z "${DYNAMO_HOME:-}" ] || terminate "DYNAMO_HOME is not set"
+DEFOLD_HOME="$(cd "${DYNAMO_HOME}/../.."; pwd)"
+
+SOURCE="${1:-}" && [ ! -z "${SOURCE}" ] || terminate_usage
+
+ZIP="zip"
+UNZIP="unzip"
+ZIPALIGN="${DEFOLD_HOME}/com.dynamo.cr/com.dynamo.cr.bob/libexec/x86-darwin/zipalign"
+APKC="${DEFOLD_HOME}/com.dynamo.cr/com.dynamo.cr.bob/libexec/x86-darwin/apkc"
+
+ENGINE_LIB="${DYNAMO_HOME}/bin/armv7-android/libdmengine.so"
+ENGINE_DEX="${DYNAMO_HOME}/share/java/classes.dex"
+
+[ $(which "${ZIP}") ] || terminate "'${ZIP}' is not installed"
+[ $(which "${UNZIP}") ] || terminate "'${UNZIP}' is not installed"
+[ $(which "${ZIPALIGN}") ] || terminate "'${ZIPALIGN}' is not installed"
+[ $(which "${APKC}") ] || terminate "'${APKC}' is not installed"
+
+[ -f "${SOURCE}" ] || terminate "Source does not exist: ${SOURCE}"
+[ -f "${ENGINE_LIB}" ] || terminate "Engine does not exist: ${ENGINE_LIB}"
+[ -f "${ENGINE_DEX}" ] || terminate "Engine does not exist: ${ENGINE_DEX}"
+
+
+# ----------------------------------------------------------------------------
+# Script
+# ----------------------------------------------------------------------------
+ROOT="$(mktemp -d)"
+trap 'terminate_trap' SIGHUP SIGINT SIGTERM EXIT
+
+BUILD="${ROOT}/build"
+REPACKZIP="${ROOT}/repack.zip"
+REPACKZIP_ALIGNED="${ROOT}/repack.aligned.zip"
+
+APPLICATION="$(basename "${SOURCE}" ".apk")"
+TARGET="$(cd "$(dirname "${SOURCE}")"; pwd)/${APPLICATION}.repack"
+
+"${UNZIP}" "${SOURCE}" -d "${BUILD}" > /dev/null 2>&1
+(
+    cd "${BUILD}"
+
+    rm -rf "META-INF"
+    cp "${ENGINE_LIB}" "lib/armeabi-v7a/lib${APPLICATION}.so"
+    cp "${ENGINE_DEX}" "classes.dex"
+
+    zip -qr "${REPACKZIP}" "."
+)
+
+"${ZIPALIGN}" -v 4 "${REPACKZIP}" "${REPACKZIP_ALIGNED}" > /dev/null 2>&1
+"${APKC}" --in="${REPACKZIP_ALIGNED}" --out="${TARGET}.apk" > /dev/null 2>&1
+
+rm -rf "${ROOT}"
+
+
+# ----------------------------------------------------------------------------
+# Script teardown
+# ----------------------------------------------------------------------------
+trap - SIGHUP SIGINT SIGTERM EXIT
+echo "Wrote ${TARGET}.apk"
+exit 0
---
+++ scripts/update-editor-binaries.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+set -eu
+
+SCRIPT_NAME="$(basename "${0}")"
+SCRIPT_PATH="$(cd "$(dirname "${0}")"; pwd)"
+DEFOLD_PATH="$(cd "${SCRIPT_PATH}/.."; pwd)"
+
+# ----------------------------------------------------------------------------
+# Functions
+# ----------------------------------------------------------------------------
+function copy_binaries() {
+    [ "$#" -eq "2" ] || return 1
+
+    _ARG_SOURCE="${1}"
+    _ARG_TARGET="${2}"
+
+    [ -d "${_ARG_SOURCE}" ] || return 1
+    [ -d "${_ARG_TARGET}" ] || mkdir -p "${_ARG_TARGET}"
+
+    for _SOURCE_FILEPATH in "${_ARG_SOURCE}"/*; do
+        _FILENAME="$(basename "${_SOURCE_FILEPATH}")"
+        _TARGET_FILEPATH="${_ARG_TARGET}/${_FILENAME}"
+        if [ "${_SOURCE_FILEPATH}" -nt "${_TARGET_FILEPATH}" ]; then
+            cp -v "${_SOURCE_FILEPATH}" "${_TARGET_FILEPATH}"
+        fi
+    done
+
+    return 0
+}
+
+function copy_file() {
+    [ "$#" -eq "2" ] || return 1
+
+    _ARG_SOURCE="${1}"
+    _ARG_TARGET="${2}"
+
+    [ -f "${_ARG_SOURCE}" ] || return 1
+
+    if [ "${_ARG_SOURCE}" -nt "${_ARG_TARGET}" ]; then
+        cp -v "${_ARG_SOURCE}" "${_ARG_TARGET}"
+    fi
+}
+
+# ----------------------------------------------------------------------------
+# Resources
+# ----------------------------------------------------------------------------
+SOURCES=(
+    "darwin"        "linux"         "x86_64-linux"  "x86_64-darwin"
+    "win32"         "armv7-darwin"  "arm64-darwin"  "armv7-android"
+    "js-web"
+)
+
+TARGETS=(
+    "x86-darwin"    "x86-linux"     "x86_64-linux"  "x86_64-darwin"
+    "x86-win32"     "armv7-darwin"  "arm64-darwin"  "armv7-android"
+    "js-web"
+)
+
+# ----------------------------------------------------------------------------
+# Update all available binaries
+# ----------------------------------------------------------------------------
+for i in "${!SOURCES[@]}"; do
+    _SOURCE="${DYNAMO_HOME}/bin/${SOURCES[$i]}"
+    _TARGET="${DEFOLD_PATH}/com.dynamo.cr/com.dynamo.cr.bob/libexec/${TARGETS[$i]}"
+    if [ -d "${_SOURCE}" ]; then
+        echo "Updating editor with ${TARGETS[$i]} binaries ..."
+        copy_binaries "${_SOURCE}" "${_TARGET}"
+    fi
+done
+
+
+# ----------------------------------------------------------------------------
+# Update extra resources
+# ----------------------------------------------------------------------------
+echo "Updating editor with classes.dex ..."
+copy_file "${DYNAMO_HOME}/share/java/classes.dex" \
+          "${DEFOLD_PATH}/com.dynamo.cr/com.dynamo.cr.bob/lib/classes.dex"
+
+
+# ----------------------------------------------------------------------------
+# Teardown
+# ----------------------------------------------------------------------------
+exit 0
