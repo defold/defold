5dc605943433c7d8920785aa18c979b04daadfb6
Björn Ritzl <bjorn.ritzl@king.com>
2016-09-16 15:45:27 +0200

    Allow trailing commas after require

--- com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/pipeline/LuaScanner.java
+++ com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/pipeline/LuaScanner.java
@@ -20,13 +20,13 @@ public class LuaScanner {
 
     private static String commentAfterRequire = "\\s*?(-{2,}.*?)?$";
     
-    private static Pattern requirePattern1 = Pattern.compile(".*?require\\s*?\"(.*?)\"" + commentAfterRequire,
+    private static Pattern requirePattern1 = Pattern.compile(".*?require\\s*?\"(.*?)\"\\s*,*" + commentAfterRequire,
              Pattern.DOTALL | Pattern.MULTILINE);
 
-    private static Pattern requirePattern2 = Pattern.compile(".*?require\\s*?\\(\\s*?\"(.*?)\"\\s*?\\)" + commentAfterRequire,
+    private static Pattern requirePattern2 = Pattern.compile(".*?require\\s*?\\(\\s*?\"(.*?)\"\\s*?\\)\\s*,*" + commentAfterRequire,
              Pattern.DOTALL | Pattern.MULTILINE);
 
-    private static Pattern requirePattern3 = Pattern.compile(".*?require\\s*?'(.*?)'" + commentAfterRequire,
+    private static Pattern requirePattern3 = Pattern.compile(".*?require\\s*?'(.*?)'\\s*,*" + commentAfterRequire,
              Pattern.DOTALL | Pattern.MULTILINE);
 
     /**
@@ -36,7 +36,7 @@ public class LuaScanner {
      * 
      * local s = 'require "should_not_match"'
      */
-    private static Pattern requirePattern4 = Pattern.compile(".*?require\\s*?\\(\\s*?'(.*?)'\\s*?\\)" + commentAfterRequire,
+    private static Pattern requirePattern4 = Pattern.compile(".*?require\\s*?\\(\\s*?'(.*?)'\\s*?\\)\\s*,*" + commentAfterRequire,
              Pattern.DOTALL | Pattern.MULTILINE);
 
     private static Pattern propertyDeclPattern = Pattern.compile("go.property\\((.*?)\\);?(\\s*?--.*?)?$");
