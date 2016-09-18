a8544326935b6d0c35df28dee46ee79449e03501
Ragnar Svensson <ragnar.svensson@king.com>
2016-09-18 10:42:04 +0200

    DEFEDIT-428 Optimised ed2 ci build
    * Only download artifacts once
    * Only add required libs per platform
    * Don't build linux32 (not used)

--- editor/scripts/bundle.py
+++ editor/scripts/bundle.py
@@ -117,56 +117,80 @@ def add_native_libs(platform, in_jar_name, out_jar):
                 print "adding", n
                 out_jar.writestr(n, d)
 
-def bundle_natives(in_jar_name, out_jar_name, ext_lib_path):
+def download_artifacts(platforms, ext_lib_path):
+    sha1 = git_sha1()
+    base_url = 'http://d.defold.com/archive/%s/engine' % (sha1)
+
+    artifacts = {'x86_64-darwin' : {'exes' : ['dmengine', 'dmengine_release'],
+                                    'libs' : ['libparticle_shared.dylib', 'libtexc_shared.dylib'],
+                                    'ext_libs' : [] },
+                 'x86-win32' : {'exes' : ['dmengine.exe', 'dmengine_release.exe'],
+                                'libs' : ['particle_shared.dll', 'texc_shared.dll'],
+                                'ext_libs' : ['OpenAL32.dll'] },
+                 'x86-linux' : {'exes' : ['dmengine', 'dmengine_release'],
+                                'libs' : ['libparticle_shared.so', 'libtexc_shared.so'],
+                                'ext_libs' : [] },
+                 'x86_64-linux' : {'exes' : ['dmengine', 'dmengine_release'],
+                                   'libs' : ['libparticle_shared.so', 'libtexc_shared.so'],
+                                   'ext_libs' : [] }}
+
+    for p in artifacts.keys():
+        print('Downloading for %s' % (p))
+        if p in platforms:
+            def download_lib(lib):
+                url = '%s/%s/%s' % (base_url, platform_to_legacy_java[p], lib)
+                return (lib, download(url, use_cache = False))
+            artifacts[p]['libs'] = map(download_lib, artifacts[p]['libs'])
+        else:
+            artifacts[p]['libs'] = []
+
+        def download_exe(exe):
+            url = '%s/%s/%s' % (base_url, platform_to_legacy_java[p], exe)
+            return (exe, download(url, use_cache = False))
+        artifacts[p]['exes'] = map(download_exe, artifacts[p]['exes'])
+
+        def fetch_ext_lib(ext_lib):
+            return (ext_lib, '%s/%s/%s' % (ext_lib_path, platform_to_legacy[p], ext_lib))
+        artifacts[p]['ext_libs'] = map(fetch_ext_lib, artifacts[p]['ext_libs'])
+
+    return artifacts
+
+def unlink_artifacts(artifacts):
+    for p in artifacts.keys():
+        for l in artifacts[p]['libs']:
+            os.unlink(l[1])
+
+        for e in artifacts[p]['exes']:
+            os.unlink(e[1])
+
+def bundle_natives(platform, artifacts, in_jar_name, out_jar_name):
     with zipfile.ZipFile(out_jar_name, 'w') as out_jar:
-        for platform in ['x86-linux', 'x86_64-linux', 'x86_64-darwin', 'x86-win32']:
-            jogl_platform = platform_to_jogl[platform]
-            add_native_libs(platform, os.path.expanduser("~/.m2/repository/org/jogamp/jogl/jogl-all/%s/jogl-all-%s-natives-%s.jar" % (JOGL_VERSION, JOGL_VERSION, jogl_platform)), out_jar)
-            add_native_libs(platform, os.path.expanduser("~/.m2/repository/org/jogamp/gluegen/gluegen-rt/%s/gluegen-rt-%s-natives-%s.jar" % (JOGL_VERSION, JOGL_VERSION, jogl_platform)), out_jar)
+
+        jogl_platform = platform_to_jogl[platform]
+        add_native_libs(platform, os.path.expanduser("~/.m2/repository/org/jogamp/jogl/jogl-all/%s/jogl-all-%s-natives-%s.jar" % (JOGL_VERSION, JOGL_VERSION, jogl_platform)), out_jar)
+        add_native_libs(platform, os.path.expanduser("~/.m2/repository/org/jogamp/gluegen/gluegen-rt/%s/gluegen-rt-%s-natives-%s.jar" % (JOGL_VERSION, JOGL_VERSION, jogl_platform)), out_jar)
 
         with zipfile.ZipFile(os.path.expanduser("~/.m2/repository/net/java/dev/jna/jna/%s/jna-%s.jar" % (JNA_VERSION, JNA_VERSION)), 'r') as in_jar:
             for jna_p, p, lib in [("darwin", "x86_64-darwin", "libjnidispatch.jnilib"),
                                   ("win32-x86", "x86-win32", "jnidispatch.dll"),
                                   ("linux-x86", "x86-linux", "libjnidispatch.so"),
                                   ("linux-x86-64", "x86_64-linux", "libjnidispatch.so")]:
-                d = in_jar.read('com/sun/jna/%s/%s' % (jna_p, lib))
-                n = 'lib/%s/%s' % (p, lib)
-                print "adding", n
-                out_jar.writestr(n, d)
-
-        sha1 = git_sha1()
-
-        artifacts = {'x86_64-darwin' : {'exes' : ['dmengine', 'dmengine_release'],
-                                        'libs' : ['libparticle_shared.dylib', 'libtexc_shared.dylib'],
-                                        'ext_libs' : [] },
-                     'x86-win32' : {'exes' : ['dmengine.exe', 'dmengine_release.exe'],
-                                    'libs' : ['particle_shared.dll', 'texc_shared.dll'],
-                                    'ext_libs' : ['OpenAL32.dll'] },
-                     'x86-linux' : {'exes' : ['dmengine', 'dmengine_release'],
-                                    'libs' : ['libparticle_shared.so', 'libtexc_shared.so'],
-                                    'ext_libs' : [] },
-                     'x86_64-linux' : {'exes' : ['dmengine', 'dmengine_release'],
-                                       'libs' : ['libparticle_shared.so', 'libtexc_shared.so'],
-                                       'ext_libs' : [] }}
-
-        base_url = 'http://d.defold.com/archive/%s/engine' % (sha1)
+                if p == platform:
+                    d = in_jar.read('com/sun/jna/%s/%s' % (jna_p, lib))
+                    n = 'lib/%s/%s' % (p, lib)
+                    print "adding", n
+                    out_jar.writestr(n, d)
 
         for p in artifacts.keys():
-            for l in artifacts[p]['libs']:
-                url = '%s/%s/%s' % (base_url, platform_to_legacy_java[p], l)
-                path = download(url, use_cache = False)
-                out_jar.write(path, 'lib/%s/%s' % (p, l))
-                os.unlink(path)
+            if platform == p:
+                for l in artifacts[p]['libs']:
+                    out_jar.write(l[1], 'lib/%s/%s' % (p, l[0]))
 
             for e in artifacts[p]['exes']:
-                url = '%s/%s/%s' % (base_url, platform_to_legacy_java[p], e)
-                path = download(url, use_cache = False)
-                out_jar.write(path, 'libexec/%s/%s' % (p, e))
-                os.unlink(path)
+                out_jar.write(e[1], 'libexec/%s/%s' % (p, e[0]))
 
             for e in artifacts[p]['ext_libs']:
-                path = '%s/%s/%s' % (ext_lib_path, platform_to_legacy[p], e)
-                out_jar.write(path, 'libexec/%s/%s' % (p, e))
+                out_jar.write(e[1], 'libexec/%s/%s' % (p, e[0]))
 
         with zipfile.ZipFile(in_jar_name, 'r') as in_jar:
             for zi in in_jar.infolist():
@@ -187,7 +211,7 @@ def git_sha1(ref = None):
     sha1 = line.split()[0]
     return sha1
 
-def bundle(platform, options):
+def bundle(platform, artifacts, options):
     if os.path.exists('tmp'):
         shutil.rmtree('tmp')
 
@@ -253,7 +277,7 @@ def bundle(platform, options):
         config.write(f)
 
     jar_file = 'defold-%s.jar' % sha1
-    bundle_natives('target/defold-editor-2.0.0-SNAPSHOT-standalone.jar', '%s/%s' % (packages_dir, jar_file), options.ext_lib_path)
+    bundle_natives(platform, artifacts, 'target/defold-editor-2.0.0-SNAPSHOT-standalone.jar', '%s/%s' % (packages_dir, jar_file))
     shutil.copy('%s/%s' % (packages_dir, jar_file), 'target/editor/update/%s' % jar_file)
     shutil.copy(launcher, '%s/Defold%s' % (exe_dir, exe_suffix))
     exec_command('chmod +x %s/Defold%s' % (exe_dir, exe_suffix))
@@ -311,8 +335,10 @@ if __name__ == '__main__':
     exec_command('./scripts/lein clean')
     exec_command('./scripts/lein with-profile +release uberjar')
 
+    artifacts = download_artifacts(options.target_platform, options.ext_lib_path)
     for platform in options.target_platform:
-        bundle(platform, options)
+        bundle(platform, artifacts, options)
+    unlink_artifacts(artifacts)
 
     sha1 = git_sha1()
     package_info = {'version' : options.version,
