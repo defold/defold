45226936b1760ae298aaa855758ee780f1244e42
Mats Gisselson <mats.gisselson@king.com>
2016-11-07 18:02:13 +0100

    DEFEDIT-539 Updated label-scene test.

--- editor/test/integration/label_test.clj
+++ editor/test/integration/label_test.clj
@@ -5,7 +5,8 @@
             [editor.math :as math]
             [editor.workspace :as workspace]
             [integration.test-util :as test-util]
-            [support.test-support :refer [with-clean-system]]))
+            [support.test-support :refer [with-clean-system]]
+            [clojure.string :as string]))
 
 (deftest label-validation
   (with-clean-system
@@ -18,8 +19,8 @@
                                         "unknown material" "/materials/unknown.material"}]]
               [case path] cases]
         (testing case
-                (test-util/with-prop [node-id prop (workspace/resolve-workspace-resource workspace path)]
-                  (is (g/error? (test-util/prop-error node-id prop)))))))))
+          (test-util/with-prop [node-id prop (workspace/resolve-workspace-resource workspace path)]
+                               (is (g/error? (test-util/prop-error node-id prop)))))))))
 
 (deftest label-aabb
   (with-clean-system
@@ -31,3 +32,18 @@
         (is (< 0.0 x))
         (is (< 0.0 y))
         (is (= 0.0 z))))))
+
+(deftest label-scene
+  (with-clean-system
+    (let [workspace (test-util/setup-workspace! world)
+          project (test-util/setup-project! workspace)
+          node-id (project/get-resource-node project "/label/test.label")]
+      (let [scene (g/node-value node-id :scene)
+            aabb (g/node-value node-id :aabb)]
+        (is (= aabb (:aabb scene)))
+        (is (= node-id (:node-id scene)))
+        (is (= node-id (some-> scene :renderable :select-batch-key)))
+        (is (= :blend-mode-alpha (some-> scene :renderable :batch-key :blend-mode)))
+        (is (= "Label" (some-> scene :renderable :user-data :text-data :text-layout :lines first)))
+        (is (string/includes? (some-> scene :renderable :user-data :material-shader :verts) "gl_Position"))
+        (is (string/includes? (some-> scene :renderable :user-data :material-shader :frags) "gl_FragColor"))))))
