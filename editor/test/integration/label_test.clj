afa7e1bc15b828c2a776f95baa82e7584dbf8d62
Mats Gisselson <mats.gisselson@king.com>
2016-11-10 10:47:35 +0100

    DEFEDIT-539 Added render batching tests for label component.

--- editor/test/integration/label_test.clj
+++ editor/test/integration/label_test.clj
@@ -1,12 +1,16 @@
 (ns integration.label-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.string :as string]
+            [clojure.test :refer :all]
             [dynamo.graph :as g]
             [editor.defold-project :as project]
+            [editor.game-object :as game-object]
+            [editor.gl.pass :as pass]
+            [editor.label :as label]
             [editor.math :as math]
+            [editor.scene :as scene]
             [editor.workspace :as workspace]
             [integration.test-util :as test-util]
-            [support.test-support :refer [with-clean-system]]
-            [clojure.string :as string]))
+            [support.test-support :refer [with-clean-system]]))
 
 (deftest label-validation
   (with-clean-system
@@ -47,3 +51,97 @@
         (is (= "Label" (some-> scene :renderable :user-data :text-data :text-layout :lines first)))
         (is (string/includes? (some-> scene :renderable :user-data :material-shader :verts) "gl_Position"))
         (is (string/includes? (some-> scene :renderable :user-data :material-shader :frags) "gl_FragColor"))))))
+
+(defn- get-render-calls-by-pass
+  [scene camera selection key-fn]
+  (let [render-data (scene/produce-render-data scene selection [] camera)
+        renderables (:renderables render-data)]
+    (into {}
+          (keep (fn [pass]
+                  (let [calls (test-util/with-logged-calls [label/render-lines label/render-tris]
+                                (scene/batch-render nil {:pass pass} (get renderables pass) false key-fn))]
+                    (when (seq calls)
+                      [pass calls]))))
+          pass/render-passes)))
+
+(defn- map-render-calls
+  [transform-calls-fn render-calls-by-pass]
+  (into {}
+        (map (fn [[pass calls-by-fn]]
+               [pass (into {}
+                           (map (fn [[fn calls]]
+                                  [fn (transform-calls-fn calls)]))
+                           calls-by-fn)]))
+        render-calls-by-pass))
+
+(deftest label-batch-render
+  (with-clean-system
+    (let [workspace (test-util/setup-workspace! world)
+          project (test-util/setup-project! workspace)
+          go (project/get-resource-node project "/game_object/test.go")
+          add-label-component! (let [resource-type (workspace/get-resource-type workspace "label")]
+                                 (fn [go]
+                                   (game-object/add-embedded-component-handler {:_node-id go :resource-type resource-type})
+                                   (first (test-util/selection project))))
+          render-calls (let [app-view (test-util/setup-app-view!)
+                             view (test-util/open-scene-view! project app-view go 128 128)]
+                         (fn [selection key-fn]
+                           (get-render-calls-by-pass (g/node-value go :scene)
+                                                     (g/node-value view :camera)
+                                                     selection
+                                                     key-fn)))
+          render-call-counts (comp (partial map-render-calls count)
+                                   render-calls)]
+
+      (testing "Single label"
+        (with-open [_ (test-util/make-graph-reverter (project/graph project))]
+          (add-label-component! go)
+          (is (= {pass/outline {label/render-lines 1}
+                  pass/transparent {label/render-tris 1}}
+                 (render-call-counts #{} :batch-key)))
+          (is (= {pass/outline {label/render-lines 1}
+                  pass/transparent {label/render-tris 1}}
+                 (render-call-counts #{} :select-batch-key)))))
+
+      (testing "Identical labels"
+        (with-open [_ (test-util/make-graph-reverter (project/graph project))]
+          (add-label-component! go)
+          (add-label-component! go)
+          (add-label-component! go)
+          (add-label-component! go)
+          (is (= {pass/outline {label/render-lines 1}
+                  pass/transparent {label/render-tris 1}}
+                 (render-call-counts #{} :batch-key)))
+          (is (= {pass/outline {label/render-lines 4}
+                  pass/transparent {label/render-tris 4}}
+                 (render-call-counts #{} :select-batch-key)))))
+
+      (testing "Blend mode differs"
+        (with-open [_ (test-util/make-graph-reverter (project/graph project))]
+          (test-util/prop! (add-label-component! go) :blend-mode :blend-mode-add)
+          (test-util/prop! (add-label-component! go) :blend-mode :blend-mode-add)
+          (test-util/prop! (add-label-component! go) :blend-mode :blend-mode-mult)
+          (test-util/prop! (add-label-component! go) :blend-mode :blend-mode-mult)
+          (is (= {pass/outline {label/render-lines 2}
+                  pass/transparent {label/render-tris 2}}
+                 (render-call-counts #{} :batch-key)))))
+
+      (testing "Font differs"
+        (with-open [_ (test-util/make-graph-reverter (project/graph project))]
+          (test-util/prop! (add-label-component! go) :font (workspace/find-resource workspace "/fonts/active_menu_item.font"))
+          (test-util/prop! (add-label-component! go) :font (workspace/find-resource workspace "/fonts/active_menu_item.font"))
+          (test-util/prop! (add-label-component! go) :font (workspace/find-resource workspace "/fonts/big_score.font"))
+          (test-util/prop! (add-label-component! go) :font (workspace/find-resource workspace "/fonts/big_score.font"))
+          (is (= {pass/outline {label/render-lines 2}
+                  pass/transparent {label/render-tris 2}}
+                 (render-call-counts #{} :batch-key)))))
+
+      (testing "Material differs"
+        (with-open [_ (test-util/make-graph-reverter (project/graph project))]
+          (test-util/prop! (add-label-component! go) :material (workspace/find-resource workspace "/fonts/active_menu_item.material"))
+          (test-util/prop! (add-label-component! go) :material (workspace/find-resource workspace "/fonts/active_menu_item.material"))
+          (test-util/prop! (add-label-component! go) :material (workspace/find-resource workspace "/fonts/big_score_font.material"))
+          (test-util/prop! (add-label-component! go) :material (workspace/find-resource workspace "/fonts/big_score_font.material"))
+          (is (= {pass/outline {label/render-lines 2}
+                  pass/transparent {label/render-tris 2}}
+                 (render-call-counts #{} :batch-key))))))))
