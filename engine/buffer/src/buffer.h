54a0b7592dc3f66bf8889c9851ff4479b11613bf
Sven Andersson <sven.andersson@king.com>
2016-11-17 20:06:24 +0100

    Initial implementation of buffer API - WIP.

---
+++ engine/buffer/src/buffer.h
@@ -0,0 +1,101 @@
+#ifndef DM_BUFFER
+#define DM_BUFFER
+
+#include <stdint.h>
+
+#include <dlib/hash.h>
+#include <dlib/align.h>
+
+namespace dmBuffer
+{
+    typedef struct Buffer* HBuffer;
+    typedef struct StreamDeclaration* BufferDeclaration;
+
+    enum ValueType
+    {
+        BUFFER_TYPE_UINT8,
+        BUFFER_TYPE_UINT16,
+        BUFFER_TYPE_UINT32,
+        BUFFER_TYPE_UINT64,
+
+        BUFFER_TYPE_INT8,
+        BUFFER_TYPE_INT16,
+        BUFFER_TYPE_INT32,
+        BUFFER_TYPE_INT64,
+
+        BUFFER_TYPE_FLOAT32,
+        BUFFER_TYPE_FLOAT64,
+    };
+
+    enum Result
+    {
+        RESULT_OK,
+
+        RESULT_GUARD_INVALID,
+
+        RESULT_ALLOCATION_ERROR,
+
+        RESULT_BUFFER_SIZE_ERROR,
+        RESULT_STREAM_SIZE_ERROR,
+
+        RESULT_STREAM_DOESNT_EXIST,
+
+        RESULT_STREAM_WRONG_TYPE,
+        RESULT_STREAM_WRONG_COUNT
+    };
+
+    struct StreamDeclaration
+    {
+        dmhash_t  m_Name;
+        ValueType m_ValueType;
+        uint8_t   m_ValueCount;
+    };
+
+    struct Buffer
+    {
+        struct Stream
+        {
+            dmhash_t  m_Name;
+            uint32_t  m_Offset; // ~4gb addressable space
+            // uint32_t  m_End;
+            ValueType m_ValueType;
+            uint8_t   m_ValueCount;
+        };
+
+        uint8_t  m_NumStreams;
+        Stream*  m_Streams;
+        uint32_t m_NumElements; // What size is the buffer?
+        void*    m_Data; // All data, including guard bytes. 16 byte aligned
+    };
+
+    /**
+     * Allocate a new Buffer with a number of different streams
+     * @param num_elements The number of elements the buffer should hold. Total byte size will be (num_elements * ( SizeOf(num_decl, decl) + guard bytes) )
+     * @param buffer_decl Array of stream declarations
+     * @param buffer_decl_count Number of stream declarations inside the decl array
+     * @param out_buffer Pointer to HBuffer where to store the newly allocated buffer
+     * @return Returns BUFFER_OK if buffer was allocated successfully
+     */
+    Result Allocate(uint32_t num_elements, const BufferDeclaration buffer_decl, uint8_t buffer_decl_count, HBuffer* out_buffer);
+
+    /**
+     * Frees a Buffer
+     * @param buffer Pointer to the buffer to free
+     */
+    void Free(HBuffer buffer);
+
+    /**
+     * Get a Stream from a Buffer
+     * @param buffer Pointer to a buffer
+     * @param stream_name Hash of stream name to get
+     * @param type Value type of expected stream
+     * @param type_count Count of values per entry in stream
+     * @param out_stream Pointer to void* where to store the stream
+     * @param out_stride Pointer to uint32_t where to store the stride
+     * @param out_element_count Pointer to uint32_t where to store the element count
+     * @return Returns BUFFER_OK if the stream was successfully accessed
+     */
+    Result GetStream(HBuffer buffer, dmhash_t stream_name, ValueType type, uint32_t type_count, void** out_stream, uint32_t* out_stride, uint32_t* out_element_count);
+
+}
+#endif
---
+++ engine/buffer/src/test/test_buffer.cpp
@@ -0,0 +1,202 @@
+#include <gtest/gtest.h>
+#include <dlib/log.h>
+#include <dlib/hash.h>
+
+#include "../buffer.h"
+
+#define RIG_EPSILON 0.0001f
+
+class BufferTest : public ::testing::Test
+{
+
+};
+
+class GetDataTest : public ::testing::Test
+{
+public:
+    dmBuffer::HBuffer buffer = 0x0;
+
+
+protected:
+    virtual void SetUp() {
+        dmBuffer::StreamDeclaration buffer_decl[] = {
+            {dmHashString64("position"), dmBuffer::BUFFER_TYPE_FLOAT32, 3},
+            {dmHashString64("texcoord"), dmBuffer::BUFFER_TYPE_UINT16, 2}
+        };
+
+        dmBuffer::Result r = dmBuffer::Allocate(4, buffer_decl, 2, &buffer);
+        ASSERT_EQ(dmBuffer::RESULT_OK, r);
+    }
+
+    virtual void TearDown() {
+        dmBuffer::Free(buffer);
+    }
+};
+
+TEST_F(BufferTest, InvalidAllocation)
+{
+    dmBuffer::HBuffer buffer = 0x0;
+    dmBuffer::BufferDeclaration buffer_decl_empty = {0};
+    dmBuffer::StreamDeclaration buffer_decl_filled[] = {
+        {dmHashString64("dummy1"), dmBuffer::BUFFER_TYPE_UINT8, 3},
+        {dmHashString64("dummy2"), dmBuffer::BUFFER_TYPE_UINT8, 1}
+    };
+
+    // Empty declaration
+    dmBuffer::Result r = dmBuffer::Allocate(0, buffer_decl_empty, 0, &buffer);
+    ASSERT_NE(dmBuffer::RESULT_OK, r);
+    ASSERT_EQ(0x0, buffer);
+
+    // Valid declaration, zero elements
+    r = dmBuffer::Allocate(0, buffer_decl_filled, 2, &buffer);
+    ASSERT_NE(dmBuffer::RESULT_OK, r);
+    ASSERT_EQ(0x0, buffer);
+
+    // Empty declaration, 1 element
+    r = dmBuffer::Allocate(1, buffer_decl_empty, 0, &buffer);
+    ASSERT_NE(dmBuffer::RESULT_OK, r);
+    ASSERT_EQ(0x0, buffer);
+}
+
+TEST_F(BufferTest, ValidAllocation)
+{
+    dmBuffer::HBuffer buffer = 0x0;
+    dmBuffer::StreamDeclaration buffer_decl[] = {
+        {dmHashString64("position"), dmBuffer::BUFFER_TYPE_UINT8, 3},
+        {dmHashString64("texcoord"), dmBuffer::BUFFER_TYPE_UINT8, 1}
+    };
+
+    // Declaration of 2 streams, with 4 elements
+    dmBuffer::Result r = dmBuffer::Allocate(4, buffer_decl, 2, &buffer);
+    ASSERT_EQ(dmBuffer::RESULT_OK, r);
+    ASSERT_NE((void*)0x0, buffer);
+    dmBuffer::Free(buffer);
+    buffer = 0x0;
+
+    // Allocation with only the first stream declaration
+    r = dmBuffer::Allocate(4, buffer_decl, 1, &buffer);
+    ASSERT_EQ(dmBuffer::RESULT_OK, r);
+    ASSERT_NE((void*)0x0, buffer);
+    dmBuffer::Free(buffer);
+    buffer = 0x0;
+
+    // Larger allocation
+    r = dmBuffer::Allocate(1024*1024, buffer_decl, 2, &buffer);
+    ASSERT_EQ(dmBuffer::RESULT_OK, r);
+    ASSERT_NE((void*)0x0, buffer);
+    dmBuffer::Free(buffer);
+}
+
+TEST_F(BufferTest, InvalidGetData)
+{
+    dmBuffer::HBuffer buffer = 0x0;
+    dmBuffer::StreamDeclaration buffer_decl[] = {
+        {dmHashString64("position"), dmBuffer::BUFFER_TYPE_FLOAT32, 3},
+        {dmHashString64("texcoord"), dmBuffer::BUFFER_TYPE_UINT16, 2}
+    };
+
+    void *out_stream = 0x0;
+    uint32_t out_stride = 0;
+    uint32_t out_element_count = 0;
+
+    dmBuffer::Result r = dmBuffer::Allocate(4, buffer_decl, 2, &buffer);
+    ASSERT_EQ(dmBuffer::RESULT_OK, r);
+
+    // Get unknown stream
+    r = dmBuffer::GetStream(buffer, dmHashString64("unknown"), dmBuffer::BUFFER_TYPE_UINT8, 2, &out_stream, &out_stride, &out_element_count);
+    ASSERT_EQ(dmBuffer::RESULT_STREAM_DOESNT_EXIST, r);
+    ASSERT_EQ(0x0, out_stream);
+    ASSERT_EQ(0, out_stride);
+    ASSERT_EQ(0, out_element_count);
+
+    // Correct stream, but wrong type
+    r = dmBuffer::GetStream(buffer, dmHashString64("position"), dmBuffer::BUFFER_TYPE_UINT16, 3, &out_stream, &out_stride, &out_element_count);
+    ASSERT_EQ(dmBuffer::RESULT_STREAM_WRONG_TYPE, r);
+    ASSERT_EQ(0x0, out_stream);
+    ASSERT_EQ(0, out_stride);
+    ASSERT_EQ(0, out_element_count);
+
+    // Correct stream, but wrong value count
+    r = dmBuffer::GetStream(buffer, dmHashString64("texcoord"), dmBuffer::BUFFER_TYPE_UINT16, 8, &out_stream, &out_stride, &out_element_count);
+    ASSERT_EQ(dmBuffer::RESULT_STREAM_WRONG_COUNT, r);
+    ASSERT_EQ(0x0, out_stream);
+    ASSERT_EQ(0, out_stride);
+    ASSERT_EQ(0, out_element_count);
+
+    dmBuffer::Free(buffer);
+}
+
+TEST_F(GetDataTest, ValidGetData)
+{
+    void *out_stream = 0x0;
+    uint32_t out_stride = 0;
+    uint32_t out_element_count = 0;
+
+    // Get texcoord stream
+    dmBuffer::Result r = dmBuffer::GetStream(buffer, dmHashString64("texcoord"), dmBuffer::BUFFER_TYPE_UINT16, 2, &out_stream, &out_stride, &out_element_count);
+    ASSERT_EQ(dmBuffer::RESULT_OK, r);
+    ASSERT_NE((void*)0x0, out_stream);
+    ASSERT_EQ(2*sizeof(uint16_t), out_stride);
+    ASSERT_EQ(4, out_element_count);
+
+    uint16_t* ptr = (uint16_t*)out_stream;
+    for (int i = 0; i < out_element_count; ++i)
+    {
+        ptr[0] = i;
+        ptr[1] = i;
+        ptr = (uint16_t*)((uintptr_t)ptr + out_stride);
+    }
+
+    // Get stream again
+    r = dmBuffer::GetStream(buffer, dmHashString64("texcoord"), dmBuffer::BUFFER_TYPE_UINT16, 2, &out_stream, &out_stride, &out_element_count);
+    ASSERT_EQ(dmBuffer::RESULT_OK, r);
+    ASSERT_NE((void*)0x0, out_stream);
+    ASSERT_EQ(2*sizeof(uint16_t), out_stride);
+    ASSERT_EQ(4, out_element_count);
+
+    // Verify the previously written data
+    ptr = (uint16_t*)out_stream;
+    for (int i = 0; i < out_element_count; ++i)
+    {
+        ASSERT_EQ(i, ptr[0]);
+        ASSERT_EQ(i, ptr[1]);
+        ptr = (uint16_t*)((uintptr_t)ptr + out_stride);
+    }
+}
+
+TEST_F(GetDataTest, WriteOutsideStream)
+{
+    void *out_stream = 0x0;
+    uint32_t out_stride = 0;
+    uint32_t out_element_count = 0;
+
+    // Get texcoord stream
+    dmBuffer::Result r = dmBuffer::GetStream(buffer, dmHashString64("texcoord"), dmBuffer::BUFFER_TYPE_UINT16, 2, &out_stream, &out_stride, &out_element_count);
+    ASSERT_EQ(dmBuffer::RESULT_OK, r);
+    ASSERT_NE((void*)0x0, out_stream);
+    ASSERT_EQ(2*sizeof(uint16_t), out_stride);
+    ASSERT_EQ(4, out_element_count);
+
+    // Write past the number of elements
+    out_element_count = out_element_count + 3;
+    uint16_t* ptr = (uint16_t*)out_stream;
+    for (int i = 0; i < out_element_count; ++i)
+    {
+        ptr[0] = i;
+        ptr[1] = i;
+        ptr = (uint16_t*)((uintptr_t)ptr + out_stride);
+    }
+
+    // Get stream again, expecting invalid guard
+    r = dmBuffer::GetStream(buffer, dmHashString64("texcoord"), dmBuffer::BUFFER_TYPE_UINT16, 2, &out_stream, &out_stride, &out_element_count);
+    ASSERT_EQ(dmBuffer::RESULT_GUARD_INVALID, r);
+    ASSERT_NE((void*)0x0, out_stream);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+
+    int ret = RUN_ALL_TESTS();
+    return ret;
+}
