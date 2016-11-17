ecde20e6252ecd507e0817ffde5ecb5a20d847a5
Ragnar Dahlén <ragnar.dahlen@king.com>
2016-11-17 09:13:17 +0100

    DEFEDIT-469 Handle differences between JDK versions better

--- editor/src/clj/editor/ui/tree_view_hack.clj
+++ editor/src/clj/editor/ui/tree_view_hack.clj
@@ -1,14 +1,7 @@
 (ns editor.ui.tree-view-hack
-  ;; NOTE: The ReadOnlyUnbackedObservableList implementation used by
-  ;; the TreeView selection model is an anonymous inner class in JDK
-  ;; 1.8.0_101, and a named class in 1.8.0_112. If/when we upgrade we
-  ;; need to change the class name.
   (:import
    (javafx.scene.control SelectionModel
-                         MultipleSelectionModelBase
-                         MultipleSelectionModelBase$4
-                         ;; 1.8.0_111/112: MultipleSelectionModelBase$BitSetReadOnlyUnbackedObservableList
-                         )))
+                         MultipleSelectionModelBase)))
 
 (set! *warn-on-reflection* true)
 
@@ -25,21 +18,46 @@
 ;; the state of those fields to the initial state, thereby disabling
 ;; the broken performance optimizations.
 
+;; The ReadOnlyUnbackedObservableList implementation used by the
+;; TreeView selection model is an anonymous inner class in JDK
+;; 1.8.0_101, and a named class in 1.8.0_111. If/when we upgrade we
+;; need to change the class name.
+
+(defmacro swallow [type & body]
+  `(try ~@body (catch ~type ~'_)))
+
+(def ^Class observable-list-class
+  (or
+    ;; JDK8u60-102
+    (swallow ClassNotFoundException   
+             (import 'javafx.scene.control.MultipleSelectionModelBase$4)
+             (Class/forName "javafx.scene.control.MultipleSelectionModelBase$4"))
+    ;; JDK8u111+
+    (swallow ClassNotFoundException
+             (import 'javafx.scene.control.MultipleSelectionModelBase$BitSetReadOnlyUnbackedObservableList)
+             (Class/forName "javafx.scene.control.MultipleSelectionModelBase$BitSetReadOnlyUnbackedObservableList"))))
+
 (def ^java.lang.reflect.Field selected-indices-seq-field
-  (doto (.getDeclaredField MultipleSelectionModelBase "selectedIndicesSeq")
-    (.setAccessible true)))
+  (swallow Throwable
+           (doto (.getDeclaredField MultipleSelectionModelBase "selectedIndicesSeq")
+             (.setAccessible true))))
 
 (def ^java.lang.reflect.Field last-get-index-field
-   (doto (.getDeclaredField MultipleSelectionModelBase$4 "lastGetIndex")
-    (.setAccessible true)))
+  (swallow Throwable
+           (doto (.getDeclaredField observable-list-class "lastGetIndex")
+             (.setAccessible true))))
 
 (def ^java.lang.reflect.Field last-get-value-field
-  (doto (.getDeclaredField MultipleSelectionModelBase$4 "lastGetValue")
-    (.setAccessible true)))
+  (swallow Throwable
+           (doto (.getDeclaredField observable-list-class "lastGetValue")
+             (.setAccessible true))))
 
 (defn subvert-broken-selection-model-optimization!
   [^SelectionModel selection-model]
-  (when (.isAssignableFrom MultipleSelectionModelBase (.getClass selection-model))
+  (when (and (.isAssignableFrom MultipleSelectionModelBase (.getClass selection-model))
+             selected-indices-seq-field
+             last-get-index-field
+             last-get-value-field)
     (let [indices-seq (.get selected-indices-seq-field selection-model)]
       (.set last-get-index-field indices-seq (int -1))
       (.set last-get-value-field indices-seq (int -1)))))
