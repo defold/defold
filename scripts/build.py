786c715fd528114bb75ab1a5cb4d2cfee026d5b4
Mathias Westerdahl <mathias.westerdahl@king.com>
2016-11-17 10:45:30 +0100

    First test (to get a unique sha1)

--- scripts/build.py
+++ scripts/build.py
@@ -1170,6 +1170,17 @@ instructions.configure=\
         self.s3buckets[bucket_name] = bucket
         return bucket
 
+    def mawe_test(self):
+        if 'android' in self.target_platform:
+            files = [
+                ('share/java', 'classes.dex'),
+                ('bin/%s' % (self.target_platform), 'dmengine.apk'),
+                ('ext/share/java', 'android.jar'),
+            ]
+            for f in files:
+                src = join(dynamo_home, f[0], f[1])
+                self.upload_file(src, '%s/%s' % (full_archive_path, f[1]))
+
     def upload_file(self, path, url):
         url = url.replace('\\', '/')
         self._log('Uploading %s -> %s' % (path, url))
@@ -1202,7 +1213,17 @@ instructions.configure=\
                 self._log('Uploaded %s -> %s' % (path, url))
 
             def upload_multipart():
-                mp = bucket.initiate_multipart_upload(p)
+                contenttypes = { '.apk': 'application/vnd.android.package-archive',
+                                 '.jar': 'application/java-archive',
+                                 '.dex': 'application/octet-stream',
+                                 '.js':  'application/javascript'
+                                }
+                headers = {}
+                contenttype = contenttypes.get(os.path.splitext(path)[1], None)
+                if contenttype is not None:
+                    headers['Content-Type'] = contenttype
+
+                mp = bucket.initiate_multipart_upload(p, headers=headers)
 
                 source_size = os.stat(path).st_size
                 chunksize = 64 * 1024 * 1024 # 64 MiB
