81d11cdb72c1bc420959531b982c45b2d84b42e0
Jakob Pogulis <jakob.pogulis@king.com>
2016-12-05 10:40:24 +0100

    DEF-2331: Input devices sometimes registered as an invalid Gamepad

--- engine/hid/src/hid_glfw.cpp
+++ engine/hid/src/hid_glfw.cpp
@@ -145,7 +145,7 @@ namespace dmHID
                 {
                     GamepadPacket& packet = pad->m_Packet;
                     pad->m_AxisCount = glfwGetJoystickParam(glfw_joystick, GLFW_AXES);
-                    pad->m_ButtonCount = glfwGetJoystickParam(glfw_joystick, GLFW_BUTTONS);
+                    pad->m_ButtonCount = dmMath::Min(MAX_GAMEPAD_BUTTON_COUNT, glfwGetJoystickParam(glfw_joystick, GLFW_BUTTONS));
                     glfwGetJoystickPos(glfw_joystick, packet.m_Axis, pad->m_AxisCount);
                     unsigned char buttons[MAX_GAMEPAD_BUTTON_COUNT];
                     glfwGetJoystickButtons(glfw_joystick, buttons, pad->m_ButtonCount);
