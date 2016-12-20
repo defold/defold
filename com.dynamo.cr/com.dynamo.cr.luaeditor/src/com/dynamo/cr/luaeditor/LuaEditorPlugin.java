2e8ca366f1448449eb220b5bc4fe0235cc57dda8
Mathias Westerdahl <mathias.westerdahl@king.com>
2016-12-20 14:55:10 +0100

    DEF-2374 Added auto completion for label module (#1166)

--- com.dynamo.cr/com.dynamo.cr.luaeditor/src/com/dynamo/cr/luaeditor/LuaEditorPlugin.java
+++ com.dynamo.cr/com.dynamo.cr.luaeditor/src/com/dynamo/cr/luaeditor/LuaEditorPlugin.java
@@ -90,6 +90,7 @@ public class LuaEditorPlugin extends AbstractUIPlugin {
                 "iac",
                 "image",
                 "json",
+                "label",
                 "msg",
                 "particlefx",
                 "push",
