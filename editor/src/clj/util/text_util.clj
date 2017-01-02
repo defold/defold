968c9cd83fb53dcea0c90d99b94a333f4d082be3
Mats Gisselson <mats.gisselson@king.com>
2017-01-02 15:50:47 +0100

    DEFEDIT-644 Added save-respects-line-endings test

--- editor/src/clj/util/text_util.clj
+++ editor/src/clj/util/text_util.clj
@@ -18,26 +18,63 @@
   (when (some? reader)
     (with-open [_ reader]
       (loop [ch (.read reader)
-             cr-count 0
-             lf-count 0]
+             prev-ch nil
+             lf-count 0
+             crlf-count 0]
         (cond
           ; If we've reached the end or seen enough, return result.
-          (or (= -1 ch) (< 250 (max cr-count lf-count)))
-          (if (> cr-count (/ lf-count 2))
-            :crlf
-            :lf)
+          (or (= -1 ch) (< 250 (max lf-count crlf-count)))
+          (cond
+            (= 0 lf-count crlf-count) nil
+            (> lf-count crlf-count) :lf
+            :else :crlf)
 
-          (= 0x0D ch) ; CR - count and recur.
-          (recur (.read reader) (inc cr-count) lf-count)
-
-          (= 0x0A ch) ; LF - count and recur.
-          (recur (.read reader) cr-count (inc lf-count))
+          (= 0x0A ch) ; LF character.
+          (if (= 0x0D prev-ch) ; Previous was CR character.
+            (recur (.read reader) ch lf-count (inc crlf-count))
+            (recur (.read reader) ch (inc lf-count) crlf-count))
 
           (not (text-char? (char ch))) ; This looks like binary! Return nil.
           nil
 
           :else
-          (recur (.read reader) cr-count lf-count))))))
+          (recur (.read reader) ch lf-count crlf-count))))))
+
+(defn scan-line-endings
+  "Reads character from the supplied fresh reader until it reaches the end of
+  the file, finds mixed line endings, or detects that the file is binary. For a
+  text file with consistent line endings, it will read the entire file. Returns
+  the type of line ending used in the file, or :mixed if the file mixes line
+  endings. If reader is nil, does not contain line endings, or the data looks
+  like it might be a binary file, returns nil. Will close the reader when done."
+  [^Reader reader]
+  (when (some? reader)
+    (with-open [_ reader]
+      (loop [ch (.read reader)
+             prev-ch nil
+             line-ending nil]
+        (cond
+          (= -1 ch) ; We've reached the end, return result.
+          line-ending
+
+          (= 0x0A ch) ; LF character.
+          (cond
+            (= 0x0D prev-ch) ; Previous was CR character.
+            (if (= :lf line-ending)
+              :mixed
+              (recur (.read reader) ch :crlf))
+
+            (= :crlf line-ending)
+            :mixed
+
+            :else
+            (recur (.read reader) ch :lf))
+
+          (not (text-char? (char ch))) ; This looks like binary! Return nil.
+          nil
+
+          :else
+          (recur (.read reader) ch line-ending))))))
 
 (defn lf->crlf
   "Converts all LF line endings in the input string to CRLF.
