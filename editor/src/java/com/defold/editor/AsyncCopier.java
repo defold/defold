9d64ff1251334b2e6fd5d689451401449540c730
Ragnar Svensson <ragnar.svensson@king.com>
2017-01-11 15:13:49 +0100

    DEFEDIT-678 Fixed lazy rendering
    * App is only re-rendered when there is something to re-render (like before, but with the working async pbos in place)
    * More similar to classic game engine input/update/render loop
    * Removed duplicated code from curve-view, now more similar to scene

--- editor/src/java/com/defold/editor/AsyncCopier.java
+++ editor/src/java/com/defold/editor/AsyncCopier.java
@@ -12,8 +12,6 @@ import javafx.scene.image.WritableImage;
 
 public class AsyncCopier {
 
-    // Amount of PBOs to copy between, one should be enough
-    private static final int N_BUFFERS = 1;
     // NOTE setting N_IMAGES to 1 puts the application in 30 fps
     // [Ragnar] I believe this is due to writing to the same WritableImage
     // currently bound to the ImageView and that it would cause stalls
@@ -21,19 +19,29 @@ public class AsyncCopier {
     // been seen when writing to the WritableImage while its bound to the ImageView.
     private static final int N_IMAGES = 2;
 
-    // Lazily created
-    private CycleBuffer<Buffer> buffers;
-    private CycleBuffer<WritableImage> images = new CycleBuffer<WritableImage>(N_IMAGES);
+    // Lazily created pbo, one is enough for copying frame-buffer=>pbo, pbo=>image
+    private Buffer buffer;
+    private CycleBuffer<ImageSlot> imageSlots = new CycleBuffer<ImageSlot>(N_IMAGES);
 
     private int width;
     private int height;
-    private int currentFrame;
+
+    private static class ImageSlot {
+        ImageSlot(WritableImage image, int frameVersion) {
+            this.image = image;
+            this.frameVersion = frameVersion;
+        }
+
+        WritableImage image;
+        int frameVersion;
+    }
 
     private static class Buffer {
         int pbo;
         int width;
         int height;
-        int frame;
+        int frameVersion;
+        int profilerFrame;
         PixelFormat<ByteBuffer> pf;
 
         Buffer(int pbo) {
@@ -51,20 +59,21 @@ public class AsyncCopier {
             gl.glBindBuffer(GL2.GL_PIXEL_PACK_BUFFER, 0);
         }
 
-        WritableImage toImage(GL2 gl, WritableImage image) {
+        void toImage(GL2 gl, ImageSlot slot) {
             ByteBuffer buffer = gl.glMapBuffer(GL2.GL_PIXEL_PACK_BUFFER, GL2.GL_READ_ONLY);
 
-            if (image.getWidth() != width || image.getHeight() != height) {
-                image = new WritableImage(width, height);
+            if (slot.image.getWidth() != width || slot.image.getHeight() != height) {
+                slot.image = new WritableImage(width, height);
             }
-            image.getPixelWriter().setPixels(0, 0, width, height, pf, buffer, width * 4);
+            slot.image.getPixelWriter().setPixels(0, 0, width, height, pf, buffer, width * 4);
+            slot.frameVersion = this.frameVersion;
 
             gl.glUnmapBuffer(GL2.GL_PIXEL_PACK_BUFFER);
-            return image;
         }
 
-        void readFrameBufferAsync(GL2 gl, int frame) {
-            this.frame = frame;
+        void readFrameBufferAsync(GL2 gl, int frameVersion, int profilerFrame) {
+            this.frameVersion = frameVersion;
+            this.profilerFrame = profilerFrame;
             // NOTE You have to know what you are doing if you want to change this value.
             // If it does not match the native format exactly, glReadPixles will take a lot more time.
             int type = GL2.GL_UNSIGNED_INT_8_8_8_8_REV;
@@ -82,7 +91,7 @@ public class AsyncCopier {
         }
     }
 
-    private class CycleBuffer<T> {
+    private static class CycleBuffer<T> {
         int size;
         int index;
         Object[] entries;
@@ -98,24 +107,14 @@ public class AsyncCopier {
             return (T)this.entries[this.index];
         }
 
-        @SuppressWarnings("unchecked")
-        T get(int index) {
-            return (T)this.entries[index];
-        }
         void set(int index, T entry) {
             this.entries[index] = entry;
         }
 
-        void setCurrent(T entry) {
-            this.entries[this.index] = entry;
-        }
-
-        void next() {
+        @SuppressWarnings("unchecked")
+        T next() {
             this.index = (this.index + 1) % this.size;
-        }
-
-        int size() {
-            return this.size;
+            return (T)this.entries[this.index];
         }
     }
 
@@ -123,48 +122,39 @@ public class AsyncCopier {
         this.width = width;
         this.height = height;
         for (int i = 0; i < N_IMAGES; ++i) {
-            images.set(i, new WritableImage(width, height));
-        }
-        this.currentFrame = 0;
-    }
-
-    private void createBuffers(GL2 gl) {
-        this.buffers = new CycleBuffer<Buffer>(N_BUFFERS);
-        IntBuffer pboBuffers = IntBuffer.allocate(N_BUFFERS);
-        gl.glGenBuffers(N_BUFFERS, pboBuffers);
-        int[] pbos = pboBuffers.array();
-        for (int i = 0; i < N_BUFFERS; ++i) {
-            this.buffers.set(i, new Buffer(pbos[i]));
+            imageSlots.set(i, new ImageSlot(new WritableImage(width, height), 0));
         }
     }
 
-    public WritableImage flip(GL2 gl) {
-        final Sample end = Profiler.begin("render-async", "end-frame");
-
-        if (buffers == null) {
-            createBuffers(gl);
+    public WritableImage flip(GL2 gl, int frameVersion) {
+        if (buffer == null) {
+            IntBuffer pboBuffers = IntBuffer.allocate(1);
+            gl.glGenBuffers(1, pboBuffers);
+            int[] pbos = pboBuffers.array();
+            this.buffer = new Buffer(pbos[0]);
         }
-        Buffer currentBuffer = buffers.current();
-        buffers.next();
-        currentBuffer.bind(gl);
-        WritableImage image = null;
-        if (currentFrame >= N_BUFFERS) {
-            Sample toImage = Profiler.begin("render-async", "to-image", currentBuffer.frame);
-            image = this.images.current();
-            image = currentBuffer.toImage(gl, image);
-            this.images.setCurrent(image);
-            this.images.next();
+        ImageSlot slot = this.imageSlots.current();
+        if (slot.frameVersion == frameVersion) {
+            return slot.image;
+        }
+        WritableImage result = null;
+        buffer.bind(gl);
+        if (buffer.frameVersion > 0) {
+            Sample toImage = Profiler.begin("render-async", "to-image", buffer.profilerFrame);
+            slot = this.imageSlots.next();
+            buffer.toImage(gl, slot);
+            result = slot.image;
             Profiler.end(toImage);
         }
-        Sample readPixels = Profiler.begin("render-async", "read-pixels");
-        currentBuffer.setSize(gl, this.width, this.height);
-        currentBuffer.readFrameBufferAsync(gl, readPixels.getFrame());
-        Profiler.end(readPixels);
+        if (buffer.frameVersion != frameVersion) {
+            Sample readPixels = Profiler.begin("render-async", "read-pixels");
+            buffer.setSize(gl, this.width, this.height);
+            buffer.readFrameBufferAsync(gl, frameVersion, readPixels.getFrame());
+            Profiler.end(readPixels);
+        }
 
-        currentBuffer.unbind(gl);
-        ++this.currentFrame;
-        Profiler.end(end);
-        return image;
+        buffer.unbind(gl);
+        return result;
     }
 
     public void setSize(int width, int height) {
@@ -173,11 +163,10 @@ public class AsyncCopier {
     }
 
     public void dispose(GL2 gl) {
-        int[] b = new int[buffers.size()];
-        for (int i = 0; i < b.length; i++) {
-            b[i] = buffers.get(i).pbo;
+        if (buffer != null) {
+            int[] b = {buffer.pbo};
+            gl.glDeleteBuffers(1, b, 0);
         }
-        gl.glDeleteBuffers(b.length, b, 0);
     }
 
 }
