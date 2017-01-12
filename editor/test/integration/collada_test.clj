da9e988533f76eac29f070c1b8752b17d778c787
Mats Gisselson <mats.gisselson@king.com>
2017-01-12 16:10:26 +0100

    DEFEDIT-669 Retain well-formed vertex buffers in mesh build data

--- editor/test/integration/collada_test.clj
+++ editor/test/integration/collada_test.clj
@@ -6,12 +6,33 @@
             [integration.test-util :as test-util]
             [support.test-support :refer [with-clean-system]]))
 
+(defn- sequence-buffer-in-mesh [mesh data-stride data-key index-key]
+  (let [partitioned-data (vec (partition data-stride (mesh data-key)))
+        sequenced-data (into [] (mapcat #(partitioned-data %) (mesh index-key)))
+        sequenced-indices (range (/ (count sequenced-data) data-stride))]
+    (assoc mesh
+      data-key sequenced-data
+      index-key sequenced-indices)))
+
+(defn- sequence-vertices-in-mesh [mesh]
+  (-> mesh
+      (sequence-buffer-in-mesh 3 :positions :indices)
+      (sequence-buffer-in-mesh 3 :normals :normals-indices)
+      (sequence-buffer-in-mesh 2 :texcoord0 :texcoord0-indices)))
+
+(defn- sequence-vertices-in-mesh-entry [mesh-entry]
+  (update mesh-entry :meshes #(mapv sequence-vertices-in-mesh %)))
+
+(defn- sequence-vertices-in-mesh-set [mesh-set]
+  (update mesh-set :mesh-entries #(mapv sequence-vertices-in-mesh-entry %)))
+
 (deftest normals
   (with-clean-system
     (let [workspace (test-util/setup-workspace! world)
           content (-> (workspace/file-resource workspace "/mesh/box_blender.dae")
                     io/input-stream
-                    collada/->mesh-set)]
+                    collada/->mesh-set
+                    sequence-vertices-in-mesh-set)]
       (is (every? (fn [[x y z]]
                     (< (Math/abs (- (Math/sqrt (+ (* x x) (* y y) (* z z))) 1.0)) 0.000001))
                   (->> (get-in content [:mesh-entries 0 :meshes 0 :normals])
@@ -22,7 +43,8 @@
     (let [workspace (test-util/setup-workspace! world)
           content (-> (workspace/file-resource workspace "/mesh/plane.dae")
                     io/input-stream
-                    collada/->mesh-set)]
+                    collada/->mesh-set
+                    sequence-vertices-in-mesh-set)]
       (let [c (get-in content [:mesh-entries 0 :meshes 0])
             zs (map #(nth % 2) (partition 3 (:positions c)))
             vs (map second (partition 2 (:texcoord0 c)))]
