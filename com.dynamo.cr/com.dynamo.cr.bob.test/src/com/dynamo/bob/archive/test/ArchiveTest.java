b895bb76c4c53b1c65e20534323d7954afaa2358
Jakob Pogulis <jakob.pogulis@king.com>
2017-01-17 18:10:14 +0100

    DEF-2257: Update the resource management system (#1190)
    * DEF-2256: Create Manifest (#1062)
    
    * Added initial protocol-buffer format for Manifest
    
    * WIP: Functionality for cryptographic functions done.
    
    * WIP: Now builds a manifest to my Desktop
    
    * DEF-2256: Create Manifest
    
    * Eclipse.. Tabs to spaces..
    
    * Fixes after review of myself
    
    * Fixed review comments
    
    * DEF-2253: Bundle "Asset pack" (#1074)
    
    * WIP: Back to code review
    
    * WIP: Create archive bundle
    
    * WIP: Now bundles resource packs.
    
    * Minor fixes after self-review, white spaces etc.
    
    * DEF-2255: Generate private/public key (#1076)
    
    * WIP
    
    * Now creates a temporary private/public key for signing the manifest unless one is provided. The public key is always bundled with the application.
    
    * Fixed a problem where the public key was seen as a required asset for the archive, this broke a couple of tests for the editor that should work now.
    
    * DEF-2252 Update DARC storage format (Live Update) (#1107)
    
    * New file formats for Live Update .acri and .arcd
    
    * DEF-2254: Exclude assets (#1137)
    
    * DEF-2254: Exclude assets
    
    * Fixed comments from review.
    
    * Added liveupdate lib to build scripts
    
    * WIP commit before task switch
    
    * Removed unnecessary throws to simplify test cases so that they can use simple and incomplete data for testing.
    
    * Build/Bundle fixes for the editor
    
    * Fixed an error that occurred during the build since ArchiveBuilder is executed through script
    
    * Editor bundle fixes for resource management (#1153)
    
    * Removed unnecessary throws to simplify test cases so that they can use simple and incomplete data for testing.
    
    * Build/Bundle fixes for the editor
    
    * Fixed an error that occurred during the build since ArchiveBuilder is executed through script
    
    * Load resources in runtime via manifest. Lots of broken resource tests
    
    * Added information about embedded files to build output
    
    * WIP, start of a huge migration from the old archive format to the new archive format
    
    * WIP: Now possible to build tests as well. Tests will still be failing
    
    * Resource system uses new format
    
    * Refactored Manifest loading
    
    * Fixed ArchiveBuilder
    
    * WIP commit
    
    * Fixed a lot of tests for resource archive
    
    * Removed runtime code for old file format
    
    * Fixed remaining issues with tests after code cleanup
    
    * Reduced the size of the buffer that as allocated on the stack for tests from 1MiB to 1 KiB.
    
    * Fixed memory leaks
    
    * Removed additional memory leaks
    
    * Fixed additional memory leaks and crashes for tests.
    
    * Moved library order for engine/script
    
    * Review fixes
    
    * Removed an unnecessary log.
    
    * Changed log message to use correct log functionality

--- com.dynamo.cr/com.dynamo.cr.bob.test/src/com/dynamo/bob/archive/test/ArchiveTest.java
+++ com.dynamo.cr/com.dynamo.cr.bob.test/src/com/dynamo/bob/archive/test/ArchiveTest.java
@@ -1,13 +1,18 @@
 package com.dynamo.bob.archive.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
@@ -19,11 +24,19 @@ import org.junit.Test;
 import com.dynamo.bob.archive.ArchiveEntry;
 import com.dynamo.bob.archive.ArchiveBuilder;
 import com.dynamo.bob.archive.ArchiveReader;
+import com.dynamo.bob.archive.ManifestBuilder;
+import com.dynamo.bob.pipeline.ResourceNode;
+import com.dynamo.liveupdate.proto.Manifest.HashAlgorithm;
 
 public class ArchiveTest {
 
     private String contentRoot;
     private File outputDarc;
+    private File outputIndex;
+    private File outputData;
+    private Path resourcePackDir;
+
+    private ManifestBuilder manifestBuilder;
 
     private String createDummyFile(String dir, String filepath, byte[] data) throws IOException {
         File tmp = new File(Paths.get(FilenameUtils.concat(dir, filepath)).toString());
@@ -37,35 +50,77 @@ public class ArchiveTest {
         return tmp.getAbsolutePath();
     }
 
+    private int bitwiseCompare(byte b1, byte b2)
+    {
+        // Need to compare bit by bit since the byte value is unsigned, but Java has
+        // no unsigned types. We use masking to filter out sign extension when shifting
+        // (https://en.wikipedia.org/wiki/Sign_extension).
+        int ones = 0xFF;
+        for(int i=7; i>=0; i--)
+        {
+            int mask = ones >> i;
+            byte b1Shift = (byte) ((b1 >> i) & mask);
+            byte b2Shift = (byte) ((b2 >> i) & mask);
+            if(b1Shift > b2Shift)
+                return 1;
+            else if(b1Shift < b2Shift)
+                return -1;
+        }
+
+        return 0;
+    }
+
+    private ResourceNode addEntry(String filename, String content, ArchiveBuilder archiveBuilder, ResourceNode parent) throws IOException {
+        String filepath = createDummyFile(contentRoot, filename, filename.getBytes());
+        archiveBuilder.add(filepath);
+        ResourceNode current = new ResourceNode(filename, FilenameUtils.concat(contentRoot, filename));
+        parent.addChild(current);
+        return current;
+    }
+
     @Before
     public void setUp() throws Exception {
         contentRoot = Files.createTempDirectory(null).toFile().getAbsolutePath();
         outputDarc = Files.createTempFile("tmp.darc", "").toFile();
+
+        outputIndex = Files.createTempFile("tmp.defold", "arci").toFile();
+        outputData = Files.createTempFile("tmp.defold", "arcd").toFile();
+
+        resourcePackDir = Files.createTempDirectory("tmp.defold.resourcepack_");
+
+        manifestBuilder = new ManifestBuilder();
+        manifestBuilder.setResourceHashAlgorithm(HashAlgorithm.HASH_SHA1);
     }
 
     @After
     public void tearDown() throws IOException {
         FileUtils.deleteDirectory(new File(contentRoot));
         FileUtils.deleteQuietly(outputDarc);
+
+        FileUtils.deleteQuietly(outputIndex);
+        FileUtils.deleteQuietly(outputData);
     }
 
     @Test
-    public void testBuilderAndReader() throws IOException {
-
+    public void testBuilderAndReader() throws IOException
+    {
         // Create
-        ArchiveBuilder ab = new ArchiveBuilder(contentRoot);
+        ArchiveBuilder ab = new ArchiveBuilder(contentRoot, manifestBuilder);
         ab.add(createDummyFile(contentRoot, "a.txt", "abc123".getBytes()));
         ab.add(createDummyFile(contentRoot, "b.txt", "apaBEPAc e p a".getBytes()));
         ab.add(createDummyFile(contentRoot, "c.txt", "åäöåäöasd".getBytes()));
 
         // Write
-        RandomAccessFile outFile = new RandomAccessFile(outputDarc, "rw");
-        outFile.setLength(0);
-        ab.write(outFile);
-        outFile.close();
+        RandomAccessFile outFileIndex = new RandomAccessFile(outputIndex, "rw");
+        RandomAccessFile outFileData = new RandomAccessFile(outputData, "rw");
+        outFileIndex.setLength(0);
+        outFileData.setLength(0);
+        ab.write(outFileIndex, outFileData, resourcePackDir, new ArrayList<String>());
+        outFileIndex.close();
+        outFileData.close();
 
         // Read
-        ArchiveReader ar = new ArchiveReader(outputDarc.getAbsolutePath());
+        ArchiveReader ar = new ArchiveReader(outputIndex.getAbsolutePath(), outputData.getAbsolutePath());
         ar.read();
         ar.close();
     }
@@ -74,24 +129,236 @@ public class ArchiveTest {
     public void testEntriesOrder() throws IOException {
 
         // Create
-        ArchiveBuilder ab = new ArchiveBuilder(FilenameUtils.separatorsToSystem(contentRoot));
+        ArchiveBuilder ab = new ArchiveBuilder(FilenameUtils.separatorsToSystem(contentRoot), manifestBuilder);
         ab.add(FilenameUtils.separatorsToSystem(createDummyFile(contentRoot, "main/a.txt", "abc123".getBytes())));
         ab.add(FilenameUtils.separatorsToSystem(createDummyFile(contentRoot, "main2/a.txt", "apaBEPAc e p a".getBytes())));
         ab.add(FilenameUtils.separatorsToSystem(createDummyFile(contentRoot, "a.txt", "åäöåäöasd".getBytes())));
 
         // Write
-        RandomAccessFile outFile = new RandomAccessFile(outputDarc, "rw");
-        outFile.setLength(0);
-        ab.write(outFile);
-        outFile.close();
+        RandomAccessFile outFileIndex = new RandomAccessFile(outputIndex, "rw");
+        RandomAccessFile outFileData = new RandomAccessFile(outputData, "rw");
+        outFileIndex.setLength(0);
+        outFileData.setLength(0);
+        ab.write(outFileIndex, outFileData, resourcePackDir, new ArrayList<String>());
+        outFileIndex.close();
+        outFileData.close();
 
         // Read
-        ArchiveReader ar = new ArchiveReader(outputDarc.getAbsolutePath());
+        ArchiveReader ar = new ArchiveReader(outputIndex.getAbsolutePath(), outputData.getAbsolutePath());
         ar.read();
         List<ArchiveEntry> entries = ar.getEntries();
-        assertEquals(entries.get(0).fileName, "/a.txt");
-        assertEquals(entries.get(1).fileName, "/main/a.txt");
-        assertEquals(entries.get(2).fileName, "/main2/a.txt");
+
+        Boolean correctOrder = false;
+        for(int i=1; i<entries.size(); i++) {
+            ArchiveEntry ePrev = entries.get(i-1);
+            ArchiveEntry eCurr = entries.get(i);
+            correctOrder = false;
+            for (int j = 0; j < eCurr.hash.length; j++) {
+                correctOrder = bitwiseCompare(eCurr.hash[j], ePrev.hash[j]) == 1;
+
+                if(correctOrder)
+                    break;
+            }
+            assertEquals(correctOrder, true);
+        }
+
         ar.close();
     }
+    
+    @Test
+    public void testArchiveIndexAlignment() throws IOException {
+    	ArchiveBuilder instance = new ArchiveBuilder(FilenameUtils.separatorsToSystem(contentRoot), manifestBuilder);
+    	for (int i = 1; i < 50; ++i) {
+			String filename = "dummy" + Integer.toString(i);
+			String content  = "dummy" + Integer.toString(i);
+			instance.add(FilenameUtils.separatorsToSystem(createDummyFile(contentRoot, filename, content.getBytes())));
+    		
+    		RandomAccessFile archiveIndex = new RandomAccessFile(outputIndex, "rw");
+	        RandomAccessFile archiveData = new RandomAccessFile(outputData, "rw");
+	        archiveIndex.setLength(0);
+	        archiveData.setLength(0);
+	        instance.write(archiveIndex, archiveData, resourcePackDir, new ArrayList<String>());
+	        archiveIndex.close();
+	        archiveData.close();
+	        
+	        archiveIndex = new RandomAccessFile(outputIndex, "r");
+	        
+	        archiveIndex.readInt();  					// Version
+	        archiveIndex.readInt();  					// Padding
+	        archiveIndex.readLong(); 					// UserData
+	        int entrySize   = archiveIndex.readInt();	// EntrySize
+	        int entryOffset = archiveIndex.readInt();	// EntryOffset
+	        int hashOffset  = archiveIndex.readInt();	// HashOffset
+	        archiveIndex.readInt();						// HashSize
+	        archiveIndex.close();
+	        
+	        assertEquals(32, hashOffset);
+	        assertEquals(32 + entrySize * ArchiveBuilder.HASH_MAX_LENGTH, entryOffset);
+	        assertTrue(entryOffset % 4 == 0);
+	        assertTrue(hashOffset % 4 == 0);
+    	}
+    }
+
+    @Test
+    public void testLoadResourceData() throws Exception {
+        byte[] content = "Hello, world".getBytes();
+        String filepath = FilenameUtils.separatorsToSystem(createDummyFile(contentRoot, "resource.tmp", content));
+        ArchiveBuilder instance = new ArchiveBuilder(FilenameUtils.separatorsToSystem(contentRoot), manifestBuilder);
+
+        byte[] actual = instance.loadResourceData(filepath);
+
+        assertArrayEquals(content, actual);
+    }
+
+    @Test
+    public void testCompressResourceData() throws Exception {
+        byte[] content = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA".getBytes();
+        ArchiveBuilder instance = new ArchiveBuilder(FilenameUtils.separatorsToSystem(contentRoot), manifestBuilder);
+
+        byte[] expected = { 31, 65, 1, 0, 75, 80, 65, 65, 65, 65, 65 };
+        byte[] actual = instance.compressResourceData(content);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testShouldUseCompressedResourceData() throws Exception {
+        ArchiveBuilder instance = new ArchiveBuilder(FilenameUtils.separatorsToSystem(contentRoot), manifestBuilder);
+
+        byte[] original = { 10, 10, 10, 10, 10 };
+        byte[] compressed = { 10, 10, 10, 10 };
+        assertTrue(instance.shouldUseCompressedResourceData(original, compressed));     // 0.80
+
+        original = new byte[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+        compressed = new byte[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+        assertTrue(instance.shouldUseCompressedResourceData(original, compressed));     // 0.95
+
+        original= new byte[] { 10, 10, 10, 10, 10, 10, 10, 10 };
+        compressed = new byte[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+        assertFalse(instance.shouldUseCompressedResourceData(original, compressed));    // 1.25
+    }
+
+    @SuppressWarnings("unused")
+	@Test
+    public void testWriteArchive() throws Exception {
+        ManifestBuilder manifestBuilder = new ManifestBuilder();
+        manifestBuilder.setResourceHashAlgorithm(HashAlgorithm.HASH_MD5);
+
+        ArchiveBuilder instance = new ArchiveBuilder(FilenameUtils.separatorsToSystem(contentRoot), manifestBuilder);
+
+        ResourceNode root = new ResourceNode("<Anonymous Root>", "<Anonymous Root>");
+        ResourceNode collection1 = addEntry("main.collectionc", "alpha", instance, root);
+        ResourceNode collectionproxy1 = addEntry("main.collectionproxyc", "beta", instance, collection1);
+        ResourceNode gameobject1 = addEntry("main.goc", "delta", instance, collectionproxy1);
+
+        manifestBuilder.setDependencies(root);
+
+        List<String> excludedResources = new ArrayList<String>();
+        excludedResources.add("/main.collectionproxyc");
+
+        // Test
+        RandomAccessFile outFileIndex = new RandomAccessFile(outputIndex, "rw");
+        RandomAccessFile outFileData = new RandomAccessFile(outputData, "rw");
+        instance.write(outFileIndex, outFileData, resourcePackDir, excludedResources);
+
+        assertEquals(2, instance.getArchiveEntrySize());
+        assertEquals("/main.collectionproxyc", instance.getArchiveEntry(0).relName);    // 987bcab01b929eb2c07877b224215c92
+        assertEquals("/main.collectionc", instance.getArchiveEntry(1).relName);         // 2c1743a391305fbf367df8e4f069f9f9
+    }
+
+    @SuppressWarnings("unused")
+	@Test
+    public void testWriteArchive_SiblingProxies() throws Exception {
+        ManifestBuilder manifestBuilder = new ManifestBuilder();
+        manifestBuilder.setResourceHashAlgorithm(HashAlgorithm.HASH_MD5);
+
+        ArchiveBuilder instance = new ArchiveBuilder(FilenameUtils.separatorsToSystem(contentRoot), manifestBuilder);
+
+        ResourceNode root = new ResourceNode("<Anonymous Root>", "<Anonymous Root>");
+        ResourceNode collection1 = addEntry("main.collectionc", "alpha", instance, root);
+        ResourceNode collectionproxy1 = addEntry("level1.collectionproxyc", "beta", instance, collection1);
+        ResourceNode collectionproxy2 = addEntry("level2.collectionproxyc", "delta", instance, collection1);
+        ResourceNode gameobject1 = addEntry("level1.goc", "gamma", instance, collectionproxy1);
+        ResourceNode gameobject2 = addEntry("level2.goc", "epsilon", instance, collectionproxy2);
+
+        manifestBuilder.setDependencies(root);
+
+        List<String> excludedResources = new ArrayList<String>();
+        excludedResources.add("/level2.collectionproxyc");
+
+        // Test
+        RandomAccessFile outFileIndex = new RandomAccessFile(outputIndex, "rw");
+        RandomAccessFile outFileData = new RandomAccessFile(outputData, "rw");
+        instance.write(outFileIndex, outFileData, resourcePackDir, excludedResources);
+
+        assertEquals(4, instance.getArchiveEntrySize());
+        assertEquals("/level1.collectionproxyc", instance.getArchiveEntry(0).relName);  // 617905b1d0e858ca35230357710cf5f2
+        assertEquals("/main.collectionc", instance.getArchiveEntry(1).relName);         // b32b3904944e63ed5a269caa47904645
+        assertEquals("/level2.collectionproxyc", instance.getArchiveEntry(2).relName);  // bc05302047f95ca60709254556402710
+        assertEquals("/level1.goc", instance.getArchiveEntry(3).relName);               // d25298c59a872b5bfd5473de7b36a4a4
+    }
+
+    @SuppressWarnings("unused")
+	@Test
+    public void testWriteArchive_DeepProxies() throws Exception {
+        ManifestBuilder manifestBuilder = new ManifestBuilder();
+        manifestBuilder.setResourceHashAlgorithm(HashAlgorithm.HASH_MD5);
+
+        ArchiveBuilder instance = new ArchiveBuilder(FilenameUtils.separatorsToSystem(contentRoot), manifestBuilder);
+
+        ResourceNode root = new ResourceNode("<Anonymous Root>", "<Anonymous Root>");
+        ResourceNode collection1 = addEntry("main.collectionc", "alpha", instance, root);
+        ResourceNode collectionproxy1 = addEntry("level1.collectionproxyc", "beta", instance, collection1);
+        ResourceNode collectionproxy2 = addEntry("level2.collectionproxyc", "delta", instance, collectionproxy1);
+        ResourceNode gameobject1 = addEntry("level1.goc", "gamma", instance, collectionproxy1);
+        ResourceNode gameobject2 = addEntry("level2.goc", "epsilon", instance, collectionproxy2);
+
+        manifestBuilder.setDependencies(root);
+
+        List<String> excludedResources = new ArrayList<String>();
+        excludedResources.add("/level2.collectionproxyc");
+
+        // Test
+        RandomAccessFile outFileIndex = new RandomAccessFile(outputIndex, "rw");
+        RandomAccessFile outFileData = new RandomAccessFile(outputData, "rw");
+        instance.write(outFileIndex, outFileData, resourcePackDir, excludedResources);
+
+        assertEquals(4, instance.getArchiveEntrySize());
+        assertEquals("/level1.collectionproxyc", instance.getArchiveEntry(0).relName);  // 617905b1d0e858ca35230357710cf5f2
+        assertEquals("/main.collectionc", instance.getArchiveEntry(1).relName);         // b32b3904944e63ed5a269caa47904645
+        assertEquals("/level2.collectionproxyc", instance.getArchiveEntry(2).relName);  // bc05302047f95ca60709254556402710
+        assertEquals("/level1.goc", instance.getArchiveEntry(3).relName);               // d25298c59a872b5bfd5473de7b36a4a4
+    }
+
+    @SuppressWarnings("unused")
+	@Test
+    public void testWriteArchive_DeepProxiesExcludeGrandparent() throws Exception {
+        ManifestBuilder manifestBuilder = new ManifestBuilder();
+        manifestBuilder.setResourceHashAlgorithm(HashAlgorithm.HASH_MD5);
+
+        ArchiveBuilder instance = new ArchiveBuilder(FilenameUtils.separatorsToSystem(contentRoot), manifestBuilder);
+
+        ResourceNode root = new ResourceNode("<Anonymous Root>", "<Anonymous Root>");
+        ResourceNode collection1 = addEntry("main.collectionc", "alpha", instance, root);
+        ResourceNode collectionproxy1 = addEntry("level1.collectionproxyc", "beta", instance, collection1);
+        ResourceNode collectionproxy2 = addEntry("level2.collectionproxyc", "delta", instance, collectionproxy1);
+        ResourceNode gameobject1 = addEntry("level1.goc", "gamma", instance, collectionproxy1);
+        ResourceNode gameobject2 = addEntry("level2.goc", "epsilon", instance, collectionproxy2);
+
+        manifestBuilder.setDependencies(root);
+
+        List<String> excludedResources = new ArrayList<String>();
+        excludedResources.add("/level1.collectionproxyc");
+
+        // Test
+        RandomAccessFile outFileIndex = new RandomAccessFile(outputIndex, "rw");
+        RandomAccessFile outFileData = new RandomAccessFile(outputData, "rw");
+        instance.write(outFileIndex, outFileData, resourcePackDir, excludedResources);
+
+        assertEquals(2, instance.getArchiveEntrySize());
+        assertEquals("/level1.collectionproxyc", instance.getArchiveEntry(0).relName);  // 617905b1d0e858ca35230357710cf5f2
+        assertEquals("/main.collectionc", instance.getArchiveEntry(1).relName);         // b32b3904944e63ed5a269caa47904645
+    }
+
+
 }
--- engine/resource/src/mount/mount_android.cpp
+++ engine/resource/src/mount/mount_android.cpp
@@ -1,4 +1,5 @@
 #include <dlib/log.h>
+#include <dlib/path.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <stdio.h>
@@ -15,48 +16,96 @@ extern struct android_app* __attribute__((weak)) g_AndroidApp ;
 
 namespace dmResource
 {
+
     struct MountInfo
     {
-        AAsset *asset;
+        AAsset *index_asset;
+        AAsset *data_asset;
     };
 
-    Result MountArchiveInternal(const char* path, dmResourceArchive::HArchive* archive, void** mount_info)
+    Result MapAsset(AAssetManager* am, const char* path,  void*& out_asset, uint32_t& out_size, void*& out_map)
     {
-        AAssetManager* am = g_AndroidApp->activity->assetManager;
-        AAsset* asset = AAssetManager_open(am, path, AASSET_MODE_RANDOM);
-        if (!asset)
+        out_asset = (void*)AAssetManager_open(am, path, AASSET_MODE_RANDOM);
+        if (!out_asset)
         {
             return RESULT_RESOURCE_NOT_FOUND;
         }
-
-        const void *map = AAsset_getBuffer(asset);
-        if (!map)
+        out_map = (void*)AAsset_getBuffer((AAsset*)out_asset);
+        if (!out_map)
         {
-            AAsset_close(asset);
+            AAsset_close((AAsset*)out_asset);
+            return RESULT_IO_ERROR;
+        }
+        out_size = AAsset_getLength((AAsset*)out_asset);
+
+        return RESULT_OK;
+    }
+
+    Result MountArchiveInternal(const char* index_path, dmResourceArchive::HArchiveIndexContainer* archive, void** mount_info)
+    {
+        // Derive path of arcd file from path to arci
+        char data_path[DMPATH_MAX_PATH];
+        memcpy(&data_path, index_path, strlen(index_path)+1); // copy NULL terminator as well
+        data_path[strlen(index_path)-1] = 'd';
+
+        AAssetManager* am = g_AndroidApp->activity->assetManager;
+        AAsset* index_asset = 0x0;
+        uint32_t index_length = 0;
+        void* index_map = 0x0;
+        Result r = MapAsset(am, index_path, (void*&)index_asset, index_length, index_map);
+        if (r != RESULT_OK)
+        {
+            dmLogInfo("Error when mapping index file, result: %i", r);
             return RESULT_IO_ERROR;
         }
 
-        uint32_t length = AAsset_getLength(asset);
-        dmResourceArchive::Result r = WrapArchiveBuffer(map, length, archive);
-        if (r != dmResourceArchive::RESULT_OK)
+        AAsset* data_asset = 0x0;
+        uint32_t data_length = 0;
+        void* data_map = 0x0;
+
+        r = MapAsset(am, data_path, (void*&)data_asset, data_length, data_map);
+        if (r != RESULT_OK)
         {
-            AAsset_close(asset);
+            AAsset_close(index_asset);
+            dmLogInfo("Error when mapping data file, result: %i", r);
+        }
+
+        dmResourceArchive::Result res = WrapArchiveBuffer(index_map, index_length, data_map, archive);
+        if (res != dmResourceArchive::RESULT_OK)
+        {
+            AAsset_close(index_asset);
+            AAsset_close(data_asset);
             return RESULT_IO_ERROR;
         }
 
         MountInfo* info = new MountInfo();
-        info->asset = asset;
+        info->index_asset = index_asset;
+        info->data_asset = data_asset;
         *mount_info = (void*)info;
         return RESULT_OK;
     }
 
-    void UnmountArchiveInternal(dmResourceArchive::HArchive archive, void* mount_info)
+    void UnmountArchiveInternal(dmResourceArchive::HArchiveIndexContainer archive, void* mount_info)
     {
         MountInfo* info = (MountInfo*) mount_info;
-        if (info)
+
+        if (!info)
         {
-            AAsset_close(info->asset);
-            delete info;
+            return;
         }
+
+        if (info->index_asset)
+        {
+            AAsset_close(info->index_asset);
+        }
+
+        if (info->data_asset)
+        {
+            AAsset_close(info->data_asset);
+        }
+
+        delete info;
+
+        dmResourceArchive::Delete(archive);
     }
 }
