df622701d3464e1f273597376f77ca9ea74f0893
Ragnar Dahlén <ragnar.dahlen@king.com>
2017-01-18 10:29:57 +0100

    DEFEDIT-704 Ensure streams/readers are closed properly

--- editor/src/clj/editor/sound.clj
+++ editor/src/clj/editor/sound.clj
@@ -28,12 +28,8 @@
 
 (defn- build-sound-source
   [self basis resource dep-resources user-data]
-  (let [source (:resource resource)
-        in (io/input-stream source)
-        out (ByteArrayOutputStream.)
-        _ (IOUtils/copy in out)
-        content (.toByteArray out)]
-    {:resource resource :content content}))
+  (with-open [in (io/input-stream (:resource resource))]
+    {:resource resource :content (IOUtils/toByteArray in)}))
 
 (g/defnk produce-source-build-targets [_node-id resource]
   [{:node-id _node-id
