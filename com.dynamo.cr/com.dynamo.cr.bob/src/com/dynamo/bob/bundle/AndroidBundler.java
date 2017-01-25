f344c4ad71cdec843ba807f5d41d825d994c23b9
Mathias Westerdahl <mathias.westerdahl@king.com>
2017-01-25 16:16:35 +0100

    DEF-2199 Create the local build cache upfront

--- com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/bundle/AndroidBundler.java
+++ com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/bundle/AndroidBundler.java
@@ -72,6 +72,7 @@ public class AndroidBundler implements IBundler {
             String platform = "armv7-android";
 
             File cacheDir = new File(project.getBuildCachePath());
+            cacheDir.mkdirs();
             String sdkVersion = project.option("defoldsdk", "");
             String buildServer = project.option("build-server", "");
             ExtenderClient extender = new ExtenderClient(buildServer, cacheDir);
