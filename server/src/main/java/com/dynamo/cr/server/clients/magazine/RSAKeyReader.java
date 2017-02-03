da22a605180913bf57e3e598e8e485d8550b1c1f
Mikael Löthman <mikael.lothman@king.com>
2017-02-03 09:28:37 +0100

    Fix private key reader to read from classpath in jar.

--- server/src/main/java/com/dynamo/cr/server/clients/magazine/RSAKeyReader.java
+++ server/src/main/java/com/dynamo/cr/server/clients/magazine/RSAKeyReader.java
@@ -1,8 +1,9 @@
 package com.dynamo.cr.server.clients.magazine;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.security.KeyFactory;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
@@ -38,12 +39,64 @@ public class RSAKeyReader {
     }
 
     private static byte[] getKeyFromFile(String resourceName) throws Exception {
-        URL systemResource = ClassLoader.getSystemResource(resourceName);
+        ResourceLoader resourceLoader = new ResourceLoader();
+        return resourceLoader.getBytesFromResource(resourceName);
+    }
 
-        if (systemResource == null) {
-            throw new Exception("Resource " + resourceName + "could not be found on classpath.");
+    private static class ResourceLoader {
+
+        private byte[] getBytesFromResource(String resourceName) throws IOException {
+            try (InputStream inputStream = getInputStream(resourceName)) {
+                return getBytesFromStream(inputStream);
+            }
         }
 
-        return Files.readAllBytes(Paths.get(systemResource.toURI()));
+        InputStream getInputStream(String resourceName) throws IOException {
+            final ClassLoader[] classLoaders = {
+                    getClass().getClassLoader(),
+                    RSAKeyReader.class.getClassLoader(),
+                    Thread.currentThread().getContextClassLoader(),
+                    ClassLoader.getSystemClassLoader()
+            };
+            resourceName = removePreSlash(resourceName);
+            URL resource = null;
+            for (ClassLoader classLoader : classLoaders) {
+                resource = classLoader.getResource(resourceName);
+
+                if (resource != null) {
+                    break;
+                }
+                resource = classLoader.getResource("/" + resourceName);
+                if (resource != null) {
+                    break;
+                }
+            }
+            if (resource == null) {
+                throw new IOException("Could not find resource: " + resourceName);
+            }
+            return resource.openStream();
+        }
+
+        private static String removePreSlash(String resourceName) {
+            if (resourceName.charAt(0) == '/') {
+                return resourceName.substring(1);
+            }
+            return resourceName;
+        }
+    }
+
+    static byte[] getBytesFromStream(InputStream inputStream) throws IOException {
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, read);
+            }
+            return outputStream.toByteArray();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
     }
 }
