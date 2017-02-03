78c11f4a774d41f1829f65eadb44848a0a87b835
Mikael Säker <mikael@sicher.org>
2017-02-03 14:43:03 +0100

    API docs. Type info added, added examples. Clarified text.

--- engine/script/src/script_zlib.cpp
+++ engine/script/src/script_zlib.cpp
@@ -47,6 +47,13 @@ namespace dmScript
      * @name zlib.inflate
      * @param buf [type:string] buffer to inflate
      * @return buf [type:string] inflated buffer
+     * @examples
+     *
+     * ```lua
+     * local data = "\120\94\11\201\200\44\86\0\162\68\133\226\146\162\204\188\116\133\242\204\146\12\133\210\188\228\252\220\130\162\212\226\226\212\20\133\148\196\146\68\61\0\44\67\14\201"
+     * local uncompressed_data = zlib.inflate(data)
+     * print(uncompressed_data) --> This is a string with uncompressed data.
+     * ```
      */
     int Zlib_Inflate(lua_State* L)
     {
@@ -77,6 +84,17 @@ namespace dmScript
      * @name zlib.deflate
      * @param buf [type:string] buffer to deflate
      * @return buf [type:string] deflated buffer
+     * @examples
+     *
+     * ```lua
+     * local data = "This is a string with uncompressed data."
+     * local compressed_data = zlib.deflate(data)
+     * local s = ""
+     * for c in string.gmatch(c_data, ".") do
+     *     s = s .. '\\' .. string.byte(c)
+     * end
+     * print(s) --> \120\94\11\201\200\44\86\0\162\68\133\226\146\162 ...
+     * ```
      */
     int Zlib_Deflate(lua_State* L)
     {
