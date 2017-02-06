3caa38cf99ce5c79bd8befc6abec18e9f41db03f
Jakob Pogulis <jakob.pogulis@king.com>
2017-02-06 11:55:38 +0100

    Design meeting fixes for LiveUpdate API - Updated resource.store_resource to execute a callback after the resource has been stored to prepare for threaded storage of resources - Updated documentation to reflect the API, temporarily disabled documentation for features we’re not ready to expose - Updated LiveUpdate Settings dialog not to display the Defold Service setting since it’s not ready

--- engine/gamesys/src/gamesys/scripts/script_resource_liveupdate.cpp
+++ engine/gamesys/src/gamesys/scripts/script_resource_liveupdate.cpp
@@ -2,9 +2,24 @@
 #include <liveupdate/liveupdate.h>
 
 #include <script/script.h>
+#include <dlib/log.h>
 
 namespace dmLiveUpdate
 {
+
+    struct StoreResourceEntry
+    {
+
+        StoreResourceEntry() {
+            memset(this, 0x0, sizeof(*this));
+        }
+
+        lua_State*  m_L;
+        int         m_Callback;
+        int         m_Self;
+        const char* m_HexDigest;
+        bool        m_Status;
+    };
 
     int Resource_GetCurrentManifest(lua_State* L)
     {
@@ -64,32 +79,33 @@ namespace dmLiveUpdate
         return 0;
     }
 
-    int Resource_VerifyResource(lua_State* L)
+    static void Callback_StoreResource(StoreResourceEntry* entry)
     {
-        int top = lua_gettop(L);
+        lua_State* L = entry->m_L;
+        DM_LUA_STACK_CHECK(L, 0);
 
-        int manifestIndex = luaL_checkint(L, 1);
+        lua_rawgeti(L, LUA_REGISTRYINDEX, entry->m_Callback);
+        lua_rawgeti(L, LUA_REGISTRYINDEX, entry->m_Self);
+        lua_pushvalue(L, -1);
 
-        size_t hexDigestLength = 0;
-        const char* hexDigest = luaL_checklstring(L, 2, &hexDigestLength);
+        dmScript::SetInstance(L);
 
-        size_t buflen = 0;
-        const char* buf = luaL_checklstring(L, 3, &buflen);
-        dmResourceArchive::LiveUpdateResource resource((const uint8_t*)buf, buflen);
-
-        dmResource::Manifest* manifest = dmLiveUpdate::GetManifest(manifestIndex);
-
-        if (manifest == 0x0)
+        if (!dmScript::IsInstanceValid(L))
         {
-            assert(top == lua_gettop(L));
-            return luaL_error(L, "The manifest identifier does not exist");
+            dmLogError("Could not run store_resource callback since the instance has been deleted.");
+            lua_pop(L, 2);
+            return;
         }
 
-        bool validResource = dmLiveUpdate::VerifyResource(manifest, hexDigest, hexDigestLength, &resource);
-        lua_pushboolean(L, (int) validResource);
+        lua_pushstring(L, entry->m_HexDigest);
+        lua_pushboolean(L, entry->m_Status);
 
-        assert(lua_gettop(L) == (top + 1));
-        return 1;
+        int ret = lua_pcall(L, 3, 0, 0);
+        if (ret != 0)
+        {
+            dmLogError("Error while running store_resource callback: %s", lua_tostring(L, -1));
+            lua_pop(L, 1);
+        }
     }
 
     int Resource_StoreResource(lua_State* L)
@@ -98,12 +114,16 @@ namespace dmLiveUpdate
 
         int manifestIndex = luaL_checkint(L, 1);
 
-        size_t hexDigestLength = 0;
-        const char* hexDigest = luaL_checklstring(L, 2, &hexDigestLength);
-
         size_t buflen = 0;
-        const char* buf = luaL_checklstring(L, 3, &buflen);
-        dmResourceArchive::LiveUpdateResource resource((const uint8_t*)buf, buflen);
+        const char* buf = luaL_checklstring(L, 2, &buflen);
+        dmResourceArchive::LiveUpdateResource resource((const uint8_t*) buf, buflen);
+
+        size_t hexDigestLength = 0;
+        const char* hexDigest = luaL_checklstring(L, 3, &hexDigestLength);
+
+        luaL_checktype(L, 4, LUA_TFUNCTION);
+        lua_pushvalue(L, 4);
+        int callback = dmScript::Ref(L, LUA_REGISTRYINDEX);
 
         dmResource::Manifest* manifest = dmLiveUpdate::GetManifest(manifestIndex);
 
@@ -112,18 +132,19 @@ namespace dmLiveUpdate
             assert(top == lua_gettop(L));
             return luaL_error(L, "The manifest identifier does not exist");
         }
-        
-        bool resourceStored = dmLiveUpdate::StoreResource(manifest, hexDigest, hexDigestLength, &resource);
-        lua_pushboolean(L, (int) resourceStored);
 
-        assert(lua_gettop(L) == top + 1);
-        return 1;
-    }
+        // BEGIN Temporary solution until async
+        StoreResourceEntry entry;
+        entry.m_L = dmScript::GetMainThread(L);
+        dmScript::GetInstance(L);
+        entry.m_Self = dmScript::Ref(L, LUA_REGISTRYINDEX);
+        entry.m_Callback = callback;
+        entry.m_HexDigest = hexDigest;
+        entry.m_Status = dmLiveUpdate::StoreResource(manifest, hexDigest, hexDigestLength, &resource);
+        Callback_StoreResource(&entry);
+        // END Temporary solution until async
 
-    int Resource_VerifyManifest(lua_State* L)
-    {
-        DM_LUA_STACK_CHECK(L, 0);
-
+        assert(lua_gettop(L) == top);
         return 0;
     }
 
