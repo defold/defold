1b3122c6612d3cb7f420e40405612fb2d6dab778
Mikael Säker <mikael@sicher.org>
2017-02-07 10:31:51 +0100

    Reformatted and updated a bunch of API docs.

--- engine/gamesys/src/gamesys/scripts/script_spine_model.cpp
+++ engine/gamesys/src/gamesys/scripts/script_spine_model.cpp
@@ -24,7 +24,7 @@ namespace dmGameSystem
 {
     /*# Spine model API documentation
      *
-     * Functions and messages for interacting with the 'Spine' 2D bone 
+     * Functions and messages for interacting with the 'Spine' 2D bone
      * animation system.
      *
      * @document
@@ -32,7 +32,7 @@ namespace dmGameSystem
      * @namespace spine
      */
 
-     /*# spine cursor (number)
+     /*# [type:number] spine cursor
      *
      * The normalized animation cursor. The type of the property is number.
      *
@@ -40,52 +40,54 @@ namespace dmGameSystem
      * @property
      *
      * @examples
-     * <p>
+     *
      * How to get the normalized cursor value:
-     * </p>
-     * <pre>
+     *
+     * ```lua
      * function init(self)
      *  -- Get the cursor value on component "spine"
      *  cursor = go.get("#spine", "cursor")
      * end
-     * </pre>
-     * <p>
+     * ```
+     *
      * How to animate the cursor from 0.0 to 1.0 using linear easing for 2.0 seconds:
-     * </p>
-     * <pre>
+     *
+     * ```lua
      * function init(self)
      *  -- Get the current value on component "spine"
      *  go.set("#spine", "cursor", 0.0)
      *  -- Animate the cursor value
      *  go.animate("#spine", "cursor", go.PLAYBACK_LOOP_FORWARD, 1.0, go.EASING_LINEAR, 2)
      * end
-     * </pre>
-     * <p>Please note that spine events may not fire as expected when the cursor is manipulated directly.</p>
+     * ```
+     *
+     * Please note that spine events may not fire as expected when the cursor is manipulated directly.
      */
 
-     /*# spine playback_rate (number)
+     /*# [type:number] spine playback_rate
      *
      * The animation playback rate. A multiplier to the animation playback rate. The type of the property is number.
+     *
+     * The playback_rate is a non-negative number, a negative value will be clamped to 0.
      *
      * @name playback_rate
      * @property
      *
      * @examples
-     * <p>
+     *
      * How to set the playback_rate on component "spine" to play at double the current speed:
-     * </p>
-     * <pre>
+     *
+     * ```lua
      * function init(self)
      *  -- Get the current value on component "spine"
      *  playback_rate = go.get("#spine", "playback_rate")
      *  -- Set the playback_rate to double the previous value.
      *  go.set("#spine", "playback_rate", playback_rate * 2)
      * end
-     * </pre>
-     * <p>The playback_rate is a non-negative number, a negative value will be clamped to 0.</p>
+     * ```
      */
 
-     /*# spine animation (hash)
+     /*# [type:hash] spine animation
      *
      * The current animation set on the component. The type of the property is hash.
      *
@@ -93,18 +95,18 @@ namespace dmGameSystem
      * @property
      *
      * @examples
-     * <p>
+     *
      * How to read the current animation from a spinemodel component:
-     * </p>
-     * <pre>
+     *
+     * ```lua
      * function init(self)
      *  -- Get the current animation on component "spinemodel"
      *  local animation = go.get("#spinemodel", "animation")
      * end
-     * </pre>
+     * ```
      */
 
-     /*# spine skin (hash)
+     /*# [type:hash] spine skin
      *
      * The current skin on the component. The type of the property is hash.
      * If setting the skin property the skin must be present on the spine
@@ -114,10 +116,10 @@ namespace dmGameSystem
      * @property
      *
      * @examples
-     * <p>
+     *
      * How to read and write the current skin from a spinemodel component:
-     * </p>
-     * <pre>
+     *
+     * ```lua
      * function init(self)
      *  -- If the hero skin is set to "bruce_banner", turn him green
      *  local skin = go.get("#hero", "skin")
@@ -125,7 +127,7 @@ namespace dmGameSystem
      *      go.set("#hero", "skin", hash("green"))
      *  end
      * end
-     * </pre>
+     * ```
      */
 
     int SpineComp_Play(lua_State* L)
@@ -168,34 +170,43 @@ namespace dmGameSystem
     }
 
     /*# play an animation on a spine model
+     * Plays a specified animation on a spine model component.
      *
      * @name spine.play_anim
-     * @param url the spine model for which to play the animation (url)
-     * @param anim_id id of the animation to play (string|hash)
-     * @param playback playback mode of the animation (constant)
-     * <ul>
-     *   <li><code>go.PLAYBACK_ONCE_FORWARD</code></li>
-     *   <li><code>go.PLAYBACK_ONCE_BACKWARD</code></li>
-     *   <li><code>go.PLAYBACK_ONCE_PINGPONG</code></li>
-     *   <li><code>go.PLAYBACK_LOOP_FORWARD</code></li>
-     *   <li><code>go.PLAYBACK_LOOP_BACKWARD</code></li>
-     *   <li><code>go.PLAYBACK_LOOP_PINGPONG</code></li>
-     * </ul>
-     * @param [play_properties] optional table with properties (table)
-     * <ul>
-     *   <li><code>blend_duration</code> duration of a linear blend between the current and new animation (number)</li>
-     *   <li><code>offset</code> the normalized initial value of the animation cursor when the animation starts playing (number)</li>
-     *   <li><code>playback_rate</code> the rate with which the animation will be played. Must be positive (number)</li>
-     * </ul>
-     * @param [complete_function] function to call when the animation has completed (function)
+     * @param url [type:string|hash|url] the spine model for which to play the animation
+     * @param anim_id [type:string|hash] id of the animation to play
+     * @param playback [type:constant] playback mode of the animation
+     *
+     * - `go.PLAYBACK_ONCE_FORWARD`
+     * - `go.PLAYBACK_ONCE_BACKWARD`
+     * - `go.PLAYBACK_ONCE_PINGPONG`
+     * - `go.PLAYBACK_LOOP_FORWARD`
+     * - `go.PLAYBACK_LOOP_BACKWARD`
+     * - `go.PLAYBACK_LOOP_PINGPONG`
+     *
+     * @param [play_properties] [type:table] optional table with properties:
+     *
+     * `blend_duration`
+     * : [type:number] duration of a linear blend between the current and new animation.
+     *
+     * `offset`
+     * : [type:number] the normalized initial value of the animation cursor when the animation starts playing.
+     *
+     * `playback_rate`
+     * : [type:number] the rate with which the animation will be played. Must be positive.
+     *
+     * @param [complete_function] [type:function(self)] function to call when the animation has completed
+     *
+     * `self`
+     * : [type:object] The current object.
+     *
      * @examples
-     * <p>
+     *
      * The following examples assumes that the spine model has id "spinemodel".
-     * </p>
-     * <p>
+     *
      * How to play the "jump" animation followed by the "run" animation:
-     * </p>
-     * <pre>
+     *
+     * ```lua
      * function init(self)
      *     local url = msg.url("#spinemodel")
      *     local play_properties = { blend_duration = 0.1 }
@@ -205,7 +216,7 @@ namespace dmGameSystem
      *         spine.play_anim(url, "run", go.PLAYBACK_LOOP_FORWARD, properties)
      *     end)
      * end
-     * </pre>
+     * ```
      */
     int SpineComp_PlayAnim(lua_State* L)
     {
@@ -265,21 +276,21 @@ namespace dmGameSystem
     }
 
     /*# cancel all animation on a spine model
+     * Cancels all running animations on a specified spine model component.
      *
      * @name spine.cancel
-     * @param url the spine model for which to cancel the animation (url)
+     * @param url [type:string|hash|url] the spine model for which to cancel the animation
      * @examples
-     * <p>
+     *
      * The following examples assumes that the spine model has id "spinemodel".
-     * </p>
-     * <p>
+     *
      * How to cancel all animation:
-     * </p>
-     * <pre>
+     *
+     * ```lua
      * function init(self)
      *     spine.cancel("#spinemodel")
      * end
-     * </pre>
+     * ```
      */
     int SpineComp_Cancel(lua_State* L)
     {
@@ -299,27 +310,27 @@ namespace dmGameSystem
     }
 
     /*# retrieve the game object corresponding to a spine model skeleton bone
+     * Returns the id of the game object that corresponds to a specified skeleton bone.
      * The returned game object can be used for parenting and transform queries.
-     * This function has complexity O(n), where n is the number of bones in the spine model skeleton.
+     * This function has complexity `O(n)`, where `n` is the number of bones in the spine model skeleton.
      * Game objects corresponding to a spine model skeleton bone can not be individually deleted.
-     * Only available from .script files.
      *
      * @name spine.get_go
-     * @param url the spine model to query (url)
-     * @param bone_id id of the corresponding bone (string|hash)
-     * @return id of the game object
+     * @param url [type:string|hash|url] the spine model to query
+     * @param bone_id [type:string|hash] id of the corresponding bone
+     * @return id [type:hash] id of the game object
      * @examples
-     * <p>
+     *
      * The following examples assumes that the spine model has id "spinemodel".
-     * <p>
+     *
      * How to parent the game object of the calling script to the "right_hand" bone of the spine model in a player game object:
-     * </p>
-     * <pre>
+     *
+     * ```lua
      * function init(self)
      *     local parent = spine.get_go("player#spinemodel", "right_hand")
      *     msg.post(".", "set_parent", {parent_id = parent})
      * end
-     * </pre>
+     * ```
      */
     int SpineComp_GetGO(lua_State* L)
     {
@@ -373,23 +384,25 @@ namespace dmGameSystem
     }
 
     /*# set the target position of an IK constraint object
-     * Only available from .script files.
+     *
+     * Sets a static (vector3) target position of an inverse kinematic (IK) object.
      *
      * @name spine.set_ik_target_position
-     * @param url the spine model containing the object (url)
-     * @param ik_constraint_id id of the corresponding IK constraint object (string|hash)
-     * @param position (vec3)
+     * @param url [type:string|hash|url] the spine model containing the object
+     * @param ik_constraint_id [type:string|hash] id of the corresponding IK constraint object
+     * @param position [type:vector3] target position
      * @examples
-     * <p>
+     *
      * The following example assumes that the spine model has id "spinemodel".
-     * <p>
+     *
      * How to set the target IK position of the right_hand_constraint constraint object of the player object
-     * </p>
-     * <pre>
+     *
+     * ```lua
      * function init(self)
-     *     spine.set_ik_target_position("player#spinemodel", "right_hand_constraint", vmath.vector3(1,2,3))
+     *  local pos = vmath.vector3(1, 2, 3)
+     *  spine.set_ik_target_position("player#spinemodel", "right_hand_constraint", pos)
      * end
-     * </pre>
+     * ```
      */
     int SpineComp_SetIKTargetPosition(lua_State* L)
     {
@@ -417,24 +430,27 @@ namespace dmGameSystem
     }
 
     /*# set the IK constraint object target position to follow position of a game object
-     * Only available from .script files.
+     *
+     * Sets a game object as target position of an inverse kinematic (IK) object. As the
+     * target game object's position is updated, the constraint object is updated with the
+     * new position.
      *
      * @name spine.set_ik_target
-     * @param url the spine model containing the object (url)
-     * @param ik_constraint_id id of the corresponding IK constraint object (string|hash)
-     * @param target_url target game object (url)
+     * @param url [type:string|hash|url] the spine model containing the object
+     * @param ik_constraint_id [type:string|hash] id of the corresponding IK constraint object
+     * @param target_url [type:string|hash|url] target game object
      * @examples
-     * <p>
+     *
      * The following example assumes that the spine model has id "spinemodel".
-     * <p>
+     *
      * How to set the target IK position of the right_hand_constraint constraint object of the player object
      * to follow the position of game object with url "some_game_object"
-     * </p>
-     * <pre>
+     *
+     * ```lua
      * function init(self)
      *     spine.set_ik_target("player#spinemodel", "right_hand_constraint", "some_game_object")
      * end
-     * </pre>
+     * ```
      */
     int SpineComp_SetIKTarget(lua_State* L)
     {
@@ -473,28 +489,28 @@ namespace dmGameSystem
     }
 
     /*# set a shader constant for a spine model
+     * Sets a shader constant for a spine model component.
      * The constant must be defined in the material assigned to the spine model.
      * Setting a constant through this function will override the value set for that constant in the material.
      * The value will be overridden until spine.reset_constant is called.
      * Which spine model to set a constant for is identified by the URL.
      *
      * @name spine.set_constant
-     * @param url the spine model that should have a constant set (url)
-     * @param name of the constant (string|hash)
-     * @param value of the constant (vec4)
+     * @param url [type:string|hash|url] the spine model that should have a constant set
+     * @param constant [type:string|hash] name of the constant
+     * @param value [type:vector4] value of the constant
      * @examples
-     * <p>
-     * The following examples assumes that the spine model has id "spinemodel" and that the default-material in builtins is used.
-     * If you assign a custom material to the spine model, you can set the constants defined there in the same manner.
-     * </p>
-     * <p>
+     *
+     * The following examples assumes that the spine model has id "spinemodel" and that the default-material in builtins is used, which defines the constant "tint".
+     * If you assign a custom material to the sprite, you can reset the constants defined there in the same manner.
+     *
      * How to tint a spine model to red:
-     * </p>
-     * <pre>
+     *
+     * ```lua
      * function init(self)
      *     spine.set_constant("#spinemodel", "tint", vmath.vector4(1, 0, 0, 1))
      * end
-     * </pre>
+     * ```
      */
     int SpineComp_SetConstant(lua_State* L)
     {
@@ -519,26 +535,26 @@ namespace dmGameSystem
     }
 
     /*# reset a shader constant for a spine model
+     * Resets a shader constant for a spine model component.
      * The constant must be defined in the material assigned to the spine model.
      * Resetting a constant through this function implies that the value defined in the material will be used.
      * Which spine model to reset a constant for is identified by the URL.
      *
      * @name spine.reset_constant
-     * @param url the spine model that should have a constant reset (url)
-     * @param name of the constant (string|hash)
+     * @param url [type:string|hash|url] the spine model that should have a constant reset
+     * @param constant [type:string|hash] name of the constant
      * @examples
-     * <p>
-     * The following examples assumes that the spine model has id "spinemodel" and that the default-material in builtins is used.
-     * If you assign a custom material to the spine model, you can reset the constants defined there in the same manner.
-     * </p>
-     * <p>
+     *
+     * The following examples assumes that the spine model has id "spinemodel" and that the default-material in builtins is used, which defines the constant "tint".
+     * If you assign a custom material to the sprite, you can reset the constants defined there in the same manner.
+     *
      * How to reset the tinting of a spine model:
-     * </p>
-     * <pre>
+     *
+     * ```lua
      * function init(self)
      *     spine.reset_constant("#spinemodel", "tint")
      * end
-     * </pre>
+     * ```
      */
     int SpineComp_ResetConstant(lua_State* L)
     {
