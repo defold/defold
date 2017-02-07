a90e92ae63fb6d4a849773d477071fa9ee1ae43d
Mathias Westerdahl <mathias.westerdahl@king.com>
2017-02-07 17:00:33 +0100

    DEF-2478 Added C++ error reporting in editor 1

--- com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/bundle/BundleHelper.java
+++ com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/bundle/BundleHelper.java
@@ -12,9 +12,12 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
@@ -24,8 +27,12 @@ import com.defold.extender.client.ExtenderClientException;
 import com.defold.extender.client.ExtenderResource;
 import com.dynamo.bob.Bob;
 import com.dynamo.bob.CompileExceptionError;
+import com.dynamo.bob.MultipleCompileExceptionError;
 import com.dynamo.bob.Platform;
 import com.dynamo.bob.Project;
+import com.dynamo.bob.TaskResult;
+import com.dynamo.bob.fs.IResource;
+import com.dynamo.bob.pipeline.ExtenderUtil;
 import com.dynamo.bob.util.BobProjectProperties;
 import com.samskivert.mustache.Mustache;
 import com.samskivert.mustache.Template;
@@ -139,7 +146,36 @@ public class BundleHelper {
         return this;
     }
 
-    public static void buildEngineRemote(ExtenderClient extender, String platform, String sdkVersion, List<ExtenderResource> allSource, File logFile, String srcName, File outputEngine) throws CompileExceptionError {
+    static class ResourceInfo
+    {
+        public String resource;
+        public String message;
+        public int lineNumber;
+
+        public ResourceInfo(String r, int l, String msg) {
+            resource = r;
+            lineNumber = l;
+            message = msg;
+        }
+    };
+
+    private static Pattern gccRe = Pattern.compile("^\\/tmp\\/upload[0-9]+\\/(.*):([0-9]+):([0-9]+):\\s+(.*)");
+
+    private static void parseLog(String log, List<ResourceInfo> infos) {
+        String[] lines = log.split("\n");
+            for (String line : lines) {
+
+            Matcher m = BundleHelper.gccRe.matcher(line);
+            if (m.matches()) {
+                String resource = m.group(1);
+                int lineNumber = Integer.parseInt(m.group(2)); // column is group 3
+                String msg = m.group(4);
+                infos.add( new BundleHelper.ResourceInfo(resource, lineNumber, msg) );
+            }
+        }
+    }
+
+    public static void buildEngineRemote(ExtenderClient extender, String platform, String sdkVersion, List<ExtenderResource> allSource, File logFile, String srcName, File outputEngine) throws CompileExceptionError, MultipleCompileExceptionError {
         File zipFile = null;
 
         try {
@@ -156,6 +192,24 @@ public class BundleHelper {
             if (logFile != null) {
                 try {
                     buildError = FileUtils.readFileToString(logFile);
+
+                    List<ResourceInfo> infos = new ArrayList<>();
+                    parseLog(buildError, infos);
+
+                    if (infos.size() > 0) {
+
+                        MultipleCompileExceptionError exception = new MultipleCompileExceptionError("Build error", e.getCause());
+                        for (ResourceInfo info : infos) {
+                            IResource resource = ExtenderUtil.getResource(info.resource, allSource);
+                            if (resource != null) {
+                                exception.addError(resource, info.message, info.lineNumber);
+                            }
+                        }
+
+                        if (exception.errors.size() > 0) {
+                            throw exception;
+                        } // else, throw the entire log, so the user has a chance to inspect the log and actually find the error
+                    }
                 } catch (IOException ioe) {
                     buildError = "<failed reading log>";
                 }
