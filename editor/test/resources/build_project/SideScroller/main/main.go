6a94fc950c6a5a8a0dc75dceb71110fc27a2d6cc
Mats Gisselson <mats.gisselson@king.com>
2017-02-10 18:20:28 +0100

    DEFEDIT-737 Particle system orientation fixes

--- editor/test/resources/build_project/SideScroller/main/main.go
+++ editor/test/resources/build_project/SideScroller/main/main.go
@@ -59,6 +59,21 @@ components {
   }
 }
 components {
+  id: "pfx2"
+  component: "/main/tail.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
   id: "sound"
   component: "/main/sound.sound"
   position {
