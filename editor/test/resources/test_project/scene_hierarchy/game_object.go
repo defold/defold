aee5b99fbfe8ba46d9d4fe7a08f52553e3511197
Mats Gisselson <mats.gisselson@king.com>
2017-02-15 10:41:31 +0100

    DEFEDIT-490 Work-in-progress node id assignment fixes in scene generation

---
+++ editor/test/resources/test_project/scene_hierarchy/game_object.go
@@ -0,0 +1,35 @@
+components {
+  id: "sprite"
+  component: "/scene_hierarchy/sprite.sprite"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "embedded_sprite"
+  type: "sprite"
+  data: "tile_set: \"/scene_hierarchy/atlas.atlas\"\n"
+  "default_animation: \"logo\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
