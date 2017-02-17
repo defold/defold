602865b59271ff20e3b1e6dd9959dac6840ded3e
Andreas Tadic <andreas.tadic@king.com>
2017-02-17 13:26:53 +0100

    DEF-2416. Documentation of Native Extensions (#1329)
    * DEF-2416. Documentation of Native Extensions
    
    * Pipeline helper files for SDK files (install, doc)
    * Documentation formatted to new documentation pipeline format
    * Documentation fixes/additions

--- engine/dlib/src/dmsdk/dlib/align.h
+++ engine/dlib/src/dmsdk/dlib/align.h
@@ -1,18 +1,52 @@
 #ifndef DMSDK_ALIGN_H
 #define DMSDK_ALIGN_H
 
-/**
- * Alignment macro. For compiler compatibility use the macro
- * between the type and the variable, e.g. int DM_ALIGNED(16) x;
- * @param a number of bytes to align to
+/*# SDK Align API documentation
+ * [file:<dmsdk/dlib/align.h>]
+ *
+ * Alignment macros. Use for compiler compatibility
+ *
+ * @document
+ * @name Align
+ * @namespace dmAlign
+ */
+
+/*# alignment macro
+ *
+ * Data structure alignment
+ *
+ * @macro
+ * @name DM_ALIGNED
+ * @param a [type:int] number of bytes to align to
+ *
+ * @examples
+ *
+ * Align m_Data to 16 bytes alignment boundary:
+ *
+ * ```cpp
+ * int DM_ALIGNED(16) m_Data;
+ * ```
  */
 #define DM_ALIGNED(a)
 #undef DM_ALIGNED
 
-/**
- * Align a value to a boundary.
- * @param x value to align
- * @param a alignment boundary
+
+/*# value alignment macro
+ *
+ * Align a value to a boundary
+ *
+ * @macro
+ * @name DM_ALIGN
+ * @param x [type:int] value to align
+ * @param a [type:int] alignment boundary
+ *
+ * @examples
+ *
+ * Align 24 to 16 alignment boundary. results is 16:
+ *
+ * ```cpp
+ * int result = DM_ALIGN(24, 16);
+ * ```
  */
 #define DM_ALIGN(x, a) (((uintptr_t) (x) + (a-1)) & ~(a-1))
 
