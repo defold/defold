6366d81243ba0d9e5624b868e55ff87ace6b19fe
Mats Gisselson <mats.gisselson@king.com>
2017-02-23 18:58:17 +0100

    DEFEDIT-583 Support resolving conflicts with moved or deleted files during sync

--- editor/src/clj/editor/git.clj
+++ editor/src/clj/editor/git.clj
@@ -12,7 +12,7 @@
            [org.eclipse.jgit.api Git ResetCommand$ResetType]
            [org.eclipse.jgit.api.errors StashApplyFailureException]
            [org.eclipse.jgit.diff DiffEntry RenameDetector]
-           [org.eclipse.jgit.lib BatchingProgressMonitor Repository]
+           [org.eclipse.jgit.lib BatchingProgressMonitor ObjectId Repository]
            [org.eclipse.jgit.revwalk RevCommit RevWalk]
            org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
            [org.eclipse.jgit.treewalk FileTreeIterator TreeWalk]
@@ -120,25 +120,23 @@
   (io/file (str (worktree git) "/" file-path)))
 
 (defn show-file
-  ([^Git git name]
+  (^bytes [^Git git name]
    (show-file git name "HEAD"))
-  ([^Git git name ref]
-   (let [repo         (.getRepository git)
-         lastCommitId (.resolve repo ref)
-         rw           (RevWalk. repo)
-         commit       (.parseCommit rw lastCommitId)
-         tree         (.getTree commit)
-         tw           (TreeWalk. repo)]
-     (.addTree tw tree)
-     (.setRecursive tw true)
-     (.setFilter tw (PathFilter/create name))
-     (.next tw)
-     (let [id     (.getObjectId tw 0)
-           loader (.open repo id)
-           ret    (.getBytes loader)]
-       (.dispose rw)
-       (.close repo)
-       ret))))
+  (^bytes [^Git git name ref]
+   (with-open [repo (.getRepository git)
+               rw (RevWalk. repo)]
+     (let [last-commit-id (.resolve repo ref)
+           commit (.parseCommit rw last-commit-id)
+           tree (.getTree commit)]
+       (with-open [tw (TreeWalk. repo)]
+         (.addTree tw tree)
+         (.setRecursive tw true)
+         (.setFilter tw (PathFilter/create name))
+         (.next tw)
+         (let [id (.getObjectId tw 0)]
+           (when (not= (ObjectId/zeroId) id)
+             (let [loader (.open repo id)]
+               (.getBytes loader)))))))))
 
 (defn pull [^Git git ^UsernamePasswordCredentialsProvider creds]
   (-> (.pull git)
