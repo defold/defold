96650746f97fbc083c1cea8af4b36e3dc87d2697
Ragnar Svensson <ragnar.svensson@king.com>
2017-03-03 13:07:36 +0100

    DEFEDIT-808 Moved bob.jar installation from 'pack' to 'local-jars'

--- editor/tasks/leiningen/local_jars.clj
+++ editor/tasks/leiningen/local_jars.clj
@@ -1,10 +1,14 @@
 (ns leiningen.local-jars
-  (:require [clojure.java.shell :as shell]
+  (:require [clojure.java.io :as io]
+            [clojure.java.shell :as shell]
             [clojure.string :as string]
             [cemerick.pomegranate.aether :as aether]
-            [leiningen.core.main :as main])
+            [leiningen.core.main :as main]
+            [leiningen.util.download :as dl])
   (:import (java.io File)
            (java.nio.file Paths)))
+
+(defn dynamo-home [] (get (System/getenv) "DYNAMO_HOME"))
 
 (def ^:private jar-decls
   [{:artifact-id "openmali"
@@ -28,10 +32,21 @@
    "-Dpackaging=jar"
    (str "-Dversion=" version)])
 
+(defn bob-artifact-file ^File
+  [sha]
+  (if sha
+    (dl/download (io/as-url (format "http://d.defold.com/archive/%s/bob/bob.jar" sha)))
+    (io/file (dynamo-home) "share/java/bob.jar")))
+
 (defn local-jars
   "Install local jar dependencies into the ~/.m2 Maven repository."
-  [_project & _arg-list]
-  (doseq [{:keys [group-id artifact-id version jar-file]} (sort-by :jar-file jar-decls)]
-       (main/info (format "Installing %s" jar-file))
-       (aether/install-artifacts
-         :files {[(symbol group-id artifact-id) version] jar-file})))
+  [_project & [git-sha]]
+  (let [jar-decls (->> (conj jar-decls {:artifact-id "bob"
+                                        :group-id "com.defold.lib"
+                                        :jar-file (.getAbsolutePath (bob-artifact-file git-sha))
+                                        :version "1.0"})
+                    (sort-by :jar-file))]
+    (doseq [{:keys [group-id artifact-id version jar-file]} jar-decls]
+      (main/info (format "Installing %s" jar-file))
+      (aether/install-artifacts
+        :files {[(symbol group-id artifact-id) version] jar-file}))))
