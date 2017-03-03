3371ea85dbc3670404a3b6cc34812917c55dd922
Ragnar Svensson <ragnar.svensson@king.com>
2017-03-03 14:56:05 +0100

    DEFEDIT-808 Fixed installation of bob.jar into maven
    * Separated out download code into util ns
    * Moved bob installation from 'pack' to 'local-jars'
    * Downloads from s3, fallbacks to DYNAMO_HOME bob.jar
    * Removed lots of old code

--- editor/tasks/leiningen/local_jars.clj
+++ editor/tasks/leiningen/local_jars.clj
@@ -16,37 +16,27 @@
     :jar-file "../com.dynamo.cr/com.dynamo.cr.bob/lib/openmali.jar"
     :version "1.0"}])
 
-(defn- working-directory []
-  (-> (Paths/get "" (into-array String [])) .toAbsolutePath .toString))
-
-(defn- maven-exe-path [dynamo-home]
-  (string/join File/separator [dynamo-home "ext" "share" "maven" "bin" "mvn"]))
-
-(defn- maven-install-command [mvn-exe-path {:keys [jar-file group-id artifact-id version] :as _jar-decl}]
-  [mvn-exe-path
-   "install:install-file"
-   (str "-DartifactId=" artifact-id)
-   "-DcreateChecksum=true"
-   (str "-Dfile=" jar-file)
-   (str "-DgroupId=" group-id)
-   "-Dpackaging=jar"
-   (str "-Dversion=" version)])
+(defn- git-sha1 []
+  (-> (shell/sh "git" "rev-parse" "HEAD")
+    :out
+    string/trim))
 
 (defn bob-artifact-file ^File
   [sha]
-  (if sha
+  (try
     (dl/download (io/as-url (format "http://d.defold.com/archive/%s/bob/bob.jar" sha)))
-    (io/file (dynamo-home) "share/java/bob.jar")))
+    (catch java.io.FileNotFoundException e
+      ;; Fallback to local bob
+      (io/file (dynamo-home) "share/java/bob.jar"))))
 
 (defn local-jars
   "Install local jar dependencies into the ~/.m2 Maven repository."
-  [_project & [git-sha]]
-  (let [jar-decls (->> (conj jar-decls {:artifact-id "bob"
-                                        :group-id "com.defold.lib"
-                                        :jar-file (.getAbsolutePath (bob-artifact-file git-sha))
-                                        :version "1.0"})
-                    (sort-by :jar-file))]
-    (doseq [{:keys [group-id artifact-id version jar-file]} jar-decls]
+  [_project]
+  (let [jar-decls (conj jar-decls {:artifact-id "bob"
+                                      :group-id "com.defold.lib"
+                                      :jar-file (.getAbsolutePath (bob-artifact-file (git-sha1)))
+                                      :version "1.0"})]
+    (doseq [{:keys [group-id artifact-id version jar-file]} (sort-by :jar-file jar-decls)]
       (main/info (format "Installing %s" jar-file))
       (aether/install-artifacts
         :files {[(symbol group-id artifact-id) version] jar-file}))))
