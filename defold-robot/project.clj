4b361662c0a0cae2adc35643619a48b65e72c00f
Ragnar Svensson <ragnar.svensson@king.com>
2017-03-20 12:29:05 +0100

    DEFEDIT-837 Robot for automated smoke testing WIP

---
+++ defold-robot/project.clj
@@ -0,0 +1,10 @@
+(defproject defold-robot "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [org.clojure/data.json "0.2.6"]]
+  :resource-paths ["resources"]
+  :main defold-robot.robot)
