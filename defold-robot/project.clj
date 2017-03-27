6d91fd2efc5abc31569eadf384a179ffbf338ae7
Ragnar Svensson <ragnar.svensson@king.com>
2017-03-27 08:25:02 +0200

    DEFEDIT-837 Smoke testing WIP
    * Fixed aot bundling for the robot
    * Added support for listening to logs
    * Robot produces HTML directly along with images and css
    * Fixed bug where editor wouldn't open a project when receiving it as boot arg

--- defold-robot/project.clj
+++ defold-robot/project.clj
@@ -1,10 +1,12 @@
-(defproject defold-robot "0.1.0-SNAPSHOT"
+(defproject defold-robot "0.1.0"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://www.defold.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.5"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [org.clojure/data.json "0.2.6"]
+                 [commons-io/commons-io "2.4"]]
   :resource-paths ["resources"]
-  :main defold-robot.robot)
+  :main defold-robot.robot
+  :aot [defold-robot.robot])
