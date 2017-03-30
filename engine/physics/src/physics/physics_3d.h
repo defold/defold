24604222d0df32f88e93aa1b64175e4ad0ddbfcb
Björn Ritzl <bjorn.ritzl@gmail.com>
2017-03-30 09:26:53 +0200

    DEF-2026 - Make buffersize for raycast requests configurable (#1431)

--- engine/physics/src/physics/physics_3d.h
+++ engine/physics/src/physics/physics_3d.h
@@ -39,6 +39,7 @@ namespace dmPhysics
         float                       m_InvScale;
         float                       m_ContactImpulseLimit;
         float                       m_TriggerEnterLimit;
+        int                         m_RayCastLimit;
     };
 
     inline void ToBt(const Vectormath::Aos::Point3& p0, btVector3& p1, float scale)
