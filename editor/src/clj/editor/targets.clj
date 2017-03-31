18d758c766a22e4af54b8ff820f56fb41937c6bb
Ragnar Svensson <ragnar.svensson@king.com>
2017-03-31 14:43:56 +0200

    DEFEDIT-863 Various target device improvements
    * In some cases, we determined the local address relative the target incorrectly, resulting in 0.0.0.0
    * Manual target device IP now works, also shows error messages in case it could not be reached
    * Game logging works when running on target devices
    * Incoming 'SSDP services' are now filtered for 'Defold' before we ask them for data; improves response times
    * Contacting the 'Defold' target devices is done concurrently; improves response times
    * Moved target-handlers from app-view to targets ns
    * The Project > Target menu always shows which device will be used to (attempt to) launch on
    * The game log shows which device we are running on
    * All non-defold services are filtered out before communication starts
    * Removed all 'blacklist' functionality - previously used to detect problematic non-defold devices, which are now filtered out instead

--- editor/src/clj/editor/targets.clj
+++ editor/src/clj/editor/targets.clj
@@ -2,38 +2,44 @@
   (:require [clojure
              [string :as str]
              [xml :as xml]]
+            [editor.dialogs :as dialogs]
+            [editor.handler :as handler]
+            [editor.prefs :as prefs]
             [editor.ui :as ui]
-            [editor.dialogs :as dialogs])
-  (:import [com.dynamo.upnp DeviceInfo SSDP SSDP$Logger]
+            [editor.util :as util])
+  (:import [clojure.lang ExceptionInfo]
+           [com.dynamo.upnp DeviceInfo SSDP SSDP$Logger]
            [java.io ByteArrayInputStream ByteArrayOutputStream IOException]
-           [java.net MalformedURLException SocketTimeoutException URL URLConnection]
+           [java.net InetAddress MalformedURLException NetworkInterface SocketTimeoutException URL URLConnection]
            [javafx.scene Parent Scene]
            [javafx.scene.control TextArea]
            [javafx.scene.input KeyCode KeyEvent]
            [javafx.stage Modality]
-           org.apache.commons.io.IOUtils))
+           [org.apache.commons.io IOUtils]))
 
 (set! *warn-on-reflection* true)
 
 (defonce ^:const local-target
   {:name "Local"
-   :url  "http://localhost:8001"
+   :url  "http://127.0.0.1:8001"
+   :address "127.0.0.1"
    ;; :local-address must be an ipv4 address. dmengine
    ;; will resolve "localhost" as an ipv6 address and not find
    ;; the editor web server.
    :local-address "127.0.0.1"})
 
-(defonce ^:private targets (atom #{local-target}))
-(defonce ^:private blacklist (atom #{}))
-(defonce ^:private descriptions (atom {}))
+(defonce ^:private targets (atom [local-target]))
+(defonce ^:private manual-device (atom nil))
 (defonce ^:private last-search (atom 0))
 (defonce ^:private running (atom false))
 (defonce ^:private worker (atom nil))
 (defonce ^:private event-log (atom []))
 (defonce ^:private ssdp-service (atom nil))
+(defonce ^:private defold-upnp-attrs {:xmlns:defold "urn:schemas-defold-com:DEFOLD-1-0", :xmlns "urn:schemas-upnp-org:device-1-0"})
 
 (def ^:const search-interval-disconnected (* 5 1000))
 (def ^:const search-interval-connected (* 30 1000))
+(def ^:const update-interval 1000)
 (def ^:const timeout 200)
 (def ^:const max-log-entries 512)
 
@@ -44,10 +50,11 @@
                                 (.setReadTimeout timeout))
         input  (.getInputStream conn)
         output (ByteArrayOutputStream.)]
-    (IOUtils/copy input output)
-    (.close input)
-    (.close output)
-    (.toString output "UTF8")))
+    (try
+      (IOUtils/copy input output)
+      (.toString output "UTF8")
+      (finally
+        (.close input)))))
 
 (defn- tag->val [tag tags]
   (->> tags
@@ -55,20 +62,6 @@
        first
        :content
        first))
-
-(defn- desc->target [desc local-address]
-  (when-let [tags (and (= {:xmlns:defold "urn:schemas-defold-com:DEFOLD-1-0", :xmlns "urn:schemas-upnp-org:device-1-0"}
-                          (:attrs desc))
-                       (->> desc :content (filter #(= :device (:tag %))) first :content))]
-    (when (some->> tags (tag->val :manufacturer) str/lower-case (= "defold"))
-      (let [target {:name     (tag->val :friendlyName tags)
-                    :model    (tag->val :modelName tags)
-                    :udn      (tag->val :UDN tags)
-                    :url      (tag->val :defold:url tags)
-                    :log-port (tag->val :defold:logPort tags)
-                    :local-address local-address}]
-        (when (not-any? nil? (vals target))
-          target)))))
 
 (defn- log [message]
   (swap! event-log (fn [xs]
@@ -80,83 +73,108 @@
                        xs)))
   nil)
 
-(defn- process-devices [{:keys [blacklist-atom descriptions-atom fetch-url-fn log-fn]} devices]
-  (reduce (fn [{:keys [targets blacklist descriptions]} ^DeviceInfo device]
-            (let [loc        (.get (.headers device) "LOCATION")
-                  ^URL url   (try (URL. loc)
-                                  (catch MalformedURLException _
-                                    (log-fn (format "[%s] not a valid URL" loc))
-                                    nil))
-                  maybe-desc (and url
-                                  (not (contains? blacklist url))
-                                  (or (get descriptions url)
-                                      (let [fetch-result (try (or (fetch-url-fn url)
-                                                                  (throw (NullPointerException. (format "The supplied fetch-url-fn broke the contract by returning nil for %s" url))))
-                                                              (catch SocketTimeoutException _
-                                                                (log-fn (format "[%s] timed out getting XML description" loc))
-                                                                ::socket-timeout)
-                                                              (catch IOException _
-                                                                (log-fn (format "[%s] error getting XML description" loc))
-                                                                ::fetch-error))]
-                                        (if-not (string? fetch-result)
-                                          fetch-result
-                                          (try (xml/parse (ByteArrayInputStream. (.getBytes ^String fetch-result)))
-                                               (catch Exception _
-                                                 (log-fn (format "[%s] error parsing XML description" loc))
-                                                 ::parse-error))))))
-                  target     (and (map? maybe-desc)
-                                  (desc->target maybe-desc (.localAddress device)))]
-
-              (when (and (not target) (not= ::socket-timeout maybe-desc))
-                (log-fn (format "[%s] not a Defold target" loc)))
-
-              {:targets      (if target
-                               (conj targets target)
-                               targets)
-               :blacklist    (if (and url (not target) (not= ::socket-timeout maybe-desc))
-                               (conj blacklist url)
-                               blacklist)
-               :descriptions (if (map? maybe-desc)
-                               (assoc descriptions url maybe-desc)
-                               descriptions)}))
-          {:targets #{}
-           :blacklist @blacklist-atom
-           :descriptions @descriptions-atom}
-          devices))
-
 (def ^:private update-targets-context
   {:targets-atom targets
-   :blacklist-atom blacklist
-   :descriptions-atom descriptions
    :log-fn log
    :fetch-url-fn http-get
    :on-targets-changed-fn ui/invalidate-menus!})
 
-(defn update-targets! [{:keys [targets-atom descriptions-atom blacklist-atom log-fn on-targets-changed-fn] :as context} devices]
-  (let [{found-targets :targets
-         updated-blacklist :blacklist
-         updated-descriptions :descriptions} (process-devices context devices)]
-    (when (not-empty found-targets)
-      (log-fn (format "Found engine(s) [%s]" (str/join "," (mapv (fn [t] (let [url (URL. (:url t))]
-                                                                           (format "%s (%s)" (:name t) (.getHost url)))) found-targets)))))
-    (let [old-targets @targets-atom]
-      (reset! descriptions-atom updated-descriptions)
-      (reset! blacklist-atom updated-blacklist)
-      (reset! targets-atom (or (not-empty found-targets) #{local-target}))
-      (when (or
-              ;; We found new/different engines
-              (and (not-empty found-targets)
-                   (not= found-targets old-targets))
+(defn- desc->target [desc local-address]
+   (when-let [tags (and (= {:xmlns:defold "urn:schemas-defold-com:DEFOLD-1-0", :xmlns "urn:schemas-upnp-org:device-1-0"}
+                           (:attrs desc))
+                        (->> desc :content (filter #(= :device (:tag %))) first :content))]
+     (when (some->> tags (tag->val :manufacturer) str/lower-case (= "defold"))
+       (let [target {:name     (tag->val :friendlyName tags)
+                     :model    (tag->val :modelName tags)
+                     :udn      (tag->val :UDN tags)
+                     :url      (tag->val :defold:url tags)
+                     :log-port (tag->val :defold:logPort tags)
+                     :local-address local-address}]
+         (when (not-any? nil? (vals target))
+           target)))))
 
-              ;; We didn't find any engines (but we had at least one in the list)
-              (and (empty? found-targets)
-                   (not= old-targets #{local-target})))
-        (on-targets-changed-fn)))))
+(defn- device->target [{:keys [fetch-url-fn]} device]
+  ;; The reason we try/throw/catch is so that this function can run through pmap,
+  ;; yet return sensible error messages in case of failure for later logging
+  (try
+    (when-let [location (get-in device [:headers "LOCATION"])]
+      (let [url (try
+                  (URL. location)
+                  (catch MalformedURLException e
+                    (throw (ex-info (format "[%s] not a valid URL: %s" location (.getMessage e)) {} e))))
+            response (try
+                       (fetch-url-fn url)
+                       (catch SocketTimeoutException e
+                         (throw (ex-info (format "[%s] timed out getting XML description: %s" location (.getMessage e)) {} e)))
+                       (catch IOException e
+                         (throw (ex-info (format "[%s] error getting XML description: %s" location (.getMessage e)) {} e))))
+            desc (try
+                   (xml/parse (ByteArrayInputStream. (.getBytes ^String response)))
+                   (catch Exception e
+                     (throw (ex-info (format "[%s] error parsing XML description: %s" location (.getMessage e)) {} e))))]
+        (when (not= (:attrs desc) defold-upnp-attrs)
+          (throw (ex-info (format "[%s] invalid UPNP attributes: %s" location (:attrs desc)) {})))
+        (let [tags (->> desc :content (filter #(= :device (:tag %))) first :content)
+              address (:address device)
+              local-address (:local-address device)
+              name (if (= address local-address)
+                     "Local"
+                     (tag->val :friendlyName tags))
+              target {:name name
+                      :model (tag->val :modelName tags)
+                      :udn (tag->val :UDN tags)
+                      :url (tag->val :defold:url tags)
+                      :log-port (tag->val :defold:logPort tags)
+                      :address address
+                      :local-address local-address}]
+          target)))
+    (catch ExceptionInfo e
+      (.getMessage e))))
+
+(defn- local-target? [target]
+  (= (:address target) (:local-address target)))
+
+(defn update-targets! [{:keys [targets-atom log-fn on-targets-changed-fn] :as context} devices]
+  (let [devices (if-let [manual-device @manual-device]
+                  (conj devices manual-device)
+                  devices)
+        old-targets @targets-atom
+        target-by-address (into {} (map (fn [t] [(:address t) t]) old-targets))
+        targets-result (pmap (fn [device] (device->target context device)) devices)
+        targets (->> targets-result
+                  (map (fn [device result]
+                         (if (string? result)
+                           (get target-by-address (:address device))
+                           result))
+                    devices)
+                  (filter some?))
+        errors (filter string? targets-result)
+        local-target (or (first (filter local-target? targets)) local-target)
+        external-targets (filter (complement local-target?) targets)
+        targets (vec (distinct (cons local-target (sort-by :name util/natural-order external-targets))))]
+    (doseq [error errors]
+      (log-fn error))
+    (reset! targets-atom targets)
+    (when (not= targets old-targets)
+      (on-targets-changed-fn))))
 
 (defn- search-interval [^SSDP ssdp]
   (if (.isConnected ssdp)
     search-interval-connected
     search-interval-disconnected))
+
+(defn- device->map [^DeviceInfo device]
+  {:address (.address device)
+   :local-address (.localAddress device)
+   :headers (.headers device)
+   :expires (.expires device)})
+
+(defn- defold-service? [device]
+  (let [server (get (:headers device) "SERVER" "")]
+    (str/starts-with? server "Defold")))
+
+(defn- devices [^SSDP ssdp]
+  (filter defold-service? (map device->map (.getDevices ssdp))))
 
 (defn- targets-worker []
   (let [ssdp-service' (SSDP. (reify SSDP$Logger
@@ -166,14 +184,14 @@
         (do
           (reset! ssdp-service ssdp-service')
           (while @running
-            (Thread/sleep 200)
+            (Thread/sleep update-interval)
             (let [now      (System/currentTimeMillis)
                   search?  (>= now (+ @last-search (search-interval ssdp-service')))
                   changed? (.update ssdp-service' search?)]
               (when search?
                 (reset! last-search now))
               (when (or search? changed?)
-                (update-targets! update-targets-context (.getDevices ssdp-service'))))))
+                (update-targets! update-targets-context (devices ssdp-service'))))))
         (do
           (reset! running false)))
       (catch Exception e
@@ -182,16 +200,13 @@
         (.dispose ssdp-service')
         (reset! ssdp-service nil)))))
 
-(defn update! []
+(defn- update! []
   (when-let [^SSDP ss @ssdp-service]
-    (update-targets! update-targets-context (.getDevices ss))
-    (reset! last-search (System/currentTimeMillis))))
+    (update-targets! update-targets-context (devices ss))))
 
 (defn start []
   (when (not @running)
     (reset! running true)
-    (reset! blacklist #{})
-    (reset! descriptions {})
     (reset! worker (future (targets-worker)))))
 
 (defn stop []
@@ -245,6 +260,65 @@
     (ui/on-closed! stage (fn [_]
                            (remove-watch event-log :dialog)))
     (ui/show! stage)))
+
+(defn selected-target [prefs]
+  (let [target-address (prefs/get-prefs prefs "selected-target-address" nil)
+        targets (get-targets)
+        targets-by-address (into {} (map (fn [t] [(:address t) t]) targets))]
+    (get targets-by-address target-address (first targets))))
+
+(defn- select-target! [prefs target]
+  (prefs/set-prefs prefs "selected-target-address" (:address target)))
+
+(handler/defhandler :target :global
+  (run [user-data prefs]
+    (when user-data
+      (select-target! prefs user-data)))
+  (state [user-data prefs]
+         (let [selected-target (selected-target prefs)]
+           (= user-data selected-target)))
+  (options [user-data prefs]
+           (when-not user-data
+             (let [targets     (get-targets)]
+               (mapv (fn [target]
+                       {:label     (format "%s (%s)" (:name target) (:address target))
+                        :command   :target
+                        :check     true
+                        :user-data target})
+                 targets)))))
+
+(defn- locate-device [ip]
+  (when (not-empty ip)
+    (let [inet-addr (InetAddress/getByName ip)
+         n-ifs (SSDP/getMCastInterfaces)
+         device (when-let [^NetworkInterface n-if (first (filter (fn [^NetworkInterface n-if] (.isReachable inet-addr n-if SSDP/SSDP_MCAST_TTL timeout)) n-ifs))]
+                  (when-let [^InetAddress local-address (first (SSDP/getIPv4Addresses n-if))]
+                    {:address ip
+                     :local-address (.getHostAddress local-address)
+                     :headers {"LOCATION" (format "http://%s:8001/upnp" ip)}}))]
+     (if device
+       device
+       (throw (ex-info (format "'%s' could not be reached from this host" ip) {}))))))
+
+(handler/defhandler :target-ip :global
+  (run [prefs]
+    (ui/run-later
+      (loop [manual-ip (dialogs/make-target-ip-dialog (prefs/get-prefs prefs "manual-target-ip" "") nil)]
+        (when (some? manual-ip)
+          (prefs/set-prefs prefs "manual-target-ip" manual-ip)
+          (let [device (try
+                         (locate-device manual-ip)
+                         (catch Exception e (.getMessage e)))]
+            (if (string? device)
+              (recur (dialogs/make-target-ip-dialog manual-ip device))
+              (do
+                (reset! manual-device device)
+                (prefs/set-prefs prefs "selected-target-address" (not-empty manual-ip))
+                (ui/invalidate-menus!)))))))))
+
+(handler/defhandler :target-log :global
+  (run []
+    (ui/run-later (make-target-log-dialog))))
 
 (ui/extend-menu ::menubar :editor.defold-project/project-end
                 [{:label "Target"
