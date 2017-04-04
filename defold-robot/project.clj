e134aeaa33d7c45ca33b1ff348dc8acac50f873d
Ragnar Svensson <ragnar.svensson@king.com>
2017-04-04 11:03:37 +0200

    DEFEDIT-837 defold-robot can be 'lein release'ed into install_ext packages

--- defold-robot/project.clj
+++ defold-robot/project.clj
@@ -6,7 +6,10 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/data.json "0.2.6"]
-                 [commons-io/commons-io "2.4"]]
+                 [commons-io/commons-io "2.4"]
+                 [org.apache.ant/ant "1.10.1"]]
   :resource-paths ["resources"]
   :main defold-robot.robot
-  :aot [defold-robot.robot])
+  :aot [defold-robot.robot]
+  :aliases {"tar-install" ["do" "uberjar" ["run" "-m" "lein-plugins.tar-install" :project/name :project/version :project/target-path]]}
+  :release-tasks [["do" "tar-install"]])
