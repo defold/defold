8a44bd8e735c9ed851c0318b28b0ef2d482779e9
Ragnar Svensson <ragnar.svensson@king.com>
2017-04-10 17:15:58 +0200

    Trigging CI

--- scripts/build.py
+++ scripts/build.py
@@ -680,7 +680,7 @@ class Configuration(object):
 
     def _build_engine_libs(self, skip_tests, skip_codesign, disable_ccache, eclipse):
         self._log('Building libs')
-        # TODO fix redundantly building dlib
+        # TODO fix redundantly building dlib, yes also trigger CI
         libs="dlib ddf particle glfw graphics lua hid input physics resource extension script tracking render rig gameobject gui sound liveupdate gamesys tools record iap push iac adtruth webview facebook crash engine sdk".split()
         for lib in libs:
             self._log('Building %s' % lib)
