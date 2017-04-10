d2e50d74373c60fd2fbf1506474e1eb4fdb2b6c1
Ragnar Svensson <ragnar.svensson@king.com>
2017-04-10 06:37:40 +0200

    DEFEDIT-875 Better error handling in the test-caching

--- build_tools/test_cache.py
+++ build_tools/test_cache.py
@@ -10,14 +10,17 @@ def md5(fname):
 def strip(fname):
     temp_dir = tempfile.gettempdir()
     temp_path = os.path.join(temp_dir, os.path.basename(fname))
-    p = subprocess.Popen(['strip', '-o', temp_path, fname])
-    p.wait()
-    if p.returncode == 0:
-        return temp_path
-    else:
+    try:
+        p = subprocess.Popen(['strip', '-o', temp_path, fname])
+        p.wait()
+        if p.returncode == 0:
+            return temp_path
+        else:
+            return fname
+    except OSError:
         return fname
 
-timeout = 100
+timeout = 150
 db_server = "metrics.defold.com"
 db_port = 8086
 db_name = 'enginetests2'
@@ -30,24 +33,27 @@ def lookup(path):
     conn = httplib.HTTPConnection(db_server, db_port, timeout = timeout)
     query = urllib.quote_plus("SELECT * from %s where hash='%s';" % (measurement, hash))
     url = ("/query?db=%s&q=%s" % (db_name, query))
-    conn.request("GET", url)
-    response = conn.getresponse()
-    if response.status == 200:
-        data = json.loads(response.read())
-        results = data.get(u'results')
-        if not results or len(results) == 0:
-            print('no result')
-            return False
-        series = results[0].get(u'series')
-        if not series or len(series) == 0:
-            print('no series')
-            return False
-        values = series[0].get(u'values')
-        if not values or len(values) == 0:
-            print('no values')
-            return False
-        return True
-    return False
+    try:
+        conn.request("GET", url)
+        response = conn.getresponse()
+        if response.status == 200:
+            data = json.loads(response.read())
+            results = data.get(u'results')
+            if not results or len(results) == 0:
+                print('test caching inactive: no result')
+                return False
+            series = results[0].get(u'series')
+            if not series or len(series) == 0:
+                print('test caching inactive: no series')
+                return False
+            values = series[0].get(u'values')
+            if not values or len(values) == 0:
+                print('test caching inactive: no values')
+                return False
+            return True
+        return False
+    except IOError:
+        return False
 
 def cache(path):
     measurement = path.replace('/', '_')
@@ -56,5 +62,8 @@ def cache(path):
     headers = {"Content-type": "application/x-www-form-urlencoded",
                "Accept": "text/plain"}
     conn = httplib.HTTPConnection(db_server, db_port, timeout = timeout)
-    conn.request("POST", "/write?db=%s" % db_name, "%s,hash=%s value=true" % (measurement, hash), headers)
-    response = conn.getresponse()
+    try:
+        conn.request("POST", "/write?db=%s" % db_name, "%s,hash=%s value=true" % (measurement, hash), headers)
+        response = conn.getresponse()
+    except IOError:
+        pass
