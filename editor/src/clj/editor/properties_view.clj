387d67bf022f220030554e2e571fda22c9850294
Ragnar Svensson <ragnar.svensson@king.com>
2017-05-10 15:56:25 +0200

    DEFEDIT-933 Numbers are rounded to 5 digits of precision in properties view

--- editor/src/clj/editor/properties_view.clj
+++ editor/src/clj/editor/properties_view.clj
@@ -50,8 +50,11 @@
 
 (declare update-field-message)
 
+(defn- format-value [v]
+  (str (if (number? v) (math/round-with-precision v 0.00001) v)))
+
 (defn- update-text-fn [^TextInputControl text values message read-only?]
-  (ui/text! text (str (properties/unify-values values)))
+  (ui/text! text (format-value (properties/unify-values values)))
   (update-field-message [text] message)
   (ui/editable! text (not read-only?)))
 
@@ -137,8 +140,8 @@
                        (.setHgap grid-hgap))
         update-ui-fn (fn [values message read-only?]
                        (doseq [[^TextInputControl t v] (map-indexed (fn [i t]
-                                                                      [t (str (properties/unify-values
-                                                                                (map #(nth % i) values)))])
+                                                                      [t (format-value (properties/unify-values
+                                                                                         (map #(nth % i) values)))])
                                                                     text-fields)]
                          (ui/text! t v)
                          (ui/editable! t (not read-only?)))
@@ -187,8 +190,8 @@
                        (doseq [[^TextInputControl t v] (map (fn [f t]
                                                               (let [get-fn (or (:get-fn f)
                                                                                #(get-in % (:path f)))]
-                                                                [t (str (properties/unify-values
-                                                                         (map get-fn values)))]))
+                                                                [t (format-value (properties/unify-values
+                                                                                  (map get-fn values)))]))
                                                             fields text-fields)]
                          (ui/text! t v)
                          (ui/editable! t (not read-only?)))
@@ -411,7 +414,7 @@
         wrapper      (doto (HBox.)
                        (.setPrefWidth Double/MAX_VALUE))
         update-ui-fn (fn [values message read-only?]
-                       (ui/text! text (properties/unify-values (map str values)))
+                       (ui/text! text (format-value (properties/unify-values values)))
                        (update-field-message [wrapper] message)
                        (ui/editable! text (not read-only?)))]
     (HBox/setHgrow text Priority/ALWAYS)
