db5f26f637a346d645706faa9844b8959f3cba8b
Mathias Westerdahl <mathias.westerdahl@king.com>
2017-05-18 18:34:24 +0200

    Added linux support to crashtool

--- engine/crash/src/tool/crashtool.cpp
+++ engine/crash/src/tool/crashtool.cpp
@@ -4,6 +4,7 @@
 #include "crash.h"
 #include "crash_private.h"
 
+#include <algorithm> // find_if
 #include <iostream>
 #include <stdexcept>
 #include <stdio.h>
@@ -17,7 +18,33 @@ static void Usage()
 	fprintf(stderr, "\n");
 }
 
-#ifdef _WIN32
+// http://stackoverflow.com/a/478960
+static int Exec(const char* cmd, std::string& result) {
+    char buffer[128];
+    result = "";
+
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe)
+    {
+        fprintf(stderr, "popen() failed!");
+        return 1;
+    }
+    while (!feof(pipe)) {
+        if (fgets(buffer, sizeof(buffer), pipe) != NULL)
+            result += buffer;
+    }
+    pclose(pipe);
+    return 0;
+}
+
+static inline std::string &rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(),
+            std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+    return s;
+}
+
+
+#if defined(_WIN32)
 
 #include <Windows.h>
 #include <Dbghelp.h>
@@ -81,6 +108,8 @@ const char* PlatformGetSymbol(PlatformInfo& info, uintptr_t ptr)
     return info.symbol->Name;
 }
 
+// http://stackoverflow.com/questions/17389968/get-linux-executable-load-address-builtin-return-address-and-addr2line
+// _AddressOfReturnAddress, GetModuleInformation + lpBaseOfDll
 
 /*
 #define PSAPI_VERSION 1
@@ -171,24 +200,6 @@ struct PlatformInfo
     char buffer[2048];
 };
 
-// http://stackoverflow.com/a/478960
-int Exec(const char* cmd, std::string& result) {
-    char buffer[128];
-    result = "";
-
-    FILE* pipe = popen(cmd, "r");
-    if (!pipe)
-    {
-        fprintf(stderr, "popen() failed!");
-        return 1;
-    }
-    while (!feof(pipe)) {
-        if (fgets(buffer, sizeof(buffer), pipe) != NULL)
-            result += buffer;
-    }
-    pclose(pipe);
-    return 0;
-}
 
 static int ExtractDSym(char* executable_path, char* targetpath)
 {
@@ -211,10 +222,13 @@ int PlatformInit(char* executable_path, PlatformInfo& info)
     info.executable_path = executable_path;
     if( executable_path )
     {
-        info.basename = strrchr(info.executable_path, '/') + 1;
-        if (!info.basename )
+        info.basename = strrchr(info.executable_path, '/');
+        if (!info.basename)
         {
             info.basename = info.executable_path;
+        } else
+        {
+            info.basename++; // Step past the '/'
         }
 
         char buffer[1024] = {0};
@@ -233,11 +247,6 @@ void PlatformExit(PlatformInfo& info)
 {
 }
 
-static inline std::string &rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
-    return s;
-}
 
 const char* PlatformGetSymbol(PlatformInfo& info, uintptr_t ptr)
 {
@@ -264,6 +273,63 @@ const char* PlatformGetSymbol(PlatformInfo& info, uintptr_t ptr)
     return info.buffer;
 }
 
+#elif defined(__linux__)
+
+struct PlatformInfo
+{
+    const char* executable_path;
+    const char* basename; // "e.g. dmengine"
+
+    char buffer[2048];
+};
+
+int PlatformInit(char* executable_path, PlatformInfo& info)
+{
+    info.basename = 0;
+    info.executable_path = executable_path;
+    if( executable_path )
+    {
+        info.basename = strrchr(info.executable_path, '/');
+        if (!info.basename)
+        {
+            info.basename = info.executable_path;
+        } else
+        {
+            info.basename++; // Step past the '/'
+        }
+    }
+    return 1;
+}
+
+void PlatformExit(PlatformInfo& info)
+{
+}
+
+const char* PlatformGetSymbol(PlatformInfo& info, uintptr_t ptr)
+{
+    if( !info.executable_path )
+    {
+        return "<unknown>";
+    }
+
+    char cmd[2048];
+    sprintf(cmd, "addr2line -e %s %p", info.executable_path, (void*)ptr);
+
+    std::string output;
+    int result = Exec(cmd, output);
+    if( result )
+    {
+        return "<unknown>";
+    }
+
+    output = rtrim(output);
+
+    snprintf(info.buffer, sizeof(info.buffer), "%s", output.c_str() );
+    info.buffer[sizeof(info.buffer)-1] = 0;
+
+    return info.buffer;
+}
+
 #endif
 
 
@@ -272,6 +338,7 @@ enum Platforms
     PLATFORM_UNKNOWN = -1,
     PLATFORM_WINDOWS,
     PLATFORM_DARWIN,
+    PLATFORM_LINUX,
 };
 
 
@@ -318,8 +385,10 @@ int main(int argc, char** argv)
 
             if( strcmp(names[i], "SYSTEM_NAME") == 0 )
             {
-                if( strcmp(value, "Darwin") )
+                if( strcmp(value, "Darwin") == 0 )
                     platform = PLATFORM_DARWIN;
+                else if( strcmp(value, "Linux") == 0 )
+                    platform = PLATFORM_LINUX;
             }
     	}
 
@@ -327,7 +396,7 @@ int main(int argc, char** argv)
         printf("%26s: %d\n", "SIG", dmCrash::GetSignum(dump));
 
     	printf("%26s:\n", "USERDATA");
-    	for( int i = 0; i < dmCrash::AppState::USERDATA_SLOTS; ++i)
+    	for( int i = 0; i < (int)dmCrash::AppState::USERDATA_SLOTS; ++i)
     	{
     		const char* value = dmCrash::GetUserField(dump, i);
     		if(!value || strcmp(value, "")==0)
@@ -357,7 +426,7 @@ int main(int argc, char** argv)
 
 
         printf("\n%s:\n", "MODULES");
-        for( int i = 0; i < dmCrash::AppState::MODULES_MAX; ++i)
+        for( int i = 0; i < (int)dmCrash::AppState::MODULES_MAX; ++i)
         {
             const char* modulename = dmCrash::GetModuleName(dump, i);
             if( modulename == 0 )
