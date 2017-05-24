88b8ac7074e0b55c5d0ca4ed7164e69696a93434
Christian Murray <christian.murray@king.com>
2017-05-24 15:23:07 +0200

    First take on analytics in editor

---
+++ editor/src/clj/editor/analytics.clj
@@ -0,0 +1,19 @@
+(ns editor.analytics
+  (:require [service.log :as log])
+  (:import [com.defold.editor.analytics Analytics]))
+
+(set! *warn-on-reflection* true)
+
+(defonce *analytics* (Analytics. "UA-83690-7" (System/getProperty "defold.version" "NO VERSION")))
+
+(defn track-event [category action label]
+  (try
+    (.trackEvent ^Analytics *analytics* category action label)
+    (catch Throwable e
+      (log/error :exception e))))
+
+(defn track-screen [screen-name]
+  (try
+    (.trackScreen ^Analytics *analytics* "defold" screen-name)
+    (catch Throwable e
+      (log/error :exception e))))
