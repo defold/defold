6757f8e5560a54e8869b1716862275b525a2904c
Mikael Säker <mikael@sicher.org>
2017-06-16 11:33:05 +0200

    Fixed factory.create default value description. Added enum variables that were missing for resoure.*

--- engine/gamesys/src/gamesys/scripts/script_resource.cpp
+++ engine/gamesys/src/gamesys/scripts/script_resource.cpp
@@ -207,6 +207,7 @@ static int GraphicsTextureTypeToImageType(int texturetype)
  * `format`
  * : [type:number] The texture format. Supported values:
  *
+ * - `resource.TEXTURE_FORMAT_LUMINANCE`
  * - `resource.TEXTURE_FORMAT_RGB`
  * - `resource.TEXTURE_FORMAT_RGBA`
  *
@@ -328,6 +329,33 @@ static const luaL_reg Module_methods[] =
 
     {0, 0}
 };
+
+
+    /*# 2D texture type
+     *
+     * @name resource.TEXTURE_TYPE_2D
+     * @variable
+     */
+
+    /*# luminance type texture format
+     *
+     * @name resource.TEXTURE_FORMAT_LUMINANCE
+     * @variable
+     */
+
+    /*# RGB type texture format
+     *
+     * @name resource.TEXTURE_FORMAT_RGB
+     * @variable
+     */
+
+    /*# RGBA type texture format
+     *
+     * @name resource.TEXTURE_FORMAT_RGBA
+     * @variable
+     */
+
+
 
 static void LuaInit(lua_State* L)
 {
