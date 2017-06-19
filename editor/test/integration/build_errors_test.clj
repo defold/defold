7a4e63c3a69aaae4984d000b477010d70f92c6a8
Mats Gisselson <mats.gisselson@king.com>
2017-06-19 11:56:55 +0200

    DEFEDIT-950 Optimized indexing phase when loading project and improved tests

--- editor/test/integration/build_errors_test.clj
+++ editor/test/integration/build_errors_test.clj
@@ -62,22 +62,24 @@
           resource (partial test-util/resource workspace)
           resource-node (partial test-util/resource-node project)
           outline-node (fn [resource-path labels] (find-outline-node (resource-node resource-path) labels))
-          make-restore-point! #(test-util/make-graph-reverter (project/graph project))
-          render-error! (test-util/make-call-logger)]
+          make-restore-point! #(test-util/make-graph-reverter (project/graph project))]
 
       (testing "Build error links to source node"
         (are [component-resource-path error-resource-path error-outline-path]
           (with-open [_ (make-restore-point!)]
-            (add-component-from-file! workspace game-object component-resource-path)
-            (project/build project main-collection {:render-error! render-error!})
-            (let [error-value (build-error render-error!)
-                  error-tree (build-errors-view/build-resource-tree error-value)]
-              (is (= (resource error-resource-path)
-                     (error-resource error-tree)))
-              (is (= (resource-node error-resource-path)
-                     (error-resource-node error-tree)))
-              (is (= (outline-node error-resource-path error-outline-path)
-                     (error-outline-node error-tree)))))
+            (let [render-error! (test-util/make-call-logger)]
+              (add-component-from-file! workspace game-object component-resource-path)
+              (project/build project main-collection {:render-error! render-error!})
+              (let [error-value (build-error render-error!)]
+                (when (is (some? error-value) component-resource-path)
+                  (let [error-tree (build-errors-view/build-resource-tree error-value)]
+                    (is (= (resource error-resource-path)
+                           (error-resource error-tree)))
+                    (is (= (resource-node error-resource-path)
+                           (error-resource-node error-tree)))
+                    (is (= (outline-node error-resource-path error-outline-path)
+                           (error-outline-node error-tree)))))))
+            true)
 
           "/errors/syntax_error.script"
           "/errors/syntax_error.script" []
@@ -102,14 +104,16 @@
 
       (testing "Errors from the same source are not duplicated"
         (with-open [_ (make-restore-point!)]
-          (add-component-from-file! workspace game-object "/errors/button_break_self.gui")
-          (add-component-from-file! workspace game-object "/errors/panel_break_button.gui")
-          (add-component-from-file! workspace game-object "/errors/panel_using_button_break_self.gui")
-          (add-component-from-file! workspace game-object "/errors/window_using_panel_break_button.gui")
-          (project/build project main-collection {:render-error! render-error!})
-          (let [error-value (build-error render-error!)
-                error-tree (build-errors-view/build-resource-tree error-value)]
-            (is (= ["/errors/button_break_self.gui" "/errors/panel_break_button.gui"]
-                   (sort (map #(resource/proj-path (get-in % [:value :resource])) (:children error-tree)))))
-            (is (= 1 (count (get-in error-tree [:children 0 :children]))))
-            (is (= 1 (count (get-in error-tree [:children 1 :children]))))))))))
+          (let [render-error! (test-util/make-call-logger)]
+            (add-component-from-file! workspace game-object "/errors/button_break_self.gui")
+            (add-component-from-file! workspace game-object "/errors/panel_break_button.gui")
+            (add-component-from-file! workspace game-object "/errors/panel_using_button_break_self.gui")
+            (add-component-from-file! workspace game-object "/errors/window_using_panel_break_button.gui")
+            (project/build project main-collection {:render-error! render-error!})
+            (let [error-value (build-error render-error!)]
+              (when (is (some? error-value))
+                (let [error-tree (build-errors-view/build-resource-tree error-value)]
+                  (is (= ["/errors/button_break_self.gui" "/errors/panel_break_button.gui"]
+                         (sort (map #(resource/proj-path (get-in % [:value :resource])) (:children error-tree)))))
+                  (is (= 1 (count (get-in error-tree [:children 0 :children]))))
+                  (is (= 1 (count (get-in error-tree [:children 1 :children])))))))))))))
