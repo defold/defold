821e78f89aae42d857ce2452497df218095ecdb2
Erik Angelin <erik.angelin@king.com>
2017-06-19 11:26:47 +0200

    DEFEDIT-1033 Review fixes
    * Build cache is now an atom instead of containing atoms

--- editor/src/clj/editor/pipeline.clj
+++ editor/src/clj/editor/pipeline.clj
@@ -6,7 +6,10 @@
    [editor.protobuf :as protobuf]
    [editor.workspace :as workspace])
   (:import
+   (java.io File)
    (java.util UUID)))
+
+(set! *warn-on-reflection* true)
 
 (defn- resolve-resource-paths
   [pb dep-resources resource-props]
@@ -109,16 +112,20 @@
         dep-resources (make-dep-resources deps build-targets-by-key)]
     (build-fn node-id basis resource dep-resources user-data)))
 
+(defn- empty-build-cache []
+  {:dir (fs/create-temp-directory! "defold-build-cache")
+   :entries {}})
+
+(defn build-cache-dir ^File [build-cache]
+  (:dir @build-cache))
+
 (defn- revalidate-build-cache-dir! [build-cache]
-  (swap! (:dir build-cache) #(if (and % (.exists %)) % (fs/create-temp-directory! "defold-build-cache"))))
+  (when-not (.exists (build-cache-dir build-cache))
+    (reset! build-cache (empty-build-cache)))
+  (build-cache-dir build-cache))
 
 (defn make-build-cache []
-  (doto {:dir     (atom nil)
-         :entries (atom {})}
-    (revalidate-build-cache-dir!)))
-
-(defn build-cache-dir [build-cache]
-  @(:dir build-cache))
+  (atom (empty-build-cache)))
 
 (defn cache!
   [build-cache resource key result]
@@ -129,12 +136,12 @@
                         (dissoc :content)
                         (assoc :id id :key key :cached true))]
     (io/copy content cache-file)
-    (swap! (:entries build-cache) assoc resource cache-value)
+    (swap! build-cache assoc-in [:entries resource] cache-value)
     (assoc result :key key)))
 
 (defn lookup
   [build-cache resource key]
-  (when-let [entry (get @(:entries build-cache) resource)]
+  (when-let [entry (get (:entries @build-cache) resource)]
     (when (= key (:key entry))
       (let [file (io/file (build-cache-dir build-cache) (:id entry))]
         (when (.exists file)
@@ -142,14 +149,13 @@
               (dissoc :id)
               (assoc :content file)))))))
 
-(defn- prune-build-cache!
-  [{:keys [dir entries] :as build-cache} build-targets-by-key]
-  (swap! entries #(reduce-kv (fn [ret resource {:keys [key] :as result}]
-                               (if (contains? build-targets-by-key key)
-                                 (assoc ret resource result)
-                                 ret))
-                             {}
-                             %))
+(defn- prune-build-cache! [build-cache build-targets-by-key]
+  (swap! build-cache update :entries #(reduce-kv (fn [ret resource {:keys [key] :as result}]
+                                                   (if (contains? build-targets-by-key key)
+                                                     (assoc ret resource result)
+                                                     ret))
+                                                 {}
+                                                 %))
   build-cache)
 
 (defn build-target-cached
