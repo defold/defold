8bf23dfa0281cd44de59f6197d242265d088fc7c
johan-becknoren-king <johan.beck-noren@king.com>
2017-06-20 10:16:27 +0200

    DEF-2704 gui pfx node (#1637)
    * WIP adding pfx gui node type
    
    * Pfx source selection from pfx node
    
    * Emitter and modifier rendering
    
    * Child pfx node to other node types
    
    * Added const aabb. Added single pfx icon for node. Cleanup. Added setting for show/hide pfx outline.
    
    * Added icons for overridden and template overridden
    
    * Gui template scenes containing pfx
    
    * Removed a bunch of whitespaces
    
    * Fixes after review

---
+++ com.dynamo.cr/com.dynamo.cr.guied/src/com/dynamo/cr/guied/handlers/AddParticleFXSceneHandler.java
@@ -0,0 +1,26 @@
+package com.dynamo.cr.guied.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.dynamo.cr.guied.core.GuiSceneNode;
+import com.dynamo.cr.guied.core.GuiScenePresenter;
+import com.dynamo.cr.sceneed.core.ISceneEditor;
+
+public class AddParticleFXSceneHandler extends AbstractHandler {
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        IEditorPart editorPart = HandlerUtil.getActiveEditor(event);
+        if (editorPart instanceof ISceneEditor) {
+            ISceneEditor sceneEditor = (ISceneEditor) editorPart;
+            GuiScenePresenter presenter = (GuiScenePresenter) sceneEditor.getNodePresenter(GuiSceneNode.class);
+            presenter.onAddParticleFXSceneNode(sceneEditor.getPresenterContext());
+        }
+        return null;
+    }
+
+}
