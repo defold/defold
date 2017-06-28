4b330fb38d0a6a1686f00c21d994d63c9296ab57
Ragnar Svensson <ragnar.svensson@king.com>
2017-06-28 11:20:20 +0200

    DEFEDIT-1043 protobuf ns uses memoization to avoid reflection perf penalties
    * Around 3x faster to convert between clj and protobuf
    * The support for checking if a value was a default or set explicitly was removed because of buggyness
    * Now all fields are written to the files

--- editor/src/clj/editor/gui.clj
+++ editor/src/clj/editor/gui.clj
@@ -1877,8 +1877,14 @@
           (add-layout-options node-id user-data))))))
 
 (defn- color-alpha [node-desc color-field alpha-field]
-  (let [color (get node-desc color-field)]
-    (if (protobuf/field-set? node-desc alpha-field) (get node-desc alpha-field) (get color 3))))
+  ;; The alpha field replaced the fourth component of color,
+  ;; to support overriding of the alpha separately from color.
+  ;; Check which one to use by comparing with the default value.
+  (let [color (get node-desc color-field)
+        alpha (get node-desc alpha-field)]
+    (if (not= alpha (protobuf/default Gui$NodeDesc alpha-field))
+      alpha
+      (get color 3))))
 
 (def node-property-fns (-> {}
                          (into (map (fn [label] [label [label (comp v4->v3 label)]]) [:position :rotation :scale :size]))
