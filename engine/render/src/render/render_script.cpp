828452e0b3344cdf9e80cb635a616c5ea76ee5fe
Sven Andersson <sven.andersson@king.com>
2017-10-12 13:23:31 +0300

    DEF-2761 - Floating point RGBA render targets (#1827)
    * Adds a couple of new texture format enums internally in the engine.
    * *Does not* expose these enums to the user, for now.

--- engine/render/src/render/render_script.cpp
+++ engine/render/src/render/render_script.cpp
@@ -2314,6 +2314,21 @@ namespace dmRender
         REGISTER_FORMAT_CONSTANT(DEPTH);
         REGISTER_FORMAT_CONSTANT(STENCIL);
 
+        /*
+         * We don't expose floating point texture for now,
+         * until we have taken an official stand how to expose
+         * rendering capabilities. See DEF-2886
+         *
+
+        REGISTER_FORMAT_CONSTANT(RGB16F);
+        REGISTER_FORMAT_CONSTANT(RGB32F);
+        REGISTER_FORMAT_CONSTANT(RGBA16F);
+        REGISTER_FORMAT_CONSTANT(RGBA32F);
+        REGISTER_FORMAT_CONSTANT(LUMINANCE16F);
+        REGISTER_FORMAT_CONSTANT(LUMINANCE32F);
+
+        */
+
 #undef REGISTER_FORMAT_CONSTANT
 
 #define REGISTER_FILTER_CONSTANT(name)\
