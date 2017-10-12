f5efbbd3f501c637104b9e746e3e104c1ba22e24
Mikael Säker <mikael@sicher.org>
2017-10-12 10:05:28 +0200

    DOC: fixed typographic mess in render.set_depth_func

--- engine/render/src/render/render_script.cpp
+++ engine/render/src/render/render_script.cpp
@@ -1659,11 +1659,11 @@ namespace dmRender
     *
     * - `render.COMPARE_FUNC_NEVER` (never passes)
     * - `render.COMPARE_FUNC_LESS` (passes if the incoming depth value is less than the stored value)
-    * - `render.COMPARE_FUNC_LEQUAL` (passes if the incoming depth value is less than or equal to )the stored value
-    * `render.COMPARE_FUNC_GREATER` (passes if the incoming depth value is greater than the stored )v- alue
-    * `render.COMPARE_FUNC_GEQUAL` (passes if the incoming depth value is greater than or equal to )t- he stored value
+    * - `render.COMPARE_FUNC_LEQUAL` (passes if the incoming depth value is less than or equal to the stored value)
+    * `render.COMPARE_FUNC_GREATER` (passes if the incoming depth value is greater than the stored value)
+    * - `render.COMPARE_FUNC_GEQUAL` (passes if the incoming depth value is greater than or equal to the stored value)
     * - `render.COMPARE_FUNC_EQUAL` (passes if the incoming depth value is equal to the stored value)
-    * - `render.COMPARE_FUNC_NOTEQUAL` (passes if the incoming depth value is not equal to the )stored value
+    * - `render.COMPARE_FUNC_NOTEQUAL` (passes if the incoming depth value is not equal to the stored value)
     * - `render.COMPARE_FUNC_ALWAYS` (always passes)
     *
     * The depth function is initially set to `render.COMPARE_FUNC_LESS`.
