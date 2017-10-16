1e12c7fe0e8f567c455050ee5e9bb5244070f34d
Ragnar Dahlén <ragnard-king@users.noreply.github.com>
2017-10-16 15:12:37 +0200

    DEFEDIT-1166 Help sentry to not forget our error IDs (#1830)
    * Technical changes
    
    - sentry forgets `event_id` after seven days. Send id as a tag too,
      ensures that we can query for it forever.

--- editor/src/clj/editor/sentry.clj
+++ editor/src/clj/editor/sentry.clj
@@ -80,9 +80,10 @@
 
 (defn make-event
   [^Exception ex ^Thread thread]
-  (let [environment (if (system/defold-version) "release" "dev")
+  (let [id (string/replace (str (java.util.UUID/randomUUID)) "-" "")
+        environment (if (system/defold-version) "release" "dev")
         gl-info (gl/gl-info)
-        event {:event_id    (string/replace (str (java.util.UUID/randomUUID)) "-" "")
+        event {:event_id    id
                :message     (.getMessage ex)
                :timestamp   (LocalDateTime/now ZoneOffset/UTC)
                :level       "error"
@@ -92,7 +93,8 @@
                :device      {:name (system/os-name) :version (system/os-version)}
                :culprit     (module-name (.getStackTrace ex))
                :release     (or (system/defold-version) "dev")
-               :tags        {:defold-editor-sha1 (system/defold-editor-sha1)
+               :tags        {:id id
+                             :defold-editor-sha1 (system/defold-editor-sha1)
                              :defold-engine-sha1 (system/defold-engine-sha1)
                              :defold-version (or (system/defold-version) "dev")
                              :os-name (system/os-name)
