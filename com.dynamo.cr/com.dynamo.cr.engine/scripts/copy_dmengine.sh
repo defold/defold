6d77335c604fa57a4e4854a6af680476ac6b77c8
Mathias Westerdahl <mathias.westerdahl@king.com>
2017-10-20 09:09:55 +0200

    DEF-2908 Removed x86-linux and x86-darwin from the bundle step (#1838)
    * DEF-2908 Removed x86-linux and x86-darwin from the bundle step
    * bob.jar bundles 64 bit darwin
    * NE only builds 64 bit osx/linux
    
    * Added 64 bit tools for Darwin
        Copied all tools over from x86-darwin to x86_64-darwin except:
        ~/android/android-sdk/build-tools/23.0.2/aapt
        ~/android/android-sdk/build-tools/25.0.2/zipalign
    
    * bundle fix for engine
    
    * Bundle unit test fix

--- com.dynamo.cr/com.dynamo.cr.engine/scripts/copy_dmengine.sh
+++ com.dynamo.cr/com.dynamo.cr.engine/scripts/copy_dmengine.sh
@@ -2,6 +2,7 @@ set -e
 mkdir -p engine/linux
 mkdir -p engine/x86_64-linux
 mkdir -p engine/darwin
+mkdir -p engine/x86_64-darwin
 mkdir -p engine/win32
 mkdir -p engine/x86_64-win32
 mkdir -p engine/ios
@@ -25,6 +26,8 @@ copy x86_64-linux/dmengine_release engine/x86_64-linux/dmengine_release
 
 copy darwin/dmengine engine/darwin/dmengine
 copy darwin/dmengine_release engine/darwin/dmengine_release
+copy x86_64-darwin/dmengine engine/x86_64-darwin/dmengine
+copy x86_64-darwin/dmengine_release engine/x86_64-darwin/dmengine_release
 copy win32/dmengine.exe engine/win32/dmengine.exe
 copy win32/dmengine_release.exe engine/win32/dmengine_release.exe
 copy x86_64-win32/dmengine.exe engine/x86_64-win32/dmengine.exe
