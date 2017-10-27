e29bc940e4be754a266d448ab54719967b7b95c3
Erik Angelin <erik.angelin@king.com>
2017-10-27 09:30:46 +0200

    DEFEDIT-988 Cubemap support
    * User facing changes
      - Can build and preview cubemaps separately or as part of model
      - Texture profiles kicks in for images and cubemaps
    
    * Technical changes
      - Split off image-util from image namespace because of circular
      dependency when introducing texture profiles
      - Support for generating cubemap gpu textures using texture profile

--- editor/src/clj/editor/pipeline/tex_gen.clj
+++ editor/src/clj/editor/pipeline/tex_gen.clj
@@ -1,7 +1,12 @@
 (ns editor.pipeline.tex-gen
-  (:require [editor.protobuf :as protobuf])
+  (:require [editor.protobuf :as protobuf]
+            [internal.util :as util])
   (:import [com.defold.editor.pipeline TextureGenerator TextureUtil]
+           [com.defold.libs TexcLibrary$FlipAxis]
+           [com.google.protobuf ByteString]
            [com.dynamo.graphics.proto Graphics$TextureImage Graphics$TextureImage$TextureFormat Graphics$TextureProfiles Graphics$TextureProfile]
+           [java.util EnumSet]
+           [java.io ByteArrayOutputStream]
            [java.awt.image BufferedImage]))
 
 (set! *warn-on-reflection* true)
@@ -34,8 +39,10 @@
   (^Graphics$TextureImage [^BufferedImage image texture-profile]
    (make-texture-image image texture-profile false))
   (^Graphics$TextureImage [^BufferedImage image texture-profile compress?]
+   (make-texture-image image texture-profile compress? true))
+  (^Graphics$TextureImage [^BufferedImage image texture-profile compress? flip-y?]
    (let [^Graphics$TextureProfile texture-profile-data (some->> texture-profile (protobuf/map->pb Graphics$TextureProfile))]
-     (TextureGenerator/generate image texture-profile-data ^boolean compress?))))
+     (TextureGenerator/generate image texture-profile-data ^boolean compress? (if ^boolean flip-y? (EnumSet/of TexcLibrary$FlipAxis/FLIP_AXIS_Y) (EnumSet/noneOf TexcLibrary$FlipAxis))))))
 
 (defn- make-preview-profile
   "Given a texture-profile, return a simplified texture-profile that can be used
@@ -57,3 +64,51 @@
   ^Graphics$TextureImage [^BufferedImage image texture-profile]
   (let [preview-profile (make-preview-profile texture-profile)]
     (make-texture-image image preview-profile false)))
+
+(defn make-cubemap-texture-images
+  ^Graphics$TextureImage [images texture-profile compress?]
+  (let [^Graphics$TextureProfile texture-profile-data (some->> texture-profile (protobuf/map->pb Graphics$TextureProfile))]
+    (util/map-vals #(make-texture-image % texture-profile compress? false) images)))
+
+(defn- make-cubemap-texture-image-alternatives [textures]
+  ;; Cube map textures are in the order px nx py ny nz pz.
+  ;; Size of texture[i].alternative[k] == texture[j].alternative[k]
+  ;; So mip map sizes & offsets also equal
+  ;; Format of alternative data n is:
+  ;; [<cube map textures' alternative n at mip level 0> <cube map textures' alternative n at mip level 1> ... <... at final mip level>]
+  (for [alt (range 0 (count (:alternatives (first textures))))]
+    (let [template-alternative (-> textures first :alternatives (nth alt))
+          data (ByteArrayOutputStream.)]
+      (doseq [mipmap (range 0 (count (:mip-map-size template-alternative))) ; mip-map-size always uncompressed
+              :let [mip-size (-> template-alternative :mip-map-size (nth mipmap))
+                    mip-offset (-> template-alternative :mip-map-offset (nth mipmap))
+                    mip-buf (byte-array mip-size)]
+              texture (range 0 (count textures))
+              :let [^ByteString alt-data (-> textures (nth texture) :alternatives (nth alt) :data)]]
+        (.copyTo alt-data mip-buf mip-offset 0 mip-size) ; (.copyTo source-byte-string target source-offset target-offset number-to-copy)
+        (.write data mip-buf))
+      (-> template-alternative
+          (assoc :data (ByteString/copyFrom (.toByteArray data)))
+          ;; thought we would have to update mip-map-size also, but CubemapBuilder doesn't so...
+          (update :mip-map-offset (fn [offsets] (map #(* 6 %) offsets)))))))
+
+(defn assemble-cubemap-texture-images
+  ^Graphics$TextureImage
+  [texture-images]
+  ;; FIXME: in graphics_opengl.cpp(1870ish) in the engine, we set the cubemap textures
+  ;; using glTexSubImage2D in the order +x, -x, +y, -y, and then ***-z***, ***+z***
+  ;; until this is fixed, if ever, we flip the order as below
+  (let [textures (mapv protobuf/pb->map ((juxt :px :nx :py :ny :nz :pz) texture-images))
+        alternatives (make-cubemap-texture-image-alternatives textures)
+        template-texture (first textures)]
+    (-> template-texture
+        (assoc :count 6)
+        (assoc :type :type-cubemap)
+        (assoc :alternatives alternatives)
+        (#(protobuf/map->pb Graphics$TextureImage %)))))
+
+(defn make-preview-cubemap-texture-images
+  ^Graphics$TextureImage [images texture-profile]
+  (let [preview-profile (make-preview-profile texture-profile)]
+    (make-cubemap-texture-images images preview-profile false)))
+
