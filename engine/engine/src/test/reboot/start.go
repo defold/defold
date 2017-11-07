6c48fff933e664af0d79b1cd339ddd28f3be68a9
Mathias Westerdahl <mathias.westerdahl@king.com>
2017-11-07 14:08:03 +0100

    Accidentally broke another test Reduced spam in the console

--- engine/engine/src/test/reboot/start.go
+++ engine/engine/src/test/reboot/start.go
@@ -1,4 +1,9 @@
 components {
   id: "start_script"
-  component: "/init_script/start.script"
+  component: "/reboot/start.script"
+}		
+embedded_components {		
+  id: "collectionproxy"		
+  type: "collectionproxy"		
+  data: "collection: \"/reboot/dynamic1.collection\""
 }
