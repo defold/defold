aeb1548a75b707cb755e5bf216a71aab547f8c26
Erik Angelin <erik.angelin@king.com>
2017-12-13 12:35:03 +0100

    DEFEDIT-1093 C++ syntax highlighting support
    Very primitive support for C/C++/ObjC.

---
+++ editor/src/clj/editor/code/text_file.clj
@@ -0,0 +1,46 @@
+(ns editor.code.text-file
+  (:require [clojure.string :as string]
+            [dynamo.graph :as g]
+            [editor.code.lang.cish :as cish]
+            [editor.code.resource :as r]
+            [editor.resource :as resource]
+            [editor.workspace :as workspace]))
+
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* :warn-on-boxed)
+
+(def ^:private cish-opts {:new-code {:grammar cish/grammar}})
+
+(def ^:private text-file-defs
+  [{:ext "cpp"
+    :label "C++"
+    :icon "icons/32/Icons_12-Script-type.png"
+    :view-opts cish-opts}
+   {:ext "h"
+    :label "C/C++ Header"
+    :icon "icons/32/Icons_12-Script-type.png"
+    :view-opts cish-opts}
+   {:ext "mm"
+    :label "Objective-C"
+    :icon "icons/32/Icons_12-Script-type.png"
+    :view-opts cish-opts}
+   {:ext "c"
+    :label "C"
+    :icon "icons/32/Icons_12-Script-type.png"
+    :view-opts cish-opts}
+   {:ext "manifest"
+    :label "Manifest"
+    :icon "icons/32/Icons_11-Script-general.png"}
+   {:ext "appmanifest"
+    :label "App Manifest"
+    :icon "icons/32/Icons_11-Script-general.png"}])
+
+(g/defnode TextNode
+  (inherits r/CodeEditorResourceNode))
+
+(defn register-resource-types [workspace]
+  (for [def text-file-defs
+        :let [args (assoc def
+                          :node-type TextNode
+                          :view-types [:new-code :default])]]
+    (apply r/register-code-resource-type workspace (mapcat identity args))))
