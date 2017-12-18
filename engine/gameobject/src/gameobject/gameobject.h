a27aeeb63fc97f48b204df7ea0d28d47e676ccf6
Mathias Westerdahl <mathias.westerdahl@king.com>
2017-12-18 16:58:37 +0100

    DEF-3052 Optimized the Sprite update transform function (#1988)
    * DEF-3052 Optimized the Sprite update transform function
    
    * Review fix

--- engine/gameobject/src/gameobject/gameobject.h
+++ engine/gameobject/src/gameobject/gameobject.h
@@ -1088,6 +1088,13 @@ namespace dmGameObject
      * @return The rig context of the specified collection
      */
     dmRig::HRigContext GetRigContext(HCollection collection);
+ 
+    /**
+     * Returns whether the scale of the instances in a collection should be applied along Z or not.
+     * @param collection Collection
+     * @return if the scale should be applied along Z
+     */
+    bool ScaleAlongZ(HCollection collection);
 
     /**
      * Returns whether the scale of the instances in a collection should be applied along Z or not.
