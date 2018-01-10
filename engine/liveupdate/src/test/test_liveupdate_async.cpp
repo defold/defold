ea1ea0f52f7e6febeefcd88e3deb4d3baedcffda
johan-becknoren-king <johan.beck-noren@king.com>
2018-01-10 13:55:47 +0100

    DEF-3027 liveupdate resource header verification (#2029)
    * Fixed liveupdate resource size verification
    
    * Run store_resource callback with failed status as well as log error

--- engine/liveupdate/src/test/test_liveupdate_async.cpp
+++ engine/liveupdate/src/test/test_liveupdate_async.cpp
@@ -62,6 +62,17 @@ static void Callback_StoreResource(dmLiveUpdate::StoreResourceCallbackData* call
     ASSERT_TRUE(callback_data->m_Status);
 }
 
+static void Callback_StoreResourceInvalidHeader(dmLiveUpdate::StoreResourceCallbackData* callback_data)
+{
+    g_TestAsyncCallbackComplete = true;
+    ASSERT_EQ(1, callback_data->m_Callback);
+    ASSERT_EQ(2, callback_data->m_ResourceRef);
+    ASSERT_EQ(3, callback_data->m_HexDigestRef);
+    ASSERT_EQ(4, callback_data->m_Self);
+    ASSERT_STREQ("DUMMY1", callback_data->m_HexDigest);
+    ASSERT_FALSE(callback_data->m_Status);
+}
+
 TEST_F(LiveUpdate, TestAsync)
 {
     dmLiveUpdate::AsyncInitialize(g_ResourceFactory);
@@ -88,6 +99,42 @@ TEST_F(LiveUpdate, TestAsync)
     request.m_Resource.Set(resource);
     request.m_CallbackData = cb;
     request.m_Callback = Callback_StoreResource;
+
+    ASSERT_TRUE(dmLiveUpdate::AddAsyncResourceRequest(request));
+    while(!g_TestAsyncCallbackComplete)
+        dmLiveUpdate::AsyncUpdate();
+
+    dmLiveUpdate::AsyncFinalize();
+}
+
+TEST_F(LiveUpdate, TestAsyncInvalidResource)
+{
+    dmLiveUpdate::AsyncInitialize(g_ResourceFactory);
+
+    uint8_t buf[sizeof(dmResourceArchive::LiveUpdateResourceHeader)+sizeof(uint32_t)];
+    const size_t buf_len = sizeof(buf);
+    *((uint32_t*)&buf[sizeof(dmResourceArchive::LiveUpdateResourceHeader)]) = 0xdeadbeef;
+    dmResourceArchive::LiveUpdateResource resource((const uint8_t*) buf, buf_len);
+
+    resource.m_Header = 0x0;
+
+    dmLiveUpdate::StoreResourceCallbackData cb;
+    cb.m_Callback = 1;
+    cb.m_ResourceRef = 2;
+    cb.m_HexDigestRef = 3;
+    cb.m_Self = 4;
+    cb.m_HexDigest = "DUMMY1";;
+
+    dmResource::Manifest manifest;
+    manifest.m_ArchiveIndex = (dmResourceArchive::HArchiveIndexContainer) 0x1234;
+
+    dmLiveUpdate::AsyncResourceRequest request;
+    request.m_Manifest = &manifest;
+    request.m_ExpectedResourceDigestLength = 6;
+    request.m_ExpectedResourceDigest = "DUMMY2";
+    request.m_Resource.Set(resource);
+    request.m_CallbackData = cb;
+    request.m_Callback = Callback_StoreResourceInvalidHeader;
 
     ASSERT_TRUE(dmLiveUpdate::AddAsyncResourceRequest(request));
     while(!g_TestAsyncCallbackComplete)
