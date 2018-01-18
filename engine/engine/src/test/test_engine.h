e0fd8e6f5f8223e53b5e2cff5d3bb3872c97fcdc
Mathias Westerdahl <mathias.westerdahl@king.com>
2018-01-18 13:32:56 +0100

    added missing file

---
+++ engine/engine/src/test/test_engine.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include <resource/resource.h>
+
+#include <hid/hid.h>
+
+#include <sound/sound.h>
+#include <gameobject/gameobject.h>
+#include <gamesys/gamesys.h>
+#include "../engine.h"
+#include "../engine_private.h"
+
+class EngineTest : public ::testing::Test
+{
+protected:
+    virtual void SetUp()
+    {
+        m_DT = 1.0f / 60.0f;
+    }
+
+    virtual void TearDown()
+    {
+    }
+
+    float m_DT;
+};
+
+
+template<typename T>
+class EngineParamsTest : public ::testing::TestWithParam<T>
+{
+protected:
+    virtual void SetUp()
+    {
+        m_DT = 1.0f / 60.0f;
+        m_Engine = dmEngine::New(0);
+    }
+
+    virtual void TearDown()
+    {
+        dmEngine::Delete(m_Engine);
+    }
+
+    dmEngine::HEngine m_Engine;
+    float m_DT;
+};
+
+struct DrawCountParams
+{
+    const char* m_ProjectPath;
+    uint32_t m_NumSkipFrames;
+    uint64_t m_ExpectedDrawCount;
+};
+
+class DrawCountTest : public EngineParamsTest<DrawCountParams>
+{
+public:
+    virtual ~DrawCountTest() {}
+};
