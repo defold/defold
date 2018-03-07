46d8b7e63d7e0f0f4acd545c46d25ca2b227a806
Mathias Westerdahl <mathias.westerdahl@king.com>
2018-03-07 09:29:05 +0100

    DEF-3139 Added support for iPhoneOS11.2 and MacOSX10.13 (#2133)
    * Removed use of (deprecated and expired) system() from Lua's os_execute()
    * Removed system() from our tests on iOS

--- engine/engine/src/test/test_engine.cpp
+++ engine/engine/src/test/test_engine.cpp
@@ -93,7 +93,9 @@ void HttpPostThread(void* params)
     HttpTestContext* http_ctx = (HttpTestContext*) params;
     char cmd[256];
     DM_SNPRINTF(cmd, sizeof(cmd), "python src/test/%s %d", http_ctx->m_Script, http_ctx->m_Port);
+#if !defined(DM_NO_SYSTEM_FUNCTION)
     g_PostExitResult = system(cmd);
+#endif
 }
 
 static void PreRunHttpPort(dmEngine::HEngine engine, void* ctx)
