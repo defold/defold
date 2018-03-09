bb0c7beedd0068177d1c62748a9930febaddd49c
Mats Gisselson <mats.gisselson@king.com>
2018-03-09 12:35:24 +0100

    DEFEDIT-1320 Replace welcome dialog & new project working

--- editor/src/clj/editor/welcome.clj
+++ editor/src/clj/editor/welcome.clj
@@ -3,17 +3,32 @@
             [clojure.java.io :as io]
             [clojure.string :as string]
             [editor.dialogs :as dialogs]
+            [editor.fs :as fs]
+            [editor.prefs :as prefs]
+            [editor.system :as system]
             [editor.ui :as ui]
-            [schema.core :as s])
-  (:import (java.net MalformedURLException URL)
-           (java.io PushbackReader)
+            [editor.updater :as updater]
+            [schema.core :as s]
+            [util.net :as net])
+  (:import (org.apache.commons.io FilenameUtils)
+           (java.io PushbackReader FileOutputStream File)
+           (java.net MalformedURLException URL)
+           (java.util.zip ZipInputStream)
+           (javafx.animation AnimationTimer)
            (javafx.event Event)
            (javafx.scene Node Scene Parent)
            (javafx.scene.control ToggleGroup RadioButton)
            (javafx.scene.image ImageView Image)
            (javafx.scene.layout HBox StackPane VBox)
            (javafx.scene.text Text TextFlow)
-           (javafx.scene.shape Rectangle)))
+           (javafx.scene.shape Rectangle)
+           (javafx.stage Stage)))
+
+(defonce ^:private open-project-directory-prefs-key "open-project-directory")
+
+;; -----------------------------------------------------------------------------
+;; Welcome config file parsing
+;; -----------------------------------------------------------------------------
 
 (def ^:private VisibleString
   (s/constrained s/Str #(not (string/blank? %)) "VisibleString"))
@@ -47,10 +62,70 @@
 (defn- load-welcome-settings [path]
   (s/validate WelcomeSettings (load-edn path)))
 
+;; -----------------------------------------------------------------------------
+;; New project creation
+;; -----------------------------------------------------------------------------
+
+(defn- download-proj-zip! [url]
+  (let [file ^File (fs/create-temp-file! "template-project" ".zip")]
+    (with-open [output (FileOutputStream. file)]
+      (do
+        ;; TODO net/download! accepts a callback for progress reporting, use that.
+        ;; The long timeout suggests we need some progress reporting even before d/l starts.
+        (let [timeout 2000]
+          (net/download! url output :connect-timeout timeout :read-timeout timeout))
+        file))))
+
+(defn- expand-proj-zip! [src dst skip-root?]
+  (with-open [zip (ZipInputStream. (io/input-stream src))]
+    (loop [entry (.getNextEntry zip)]
+      (when entry
+        (let [parts (cond-> (string/split (FilenameUtils/separatorsToUnix (.getName entry)) #"/")
+                            skip-root? (next))
+              entry-dst (apply io/file dst parts)]
+          (do
+            (if (.isDirectory entry)
+              (.mkdir entry-dst)
+              (with-open [output (FileOutputStream. entry-dst)]
+                (io/copy zip output)))
+            (recur (.getNextEntry zip))))))))
+
+;; -----------------------------------------------------------------------------
+;; Browse field control
+;; -----------------------------------------------------------------------------
+
+(defn- on-directory-field-browse-button-click! [dialog-title ^Event event]
+  (let [^Node button (.getSource event)
+        browse-field (.getParent button)
+        text-field (.lookup browse-field "TextField")
+        scene (.getScene button)
+        window (.getWindow scene)
+        initial-directory (some-> text-field ui/text not-empty io/as-file)
+        initial-directory (when (some-> initial-directory (.exists)) initial-directory)]
+    (when-some [directory-path (ui/choose-directory dialog-title initial-directory window)]
+      (ui/text! text-field directory-path))))
+
+(defn- setup-directory-browse-field! [dialog-title ^Parent browse-field]
+  (let [button (.lookup browse-field "Button")]
+    (ui/on-action! button (partial on-directory-field-browse-button-click! dialog-title))))
+
+(defn- browse-field-file
+  ^File [^Node browse-field]
+  (when-some [path (not-empty (ui/text (.lookup browse-field "TextField")))]
+    (io/as-file path)))
+
+;; -----------------------------------------------------------------------------
+;; Home pane
+;; -----------------------------------------------------------------------------
+
 (defn- make-home-pane
   ^Parent []
   (let [pane (ui/load-fxml "welcome/home-pane.fxml")]
     pane))
+
+;; -----------------------------------------------------------------------------
+;; New project pane
+;; -----------------------------------------------------------------------------
 
 (defn- make-icon-view
   ^Node [image-file-name]
@@ -87,47 +162,46 @@
                    (make-description-view (:name project-template)
                                           (:description project-template))])))
 
-(defn- on-directory-field-browse-button-click! [dialog-title ^Event event]
-  (let [^Node button (.getSource event)
-        browse-field (.getParent button)
-        text-field (.lookup browse-field "TextField")
-        scene (.getScene button)
-        window (.getWindow scene)
-        initial-directory (some-> text-field ui/text not-empty io/as-file)
-        initial-directory (when (some-> initial-directory (.exists)) initial-directory)]
-    (when-some [directory-path (ui/choose-directory dialog-title initial-directory window)]
-      (ui/text! text-field directory-path))))
-
-(defn- setup-directory-browse-field! [dialog-title ^Parent browse-field]
-  (let [button (.lookup browse-field "Button")]
-    (ui/on-action! button (partial on-directory-field-browse-button-click! dialog-title))))
-
-(defn- browse-field-text
-  ^String [^Node browse-field]
-  (ui/text (.lookup browse-field "TextField")))
-
 (defn- make-new-project-pane
-  ^Parent [welcome-settings]
+  ^Parent [welcome-settings close-dialog-and-open-project!]
   (doto (ui/load-fxml "welcome/new-project-pane.fxml")
     (ui/with-controls [template-list title-field location-browse-field submit-button]
       (setup-directory-browse-field! "Select New Project Location" location-browse-field)
-      (ui/on-action! submit-button
-                     (fn [_]
-                       (let [project-title (ui/text title-field)
-                             project-location (browse-field-text location-browse-field)
-                             project-template (first (ui/selection template-list))]
-                         (dialogs/make-alert-dialog (pr-str {:project-title project-title
-                                                             :project-location project-location
-                                                             :project-template project-template})))))
       (doto template-list
         (ui/items! (get-in welcome-settings [:new-project :templates]))
         (ui/cell-factory! (fn [project-template]
-                            {:graphic (make-template-entry project-template)}))))))
+                            {:graphic (make-template-entry project-template)})))
+      (ui/on-action! submit-button
+                     (fn [_]
+                       (when-some [project-template (first (ui/selection template-list))]
+                         (let [project-title (ui/text title-field)
+                               project-location (browse-field-file location-browse-field)]
+                           (cond
+                             (string/blank? project-title)
+                             (dialogs/make-message-box "Invalid Project Title"
+                                                       "Please enter a title for the new project.")
+
+                             (not (fs/empty-directory? project-location))
+                             (dialogs/make-message-box "Invalid Project Location"
+                                                       "Please select an empty directory for the new project.")
+
+                             :else
+                             (when-some [template-zip-file (download-proj-zip! (:zip-url project-template))]
+                               (expand-proj-zip! template-zip-file project-location (:skip-root? project-template))
+                               (close-dialog-and-open-project! (io/file project-location "game.project")))))))))))
+
+;; -----------------------------------------------------------------------------
+;; Import project pane
+;; -----------------------------------------------------------------------------
 
 (defn- make-open-project-pane
   ^Parent []
   (let [pane (ui/load-fxml "welcome/open-project-pane.fxml")]
     pane))
+
+;; -----------------------------------------------------------------------------
+;; Welcome dialog
+;; -----------------------------------------------------------------------------
 
 (defn- make-pane-button [label pane]
   (doto (RadioButton.)
@@ -139,22 +213,47 @@
   ^Parent [pane-button]
   (ui/user-data pane-button :pane))
 
-(defn open-welcome-dialog! []
+(defn- install-pending-update-check! [^Stage stage update-context]
+  (let [tick-fn (fn [^AnimationTimer timer _]
+                  (when-let [pending (updater/pending-update update-context)]
+                    (when (not= pending (system/defold-editor-sha1))
+                      (.stop timer) ; we only ask once on the start screen
+                      (ui/run-later
+                        (when (dialogs/make-pending-update-dialog stage)
+                          (when (updater/install-pending-update! update-context (io/file (system/defold-resourcespath)))
+                            (updater/restart!)))))))
+        timer (ui/->timer 0.1 "pending-update-check" tick-fn)]
+    (.setOnShown stage (ui/event-handler event (ui/timer-start! timer)))
+    (.setOnHiding stage (ui/event-handler event (ui/timer-stop! timer)))))
+
+(defn show-welcome-dialog! [prefs update-context open-project-fn]
   (let [welcome-settings (load-welcome-settings "welcome/welcome.edn")
         root (ui/load-fxml "welcome/welcome-dialog.fxml")
         stage (ui/make-dialog-stage)
         scene (Scene. root)
+        close-dialog-and-open-project! (fn [^File defold-project-file]
+                                         (ui/close! stage)
+                                         (when (.isFile defold-project-file)
+                                           (prefs/set-prefs prefs open-project-directory-prefs-key (.getParent defold-project-file)))
+
+                                         ;; NOTE (TODO): We load the project in the same class-loader as welcome is loaded from.
+                                         ;; In other words, we can't reuse the welcome page and it has to be closed.
+                                         ;; We should potentially changed this when we have uberjar support and hence
+                                         ;; faster loading.
+                                         (open-project-fn (.getAbsolutePath defold-project-file))
+                                         nil)
         left-pane ^Parent (.lookup root "#left-pane")
         pane-buttons-toggle-group (ToggleGroup.)
         pane-buttons [(make-pane-button "HOME" (make-home-pane))
-                      (make-pane-button "NEW PROJECT" (make-new-project-pane welcome-settings))
+                      (make-pane-button "NEW PROJECT" (make-new-project-pane welcome-settings close-dialog-and-open-project!))
                       (make-pane-button "OPEN PROJECT" (make-open-project-pane))]]
 
-    ;; TEMP HACK, REMOVE!
-    ;; Reload stylesheets.
-    (let [styles (vec (.getStylesheets root))]
-      (.forget (com.sun.javafx.css.StyleManager/getInstance) scene)
-      (.setAll (.getStylesheets root) ^java.util.Collection styles))
+    ;; Make ourselves the main stage.
+    (ui/set-main-stage stage)
+
+    ;; Install pending update check.
+    (when update-context
+      (install-pending-update-check! stage update-context))
 
     ;; Add the pane buttons to the left panel and configure them to toggle between the panes.
     (doseq [pane-button pane-buttons]
@@ -168,6 +267,16 @@
 
     ;; Select the home pane button.
     (.selectToggle pane-buttons-toggle-group (second pane-buttons))
+
+    ;; ---------- TEMP HACK, REMOVE! ----------
+    ;; Press Cmd-R to reload the view.
+    (.setOnKeyPressed root (ui/event-handler event
+                             (let [key-event ^javafx.scene.input.KeyEvent event]
+                               (when (and (.isShortcutDown key-event)
+                                          (= "r" (.getText key-event)))
+                                 (ui/close! stage)
+                                 (show-welcome-dialog! prefs update-context open-project-fn)))))
+    ;; ---------- TEMP HACK, REMOVE! ----------
 
     ;; Show the dialog.
     (.setScene stage scene)
