75655f42c4f8ade55bc5d46d546e67efbd826bd8
Dan Engelbrecht <37074944+danengelbrecht-king@users.noreply.github.com>
2018-03-20 14:29:15 +0100

    Make sure to take spine Component position into account when writing back the transforms from the animated state (#2166)

--- engine/gamesys/src/gamesys/components/comp_spine_model.cpp
+++ engine/gamesys/src/gamesys/components/comp_spine_model.cpp
@@ -211,6 +211,7 @@ namespace dmGameSystem
         // Include instance transform in the GO instance reflecting the root bone
         dmArray<dmTransform::Transform>& pose = *dmRig::GetPose(component->m_RigInstance);
         if (!pose.Empty()) {
+            pose[0] = dmTransform::Mul(component->m_Transform, pose[0]);
             dmGameObject::SetBoneTransforms(component->m_NodeInstances[0], pose.Begin(), pose.Size());
         }
     }
