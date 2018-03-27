c8a2852b284b2f74ec1bd412cbaa07806c9c2984
Andreas Tadic <andreas.tadic@king.com>
2018-03-27 12:30:09 +0200

    DEF-3152. Split vertex buffers to adhere to max vertices reported by OpenGL (#2129)
    DEF-3152 Split vertex buffers to adhere to max vertices reported by OpenGL
    
    Added minor order to RenderListEntry for fine grained control on batching
    Added vertex buffer batching to comp_model.
    
    Split vertex buffers to adhere to max vertices reported by OpenGL
    
    Hardcoded values for iOS & Android

--- engine/graphics/src/opengl/graphics_opengl.h
+++ engine/graphics/src/opengl/graphics_opengl.h
@@ -25,6 +25,8 @@ namespace dmGraphics
         uint32_t                m_MaxTextureSize;
         TextureFilter           m_DefaultTextureMinFilter;
         TextureFilter           m_DefaultTextureMagFilter;
+        uint32_t                m_MaxElementVertices;
+        uint32_t                m_MaxElementIndices;
         // Counter to keep track of various modifications. Used for cache flush etc
         // Version zero is never used
         uint32_t                m_ModificationVersion;
