246d7091c28e2b9408e7cee646dff839b521dcee
Dan Engelbrecht <37074944+danengelbrecht-king@users.noreply.github.com>
2018-05-09 11:49:03 +0200

    DEF-3265 Script Instance Context (#2270)
    DEF-3265 Added new instance context table for script instances and improved the Lua callback helpers to be "leakproof"

--- engine/gui/src/gui_private.h
+++ engine/gui/src/gui_private.h
@@ -253,7 +253,7 @@ namespace dmGui
     {
         int                     m_InstanceReference;
         int                     m_DataReference;
-        int                     m_RefTableReference;
+        int                     m_ContextTableReference;
         Context*                m_Context;
         Script*                 m_Script;
         dmIndexPool16           m_NodePool;
