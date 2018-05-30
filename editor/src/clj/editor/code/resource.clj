beaac981f26378cdb82363b42133956960ab7d3a
Mats Gisselson <mats.gisselson@king.com>
2018-05-30 19:02:11 +0200

    DEFEDIT-1235 Projects with lots of JSON data use less memory

--- editor/src/clj/editor/code/resource.clj
+++ editor/src/clj/editor/code/resource.clj
@@ -3,8 +3,6 @@
             [dynamo.graph :as g]
             [editor.code.data :as data]
             [editor.code.util :as util]
-            [editor.graph-util :as gu]
-            [editor.defold-project :as project]
             [editor.resource-node :as resource-node]
             [editor.workspace :as workspace]
             [schema.core :as s])
@@ -29,18 +27,60 @@
 (g/deftype Regions [TRegion])
 (g/deftype RegionGrouping {s/Any [TRegion]})
 
+(defn guess-indent-type [lines]
+  ;; TODO: Use default from preferences if indeterminate.
+  (or (data/guess-indent-type (take 512 lines) 4)
+      :tabs))
+
 (defn read-fn [resource]
   (util/split-lines (slurp resource)))
 
 (defn write-fn [lines]
   (string/join "\n" lines))
 
-(defn- load-fn [project self resource]
+;; To save memory, we defer loading the file contents until it has been modified
+;; and read directly from disk up to that point. Once the file is edited we need
+;; the unmodified lines to be able to undo past the point when the file was last
+;; saved. When the first edit is performed, we load the original lines from disk
+;; and store it as user-data in the graph. We cannot store this in a property of
+;; the node, because undoing would clear out the state of the unmodified file as
+;; well, which would defeat the purpose.
+
+(defn- loaded-unmodified-lines
+  "Returns the unmodified lines for the specified node-id, or nil if the node
+  was never modified."
+  [node-id]
+  (g/user-data node-id :unmodified-lines))
+
+(defn- set-unmodified-lines!
+  "Sets the unmodified lines for the specified node-id."
+  [node-id lines]
+  (g/user-data! node-id :unmodified-lines lines))
+
+(defn- ensure-unmodified-lines!
+  "Ensures the unmodified lines of a resource has been loaded, and returns the
+  unmodified lines. The first time this is called for a node-id, the unmodified
+  lines will be read from disk. Subsequent calls return the unmodified lines
+  loaded during the first call. The loaded-unmodified-lines function will return
+  a non-nil value after this has been called."
+  [node-id evaluation-context]
+  (g/user-data-swap! node-id :unmodified-lines
+                     (fn [loaded-unmodified-lines]
+                       (or loaded-unmodified-lines
+                           (read-fn (g/node-value node-id :resource evaluation-context))))))
+
+(defn- eager-load [self resource]
   (let [lines (read-fn resource)
-        indent-type (data/guess-indent-type (take 10000 lines) 4)]
-    [(g/set-property self :indent-type indent-type)
-     (g/set-property self :lines lines)
-     (g/connect self :breakpoints project :breakpoints)]))
+        indent-type (guess-indent-type lines)]
+    (set-unmodified-lines! self lines) ; Avoids a disk read in modified-lines property setter.
+    (g/set-property self :modified-lines lines :modified-indent-type indent-type)))
+
+(defn- load-fn [eager-loading? connect-breakpoints? project self resource]
+  (concat
+    (when eager-loading?
+      (eager-load self resource))
+    (when connect-breakpoints?
+      (g/connect self :breakpoints project :breakpoints))))
 
 (g/defnk produce-breakpoint-rows [regions]
   (into (sorted-set)
@@ -48,36 +88,46 @@
               (map data/breakpoint-row))
         regions))
 
-(g/defnk produce-breakpoints [resource regions]
-  (into []
-        (comp (filter data/breakpoint-region?)
-              (map (fn [region]
-                     {:resource resource
-                      :line (data/breakpoint-row region)})))
-        regions))
-
 (g/defnode CodeEditorResourceNode
   (inherits resource-node/ResourceNode)
 
   (property cursor-ranges CursorRanges (default [data/document-start-cursor-range]) (dynamic visible (g/constantly false)))
-  (property indent-type IndentType (dynamic visible (g/constantly false)))
   (property invalidated-rows InvalidatedRows (default []) (dynamic visible (g/constantly false)))
-  (property lines Lines (default [""]) (dynamic visible (g/constantly false)))
+  (property modified-indent-type IndentType (dynamic visible (g/constantly false)))
+  (property modified-lines Lines (dynamic visible (g/constantly false))
+            (set (fn [evaluation-context self _old-value _new-value]
+                   (ensure-unmodified-lines! self evaluation-context)
+                   nil)))
   (property regions Regions (default []) (dynamic visible (g/constantly false)))
 
-  (output completions g/Any :cached (g/constantly {}))
   (output breakpoint-rows BreakpointRows :cached produce-breakpoint-rows)
+  (output completions g/Any (g/constantly {}))
+  (output indent-type IndentType :cached (g/fnk [modified-indent-type resource] (or modified-indent-type (guess-indent-type (read-fn resource)))))
+  (output lines Lines (g/fnk [save-value resource] (or save-value (read-fn resource))))
+  (output save-value Lines (g/fnk [_node-id modified-lines] (or modified-lines (loaded-unmodified-lines _node-id))))
 
-  ;; Breakpoints output only consumed by project (array input of all code files)
-  ;; and already cached there. Changing breakpoints and pulling project breakpoints
-  ;; does imply a pass over all C.E.R.N's of the project to produce new breakpoints,
-  ;; but does not seem to be much of a perf issue.
-  (output breakpoints project/Breakpoints produce-breakpoints)
-  (output save-value Lines (gu/passthrough lines)))
+  ;; To save memory, save-data is not cached. It is trivial to produce.
+  (output save-data g/Any resource-node/produce-save-data)
 
-(defn register-code-resource-type [workspace & {:keys [ext node-type icon view-types view-opts tags tag-opts label] :as args}]
-  (let [args (assoc args :load-fn load-fn
-                         :read-fn read-fn
-                         :write-fn write-fn
-                         :textual? true)]
+  ;; To save memory, we don't store the source-value in the graph.
+  ;; Instead we use its hash to determine if we've been dirtied.
+  ;; This output must still be named source-value since it is
+  ;; invalidated when saving.
+  (output source-value g/Any :cached (g/fnk [editable? resource]
+                                       (when editable?
+                                         (hash (read-fn resource)))))
+
+  ;; We're dirty if the hash of our non-nil save-value differs from the source.
+  (output dirty? g/Bool (g/fnk [_node-id editable? resource save-value source-value]
+                          (and editable? (some? save-value) (not= source-value (hash save-value))))))
+
+(defn register-code-resource-type [workspace & {:keys [ext node-type icon view-types view-opts tags tag-opts label eager-loading?] :as args}]
+  (let [debuggable? (contains? tags :debuggable)
+        load-fn (partial load-fn eager-loading? debuggable?)
+        args (-> args
+                 (dissoc :eager-loading?)
+                 (assoc :load-fn load-fn
+                        :read-fn read-fn
+                        :write-fn write-fn
+                        :textual? true))]
     (apply workspace/register-resource-type workspace (mapcat identity args))))
--- editor/src/clj/editor/json.clj
+++ editor/src/clj/editor/json.clj
@@ -5,6 +5,7 @@
             [editor.code.data :as data]
             [editor.code.resource :as r]
             [editor.graph-util :as gu]
+            [editor.resource :as resource]
             [editor.workspace :as workspace])
   (:import [java.io PushbackReader]))
 
@@ -44,29 +45,47 @@
 (def ^:private resource->json (comp read-then-close io/reader))
 (def ^:private lines->json (comp read-then-close data/lines-reader))
 
+(defn invalid-json-error [node-id resource]
+  (g/->error node-id nil :fatal nil
+             (format "The file '%s' contains invalid JSON data." (resource/proj-path resource))
+             {:type :invalid-content :resource resource}))
+
 (g/defnode JsonNode
   (inherits r/CodeEditorResourceNode)
   (property content-transform g/Any (dynamic visible (g/constantly false)))
   (input consumer-passthrough g/Any)
   (output consumer-passthrough g/Any (gu/passthrough consumer-passthrough))
-  (output content g/Any (g/fnk [content-transform lines resource editable?]
-                          (content-transform (if editable?
-                                               (lines->json lines)
-                                               (resource->json resource))))))
+  (output content g/Any (g/fnk [_node-id content-transform editable? lines resource]
+                          (try
+                            (content-transform (if editable?
+                                                 (lines->json lines)
+                                                 (resource->json resource)))
+                            (catch Exception _
+                              (invalid-json-error _node-id resource))))))
 
-(defn load-json [_project self resource]
+(defn- make-code-editable [project self resource lines]
+  (concat
+    (g/set-property self :content-transform identity :editable? true :modified-indent-type (r/guess-indent-type lines))
+    (when (resource/file-resource? resource)
+      (g/connect self :save-data project :save-data))))
+
+(defn load-json [project self resource]
   (let [lines (r/read-fn resource)
-        content (lines->json lines)
-        [load-fn accept-fn new-content] (some (fn [[_loader-id {:keys [accept-fn load-fn]}]]
-                                                (when-some [new-content (accept-fn content)]
-                                                  [load-fn accept-fn new-content]))
-                                              @json-loaders)]
-    (if (some? load-fn)
-      (concat
-        (g/set-property self :content-transform accept-fn :editable? false)
-        (load-fn self new-content))
-      (let [indent-type (data/guess-indent-type (take 500 lines) 4)]
-        (g/set-property self :content-transform identity :editable? true :indent-type indent-type :lines lines)))))
+        content (try
+                  (lines->json lines)
+                  (catch Exception error
+                    error))]
+    (if (instance? Exception content)
+      (make-code-editable project self resource lines)
+      (let [[load-fn accept-fn new-content] (some (fn [[_loader-id {:keys [accept-fn load-fn]}]]
+                                                    (when-some [new-content (accept-fn content)]
+                                                      [load-fn accept-fn new-content]))
+                                                  @json-loaders)]
+        (if (nil? load-fn)
+          (make-code-editable project self resource lines)
+          (concat
+            (g/set-property self :content-transform accept-fn :editable? false)
+            (load-fn self new-content)))))))
 
 (defn register-resource-types [workspace]
   (workspace/register-resource-type workspace
@@ -79,7 +98,8 @@
                                     :load-fn load-json
                                     :read-fn r/read-fn
                                     :write-fn r/write-fn
-                                    :textual? true))
+                                    :textual? true
+                                    :auto-connect-save-data? false))
 
 (defn register-json-loader [loader-id accept-fn load-fn]
   (swap! json-loaders assoc loader-id {:accept-fn accept-fn :load-fn load-fn}))
