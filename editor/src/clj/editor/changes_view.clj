8ccdadfbc32d56c36790b2a5e86df72757b9933f
Mats Gisselson <mats.gisselson@king.com>
2018-09-27 18:21:23 +0200

    DEFEDIT-1466 Hot Reload everything

--- editor/src/clj/editor/changes_view.clj
+++ editor/src/clj/editor/changes_view.clj
@@ -75,8 +75,8 @@
                   :command :referencing-files}
                  {:label "Dependencies..."
                   :command :dependencies}
-                 {:label "Hot Reload"
-                  :command :hot-reload}
+                 {:label "Hot Reload File"
+                  :command :hot-reload-file}
                  {:label :separator}
                  {:label "View Diff"
                   :icon "icons/32/Icons_S_06_arrowup.png"
