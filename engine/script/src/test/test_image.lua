14c52aa022d483884c778d3b0c48b77c858f146a
mathiaswking <mathias.westerdahl@king.com>
2018-10-09 17:52:05 +0200

    Updated with tests + test data

--- engine/script/src/test/test_image.lua
+++ engine/script/src/test/test_image.lua
@@ -1,20 +1,71 @@
-function test_image()
-    local file = io.open("src/test/data/color_check_2x2.png",  "rb")
-    assert(file ~= nil)
-    local buf = file:read("*all")
-    local img = image.load(buf)
-    assert(img.width == 2)
-    assert(img.height == 2)
-    assert(#img.buffer == 2 * 2 * 4)
-    assert(img.type == image.TYPE_RGBA)
-
-    img = image.load(buf, true)
-    assert(img.width == 2)
-    assert(img.height == 2)
-    assert(#img.buffer == 2 * 2 * 4)
-    assert(img.type == image.TYPE_RGBA)
-
-    file:close()
+function string.tohex(str)
+    return (str:gsub('.', function (c)
+        return string.format('%02X', string.byte(c))
+    end))
 end
 
-functions = { test_image = test_image }
+
+local img_expected_bytes_rgba = {
+    0xFF, 0x00, 0x00, 0xFF,
+    0x00, 0xFF, 0x00, 0xD5,
+    0x00, 0x00, 0xFF, 0x57,
+    0xFF, 0xFF, 0xFF, 0x00 }
+
+local img_expected_bytes_rgb = {
+    0xFF, 0x00, 0x00,
+    0x00, 0xFF, 0x00,
+    0x00, 0x00, 0xFF,
+    0xFF, 0xFF, 0x00}
+
+local img_expected_bytes_png_premultiplied = {
+    0xFF, 0x00, 0x00, 0xFF,
+    0x00, 0xD5, 0x00, 0xD5,
+    0x00, 0x00, 0x57, 0x57,
+    0x00, 0x00, 0x00, 0x00 }
+
+local img_expected_bytes_jpg = {
+    0xFE, 0x00, 0x00,
+    0x00, 0xFF, 0x00,
+    0x00, 0x00, 0xFE,
+    0xFF, 0xFF, 0x01}
+
+local function verify_image(img, bpp, image_type, expected_bytes)
+    --print(img.width, img.height, img.type)
+    assert(img.width == 2)
+    assert(img.height == 2)
+    assert(#img.buffer == 2 * 2 * bpp)
+    assert(img.type == image_type)
+
+    local bytes = { string.byte(img.buffer, 1,-1) }
+
+    for i = 1,#bytes do
+        if expected_bytes[i] ~= bytes[i] then
+            print("image   ", string.tohex(img.buffer))
+            print("diff bytes", i, expected_bytes[i], bytes[i])
+        end
+        assert(expected_bytes[i] == bytes[i])
+    end
+end
+
+local function test_image(path, bpp, image_type, premultiply, expected_bytes)
+    local file = io.open(path, "rb")
+    assert(file ~= nil)
+    local buf = file:read("*all")
+    file:close()
+
+    print("Loading", path)
+    local img = image.load(buf, premultiply)
+    verify_image(img, bpp, image_type,  expected_bytes)
+
+end
+
+function test_images()
+    test_image("src/test/data/color_check_2x2.png", 4, image.TYPE_RGBA, false, img_expected_bytes_rgba)
+    test_image("src/test/data/color_check_2x2.png", 4, image.TYPE_RGBA, true, img_expected_bytes_png_premultiplied)
+    test_image("src/test/data/color_check_2x2.jpg", 3, image.TYPE_RGB, false, img_expected_bytes_jpg)
+    test_image("src/test/data/color_check_2x2_16.png", 4, image.TYPE_RGBA, false, img_expected_bytes_rgba) -- 16 bit
+    test_image("src/test/data/color_check_2x2_indexed.png", 3, image.TYPE_RGB, false, img_expected_bytes_rgb) -- 3 color palette
+end
+
+
+functions = { test_images = test_images }
