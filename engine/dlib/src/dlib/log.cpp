b53f879cdbb428e49be03ab834889815cc1c6880
Dan Engelbrecht <37074944+danengelbrecht-king@users.noreply.github.com>
2018-10-17 14:09:04 +0200

    DEF-3433 Improved pprint (#2576)
    DEF-3433 Improved pprint
    
    Added support to pprint multiple arguments
    Recursive printing is stopped by not expanding recursive table references
    Print length increased to just below 4Kb of characters
    "[Output truncated]" info message at end of all log output that gets truncated

--- engine/dlib/src/dlib/log.cpp
+++ engine/dlib/src/dlib/log.cpp
@@ -14,11 +14,14 @@
 #include "path.h"
 #include "sys.h"
 #include "network_constants.h"
+#include "align.h"
+#include "message.h"
 
 #ifdef ANDROID
 #include <android/log.h>
 #endif
 
+const char* LOG_OUTPUT_TRUNCATED_MESSAGE = "...\n[Output truncated]\n";
 const int DM_LOG_MAX_LOG_FILE_SIZE = 1024 * 1024 * 32;
 
 struct dmLogConnection
@@ -29,18 +32,6 @@ struct dmLogConnection
     }
 
     dmSocket::Socket m_Socket;
-};
-
-struct dmLogMessage
-{
-    enum Type
-    {
-        MESSAGE = 0,
-        SHUTDOWN = 1,
-    };
-
-    uint8_t m_Type;
-    char    m_Message[0];
 };
 
 static const uint32_t DLIB_MAX_LOG_CONNECTIONS = 16;
@@ -435,26 +426,29 @@ void dmLogInternal(dmLogSeverity severity, const char* domain, const char* forma
             break;
     }
 
-
-    // NOTE: Must be less than DM_MESSAGE_PAGE_SIZE
-    const int str_buf_size = 2048;
-    char tmp_buf[sizeof(dmLogMessage) + str_buf_size];
+    char tmp_buf[sizeof(dmLogMessage) + DM_LOG_MAX_STRING_SIZE];
     dmLogMessage* msg = (dmLogMessage*) &tmp_buf[0];
     char* str_buf = &tmp_buf[sizeof(dmLogMessage)];
 
     int n = 0;
-    n += DM_SNPRINTF(str_buf + n, str_buf_size - n, "%s:%s: ", severity_str, domain);
-    if (n < str_buf_size)
+    n += DM_SNPRINTF(str_buf + n, DM_LOG_MAX_STRING_SIZE - n, "%s:%s: ", severity_str, domain);
+    if (n < DM_LOG_MAX_STRING_SIZE)
     {
-        n += vsnprintf(str_buf + n, str_buf_size - n, format, lst);
+        n += vsnprintf(str_buf + n, DM_LOG_MAX_STRING_SIZE - n, format, lst);
     }
 
-    if (n < str_buf_size)
+    if (n < DM_LOG_MAX_STRING_SIZE)
     {
-        n += DM_SNPRINTF(str_buf + n, str_buf_size - n, "\n");
+        n += DM_SNPRINTF(str_buf + n, DM_LOG_MAX_STRING_SIZE - n, "\n");
     }
-    str_buf[str_buf_size-1] = '\0';
-    int actual_n = dmMath::Min(n, str_buf_size-1);
+
+    if (n >= DM_LOG_MAX_STRING_SIZE)
+    {
+        strcpy(&str_buf[DM_LOG_MAX_STRING_SIZE - (strlen(LOG_OUTPUT_TRUNCATED_MESSAGE) + 1)], LOG_OUTPUT_TRUNCATED_MESSAGE);
+    }
+
+    str_buf[DM_LOG_MAX_STRING_SIZE-1] = '\0';
+    int actual_n = dmMath::Min(n, (int)(DM_LOG_MAX_STRING_SIZE-1));
 
     g_TotalBytesLogged += actual_n;
 
@@ -487,7 +481,7 @@ void dmLogInternal(dmLogSeverity severity, const char* domain, const char* forma
         receiver.m_Socket = self->m_MessgeSocket;
         receiver.m_Path = 0;
         receiver.m_Fragment = 0;
-        dmMessage::Post(0, &receiver, 0, 0, 0, msg, dmMath::Min(sizeof(dmLogMessage) + n + 1, sizeof(tmp_buf)), 0);
+        dmMessage::Post(0, &receiver, 0, 0, 0, msg, dmMath::Min(sizeof(dmLogMessage) + actual_n + 1, sizeof(tmp_buf)), 0);
     }
 }
 
