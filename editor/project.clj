3ec54724a98f5ddab3a55e0bd3dfd9cf24f2b02d
Erik Angelin <erik.angelin@king.com>
2018-11-02 16:30:43 +0100

    remove TraceGL setting from dev profile

--- editor/project.clj
+++ editor/project.clj
@@ -135,8 +135,7 @@
                                 :resource-paths    ["test/resources"]
                                 :jvm-opts          ["-Ddefold.unpack.path=tmp/unpack"
                                                     "-Ddefold.log.dir="
-                                                    "-Djogl.debug.TraceGL"
-                                                    "-Djogl.debug.DebugGL"
+                                                    "-Djogl.debug.DebugGL" ; TraceGL is also useful
                                                     "-Djogl.texture.notexrect=true"
                                                     "-XX:+UnlockCommercialFeatures"
                                                     "-XX:+FlightRecorder"
