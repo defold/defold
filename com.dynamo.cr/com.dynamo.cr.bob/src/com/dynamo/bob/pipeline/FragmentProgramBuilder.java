4a6d40e3b8d2ebe47583f82dac878f02de4eb25e
Dan Engelbrecht <dan.engelbrecht@king.com>
2018-11-13 09:26:40 +0100

    Cleaned up some hardcoded paths

--- com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/pipeline/FragmentProgramBuilder.java
+++ com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/pipeline/FragmentProgramBuilder.java
@@ -1,16 +1,31 @@
 package com.dynamo.bob.pipeline;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-
+import com.dynamo.bob.Bob;
 import com.dynamo.bob.BuilderParams;
 import com.dynamo.bob.CompileExceptionError;
 import com.dynamo.bob.Task;
 import com.dynamo.bob.fs.IResource;
 import com.dynamo.bob.pipeline.ShaderProgramBuilder;
+import com.dynamo.bob.pipeline.ShaderUtil.ES2ToES3Converter;
+import com.dynamo.graphics.proto.Graphics.ShaderDesc;
 
 @BuilderParams(name = "FragmentProgram", inExts = ".fp", outExt = ".fpc")
 public class FragmentProgramBuilder extends ShaderProgramBuilder {
+
+    @Override
+    public void build(Task<Void> task) throws IOException, CompileExceptionError {
+        IResource in = task.getInputs().get(0);
+        try (ByteArrayInputStream is = new ByteArrayInputStream(in.getContent())) {
+            boolean isDebug = (project.hasOption("debug") || (project.option("variant", Bob.VARIANT_RELEASE) != Bob.VARIANT_RELEASE));
+
+            ShaderDesc shaderDesc = compile(is, ES2ToES3Converter.ShaderType.FRAGMENT_SHADER, in, task.getOutputs().get(0).getPath(), project.getPlatformStrings()[0], isDebug);
+            task.output(0).setContent(shaderDesc.toByteArray());
+        }
+    }
 
     public Task<Void> create(IResource input) throws IOException, CompileExceptionError {
 
@@ -22,8 +37,23 @@ public class FragmentProgramBuilder extends ShaderProgramBuilder {
         return taskBuilder.build();
     }
 
+    public void writeExtraDirectives(PrintWriter writer)
+    {
+        writer.println("#ifdef GL_ES");
+        writer.println("precision mediump float;");
+        writer.println("#endif");
+        writer.println("#ifndef GL_ES");
+        writer.println("#define lowp");
+        writer.println("#define mediump");
+        writer.println("#define highp");
+        writer.println("#endif");
+        writer.println();
+    }
+
     public static void main(String[] args) throws IOException, CompileExceptionError {
-        ShaderProgramBuilder.main(args);
+        System.setProperty("java.awt.headless", "true");
+        FragmentProgramBuilder builder = new FragmentProgramBuilder();
+        builder.BuildShader(args, ES2ToES3Converter.ShaderType.FRAGMENT_SHADER);
     }
 
 }
