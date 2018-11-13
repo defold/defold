d5ea51aa6273de7965a713c389d4b3746b203543
Dan Engelbrecht <dan.engelbrecht@king.com>
2018-11-13 09:26:40 +0100

    Cleaned up some hardcoded paths

--- com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/pipeline/VertexProgramBuilder.java
+++ com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/pipeline/VertexProgramBuilder.java
@@ -1,15 +1,31 @@
 package com.dynamo.bob.pipeline;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import com.dynamo.bob.Bob;
 import com.dynamo.bob.BuilderParams;
 import com.dynamo.bob.CompileExceptionError;
 import com.dynamo.bob.Task;
 import com.dynamo.bob.fs.IResource;
 import com.dynamo.bob.pipeline.ShaderProgramBuilder;
+import com.dynamo.bob.pipeline.ShaderUtil.ES2ToES3Converter;
+import com.dynamo.graphics.proto.Graphics.ShaderDesc;
 
 @BuilderParams(name = "VertexProgram", inExts = ".vp", outExt = ".vpc")
 public class VertexProgramBuilder extends ShaderProgramBuilder {
+
+    @Override
+    public void build(Task<Void> task) throws IOException, CompileExceptionError {
+        IResource in = task.getInputs().get(0);
+        try (ByteArrayInputStream is = new ByteArrayInputStream(in.getContent())) {
+            boolean isDebug = (project.hasOption("debug") || (project.option("variant", Bob.VARIANT_RELEASE) != Bob.VARIANT_RELEASE));
+
+            ShaderDesc shaderDesc = compile(is, ES2ToES3Converter.ShaderType.VERTEX_SHADER, in, task.getOutputs().get(0).getPath(), project.getPlatformStrings()[0], isDebug);
+            task.output(0).setContent(shaderDesc.toByteArray());
+        }
+    }
 
     public Task<Void> create(IResource input) throws IOException, CompileExceptionError {
 
@@ -22,8 +38,19 @@ public class VertexProgramBuilder extends ShaderProgramBuilder {
         return taskBuilder.build();
     }
 
+    public void writeExtraDirectives(PrintWriter writer)
+    {
+        writer.println("#ifndef GL_ES");
+        writer.println("#define lowp");
+        writer.println("#define mediump");
+        writer.println("#define highp");
+        writer.println("#endif");
+        writer.println();
+    }
+
     public static void main(String[] args) throws IOException, CompileExceptionError {
-        ShaderProgramBuilder.main(args);
+    	VertexProgramBuilder builder = new VertexProgramBuilder();
+        builder.BuildShader(args, ES2ToES3Converter.ShaderType.VERTEX_SHADER);
     }
 
 }
