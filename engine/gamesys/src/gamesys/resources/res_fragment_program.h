30f4919a8c2a978f57d39dea17284815214d3e85
Dan Engelbrecht <dan.engelbrecht@king.com>
2018-11-21 15:47:03 +0100

    Make sure to release the shader resources properly

--- engine/gamesys/src/gamesys/resources/res_fragment_program.h
+++ engine/gamesys/src/gamesys/resources/res_fragment_program.h
@@ -12,7 +12,6 @@ namespace dmGameSystem
     dmResource::Result ResFragmentProgramDestroy(const dmResource::ResourceDestroyParams& params);
 
     dmResource::Result ResFragmentProgramRecreate(const dmResource::ResourceRecreateParams& params);
-
 }
 
 #endif // DM_GAMESYS_RES_FRAGMENT_PROGRAM_H
