6fe69925df10da34c295412b6791b1a2671730f4
mathiaswking <mathias.westerdahl@king.com>
2018-11-29 17:12:38 +0100

    DEF-3662 Fixed issue of sprite.play_flipbook() sending 'animation_done' to deleted instances

--- engine/gamesys/src/gamesys/components/comp_sprite.cpp
+++ engine/gamesys/src/gamesys/components/comp_sprite.cpp
@@ -41,7 +41,6 @@ namespace dmGameSystem
         uint32_t                    m_MixedHash;
         dmMessage::URL              m_Listener;
         uint32_t                    m_AnimationID;
-        dmGameObject::HInstance     m_ListenerInstance;
         dmhash_t                    m_ListenerComponent;
         SpriteResource*             m_Resource;
         CompRenderConstants         m_RenderConstants;
@@ -240,7 +239,6 @@ namespace dmGameSystem
         component->m_Rotation = params.m_Rotation;
         component->m_Resource = (SpriteResource*)params.m_Resource;
         dmMessage::ResetURL(component->m_Listener);
-        component->m_ListenerInstance = 0x0;
         component->m_ListenerComponent = 0xff;
         component->m_ComponentIndex = params.m_ComponentIndex;
         component->m_Enabled = 1;
@@ -492,13 +490,13 @@ namespace dmGameSystem
             if (once && component->m_AnimTimer >= 1.0f)
             {
                 component->m_Playing = 0;
-                if (component->m_ListenerInstance != 0x0)
+                if (component->m_ListenerComponent != 0xff)
                 {
                     dmMessage::URL sender;
                     dmMessage::URL receiver = component->m_Listener;
                     if (!GetSender(component, &sender))
                     {
-                        dmLogError("Could not send animation_done to listener.");
+                        dmLogError("Could not send animation_done to sender: %s#%s", dmHashReverseSafe64(component->m_Listener.m_Path), dmHashReverseSafe64(component->m_Listener.m_Fragment));
                         return;
                     }
 
@@ -507,20 +505,29 @@ namespace dmGameSystem
                     // Engine has 0-based indices, scripts use 1-based
                     message.m_CurrentTile = component->m_CurrentAnimationFrame + 1;
                     message.m_Id = component->m_CurrentAnimation;
-                    receiver.m_Socket = dmGameObject::GetMessageSocket(dmGameObject::GetCollection(component->m_ListenerInstance));
+
+                    dmGameObject::HInstance listener_instance = dmGameObject::GetInstanceFromIdentifier(dmGameObject::GetCollection(component->m_Instance), component->m_Listener.m_Path);
+                    if (!listener_instance)
+                    {
+                        dmLogError("Could not send animation_done to instance: %s#%s", dmHashReverseSafe64(component->m_Listener.m_Path), dmHashReverseSafe64(component->m_Listener.m_Fragment));
+                        return;
+                    }
+
+                    receiver.m_Socket = dmGameObject::GetMessageSocket(dmGameObject::GetCollection(listener_instance));
                     sender.m_Socket = dmGameObject::GetMessageSocket(dmGameObject::GetCollection(component->m_Instance));
                     if (dmMessage::IsSocketValid(receiver.m_Socket) && dmMessage::IsSocketValid(sender.m_Socket))
                     {
                         dmGameObject::Result go_result = dmGameObject::GetComponentId(component->m_Instance, component->m_ComponentIndex, &sender.m_Fragment);
                         if (go_result == dmGameObject::RESULT_OK)
                         {
-                            receiver.m_Path = dmGameObject::GetIdentifier(component->m_ListenerInstance);
+                            receiver.m_Path = dmGameObject::GetIdentifier(listener_instance);
+                            assert(receiver.m_Path == component->m_Listener.m_Path); // Really, shouldn't they be the same??!?!
+
                             receiver.m_Fragment = component->m_ListenerComponent;
                             sender.m_Path = dmGameObject::GetIdentifier(component->m_Instance);
                             uintptr_t descriptor = (uintptr_t)dmGameSystemDDF::AnimationDone::m_DDFDescriptor;
                             uint32_t data_size = sizeof(dmGameSystemDDF::AnimationDone);
                             dmMessage::Result result = dmMessage::Post(&sender, &receiver, message_id, 0, descriptor, &message, data_size, 0);
-                            component->m_ListenerInstance = 0x0;
                             component->m_ListenerComponent = 0xff;
                             dmMessage::ResetURL(component->m_Listener);
                             if (result != dmMessage::RESULT_OK)
@@ -535,7 +542,6 @@ namespace dmGameSystem
                     }
                     else
                     {
-                        component->m_ListenerInstance = 0x0;
                         component->m_ListenerComponent = 0xff;
                     }
                 }
@@ -730,7 +736,6 @@ namespace dmGameSystem
                 dmGameSystemDDF::PlayAnimation* ddf = (dmGameSystemDDF::PlayAnimation*)params.m_Message->m_Data;
                 if (PlayAnimation(component, ddf->m_Id))
                 {
-                    component->m_ListenerInstance = dmGameObject::GetInstanceFromIdentifier(dmGameObject::GetCollection(component->m_Instance), params.m_Message->m_Sender.m_Path);
                     component->m_ListenerComponent = params.m_Message->m_Sender.m_Fragment;
                     component->m_Listener = params.m_Message->m_Sender;
                 }
