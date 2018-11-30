2a52990c02bcfa7a717121aa373799524b203b68
Sven Andersson <sven.andersson@king.com>
2018-11-30 11:19:27 +0100

    Minor fix.

--- editor/src/clj/editor/game_project.clj
+++ editor/src/clj/editor/game_project.clj
@@ -34,10 +34,10 @@
   (let [{:keys [settings-map meta-settings path->built-resource-settings]} user-data
         settings (into []
                        (comp (keep (fn [[path value]]
-                                     (if-not (:unknown-setting? (settings-core/get-meta-setting meta-settings path))
+                                     (if (:unknown-setting? (settings-core/get-meta-setting meta-settings path))
+                                       {:path path :value value}
                                        (when (and (some? value) (not= "" value))
-                                         {:path path :value value})
-                                       {:path path :value value})))
+                                         {:path path :value value}))))
                              (remove ignored-setting?)
                              (keep (fn [{:keys [path value] :as setting}]
                                      (let [meta-setting (settings-core/get-meta-setting meta-settings path)]
