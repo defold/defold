65064dbaabdfd2f168065f169857aeeb626e5616
Dan Engelbrecht <dan.engelbrecht@king.com>
2018-11-30 12:25:25 +0100

    First iteration of glslc build script

---
+++ share/ext/glslc/build_glslc.sh
@@ -0,0 +1,115 @@
+#!/usr/bin/env bash
+
+readonly PRODUCT=glslc
+readonly VERSION=v2018.0
+readonly FILE_URL=${VERSION}.tar.gz
+readonly BASE_URL=https://github.com/google/shaderc/archive/
+
+. ../common.sh
+
+function cmi_configure() {
+    pushd third_party >/dev/null
+    git clone --depth 1 https://github.com/google/googletest.git
+    git clone --depth 1 https://github.com/google/glslang.git
+    git clone --depth 1 https://github.com/KhronosGroup/SPIRV-Tools.git spirv-tools
+    git clone --depth 1 https://github.com/KhronosGroup/SPIRV-Headers.git spirv-headers
+    git clone --depth 1 https://github.com/google/re2.git
+    git clone --depth 1 https://github.com/google/effcee.git
+    popd >/dev/null
+
+    case $PLATFORM in
+        darwin)
+            CMAKE_GENERATOR=""
+            ;;
+
+        x86_64-darwin)
+            CMAKE_GENERATOR=""
+            ;;
+
+        linux)
+            CMAKE_GENERATOR=""
+            ;;
+
+        x86_64-linux)
+            CMAKE_GENERATOR=""
+            ;;
+    	win32)
+            CMAKE_GENERATOR="Visual Studio 14 2015"
+            ;;
+        x86_64-win32)
+            CMAKE_GENERATOR="Visual Studio 14 2015 Win64"
+            ;;
+    esac
+
+    set -e
+    mkdir -p build >/dev/null
+    pushd build >/dev/null
+    cmake -G"${CMAKE_GENERATOR}" ..
+    popd >/dev/null
+}
+
+function cmi_make() {
+    set -e
+
+    pushd build >/dev/null
+    cmake --build . --config Release --target glslc_exe
+    mkdir -p $PREFIX/bin
+    cp $PRODUCT/Release/$PRODUCT.exe $PREFIX/bin
+    popd >/dev/null
+
+    set +e
+}
+
+function cmi_buildplatform() {
+    cmi_do $PLATFORM ""
+
+    local TGZ="$PRODUCT-$VERSION-$PLATFORM.tar.gz"
+
+    pushd $PREFIX  >/dev/null
+    tar cfvz $TGZ bin
+
+    popd >/dev/null
+    popd >/dev/null
+
+    mkdir ../build
+
+    mv -v $PREFIX/$TGZ ../build
+    echo "../build/$TGZ created"
+
+    rm -rf tmp
+    rm -rf $PREFIX
+}
+
+function cmi() {
+    export PREFIX=`pwd`/build
+    export PLATFORM=$1
+
+    case $PLATFORM in
+        darwin)
+            cmi_buildplatform $PLATFORM
+            ;;
+
+        x86_64-darwin)
+            cmi_buildplatform $PLATFORM
+            ;;
+
+        linux)
+            cmi_buildplatform $PLATFORM
+            ;;
+
+        x86_64-linux)
+            cmi_buildplatform $PLATFORM
+            ;;
+
+    	win32|x86_64-win32)
+            cmi_setup_vs2015_env $PLATFORM
+            cmi_buildplatform $PLATFORM
+            ;;
+        *)
+            echo "Unknown target $PLATFORM" && exit 1
+            ;;
+    esac
+}
+
+download
+cmi $1
