feecaabf18ddafd180db6b8ff33a329adc270f1c
mathiaswking <mathias.westerdahl@king.com>
2018-12-09 11:46:25 +0100

    Added support for .RC files

--- build_tools/waf_dynamo.py
+++ build_tools/waf_dynamo.py
@@ -355,7 +355,7 @@ def default_flags(self):
                 # good command line: https://www.bountysource.com/issues/56805751-strdup-is-deprecated-in-msvc-use-the-iso-c-conformant-_strdup-instead
                 # '-fno-exceptions',
                 self.env.append_value(f, ['-msse4.2', '-fno-rtti', '-fms-compatibility','-fdelayed-template-parsing','-fms-extensions','-Wno-nonportable-include-path', '-Wno-ignored-attributes', '-target', 'x86_64-pc-win32-msvc', '-m64', '-g', '-gcodeview', '-gno-column-info', '-Wall', '-Werror=format', '-fvisibility=hidden','-Wno-expansion-to-defined', '-Wno-c++11-narrowing'])
-                self.env.append_value(f, ['-mincremental-linker-compatible', '-mthread-model', 'posix'])
+                self.env.append_value(f, ['-flto', '-mthread-model', 'posix'])
                 self.env.append_value(f, ['-DLUA_BYTECODE_ENABLE', '-DDDF_EXPOSE_DESCRIPTORS', '-DGOOGLE_PROTOBUF_NO_RTTI', '-D_CRT_SECURE_NO_WARNINGS', '-D_SCL_SECURE_NO_WARNINGS', '-D_WINSOCK_DEPRECATED_NO_WARNINGS', '-D__STDC_LIMIT_MACROS', '-DWINVER=0x0600', '-D_WIN32_WINNT=0x0600', '-DWIN32', '-D_MT'])
                 
                 for i in clang_includes + sdk_includes + llvm_includes:
@@ -1497,6 +1497,9 @@ def detect(conf):
             else:
                 path = '/usr/bin'
 
+            conf.find_program('windres', var='WINRC', mandatory = True)
+            conf.check_tool('winres')
+            
             os.environ['CC'] = '%s/clang' % path
             os.environ['CXX'] = '%s/clang++' % path
             conf.env['CC']      = '%s/clang' % path
