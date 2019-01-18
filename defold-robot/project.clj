fc15000e65e8283163d5c397438e3498b71c4837
Vlad Protsenko <vladislav.protsenko@king.com>
2019-01-18 11:41:21 +0100

    Recompile defold-robot with JDK 11

--- defold-robot/project.clj
+++ defold-robot/project.clj
@@ -1,9 +1,9 @@
-(defproject defold-robot "0.1.0"
+(defproject defold-robot "0.2.0"
   :description "FIXME: write description"
   :url "https://www.defold.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/data.json "0.2.6"]
                  [commons-io/commons-io "2.4"]
