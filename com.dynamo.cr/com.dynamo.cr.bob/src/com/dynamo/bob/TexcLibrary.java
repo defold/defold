8c253068a4a4310cfd7ec96a906c508638b39309
danengelbrecht-king <dan.engelbrecht@king.com>
2019-01-23 10:54:53 +0100

    Reinstating workaround for not being able to load find PVRTexLib on linux

--- com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/TexcLibrary.java
+++ com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/TexcLibrary.java
@@ -39,10 +39,14 @@ public class TexcLibrary {
             addToPath("jna.library.path", libPath);
             addToPath("java.library.path", libPath);
 
-            Bob.getLib(platform, "PVRTexLib"); // dependency of texc_shared
+            String pvrTextLibPath = Bob.getLib(platform, "PVRTexLib"); // dependency of texc_shared
 
             // TODO: sad with a platform specific hack and placing dependency knowledge here but...
-            if (platform == Platform.X86_64Win32 || platform == Platform.X86Win32) {
+            if (platform == Platform.X86Linux || platform == Platform.X86_64Linux) {
+                Bob.verbose("Loading PVRTexLib:'%s'", pvrTextLibPath);
+                System.load(pvrTextLibPath);		
+            }
+            else if (platform == Platform.X86_64Win32 || platform == Platform.X86Win32) {
                 Bob.getLib(platform, "msvcr120"); // dependency of PVRTexLib
             }
 
