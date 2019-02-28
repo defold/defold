9e6d61bfa9809a8640d8769440d70cf6fcb2dfd6
Vlad Protsenko <vladislav.protsenko@king.com>
2019-02-28 11:05:52 +0100

    Actualize linux and windows shortcuts

--- editor/test/editor/keymap_test.clj
+++ editor/test/editor/keymap_test.clj
@@ -2,9 +2,6 @@
   (:require [clojure.test :refer [are deftest is testing]]
             [editor.keymap :as keymap]))
 
-(def shortcut-keys {:control-down? "Ctrl"
-                    :meta-down?    "Meta"})
-
 (deftest default-bindings-are-valid
   (doseq [[_ key-bindings] keymap/platform->default-key-bindings]
     (is (keymap/make-keymap key-bindings {:valid-command? (constantly true)
@@ -41,37 +38,5 @@
                          :shortcut "Shift+Alt+X"}}))))))
 
 (deftest keymap-does-not-allow-shortcut-key
-  (make-keymap-errors [["Shortcut+A" :a]]))
-
-(deftest make-keymap-test
-  #_(testing "canonicalizes shortcut keys correctly"
-      (doseq [[shortcut-key _] shortcut-keys]
-        (are [shortcut command expected-key expected-modifiers]
-            (= [{:key           expected-key
-                 :alt-down?     (boolean (expected-modifiers :alt-down?))
-                 :control-down? (boolean (expected-modifiers :control-down?))
-                 :meta-down?    (boolean (expected-modifiers :meta-down?))
-                 :shift-down?   (boolean (expected-modifiers :shift-down?))}
-                [{:command command
-                  :shortcut shortcut
-                  :key-combo (KeyCombination/keyCombination shortcut)}]]
-               (first (keymap/make-keymap [[shortcut command]]
-                                          {:platform-shortcut-key shortcut-key
-                                           :throw-on-error? true
-                                           :valid-command? (constantly true)})))
-          ;; Here we exploit that 'A' is a default allowed typable shortcut
-          "A"               :a "A" #{}
-          "Shortcut+A"      :a "A" #{shortcut-key}
-          "Ctrl+A"          :a "A" #{:control-down?}
-          "Meta+A"          :a "A" #{:meta-down?}
-          "Shift+A"         :a "A" #{:shift-down?}
-          "Ctrl+Shortcut+A" :a "A" (hash-set :control-down? shortcut-key)
-          "Meta+Shortcut+A" :a "A" (hash-set :meta-down? shortcut-key))))
-
-  #_(testing "prefers shortcut key to the corresponding platform modifier key"
-      (doseq [[shortcut-key shortcut-name] shortcut-keys]
-        (let [km (keymap/make-keymap [["Shortcut+A" :shortcut]
-                                      [(str shortcut-name "+A") :platform-modifier]]
-                                     {:platform-shortcut-key shortcut-key})]
-          (is (= 1 (count km)))
-          (is (= :shortcut (-> km first val first :command)))))))
+  (is (= (make-keymap-errors [["Shortcut+A" :a]])
+         #{{:type :shortcut-key, :command :a, :shortcut "Shortcut+A"}})))
