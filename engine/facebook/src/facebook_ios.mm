1faf4a78a067b6538dd0761cc6c1b7f3f715cb5b
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-03-04 09:18:50 +0100

    Removed documentation for disabled Facebook events

--- engine/facebook/src/facebook_ios.mm
+++ engine/facebook/src/facebook_ios.mm
@@ -975,18 +975,15 @@ int Facebook_Me(lua_State* L)
  * registered with Facebook Analytics.
  *
  * - `facebook.EVENT_ACHIEVED_LEVEL`
- * - `facebook.EVENT_ACTIVATED_APP`
  * - `facebook.EVENT_ADDED_PAYMENT_INFO`
  * - `facebook.EVENT_ADDED_TO_CART`
  * - `facebook.EVENT_ADDED_TO_WISHLIST`
  * - `facebook.EVENT_COMPLETED_REGISTRATION`
  * - `facebook.EVENT_COMPLETED_TUTORIAL`
- * - `facebook.EVENT_DEACTIVATED_APP`
  * - `facebook.EVENT_INITIATED_CHECKOUT`
  * - `facebook.EVENT_PURCHASED`
  * - `facebook.EVENT_RATED`
  * - `facebook.EVENT_SEARCHED`
- * - `facebook.EVENT_SESSION_INTERRUPTIONS`
  * - `facebook.EVENT_SPENT_CREDITS`
  * - `facebook.EVENT_TIME_BETWEEN_SESSIONS`
  * - `facebook.EVENT_UNLOCKED_ACHIEVEMENT`
