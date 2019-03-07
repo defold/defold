1f94b01d1bb707282a8894520a28d1831d74ffc6
Markus Gustavsson <markus.gustavsson@king.com>
2019-03-07 09:37:52 +0100

    DEFEDIT-1568 Replace early require of editor.boot with bootloader

--- editor/src/clj/editor/bootloader.clj
+++ editor/src/clj/editor/bootloader.clj
@@ -3,8 +3,10 @@
   (:require [clojure.java.io :as io])
   (:import [java.util.concurrent LinkedBlockingQueue]))
 
+(def load-info (atom nil))
+
 (defn load-boot
-  [args]
+  []
   (let [batches (->> (io/resource "sorted_clojure_ns_list.edn")
                      (slurp)
                      (read-string))
@@ -16,8 +18,16 @@
                      (when (contains? batch 'editor.boot)
                        (.put boot-loaded? (Object.))))
                    (println "****** LOADING NSES TOOK:" (/ (- (System/nanoTime) ns-load-time) 1000000000.0))))]
-    ;; Wait until edior.boot is loaded
+    (reset! load-info [loader boot-loaded?])))
+
+(defn main
+  [args]
+  ;; Wait until edior.boot is loaded
+  ;; TODO: This causes a hiccup in the splash screen animation. Fix?
+  (let [[loader boot-loaded?] @load-info]
+    (reset! load-info nil)
+    (ns-unmap *ns* 'load-info)
     (.take boot-loaded?)
-    (let [main (resolve 'editor.boot/main)]
-      (main args loader))))
+    (let [boot-main (resolve 'editor.boot/main)]
+      (boot-main args loader))))
 
