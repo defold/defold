6f6e54674f1069adda5adfb1dd60d56945f182d3
Markus Gustavsson <markus.gustavsson@king.com>
2019-03-07 15:42:01 +0100

    DEFEDIT-1568 Remove debug printing

--- editor/src/clj/editor/bootloader.clj
+++ editor/src/clj/editor/bootloader.clj
@@ -12,12 +12,10 @@
                      (read-string))
         boot-loaded? (LinkedBlockingQueue.)
         loader (future
-                 (let [ns-load-time (System/nanoTime)]
-                   (doseq [batch batches]
-                     (dorun (pmap require batch))
-                     (when (contains? batch 'editor.boot)
-                       (.put boot-loaded? (Object.))))
-                   (println "****** LOADING NSES TOOK:" (/ (- (System/nanoTime) ns-load-time) 1000000000.0))))]
+                 (doseq [batch batches]
+                   (dorun (pmap require batch))
+                   (when (contains? batch 'editor.boot)
+                     (.put boot-loaded? (Object.)))))]
     (reset! load-info [loader boot-loaded?])))
 
 (defn wait-until-editor-boot-loaded
