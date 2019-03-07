8a2d42372516638c4a37762cb0d984bf82f0854b
Markus Gustavsson <markus.gustavsson@king.com>
2019-03-07 13:53:09 +0100

    DEFEDIT-1568 Clean up Start.java

--- editor/src/java/com/defold/editor/Start.java
+++ editor/src/java/com/defold/editor/Start.java
@@ -64,69 +64,7 @@ public class Start extends Application {
         return urls;
     }
 
-    //private LinkedBlockingQueue<Object> pool;
-    //private ThreadPoolExecutor threadPool;
     private static boolean createdFromMain = false;
-
-    // public Start() throws IOException {
-    //     pool = new LinkedBlockingQueue<>(1);
-    //     threadPool = new ThreadPoolExecutor(1, 1, 3000, TimeUnit.MILLISECONDS,
-    //             new LinkedBlockingQueue<Runnable>());
-    //     threadPool.allowCoreThreadTimeOut(true);
-    // }
-
-    // private ClassLoader makeClassLoader() {
-    //     ArrayList<URL> urls = extractURLs(System.getProperty("java.class.path"));
-    //     // The "boot class-loader", i.e. for java.*, sun.*, etc
-    //     ClassLoader parent = ClassLoader.getSystemClassLoader();
-    //     // Per instance class-loader
-    //     ClassLoader classLoader = new URLClassLoader(urls.toArray(new URL[urls.size()]), parent);
-    //     return classLoader;
-    // }
-
-    // private Object makeEditor() throws Exception {
-    //     ClassLoader classLoader = makeClassLoader();
-
-    //     // NOTE: Is context classloader required?
-    //     // Thread.currentThread().setContextClassLoader(classLoader);
-
-    //     Class<?> editorApplicationClass = classLoader.loadClass("com.defold.editor.EditorApplication");
-    //     Object editorApplication = editorApplicationClass.getConstructor(new Class[] { ClassLoader.class }).newInstance(classLoader);
-    //     return editorApplication;
-    // }
-
-    // private void poolEditor(long delay) {
-    //     FutureTask<Object> future = new FutureTask<>(new Callable<Object>() {
-
-    //         @Override
-    //         public Object call() throws Exception {
-    //             // Arbitrary sleep in order to reduce the CPU load while loading the project
-    //             try {
-    //                 //Thread.sleep(delay);
-    //                 Object editorApplication = makeEditor();
-    //                 pool.add(editorApplication);
-    //             } catch (Throwable t) {
-    //                 t.printStackTrace();
-    //                 logger.error("failed to make editor", t);
-    //                 throw t;
-    //             }
-    //             return null;
-    //         }
-
-    //     });
-    //     threadPool.submit(future);
-    // }
-
-    // public void openEditor(String[] args) throws Exception {
-    //     if (!createdFromMain) {
-    //         throw new RuntimeException(String.format("Multiple %s classes. ClassLoader errors?", this.getClass().getName()));
-    //     }
-    //     //poolEditor(3000);
-    //     //Object editorApplication = pool.take();
-    //     Object editorApplication = makeEditor();
-    //     Method run = editorApplication.getClass().getMethod("run", new Class[]{ String[].class });
-    //     run.invoke(editorApplication, new Object[] { args });
-    // }
 
     private void reKickLoading(Splash splash) throws Exception {
         // Do this work in a different thread or it will stop the splash screen from showing/animating.
@@ -166,52 +104,6 @@ public class Start extends Application {
             });
         kickThread.start();
     }
-
-    // private void kickLoading(Splash splash) throws Exception {
-    //     threadPool.submit(() -> {
-    //         try {
-    //             // A terrible hack as an attempt to avoid a deadlock when loading native libraries
-    //             // Prism might be loading native libraries at this point, although we kick this loading after the splash has been shown.
-    //             // The current hypothesis is that the splash is "onShown" before the loading has finished and rendering can start.
-    //             // Occular inspection shows the splash as grey for a few frames (1-3?) before filled in with graphics. That grey-time also seems to differ between runs.
-    //             // This is an attempt to make the deadlock less likely to happen and hopefully avoid it altogether. No guarantees.
-    //             //Thread.sleep(200);
-    //             ResourceUnpacker.unpackResources();
-    //             ClassLoader parent = ClassLoader.getSystemClassLoader();
-    //             Class<?> glprofile = parent.loadClass("com.jogamp.opengl.GLProfile");
-    //             Method init = glprofile.getMethod("initSingleton");
-    //             init.invoke(null);
-    //         } catch (Throwable t) {
-    //             t.printStackTrace();
-    //             logger.error("failed to extract native libs", t);
-    //         }
-    //         try {
-    //             //pool.add(makeEditor());
-    //             javafx.application.Platform.runLater(new Runnable() {
-    //                 @Override
-    //                 public void run() {
-    //                     try {
-    //                         List<String> params = getParameters().getRaw();
-    //                         openEditor(params.toArray(new String[params.size()]));
-    //                         splash.close();
-    //                     } catch (Throwable t) {
-    //                         t.printStackTrace();
-    //                         logger.error("failed to open editor", t);
-    //                     }
-    //                 }
-    //             });
-    //         } catch (Throwable t) {
-    //             t.printStackTrace();
-    //             String message = (t instanceof InvocationTargetException) ? t.getCause().getMessage() : t.getMessage();
-    //             logger.error(message, t);
-    //             javafx.application.Platform.runLater(() -> {
-    //                 splash.setLaunchError(message);
-    //                 splash.setErrorShowing(true);
-    //             });
-    //         }
-    //         return null;
-    //     });
-    // }
 
     private void prunePackages() {
         String sha1 = System.getProperty("defold.editor.sha1");
