12212cb97320922fa4036ccd80bd90e92d6260ef
Markus Gustavsson <markus.gustavsson@king.com>
2019-04-02 12:15:54 +0200

    DEFEDIT-992 Use a delay instead of explicit atom

--- editor/src/clj/service/smoke_log.clj
+++ editor/src/clj/service/smoke_log.clj
@@ -5,20 +5,11 @@
 
 (set! *warn-on-reflection* true)
 
-(def ^:private enabled-atom (atom nil))
-
-(defn- enabled?
-  []
-  (let [enabled @enabled-atom]
-    (if (nil? enabled)
-      (let [enabled (Boolean/valueOf (System/getProperty "defold.smoke.log"))]
-        (reset! enabled-atom enabled)
-        enabled)
-      enabled)))
+(def ^:constant ^:private enabled? (delay (Boolean/valueOf (System/getProperty "defold.smoke.log"))))
 
 (defn smoke-log
   [^String message]
-  (when (enabled?)
+  (when @enabled?
     (let [logger (LoggerFactory/getLogger "DEFOLD-SMOKE-LOG")]
       (.info logger message))))
 
