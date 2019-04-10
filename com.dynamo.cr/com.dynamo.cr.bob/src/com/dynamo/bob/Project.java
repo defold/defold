3c90433c8e9304a16c63ed79116791496de0b510
mathiaswking <mathias.westerdahl@king.com>
2019-04-10 09:46:51 +0200

    Added snakeyaml support More cleanup

--- com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/Project.java
+++ com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/Project.java
@@ -508,7 +508,6 @@ public class Project {
 
         try {
             // Include built-in/default facebook and gms resources
-            resourceDirectories.add(Bob.getPath("res/facebook"));
             resourceDirectories.add(Bob.getPath("res/com.android.support.support-compat-27.1.1"));
             resourceDirectories.add(Bob.getPath("res/com.android.support.support-core-ui-27.1.1"));
             resourceDirectories.add(Bob.getPath("res/com.android.support.support-media-compat-27.1.1"));
