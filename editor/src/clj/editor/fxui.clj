664177b9b24f8fba4ac93caa5fdd4a802274a250
Vlad Protsenko <vladislav.protsenko@king.com>
2019-04-17 17:00:35 +0200

    Finish resolution dialog

--- editor/src/clj/editor/fxui.clj
+++ editor/src/clj/editor/fxui.clj
@@ -1,13 +1,13 @@
 (ns editor.fxui
   (:require [cljfx.api :as fx]
-            [editor.util :as eutil]
-            [editor.ui :as ui])
-  (:import [javafx.stage Stage]
-           [javafx.beans.value ChangeListener]
-           [cljfx.lifecycle Lifecycle]))
+            [editor.error-reporting :as error-reporting]
+            [editor.ui :as ui]
+            [editor.util :as eutil])
+  (:import [cljfx.lifecycle Lifecycle]
+           [javafx.application Platform]))
 
 (def ext-value
-  "Extension lifecycle that just returns a value on `:value` key"
+  "Extension lifecycle that returns value on `:value` key"
   (reify Lifecycle
     (create [_ desc _]
       (:value desc))
@@ -15,13 +15,38 @@
       (:value desc))
     (delete [_ _ _])))
 
+(defn mount-renderer-and-await-result!
+  "Mounts `renderer` and blocks current thread until `state-atom`'s value
+  receives has a `:result` key"
+  [state-atom renderer]
+  (let [event-loop-key (Object.)
+        result-promise (promise)]
+    (future
+      (error-reporting/catch-all!
+        (let [result @result-promise]
+          (fx/on-fx-thread
+            (Platform/exitNestedEventLoop event-loop-key result)))))
+    (add-watch state-atom event-loop-key
+               (fn [k r _ n]
+                 (let [result (:result n ::no-result)]
+                   (when-not (= result ::no-result)
+                     (deliver result-promise result)
+                     (remove-watch r k)))))
+    (fx/mount-renderer state-atom renderer)
+    (Platform/enterNestedEventLoop event-loop-key)))
+
+(defn wrap-state-handler [state-atom f]
+  (-> f
+      (fx/wrap-co-effects
+        {:state (fx/make-deref-co-effect state-atom)})
+      (fx/wrap-effects
+        {:state (fx/make-reset-effect state-atom)})))
+
 (defn stage [props]
-  {:fx/type fx/ext-on-instance-lifecycle
-   :on-created (fn [^Stage stage]
-                 (.addListener (.focusedProperty stage) ^ChangeListener ui/focus-change-listener))
-   :desc (assoc props
-           :fx/type :stage
-           :icons (if (eutil/is-mac-os?) [] [ui/application-icon-image]))})
+  (assoc props
+    :fx/type :stage
+    :on-focused-changed ui/focus-change-listener
+    :icons (if (eutil/is-mac-os?) [] [ui/application-icon-image])))
 
 (defn dialog-stage [props]
   (assoc props
@@ -31,28 +56,31 @@
     :modality (if (contains? props :owner) :window-modal :application-modal)))
 
 (defn dialog-body [{:keys [size header content footer]
-                    :or {size :small}}]
-  {:fx/type :v-box
-   :style-class ["dialog-body" (case size
-                                 :small "dialog-body-small"
-                                 :large "dialog-body-large")]
-   :children (if (some? content)
-               [{:fx/type :v-box
-                 :style-class "dialog-with-content-header"
-                 :children [header]}
-                {:fx/type :v-box
-                 :style-class "dialog-content"
-                 :children [content]}
-                {:fx/type :v-box
-                 :style-class "dialog-with-content-footer"
-                 :children [footer]}]
-               [{:fx/type :v-box
-                 :style-class "dialog-without-content-header"
-                 :children [header]}
-                {:fx/type :region :style-class "dialog-no-content"}
-                {:fx/type :v-box
-                 :style-class "dialog-without-content-footer"
-                 :children [footer]}])})
+                    :or {size :small}
+                    :as props}]
+  (-> props
+      (dissoc :size :header :content :footer)
+      (assoc :fx/type :v-box
+             :style-class ["dialog-body" (case size
+                                           :small "dialog-body-small"
+                                           :large "dialog-body-large")]
+             :children (if (some? content)
+                         [{:fx/type :v-box
+                           :style-class "dialog-with-content-header"
+                           :children [header]}
+                          {:fx/type :v-box
+                           :style-class "dialog-content"
+                           :children [content]}
+                          {:fx/type :v-box
+                           :style-class "dialog-with-content-footer"
+                           :children [footer]}]
+                         [{:fx/type :v-box
+                           :style-class "dialog-without-content-header"
+                           :children [header]}
+                          {:fx/type :region :style-class "dialog-no-content"}
+                          {:fx/type :v-box
+                           :style-class "dialog-without-content-footer"
+                           :children [footer]}]))))
 
 (defn dialog-buttons [props]
   (assoc props :fx/type :h-box :style-class "dialog-buttons"))
