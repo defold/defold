e1d1b84999d72ee8b51e00741e35c5f207479848
Jhonny Göransson <jhonny.goransson@king.com>
2019-04-24 14:48:26 +0200

    Style fixes before review

--- engine/dlib/src/dlib/dns.h
+++ engine/dlib/src/dlib/dns.h
@@ -3,18 +3,18 @@
 
 namespace dmSocket
 {
-	struct Address;
+    struct Address;
 }
 
 namespace dmDNS
 {
-	/**
+    /**
      * A channel roughly translates to a socket on which to put the name lookup requests on.
      * Internal implementation resides in dns.cpp.
      */
-	typedef void* HChannel;
+    typedef void* HChannel;
 
-	enum Result
+    enum Result
     {
         RESULT_OK             = 0,
         RESULT_INIT_ERROR     = -1,
@@ -23,29 +23,29 @@ namespace dmDNS
         RESULT_UNKNOWN_ERROR  = -4
     };
 
-	Result Initialize();
-	Result Finalize();
+    Result Initialize();
+    Result Finalize();
 
-	/**
+    /**
      * Creates a new channel that can be used for DNS queries.
      * @param channel Pointer to the created channel if successfull, will be left alone otherwise
      * @return RESULT_OK on succcess
      */
-	Result NewChannel(HChannel* channel);
+    Result NewChannel(HChannel* channel);
 
-	/**
+    /**
      * Stops the current request (if available) on a channel. Thread-safe.
      * @param channel Handle to the channel
      */
-	void   StopChannel(HChannel channel);
+    void   StopChannel(HChannel channel);
 
-	/**
+    /**
      * Deletes the current channel and cancels all requests.
      * @param channel Handle to the channel
      */
-	void   DeleteChannel(HChannel channel);
+    void   DeleteChannel(HChannel channel);
 
-	/**
+    /**
      * Get host by name.
      * @param name  Hostname to resolve
      * @param address Host address result
@@ -54,7 +54,7 @@ namespace dmDNS
      * @param ipv6 Whether or not to search for IPv6 addresses
      * @return RESULT_OK on success
      */
-	Result GetHostByName(const char* name, dmSocket::Address* address, HChannel channel, bool ipv4 = true, bool ipv6 = true);
+    Result GetHostByName(const char* name, dmSocket::Address* address, HChannel channel, bool ipv4 = true, bool ipv6 = true);
 }
 
 #endif //DM_DSTRINGS_H
--- engine/dlib/src/dlib/dns_null.cpp
+++ engine/dlib/src/dlib/dns_null.cpp
@@ -3,9 +3,9 @@
 
 namespace dmDNS
 {
-    static Result SocketResultToDNS(const dmSocket::Result res)
+    static Result SocketToDNSResult(const dmSocket::Result res)
     {
-        Result dns_res = dmDNS::RESULT_UNKNOWN_ERROR;
+        Result dns_res;
         switch(res)
         {
             case(dmSocket::RESULT_OK):
@@ -14,6 +14,8 @@ namespace dmDNS
             case(dmSocket::RESULT_HOST_NOT_FOUND):
                 dns_res = dmDNS::RESULT_HOST_NOT_FOUND;
                 break;
+            default:
+                dns_res = dmDNS::RESULT_UNKNOWN_ERROR;
         }
 
         return dns_res;
@@ -26,6 +28,6 @@ namespace dmDNS
     void   DeleteChannel(HChannel channel) {}
     Result GetHostByName(const char* name, dmSocket::Address* address, HChannel channel, bool ipv4, bool ipv6)
     {
-        return SocketResultToDNS(dmSocket::GetHostByName(name, address, ipv4, ipv6));
+        return SocketToDNSResult(dmSocket::GetHostByName(name, address, ipv4, ipv6));
     }
 }
