9248fffbd7717338abdab10a40fb6b4ff4c44979
Markus Gustavsson <markus.gustavsson@king.com>
2019-04-25 16:30:03 +0200

    DEFEDIT-1536 Move all service connection properties to a file for easier changing

---
+++ editor/src/clj/editor/connection_properties.clj
@@ -0,0 +1,9 @@
+(ns editor.connection-properties)
+
+(def ^:const connection-properties
+  {:sentry {:project-id "97739"
+            :key        "9e25fea9bc334227b588829dd60265c1"
+            :secret     "f694ef98d47d42cf8bb67ef18a4e9cdb"}
+   :google-analytics {:tid "UA-83690-7"}
+   :git-issues {:url "https://github.com/defold/editor2-issues"}
+   :native-extensions {:build-server-url "https://build.defold.com"}})
