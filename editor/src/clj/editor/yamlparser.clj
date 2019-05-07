d8cd94f3ba59fd6b0609261eab63c47bfae8fbb8
Markus Gustavsson <markus.gustavsson@king.com>
2019-05-07 14:27:29 +0200

    DEFEDIT-1532 Possibility to pass key transformation function to `load`

--- editor/src/clj/editor/yamlparser.clj
+++ editor/src/clj/editor/yamlparser.clj
@@ -7,11 +7,11 @@
             [clojure.walk :refer [walk]]))
 
 (defn- translate
-  [x]
-  (let [wlk (partial walk translate identity)
+  [x key-transform-fn]
+  (let [wlk (partial walk #(translate % key-transform-fn) identity)
         f (condp instance? x
             ;; Turn maps into clojure maps and change keys to keywords, recurse
-            Map #(wlk (reduce (fn [m e] (assoc m (keyword (key e)) (val e))) {} %))
+            Map #(wlk (reduce (fn [m [k v]] (assoc m (key-transform-fn k) v)) {} %))
             ;; Turn lists into clojure vectors, recurse
             List #(wlk (into [] %))
             ;; Other values we leave as is.
@@ -29,12 +29,14 @@
 
   `source` must be an instance of `java.lang.String` or `java.io.Reader`.
 
-  The result is a nested collection of maps and vectors where the keys in maps
-  have been translated from strings to keywords."
-  [source]
-  (->
-    (condp instance? source
-      String (.loadFromString (new-loader) source)
-      Reader (.loadFromReader (new-loader) source)
-      (throw (IllegalArgumentException. "Can only load from a String or Reader.")))
-    (translate)))
+  The result is a nested collection of maps and vectors. You may optionally pass
+  in a function to transform the keys of any maps in the result. For example,
+  passing the keyword function will transform all the map keys to keywords."
+  ([source]
+   (load source identity))
+  ([source key-transform-fn]
+   (-> (condp instance? source
+         String (.loadFromString (new-loader) source)
+         Reader (.loadFromReader (new-loader) source)
+         (throw (IllegalArgumentException. "Can only load from a String or Reader.")))
+       (translate key-transform-fn))))
