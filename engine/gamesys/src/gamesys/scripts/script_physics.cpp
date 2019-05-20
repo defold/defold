3841454bde2ef55fd3b7c919cb154a8c4df4bb6d
Sven Andersson <sven.andersson@king.com>
2019-05-20 10:44:03 +0200

    Review fix.

--- engine/gamesys/src/gamesys/scripts/script_physics.cpp
+++ engine/gamesys/src/gamesys/scripts/script_physics.cpp
@@ -343,7 +343,7 @@ namespace dmGameSystem
 
     /*# set the gravity for collection
      *
-     * Set the gravity in runtime. The gravity change it is not global, it will only affect
+     * Set the gravity in runtime. The gravity change is not global, it will only affect
      * the collection that the function is called from.
      *
      * Note: For 2D physics the z component of the gravity vector will be ignored.
