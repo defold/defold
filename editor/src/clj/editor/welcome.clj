972244db0cacea65c082df281818bb09707e616b
Markus Gustavsson <markus.gustavsson@king.com>
2019-06-11 09:12:24 +0200

    #4164 Remove destination directory if git clone fails.

--- editor/src/clj/editor/welcome.clj
+++ editor/src/clj/editor/welcome.clj
@@ -689,6 +689,7 @@
                                       (fs/delete-directory! dest-directory {:fail :silently}))
                                     (close-dialog-and-open-project! (io/file dest-directory "game.project") false)))
                                 (catch Throwable error
+                                  (fs/delete-directory! dest-directory {:fail :silently})
                                   (ui/run-later
                                     (hide-progress! root)
                                     (error-reporting/report-exception! error)))))))
