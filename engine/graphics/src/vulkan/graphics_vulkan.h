9623b03fc4c8f74315ad3fc59fbeb0d9611a5678
Jhonny Göransson <jhonny.goransson@king.com>
2019-06-25 14:17:44 +0200

    Add graphics_vulkan when building with with_vulkan

---
+++ engine/graphics/src/vulkan/graphics_vulkan.h
@@ -0,0 +1,12 @@
+#ifndef __GRAPHICS_DEVICE_VULKAN__
+#define __GRAPHICS_DEVICE_VULKAN__
+
+namespace dmGraphics
+{
+    struct Context
+    {
+        uint32_t dummy;
+    };
+}
+#endif // __GRAPHICS_DEVICE_VULKAN__
+
