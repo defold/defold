31d8c5ea2d9d3788a513a8e44e7352e82b2ffc77
mathiaswking <mathias.westerdahl@king.com>
2019-08-29 18:36:47 +0200

    Updated android packaging script Added JDK 10 to Docker container

--- scripts/mobile/package_android_sdk.sh
+++ scripts/mobile/package_android_sdk.sh
@@ -2,14 +2,13 @@
 
 set -e
 
-ANDROID_NDK_VERSION=r10e
+ANDROID_NDK_VERSION=r20
 ANDROID_NDK=android-ndk-${ANDROID_NDK_VERSION}
-# Yikes!?
-ANDROID_PLATFORM=21
-ANDROID_TARGET_API_LEVEL=23
+
+ANDROID_PLATFORM=28
+ANDROID_TARGET_API_LEVEL=28
 ANDROID_BUILD_TOOLS_VERSION=23.0.2
 
-# Do we really need 14 for 32 bit?
 PLATFORM=android-${ANDROID_PLATFORM}
 
 HOST=`uname | tr '[:upper:]' '[:lower:]'`
@@ -19,19 +18,11 @@ ANDROID_NDK_FILENAME=android-ndk-${ANDROID_NDK_VERSION}-${HOST}-x86_64.zip
 ANDROID_NDK_URL=https://dl.google.com/android/repository/${ANDROID_NDK_FILENAME}
 
 # Contains the sdkmanager, which is used to install the sdk
-ANDROID_TOOLS_FILENAME=sdk-tools-${HOST}-3859397.zip
+ANDROID_TOOLS_FILENAME=sdk-tools-${HOST}-4333796.zip
 ANDROID_TOOLS_URL=https://dl.google.com/android/repository/${ANDROID_TOOLS_FILENAME}
-
-TOOL=~/android/${ANDROID_NDK}/build/tools/make-standalone-toolchain.sh
 
 PWD=`pwd`
 TMP=${PWD}/_tmpdir
-
-TOOLCHAIN_NAME=android-toolchain-${HOST}-${ANDROID_NDK_VERSION}
-TOOLCHAIN_INSTALLDIR=${TMP}/${TOOLCHAIN_NAME}
-if [ ! -e "${TOOLCHAIN_INSTALLDIR}" ]; then
-	mkdir -p ${TOOLCHAIN_INSTALLDIR}
-fi
 
 ANDROID_SDK_NAME=android-sdk-${HOST}-android-${ANDROID_TARGET_API_LEVEL}
 ANDROID_SDK_INSTALLDIR=${TMP}/${ANDROID_SDK_NAME}
@@ -55,16 +46,24 @@ if [ ! -e "${TMP}/${ANDROID_TOOLS_FILENAME}" ]; then
 	(cd ${ANDROID_SDK_INSTALLDIR} && unzip -q ${TMP}/${ANDROID_TOOLS_FILENAME})
 fi
 
-${TOOL} --arch=arm --platform=${PLATFORM} --toolchain=arm-linux-androideabi-4.8 --install-dir=${TOOLCHAIN_INSTALLDIR}
-${TOOL} --arch=arm64 --platform=${PLATFORM} --toolchain=aarch64-linux-android-4.9 --install-dir=${TOOLCHAIN_INSTALLDIR}
-(cd ${TMP} && tar -czf ${PWD}/${TOOLCHAIN_NAME}.tar.gz ${TOOLCHAIN_NAME})
 
-echo "CD ${ANDROID_SDK_INSTALLDIR}"
+echo "cd ${ANDROID_SDK_INSTALLDIR}"
+
+# Since Android SDK only works with JDK8, here's a config hack (for JDK 9 and 10, not 11)
+# e.g. export PATH=/usr/lib/jdk/jdk-10.0.2/bin:$PATH
+#export JAVA_OPTS="-XX:+IgnoreUnrecognizedVMOptions --add-modules java.se.ee"
+
 (cd ${ANDROID_SDK_INSTALLDIR} && echo y | ./tools/bin/sdkmanager --verbose "tools" "platform-tools" )
 (cd ${ANDROID_SDK_INSTALLDIR} && echo y | ./tools/bin/sdkmanager --verbose "extras;android;m2repository" "platforms;android-${ANDROID_TARGET_API_LEVEL}" "build-tools;${ANDROID_BUILD_TOOLS_VERSION}" )
-(cd ${ANDROID_SDK_INSTALLDIR} && echo y | ./tools/bin/sdkmanager --verbose --uninstall "emulator" )
-#(cd ${ANDROID_SDK_INSTALLDIR} && rm -rf ${ANDROID_SDK_INSTALLDIR}/emulator)
+
 # make sure it installed properly!
 (cd ${ANDROID_SDK_INSTALLDIR} && ls -la ./build-tools/${ANDROID_BUILD_TOOLS_VERSION}/dx )
+
+echo "Removing folders..."
+(cd ${TMP} && rm -rf ${ANDROID_SDK_NAME}/extras/android/m2repository/com/android/support/test)
+
+echo "Packing ${ANDROID_SDK_NAME}..."
+
 (cd ${TMP} && tar -czf ${PWD}/${ANDROID_SDK_NAME}.tar.gz ${ANDROID_SDK_NAME} )
 
+echo "Wrote ${PWD}/${ANDROID_SDK_NAME}.tar.gz"
