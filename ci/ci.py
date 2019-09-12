56c00b43d6687510ddb7a8aae55c96b7bd07808a
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-09-12 08:27:51 +0200

    Update ci.py

--- ci/ci.py
+++ ci/ci.py
@@ -9,10 +9,51 @@ def call(args):
     print(' '.join(args))
     subprocess.call(args, stdin=None, stdout=None, stderr=None, shell=False)
 
+def aptget(package):
+    call("sudo apt-get install --no-install-recommends " + package)
 
 def install(platform):
     if platform == 'linux' or platform == 'linux-64':
-        call("sudo apt-get install libxi-dev freeglut3-dev libglu1-mesa-dev libgl1-mesa-dev libxext-dev x11proto-xext-dev mesa-common-dev libxt-dev libx11-dev libcurl4-openssl-dev uuid-dev python-setuptools build-essential libopenal-dev rpm git curl autoconf libtool automake cmake tofrodos valgrind tree silversearcher-ag".split(" "))
+        aptget("gcc-5")
+        aptget("g++-5")
+        aptget("libssl-dev")
+        aptget("openssl")
+        aptget("libtool")
+        aptget("autoconf")
+        aptget("automake")
+        aptget("build-essential")
+        aptget("uuid-dev")
+        aptget("libxi-dev")
+        aptget("libopenal-dev")
+        aptget("libgl1-mesa-dev")
+        aptget("libglw1-mesa-dev")
+        aptget("freeglut3-dev")
+        call("update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 10")
+        call("update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 20")
+        call("update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 10")
+        call("update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 20")
+        call("update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 30")
+        call("update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 30")
+        call("update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 30")
+        call("update-alternatives --set c++ /usr/bin/g++")
+        aptget("clang-6.0")
+        call("update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-6.0 1000")
+        call("update-alternatives --install /usr/bin/clang clang /usr/bin/clang-6.0 1000")
+        call("update-alternatives --config clang")
+        call("update-alternatives --config clang++")
+        call("")
+        call("")
+        call("")
+        call("")
+        call("")
+        aptget("tofrodos")
+        aptget("cmake")
+        aptget("curl")
+        aptget("tree")
+        aptget("silversearcher-ag")
+        aptget("valgrind")
+
+        # call("sudo apt-get install libxi-dev freeglut3-dev libglu1-mesa-dev libgl1-mesa-dev libxext-dev x11proto-xext-dev mesa-common-dev libxt-dev libx11-dev libcurl4-openssl-dev uuid-dev python-setuptools build-essential libopenal-dev rpm git curl autoconf libtool automake cmake tofrodos valgrind tree silversearcher-ag".split(" "))
 
 
 def make_engine_builder(platform, skip_tests = True, with_valgrind = False, with_asan = False, with_vanilla_lua = False, skip_codesign = True, skip_docs = True, skip_builtins = True, branch = None, archive = False, org = 'defold', name = None):
