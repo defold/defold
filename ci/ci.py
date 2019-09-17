44a99e46581ff2849edd94b731398036657920b9
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-09-17 11:33:39 +0200

    Update ci.py

--- ci/ci.py
+++ ci/ci.py
@@ -48,68 +48,23 @@ def install():
         aptget("libgl1-mesa-dev")
         aptget("libglw1-mesa-dev")
         aptget("freeglut3-dev")
-        # call("update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 10")
-        # call("update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 20")
-        # call("update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 10")
-        # call("update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 20")
-        # call("update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 30")
-        # call("update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 30")
-        # call("update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 30")
-        # call("update-alternatives --set c++ /usr/bin/g++")
-        # aptget("clang-6.0") # part of GitHub CI virtual environment
-        # call("update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-6.0 1000")
-        # call("update-alternatives --install /usr/bin/clang clang /usr/bin/clang-6.0 1000")
-        # call("update-alternatives --config clang")
-        # call("update-alternatives --config clang++")
         aptget("tofrodos")
-        # aptget("cmake") # part of GitHub CI virtual environment
-        # aptget("curl") # part of GitHub CI virtual environment
         aptget("tree")
         aptget("silversearcher-ag")
         aptget("valgrind")
-    # elif system == "Windows":
-        # choco("visualstudio2017-workload-vctools")
-        # choco("mingw")
-        # choco("mingw-get")
-        # mingwget("msys-wget-bin")
-        # mingwget("msys-zip")
-        # mingwget("msys-unzip")
 
 
 
-def build_engine(platform, skip_tests = True, with_valgrind = False, with_asan = False, with_vanilla_lua = False, skip_codesign = True, skip_docs = True, skip_builtins = True, archive = False):
+def build_engine(platform, with_valgrind = False, with_asan = False, with_vanilla_lua = False, skip_tests = False, skip_codesign = True, skip_docs = False, skip_builtins = False, archive = False):
     args = 'python scripts/build.py distclean install_ext'.split()
     sub_args = []
 
-    if platform == 'ios':
-        args.append('--platform=armv7-darwin')
-    elif platform == 'ios-64':
-        args.append('--platform=arm64-darwin')
-    elif platform == 'ios-simulator':
-        args.append('--platform=x86_64-ios')
-    elif platform == 'darwin-64':
-        args.append('--platform=x86_64-darwin')
-    elif platform == 'android':
-        args.append('--platform=armv7-android')
-    elif platform == 'android-64':
-        args.append('--platform=arm64-android')
-    elif platform == 'js-web':
+    args.append('--platform=' + platform)
+
+    if platform == 'js-web' or platform == 'wasm-web':
         args.append('install_ems')
-        args.append('--platform=js-web')
-    elif platform == 'wasm-web':
-        args.append('install_ems')
-        args.append('--platform=wasm-web')
-    elif platform == 'win32':
-        args.append('--platform=win32')
-    elif platform == 'win32-64':
-        args.append('--platform=x86_64-win32')
-    elif platform == 'linux-64':
-        args.append('--platform=x86_64-linux')
-    else:
-        target_platform = ''
 
     args.append('build_engine')
-    args.append('--no-colors')
 
     if archive:
         args.append('archive_engine')
@@ -151,8 +106,6 @@ def build_editor(channel = None, release = False, engine_artifacts = None):
     # for install_ext
     args.append('--platform=x86_64-darwin')
 
-    args.append('--no-colors')
-
     # Specifies from where to get the engine SHA1.
     # From build.py:
     #     'What engine version to bundle the Editor with (auto, dynamo-home, archived, archived-stable or a SHA1)'
@@ -173,7 +126,6 @@ def build_bob(branch = None, channel = None, release = False):
         args.append("release")
 
     args.append("--")
-    args.append("--no-colors")
 
     if branch:
         args.append("--branch=" + branch)
@@ -186,11 +138,11 @@ def build_bob(branch = None, channel = None, release = False):
 
 
 def build_sdk():
-    call('python scripts/build.py build_sdk --no-colors')
+    call('python scripts/build.py build_sdk')
 
 
 def smoke_test():
-    call('python scripts/build.py distclean install_ext smoke_test --no-colors')
+    call('python scripts/build.py distclean install_ext smoke_test')
 
 
 def main(argv):
@@ -201,6 +153,10 @@ def main(argv):
     parser.add_argument("--with-asan", dest="with_asan", action='store_true', help="")
     parser.add_argument("--with-valgrind", dest="with_valgrind", action='store_true', help="")
     parser.add_argument("--with-vanilla-lua", dest="with_vanilla_lua", action='store_true', help="")
+    parser.add_argument("--archive", dest="archive", action='store_true', help="")
+    parser.add_argument("--skip-tests", dest="skip_tests", action='store_true', help="")
+    parser.add_argument("--skip-builtins", dest="skip_builtins", action='store_true', help="")
+    parser.add_argument("--skip-docs", dest="skip_docs", action='store_true', help="")
     args = parser.parse_args()
 
     platform = args.platform
@@ -230,7 +186,7 @@ def main(argv):
             if not platform:
                 raise Exception("No --platform specified.")
             with_valgrind = args.with_valgrind or (branch in [ "master", "beta" ])
-            build_engine(platform, with_valgrind = with_valgrind, with_asan = args.with_asan, with_vanilla_lua = args.with_vanilla_lua)
+            build_engine(platform, with_valgrind = with_valgrind, with_asan = args.with_asan, with_vanilla_lua = args.with_vanilla_lua, archive = args.archive, skip_tests = args.skip_tests, skip_builtins = args.skip_builtins, skip_docs = args.skip_docs)
         elif command == "editor":
             if branch == "master" or branch == "beta" or branch == "dev":
                 build_editor(channel = channel, release = True, engine_artifacts = "archived")
