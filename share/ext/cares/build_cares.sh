630e002c2bc865fceb84a373ba4be2c6b3269134
Jhonny Göransson <jhonny.goransson@king.com>
2019-09-20 14:24:23 +0200

    Bump git version

--- share/ext/cares/build_cares.sh
+++ share/ext/cares/build_cares.sh
@@ -2,9 +2,12 @@
 
 export CONF_TARGET=$1
 
+# Build cares with debug info
+# export CONFIGURE_FLAGS="--enable-debug"
+
 readonly PRODUCT=cares
 readonly TAR_INCLUDES=1
-readonly VERSION=602aaec984f862a5d59c9eb022f4317954c53917
+readonly VERSION=804753224fc27f806992ed8c7fe52e5762fd317a
 readonly BASE_URL=https://github.com/c-ares/c-ares.git
 
 . ../common.sh
@@ -18,7 +21,7 @@ function cmi_cleanup() {
 case $CONF_TARGET in
     *arm64-android)
         function cmi_unpack() {
-            echo -e "Checking out cares into libunwind_android"
+            echo -e "Checking out cares into cares_android_arm64"
             git clone $BASE_URL cares_android_arm64
             pushd cares_android_arm64
             git checkout $VERSION
