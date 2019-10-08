e6d790bfa63b7e9fd006bba59ed8d10944caae18
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-10-08 23:02:03 +0200

    Update ci.py

--- ci/ci.py
+++ ci/ci.py
@@ -180,9 +180,6 @@ def main(argv):
         release = False
 
 
-    print("S3 ACCESS KEY: %s" % (os.getenv('S3_ACCESS_KEY')))
-    print("Platform: %s Branch: %s Channel: %s" % (platform, branch, channel))
-
     for command in args.commands:
         if command == "engine":
             if not platform:
