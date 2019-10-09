c5340c6c2cb51afd5712e28bdc02f45383084284
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-10-09 16:20:30 +0200

    Update ci.py

--- ci/ci.py
+++ ci/ci.py
@@ -181,7 +181,7 @@ def main(argv):
     # set DYNAMO_HOME
     # call('python scripts/build.py shell')
     call('set')
-    call('echo("DYNAMO HOME ${DYNAMO_HOME}")')
+    call('echo "DYNAMO HOME ${DYNAMO_HOME}"')
     call('cd engine/liveupdate')
     call('PREFIX=$DYNAMO_HOME waf configure --platform=x86_64-darwin')
     exit(1)
