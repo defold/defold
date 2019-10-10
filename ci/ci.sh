fa46f445e353f85bb3b86130e8e097570d96b5c9
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-10-10 08:02:42 +0200

    Update ci.sh

--- ci/ci.sh
+++ ci/ci.sh
@@ -5,9 +5,10 @@ source ci/env.sh
 echo "DYNAMO $DYNAMO_HOME"
 echo "PATH $PATH"
 ./scripts/build.py --platform=x86_64-darwin install_ext
-./scripts/build.py --platform=x86_64-darwin --skip-tests build_engine -- --skip-build-tests
-cd engine/liveupdate
-PREFIX=$DYNAMO_HOME
-waf configure --platform=x86_64-darwin
+# ./scripts/build.py --platform=x86_64-darwin --skip-tests build_engine -- --skip-build-tests
+./scripts/build.py --platform=x86_64-darwin build_engine
+# cd engine/liveupdate
+# PREFIX=$DYNAMO_HOME
+# waf configure --platform=x86_64-darwin
 
 # python ./ci/ci.py "$@"
