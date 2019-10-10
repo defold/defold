f9b5288e5e464914eeab0a49ad6f9d85d167f610
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-10-10 11:12:35 +0200

    Update test_sound.cpp

--- engine/sound/src/test/test_sound.cpp
+++ engine/sound/src/test/test_sound.cpp
@@ -908,158 +908,158 @@ void DeviceLoopbackStop(dmSound::HDevice device)
 //     ASSERT_EQ(dmSound::RESULT_OK, r);
 // }
 //
-// TEST_P(dmSoundTestPlaySpeedTest, Play)
-// {
-//     TestParams params = GetParam();
-//     dmSound::Result r;
-//     dmSound::HSoundData sd = 0;
-//     dmSound::NewSoundData(params.m_Sound, params.m_SoundSize, params.m_Type, &sd, 1234);
-//
-//     dmSound::HSoundInstance instance = 0;
-//     r = dmSound::NewSoundInstance(sd, &instance);
-//     ASSERT_EQ(dmSound::RESULT_OK, r);
-//     ASSERT_NE((dmSound::HSoundInstance) 0, instance);
-//
-//     r = dmSound::SetParameter(instance, dmSound::PARAMETER_GAIN, Vectormath::Aos::Vector4(0.5f,0,0,0));
-//     ASSERT_EQ(dmSound::RESULT_OK, r);
-//     r = dmSound::SetParameter(instance, dmSound::PARAMETER_SPEED, Vectormath::Aos::Vector4(params.m_Speed,0,0,0));
-//     ASSERT_EQ(dmSound::RESULT_OK, r);
-//
-//     r = dmSound::Play(instance);
-//     ASSERT_EQ(dmSound::RESULT_OK, r);
-//
-//     r = dmSound::Play(instance);
-//     ASSERT_EQ(dmSound::RESULT_OK, r);
-//
-//     float a = 0.0f;
-//     do {
-//         r = dmSound::Update();
-//         ASSERT_EQ(dmSound::RESULT_OK, r);
-//         a += M_PI / 20000000.0f;
-//         if (a > M_PI*2) {
-//             a-= M_PI*2;
-//         }
-//         r = dmSound::SetParameter(instance, dmSound::PARAMETER_PAN, Vectormath::Aos::Vector4(cosf(a),0,0,0));
-//         ASSERT_EQ(dmSound::RESULT_OK, r);
-//
-//     } while (dmSound::IsPlaying(instance));
-//
-//     r = dmSound::DeleteSoundInstance(instance);
-//     ASSERT_EQ(dmSound::RESULT_OK, r);
-//
-//     r = dmSound::DeleteSoundData(sd);
-//     ASSERT_EQ(dmSound::RESULT_OK, r);
-// }
-//
-// const TestParams params_verify_ogg_test[] = {TestParams("loopback",
-//                                             MONO_RESAMPLE_FRAMECOUNT_16000_OGG,
-//                                             MONO_RESAMPLE_FRAMECOUNT_16000_OGG_SIZE,
-//                                             dmSound::SOUND_DATA_TYPE_OGG_VORBIS,
-//                                             2000,
-//                                             44100,
-//                                             35200,
-//                                             2048)};
-// INSTANTIATE_TEST_CASE_P(dmSoundVerifyOggTest, dmSoundVerifyOggTest, jc_test_values_in(params_verify_ogg_test));
-//
-// const TestParams params_test_play_test[] = {
-//     TestParams("default",
-//                 MONO_TONE_440_32000_64000_WAV,
-//                 MONO_TONE_440_32000_64000_WAV_SIZE,
-//                 dmSound::SOUND_DATA_TYPE_WAV,
-//                 440,
-//                 32000,
-//                 64000,
-//                 2048),
-//     TestParams("default",
-//                 MONO_TONE_440_44000_88000_WAV,
-//                 MONO_TONE_440_44000_88000_WAV_SIZE,
-//                 dmSound::SOUND_DATA_TYPE_WAV,
-//                 440,
-//                 44000,
-//                 88000,
-//                 2048),
-//     TestParams("default",
-//                 STEREO_TONE_440_32000_64000_WAV,
-//                 STEREO_TONE_440_32000_64000_WAV_SIZE,
-//                 dmSound::SOUND_DATA_TYPE_WAV,
-//                 440,
-//                 32000,
-//                 64000,
-//                 2048),
-//     TestParams("default",
-//                 STEREO_TONE_440_44000_88000_WAV,
-//                 STEREO_TONE_440_44000_88000_WAV_SIZE,
-//                 dmSound::SOUND_DATA_TYPE_WAV,
-//                 440,
-//                 44000,
-//                 88000,
-//                 2048),
-//     TestParams("default",
-//                 MONO_TONE_440_44100_88200_WAV,
-//                 MONO_TONE_440_44100_88200_WAV_SIZE,
-//                 dmSound::SOUND_DATA_TYPE_WAV,
-//                 440,
-//                 44100,
-//                 44100,
-//                 2048),
-// };
-// INSTANTIATE_TEST_CASE_P(dmSoundTestPlayTest, dmSoundTestPlayTest, jc_test_values_in(params_test_play_test));
-//
-//
-// const TestParams params_test_play_speed_test[] = {
-//     TestParams("default",
-//             MONO_TONE_440_44100_88200_WAV,
-//             MONO_TONE_440_44100_88200_WAV_SIZE,
-//             dmSound::SOUND_DATA_TYPE_WAV,
-//             440,
-//             44100,
-//             88200,
-//             2048,
-//             0.0f,
-//             2.0f),
-//     TestParams("default",
-//             MONO_TONE_440_32000_64000_WAV,
-//             MONO_TONE_440_32000_64000_WAV_SIZE,
-//             dmSound::SOUND_DATA_TYPE_WAV,
-//             440,
-//             32000,
-//             64000,
-//             2048,
-//             0.0f,
-//             2.0f),
-//     TestParams("default",
-//             STEREO_TONE_440_32000_64000_WAV,
-//             STEREO_TONE_440_32000_64000_WAV_SIZE,
-//             dmSound::SOUND_DATA_TYPE_WAV,
-//             440,
-//             32000,
-//             64000,
-//             2048,
-//             0.0f,
-//             2.0f),
-//     TestParams("default",
-//             MONO_TONE_440_44100_88200_WAV,
-//             MONO_TONE_440_44100_88200_WAV_SIZE,
-//             dmSound::SOUND_DATA_TYPE_WAV,
-//             440,
-//             44100,
-//             88200,
-//             2048,
-//             0.0f,
-//             0.5f),
-//     TestParams("default",
-//             STEREO_TONE_440_32000_64000_WAV,
-//             STEREO_TONE_440_32000_64000_WAV_SIZE,
-//             dmSound::SOUND_DATA_TYPE_WAV,
-//             440,
-//             32000,
-//             64000,
-//             2048,
-//             0.0f,
-//             0.5f),
-// };
-// INSTANTIATE_TEST_CASE_P(dmSoundTestPlaySpeedTest, dmSoundTestPlaySpeedTest, jc_test_values_in(params_test_play_speed_test));
-//
+TEST_P(dmSoundTestPlaySpeedTest, Play)
+{
+    TestParams params = GetParam();
+    dmSound::Result r;
+    dmSound::HSoundData sd = 0;
+    dmSound::NewSoundData(params.m_Sound, params.m_SoundSize, params.m_Type, &sd, 1234);
+
+    dmSound::HSoundInstance instance = 0;
+    r = dmSound::NewSoundInstance(sd, &instance);
+    ASSERT_EQ(dmSound::RESULT_OK, r);
+    ASSERT_NE((dmSound::HSoundInstance) 0, instance);
+
+    r = dmSound::SetParameter(instance, dmSound::PARAMETER_GAIN, Vectormath::Aos::Vector4(0.5f,0,0,0));
+    ASSERT_EQ(dmSound::RESULT_OK, r);
+    r = dmSound::SetParameter(instance, dmSound::PARAMETER_SPEED, Vectormath::Aos::Vector4(params.m_Speed,0,0,0));
+    ASSERT_EQ(dmSound::RESULT_OK, r);
+
+    r = dmSound::Play(instance);
+    ASSERT_EQ(dmSound::RESULT_OK, r);
+
+    r = dmSound::Play(instance);
+    ASSERT_EQ(dmSound::RESULT_OK, r);
+
+    float a = 0.0f;
+    do {
+        r = dmSound::Update();
+        ASSERT_EQ(dmSound::RESULT_OK, r);
+        a += M_PI / 20000000.0f;
+        if (a > M_PI*2) {
+            a-= M_PI*2;
+        }
+        r = dmSound::SetParameter(instance, dmSound::PARAMETER_PAN, Vectormath::Aos::Vector4(cosf(a),0,0,0));
+        ASSERT_EQ(dmSound::RESULT_OK, r);
+
+    } while (dmSound::IsPlaying(instance));
+
+    r = dmSound::DeleteSoundInstance(instance);
+    ASSERT_EQ(dmSound::RESULT_OK, r);
+
+    r = dmSound::DeleteSoundData(sd);
+    ASSERT_EQ(dmSound::RESULT_OK, r);
+}
+
+const TestParams params_verify_ogg_test[] = {TestParams("loopback",
+                                            MONO_RESAMPLE_FRAMECOUNT_16000_OGG,
+                                            MONO_RESAMPLE_FRAMECOUNT_16000_OGG_SIZE,
+                                            dmSound::SOUND_DATA_TYPE_OGG_VORBIS,
+                                            2000,
+                                            44100,
+                                            35200,
+                                            2048)};
+INSTANTIATE_TEST_CASE_P(dmSoundVerifyOggTest, dmSoundVerifyOggTest, jc_test_values_in(params_verify_ogg_test));
+
+const TestParams params_test_play_test[] = {
+    TestParams("default",
+                MONO_TONE_440_32000_64000_WAV,
+                MONO_TONE_440_32000_64000_WAV_SIZE,
+                dmSound::SOUND_DATA_TYPE_WAV,
+                440,
+                32000,
+                64000,
+                2048),
+    TestParams("default",
+                MONO_TONE_440_44000_88000_WAV,
+                MONO_TONE_440_44000_88000_WAV_SIZE,
+                dmSound::SOUND_DATA_TYPE_WAV,
+                440,
+                44000,
+                88000,
+                2048),
+    TestParams("default",
+                STEREO_TONE_440_32000_64000_WAV,
+                STEREO_TONE_440_32000_64000_WAV_SIZE,
+                dmSound::SOUND_DATA_TYPE_WAV,
+                440,
+                32000,
+                64000,
+                2048),
+    TestParams("default",
+                STEREO_TONE_440_44000_88000_WAV,
+                STEREO_TONE_440_44000_88000_WAV_SIZE,
+                dmSound::SOUND_DATA_TYPE_WAV,
+                440,
+                44000,
+                88000,
+                2048),
+    TestParams("default",
+                MONO_TONE_440_44100_88200_WAV,
+                MONO_TONE_440_44100_88200_WAV_SIZE,
+                dmSound::SOUND_DATA_TYPE_WAV,
+                440,
+                44100,
+                44100,
+                2048),
+};
+INSTANTIATE_TEST_CASE_P(dmSoundTestPlayTest, dmSoundTestPlayTest, jc_test_values_in(params_test_play_test));
+
+
+const TestParams params_test_play_speed_test[] = {
+    TestParams("default",
+            MONO_TONE_440_44100_88200_WAV,
+            MONO_TONE_440_44100_88200_WAV_SIZE,
+            dmSound::SOUND_DATA_TYPE_WAV,
+            440,
+            44100,
+            88200,
+            2048,
+            0.0f,
+            2.0f),
+    TestParams("default",
+            MONO_TONE_440_32000_64000_WAV,
+            MONO_TONE_440_32000_64000_WAV_SIZE,
+            dmSound::SOUND_DATA_TYPE_WAV,
+            440,
+            32000,
+            64000,
+            2048,
+            0.0f,
+            2.0f),
+    TestParams("default",
+            STEREO_TONE_440_32000_64000_WAV,
+            STEREO_TONE_440_32000_64000_WAV_SIZE,
+            dmSound::SOUND_DATA_TYPE_WAV,
+            440,
+            32000,
+            64000,
+            2048,
+            0.0f,
+            2.0f),
+    TestParams("default",
+            MONO_TONE_440_44100_88200_WAV,
+            MONO_TONE_440_44100_88200_WAV_SIZE,
+            dmSound::SOUND_DATA_TYPE_WAV,
+            440,
+            44100,
+            88200,
+            2048,
+            0.0f,
+            0.5f),
+    TestParams("default",
+            STEREO_TONE_440_32000_64000_WAV,
+            STEREO_TONE_440_32000_64000_WAV_SIZE,
+            dmSound::SOUND_DATA_TYPE_WAV,
+            440,
+            32000,
+            64000,
+            2048,
+            0.0f,
+            0.5f),
+};
+INSTANTIATE_TEST_CASE_P(dmSoundTestPlaySpeedTest, dmSoundTestPlaySpeedTest, jc_test_values_in(params_test_play_speed_test));
+
 TEST_P(dmSoundVerifyWavTest, Mix)
 {
     TestParams params = GetParam();
