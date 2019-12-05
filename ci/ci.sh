c39fc1cdf8370dd909d2057e1446356a48fa31c5
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-12-05 09:24:09 +0100

    Update ci.sh

--- ci/ci.sh
+++ ci/ci.sh
@@ -3,12 +3,14 @@ echo -e "\n\n\n\n\n-----------------\nci.sh started with args: $@"
 echo "Saving environment variables to file"
 ./scripts/build.py --save-env-path=ci/env.sh save_env
 
-echo "Sourcing saved env"
+echo "Saved environment variables:"
 cat ci/env.sh
+
+echo "Exporting saved environment variables"
 source ci/env.sh
 
-echo "GITHUB_REF is ${GITHUB_REF}"
 echo "Calling ci.py with args: $@"
+# -u to run python unbuffered to guarantee that output ends up in the correct order in logs
 python -u ./ci/ci.py "$@"
 
 echo -e "ci.sh finished\n-----------------\n\n\n\n\n"
