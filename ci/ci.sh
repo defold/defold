7d931f9e0c74b5e3acc3cb5be86b0efdc4440458
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-12-05 08:28:14 +0100

    Update ci.sh

--- ci/ci.sh
+++ ci/ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "ci.sh was started with args: $@"
+echo -e "\n\n\n\n\n-----------------ci.sh was started with args: $@"
 echo "Saving environment variables to file"
 ./scripts/build.py --save-env-path=ci/env.sh save_env
 
@@ -10,3 +10,5 @@ source ci/env.sh
 echo "GITHUB_REF is ${GITHUB_REF}"
 echo "Calling ci.py with args: $@"
 python ./ci/ci.py "$@"
+
+echo -e "Done calling cp.py with args: $@\n-----------------"
