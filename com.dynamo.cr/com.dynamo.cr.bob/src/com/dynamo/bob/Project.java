71a5c53b29a184253c6643551da2e7a38c537ce6
JCash <mwesterdahl76@gmail.com>
2019-12-10 12:09:13 +0100

    Issue 4594: The project settings are now read before the library resolve is done in bob.jar

--- com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/Project.java
+++ com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/Project.java
@@ -396,7 +396,7 @@ public class Project {
         return properties;
     }
 
-    public void loadProjectFile() throws IOException, ParseException {
+    public void loadProjectFile() throws IOException {
         IResource gameProject = getGameProjectResource();
         if (gameProject.exists()) {
             projectProperties = Project.loadProperties(gameProject, this.getPropertyFiles());
