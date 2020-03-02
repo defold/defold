164176ee1915d40fe10b0a9901bba7746b711c2e
JCash <mwesterdahl76@gmail.com>
2020-03-02 12:04:08 +0100

    Added support for gui lib

--- engine/gui/src/test/test_gui.cpp
+++ engine/gui/src/test/test_gui.cpp
@@ -24,6 +24,11 @@ extern "C"
 extern unsigned char BUG352_LUA[];
 extern uint32_t BUG352_LUA_SIZE;
 
+#if defined(__NX__)
+    #define MOUNTFS "host:/"
+#else
+    #define MOUNTFS ""
+#endif
 
 // Basic
 //  - Create scene
@@ -5596,7 +5601,7 @@ TEST_F(dmGuiTest, SpineNodeGetBoneNodes)
 bool LoadParticlefxPrototype(const char* filename, dmParticle::HPrototype* prototype)
 {
     char path[64];
-    dmSnPrintf(path, 64, "build/default/src/test/%s", filename);
+    dmSnPrintf(path, 64, MOUNTFS "build/default/src/test/%s", filename);
     const uint32_t MAX_FILE_SIZE = 4 * 1024;
     unsigned char buffer[MAX_FILE_SIZE];
     uint32_t file_size = 0;
