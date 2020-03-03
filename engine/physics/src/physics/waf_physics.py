8a3e22ab13bec81c35d46fc2f74837252e338db8
JCash <mwesterdahl76@gmail.com>
2020-03-03 14:24:24 +0100

    Physics lib config cleanup for feature selection

--- engine/physics/src/physics/waf_physics.py
+++ engine/physics/src/physics/waf_physics.py
@@ -1,11 +1,20 @@
-import Task, TaskGen
-import Options
-from TaskGen import extension
-import Logs
+#! /usr/bin/env python
+
+import Logs, Options
+from waf_dynamo import platform_supports_feature
 
 def configure(conf):
     if 'physics' in Options.options.disable_features:
         Logs.info("physics disabled")
         conf.env['STATICLIB_PHYSICS'] = ['physics_null']
     else:
-        conf.env['STATICLIB_PHYSICS'] = ['physics', 'BulletDynamics', 'BulletCollision', 'LinearMath', 'Box2D']
+
+        box2d = platform_supports_feature(platform, 'box2d', {})
+        bullet3d = platform_supports_feature(platform, 'bullet3d', {})
+
+        if bullet3d and box2d:
+            conf.env['STATICLIB_PHYSICS'] = ['physics', 'BulletDynamics', 'BulletCollision', 'LinearMath', 'Box2D']
+        elif box2d:
+            conf.env['STATICLIB_PHYSICS'] = ['physics_2d', 'Box2D']
+        else:
+            conf.env['STATICLIB_PHYSICS'] = ['physics_3d', 'BulletDynamics', 'BulletCollision', 'LinearMath']
