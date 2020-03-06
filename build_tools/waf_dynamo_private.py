c8f67439585c5ec05b1c32768afd10456dd6b51a
JCash <mwesterdahl76@gmail.com>
2020-03-06 14:46:51 +0100

    Added timeout to unit tests

--- build_tools/waf_dynamo_private.py
+++ build_tools/waf_dynamo_private.py
@@ -71,7 +71,7 @@ def setup_tools_nx(conf, build_util):
     conf.env['MAKEMETA']        = '%s/MakeMeta/MakeMeta.exe' % commandline_folder
     conf.env['MAKENSO']         = '%s/MakeNso/MakeNso.exe' % commandline_folder
 
-    conf.env['TEST_LAUNCH_PATTERN'] = 'RunOnTarget.exe --pattern-failure-exit "tests FAILED" --working-directory . %s %s' # program + args
+    conf.env['TEST_LAUNCH_PATTERN'] = 'RunOnTarget.exe --pattern-failure-exit "tests FAILED" --failure-timeout 30 --working-directory . %s %s' # program + args
 
 def get_sdk_root():
     return os.environ['NINTENDO_SDK_ROOT']
