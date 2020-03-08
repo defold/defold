d6feb9dba7f0ebdbef957dc7cb53dcc8dbc78a53
Björn Ritzl <bjorn.ritzl@gmail.com>
2020-03-08 22:14:50 +0100

    Update profiler_cocoa.mm

--- engine/profiler/src/profiler_cocoa.mm
+++ engine/profiler/src/profiler_cocoa.mm
@@ -10,7 +10,10 @@ uint64_t dmProfilerExt::GetMemoryUsage()
     struct task_basic_info t_info;
     mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
 
-    task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)&t_info, &t_info_count);
+    if (KERN_SUCCESS != task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)&t_info, &t_info_count))
+    {
+        return 0;
+    }
     return t_info.resident_size;
 }
 
