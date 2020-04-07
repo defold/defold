78b3828ba8ea63db74bf81f78b0ef5fe2eb63416
Alexey Gulev <me@agulev.com>
2020-04-07 19:42:48 +0200

    Issue-3185 Fixed game crash when running html5.run()(DEF-3869)
    * Fixed game crash when running html5.run()
    
    * Changed naming
    
    * Changed naming

--- engine/script/src/script_html5_js.cpp
+++ engine/script/src/script_html5_js.cpp
@@ -25,6 +25,12 @@ namespace dmScript
      * @namespace html5
      */
 
+    static bool is_operation_successful;
+    extern "C" void EMSCRIPTEN_KEEPALIVE dmScript_html5_report_operation_success(int result)
+    {
+        is_operation_successful = (bool)result;
+    }
+
     /*# run JavaScript code, in the browser, from Lua
      *
      * Executes the supplied string as JavaScript inside the browser.
@@ -46,8 +52,34 @@ namespace dmScript
     int Html5_Run(lua_State* L)
     {
         const char* code = luaL_checkstring(L, 1);
-        char *ret = emscripten_run_script_string(code);
-        lua_pushstring(L, ret);
+
+        char* result = (char*)EM_ASM_INT({
+            var jsResult;
+            var is_success = 1;
+            try 
+            {
+                jsResult = eval(UTF8ToString($0));
+            }
+            catch (err)
+            {
+                is_success = 0;
+                jsResult = err;
+            }
+            _dmScript_html5_report_operation_success(is_success);
+            jsResult += '';
+            var lengthBytes = lengthBytesUTF8(jsResult) + 1; 
+            var stringOnWasmHeap = _malloc(lengthBytes);
+            stringToUTF8(jsResult, stringOnWasmHeap, lengthBytes);
+            return stringOnWasmHeap;
+        }, code);
+        if (!is_operation_successful)
+        {
+            luaL_error(L, result);
+            free(result);
+            return 0;
+        }
+        lua_pushstring(L, result);
+        free(result);
         return 1;
     }
 
