1c0ca88075437aa69016e23c6452615058a6ec6e
JCash <mwesterdahl76@gmail.com>
2020-04-23 18:28:03 +0200

    Compile fix

--- engine/graphics/src/graphics_native.h
+++ engine/graphics/src/graphics_native.h
@@ -5,29 +5,6 @@
 
 namespace dmGraphics
 {
-	bool NativeInit(const struct ContextParams& params);
-	void NativeExit();
-    
-    void NativeBeginFrame(HContext context);
-
-    void SwapBuffers();
-
-    void SetSwapInterval(HContext context, uint32_t swap_interval);
-
-    WindowResult VulkanOpenWindow(HContext context, WindowParams* params);
-    void VulkanCloseWindow(HContext context);
-    uint32_t VulkanGetDisplayDpi(HContext context);
-    uint32_t VulkanGetWidth(HContext context);
-    uint32_t VulkanGetHeight(HContext context);
-    uint32_t VulkanGetWindowWidth(HContext context);
-    uint32_t VulkanGetWindowHeight(HContext context);
-    uint32_t VulkanGetWindowRefreshRate(HContext context);
-    void VulkanSetWindowSize(HContext context, uint32_t width, uint32_t height);
-    void VulkanResizeWindow(HContext context, uint32_t width, uint32_t height);
-    void VulkanSetWindowSize(HContext context, uint32_t width, uint32_t height);
-    void VulkanGetNativeWindowSize(uint32_t* width, uint32_t* height);
-    void VulkanIconifyWindow(HContext context);
-    uint32_t VulkanGetWindowState(HContext context, WindowState state);
 }
 
 #endif // DM_GRAPHICS_NATIVE_H
