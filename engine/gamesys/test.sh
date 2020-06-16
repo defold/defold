8ee64c45721aacacb30e15f9ee4fc303a9d635f7
Pavel Zavoiskikh <hapass@ya.ru>
2020-06-16 15:56:09 +0300

    #3181. Remove test.sh and create rawc copy task instead.

--- engine/gamesys/test.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p build/default/src/gamesys/test/texture
-yes | cp -rf src/gamesys/test/texture/valid_png.png build/default/src/gamesys/test/texture/valid_png.png
-waf --skip-tests && ./build/default/src/gamesys/test/test_gamesys
