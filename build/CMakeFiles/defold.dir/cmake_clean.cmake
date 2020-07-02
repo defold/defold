400694e9ede4c333092d416d5a40ee3681bb8b55
Nguyen The Trung <thetrung@TheTrung.local>
2020-07-02 11:24:23 +0700

    improve intellisense support in vscode

---
+++ build/CMakeFiles/defold.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/defold.dir/defold.cpp.o"
+  "libdefold.a"
+  "libdefold.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/defold.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
