dc3e82941b6969b030b72cd8e24a1946cec55892
Nguyen The Trung <thetrung@TheTrung.local>
2020-07-23 12:00:51 +0700

    add 'sh cmd.sh --copy' to copy dmengine to editor

--- cmd.sh
+++ cmd.sh
@@ -19,9 +19,8 @@ EDITOR="(cd editor/;lein init;lein run)"
 FORCE="sudo chmod -R 777 ./"
 BUILD_MODULE="./scripts/submodule.sh x86_64-darwin $2 $3"
 
-CP_DME_1="cp ./tmp/dynamo_home/bin/x86_64-darwin/dmengine ./editor/tmp/unpack/x86_64-darwin/bin/dmengine"
-CP_DME_2="cp ./tmp/dynamo_home/bin/x86_64-darwin/dmengine_release ./editor/tmp/unpack/x86_64-darwin/bin/dmengine_release"
-CP_DME_3="cp ./tmp/dynamo_home/bin/x86_64-darwin/dmengine_headless ./editor/tmp/unpack/x86_64-darwin/bin/dmengine_headless"
+ENGINE_PATH="./tmp/dynamo_home/bin/x86_64-darwin/*"
+EDITOR_PATH="./editor/tmp/unpack/x86_64-darwin/bin/"
 
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
@@ -70,6 +69,14 @@ while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
     eval $EDITOR
     exit
     ;;
+  -cp| --copy )
+    echo "--------------------------------------------------"
+    echo "COPY TO EDITOR ..."
+    cp ${ENGINE_PATH} ${EDITOR_PATH}
+    echo "copied \nfrom ${GREEN}${ENGINE_PATH}${NC} \nto ${GREEN}${EDITOR_PATH}${NC}"
+    echo "--------------------------------------------------"
+    exit
+    ;;
   -e | --engine )
     eval $BUILD_ENGINE
     exit
@@ -99,6 +106,7 @@ while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
     echo ""
     echo "__________________[COMMAND]___________________"
     echo "sh cmd.sh --setup | -s : for environment setup"
+    echo "sh cmd.sh --copy  | -cp: to copy dmengine -> editor"
     echo "sh cmd.sh --engine| -e : for building engine alone"
     echo "sh cmd.sh --editor| -e : for building editor + launch"
     echo "sh cmd.sh --misc  | -m : for building bob + builtin"
