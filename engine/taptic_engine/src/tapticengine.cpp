5aeaba0f7ec9bae2a57cc275730fb5efa3eaeef3
Nguyen The Trung <thetrung@TheTrung.local>
2020-07-28 12:35:22 +0700

    add doc

--- engine/taptic_engine/src/tapticengine.cpp
+++ engine/taptic_engine/src/tapticengine.cpp
@@ -5,28 +5,56 @@
 namespace dmTapticEngine
 {
 
-
 /*# TapticEngine
- * @namespace taptic_engine
  *
+ * Original from : 
+ * https://github.com/MaratGilyazov/def_taptic_engine
+ * 
+ * Integrated by dotGears / TrungB
+ * We use this as native module, instead of native extension.
+ * Which save time for not building Docker for Extender.
+ * This send impact to device that support TapticEngine (iOS).
+ * 
+ * @document
+ * @name Taptic Engine
+ * @namespace taptic_engine
  */
 
-/*# Collision object physics API documentation
-     *
-     * Functions and messages for collision object physics interaction
-     * with other objects (collisions and ray-casting) and control of
-     * physical behaviors.
-     *
-     * @document
-     * @name Collision object
-     * @namespace physics
-     */
+/*# 
+ *
+ * @name taptic_engine.IMPACT_LIGHT
+ * @variable
+ */
 
-/*# Physics.COPY_POSITION_X
-     *
-     * @name physics.COPY_POSITION_X
-     * @variable
-     */
+/*# 
+ *
+ * @name taptic_engine.IMPACT_MEDIUM
+ * @variable
+ */
+
+/*# 
+ *
+ * @name taptic_engine.IMPACT_HEAVY
+ * @variable
+ */
+
+/*# 
+ *
+ * @name taptic_engine.NOTIFICATION_SUCCESS
+ * @variable
+ */
+
+/*# 
+ *
+ * @name taptic_engine.NOTIFICATION_WARNING
+ * @variable
+ */
+
+/*# 
+ *
+ * @name taptic_engine.NOTIFICATION_ERROR
+ * @variable
+ */
 
 #if defined(DM_PLATFORM_IOS)
 #include "tapticengine.h"
