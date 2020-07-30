e8b83ec9ad4d5cc0054baadc7128b783b7a1df66
Trung Nguyen <thetrung.research@gmail.com>
2020-07-30 21:15:06 +0700

    improve speed for build.py

--- scripts/build.py
+++ scripts/build.py
@@ -59,53 +59,15 @@ DMSDK_PACKAGES_ALL="vectormathlibrary-r1649".split()
 CDN_PACKAGES_URL=os.environ.get("DM_PACKAGES_URL", None)
 CDN_UPLOAD_URL="s3://d.defold.com/archive"
 
+env = dict(os.environ)
+PACKAGES_IOS_SDK           = env['PACKAGES_IOS_SDK'] if env.__contains__('PACKAGES_IOS_SDK') else ''
+PACKAGES_XCODE_TOOLCHAIN   = env['PACKAGES_XCODE_TOOLCHAIN'] if env.__contains__('PACKAGES_XCODE_TOOLCHAIN') else ''
+PACKAGES_IOS_SIMULATOR_SDK = env['PACKAGES_IOS_SIMULATOR_SDK'] if env.__contains__('PACKAGES_IOS_SIMULATOR_SDK')  else ''
 
 PACKAGES_MACOS_SDK="MacOSX10.15.sdk"
 
 ## Let's fixed the local SDK into this folder:
 PACKAGES_FOLDER = "./local_sdks"
-
-def get_xcode_filename():
-    prefix = "XcodeDefault"
-    extension = ".xctoolchain.tar.gz"
-
-    files = []
-    # r=root, d=directories, f = files
-    for r, d, f in os.walk(PACKAGES_FOLDER):
-        for file in f:
-            if prefix and extension in file:
-                # print(file)
-                files.append(file)
-
-    if len(files) > 0:
-        xcode_package = files[0].replace('.tar.gz','')
-        print("[build.py] -- Found " + xcode_package)
-        return xcode_package
-    else:
-        return "XcodeDefault11.5.xctoolchain"
-
-
-PACKAGES_XCODE_TOOLCHAIN = get_xcode_filename()
-
-
-def get_ios_version(prefix, extension, backup):
-    files = []
-    # r=root, d=directories, f = files
-    for r, d, f in os.walk("./local_sdks"):
-        for file in f:
-            if prefix in file:
-                print(file)
-                files.append(file)
-
-    if len(files) > 0:
-        ret = files[0].replace(extension, '')
-        print("[waf_dynamo.py] -- Found iOS " + ret)
-        return ret
-    else:
-        return backup
-
-PACKAGES_IOS_SDK=get_ios_version("iPhoneOS", ".tar.gz", "iPhoneOS13.5.sdk")
-PACKAGES_IOS_SIMULATOR_SDK=get_ios_version("iPhoneSimulator", ".tar.gz", "iPhoneSimulator13.5.sdk")
 
 WINDOWS_SDK_10_VERSION="10.0.18362.0"
 WINDOWS_MSVC_2019_VERSION="14.25.28610"
@@ -1852,6 +1814,22 @@ class Configuration(object):
             f()
         self.futures = []
 
+    def get_sdk_version(self, prefix, extension, backup):
+        files = []
+        # r=root, d=directories, f = files
+        for r, d, f in os.walk(PACKAGES_FOLDER):
+            for file in f:
+                if prefix in file:
+                    # print(file)
+                    files.append(file)
+
+        if len(files) > 0:
+            ret = files[0].replace(extension, '')
+            print("[build.py] -- Found SDK" + ret)
+            return ret
+        else:
+            return backup
+
     def _form_env(self):
         env = dict(os.environ)
 
@@ -1866,7 +1844,18 @@ class Configuration(object):
             ld_library_paths.append('%s/ext/SDKs/linux/%s/tapi1.4/lib' % (self.dynamo_home, PACKAGES_LINUX_CLANG))
 
         env[ld_library_path] = os.path.pathsep.join(ld_library_paths)
-
+        #
+        # Auto detect iOS/XCode version part
+        #
+        if PACKAGES_IOS_SDK == '':
+            env['PACKAGES_IOS_SDK']= self.get_sdk_version("iPhoneOS", ".tar.gz", "iPhoneOS13.5.sdk")
+        
+        if PACKAGES_IOS_SIMULATOR_SDK == '':
+            env['PACKAGES_IOS_SIMULATOR_SDK']= self.get_sdk_version("iPhoneSimulator", ".tar.gz", "iPhoneSimulator13.5.sdk")
+        
+        if PACKAGES_XCODE_TOOLCHAIN == '':
+            env['PACKAGES_XCODE_TOOLCHAIN'] = self.get_sdk_version("XcodeDefault", ".tar.gz", "XcodeDefault11.5.xctoolchain")
+        
         env['PYTHONPATH'] = os.path.pathsep.join(['%s/lib/python' % self.dynamo_home,
                                                   '%s/build_tools' % self.defold,
                                                   '%s/ext/lib/python' % self.dynamo_home])
