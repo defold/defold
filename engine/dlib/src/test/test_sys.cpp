1cad039b7089c9fc1afb5d0134040f0348f1646c
JCash <mwesterdahl76@gmail.com>
2020-08-03 08:20:42 +0200

    test fix

--- engine/dlib/src/test/test_sys.cpp
+++ engine/dlib/src/test/test_sys.cpp
@@ -1,10 +1,10 @@
 // Copyright 2020 The Defold Foundation
 // Licensed under the Defold License version 1.0 (the "License"); you may not use
 // this file except in compliance with the License.
-// 
+//
 // You may obtain a copy of the License, together with FAQs at
 // https://www.defold.com/license
-// 
+//
 // Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -138,13 +138,15 @@ TEST(dmSys, EngineInfo)
     dmSys::EngineInfoParam engine_info;
     engine_info.m_Version = "1.2.1";
     engine_info.m_VersionSHA1 = "hellothere";
+    engine_info.m_Platform = "testplatform";
     engine_info.m_IsDebug = true;
     dmSys::SetEngineInfo(engine_info);
     dmSys::EngineInfo info;
     dmSys::GetEngineInfo(&info);
-    ASSERT_STREQ(engine_info.m_Version, info.m_Version);
-    ASSERT_STREQ(engine_info.m_VersionSHA1, info.m_VersionSHA1);
-    ASSERT_EQ(engine_info.m_IsDebug, info.m_IsDebug);
+    ASSERT_STREQ("1.2.1", info.m_Version);
+    ASSERT_STREQ("hellothere", info.m_VersionSHA1);
+    ASSERT_STREQ("testplatform", info.m_Platform);
+    ASSERT_TRUE(info.m_IsDebug);
 }
 
 TEST(dmSys, LoadResource)
