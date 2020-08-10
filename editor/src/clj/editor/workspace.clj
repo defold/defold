87396c75b2972c1a6ceb1aad60012d99278cfc7a
JCash <mwesterdahl76@gmail.com>
2020-08-10 11:15:53 +0200

    Issue 5040: Custom resources can now be without suffix

--- editor/src/clj/editor/workspace.clj
+++ editor/src/clj/editor/workspace.clj
@@ -1,10 +1,10 @@
 ;; Copyright 2020 The Defold Foundation
 ;; Licensed under the Defold License version 1.0 (the "License"); you may not use
 ;; this file except in compliance with the License.
-;; 
+;;
 ;; You may obtain a copy of the License, together with FAQs at
 ;; https://www.defold.com/license
-;; 
+;;
 ;; Unless required by applicable law or agreed to in writing, software distributed
 ;; under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 ;; CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -53,10 +53,11 @@ ordinary paths."
   (source-type [this] (resource/source-type resource))
   (read-only? [this] false)
   (path [this] (let [ext (resource/ext this)
+                     ext (if (not-empty ext) (str "." ext) "")
                      suffix (format "%x" (resource/resource-hash this))]
                  (if-let [path (resource/path resource)]
-                   (str (FilenameUtils/removeExtension path) "." ext)
-                   (str prefix "_generated_" suffix "." ext))))
+                   (str (FilenameUtils/removeExtension path) ext)
+                   (str prefix "_generated_" suffix ext))))
   (abs-path [this] (.getAbsolutePath (io/file (build-path (resource/workspace this)) (resource/path this))))
   (proj-path [this] (str "/" (resource/path this)))
   (resource-name [this] (resource/resource-name resource))
