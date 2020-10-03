62bfa6625be2a7081e1c45e61a9aaa7562303dbf
Björn Ritzl <bjorn.ritzl@gmail.com>
2020-10-03 15:46:02 +0200

    Patch the bundled JDK to include jarsigner (#5257)
    * Accept both jks and keystore files when validating input fields
    
    * Prep for jdk update to include jarsigner
    
    * Updated path to editor jdk when codesigning macOS version

--- editor/src/clj/editor/updater.clj
+++ editor/src/clj/editor/updater.clj
@@ -1,10 +1,10 @@
 ;; Copyright 2020 The Defold Foundation
 ;; Licensed under the Defold License version 1.0 (the "License"); you may not use
 ;; this file except in compliance with the License.
-;; 
+;;
 ;; You may obtain a copy of the License, together with FAQs at
 ;; https://www.defold.com/license
-;; 
+;;
 ;; Unless required by applicable law or agreed to in writing, software distributed
 ;; under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 ;; CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -329,7 +329,7 @@
                           (or "")
                           io/file
                           .getCanonicalFile)
-        protected-dirs [(io/file resources-dir "packages" "jdk11.0.1")]
+        protected-dirs [(io/file resources-dir "packages" "jdk11.0.1-p1")]
         install-dir (.getCanonicalFile
                       (if-let [path (system/defold-resourcespath)]
                         (case os
