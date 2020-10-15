7a81bec1a1061cd2eddc5c894d2ac9650d5e467b
Björn Ritzl <bjorn.ritzl@gmail.com>
2020-10-15 13:35:23 +0200

    Destroy callback early if we failed to invoke it

--- engine/gamesys/src/gamesys/scripts/script_particlefx.cpp
+++ engine/gamesys/src/gamesys/scripts/script_particlefx.cpp
@@ -1,10 +1,10 @@
 // Copyright 2020 The Defold Foundation
 // Licensed under the Defold License version 1.0 (the "License"); you may not use
 // this file except in compliance with the License.
-// 
+//
 // You may obtain a copy of the License, together with FAQs at
 // https://www.defold.com/license
-// 
+//
 // Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -106,13 +106,15 @@ namespace dmGameSystem
 
             EmitterStateChangedCallbackArgs args(data.m_ComponentId, emitter_id, emitter_state);
 
-            if (!dmScript::InvokeCallback(data.m_CallbackInfo, EmitterStateChangedCallbackArgsCB, &args))
+            int ok = dmScript::InvokeCallback(data.m_CallbackInfo, EmitterStateChangedCallbackArgsCB, &args);
+            if (!ok)
             {
-                dmLogError("Could not run particlefx callback because the instance has been deleted.");
+                dmLogWarning("Could not run particlefx callback because the instance has been deleted.");
             }
 
-            // The last emitter belonging to this particlefx har gone to sleep, release lua reference.
-            if(num_awake_emitters == 0 && emitter_state == dmParticle::EMITTER_STATE_SLEEPING)
+            // Release the lua ref to the callback if we failed to invoke callback or if the last emitter
+            // belonging to this particlefx har gone to sleep.
+            if(!ok || (num_awake_emitters == 0 && emitter_state == dmParticle::EMITTER_STATE_SLEEPING))
             {
                 dmScript::DestroyCallback(data.m_CallbackInfo);
                 data.m_CallbackInfo = 0x0;
