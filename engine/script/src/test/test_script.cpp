cb49fd0fadd55ccddc5387ef23f8950943454dd7
JCash <mwesterdahl76@gmail.com>
2020-11-13 15:16:38 +0100

    Issue 5334: Added file and line number to DM_LUA_STACKCHECK errors

--- engine/script/src/test/test_script.cpp
+++ engine/script/src/test/test_script.cpp
@@ -1,10 +1,10 @@
 // Copyright 2020 The Defold Foundation
 // Licensed under the Defold License version 1.0 (the "License"); you may not use
 // this file except in compliance with the License.
-// 
+//
 // You may obtain a copy of the License, together with FAQs at
 // https://www.defold.com/license
-// 
+//
 // Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -445,6 +445,24 @@ TEST_F(ScriptTest, TestStackCheck) {
         lua_pushnumber(L, 0);
     }
     lua_pop(L, 1);
+}
+
+static void TestStackCheckAssert(lua_State* L, int expected, int num_stack_vars) {
+    DM_LUA_STACK_CHECK(L, expected);
+
+    for (int i = 0; i < num_stack_vars; ++i)
+    {
+        lua_pushnumber(L, i);
+    }
+}
+
+TEST_F(ScriptTest, TestStackCheckAssert) {
+    for (int i = 0; i < 3; ++i )
+    {
+        int num_stack_vars = i+1;
+        ASSERT_DEATH(TestStackCheckAssert(L, i, i+1),"");
+        lua_pop(L, num_stack_vars);
+    }
 }
 
 static int TestStackCheckErrorFunc(lua_State* L) {
