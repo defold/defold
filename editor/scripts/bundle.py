4ed30ff00bb58ebfbc18e1218d1bef4c8677bdb8
Marius Petcu <dapetcu21@gmail.com>
2020-11-15 13:19:44 +0200

    Update document icon for Big Sur

--- editor/scripts/bundle.py
+++ editor/scripts/bundle.py
@@ -308,6 +308,7 @@ def create_bundle(options):
 
         if is_mac:
             shutil.copy('bundle-resources/Info.plist', '%s/Contents' % bundle_dir)
+            shutil.copy('bundle-resources/Assets.car', resources_dir)
         if icon:
             shutil.copy('bundle-resources/%s' % icon, resources_dir)
 
