4f02602ec37a72e5056f9d664d15a7c165d0eeeb
JCash <mwesterdahl76@gmail.com>
2020-11-22 12:02:58 +0100

    Added dmHashReverse64 to dmSDK

--- engine/dlib/src/dlib/hash.cpp
+++ engine/dlib/src/dlib/hash.cpp
@@ -1,10 +1,10 @@
 // Copyright 2020 The Defold Foundation
 // Licensed under the Defold License version 1.0 (the "License"); you may not use
 // this file except in compliance with the License.
-// 
+//
 // You may obtain a copy of the License, together with FAQs at
 // https://www.defold.com/license
-// 
+//
 // Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -295,7 +295,7 @@ uint64_t dmHashBufferNoReverse64(const void * key, uint32_t len)
     return h;
 }
 
-uint64_t dmHashBuffer64(const void * key, uint32_t len)
+dmhash_t dmHashBuffer64(const void * key, uint32_t len)
 {
     uint64_t h = dmHashBufferNoReverse64(key, len);
 
@@ -324,7 +324,7 @@ uint32_t DM_DLLEXPORT dmHashString32(const char* string)
     return dmHashBuffer32(string, strlen(string));
 }
 
-uint64_t DM_DLLEXPORT dmHashString64(const char* string)
+dmhash_t DM_DLLEXPORT dmHashString64(const char* string)
 {
     return dmHashBuffer64(string, strlen(string));
 }
