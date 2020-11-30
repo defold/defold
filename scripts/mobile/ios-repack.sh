5eb5795360cc620c46b0311080898465e1eb6601
Marco Nett <mail@marco-nett.de>
2020-11-30 17:22:30 +0100

    ios-repack.sh: allow for app names with spaces

--- scripts/mobile/ios-repack.sh
+++ scripts/mobile/ios-repack.sh
@@ -2,10 +2,10 @@
 # Copyright 2020 The Defold Foundation
 # Licensed under the Defold License version 1.0 (the "License"); you may not use
 # this file except in compliance with the License.
-# 
+#
 # You may obtain a copy of the License, together with FAQs at
 # https://www.defold.com/license
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed
 # under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -109,7 +109,7 @@ TARGET="$(cd "$(dirname "${SOURCE}")"; pwd)/${APPLICATION}.repack"
     "${SECURITY}" cms -D -i "${PROFILE}" > "${PROVISION}"
     "${PLISTBUDDY}" -x -c 'Print :Entitlements' "${PROVISION}" > "${ENTITLEMENT}"
 
-    EXENAME=$(defaults read ${BUILD}/Payload/${APPLICATION}.app/Info.plist CFBundleExecutable)
+    EXENAME=$(defaults read ${BUILD}/Payload/"${APPLICATION}".app/Info.plist CFBundleExecutable)
 
     cp -v "${ENGINE}" "Payload/${APPLICATION}.app/${EXENAME}"
     chmod +x "Payload/${APPLICATION}.app/${EXENAME}"
