168a6387b2841295889864a4f396597a685bfc97
Björn Ritzl <bjorn.ritzl@gmail.com>
2020-12-16 00:13:55 +0100

    Review fixes

--- editor/src/clj/editor/gui.clj
+++ editor/src/clj/editor/gui.clj
@@ -1,10 +1,10 @@
 ;; Copyright 2020 The Defold Foundation
 ;; Licensed under the Defold License version 1.0 (the "License"); you may not use
 ;; this file except in compliance with the License.
-;; 
+;;
 ;; You may obtain a copy of the License, together with FAQs at
 ;; https://www.defold.com/license
-;; 
+;;
 ;; Unless required by applicable law or agreed to in writing, software distributed
 ;; under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 ;; CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -2981,7 +2981,7 @@
                            (selection->layer-node selection)))
   (enabled? [selection] (let [selected-node-id (g/override-root (handler/selection->node-id selection))
                               node-child-index (g/node-value selected-node-id :child-index)]
-                          (> node-child-index 0)))
+                          (pos? node-child-index)))
   (run [selection] (let [selected (g/override-root (handler/selection->node-id selection))]
                      (move-child-node! selected -1))))
 
@@ -2992,7 +2992,7 @@
                               parent (core/scope selected-node-id)
                               node-child-index (g/node-value selected-node-id :child-index)
                               child-indices (g/node-value parent :child-indices)]
-                          (< node-child-index (- (.size child-indices) 1))))
+                          (< node-child-index (dec (.size child-indices)))))
   (run [selection] (let [selected (g/override-root (handler/selection->node-id selection))]
                      (move-child-node! selected 1))))
 
