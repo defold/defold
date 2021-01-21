035021044fcbd72c2cac5dbc4b7e3d7216c7cc10
Björn Ritzl <bjorn.ritzl@gmail.com>
2021-01-21 11:30:22 +0100

    Always use an architecture matching the platform on Windows (#5489)

--- com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/PlatformArchitectures.java
+++ com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/PlatformArchitectures.java
@@ -1,10 +1,10 @@
 // Copyright 2020 The Defold Foundation
 // Licensed under the Defold License version 1.0 (the "License"); you may not use
 // this file except in compliance with the License.
-// 
+//
 // You may obtain a copy of the License, together with FAQs at
 // https://www.defold.com/license
-// 
+//
 // Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -14,7 +14,8 @@ package com.dynamo.bob;
 
 public enum PlatformArchitectures {
     OSX(new String[] {"x86_64-darwin"}, new String[] {"x86_64-darwin"}),
-    Windows(new String[] {"x86_64-win32", "x86-win32"}, new String[] {"x86_64-win32", "x86-win32"}),
+    Windows32(new String[] {"x86-win32"}, new String[] {"x86-win32"}),
+    Windows64(new String[] {"x86_64-win32"}, new String[] {"x86_64-win32"}),
     Linux(new String[] {"x86_64-linux"}, new String[] {"x86_64-linux"}),
     iOS(new String[] {"arm64-darwin", "armv7-darwin", "x86_64-ios"}, new String[] {"arm64-darwin", "armv7-darwin"}),
     Android(new String[] {"arm64-android", "armv7-android"}, new String[] {"armv7-android","arm64-android"}),
