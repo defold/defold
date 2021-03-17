41e1405046991c7059a47e44f8ae4fc2775b11d5
alcorrius <dotsenko.vitaliy@gmail.com>
2021-03-17 16:27:37 +0200

    Issue 5515: Rewrote dialog from make-confirmation-dialog to make-info-dialog

--- editor/src/clj/editor/asset_browser.clj
+++ editor/src/clj/editor/asset_browser.clj
@@ -426,19 +426,14 @@
                            {:text "Delete"
                             :variant :danger
                             :result true}]})
-              (dialogs/make-confirmation-dialog
+              (dialogs/make-info-dialog
                 {:title "Delete Files?"
                  :icon :icon/circle-question
                  :header "Are you sure you want to delete these files?"
-                 :content {:fx/type :v-box
-                           :style-class "dialog-content-padding"
-                           :children [{:fx/type fxui/label
-                                       :text "You are about to delete:"}
-                                      {:fx/type fxui/text-area
-                                       :text (->> selection
+                 :content {:text (str "You are about to delete:\n" (->> selection
                                                     (map #(str "\u00A0\u00A0\u2022\u00A0"
                                                               (resource/resource-name %)))
-                                                    (string/join "\n"))}]}
+                                                    (string/join "\n")))}
                  :buttons [{:text "Cancel"
                             :cancel-button true
                             :default-button true
