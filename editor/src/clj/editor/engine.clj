907e25c907050f7907b747f48a107f7848688825
Mathias Westerdahl <mwesterdahl76@gmail.com>
2021-03-22 08:24:22 +0100

    Issue 5627: Use OpenGL 3.1+ on desktop (#5628)
    * Fix for texture2D in vertex programs
    
    * Issue 5627: Use OpenGL 3.1+ on desktop
    
    * Use OpenGL 3.1+ on windows too
    
    * Removed use of core profile for desktop
    
    * Removed the MESA_GL_VERSION_OVERRIDE when launching the engine
    
    * Added removeenv argument to process start function
    
    * Added debug print for the process builder
    
    * Added define to disable gl3 spam on macOS

--- editor/src/clj/editor/engine.clj
+++ editor/src/clj/editor/engine.clj
@@ -1,10 +1,10 @@
 ;; Copyright 2020 The Defold Foundation
 ;; Licensed under the Defold License version 1.0 (the "License"); you may not use
 ;; this file except in compliance with the License.
-;; 
+;;
 ;; You may obtain a copy of the License, together with FAQs at
 ;; https://www.defold.com/license
-;; 
+;;
 ;; Unless required by applicable law or agreed to in writing, software distributed
 ;; under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 ;; CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -262,9 +262,12 @@
         env {"DM_SERVICE_PORT" "dynamic"
              "DM_QUIT_ON_ESC" (if (prefs/get-prefs prefs "general-quit-on-esc" false)
                                 "1" "0")}
+        removeenv ["MESA_GL_VERSION_OVERRIDE" "MESA_LOADER_DRIVER_OVERRIDE"]
+
         opts {:directory project-directory
               :redirect-error-stream? true
-              :env env}]
+              :env env
+              :removeenv removeenv}]
     ;; Closing "is" seems to cause any dmengine output to stdout/err
     ;; to generate SIGPIPE and close/crash. Also we need to read
     ;; the output of dmengine because there is a risk of the stream
