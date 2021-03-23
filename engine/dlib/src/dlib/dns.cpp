0ad4a8f72a1cd8c4a5ddee815b79f6a2d67ecdf9
Björn Ritzl <bjorn.ritzl@gmail.com>
2021-03-23 08:20:25 +0100

    Unused include

--- engine/dlib/src/dlib/dns.cpp
+++ engine/dlib/src/dlib/dns.cpp
@@ -10,7 +10,6 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-#include "log.h"
 #include "dns.h"
 #include "dns_private.h"
 #include "socket.h"
