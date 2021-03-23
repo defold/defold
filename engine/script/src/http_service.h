60d72313ae516e1a6aaae76154ad28b6f6dfc3bb
Björn Ritzl <bjorn.ritzl@gmail.com>
2021-03-23 09:38:53 +0100

    Uninitialized pointer

--- engine/script/src/http_service.h
+++ engine/script/src/http_service.h
@@ -21,13 +21,14 @@ namespace dmHttpService
 
     struct Params
     {
-    	Params() :
-    		m_ThreadCount(4),
-            m_UseHttpCache(1)
+        Params() :
+            m_ThreadCount(4),
+            m_UseHttpCache(1),
+            m_DNSServers(0)
     	{}
     	uint32_t m_ThreadCount:4;
         uint32_t m_UseHttpCache:1;
-        const char* m_DNSServers;
+        const char* m_DNSServers:0;
     };
     HHttpService New(const Params* params);
     dmMessage::HSocket GetSocket(HHttpService http_service);
