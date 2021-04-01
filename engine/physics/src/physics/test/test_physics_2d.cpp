5bc2af261259fcd6b85a075d3bd2ce96618c3fa1
Artsiom Trubchyk <aglitchman@gmail.com>
2021-04-01 10:29:20 +0300

    Implement the Weld joint for 2D physics (#5694)

--- engine/physics/src/physics/test/test_physics_2d.cpp
+++ engine/physics/src/physics/test/test_physics_2d.cpp
@@ -1102,6 +1102,27 @@ TYPED_TEST(PhysicsTest, JointGeneral)
     DeleteJoint2D(TestFixture::m_World, joint);
     joint = 0x0;
 
+    //////////////////////////////////////////////////////////////
+    // Create WELD joint
+    joint_type = dmPhysics::JOINT_TYPE_WELD;
+    joint_params = dmPhysics::ConnectJointParams(joint_type);
+    joint = dmPhysics::CreateJoint2D(TestFixture::m_World, static_co, p_zero, dynamic_co, p_zero, joint_type, joint_params);
+    ASSERT_NE((dmPhysics::HJoint)0x0, joint);
+
+    // Update WELD joint
+    joint_params.m_WeldJointParams.m_DampingRatio = 1.0f;
+    r = dmPhysics::SetJointParams2D(TestFixture::m_World, joint, joint_type, joint_params);
+    ASSERT_TRUE(r);
+
+    // Get WELD joint params
+    r = dmPhysics::GetJointParams2D(TestFixture::m_World, joint, joint_type, joint_params);
+    ASSERT_TRUE(r);
+    ASSERT_NEAR(1.0f, joint_params.m_WeldJointParams.m_DampingRatio, FLT_EPSILON);
+
+    // Delete WELD joint
+    DeleteJoint2D(TestFixture::m_World, joint);
+    joint = 0x0;
+
 
     (*TestFixture::m_Test.m_DeleteCollisionObjectFunc)(TestFixture::m_World, static_co);
     (*TestFixture::m_Test.m_DeleteCollisionObjectFunc)(TestFixture::m_World, dynamic_co);
@@ -1290,6 +1311,52 @@ TYPED_TEST(PhysicsTest, JointHinge)
     }
 
     // Delete HINGE joint
+    DeleteJoint2D(TestFixture::m_World, joint);
+    joint = 0x0;
+
+    (*TestFixture::m_Test.m_DeleteCollisionObjectFunc)(TestFixture::m_World, static_co);
+    (*TestFixture::m_Test.m_DeleteCollisionObjectFunc)(TestFixture::m_World, dynamic_co);
+    (*TestFixture::m_Test.m_DeleteCollisionShapeFunc)(shape_a);
+    (*TestFixture::m_Test.m_DeleteCollisionShapeFunc)(shape_b);
+
+}
+
+TYPED_TEST(PhysicsTest, JointWeld)
+{
+    VisualObject vo_a;
+    dmPhysics::CollisionObjectData data;
+    vo_a.m_Position.setX(-2.0f);
+    data.m_Type = dmPhysics::COLLISION_OBJECT_TYPE_KINEMATIC;
+    data.m_Mass = 0.0f;
+    data.m_UserData = &vo_a;
+    typename TypeParam::CollisionShapeType shape_a = (*TestFixture::m_Test.m_NewBoxShapeFunc)(TestFixture::m_Context, Vector3(0.5f, 0.5f, 0.0f));
+    typename TypeParam::CollisionObjectType static_co = (*TestFixture::m_Test.m_NewCollisionObjectFunc)(TestFixture::m_World, data, &shape_a, 1u);
+
+    VisualObject vo_b;
+    vo_b.m_Position.setX(2.0f);
+    data.m_Type = dmPhysics::COLLISION_OBJECT_TYPE_DYNAMIC;
+    data.m_Mass = 1.0f;
+    data.m_UserData = &vo_b;
+    typename TypeParam::CollisionShapeType shape_b = (*TestFixture::m_Test.m_NewBoxShapeFunc)(TestFixture::m_Context, Vector3(0.5f, 0.5f, 0.0f));
+    typename TypeParam::CollisionObjectType dynamic_co = (*TestFixture::m_Test.m_NewCollisionObjectFunc)(TestFixture::m_World, data, &shape_b, 1u);
+
+    // Create WELD joint
+    dmPhysics::JointType joint_type = dmPhysics::JOINT_TYPE_WELD;
+    Vectormath::Aos::Point3 p_1(5.0f, 0.0f, 0.0f);
+    Vectormath::Aos::Point3 p_2(-5.0f, 0.0f, 0.0f);
+    dmPhysics::ConnectJointParams joint_params(joint_type);
+    joint_params.m_WeldJointParams.m_ReferenceAngle = 0.314f;
+    dmPhysics::HJoint joint = dmPhysics::CreateJoint2D(TestFixture::m_World, static_co, p_1, dynamic_co, p_2, joint_type, joint_params);
+    ASSERT_NE((dmPhysics::HJoint)0x0, joint);
+
+    for (uint32_t i = 0; i < 40; ++i)
+    {
+        (*TestFixture::m_Test.m_StepWorldFunc)(TestFixture::m_World, TestFixture::m_StepWorldContext);
+        ASSERT_NEAR(-2.0f, vo_a.m_Position.getX(), FLT_EPSILON);
+        ASSERT_NEAR(7.75f, vo_b.m_Position.getX(), 0.01f);
+    }
+
+    // Delete WELD joint
     DeleteJoint2D(TestFixture::m_World, joint);
     joint = 0x0;
 
