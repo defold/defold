029bfa610b716dd9a794530f9190eadf79e7ba43
Mathias Westerdahl <mwesterdahl76@gmail.com>
2021-04-07 14:25:02 +0200

    Issue 5658: Move spine builders to separate jar file (#5662)
    * Moved Spine java source to separate folder
    
    Creates a separate fmt-spine.jar
    
    * Added fmt-spine.jar as is to bob.jar
    
    Loads jar file at runtime
    
    * Removed hardcoded extension-to-class table in GameProjectBuilder
    
    GuiBuilder uses class reported to ProtoBuilder to create the bone nodes
    
    * Moved SpineSceneUtill to extension
    
    * Editor now uses fmt-spine.jar
    
    * review fixes
    
    * Bob test fixes
    
    * review cleanup
    
    * test fixes

--- com.dynamo.cr/com.dynamo.cr.bob.test/src/com/dynamo/bob/pipeline/GuiBuilderTest.java
+++ com.dynamo.cr/com.dynamo.cr.bob.test/src/com/dynamo/bob/pipeline/GuiBuilderTest.java
@@ -1,10 +1,10 @@
 // Copyright 2020 The Defold Foundation
 // Licensed under the Defold License version 1.0 (the "License"); you may not use
 // this file except in compliance with the License.
-// 
+//
 // You may obtain a copy of the License, together with FAQs at
 // https://www.defold.com/license
-// 
+//
 // Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -13,11 +13,13 @@
 package com.dynamo.bob.pipeline;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.io.IOUtils;
 import org.eclipse.core.runtime.IPath;
@@ -28,8 +30,23 @@ import static org.junit.Assert.assertTrue;
 
 import com.dynamo.gui.proto.Gui;
 import com.google.protobuf.Message;
+import com.dynamo.bob.Bob;
+import com.dynamo.bob.BuilderParams;
+import com.dynamo.bob.ProtoParams;
+import com.dynamo.bob.ProtoBuilder;
+import com.dynamo.bob.ClassLoaderScanner;
 
 public class GuiBuilderTest extends AbstractProtoBuilderTest {
+
+    private ClassLoaderScanner scanner = null;
+
+
+    public GuiBuilderTest() throws IOException {
+        Bob.createClassLoaderScanner();
+        this.scanner = Bob.getClassLoaderScanner();
+
+        registerProtoBuilderNames();
+    }
 
     private boolean nodeExists(Gui.SceneDesc scene, String nodeId)
     {
@@ -41,6 +58,37 @@ public class GuiBuilderTest extends AbstractProtoBuilderTest {
         }
 
         return false;
+    }
+
+    private void registerProtoBuilderNames() {
+        Set<String> classNames = this.scanner.scan("com.dynamo.bob.pipeline");
+
+        for (String className : classNames) {
+            // Ignore TexcLibrary to avoid it being loaded and initialized
+            boolean skip = className.startsWith("com.dynamo.bob.TexcLibrary");
+            if (!skip) {
+                try {
+                    Class<?> klass = Class.forName(className, true, this.scanner.getClassLoader());
+                    BuilderParams builderParams = klass.getAnnotation(BuilderParams.class);
+                    if (builderParams != null) {
+                        ProtoParams protoParams = klass.getAnnotation(ProtoParams.class);
+                        if (protoParams != null) {
+                            ProtoBuilder.addMessageClass(builderParams.outExt(), protoParams.messageClass());
+
+                            for (String ext : builderParams.inExts()) {
+                                Class<?> inputKlass = protoParams.srcClass();
+                                if (inputKlass != null) {
+                                    ProtoBuilder.addMessageClass(ext, protoParams.srcClass());
+                                }
+                            }
+                        }
+                    }
+
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 
     @Test
