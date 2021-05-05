c7dc6e6c842b38974fd81d2dbf07c2a88013fb0f
JCash <mwesterdahl76@gmail.com>
2021-05-05 14:09:25 +0200

    Added documentation page to dmGraphics in dmSDK

--- engine/graphics/src/dmsdk/graphics/graphics.h
+++ engine/graphics/src/dmsdk/graphics/graphics.h
@@ -1,10 +1,10 @@
 // Copyright 2020 The Defold Foundation
 // Licensed under the Defold License version 1.0 (the "License"); you may not use
 // this file except in compliance with the License.
-// 
+//
 // You may obtain a copy of the License, together with FAQs at
 // https://www.defold.com/license
-// 
+//
 // Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -13,10 +13,26 @@
 #ifndef DMSDK_GRAPHICS_H
 #define DMSDK_GRAPHICS_H
 
+/*# Graphics API documentation
+ * [file:<dmsdk/graphics/graphics.h>]
+ *
+ * Graphics API
+ *
+ * @document
+ * @name Graphics
+ * @namespace dmGraphics
+ */
+
 namespace dmGraphics
 {
+    /*#
+     * Texture handle
+     * @typedef
+     * @name HTexture
+     */
     typedef struct Texture*      HTexture;
-    typedef struct RenderTarget* HRenderTarget;
+
+    typedef struct RenderTarget* HRenderTarget; // Where is this currently used?
 
     enum HandleResult
     {
