4e13c19a8d0ea769c6ca15a417b8975f9b4291cb
JCash <mwesterdahl76@gmail.com>
2021-05-08 10:28:19 +0200

    Added link to archive format documentation

--- com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/archive/ArchiveBuilder.java
+++ com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/archive/ArchiveBuilder.java
@@ -10,6 +10,9 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
+// Please keep the overview documentation up-to-date:
+// https://github.com/defold/defold/blob/dev/engine/docs/ARCHIVE_FORMAT.md
+
 package com.dynamo.bob.archive;
 
 import java.io.File;
