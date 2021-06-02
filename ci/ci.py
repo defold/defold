fb9290ec2014704e242e2b15419da228583bf841
JCash <mwesterdahl76@gmail.com>
2021-06-02 13:36:45 +0200

    Added ci.py detection for private repos
    Disables certain workflows
    Null build for non supported private engine builds

--- ci/ci.py
+++ ci/ci.py
@@ -19,6 +19,7 @@ import platform
 import os
 import base64
 from argparse import ArgumentParser
+from ci_helper import is_platform_supported, is_repo_private
 
 # The platforms we deploy our editor on
 PLATFORMS_DESKTOP = ('x86_64-linux', 'x86_64-win32', 'x86_64-darwin')
@@ -368,7 +369,20 @@ def get_branch():
         branch = call("git rev-parse HEAD")
     return branch
 
+def is_workflow_enabled_in_repo():
+    if not is_repo_private():
+        return True # all workflows are enabled by deefault
+
+    workflow = os.environ.get('GITHUB_WORKFLOW', '')
+    if workflow in ('CI - Main',):
+        return True
+    return False
+
 def main(argv):
+    if not is_workflow_enabled_in_repo():
+        print("Workflow '{}' is disabled in repo '{}'. Skipping".format(os.environ.get('GITHUB_WORKFLOW', ''), os.environ.get('GITHUB_REPOSITORY', '')))
+        return
+
     parser = ArgumentParser()
     parser.add_argument('commands', nargs="+", help="The command to execute (engine, build-editor, notarize-editor, archive-editor, bob, sdk, install, smoke)")
     parser.add_argument("--platform", dest="platform", help="Platform to build for (when building the engine)")
@@ -395,6 +409,11 @@ def main(argv):
     args = parser.parse_args()
 
     platform = args.platform
+
+    if not is_platform_supported(platform):
+        print("Platform {} is private and current repo cannot build for this platform. Skipping".format(platform))
+        return;
+
     branch = get_branch()
 
     # configure build flags based on the branch
---
+++ ci/ci_helper.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+""" Since github actions are quite difficult to use when writing scriptable things.
+We rely on this script being able to output the unaltered text.
+"""
+
+import os, sys, platform
+
+PLATFORMS_PRIVATE = ('x86_64-ps4', 'arm64-nx64')
+
+DIRNAME=os.path.dirname(os.path.abspath(__file__))
+
+# We use this function to determine if we should skip this platform
+def can_build_private_platform(platform):
+    return False
+
+def repo_name_to_platforms(repository):
+    return None
+
+if os.path.exists(os.path.join(DIRNAME, 'ci_private.py')):
+    import ci_private
+    can_build_private_platform = ci_private.can_build_private_platform
+    repo_name_to_platforms = ci_private.repo_name_to_platforms
+
+def is_platform_private(platform):
+    return platform in PLATFORMS_PRIVATE
+
+def is_platform_supported(platform):
+    if is_platform_private(platform):
+        return can_build_private_platform(platform)
+    return True
+
+def is_platform_supported_by_repo(args):
+    platform=args[0]
+    if not is_platform_private(platform):
+        # By default all public platforms are supported
+        return True
+
+    repository = os.environ.get('GITHUB_REPOSITORY', None)
+    if repository is None:
+        return True # probably a local build
+
+    platforms = repo_name_to_platforms(repository)
+    if platforms is not None and platform in platforms:
+        return True
+
+    return False
+
+def is_repo_private():
+    repository = os.environ.get('GITHUB_REPOSITORY', None)
+    if repository is None:
+        return False # probably a local build
+
+    # get the platforms
+    if platforms is not None:
+        for platform in platforms:
+            if is_platform_private(platform):
+                return True
+    return False
+
+
+def should_build_on_private_repo(args):
+    platform=args[0]
+    if not is_platform_private(platform):
+        # By default all public platforms are supported
+        return False
+    return True
+
+
+def print_values(values):
+    with sys.stdout as f:
+        for i, value in enumerate(values):
+            f.write("%s" % value)
+            if i < len(values)-1:
+                f.write(" ")
+
+if __name__ == '__main__':
+    command = sys.argv[1]
+    if command == 'is_platform_supported_by_repo':
+        result = is_platform_supported_by_repo(sys.argv[2:])
+        print_values([result])
+    elif command == 'should_build_on_private_repo':
+        result = should_build_on_private_repo(sys.argv[2:])
+        print_values([result])
