e7d8518632ae3f283c84a5e366d6ee7490e6cc57
JCash <mwesterdahl76@gmail.com>
2021-06-02 16:22:50 +0200

    Cleanup in ci_helper.py

--- ci/ci_helper.py
+++ ci/ci_helper.py
@@ -29,6 +29,21 @@ def is_platform_supported(platform):
         return can_build_private_platform(platform)
     return True
 
+def is_repo_private():
+    repository = os.environ.get('GITHUB_REPOSITORY', None)
+    if repository is None:
+        return False # probably a local build
+
+    # get the platforms
+    platforms = repo_name_to_platforms(repository)
+    if platforms is not None:
+        for platform in platforms:
+            if is_platform_private(platform):
+                return True
+    return False
+
+# output commands
+
 def is_platform_supported_by_repo(args):
     platform=args[0]
     if not is_platform_private(platform):
@@ -45,28 +60,6 @@ def is_platform_supported_by_repo(args):
 
     return False
 
-def is_repo_private():
-    repository = os.environ.get('GITHUB_REPOSITORY', None)
-    if repository is None:
-        return False # probably a local build
-
-    # get the platforms
-    platforms = repo_name_to_platforms(repository)
-    if platforms is not None:
-        for platform in platforms:
-            if is_platform_private(platform):
-                return True
-    return False
-
-
-def should_build_on_private_repo(args):
-    platform=args[0]
-    if not is_platform_private(platform):
-        # By default all public platforms are supported
-        return False
-    return True
-
-
 def print_values(values):
     with sys.stdout as f:
         for i, value in enumerate(values):
@@ -74,11 +67,9 @@ def print_values(values):
             if i < len(values)-1:
                 f.write(" ")
 
+# For use from the ci.yml
 if __name__ == '__main__':
     command = sys.argv[1]
     if command == 'is_platform_supported_by_repo':
         result = is_platform_supported_by_repo(sys.argv[2:])
-        print_values([result])
-    elif command == 'should_build_on_private_repo':
-        result = should_build_on_private_repo(sys.argv[2:])
         print_values([result])
