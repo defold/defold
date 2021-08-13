b8e694734ee164d0b68b2963f89b384e3efc7d47
Björn Ritzl <bjorn.ritzl@gmail.com>
2021-08-13 08:26:22 +0200

    Sort project options before calculating cache key

--- com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/cache/ResourceCacheKey.java
+++ com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/cache/ResourceCacheKey.java
@@ -3,7 +3,10 @@ package com.dynamo.bob.cache;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.Set;
+import java.util.Collections;
 import java.io.IOException;
 import java.math.BigInteger;
 
@@ -13,12 +16,32 @@ import com.dynamo.bob.archive.EngineVersion;
 
 public class ResourceCacheKey {
 
-	public static String calculate(Task<?> task, Map<String, String> options, IResource outputResource) throws RuntimeException, IOException {
+	/**
+	 * Calculate the key to use when caching a resource.
+	 * The key is created from the task used which created the resource, the
+	 * bytes of the resource itself, as well as engine sha and project options.
+	 * @param task The task which created the resource
+	 * @param projectOptions The project options
+	 * @param resource The resource to calculate cache key for
+	 * @return The cache key as a hex string
+	 */
+	public static String calculate(Task<?> task, Map<String, String> projectOptions, IResource resource) throws RuntimeException, IOException {
 		MessageDigest digest = task.calculateSignatureDigest();
 
-		digest.update(outputResource.getPath().getBytes());
+		digest.update(resource.getPath().getBytes());
 		digest.update(EngineVersion.sha1.getBytes());
-		digest.update(options.toString().getBytes());
+
+		// sort and add project options
+		List<String> keys = new ArrayList<String>(projectOptions.keySet());
+		Collections.sort(keys);
+		for (String key : keys) {
+			digest.update(key.getBytes());
+			String value = projectOptions.get(key);
+			if (value == null) {
+				value = "";
+			}
+			digest.update(value.getBytes());
+		}
 
 		byte[] key = digest.digest();
 		// byte digest to hex string
