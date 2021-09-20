2a4f557f9747de4a8d27f784d29aad777587ff2f
Alexey Gulev <me@agulev.com>
2021-09-20 16:38:38 +0200

    fix DEF-1480 test

---
+++ engine/engine/src/test/def-1480/object_with_collectionproxy.go
@@ -0,0 +1,18 @@
+embedded_components {
+  id: "collectionproxy"
+  type: "collectionproxy"
+  data: "collection: \"/def-1480/empty.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
