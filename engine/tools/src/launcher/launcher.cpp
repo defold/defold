239d7e27e28e90293fe424c929ca750c7c47d29c
Björn Ritzl <bjorn.ritzl@gmail.com>
2021-09-30 19:13:57 +0200

    Increase max memory to 32gb

--- engine/tools/src/launcher/launcher.cpp
+++ engine/tools/src/launcher/launcher.cpp
@@ -1,10 +1,10 @@
 // Copyright 2020 The Defold Foundation
 // Licensed under the Defold License version 1.0 (the "License"); you may not use
 // this file except in compliance with the License.
-// 
+//
 // You may obtain a copy of the License, together with FAQs at
 // https://www.defold.com/license
-// 
+//
 // Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -245,8 +245,9 @@ int Launch(int argc, char **argv) {
     }
 
     char ram_str_buf[100];
-    GetThreeQuartersRAMStr(ram_str_buf, 8589934592ull); // Max 8 gigs.
+    GetThreeQuartersRAMStr(ram_str_buf, 34359738368ull); // Max 32 gigs.
     args[i++] = ram_str_buf;
+    dmLogDebug("ram_str_buf %s", ram_str_buf);
 
     args[i++] = (char*) main;
 #if defined(__MACH__)
