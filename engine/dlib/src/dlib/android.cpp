0f8dd0e76e1ccf95253771d269c851a5cee8487a
Mathias Westerdahl <mwesterdahl76@gmail.com>
2021-10-04 10:21:37 +0200

    Issue 6081: Added JNI thread attach/detach struct to dmSDK (#6082)
    * Issue 6081: Added JNI thread attach/detach struct to dmSDK
    
    * review fixes
    
    * doc fix

---
+++ engine/dlib/src/dlib/android.cpp
@@ -0,0 +1,69 @@
+// Copyright 2020 The Defold Foundation
+// Licensed under the Defold License version 1.0 (the "License"); you may not use
+// this file except in compliance with the License.
+//
+// You may obtain a copy of the License, together with FAQs at
+// https://www.defold.com/license
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+#if defined(ANDROID)
+
+#include <assert.h>
+#include <dmsdk/dlib/android.h>
+
+extern struct android_app* __attribute__((weak)) g_AndroidApp;
+
+namespace dmAndroid
+{
+
+ThreadAttacher::ThreadAttacher()
+: m_Activity(g_AndroidApp->activity)
+, m_Env(NULL)
+, m_IsAttached(false)
+{
+    JNIEnv* env = 0;
+    if (m_Activity->vm->GetEnv((void **)&m_Env, JNI_VERSION_1_6) != JNI_OK)
+    {
+        m_Activity->vm->AttachCurrentThread(&m_Env, 0);
+        m_IsAttached = true;
+    }
+}
+
+bool ThreadAttacher::Detach()
+{
+    bool ok = true;
+    if (m_IsAttached)
+    {
+        if (m_Env->ExceptionCheck())
+        {
+            m_Env->ExceptionDescribe();
+            ok = false;
+        }
+        m_Env->ExceptionClear();
+        m_Activity->vm->DetachCurrentThread();
+        m_IsAttached = false;
+    }
+    return ok;
+}
+
+jclass LoadClass(JNIEnv* env, const char* class_name)
+{
+    jclass activity_class = env->FindClass("android/app/NativeActivity");
+    jmethodID get_class_loader = env->GetMethodID(activity_class,"getClassLoader", "()Ljava/lang/ClassLoader;");
+    jobject cls = env->CallObjectMethod(g_AndroidApp->activity->clazz, get_class_loader);
+    jclass class_loader = env->FindClass("java/lang/ClassLoader");
+    jmethodID find_class = env->GetMethodID(class_loader, "loadClass", "(Ljava/lang/String;)Ljava/lang/Class;");
+    jstring str_class_name = env->NewStringUTF(class_name);
+    jclass klass = (jclass)env->CallObjectMethod(cls, find_class, str_class_name);
+    assert(klass);
+    env->DeleteLocalRef(str_class_name);
+    return klass;
+}
+
+} // namespace
+
+#endif // ANDROID
--- engine/gamesys/src/gamesys/scripts/window/script_window_android.cpp
+++ engine/gamesys/src/gamesys/scripts/window/script_window_android.cpp
@@ -11,13 +11,11 @@
 // specific language governing permissions and limitations under the License.
 
 #include <scripts/script_window.h>
+#include <dlib/log.h>
+#include <dmsdk/dlib/android.h>
 
 #include <android_native_app_glue.h>
-#include <dlib/log.h>
-#include <assert.h>
 #include <jni.h>
-
-extern struct android_app* g_AndroidApp;
 
 struct Window
 {
@@ -40,44 +38,15 @@ struct Window g_Window;
 
 namespace
 {
-
-    bool CheckException(JNIEnv* environment)
-    {
-        assert(environment != NULL);
-        if ((bool) environment->ExceptionCheck())
-        {
-            dmLogError("An exception occurred within the JNI environment (%p)", environment);
-            environment->ExceptionDescribe();
-            environment->ExceptionClear();
-            return false;
-        }
-
-        return true;
-    }
-
-    JNIEnv* Attach()
-    {
-        JNIEnv* environment = NULL;
-        g_AndroidApp->activity->vm->AttachCurrentThread(&environment, NULL);
-        return environment;
-    }
-
-    bool Detach(JNIEnv* environment)
-    {
-        assert(environment != NULL);
-        bool result = CheckException(environment);
-        g_AndroidApp->activity->vm->DetachCurrentThread();
-        return result;
-    }
-
     void Initialize()
     {
-        JNIEnv* environment = ::Attach();
+        dmAndroid::ThreadAttacher thread;
+        JNIEnv* environment = thread.GetEnv();
         if (environment != NULL)
         {
             jclass      jni_class_NativeActivity  = environment->FindClass("android/app/NativeActivity");
             jmethodID   jni_method_getClassLoader = environment->GetMethodID(jni_class_NativeActivity, "getClassLoader", "()Ljava/lang/ClassLoader;");
-            jobject     jni_object_getClassLoader = environment->CallObjectMethod(g_AndroidApp->activity->clazz, jni_method_getClassLoader);
+            jobject     jni_object_getClassLoader = environment->CallObjectMethod(thread.GetActivity()->clazz, jni_method_getClassLoader);
             jclass      jni_class_ClassLoader     = environment->FindClass("java/lang/ClassLoader");
             jmethodID   jni_method_loadClass      = environment->GetMethodID(jni_class_ClassLoader, "loadClass", "(Ljava/lang/String;)Ljava/lang/Class;");
 
@@ -85,7 +54,7 @@ namespace
             jclass      jni_class_Window          = (jclass) environment->CallObjectMethod(jni_object_getClassLoader, jni_method_loadClass, jni_string_Window);
             jmethodID   jni_constructor_Window    = environment->GetMethodID(jni_class_Window, "<init>", "(Landroid/app/Activity;)V");
 
-            g_Window.m_Window                     = environment->NewGlobalRef(environment->NewObject(jni_class_Window, jni_constructor_Window, g_AndroidApp->activity->clazz));
+            g_Window.m_Window                     = environment->NewGlobalRef(environment->NewObject(jni_class_Window, jni_constructor_Window, thread.GetActivity()->clazz));
             g_Window.m_EnableScreenDimming        = environment->GetMethodID(jni_class_Window, "enableScreenDimming", "()V");
             g_Window.m_DisableScreenDimming       = environment->GetMethodID(jni_class_Window, "disableScreenDimming", "()V");
             g_Window.m_IsScreenDimmingEnabled     = environment->GetMethodID(jni_class_Window, "isScreenDimmingEnabled", "()Z");
@@ -93,7 +62,6 @@ namespace
             g_Window.m_Initialized                = true;
 
             environment->DeleteLocalRef(jni_string_Window);
-            (void) ::Detach(environment);
         }
         else
         {
@@ -115,7 +83,8 @@ namespace dmGameSystem
 
         if (g_Window.m_Initialized)
         {
-            JNIEnv* environment = ::Attach();
+            dmAndroid::ThreadAttacher thread;
+            JNIEnv* environment = thread.GetEnv();
             if (environment != NULL)
             {
                 if (mode == DIMMING_ON)
@@ -126,8 +95,6 @@ namespace dmGameSystem
                 {
                     environment->CallVoidMethod(g_Window.m_Window, g_Window.m_DisableScreenDimming);
                 }
-
-                ::Detach(environment);
             }
             else
             {
@@ -149,11 +116,11 @@ namespace dmGameSystem
 
         if (g_Window.m_Initialized)
         {
-            JNIEnv* environment = ::Attach();
+            dmAndroid::ThreadAttacher thread;
+            JNIEnv* environment = thread.GetEnv();
             if (environment != NULL)
             {
                 bool dimming = (bool) environment->CallBooleanMethod(g_Window.m_Window, g_Window.m_IsScreenDimmingEnabled);
-                (void) ::Detach(environment);
                 return dimming ? DIMMING_ON : DIMMING_OFF;
             }
             else
