d9764b082c5049804678ded0738503b21096d5a1
JCash <mwesterdahl76@gmail.com>
2021-10-05 12:06:26 +0200

    FIxed editor tests after removing spine from the engine

--- editor/test/resources/all_types_project/test.go
+++ editor/test/resources/all_types_project/test.go
@@ -164,21 +164,6 @@ components {
   }
 }
 components {
-  id: "spinemodel"
-  component: "/test.spinemodel"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-components {
   id: "sprite"
   component: "/test.sprite"
   position {
---
+++ editor/test/resources/build_project/SideScroller/main/raw_sound.go
@@ -0,0 +1,15 @@
+components {
+  id: "raw"
+  component: "/sound/test_wav.wav"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
