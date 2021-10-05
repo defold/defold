d568e5b82ea644043c398565e7f07a205ada2c41
Mathias Westerdahl <mwesterdahl76@gmail.com>
2021-10-05 15:35:19 +0200

    Fix editor tests (#6088)
    * FIxed editor tests after removing spine from the engine
    
    * Revert "Issue 3775 collision sticking (#6076)"
    
    This reverts commit 3aa72b50354fc7763a7fdaa26b1f7ee3041a2cc2.

---
+++ editor/test/resources/build_project/SideScroller/raptor/raptor.go
@@ -0,0 +1,15 @@
+components {
+  id: "raptor"
+  component: "/merge/empty.sprite"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ editor/test/resources/build_project/SideScroller/spaceship/spaceship_tile_source.go
@@ -0,0 +1,20 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/builtins/graphics/particle_blob.tilesource\"\n"
+  "default_animation: \"anim\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
