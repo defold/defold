b2b42ad536493d22207d37dfbfb9489a188a4458
JCash <mwesterdahl76@gmail.com>
2021-10-19 17:07:15 +0200

    review fix

--- scripts/build.py
+++ scripts/build.py
@@ -899,9 +899,6 @@ class Configuration(object):
 
                 self._add_files_to_zip(zip, protobuf_files, self.dynamo_home, topfolder)
 
-                for x in protobuf_files:
-                    print "MAWE added protobuf file", x
-
                 # bob pipeline classes
                 bob_light = os.path.join(self.dynamo_home, 'share/java/bob-light.jar')
                 self._add_files_to_zip(zip, [bob_light], self.dynamo_home, topfolder)
