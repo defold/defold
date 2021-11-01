a2b39b036f5870688356cc9492763e9f31ab8053
Björn Ritzl <bjorn.ritzl@gmail.com>
2021-11-01 09:12:22 +0100

    Update sound_test.clj (#6153)

--- editor/test/integration/sound_test.clj
+++ editor/test/integration/sound_test.clj
@@ -1,10 +1,10 @@
 ;; Copyright 2020 The Defold Foundation
 ;; Licensed under the Defold License version 1.0 (the "License"); you may not use
 ;; this file except in compliance with the License.
-;; 
+;;
 ;; You may obtain a copy of the License, together with FAQs at
 ;; https://www.defold.com/license
-;; 
+;;
 ;; Unless required by applicable law or agreed to in writing, software distributed
 ;; under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 ;; CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -52,6 +52,5 @@
         (test-util/with-prop [node-id :sound v]
           (is (g/error? (test-util/prop-error node-id :sound)))))
       (is (nil? (test-util/prop-error node-id :gain)))
-      (doseq [v [-0.5 1.5]]
-        (test-util/with-prop [node-id :gain v]
-          (is (g/error? (test-util/prop-error node-id :gain))))))))
+      (test-util/with-prop [node-id :gain -0.5]
+          (is (g/error? (test-util/prop-error node-id :gain)))))))
