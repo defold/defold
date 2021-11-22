df30b35a4ed10abb3fc8ae75b9af45e4917d260a
Alexey Gulev <me@agulev.com>
2021-11-22 19:55:23 +0100

    fix test done counter

--- engine/engine/src/test/script_props/test_done.lua
+++ engine/engine/src/test/script_props/test_done.lua
@@ -5,7 +5,7 @@ M.test_count = 3
 function M.done()
 	M.test_count = M.test_count - 1
 	if M.test_count == 0 then
-		msg.post("@system:", "exit", { code = error_code })
+        msg.post("main:/main#script", "done")
 	end
 end
 
