16d514f0233e62f6c6546d31dd324754bf659d8a
Artsiom Trubchyk <aglitchman@gmail.com>
2022-01-07 21:18:52 +0300

    Update opening of OpenGL 3.1 compat mode window

--- engine/graphics/src/opengl/graphics_opengl.cpp
+++ engine/graphics/src/opengl/graphics_opengl.cpp
@@ -737,9 +737,19 @@ static uintptr_t GetExtProcAddress(const char* name, const char* extension_name,
             if (is_desktop) {
                 dmLogWarning("Trying OpenGL 3.1 compat mode");
 
-                // Try a second time, this time without core profile, and lower the minor version
-                glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 1); // We currently cannot go lower since we support shader model 140
-                glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+                // Try a second time, this time without core profile, and lower the minor version.
+                // And GLFW clears hints, so we have to set them again.
+                if (params->m_HighDPI) {
+                    glfwOpenWindowHint(GLFW_WINDOW_HIGH_DPI, 1);
+                }
+                glfwOpenWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+                glfwOpenWindowHint(GLFW_FSAA_SAMPLES, params->m_Samples);
+
+                // We currently cannot go lower since we support shader model 140
+                glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
+                glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 1);
+
+                glfwOpenWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
                 if (!glfwOpenWindow(params->m_Width, params->m_Height, 8, 8, 8, 8, 32, 8, mode))
                 {
