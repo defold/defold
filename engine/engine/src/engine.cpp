a7d0fb680d79f28a6de122cdcffb492180743499
Alexey Gulev <me@agulev.com>
2022-01-29 12:07:21 +0100

    review fixes

--- engine/engine/src/engine.cpp
+++ engine/engine/src/engine.cpp
@@ -693,28 +693,34 @@ namespace dmEngine
         int write_log = dmConfigFile::GetInt(engine->m_Config, "project.write_log", 0);
         if (write_log) {
             uint32_t count = 0;
-            char* log_paths[2];
+            char* log_paths[3];
+
+            const char* LOG_FILE_NAME = "log.txt";
+
+            const char* log_dir = dmConfigFile::GetString(engine->m_Config, "project.log_dir", NULL);
+            char log_config_path[DMPATH_MAX_PATH];
+            if (log_dir != NULL)
+            {
+                dmPath::Concat(log_dir, LOG_FILE_NAME, log_config_path, sizeof(log_config_path));
+                log_paths[count] = log_config_path;
+                count++;
+            }
+
             char sys_path[DMPATH_MAX_PATH];
             char main_log_path[DMPATH_MAX_PATH];
             if (dmSys::GetLogPath(sys_path, sizeof(sys_path)) == dmSys::RESULT_OK)
             {
-                const char* main_dir = dmConfigFile::GetString(engine->m_Config, "project.log_dir", sys_path);
-                dmPath::Concat(main_dir, "log.txt", main_log_path, sizeof(main_log_path));
+                dmPath::Concat(sys_path, LOG_FILE_NAME, main_log_path, sizeof(main_log_path));
                 log_paths[count] = main_log_path;
                 count++;
             }
-            else
-            {
-                dmLogFatal("Unable to get log-file path");
-            }
-
 
             char application_support_path[DMPATH_MAX_PATH];
             char application_support_log_path[DMPATH_MAX_PATH];
-            const char* logs_dir = dmConfigFile::GetString(engine->m_Config, "project.title_as_file_name", "DefoldLogs");
+            const char* logs_dir = dmConfigFile::GetString(engine->m_Config, "project.title_as_file_name", "defoldlogs");
             if (dmSys::GetApplicationSavePath(logs_dir, application_support_path, sizeof(application_support_path)) == dmSys::RESULT_OK)
             {
-                dmPath::Concat(application_support_path, "log.txt", application_support_log_path, sizeof(application_support_log_path));
+                dmPath::Concat(application_support_path, LOG_FILE_NAME, application_support_log_path, sizeof(application_support_log_path));
                 log_paths[count] = application_support_log_path;
                 count++;
             }
