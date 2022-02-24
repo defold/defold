a4097fb0c609a9701d77111109d91cc0c2aabcdd
Björn Ritzl <bjorn.ritzl@gmail.com>
2022-02-24 14:11:41 +0100

    Reset input on focus loss

--- engine/glfw/js/library_glfw.js
+++ engine/glfw/js/library_glfw.js
@@ -423,8 +423,8 @@ var LibraryGLFW = {
       // If a key is pressed while the game lost focus and that key is released while
       // not in focus the event will not be received for the key release. This will
       // result in the key remaining in the pressed state when the game regains focus.
-      // To fix this we set all pressed keys to released before regaining focus.
-      if (focus == 1) {
+      // To fix this we set all pressed keys to released when focus is lost.
+      if (focus == 0) {
         for (var i = 0; i < GLFW.keys.length; i++) {
           GLFW.keys[i] = 0;
         }
