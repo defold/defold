ef75768d936dbb40001047187b8b72e42e49c189
Björn Ritzl <bjorn.ritzl@gmail.com>
2022-03-25 14:28:24 +0100

    Java plugin capabilities for resource encryption

---
+++ com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/archive/ResourceEncryption.java
@@ -0,0 +1,67 @@
+// Copyright 2020-2022 The Defold Foundation
+// Copyright 2014-2020 King
+// Copyright 2009-2014 Ragnar Svensson, Christian Murray
+// Licensed under the Defold License version 1.0 (the "License"); you may not use
+// this file except in compliance with the License.
+// 
+// You may obtain a copy of the License, together with FAQs at
+// https://www.defold.com/license
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+package com.dynamo.bob.archive;
+
+import com.dynamo.crypt.Crypt;
+import com.dynamo.bob.Bob;
+import com.dynamo.bob.CompileExceptionError;
+import com.dynamo.bob.plugin.PluginScanner;
+
+import java.util.List;
+
+
+public class ResourceEncryption {
+
+	private static ResourceEncryptionPlugin encryptionPlugin;
+
+	// create default encryption implementation
+	private static ResourceEncryptionPlugin defaultEncryption = new ResourceEncryptionPlugin() {
+		private final byte[] KEY = "aQj8CScgNP4VsfXK".getBytes();
+	
+		@Override
+		public byte[] encrypt(byte[] resource) throws Exception {
+			return Crypt.encryptCTR(resource, KEY);
+		}
+	};
+
+	/**
+	 * Encrypt a resource
+	 * @param resource Bytes of resource data to encrypt
+	 * @return Bytes of encrypted resource data
+	 */
+	public static byte[] encrypt(byte[] resource) throws CompileExceptionError {
+		try {
+			if (encryptionPlugin == null) {
+				// find and create plugin instances
+				List<ResourceEncryptionPlugin> encryptionPlugins = PluginScanner.createPlugins("com.dynamo.bob.archive", ResourceEncryptionPlugin.class);
+				
+				// only accept a single encryption instance
+				if (encryptionPlugins.size() > 1) {
+					throw new CompileExceptionError("Found more than one ResourceEncryptionPlugin");
+				}
+				
+				// default or custom encryption
+				encryptionPlugin = encryptionPlugins.isEmpty() ? defaultEncryption : encryptionPlugins.get(0);
+			}
+
+			// do the encryption
+			return encryptionPlugin.encrypt(resource);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			throw new CompileExceptionError("Unable to encrypt resource", e);
+		}
+	}
+}
---
+++ com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/plugin/PluginScanner.java
@@ -0,0 +1,60 @@
+// Copyright 2020-2022 The Defold Foundation
+// Copyright 2014-2020 King
+// Copyright 2009-2014 Ragnar Svensson, Christian Murray
+// Licensed under the Defold License version 1.0 (the "License"); you may not use
+// this file except in compliance with the License.
+// 
+// You may obtain a copy of the License, together with FAQs at
+// https://www.defold.com/license
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+package com.dynamo.bob.plugin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.lang.reflect.Modifier;
+
+import com.dynamo.bob.Bob;
+import com.dynamo.bob.Project;
+import com.dynamo.bob.IClassScanner;
+import com.dynamo.bob.CompileExceptionError;
+
+
+public class PluginScanner {
+
+	/**
+	 * Find and create instances of classes extending a specific base class
+	 * and located within a specific package
+	 * @param packageName 
+	 * @param pluginBaseClass
+	 * @return List of instances
+	 */
+	public static <T> List<T> createPlugins(String packageName, Class<T> pluginBaseClass) throws CompileExceptionError {
+		List<T> plugins = new ArrayList<>();
+
+		IClassScanner scanner = Project.getClassLoaderScanner();
+		Set<String> classNames = scanner.scan(packageName);
+		for (String className : classNames) {
+			try {
+				Class<?> klass = Class.forName(className, true, scanner.getClassLoader());
+				// check that the class extends or is of type pluginBaseClass and that it is not abstract
+				if (klass.isAssignableFrom(pluginBaseClass) && !Modifier.isAbstract(klass.getModifiers())) {
+					Bob.verbose("Found plugin " + className);
+					plugins.add((T)klass.newInstance());
+				}
+			}
+			catch(InstantiationException | IllegalAccessException e) {
+				throw new CompileExceptionError("Unable to create plugin " + className, e);
+			}
+			catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return plugins;
+	}
+}
