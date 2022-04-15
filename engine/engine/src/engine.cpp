06aecae1ea75b71b309ab29e620e1af82c93ef2d
JCash <mwesterdahl76@gmail.com>
2022-04-15 14:52:24 +0200

    Fix for deleting the game object system too early during shutdown

--- engine/engine/src/engine.cpp
+++ engine/engine/src/engine.cpp
@@ -275,7 +275,7 @@ namespace dmEngine
             dmResource::Release(engine->m_Factory, engine->m_MainCollection);
         dmGameObject::PostUpdate(engine->m_Register);
 
-        dmGameObject::DeleteRegister(engine->m_Register); // Delete all collections and game objects
+        dmGameObject::DeleteCollections(engine->m_Register); // Delete all collections and game objects
 
         dmHttpClient::ShutdownConnectionPool();
 
@@ -305,6 +305,8 @@ namespace dmEngine
         }
 
         dmHttpClient::ReopenConnectionPool();
+
+        dmGameObject::DeleteRegister(engine->m_Register);
 
         UnloadBootstrapContent(engine);
 
--- engine/gameobject/src/gameobject/gameobject.cpp
+++ engine/gameobject/src/gameobject/gameobject.cpp
@@ -249,7 +249,7 @@ namespace dmGameObject
         return regist->m_DefaultInputStackCapacity;
     }
 
-    void DeleteRegister(HRegister regist)
+    void DeleteCollections(HRegister regist)
     {
         uint32_t collection_count = regist->m_Collections.Size();
         for (uint32_t i = 0; i < collection_count; ++i)
@@ -260,6 +260,12 @@ namespace dmGameObject
             FinalCollection(collection);
             DeleteCollection(collection);
         }
+        regist->m_Collections.SetSize(0);
+    }
+
+    void DeleteRegister(HRegister regist)
+    {
+        DeleteCollections(regist);
         delete regist;
     }
 
