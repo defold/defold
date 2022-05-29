217fed2049afc10b59631901d5a1431fb43e3146
JCash <mwesterdahl76@gmail.com>
2022-05-29 21:43:45 +0200

    FIx for DM_PLATFORM

--- engine/dlib/src/dlib/platform.h
+++ engine/dlib/src/dlib/platform.h
@@ -16,13 +16,14 @@
 #define DM_PLATFORM_H
 
 #define DM_PLATFORM_LINUX   "linux"
-#define DM_PLATFORM_MACOS   "macos"
-#define DM_PLATFORM_OSX     DM_PLATFORM_MACOS
+#define DM_PLATFORM_OSX     "osx""
 #define DM_PLATFORM_WINDOWS "windows"
 #define DM_PLATFORM_WEB     "web"
 #define DM_PLATFORM_ANDROID "android"
 #define DM_PLATFORM_IOS 	"ios"
 #define DM_PLATFORM_SWITCH 	"switch"
+
+// Note: DM_PLATFORM is used as a key in data files, e.g. "".gamepads"
 
 #if defined(ANDROID)
 #define DM_PLATFORM DM_PLATFORM_ANDROID
@@ -31,7 +32,7 @@
 #elif defined(__MACH__) && (defined(__arm__) || defined(__arm64__))
 #define DM_PLATFORM DM_PLATFORM_IOS
 #elif defined(__MACH__)
-#define DM_PLATFORM DM_PLATFORM_MACOS
+#define DM_PLATFORM DM_PLATFORM_OSX
 #elif defined(_WIN32)
 #define DM_PLATFORM DM_PLATFORM_WINDOWS
 #elif defined(__EMSCRIPTEN__)
