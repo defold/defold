eacf1ef58c51ac45ea9bf03e337e9d6baf350047
Jhonny Göransson <jhonny.goransson@gmail.com>
2022-06-02 23:45:16 +0200

    Fix exception

--- editor/scripts/bundle.py
+++ editor/scripts/bundle.py
@@ -4,10 +4,10 @@
 # Copyright 2009-2014 Ragnar Svensson, Christian Murray
 # Licensed under the Defold License version 1.0 (the "License"); you may not use
 # this file except in compliance with the License.
-# 
+#
 # You may obtain a copy of the License, together with FAQs at
 # https://www.defold.com/license
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed
 # under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -36,7 +36,7 @@ try:
     sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', 'scripts'))
     sys.dont_write_bytecode = True
     import build_private
-except Exception, e:
+except (Exception, e):
     class build_private(object):
         @classmethod
         def get_tag_suffix(self):
