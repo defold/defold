8e6e6e2525a28da758483bbc463b33386e6a3e7e
JCash <mwesterdahl76@gmail.com>
2022-06-03 16:57:21 +0200

    Disabled memtest for now

--- engine/dlib/scripts/run_test_memprofile.sh
+++ engine/dlib/scripts/run_test_memprofile.sh
@@ -17,8 +17,6 @@
 set -e
 platform=`uname -s`
 
-export DMMEMPROFILE_TRACE=1
-
 if [ "Darwin" == $platform ]; then
     # NOTE: We used to link with -flat_namespace but go spurious link errors
     export DYLD_FORCE_FLAT_NAMESPACE=yes
@@ -26,16 +24,13 @@ if [ "Darwin" == $platform ]; then
 fi
 
 if [ "Linux" == $platform ]; then
-    # Currently, there is something that hangs the exiting the remotery thread upon exit
-    # we skip this for now
-    unset DMMEMPROFILE_TRACE
-
     export LD_PRELOAD=./build/default/src/libdlib_memprofile.so
 fi
 
 
 echo "# Test config " > memprofile.cfg
 
+export DMMEMPROFILE_TRACE=1
 ./build/default/src/test/test_memprofile memprofile.cfg dummy $@
 
 
