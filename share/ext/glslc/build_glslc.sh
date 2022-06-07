1fc8f169364123e77df8d2b7fe8b478903e00012
JCash <mwesterdahl76@gmail.com>
2022-06-07 09:39:31 +0200

    Added new build script to build glslc

--- share/ext/glslc/build_glslc.sh
+++ share/ext/glslc/build_glslc.sh
@@ -13,122 +13,48 @@
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
+PLATFORM=$1
+PWD=$(pwd)
+SOURCE_DIR=${PWD}/source
+BUILD_DIR=${PWD}/build/${PLATFORM}
 
+case $PLATFORM in
+    arm64-macos)
+        CMAKE_OSX_ARCHITECTURES=arm64
+        ;;
+    x86_64-macos)
+        CMAKE_OSX_ARCHITECTURES=x86_64
+        ;;
+esac
 
-readonly PRODUCT=glslc
-readonly VERSION=v2018.0
-readonly FILE_URL=${VERSION}.tar.gz
-readonly BASE_URL=https://github.com/google/shaderc/archive/
+# Follow the build instructions on https://github.com/google/shaderc
 
-. ../common.sh
+git clone https://github.com/google/shaderc $SOURCE_DIR
+pushd $SOURCE_DIR
+./utils/git-sync-deps
+popd
 
-function cmi_configure() {
-    pushd third_party >/dev/null
-    git clone --depth 1 https://github.com/google/googletest.git
-    git clone --depth 1 https://github.com/google/glslang.git
-    git clone --depth 1 https://github.com/KhronosGroup/SPIRV-Tools.git spirv-tools
-    git clone --depth 1 https://github.com/KhronosGroup/SPIRV-Headers.git spirv-headers
-    git clone --depth 1 https://github.com/google/re2.git
-    git clone --depth 1 https://github.com/google/effcee.git
-    popd >/dev/null
+# Build
 
-    case $PLATFORM in
-        x86_64-macos)
-            CMAKE_GENERATOR="Unix Makefiles"
-            MAKE_OPTIONS="-- -j8"
-            OUTPUT_EXECUTABLE_FILE=$PRODUCT/$PRODUCT
-            ;;
+mkdir -p ${BUILD_DIR}
 
-        x86_64-linux)
-            CMAKE_GENERATOR="Unix Makefiles"
-            MAKE_OPTIONS="-- -j8"
-            OUTPUT_EXECUTABLE_FILE=$PRODUCT/$PRODUCT
-            ;;
+pushd $BUILD_DIR
 
-    	win32)
-            CMAKE_GENERATOR="Visual Studio 14 2015"
-            MAKE_OPTIONS=""
-            OUTPUT_EXECUTABLE_FILE=$PRODUCT/Release/$PRODUCT.exe
-            ;;
+cmake  -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES} $SOURCE_DIR
+make -j8
 
-        x86_64-win32)
-            CMAKE_GENERATOR="Visual Studio 14 2015 Win64"
-            MAKE_OPTIONS=""
-            OUTPUT_EXECUTABLE_FILE=$PRODUCT/Release/$PRODUCT.exe
-            ;;
-    esac
+mkdir -p ./bin/$PLATFORM
+cp -v ./glslc/glslc ./bin/$PLATFORM
 
-    set -e
-    mkdir -p build >/dev/null
-    pushd build >/dev/null
-    cmake -DSHADERC_SKIP_TESTS=ON -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -G"${CMAKE_GENERATOR}" ..
-    popd >/dev/null
-}
+popd
 
-function cmi_make() {
-    set -e
+# Package
 
-    pushd build >/dev/null
-    cmake --build . --config Release --target glslc_exe $MAKE_OPTIONS
-    mkdir -p $PREFIX/bin/$PLATFORM
-    cp $OUTPUT_EXECUTABLE_FILE $PREFIX/bin/$PLATFORM
-    popd >/dev/null
-    cmi_strip
+VERSION=$(cd $SOURCE_DIR && git rev-parse --short HEAD)
+echo VERSION=${VERSION}
 
-    set +e
-}
+PACKAGE=glslc-${VERSION}-${PLATFORM}.tar.gz
 
-function cmi_buildplatform() {
-    cmi_do $PLATFORM ""
-
-    local TGZ="$PRODUCT-$VERSION-$PLATFORM.tar.gz"
-
-    pushd $PREFIX  >/dev/null
-    tar cfvz $TGZ bin
-
-    popd >/dev/null
-    popd >/dev/null
-
-    mkdir ../build
-
-    mv -v $PREFIX/$TGZ ../build
-    echo "../build/$TGZ created"
-
-    rm -rf tmp
-    rm -rf $PREFIX
-}
-
-function cmi() {
-    export PREFIX=`pwd`/build
-    export PLATFORM=$1
-
-    case $PLATFORM in
-        x86_64-macos)
-            function cmi_strip() {
-                strip -S -x $PREFIX/bin/$PLATFORM/$PRODUCT
-            }
-            cmi_buildplatform $PLATFORM
-            ;;
-
-        x86_64-linux)
-            function cmi_strip() {
-                strip -s $PREFIX/bin/$PLATFORM/$PRODUCT
-            }
-            cmi_buildplatform $PLATFORM
-            ;;
-
-    	win32|x86_64-win32)
-            function cmi_strip() {
-                echo "No stripping supported"
-            }
-            cmi_setup_vs2015_env $PLATFORM
-            cmi_buildplatform $PLATFORM
-            ;;
-        *)
-            echo "Unknown target $PLATFORM" && exit 1
-            ;;
-    esac
-}
-
-download
-cmi $1
+pushd $BUILD_DIR
+tar cfvz $PACKAGE bin
+popd
