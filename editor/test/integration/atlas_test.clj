4fdedce795c91095547093e612e226b8aa6b5cad
Mats Gisselson <mats.gisselson@gmail.com>
2022-06-20 14:42:54 +0200

    Added tests for disk errors when generating convex hulls from atlas images

--- editor/test/integration/atlas_test.clj
+++ editor/test/integration/atlas_test.clj
@@ -13,9 +13,14 @@
 ;; specific language governing permissions and limitations under the License.
 
 (ns integration.atlas-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer :all]
             [dynamo.graph :as g]
-            [integration.test-util :as test-util]))
+            [editor.defold-project :as project]
+            [editor.fs :as fs]
+            [editor.workspace :as workspace]
+            [integration.test-util :as test-util]
+            [support.test-support :as test-support]))
 
 (deftest valid-fps
   (test-util/with-loaded-project
@@ -40,3 +45,55 @@
               "block_anim"
               "pow_anim"]
              animation-ids-in-ddf)))))
+
+(deftest sprite-trim-mode-image-io-error
+  (test-support/with-clean-system
+    (let [workspace (test-util/setup-scratch-workspace! world "test/resources/small_project")
+          project (test-util/setup-project! workspace)
+          atlas (project/get-resource-node project "/main/logo.atlas")
+          atlas-image (:node-id (test-util/outline atlas [0]))
+          image-file (io/as-file (g/node-value atlas-image :image))
+          image-bytes (fs/read-bytes image-file)]
+
+      ;; Enable sprite trimming for our AtlasImage.
+      (g/set-property! atlas-image :sprite-trim-mode :sprite-trim-mode-6)
+      (is (not (g/error? (g/node-value atlas :scene))))
+
+      (testing "Corrupting referenced image file"
+        (test-support/spit-until-new-mtime image-file "This is no longer an image file.")
+        (testing "Before resource-sync"
+          (g/clear-system-cache!)
+          (is (g/error? (g/node-value atlas :scene)))
+          (is (g/error? (g/node-value atlas :build-targets)))
+          (is (not (g/error? (g/node-value atlas :save-data)))))
+        (testing "After resource-sync"
+          (workspace/resource-sync! workspace)
+          (is (g/error? (g/node-value atlas :scene)))
+          (is (g/error? (g/node-value atlas :build-targets)))
+          (is (not (g/error? (g/node-value atlas :save-data))))))
+
+      (testing "Restoring referenced image file"
+        (test-support/write-until-new-mtime image-file image-bytes)
+        (testing "Before resource-sync"
+          (g/clear-system-cache!)
+          (is (not (g/error? (g/node-value atlas :scene))))
+          (is (not (g/error? (g/node-value atlas :build-targets))))
+          (is (not (g/error? (g/node-value atlas :save-data)))))
+        (testing "After resource-sync"
+          (workspace/resource-sync! workspace)
+          (is (not (g/error? (g/node-value atlas :scene))))
+          (is (not (g/error? (g/node-value atlas :build-targets))))
+          (is (not (g/error? (g/node-value atlas :save-data))))))
+
+      (testing "Deleting referenced image file"
+        (fs/delete! image-file)
+        (testing "Before resource-sync"
+          (g/clear-system-cache!)
+          (is (g/error? (g/node-value atlas :scene)))
+          (is (g/error? (g/node-value atlas :build-targets)))
+          (is (not (g/error? (g/node-value atlas :save-data)))))
+        (testing "After resource-sync"
+          (workspace/resource-sync! workspace)
+          (is (g/error? (g/node-value atlas :scene)))
+          (is (g/error? (g/node-value atlas :build-targets)))
+          (is (not (g/error? (g/node-value atlas :save-data)))))))))
