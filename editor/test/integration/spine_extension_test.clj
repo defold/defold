0db2a324a67f7adc18a42726008e5f1a82076df0
Mats Gisselson <mats.gisselson@gmail.com>
2022-06-27 14:39:57 +0200

    Added save data integrity checks to legacy spine project editor tests

--- editor/test/integration/spine_extension_test.clj
+++ editor/test/integration/spine_extension_test.clj
@@ -14,20 +14,54 @@
 
 (ns integration.spine-extension-test
   (:require [clojure.java.io :as io]
+            [clojure.string :as string]
             [clojure.test :refer :all]
             [dynamo.graph :as g]
             [editor.build-errors-view :as build-errors-view]
+            [editor.collection-data :as collection-data]
             [editor.defold-project :as project]
+            [editor.diff-view :as diff-view]
             [editor.game-project :as game-project]
             [editor.gui :as gui]
+            [editor.resource :as resource]
             [editor.resource-node :as resource-node]
             [editor.workspace :as workspace]
             [integration.test-util :as test-util]
             [support.test-support :as test-support]))
 
+(set! *warn-on-reflection* true)
+
 (defonce ^:private extension-spine-url "https://github.com/defold/extension-spine/archive/main.zip")
 
 (def ^:private error-item-open-info-without-opts (comp pop build-errors-view/error-item-open-info))
+
+(defn- save-data-content-by-proj-path [project]
+  (into {}
+        (map (juxt (comp resource/proj-path :resource)
+                   :content))
+        (project/all-save-data project)))
+
+(defn- diff-lines-range [{:keys [begin end]} text]
+  (into []
+        (comp (drop begin)
+              (take (- end begin)))
+        (string/split-lines text)))
+
+(defn- diff-save-data-content-by-proj-path [save-data-content-by-proj-path-before save-data-content-by-proj-path-after]
+  (into {}
+        (keep (fn [[proj-path save-data-content-after]]
+                (let [save-data-content-before (save-data-content-by-proj-path-before proj-path)
+                      differences (filter (comp (partial not= :nop) :type)
+                                          (diff-view/diff save-data-content-before save-data-content-after))]
+                  (when-not (empty? differences)
+                    [proj-path (mapv (fn [difference]
+                                       (case (:type difference)
+                                         :replace (-> difference
+                                                      (update :left diff-lines-range save-data-content-before)
+                                                      (update :right diff-lines-range save-data-content-after))
+                                         difference))
+                                     differences)]))))
+        save-data-content-by-proj-path-after))
 
 (deftest legacy-spine-project-user-migration-test
   ;; Clear custom gui scene loaders to ensure a clean test.
@@ -123,8 +157,9 @@
             ;; already contains updated Spine JSON data that has been exported
             ;; from an up-to-date version of Spine, so we just have to update
             ;; our references to point to the new Spine JSON.
-            (testing "Updating references to the new version of the Spine JSON fixes the build errors."
-              (let [new-spine-json-resource (workspace/find-resource workspace "/assets/spineboy/spineboy.spinejson")
+            (testing "Updating references to the new version of the Spine JSON."
+              (let [save-data-content-by-proj-path-before (save-data-content-by-proj-path project)
+                    new-spine-json-resource (workspace/find-resource workspace "/assets/spineboy/spineboy.spinejson")
                     new-spine-atlas-resource (workspace/find-resource workspace "/assets/spineboy/spineboy.atlas")
                     new-spine-material-resource (workspace/find-resource workspace "/defold-spine/assets/spine.material")
                     old-spine-scene (test-util/resource-node project "/assets/spineboy.spinescene")
@@ -132,40 +167,71 @@
                 (test-util/prop! old-spine-scene :spine-json new-spine-json-resource)
                 (test-util/prop! old-spine-scene :atlas new-spine-atlas-resource)
                 (test-util/prop! embedded-spine-model :material new-spine-material-resource)
-                (is (not (g/error? (g/node-value main-collection :scene))))
-                (is (not (g/error? (g/node-value main-gui :scene))))
-                (is (not (g/error? (test-util/build-error! main-collection))))
-                (is (not (g/error? (test-util/build-error! main-gui))))
-                (let [main-gui-save-value (g/node-value main-gui :save-value)
-                      main-gui-nodes (:nodes main-gui-save-value)
-                      spine-gui-node (first main-gui-nodes)]
-                  (is (not (contains? main-gui-save-value :spine-scenes)))
-                  (is (= [{:name "spineboy"
-                           :path "/assets/spineboy.spinescene"}]
-                         (:resources main-gui-save-value)))
-                  (is (= 1 (count main-gui-nodes)))
-                  (is (= [701.0 0.0 0.0 1.0] (:position spine-gui-node)))
-                  (is (= [0.0 0.0 0.0 1.0] (:rotation spine-gui-node)))
-                  (is (= [1.0 1.0 1.0 1.0] (:scale spine-gui-node)))
-                  (is (= [1.0 1.0 0.0 1.0] (:size spine-gui-node)))
-                  (is (= [1.0 1.0 1.0 1.0] (:color spine-gui-node)))
-                  (is (= :type-custom (:type spine-gui-node)))
-                  (is (= 405028931 (:custom-type spine-gui-node)))
-                  (is (= :blend-mode-alpha (:blend-mode spine-gui-node)))
-                  (is (= "spine" (:id spine-gui-node)))
-                  (is (= :xanchor-none (:xanchor spine-gui-node)))
-                  (is (= :yanchor-none (:yanchor spine-gui-node)))
-                  (is (= :pivot-center (:pivot spine-gui-node)))
-                  (is (= :adjust-mode-fit (:adjust-mode spine-gui-node)))
-                  (is (= "" (:layer spine-gui-node)))
-                  (is (= true (:inherit-alpha spine-gui-node)))
-                  (is (= :clipping-mode-none (:clipping-mode spine-gui-node)))
-                  (is (= true (:clipping-visible spine-gui-node)))
-                  (is (= false (:clipping-inverted spine-gui-node)))
-                  (is (= 1.0 (:alpha spine-gui-node)))
-                  (is (= false (:template-node-child spine-gui-node)))
-                  (is (= :size-mode-auto (:size-mode spine-gui-node)))
-                  (is (= "spineboy" (:spine-scene spine-gui-node)))
-                  (is (= "walk" (:spine-default-animation spine-gui-node)))
-                  (is (= "" (:spine-skin spine-gui-node)))
-                  (is (empty? (:overridden-fields spine-gui-node))))))))))))
+
+                (testing "Updating references to the new version of the Spine JSON fixes all errors."
+                  (is (not (g/error? (g/node-value main-collection :scene))))
+                  (is (not (g/error? (g/node-value main-gui :scene))))
+                  (is (not (g/error? (test-util/build-error! main-collection))))
+                  (is (not (g/error? (test-util/build-error! main-gui)))))
+
+                (testing "Collection properties are retained post-update."
+                  (let [ext->resource-type (workspace/get-resource-type-map workspace)
+                        main-collection-save-value (g/node-value main-collection :save-value)
+                        main-collection-data (collection-data/string-decode-collection-data ext->resource-type main-collection-save-value)
+                        embedded-spine-model-data (get-in main-collection-data [:embedded-instances 0 :data :embedded-components 0 :data])]
+                    (is (= "/assets/spineboy.spinescene" (:spine-scene embedded-spine-model-data)))
+                    (is (= "idle" (:default-animation embedded-spine-model-data)))
+                    (is (= "" (:skin embedded-spine-model-data)))
+                    (is (= :blend-mode-alpha (:blend-mode embedded-spine-model-data)))
+                    (is (= "/defold-spine/assets/spine.material" (:material embedded-spine-model-data)))))
+
+                (testing "Gui properties are retained post-update."
+                  (let [main-gui-save-value (g/node-value main-gui :save-value)
+                        main-gui-nodes (:nodes main-gui-save-value)
+                        spine-gui-node (first main-gui-nodes)]
+                    (is (not (contains? main-gui-save-value :spine-scenes)))
+                    (is (= [{:name "spineboy"
+                             :path "/assets/spineboy.spinescene"}]
+                           (:resources main-gui-save-value)))
+                    (is (= 1 (count main-gui-nodes)))
+                    (is (= [701.0 0.0 0.0 1.0] (:position spine-gui-node)))
+                    (is (= [0.0 0.0 0.0 1.0] (:rotation spine-gui-node)))
+                    (is (= [1.0 1.0 1.0 1.0] (:scale spine-gui-node)))
+                    (is (= [1.0 1.0 0.0 1.0] (:size spine-gui-node)))
+                    (is (= [1.0 1.0 1.0 1.0] (:color spine-gui-node)))
+                    (is (= :type-custom (:type spine-gui-node)))
+                    (is (= 405028931 (:custom-type spine-gui-node)))
+                    (is (= :blend-mode-alpha (:blend-mode spine-gui-node)))
+                    (is (= "spine" (:id spine-gui-node)))
+                    (is (= :xanchor-none (:xanchor spine-gui-node)))
+                    (is (= :yanchor-none (:yanchor spine-gui-node)))
+                    (is (= :pivot-center (:pivot spine-gui-node)))
+                    (is (= :adjust-mode-fit (:adjust-mode spine-gui-node)))
+                    (is (= "" (:layer spine-gui-node)))
+                    (is (= true (:inherit-alpha spine-gui-node)))
+                    (is (= :clipping-mode-none (:clipping-mode spine-gui-node)))
+                    (is (= true (:clipping-visible spine-gui-node)))
+                    (is (= false (:clipping-inverted spine-gui-node)))
+                    (is (= 1.0 (:alpha spine-gui-node)))
+                    (is (= false (:template-node-child spine-gui-node)))
+                    (is (= :size-mode-auto (:size-mode spine-gui-node)))
+                    (is (= "spineboy" (:spine-scene spine-gui-node)))
+                    (is (= "walk" (:spine-default-animation spine-gui-node)))
+                    (is (= "" (:spine-skin spine-gui-node)))
+                    (is (empty? (:overridden-fields spine-gui-node)))))
+
+                (testing "Expected save-data differences post-update."
+                  (let [save-data-content-by-proj-path-after (save-data-content-by-proj-path project)
+                        save-data-diffs-by-proj-path (diff-save-data-content-by-proj-path save-data-content-by-proj-path-before save-data-content-by-proj-path-after)]
+                    (is (= {"/assets/spineboy.spinescene"
+                            [{:type  :replace
+                              :left  ["spine_json: \"/assets/spineboy.json\""
+                                      "atlas: \"/assets/spineboy.atlas\""]
+                              :right ["spine_json: \"/assets/spineboy/spineboy.spinejson\""
+                                      "atlas: \"/assets/spineboy/spineboy.atlas\""]}]
+
+                            "/main/main.collection"
+                            [{:type  :replace
+                              :left  ["  \"material: \\\\\\\"/builtins/materials/spine.material\\\\\\\"\\\\n\""]
+                              :right ["  \"material: \\\\\\\"/defold-spine/assets/spine.material\\\\\\\"\\\\n\""]}]}
+                           save-data-diffs-by-proj-path))))))))))))
