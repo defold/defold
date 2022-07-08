34ad7fa33e2d988cb5c32f8d376805ce096bb27d
Jhonny Göransson <jhonny.goransson@gmail.com>
2022-07-08 13:29:27 +0200

    Use lib prefix when generating stlibs

--- build_tools/waf_dynamo.py
+++ build_tools/waf_dynamo.py
@@ -490,6 +490,12 @@ def default_flags(self):
         self.env.append_value('LINKFLAGS', ['shell32.lib', 'WS2_32.LIB', 'Iphlpapi.LIB', 'AdvAPI32.Lib', 'Gdi32.lib'])
         self.env.append_unique('ARFLAGS', '/WX')
 
+        self.env.LIB_ST           = 'lib%s.lib'
+        self.env.STLIB_ST         = 'lib%s.lib'
+        self.env.implib_PATTERN   = 'lib%s.lib'
+        self.env.cstlib_PATTERN   = 'lib%s.lib'
+        self.env.cxxstlib_PATTERN = 'lib%s.lib'
+
     platform_setup_vars(self, build_util)
 
     if Options.options.with_iwyu and 'IWYU' in self.env:
