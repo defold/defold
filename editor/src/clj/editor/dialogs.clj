bf0a1d0c8606d2962364234720f2459f69fd62de
vlaaad <vlad@refold.io>
2022-08-03 17:29:05 +0200

    Actualize rename clash dialog (#6853)
    When I started looking into #6690, I noticed that we still use an old dialog for resolving conflicts during file rename operations. This commit actualizes the dialog so it uses the new dialog UI.

--- editor/src/clj/editor/dialogs.clj
+++ editor/src/clj/editor/dialogs.clj
@@ -959,35 +959,24 @@
                                            (.getCanonicalFile)))))
     :description {:fx/type new-file-dialog}))
 
-(handler/defhandler ::rename-conflicting-files :dialog
-  (run [^Stage stage]
-    (ui/user-data! stage ::file-conflict-resolution-strategy :rename)
-    (ui/close! stage)))
-
-(handler/defhandler ::overwrite-conflicting-files :dialog
-  (run [^Stage stage]
-    (ui/user-data! stage ::file-conflict-resolution-strategy :overwrite)
-    (ui/close! stage)))
-
 (defn ^:dynamic make-resolve-file-conflicts-dialog
   [src-dest-pairs]
-  (let [^Parent root (ui/load-fxml "resolve-file-conflicts.fxml")
-        scene (Scene. root)
-        ^Stage stage (doto (ui/make-dialog-stage (ui/main-stage))
-                       (ui/title! "Name Conflict")
-                       (.setScene scene))
-        controls (ui/collect-controls root ["message" "rename" "overwrite" "cancel"])]
-    (ui/context! root :dialog {:stage stage} nil)
-    (ui/bind-action! (:rename controls) ::rename-conflicting-files)
-    (ui/bind-action! (:overwrite controls) ::overwrite-conflicting-files)
-    (ui/bind-action! (:cancel controls) ::close)
-    (.setCancelButton ^Button (:cancel controls) true)
-    (ui/text! (:message controls) (let [conflict-count (count src-dest-pairs)]
-                                    (if (= 1 conflict-count)
-                                      "The destination has an entry with the same name."
-                                      (format "The destination has %d entries with conflicting names." conflict-count))))
-    (ui/show-and-wait! stage)
-    (ui/user-data stage ::file-conflict-resolution-strategy)))
+  (make-confirmation-dialog
+    {:icon :icon/circle-question
+     :size :large
+     :title "Name Conflict"
+     :header (let [conflict-count (count src-dest-pairs)]
+               (if (= 1 conflict-count)
+                 "The destination has an entry with the same name."
+                 (format "The destination has %d entries with conflicting names." conflict-count)))
+     :buttons [{:text "Cancel"
+                :cancel-button true
+                :default-button true}
+               {:text "Name Differently"
+                :result :rename}
+               {:text "Overwrite Files"
+                :variant :danger
+                :result :overwrite}]}))
 
 (def ext-with-selection-props
   (fx/make-ext-with-props
