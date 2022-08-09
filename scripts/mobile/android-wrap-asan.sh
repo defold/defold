77dfd13e61c7116cc7b7dcf09270ac64ed58922f
JCash <mwesterdahl76@gmail.com>
2022-08-09 10:50:42 +0200

    cleanup

--- scripts/mobile/android-wrap-asan.sh
+++ scripts/mobile/android-wrap-asan.sh
@@ -14,9 +14,6 @@ cmd=$1
 shift
 
 os_version=$(getprop ro.build.version.sdk)
-#os_version=27
-
-echo "MAWE OS VERSION: ${os_version}"
 
 if [ "$os_version" -eq "27" ]; then
   cmd="$cmd -Xrunjdwp:transport=dt_android_adb,suspend=n,server=y -Xcompiler-option --debuggable $@"
