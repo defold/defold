8ad212a4b67ab4f7fa82b38f39e9033051104448
JCash <mwesterdahl76@gmail.com>
2022-08-16 13:51:59 +0200

    Convert all args to str

--- build_tools/run.py
+++ build_tools/run.py
@@ -21,9 +21,15 @@ class ExecException(Exception):
         self.retcode = retcode
         self.output = output
 
+def _to_str(x):
+    if isinstance(x, (bytes, bytearray)):
+        x = str(x, encoding='utf-8')
+    return x
+
 def _exec_command(arg_list, **kwargs):
     arg_str = arg_list
     if not isinstance(arg_str, str):
+        arg_list = [_to_str(x) for x in arg_list]
         arg_str = ' '.join(arg_list)
     log('[exec] %s' % arg_str)
 
@@ -34,7 +40,7 @@ def _exec_command(arg_list, **kwargs):
         process = subprocess.Popen(arg_list, **kwargs)
         output = process.communicate()[0]
         if process.returncode != 0:
-            log(str(output, encoding='utf-8'))
+            log(_to_str(output))
     else:
         # On the CI machines, we make sure we produce a steady stream of output
         # However, this also makes us lose the color information
@@ -54,10 +60,7 @@ def _exec_command(arg_list, **kwargs):
     if process.wait() != 0:
         raise ExecException(process.returncode, output)
 
-    if output is None:
-        return ''
-    if isinstance(output, (bytes, bytearray)):
-        output = str(output, encoding='utf-8')
+    output = _to_str(output)
     return output.strip()
 
 def command(args, **kwargs):
