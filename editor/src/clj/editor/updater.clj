b8296f6779f61c04022d54df1a7f8150d26db706
Björn Ritzl <bjorn.ritzl@gmail.com>
2022-08-29 23:35:03 +0200

    Update updater.clj

--- editor/src/clj/editor/updater.clj
+++ editor/src/clj/editor/updater.clj
@@ -23,7 +23,7 @@
             [service.log :as log]
             [util.net :as net])
   (:import [com.defold.editor Editor]
-           [com.defold.editor Platform]
+           [com.dynamo.bob Platform]
            [java.io File IOException]
            [java.nio.file Files CopyOption StandardCopyOption]
            [java.nio.file.attribute FileAttribute]
