041865153c2e75129e832e256d46f9316f92ebc4
Björn Ritzl <bjorn.ritzl@gmail.com>
2022-08-30 14:59:13 +0200

    Increase table serialization limits (#6839)
    * Documented version changes
    
    * Handle more than 65535 keys
    
    * Added function to get table size
    
    * Explicit version checks for index serialization
    
    * Added dynamic allocation if size is above 512kb
    
    * Removed overflow test
    
    No point in testing this anymore. The max we support is larger than the allowed integer index in a Lua table (26 bits)
    
    * Check room for table count
    
    * Clear count
    
    * Update script_sys.cpp
    
    * No need to check for eof
    
    We know the file size
    
    * Improved table length calculation
    
    * Added v3 table load test
    
    * Updated Lua sys load/save test
    
    * Update test_script_sys.cpp
    
    * Fixed align calculation
    
    * Update test_sys.lua
    
    * Update run.py
    
    * Update run.py
    
    * Update test_sys.lua
    
    * Catch more errors
    
    * Update test_sys.lua
    
    * Update test_sys.lua
    
    * Update test_sys.lua
    
    * Update script_sys.cpp
    
    * Changed function name
    
    * Update script_sys.cpp
    
    * Update script_sys.cpp
    
    * Update script_sys.cpp
    
    * Update script_sys.cpp

--- engine/script/src/test/test_sys.lua
+++ engine/script/src/test/test_sys.lua
@@ -14,7 +14,7 @@
 
 function test_sys()
     local filename = "save001.save"
-    local max_table_size = 512 * 1024
+    local max_table_size_v3 = 512 * 1024
     local file = sys.get_save_file("my_game", filename)
     -- Get file again, test mkdir
     file = sys.get_save_file("my_game", filename)
@@ -24,58 +24,87 @@ function test_sys()
     local data = sys.load(file)
     assert(#data == 0)
 
-    -- save file exceeding max buffer size, expected to fail
-    for i=1,max_table_size+1 do data[i] = i end
+    -- save large table, it should not fail with v4+
+    print("Saving large table")
+    for i=1,65536 do data[i] = i end
     local ret, msg = pcall(function() sys.save(file, data) end)
-    if ret then assert(false, "expected lua error for sys.save with data table exceeding max size " .. max_table_size .. " bytes") end
+    if not ret then
+        print("An error occurred when calling sys.save()")
+        print(msg)
+        assert(false, "expected sys.save to work with data table exceeding max size " .. max_table_size_v3 .. " bytes")
+    end
+    -- load large table, it should not fail with v4+
+    print("Loading large table")
+    local ret, msg = pcall(function() sys.load(file) end)
+    if not ret then
+        print("An error occurred when calling sys.load()")
+        print(msg)
+        assert(false, "expected sys.load to work with data table exceeding max size " .. max_table_size_v3 .. " bytes")
+    end
 
     -- save file with too long path (>1024 chars long), expected to fail
+    print("Testing long filename")
     local valid_data = { high_score = 1234, location = vmath.vector3(1,2,3), xp = 99, name = "Mr Player" }
     local long_filename = "dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy_dummy.save"
     local long_file = sys.get_save_file("my_game", long_filename)
 
     ret, msg = pcall(function() sys.save(long_file, valid_data) end)
-    if ret then assert(false, "expected lua error for sys.save with too long path") end
+    if ret then
+        assert(false, "expected lua error for sys.save with too long path")
+    end
 
     -- save file
+    print("Saving file")
     local data = { high_score = 1111, location = vmath.vector3(2,2,2), xp = 33, name = "First Save" }
-    local result = sys.save(file, data)
-    assert(result)
+    ret, msg = pcall(function() sys.save(file, data) end)
+    if not ret then
+        print("An error occurred when calling sys.save() on a small table")
+        print(msg)
+        assert(false, "expected sys.save to successfully save small table")
+    end
 
     -- resave file (checking the atomic move works)
     data = { high_score = 1234, location = vmath.vector3(1,2,3), xp = 99, name = "Mr Player" }
-    result = sys.save(file, data)
-    assert(result)
+    print("Resaving file")
+    ret, msg = pcall(function() sys.save(file, data) end)
+    if not ret then
+        print(msg)
+        assert(false, "expected sys.save() on an existing file to work")
+    end
 
     -- reload saved file
-    local data_prim = sys.load(file)
-
+    print("Reloading file")
+    local data_prim
+    ret, msg = pcall(function() data_prim = sys.load(file) end)
+    if not ret then
+        print(msg)
+        assert(false, "expected sys.load() to work")
+    end
+    print("Asserting loaded values")
     assert(data['high_score'] == data_prim['high_score'])
     assert(data['location'] == data_prim['location'])
     assert(data['xp'] == data_prim['xp'])
     assert(data['name'] == data_prim['name'])
 
-    -- load file exceeding max buffer size, expected to fail
-    fh = io.open(file, "a+")
-    for i=1,(max_table_size/8) do fh:write("deadbeef") end
-    fh:close()
-    local ret, msg = pcall(function() sys.load(file, data) end)
-    if ret then assert(false, "expected lua error for sys.load with data table exceeding max size " .. max_table_size .. " bytes") end
 
     -- get_config
+    print("Testing get_config")
     assert(sys.get_config("main.does_not_exists") == nil)
     assert(sys.get_config("main.does_not_exists", "foobar") == "foobar")
     assert(sys.get_config("foo.value") == "123")
     assert(sys.get_config("foo.value", 456) == "123")
 
     -- load_resource
+    print("Load existing resource")
     local test_resource = sys.load_resource("/src/test/test_resource.txt")
     assert(test_resource == "defold")
 
+    print("Load missing resource")
     local does_not_exists = sys.load_resource("/does_not_exists")
     assert(does_not_exists == nil)
 
     -- get_sys_info
+    print("Getting sys_info")
     local info = sys.get_sys_info()
     print("device_model: " .. info.device_model)
     print("system_name: " .. info.system_name)
@@ -86,6 +115,7 @@ function test_sys()
     print("device_ident" .. info.device_ident)
 
     -- get_ifaddrs
+    print("Getting ifaddrs")
     local addresses = sys.get_ifaddrs()
     for i, a in ipairs(addresses) do
         local s = string.format("%d ip:%16s mac:%20s up:%s running:%s name:%s", i, tostring(a.address), tostring(a.mac), tostring(a.up), tostring(a.running), a.name)
@@ -93,6 +123,7 @@ function test_sys()
     end
 
     -- serialize/deserialize
+    print("Testing serialize and deserialize")
     data = { high_score = 1234, location = vmath.vector3(1,2,3), xp = 99, name = "Mr Player", [1] = "lala" }
     local serialized = sys.serialize(data)
     local deserialized = sys.deserialize(serialized)
@@ -100,6 +131,7 @@ function test_sys()
         assert(deserialized[k] == v)
     end
 
+    print("Done")
 end
 
 functions = { test_sys = test_sys }
