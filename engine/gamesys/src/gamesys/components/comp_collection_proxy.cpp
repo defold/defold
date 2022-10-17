cb7fe98611713db602d1577037f045283d3b6f15
Mathias Westerdahl <mwesterdahl76@gmail.com>
2022-10-17 10:41:31 +0200

    Fixed missing collection proxy documentation (#7065)
    * Fixed missing collection proxy documentation

--- engine/gamesys/src/gamesys/components/comp_collection_proxy.cpp
+++ engine/gamesys/src/gamesys/components/comp_collection_proxy.cpp
@@ -796,4 +796,77 @@ namespace dmGameSystem
      * @message
      * @name proxy_unloaded
      */
+
+    /*# return an indexed table of all the resources of a collection proxy
+     *
+     * return an indexed table of resources for a collection proxy. Each
+     * entry is a hexadecimal string that represents the data of the specific
+     * resource. This representation corresponds with the filename for each
+     * individual resource that is exported when you bundle an application with
+     * LiveUpdate functionality.
+     *
+     * @namespace collectionproxy
+     * @name collectionproxy.get_resources
+     * @param collectionproxy [type:url] the collectionproxy to check for resources.
+     * @return resources [type:table] the resources
+     *
+     * @examples
+     *
+     * ```lua
+     * local function print_resources(self, cproxy)
+     *     local resources = collectionproxy.get_resources(cproxy)
+     *     for _, v in ipairs(resources) do
+     *         print("Resource: " .. v)
+     *     end
+     * end
+     * ```
+     */
+
+    /*# return an array of missing resources for a collection proxy
+     *
+     * return an array of missing resources for a collection proxy. Each
+     * entry is a hexadecimal string that represents the data of the specific
+     * resource. This representation corresponds with the filename for each
+     * individual resource that is exported when you bundle an application with
+     * LiveUpdate functionality. It should be considered good practise to always
+     * check whether or not there are any missing resources in a collection proxy
+     * before attempting to load the collection proxy.
+     *
+     * @namespace collectionproxy
+     * @name collectionproxy.missing_resources
+     * @param collectionproxy [type:url] the collectionproxy to check for missing
+     * resources.
+     * @return resources [type:table] the missing resources
+     *
+     * @examples
+     *
+     * ```lua
+     * function init(self)
+     *     self.manifest = resource.get_current_manifest()
+     * end
+     *
+     * local function callback(self, id, response)
+     *     local expected = self.resources[id]
+     *     if response ~= nil and response.status == 200 then
+     *         print("Successfully downloaded resource: " .. expected)
+     *         resource.store_resource(response.response)
+     *     else
+     *         print("Failed to download resource: " .. expected)
+     *         -- error handling
+     *     end
+     * end
+     *
+     * local function download_resources(self, cproxy)
+     *     self.resources = {}
+     *     local resources = collectionproxy.missing_resources(cproxy)
+     *     for _, v in ipairs(resources) do
+     *         print("Downloading resource: " .. v)
+     *
+     *         local uri = "http://example.defold.com/" .. v
+     *         local id = http.request(uri, "GET", callback)
+     *         self.resources[id] = v
+     *     end
+     * end
+     * ```
+     */
 }
