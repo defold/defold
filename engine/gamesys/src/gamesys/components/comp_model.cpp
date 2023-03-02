d55e84319574d8f02807c72e2a12d51ba5ff2d79
JCash <mwesterdahl76@gmail.com>
2023-03-02 16:02:56 +0100

    Review fixes

--- engine/gamesys/src/gamesys/components/comp_model.cpp
+++ engine/gamesys/src/gamesys/components/comp_model.cpp
@@ -3,10 +3,10 @@
 // Copyright 2009-2014 Ragnar Svensson, Christian Murray
 // Licensed under the Defold License version 1.0 (the "License"); you may not use
 // this file except in compliance with the License.
-// 
+//
 // You may obtain a copy of the License, together with FAQs at
 // https://www.defold.com/license
-// 
+//
 // Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -410,9 +410,6 @@ namespace dmGameSystem
             item.m_AabbMax = item.m_Mesh->m_AabbMax;
             item.m_Enabled = 1;
             component->m_RenderItems.Push(item);
-
-            // printf("AABB: (%f, %f, %f)  (%f, %f, %f)\n", item.m_AabbMin.getX(), item.m_AabbMin.getY(), item.m_AabbMin.getZ(),
-            //         item.m_AabbMax.getX(), item.m_AabbMax.getY(), item.m_AabbMax.getZ());
         }
     }
 
