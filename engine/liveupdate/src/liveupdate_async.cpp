c48f523a8dc133e12af42a38cf78a1271a729090
JCash <mwesterdahl76@gmail.com>
2023-03-15 10:15:50 +0100

    Fixed TSan issues with live update async thread

--- engine/liveupdate/src/liveupdate_async.cpp
+++ engine/liveupdate/src/liveupdate_async.cpp
@@ -15,12 +15,14 @@
 #include "liveupdate.h"
 #include "liveupdate_private.h"
 
+#include <dlib/array.h>
+#include <dlib/atomic.h>
+#include <dlib/condition_variable.h>
+#include <dlib/mutex.h>
+#include <dlib/thread.h>
 #include <resource/resource.h>
 #include <resource/resource_archive.h>
-#include <dlib/thread.h>
-#include <dlib/mutex.h>
-#include <dlib/condition_variable.h>
-#include <dlib/array.h>
+
 #include <dmsdk/dlib/profile.h>
 
 
@@ -35,8 +37,8 @@ namespace dmLiveUpdate
     static dmThread::Thread m_AsyncThread = 0x0;
     static dmMutex::HMutex  m_ConsumerThreadMutex;
     static dmConditionVariable::HConditionVariable m_ConsumerThreadCondition;
-    static volatile bool m_ThreadJobComplete = false;
-    static volatile bool m_Active = false;
+    static int32_atomic_t m_ThreadJobComplete = 0;
+    static int32_atomic_t m_Active = 0;
 
     /// job input and output queues
     static dmArray<AsyncResourceRequest> m_JobQueue;
@@ -91,7 +93,7 @@ namespace dmLiveUpdate
 
         // Liveupdate async thread batch processing requested liveupdate tasks
         AsyncResourceRequest request;
-        while (m_Active)
+        while (dmAtomicGet32(&m_Active))
         {
             DM_PROFILE("Update");
             // Lock and sleep until signaled there is requests queued up
@@ -99,30 +101,30 @@ namespace dmLiveUpdate
                 dmMutex::ScopedLock lk(m_ConsumerThreadMutex);
                 while(m_ThreadJobQueue.Empty())
                     dmConditionVariable::Wait(m_ConsumerThreadCondition, m_ConsumerThreadMutex);
-                if((m_ThreadJobComplete) || (!m_Active))
+                if( dmAtomicGet32(&m_ThreadJobComplete) || !dmAtomicGet32(&m_Active))
                     continue;
                 request = m_ThreadJobQueue.Back();
                 m_ThreadJobQueue.Pop();
             }
             ProcessRequest(request);
-            m_ThreadJobComplete = true;
+            dmAtomicStore32(&m_ThreadJobComplete, 1);
         }
     }
 
     void AsyncUpdate()
     {
-        if(m_Active && ((m_ThreadJobComplete) || (!m_JobQueue.Empty())))
+        if(m_Active && (dmAtomicGet32(&m_ThreadJobComplete) || (!m_JobQueue.Empty())))
         {
             // Process any completed jobs, lock resource loadmutex as we will swap (update) archive containers archiveindex data
             dmMutex::ScopedLock lk(m_ConsumerThreadMutex);
-            if(m_ThreadJobComplete)
+            if(dmAtomicGet32(&m_ThreadJobComplete))
             {
                 dmMutex::HMutex mutex = dmResource::GetLoadMutex(m_ResourceFactory);
                 if(!dmMutex::TryLock(mutex))
                     return;
                 ProcessRequestComplete();
                 dmMutex::Unlock(mutex);
-                m_ThreadJobComplete = false;
+                dmAtomicStore32(&m_ThreadJobComplete, 0);
             }
             // Push any accumulated request queue batch to job queue
             if(!m_JobQueue.Empty())
@@ -161,8 +163,8 @@ namespace dmLiveUpdate
         m_ThreadJobQueue.SetSize(0);
         m_ConsumerThreadMutex = dmMutex::New();
         m_ConsumerThreadCondition = dmConditionVariable::New();
-        m_ThreadJobComplete = false;
-        m_Active = true;
+        dmAtomicStore32(&m_ThreadJobComplete, 0);
+        dmAtomicStore32(&m_Active, 1);
         m_AsyncThread = dmThread::New(AsyncThread, 0x80000, 0, "liveupdate");
     }
 
@@ -171,12 +173,13 @@ namespace dmLiveUpdate
         if(m_AsyncThread)
         {
             // When shutting down, discard any complete jobs as they are going to be mounted at boot time in any case
-            m_Active = false;
-            dmMutex::Lock(m_ConsumerThreadMutex);
-            m_JobQueue.SetSize(0);
-            m_ThreadJobQueue.SetSize(1);
-            dmConditionVariable::Signal(m_ConsumerThreadCondition);
-            dmMutex::Unlock(m_ConsumerThreadMutex);
+            dmAtomicStore32(&m_Active, 0);
+            {
+                DM_MUTEX_SCOPED_LOCK(m_ConsumerThreadMutex);
+                m_JobQueue.SetSize(0);
+                m_ThreadJobQueue.SetSize(1);
+                dmConditionVariable::Signal(m_ConsumerThreadCondition);
+            }
             dmThread::Join(m_AsyncThread);
             dmConditionVariable::Delete(m_ConsumerThreadCondition);
             dmMutex::Delete(m_ConsumerThreadMutex);
@@ -199,7 +202,7 @@ namespace dmLiveUpdate
     bool AddAsyncResourceRequest(AsyncResourceRequest& request)
     {
         // Add single job to job queue that will be batch pushed to thread worker in AsyncUpdate
-        if(!m_Active)
+        if(!dmAtomicGet32(&m_Active))
             return false;
         if(m_JobQueue.Full())
         {
@@ -212,12 +215,12 @@ namespace dmLiveUpdate
     void AsyncInitialize(const dmResource::HFactory factory)
     {
         m_ResourceFactory = factory;
-        m_Active = true;
+        dmAtomicStore32(&m_Active, 1);
     }
 
     void AsyncFinalize()
     {
-        m_Active = false;
+        dmAtomicStore32(&m_Active, 0);
         m_JobQueue.SetSize(0);
     }
 
