fccc521aa2107d86ceec59340b95ad56dc00faad
Alexey Gulev <me@agulev.com>
2023-04-19 16:23:05 +0200

    Added back gesture support for Android 13 (#7591)
    * add java callback
    
    * implement back button
    
    * add android manifest flag
    
    * fix issue with released action
    
    * make sure new back gesture works the same way as the back button before

--- engine/glfw/lib/android/android_window.c
+++ engine/glfw/lib/android/android_window.c
@@ -71,6 +71,12 @@ JNIEXPORT void JNICALL Java_com_dynamo_android_DefoldActivity_FakeEnter(JNIEnv* 
     _glfwInputKey( GLFW_KEY_ENTER, GLFW_PRESS );
 }
 
+JNIEXPORT void JNICALL Java_com_dynamo_android_DefoldActivity_glfwInputBackButton(JNIEnv* env, jobject obj)
+{
+    g_SpecialKeyActive = 10;
+    _glfwInputKey( GLFW_KEY_BACK, GLFW_PRESS );
+}
+
 JNIEXPORT void JNICALL Java_com_dynamo_android_DefoldActivity_glfwInputCharNative(JNIEnv* env, jobject obj, jint unicode)
 {
     struct Command cmd;
@@ -448,6 +454,7 @@ void _glfwPlatformPollEvents( void )
        if (g_SpecialKeyActive == 0) {
            _glfwInputKey( GLFW_KEY_BACKSPACE, GLFW_RELEASE );
            _glfwInputKey( GLFW_KEY_ENTER, GLFW_RELEASE );
+           _glfwInputKey( GLFW_KEY_BACK, GLFW_RELEASE );
        }
     }
 
