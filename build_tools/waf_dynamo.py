46a16002d38ccf9dc0abf34dfe84c6642a87c82d
Mathias Westerdahl <mwesterdahl76@gmail.com>
2023-04-24 16:16:04 +0200

    Updated MoltenVK to v1.2.3 db8512a6 (#7373)
    * Updated to version MoltenVK 1.2.3 db8512a6
    
    * Updated libraries with static linkage
    
    * Update macos packages for MVK
    
    * Reverted to MoltenVK 1.0.41 for iOS
    
    ---------
    
    Co-authored-by: Jhonny Göransson <jhonny.goransson@gmail.com>

--- build_tools/waf_dynamo.py
+++ build_tools/waf_dynamo.py
@@ -3,10 +3,10 @@
 # Copyright 2009-2014 Ragnar Svensson, Christian Murray
 # Licensed under the Defold License version 1.0 (the "License"); you may not use
 # this file except in compliance with the License.
-# 
+#
 # You may obtain a copy of the License, together with FAQs at
 # https://www.defold.com/license
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed
 # under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -1739,7 +1739,7 @@ def detect(conf):
         conf.env['FRAMEWORK_DMGLFW'] = ['QuartzCore']
     elif platform in ('arm64-ios','x86_64-ios'):
         conf.env['STLIB_VULKAN'] = 'MoltenVK'
-        conf.env['FRAMEWORK_VULKAN'] = 'Metal'
+        conf.env['FRAMEWORK_VULKAN'] = ['Metal', 'IOSurface']
         conf.env['FRAMEWORK_DMGLFW'] = ['QuartzCore', 'OpenGLES', 'CoreVideo', 'CoreGraphics']
     elif platform in ('x86_64-linux',):
         conf.env['SHLIB_VULKAN'] = ['vulkan', 'X11-xcb']
