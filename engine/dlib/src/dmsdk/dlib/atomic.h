4f8ad66b8d4707a7ae3505cc7f9caee0a568532a
JCash <mwesterdahl76@gmail.com>
2023-05-16 17:13:05 +0200

    Added documentation to atomic.h

--- engine/dlib/src/dmsdk/dlib/atomic.h
+++ engine/dlib/src/dmsdk/dlib/atomic.h
@@ -16,9 +16,13 @@
 #ifndef DMSDK_ATOMIC_H
 #define DMSDK_ATOMIC_H
 
-/*
- * 64-bit notes
- *  - On win32 use Interlocked*64
+/*# Atomic functions
+ * Atomic functions
+ *
+ * @document
+ * @name Atomic
+ * @namespace dmAtomic
+ * @path engine/dlib/src/dmsdk/dlib/atomic.h
  */
 
 // platform independent includes
@@ -29,102 +33,114 @@
 #include <dmsdk/dlib/safe_windows.h>
 #endif
 
-//! Type definitions of atomic types
+/*# 32 bit signed integer atomic
+ * @typedef
+ * @name int32_atomic_t
+ */
 typedef volatile int32_t int32_atomic_t;
 
-/**
- * Atomic increment of a int32_atomic_t.
- * @param ptr Pointer to a int32_atomic_t to increment.
- * @return Previous value
+/*#
+ * Atomic increment of an int32_atomic_t.
+ * @name dmAtomicIncrement32
+ * @param ptr [type: int32_atomic_t*] Pointer to an int32_atomic_t to increment.
+ * @return prev [type: int32_t] Previous value
  */
 inline int32_t dmAtomicIncrement32(int32_atomic_t* ptr)
 {
 #if defined(_MSC_VER)
-	return InterlockedIncrement((volatile long*) ptr)-1;
+    return InterlockedIncrement((volatile long*) ptr)-1;
 #else
-	return __sync_fetch_and_add((int32_atomic_t*) ptr, 1);
+    return __sync_fetch_and_add((int32_atomic_t*) ptr, 1);
 #endif
 }
 
-/**
- * Atomic decrement of a int32_atomic_t.
- * @param ptr Pointer to a int32_atomic_t to decrement.
- * @return Previous value
+/*#
+ * Atomic decrement of an int32_atomic_t.
+ * @name dmAtomicDecrement32
+ * @param ptr [type: int32_atomic_t*] Pointer to an int32_atomic_t to decrement.
+ * @return prev [type: int32_t] Previous value
  */
 inline int32_t dmAtomicDecrement32(int32_atomic_t* ptr)
 {
 #if defined(_MSC_VER)
-	return InterlockedDecrement((volatile long*) ptr)+1;
+    return InterlockedDecrement((volatile long*) ptr)+1;
 #else
-	return __sync_fetch_and_sub((int32_atomic_t*) ptr, 1);
+    return __sync_fetch_and_sub((int32_atomic_t*) ptr, 1);
 #endif
 }
 
-/**
- * Atomic addition of a int32_atomic_t.
- * @param ptr Pointer to a int32_atomic_t to add.
- * @param value Value to add.
- * @return Previous value
+
+/*#
+ * Atomic addition of an int32_atomic_t.
+ * @name dmAtomicAdd32
+ * @param ptr [type: int32_atomic_t*] Pointer to an int32_atomic_t to add to.
+ * @param value [type: int32_t] Value to add.
+ * @return prev [type: int32_t] Previous value
  */
 inline int32_t dmAtomicAdd32(int32_atomic_t *ptr, int32_t value)
 {
 #if defined(_MSC_VER)
-	return InterlockedExchangeAdd((volatile long*) ptr, (long) value);
+    return InterlockedExchangeAdd((volatile long*) ptr, (long) value);
 #else
-	return __sync_fetch_and_add((int32_atomic_t*) ptr, (long) value);
+    return __sync_fetch_and_add((int32_atomic_t*) ptr, (long) value);
 #endif
 }
 
-/**
- * Atomic subtraction of a int32_atomic_t.
- * @param ptr Pointer to a int32_atomic_t to subtract.
- * @param value Value to subtract.
- * @return Previous value
+/*#
+ * Atomic subtraction of an int32_atomic_t.
+ * @name dmAtomicSub32
+ * @param ptr [type: int32_atomic_t*] Pointer to an int32_atomic_t to subtract from.
+ * @param value [type: int32_t] Value to subtract.
+ * @return prev [type: int32_t] Previous value
  */
 inline int32_t dmAtomicSub32(int32_atomic_t *ptr, int32_t value)
 {
 #if defined(_MSC_VER)
-	return InterlockedExchangeAdd((volatile long*) ptr, -((long)value));
+    return InterlockedExchangeAdd((volatile long*) ptr, -((long)value));
 #else
-	return __sync_fetch_and_sub((int32_atomic_t*) ptr, (long) value);
+    return __sync_fetch_and_sub((int32_atomic_t*) ptr, (long) value);
 #endif
 }
 
-/**
- * Atomic exchange of a int32_atomic_t.
- * @param ptr Pointer to a int32_atomic_t to store into.
- * @param value Value to store.
- * @return Previous value.
+/*#
+ * Atomic set (or exchange) of an int32_atomic_t.
+ * @name dmAtomicStore32
+ * @param ptr [type: int32_atomic_t*] Pointer to an int32_atomic_t to store into.
+ * @param value [type: int32_t] Value to set.
+ * @return prev [type: int32_t] Previous value
  */
 inline int32_t dmAtomicStore32(int32_atomic_t *ptr, int32_t value)
 {
 #if defined(_MSC_VER)
-	return InterlockedExchange((volatile long*) ptr, (long) value);
+    return InterlockedExchange((volatile long*) ptr, (long) value);
 #else
-	return __sync_lock_test_and_set((int32_atomic_t*) ptr, value);
+    return __sync_lock_test_and_set((int32_atomic_t*) ptr, value);
 #endif
 }
 
-/**
- * Atomic exchange of a int32_atomic_t if comparand is equal to the value of #ptr
- * @param ptr Pointer to a int32_atomic_t to store into.
- * @param value Value to store.
- * @param comparand Value to compare to.
- * @return Previous value
+/*#
+ * Atomic set (or exchange) of an int32_atomic_t if comparand is equal to the value of ptr.
+ * @name dmAtomicCompareStore32
+ * @param ptr [type: int32_atomic_t*] Pointer to an int32_atomic_t to store into.
+ * @param value [type: int32_t] Value to set.
+ * @param comparand [type: int32_t] Value to compare to.
+ * @return prev [type: int32_t] Previous value
  */
 inline int32_t dmAtomicCompareStore32(int32_atomic_t *ptr, int32_t value, int32_t comparand)
 {
 #if defined(_MSC_VER)
-	return InterlockedCompareExchange((volatile long*) ptr, (long) value, (long) comparand);
+    return InterlockedCompareExchange((volatile long*) ptr, (long) value, (long) comparand);
 #else
-	return __sync_val_compare_and_swap((int32_atomic_t*) ptr, comparand, value);
+    return __sync_val_compare_and_swap((int32_atomic_t*) ptr, comparand, value);
 #endif
 }
 
-/**
- * Retrieves the current value by adding 0
- * @param ptr Pointer to a int32_atomic_t to store into.
- * @return Previous value.
+/*#
+ * Atomic getof an int32_atomic_t
+ * @name dmAtomicGet32
+ * @param ptr [type: int32_atomic_t*] Pointer to an int32_atomic_t to get from.
+ * @return value [type: int32_t] Current value
+ * @note Retrieves the current value by adding 0
  */
 inline int32_t dmAtomicGet32(int32_atomic_t *ptr)
 {
