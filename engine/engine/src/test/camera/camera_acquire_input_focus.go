535be685d6b279750fc64ecde1e48c179759f3ba
Jhonny Göransson <jhonny.goransson@gmail.com>
2023-07-16 16:43:22 +0200

    Add camera focus tests

---
+++ engine/engine/src/test/camera/camera_acquire_input_focus.go
@@ -0,0 +1,25 @@
+components {
+  id: "script"
+  component: "/camera/camera_acquire_input_focus.script"
+}
+
+embedded_components {
+  id: "camera"
+  type: "camera"
+  data: "aspect_ratio: 1.0\n"
+  "fov: 0.0\n"
+  "near_z: 0.0\n"
+  "far_z: 0.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
