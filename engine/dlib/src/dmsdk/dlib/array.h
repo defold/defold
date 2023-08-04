ff94ba2cb76e37f176fa2834b1b89952606fbe1d
JCash <mwesterdahl76@gmail.com>
2023-08-04 14:22:21 +0200

    Added Set(c_array, size) function to dmArray

--- engine/dlib/src/dmsdk/dlib/array.h
+++ engine/dlib/src/dmsdk/dlib/array.h
@@ -3,10 +3,10 @@
 // Copyright 2009-2014 Ragnar Svensson, Christian Murray
 // Licensed under the Defold License version 1.0 (the "License"); you may not use
 // this file except in compliance with the License.
-// 
+//
 // You may obtain a copy of the License, together with FAQs at
 // https://www.defold.com/license
-// 
+//
 // Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -282,6 +282,17 @@ public:
      */
     void SetSize(uint32_t size);
 
+    /*# Set user-allocated memory
+     *
+     * user-allocated array with initial size and capacity
+     *
+     * @name dmArray
+     * @param user_array [type:T*] User-allocated array to be used as storage.
+     * @param size [type:uint32_t] Initial size
+     * @param capacity [type:uint32_t] Initial capacity
+     */
+    void Set(T* user_array, uint32_t size, uint32_t capacity);
+
     /*# array eraseswap
      *
      * Remove the element at the specified index.
@@ -374,12 +385,7 @@ dmArray<T>::dmArray()
 template <typename T>
 dmArray<T>::dmArray(T *user_array, uint32_t size, uint32_t capacity)
 {
-    assert(user_array != 0);
-    assert(size  <= capacity);
-    m_Front = user_array;
-    m_End = user_array + size;
-    m_Back = user_array + capacity;
-    m_UserAllocated = 1;
+    Set(user_array, size, capacity);
 }
 
 template <typename T>
@@ -505,6 +511,22 @@ void dmArray<T>::SetSize(uint32_t size)
 {
     assert(size <= Capacity());
     m_End = m_Front + size;
+}
+
+template <typename T>
+void dmArray<T>::Set(T* user_array, uint32_t size, uint32_t capacity)
+{
+    assert(user_array != 0);
+    assert(size  <= capacity);
+
+    if (!m_UserAllocated && m_Front)
+    {
+        delete[] (uint8_t*) m_Front;
+    }
+    m_Front = user_array;
+    m_End = user_array + size;
+    m_Back = user_array + capacity;
+    m_UserAllocated = 1;
 }
 
 template <typename T>
