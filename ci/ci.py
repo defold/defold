f741c17fe639713f3237fe4f51353b31de6812f5
Björn Ritzl <bjorn.ritzl@gmail.com>
2023-08-18 15:01:20 +0200

    Update ci.py

--- ci/ci.py
+++ ci/ci.py
@@ -139,7 +139,8 @@ def setup_gcloud(args):
     call("echo 'deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main' | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list")
 
     # Import the Google Cloud public key.
-    call("curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.gpg")
+    # call("curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.gpg")
+    call("curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg")
 
     # Update and install the gcloud CLI
     call("sudo apt-get update", failonerror=False)
