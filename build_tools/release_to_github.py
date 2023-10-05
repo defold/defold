a20dcfafed526967d7a8fc0aea4d607f17954d4a
colorizer <jebin12raj@gmail.com>
2023-10-06 01:14:59 +0530

    added support for tar gz in linux

--- build_tools/release_to_github.py
+++ build_tools/release_to_github.py
@@ -145,7 +145,7 @@ def release(config, tag_name, release_sha, s3_release, release_name=None, body=N
     def is_editor_file(path):
         return os.path.basename(path) in ('Defold-arm64-macos.dmg',
                                           'Defold-x86_64-macos.dmg',
-                                          'Defold-x86_64-linux.zip',
+                                          'Defold-x86_64-linux.tar.gz',
                                           'Defold-x86_64-win32.zip')
 
     filenames = ['bob.jar', 'ref-doc.zip']
