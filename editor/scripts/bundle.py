adf448d5983af9e50071a09a03d48e47eae1e065
colorizer <jebin12raj@gmail.com>
2023-10-06 22:16:39 +0530

    Revert "corrected fmt for tar gz in archive"
    This reverts commit c74fccb3af55bbc900aaacea2f2d514be4ca658c.

--- editor/scripts/bundle.py
+++ editor/scripts/bundle.py
@@ -119,7 +119,7 @@ def archivetree(path, outfile, directory = None, fmt= 'zip'):
     # Directory is similar to -C in tar
     if fmt=='zip':
         archive = zipfile.ZipFile(outfile, 'w')
-    elif fmt=='tar.gz':
+    elif fmt=='gz':
         archive = tarfile.open(outfile, 'w:gz')
     else:
         assert False, "Unknown format %s" % fmt
@@ -130,7 +130,7 @@ def archivetree(path, outfile, directory = None, fmt= 'zip'):
             an = p
             if directory:
                 an = os.path.relpath(p, directory)
-            if fmt=='tar.gz':
+            if fmt=='gz':
                 archive.add(p, an)
             else:    
                 archive.write(p, an)
