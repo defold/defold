4ae4e04d88d4408c0964068b56b81141b4041aca
Alexey Gulev <me@agulev.com>
2023-10-11 17:08:50 +0200

    Fix issue with window focus on macOS Sonoma (14.0) (#8134)
    * fix warning
    
    * fix focus issue on Sonoma
    
    * fix bundled/unbundled issues

--- engine/glfw/lib/cocoa/cocoa_window.m
+++ engine/glfw/lib/cocoa/cocoa_window.m
@@ -132,12 +132,37 @@
     // Wait for the first update to make window inactive and then activate it again
     // It helps to avoid issue when the window opens inactive
     // https://github.com/defold/defold/issues/6709
-    if( !_glfwLibrary.Unbundled ) {
-        ProcessSerialNumber psn = { 0, kCurrentProcess };
-        TransformProcessType( &psn, kProcessTransformToBackgroundApplication );
-        [self performSelector:@selector(activateProcess) withObject:nil afterDelay:0.1];
-        _glfwLibrary.Unbundled = GL_TRUE;
+    if( _glfwLibrary.FirstUpdate )
+    {
+        // Starting from macOS Sonoma (14.0) it should be solved in two different ways depends 
+        // if it's bundled app or not
+        // https://github.com/defold/defold/issues/8066
+        NSOperatingSystemVersion version = [[NSProcessInfo processInfo] operatingSystemVersion];
+        if (version.majorVersion < 14)
+        {
+            [self deactivateProcess];
+        }
+        else
+        {
+            if ( _glfwLibrary.Unbundled )
+            {
+                [self deactivateProcess];
+            }
+            else
+            {
+                [self activateWindow];
+            }
+        }
+
+        _glfwLibrary.FirstUpdate = GL_FALSE;
     }
+}
+
+- (void)deactivateProcess
+{
+    ProcessSerialNumber psn = { 0, kCurrentProcess };
+    TransformProcessType( &psn, kProcessTransformToBackgroundApplication );
+    [self performSelector:@selector(activateProcess) withObject:nil afterDelay:0.1];
 }
 
 - (void)activateProcess
@@ -150,7 +175,19 @@
 - (void)activateWindow
 {
     [_glfwWin.window makeKeyAndOrderFront:nil];
+    // Starting from macOS Sonoma (14.0) an extra step needed
+    NSOperatingSystemVersion version = [[NSProcessInfo processInfo] operatingSystemVersion];
+    if (version.majorVersion >= 14) {
+        [NSApp activateIgnoringOtherApps:YES];
+    }
     [[NSRunningApplication currentApplication] activateWithOptions:(NSApplicationActivateAllWindows | NSApplicationActivateIgnoringOtherApps)];
+}
+
+// macos 12+
+// https://sector7.computest.nl/post/2022-08-process-injection-breaking-all-macos-security-layers-with-a-single-vulnerability/
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app
+{
+    return YES;
 }
 
 @end
@@ -646,6 +683,7 @@ int  _glfwPlatformOpenWindow( int width, int height,
     _glfwWin.context = nil;
     _glfwWin.aux_context = nil;
     _glfwWin.delegate = nil;
+    _glfwLibrary.FirstUpdate = GL_TRUE;
 
     _glfwWin.swapInterval = 1;
     _glfwWin.countDown = 1;
@@ -933,8 +971,6 @@ void _glfwPlatformCloseWindow( void )
 
     [_glfwWin.window close];
     _glfwWin.window = nil;
-
-    _glfwLibrary.Unbundled = 0;
 
     // TODO: Probably more cleanup
 }
