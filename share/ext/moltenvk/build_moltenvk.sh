fb6711705ad06ef683f3c563cbcc078eba76c360
Jhonny Göransson <jhonny.goransson@gmail.com>
2023-10-12 16:56:56 +0200

    Update the helper scripts

--- share/ext/moltenvk/build_moltenvk.sh
+++ share/ext/moltenvk/build_moltenvk.sh
@@ -13,8 +13,6 @@
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-which lipo
-
 . ../common.sh
 
 BUILD_DIR=build
