6bab22bb9541952aa12eda4c58b1422c0845dc3d
Jhonny Göransson <jhonny.goransson@gmail.com>
2023-11-14 16:12:23 +0100

    Indentation fix

--- engine/gamesys/src/gamesys/resources/res_compute_program.cpp
+++ engine/gamesys/src/gamesys/resources/res_compute_program.cpp
@@ -5,7 +5,7 @@
 
 namespace dmGameSystem
 {
-	dmResource::Result AcquireResources(dmResource::HFactory factory, dmRenderDDF::ComputeProgramDesc* ddf, void** program)
+    dmResource::Result AcquireResources(dmResource::HFactory factory, dmRenderDDF::ComputeProgramDesc* ddf, void** program)
     {
         dmResource::Result factory_e;
         factory_e = dmResource::Get(factory, ddf->m_Program, program);
@@ -19,14 +19,14 @@ namespace dmGameSystem
 
     dmResource::Result ResComputeProgramCreate(const dmResource::ResourceCreateParams& params)
     {
-    	dmRender::HRenderContext render_context = (dmRender::HRenderContext) params.m_Context;
+        dmRender::HRenderContext render_context = (dmRender::HRenderContext) params.m_Context;
         dmRenderDDF::ComputeProgramDesc* ddf   = (dmRenderDDF::ComputeProgramDesc*)params.m_PreloadData;
 
         void* shader;
         dmResource::Result r = AcquireResources(params.m_Factory, ddf, &shader);
         if (r == dmResource::RESULT_OK)
         {
-        	dmRender::HComputeProgram program = dmRender::NewComputeProgram(render_context, (dmGraphics::HComputeProgram) shader);
+            dmRender::HComputeProgram program = dmRender::NewComputeProgram(render_context, (dmGraphics::HComputeProgram) shader);
 
             dmResource::SResourceDescriptor desc;
             dmResource::Result factory_e;
@@ -49,17 +49,17 @@ namespace dmGameSystem
         dmResource::Release(params.m_Factory, (void*) dmRender::GetComputeProgramShader(program));
         dmRender::DeleteComputeProgram(render_context, program);
 
-    	return dmResource::RESULT_OK;
+        return dmResource::RESULT_OK;
     }
 
     dmResource::Result ResComputeProgramRecreate(const dmResource::ResourceRecreateParams& params)
     {
-    	return dmResource::RESULT_OK;
+        return dmResource::RESULT_OK;
     }
 
     dmResource::Result ResComputeProgramPreload(const dmResource::ResourcePreloadParams& params)
     {
-    	dmRenderDDF::ComputeProgramDesc* ddf;
+        dmRenderDDF::ComputeProgramDesc* ddf;
         dmDDF::Result e = dmDDF::LoadMessage<dmRenderDDF::ComputeProgramDesc>(params.m_Buffer, params.m_BufferSize, &ddf);
         if (e != dmDDF::RESULT_OK)
         {
