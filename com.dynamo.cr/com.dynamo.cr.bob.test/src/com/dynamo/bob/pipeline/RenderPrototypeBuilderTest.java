c886a126d54d7a4cb40dd41f712a4a01426ca663
Jhonny Göransson <jhonny.goransson@gmail.com>
2024-01-19 15:50:21 +0100

    Fix bob tests

--- com.dynamo.cr/com.dynamo.cr.bob.test/src/com/dynamo/bob/pipeline/RenderPrototypeBuilderTest.java
+++ com.dynamo.cr/com.dynamo.cr.bob.test/src/com/dynamo/bob/pipeline/RenderPrototypeBuilderTest.java
@@ -21,6 +21,7 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.dynamo.bob.CompileExceptionError;
 import com.google.protobuf.Message;
 import com.dynamo.render.proto.Render.RenderPrototypeDesc;
 
@@ -33,9 +34,9 @@ public class RenderPrototypeBuilderTest extends AbstractProtoBuilderTest {
 
     @Test
     public void testDataMigration() throws Exception {
-    	addFile("/test.render_script", "");
+        addFile("/test.render_script", "");
 
-    	addFile("/test.material", "");
+        addFile("/test.material", "");
         addFile("/test.vp", "");
         addFile("/test.fp", "");
 
@@ -48,21 +49,72 @@ public class RenderPrototypeBuilderTest extends AbstractProtoBuilderTest {
         materialSrc.append("fragment_program: \"/test.fp\"\n");
         build("/test.material", materialSrc.toString());
 
-    	final String srcOneMaterial =
-			"script: \"/test.render_script\"\n" +
-			"materials {\n" +
-			"	name: \"test\"\n" +
-			"	material: \"/test.material\"\n" +
-			"}\n";
+        {
+            final String srcOneMaterial =
+                "script: \"/test.render_script\"\n" +
+                "materials {\n" +
+                "   name: \"test\"\n" +
+                "   material: \"/test.material\"\n" +
+                "}\n";
 
-        List<Message> outputs = build("/test.render", srcOneMaterial);
-        RenderPrototypeDesc output = (RenderPrototypeDesc) outputs.get(0);
+            List<Message> outputs = build("/test.render", srcOneMaterial);
+            RenderPrototypeDesc output = (RenderPrototypeDesc) outputs.get(0);
 
-        assertEquals(0, output.getMaterialsList().size());
-        assertEquals(0, output.getRenderResourcesList().size());
+            assertEquals(0, output.getMaterialsList().size());
+            assertEquals(1, output.getRenderResourcesList().size());
 
-        RenderPrototypeDesc.RenderResourceDesc res_desc = output.getRenderResourcesList().get(0);
-        assertTrue(res_desc.getName().equals("test"));
-        assertTrue(res_desc.getPath().equals("/test.materialc"));
+            RenderPrototypeDesc.RenderResourceDesc res_desc = output.getRenderResourcesList().get(0);
+            assertTrue(res_desc.getName().equals("test"));
+            assertTrue(res_desc.getPath().equals("/test.materialc"));
+        }
+
+        {
+            final String srcOneMaterialOneRenderResource =
+                "script: \"/test.render_script\"\n" +
+                "materials {\n" +
+                "   name: \"test\"\n" +
+                "   material: \"/test.material\"\n" +
+                "}\n" +
+                "render_resources {\n" +
+                "   name: \"test_2\"\n" +
+                "   path: \"/test.material\"\n" +
+                "}\n";
+
+            List<Message> outputs = build("/test2.render", srcOneMaterialOneRenderResource);
+            RenderPrototypeDesc output = (RenderPrototypeDesc) outputs.get(0);
+
+            assertEquals(0, output.getMaterialsList().size());
+            assertEquals(2, output.getRenderResourcesList().size());
+
+            RenderPrototypeDesc.RenderResourceDesc res_desc_1 = output.getRenderResourcesList().get(0);
+            assertTrue(res_desc_1.getName().equals("test"));
+            assertTrue(res_desc_1.getPath().equals("/test.materialc"));
+
+            RenderPrototypeDesc.RenderResourceDesc res_desc_2 = output.getRenderResourcesList().get(1);
+            assertTrue(res_desc_2.getName().equals("test_2"));
+            assertTrue(res_desc_2.getPath().equals("/test.materialc"));
+        }
+
+        {
+            final String srcDuplicateResourceNames =
+                "script: \"/test.render_script\"\n" +
+                "materials {\n" +
+                "   name: \"test\"\n" +
+                "   material: \"/test.material\"\n" +
+                "}\n" +
+                "render_resources {\n" +
+                "   name: \"test\"\n" +
+                "   path: \"/test.material\"\n" +
+                "}\n";
+
+            boolean didFail = false;
+            try {
+                List<Message> outputs = build("/test3.render", srcDuplicateResourceNames);
+            } catch (CompileExceptionError e) {
+                didFail = true;
+                assertTrue(e.getMessage().contains("contain one or more entries with duplicate names"));
+            }
+            assertTrue(didFail);
+        }
     }
 }
