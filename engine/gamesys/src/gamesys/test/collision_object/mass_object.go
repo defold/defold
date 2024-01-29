4fffa21dd4ac6d57cbf67d650879ff9d275a5398
Alexey Gulev <me@agulev.com>
2024-01-29 08:37:17 +0100

    New function `physics.update_mass(url, mass)` (#8456)
    * initial commit
    
    * add test
    
    * review fix
    
    * fix null implementation
    
    * make variable names consistent

---
+++ engine/gamesys/src/gamesys/test/collision_object/mass_object.go
@@ -0,0 +1,30 @@
+components {
+  id: "script"
+  component: "/collision_object/mass.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "mass_object"
+  component: "/collision_object/mass_object.collisionobject"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- engine/physics/src/physics/physics_2d.cpp
+++ engine/physics/src/physics/physics_2d.cpp
@@ -1234,56 +1234,84 @@ namespace dmPhysics
     }
 
     void SetGroup2D(HCollisionObject2D collision_object, uint16_t groupbit) {
-		b2Fixture* fixture = ((b2Body*)collision_object)->GetFixtureList();
-		while (fixture) {
-			// do sth with the fixture
-			if (fixture->GetType() != b2Shape::e_grid) {
-				b2Filter filter = fixture->GetFilterData(0); // all non-grid shapes have only one filter item indexed at position 0
-				filter.categoryBits = groupbit;
-				fixture->SetFilterData(filter, 0);
-			}
-			fixture = fixture->GetNext();	// NOTE: No guard condition in loop. Assumes proper state of Box2D fixture list.
-		}
-	}
+        b2Fixture* fixture = ((b2Body*)collision_object)->GetFixtureList();
+        while (fixture) {
+            // do sth with the fixture
+            if (fixture->GetType() != b2Shape::e_grid) {
+                b2Filter filter = fixture->GetFilterData(0); // all non-grid shapes have only one filter item indexed at position 0
+                filter.categoryBits = groupbit;
+                fixture->SetFilterData(filter, 0);
+            }
+            fixture = fixture->GetNext();   // NOTE: No guard condition in loop. Assumes proper state of Box2D fixture list.
+        }
+    }
 
-	uint16_t GetGroup2D(HCollisionObject2D collision_object) {
-		b2Fixture* fixture = ((b2Body*)collision_object)->GetFixtureList();
-		if (fixture) {
-			if (fixture->GetType() != b2Shape::e_grid) {
-				b2Filter filter = fixture->GetFilterData(0);
-				return filter.categoryBits;
-			}
-		}
-		return 0;
-	}
+    uint16_t GetGroup2D(HCollisionObject2D collision_object) {
+        b2Fixture* fixture = ((b2Body*)collision_object)->GetFixtureList();
+        if (fixture) {
+            if (fixture->GetType() != b2Shape::e_grid) {
+                b2Filter filter = fixture->GetFilterData(0);
+                return filter.categoryBits;
+            }
+        }
+        return 0;
+    }
 
-	// updates a specific group bit of a collision object's current mask
-	void SetMaskBit2D(HCollisionObject2D collision_object, uint16_t groupbit, bool boolvalue) {
-		b2Fixture* fixture = ((b2Body*)collision_object)->GetFixtureList();
-		while (fixture) {
-			// do sth with the fixture
-			if (fixture->GetType() != b2Shape::e_grid) {
-				b2Filter filter = fixture->GetFilterData(0); // all non-grid shapes have only one filter item indexed at position 0
-				if (boolvalue)
-					filter.maskBits |= groupbit;
-				else
-					filter.maskBits &= ~groupbit;
-				fixture->SetFilterData(filter, 0);
-			}
-			fixture = fixture->GetNext();
-		}
-	}
+    // updates a specific group bit of a collision object's current mask
+    void SetMaskBit2D(HCollisionObject2D collision_object, uint16_t groupbit, bool boolvalue) {
+        b2Fixture* fixture = ((b2Body*)collision_object)->GetFixtureList();
+        while (fixture) {
+            // do sth with the fixture
+            if (fixture->GetType() != b2Shape::e_grid) {
+                b2Filter filter = fixture->GetFilterData(0); // all non-grid shapes have only one filter item indexed at position 0
+                if (boolvalue)
+                    filter.maskBits |= groupbit;
+                else
+                    filter.maskBits &= ~groupbit;
+                fixture->SetFilterData(filter, 0);
+            }
+            fixture = fixture->GetNext();
+        }
+    }
 
-	bool GetMaskBit2D(HCollisionObject2D collision_object, uint16_t groupbit) {
-		b2Fixture* fixture = ((b2Body*)collision_object)->GetFixtureList();
-		if (fixture) {
-			if (fixture->GetType() != b2Shape::e_grid) {
-				b2Filter filter = fixture->GetFilterData(0);
-				return !!(filter.maskBits & groupbit);
-			}
-		}
-		return false;
-	}
+    bool UpdateMass2D(HCollisionObject2D collision_object, float mass) {
+        b2Body* body = (b2Body*)collision_object;
+        if (body->GetType() != b2_dynamicBody) {
+            return false;
+        }
+        b2Fixture* fixture = body->GetFixtureList();
+        float total_area = 0.0f;
+        while (fixture) {
+            b2MassData mass_data;
+            fixture->GetShape()->ComputeMass(&mass_data, 1.0f);
+            // Since density is 1.0, massData.mass represents the area.
+            total_area += mass_data.mass;
+            fixture = fixture->GetNext();
+
+        }
+        fixture = body->GetFixtureList();
+        if (total_area <= 0.0f) {
+            return false;
+        }
+        float new_density = mass / total_area;
+        while (fixture) {
+            fixture->SetDensity(new_density);
+            fixture = fixture->GetNext();
+        }
+        body->ResetMassData();
+        return true;
+    }
+
+    bool GetMaskBit2D(HCollisionObject2D collision_object, uint16_t groupbit) {
+        b2Fixture* fixture = ((b2Body*)collision_object)->GetFixtureList();
+        if (fixture) {
+            if (fixture->GetType() != b2Shape::e_grid) {
+                b2Filter filter = fixture->GetFilterData(0);
+                return !!(filter.maskBits & groupbit);
+            }
+        }
+        return false;
+    }
 
     void RequestRayCast2D(HWorld2D world, const RayCastRequest& request)
     {
