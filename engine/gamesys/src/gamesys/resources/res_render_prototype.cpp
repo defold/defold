b5b4a5993a6559fb40b26c753aa1bdcac9c91e83
Jhonny Göransson <jhonny.goransson@gmail.com>
2024-02-12 11:43:27 +0100

    Pass HMaterial handle and not resource handle to render prototype

--- engine/gamesys/src/gamesys/resources/res_render_prototype.cpp
+++ engine/gamesys/src/gamesys/resources/res_render_prototype.cpp
@@ -90,6 +90,8 @@ namespace dmGameSystem
                         break;
                     }
 
+                    MaterialResource* mat = (MaterialResource*) res;
+
                     prototype->m_RenderResources.Push(res);
 
                     RenderResourceMeta meta;
@@ -116,7 +118,8 @@ namespace dmGameSystem
                         } break;
                         case dmRender::RENDER_RESOURCE_TYPE_MATERIAL:
                         {
-                            render_resource_val = (uint64_t) render_resource;
+                            dmGameSystem::MaterialResource* material_res = (dmGameSystem::MaterialResource*) render_resource;
+                            render_resource_val = (uint64_t) material_res->m_Material;
                         } break;
                     }
 
