fe9a5cc0c1968b7ce61116b10e2adf99915d1704
Mats Gisselson <mats.gisselson@gmail.com>
2024-02-21 17:44:56 +0100

    Strip defaults from .render_target files

--- editor/src/clj/editor/render_target.clj
+++ editor/src/clj/editor/render_target.clj
@@ -23,7 +23,7 @@
             [editor.validation :as validation]
             [editor.workspace :as workspace])
   (:import [com.dynamo.graphics.proto Graphics$TextureImage$TextureFormat]
-           [com.dynamo.render.proto RenderTarget$RenderTargetDesc]))
+           [com.dynamo.render.proto RenderTarget$RenderTargetDesc RenderTarget$RenderTargetDesc$ColorAttachment RenderTarget$RenderTargetDesc$DepthStencilAttachment]))
 
 (def ^:const texture-icon "icons/32/Icons_36-Texture.png")
 
@@ -65,6 +65,7 @@
               {:path [:depth-stencil-attachment-texture-storage]
                :label "Depth Texture Storage"
                :type :boolean}]}]})
+
 (g/defnk produce-form-data [_node-id color-attachments depth-stencil-attachment-width depth-stencil-attachment-height depth-stencil-attachment-texture-storage :as args]
   (let [values (select-keys args (mapcat :path (get-in form-data [:sections 0 :fields])))
         form-values (into {} (map (fn [[k v]] [[k] v]) values))]
@@ -74,29 +75,38 @@
                           :set set-form-op
                           :clear clear-form-op}))))
 
-(g/defnk produce-pb-msg
+(g/defnk produce-save-value
   [color-attachments depth-stencil-attachment-width depth-stencil-attachment-height depth-stencil-attachment-format depth-stencil-attachment-texture-storage]
-  {:color-attachments color-attachments
-   :depth-stencil-attachment {:width depth-stencil-attachment-width
-                              :height depth-stencil-attachment-height
-                              :format depth-stencil-attachment-format
-                              :texture-storage depth-stencil-attachment-texture-storage}})
+  (let [color-attachments
+        (mapv #(protobuf/clear-defaults RenderTarget$RenderTargetDesc$ColorAttachment %)
+              color-attachments)
+
+        depth-stencil-attachment
+        (protobuf/make-map-without-defaults RenderTarget$RenderTargetDesc$DepthStencilAttachment
+          :width depth-stencil-attachment-width
+          :height depth-stencil-attachment-height
+          :format depth-stencil-attachment-format
+          :texture-storage depth-stencil-attachment-texture-storage)]
+
+    (protobuf/make-map-without-defaults RenderTarget$RenderTargetDesc
+      :color-attachments color-attachments
+      :depth-stencil-attachment depth-stencil-attachment)))
 
 (defn build-render-target
-  [resource dep-resources user-data]
+  [resource _dep-resources user-data]
   {:resource resource
    :content (protobuf/map->bytes RenderTarget$RenderTargetDesc (:pb-msg user-data))})
 
 (g/defnk produce-build-targets
-  [_node-id resource pb-msg build-errors]
+  [_node-id resource save-value build-errors]
   (g/precluding-errors build-errors
     [(bt/with-content-hash
        {:node-id _node-id
         :resource (workspace/make-build-resource resource)
         :build-fn build-render-target
-        :user-data {:pb-msg pb-msg}})]))
+        :user-data {:pb-msg save-value}})]))
 
-(defn- generate-gpu-texture [args request-id params unit]
+(defn- generate-gpu-texture [_args request-id _params _unit]
   (texture/image-texture request-id nil))
 
 (defn- validate-color-attachment-count [v name]
@@ -118,16 +128,20 @@
 (g/defnode RenderTargetNode
   (inherits resource-node/ResourceNode)
 
-  (property color-attachments g/Any (dynamic visible (g/constantly false)))
-  (property depth-stencil-attachment-width g/Int (dynamic visible (g/constantly false)))
-  (property depth-stencil-attachment-height g/Int (dynamic visible (g/constantly false)))
-  (property depth-stencil-attachment-format g/Any (dynamic visible (g/constantly false)))
-  (property depth-stencil-attachment-texture-storage g/Bool (dynamic visible (g/constantly false)))
+  (property color-attachments g/Any ; Vector assigned in load-fn.
+            (dynamic visible (g/constantly false)))
+  (property depth-stencil-attachment-width g/Int ; Required protobuf field.
+            (dynamic visible (g/constantly false)))
+  (property depth-stencil-attachment-height g/Int ; Required protobuf field.
+            (dynamic visible (g/constantly false)))
+  (property depth-stencil-attachment-format g/Any (default (protobuf/default RenderTarget$RenderTargetDesc$DepthStencilAttachment :format))
+            (dynamic visible (g/constantly false)))
+  (property depth-stencil-attachment-texture-storage g/Bool (default (protobuf/default RenderTarget$RenderTargetDesc$DepthStencilAttachment :texture-storage))
+            (dynamic visible (g/constantly false)))
 
-  (output pb-msg g/Any :cached produce-pb-msg)
-  (output save-value g/Any (gu/passthrough pb-msg))
+  (output save-value g/Any :cached produce-save-value)
   (output form-data g/Any produce-form-data)
-  (output gpu-texture-generator g/Any {:f generate-gpu-texture })
+  (output gpu-texture-generator g/Any {:f generate-gpu-texture})
   (output build-targets g/Any :cached produce-build-targets)
   (output build-errors g/Any (g/fnk [_node-id color-attachments depth-stencil-attachment-width depth-stencil-attachment-height]
                                (g/package-errors _node-id
@@ -145,14 +159,18 @@
                                                    (g/->error _node-id :depth-stencil-attachment-height :fatal depth-stencil-attachment-height
                                                               (format "Incorrect Depth/Stencil attachment: The height is greater than zero, but the width is zero")))))))
 
-(defn load-render-target [project self resource render-target]
-  (let [depth-stencil-attachment (:depth-stencil-attachment render-target)]
+(defn load-render-target [_project self _resource render-target-desc]
+  ;; Inject any missing defaults into the stripped pb-map for form-view editing.
+  (let [render-target-desc (protobuf/inject-defaults RenderTarget$RenderTargetDesc render-target-desc)
+        depth-stencil-attachment (:depth-stencil-attachment render-target-desc)]
     (concat
-      (g/set-property self :color-attachments (:color-attachments render-target))
-      (g/set-property self :depth-stencil-attachment-width (:width depth-stencil-attachment))
-      (g/set-property self :depth-stencil-attachment-height (:height depth-stencil-attachment))
-      (g/set-property self :depth-stencil-attachment-format (:format depth-stencil-attachment))
-      (g/set-property self :depth-stencil-attachment-texture-storage (:texture-storage depth-stencil-attachment)))))
+      (gu/set-properties-from-map self render-target-desc
+        color-attachments :color-attachments)
+      (gu/set-properties-from-map self depth-stencil-attachment
+        depth-stencil-attachment-width :width
+        depth-stencil-attachment-height :height
+        depth-stencil-attachment-format :format
+        depth-stencil-attachment-texture-storage :texture-storage))))
 
 (defn register-resource-types
   [workspace]
@@ -160,6 +178,7 @@
     :ext "render_target"
     :node-type RenderTargetNode
     :ddf-type RenderTarget$RenderTargetDesc
+    :read-defaults false
     :load-fn load-render-target
     :icon texture-icon
     :view-types [:cljfx-form-view :text]
