632ee62cabc50bf8de5a4fe69f1b52bf7f6682fe
Jhonny Göransson <jhonny.goransson@gmail.com>
2024-02-26 12:39:47 +0100

    Editor fixes

--- engine/particle/src/particle.h
+++ engine/particle/src/particle.h
@@ -139,30 +139,6 @@ namespace dmParticle
         GENERATE_VERTEX_DATA_MAX_PARTICLES_EXCEEDED = 2,
     };
 
-    /*
-    struct ParticleVertexAttributeInfo
-    {
-        dmhash_t                                  m_NameHash;
-        dmGraphics::VertexAttribute::SemanticType m_SemanticType;
-        dmGraphics::CoordinateSpace               m_CoordinateSpace;
-        uint8_t*                                  m_ValuePtr;
-        uint32_t                                  m_ValueByteSize;
-    };
-
-    struct ParticleVertexAttributeInfos
-    {
-        ParticleVertexAttributeInfos()
-        {
-            m_StructSize = sizeof(*this);
-        }
-
-        ParticleVertexAttributeInfo m_Infos[dmGraphics::MAX_VERTEX_STREAM_COUNT];
-        uint32_t                    m_VertexStride;
-        uint32_t                    m_NumInfos;
-        uint32_t                    m_StructSize;
-    };
-    */
-
     struct EmitterRenderData
     {
         EmitterRenderData()
