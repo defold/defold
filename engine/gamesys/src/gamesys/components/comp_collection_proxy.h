54e2f66651d16f9fc4d8fa31f895dc70cf1420f4
JCash <mwesterdahl76@gmail.com>
2024-03-07 11:04:09 +0100

    Added public collection proxy functions to dmSDK

--- engine/gamesys/src/gamesys/components/comp_collection_proxy.h
+++ engine/gamesys/src/gamesys/components/comp_collection_proxy.h
@@ -3,10 +3,10 @@
 // Copyright 2009-2014 Ragnar Svensson, Christian Murray
 // Licensed under the Defold License version 1.0 (the "License"); you may not use
 // this file except in compliance with the License.
-// 
+//
 // You may obtain a copy of the License, together with FAQs at
 // https://www.defold.com/license
-// 
+//
 // Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -16,11 +16,10 @@
 #define DM_GAMESYS_COLLECTION_PROXY_H
 
 #include <gameobject/component.h>
+#include <dmsdk/gamesys/components/comp_collection_proxy.h>
 
 namespace dmGameSystem
 {
-    typedef struct CollectionProxyWorld* HCollectionProxyWorld;
-
     dmhash_t GetUrlHashFromComponent(const HCollectionProxyWorld world, dmhash_t instanceId, uint32_t index);
 
     dmGameObject::CreateResult CompCollectionProxyNewWorld(const dmGameObject::ComponentNewWorldParams& params);
