e400c5324c08bf0d3bc7e095af208611091882f5
JCash <mwesterdahl76@gmail.com>
2024-04-02 11:32:02 +0200

    Changed some opengl backend state flags to atomics to silence thread sanitizer

--- engine/graphics/src/opengl/graphics_opengl.cpp
+++ engine/graphics/src/opengl/graphics_opengl.cpp
@@ -518,7 +518,7 @@ static void LogFrameBufferError(GLenum status)
     {
         OpenGLContext* context = (OpenGLContext*) _context;
         bool acquire_flag = (uintptr_t) _acquire_flag;
-        assert(context->m_AuxContextJobPending);
+        assert(dmAtomicGet32(&context->m_AuxContextJobPending));
 
         if (acquire_flag)
         {
@@ -529,8 +529,7 @@ static void LogFrameBufferError(GLenum status)
             dmPlatform::UnacquireAuxContext(context->m_Window, context->m_AuxContext);
         }
 
-        context->m_AuxContextJobPending = false;
-
+        dmAtomicStore32(&context->m_AuxContextJobPending, 0);
         return 0;
     }
 
@@ -544,10 +543,12 @@ static void LogFrameBufferError(GLenum status)
         //       But since we only have one worker thread right now, we can leave that for when we have more.
         assert(dmJobThread::GetWorkerCount(context->m_JobThread) == 1);
 
-        context->m_AuxContextJobPending = true;
+        dmAtomicStore32(&context->m_AuxContextJobPending, 1);
+
         dmJobThread::PushJob(context->m_JobThread, WorkerAcquireContextRunner, 0, (void*) context, (void*) (uintptr_t) acquire_flag);
 
-        while(context->m_AuxContextJobPending)
+        // Block until the job is done
+        while(dmAtomicGet32(&context->m_AuxContextJobPending))
         {
             dmTime::Sleep(100);
         }
@@ -2954,7 +2955,7 @@ static void LogFrameBufferError(GLenum status)
     {
         OpenGLTexture* tex = GetAssetFromContainer<OpenGLTexture>(g_Context->m_AssetHandleContainer, texture);
         uint32_t flags     = TEXTURE_STATUS_OK;
-        if(tex && tex->m_DataState)
+        if(tex && dmAtomicGet32(&tex->m_DataState))
         {
             flags |= TEXTURE_STATUS_DATA_PENDING;
         }
@@ -2977,8 +2978,9 @@ static void LogFrameBufferError(GLenum status)
         glFlush();
 
         OpenGLTexture* tex = GetAssetFromContainer<OpenGLTexture>(context->m_AssetHandleContainer, ap.m_Texture);
-        tex->m_DataState &= ~(1<<ap.m_Params.m_MipMap);
-
+        int32_t data_state = dmAtomicGet32(&tex->m_DataState);
+        data_state &= ~(1<<ap.m_Params.m_MipMap);
+        dmAtomicStore32(&tex->m_DataState, data_state);
         return 0;
     }
 
