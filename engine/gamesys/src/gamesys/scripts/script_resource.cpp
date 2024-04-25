fd07cc39b95bba5f7204004a1d03fe899cb47554
Roman Silin <astrochili@users.noreply.github.com>
2024-04-25 14:47:03 +0300

    Fixed typo in the documentation

--- engine/gamesys/src/gamesys/scripts/script_resource.cpp
+++ engine/gamesys/src/gamesys/scripts/script_resource.cpp
@@ -1054,7 +1054,7 @@ static int CreateTexture(lua_State* L)
  * @param buffer [type:buffer] optional buffer of precreated pixel data
  *
  * @return path [type:hash] The path to the resource.
- * @return request_id [type:handle] the request id for the async request.
+ * @return request_id [type:handle] The request id for the async request.
  *
  * @examples
  * Create a texture resource asyncronously with a buffer and a callback
