10362a5d3d6868830dcd1f09ffc2b75d95c2e23d
vlaaad <vlad@refold.io>
2024-05-20 11:57:53 +0200

    Don't suggest completion popup unnecessarily (#8959)
    The editor no longer shows the completion popup after typing `{` and `,`.
    
    Fixes #8946

--- editor/src/clj/editor/code/script.clj
+++ editor/src/clj/editor/code/script.clj
@@ -59,6 +59,7 @@
                        :field #{"."}
                        :module #{"."}}
    :completion-trigger-characters #{"."}
+   :ignored-completion-trigger-characters #{"{" ","}
    :patterns [{:captures {1 {:name "keyword.control.lua"}
                           2 {:name "entity.name.function.scope.lua"}
                           3 {:name "entity.name.function.lua"}
