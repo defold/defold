47e8731c58ea74cf3bd01362840d9cfc2fdfcbf6
Mats Gisselson <mats.gisselson@gmail.com>
2024-05-20 12:32:47 +0200

    Editor: Review fixes

--- editor/src/clj/editor/protobuf.clj
+++ editor/src/clj/editor/protobuf.clj
@@ -56,7 +56,9 @@ Macros currently mean no foreseeable performance gain however."
       (TextFormat/escapeBytes)))
 
 (defn pb-class? [value]
-  (isa? value Message))
+  (and (class? value)
+       (not= Message value)
+       (.isAssignableFrom Message value)))
 
 (defn- new-builder
   ^GeneratedMessage$Builder [class]
