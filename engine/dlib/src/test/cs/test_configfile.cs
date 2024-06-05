24157dbfe91dc7d0dcca005d1578f6415e3a259d
JCash <mwesterdahl76@gmail.com>
2024-06-05 12:51:19 +0200

    cleanup of unit test

--- engine/dlib/src/test/cs/test_configfile.cs
+++ engine/dlib/src/test/cs/test_configfile.cs
@@ -9,21 +9,9 @@ public unsafe class TestConfigFile
     {
         return "test_configfile.cs";
     }
-    private static int AssertEq(string expected, string v)
+    private static int AssertEq<T>(T expected, T v)
     {
-        if (expected == v) return 0;
-        Console.WriteLine(String.Format("{0}: Error: Expected: {1}, but got {2}", GetTestName(), expected, v));
-        return 1;
-    }
-    private static int AssertEq(int expected, int v)
-    {
-        if (expected == v) return 0;
-        Console.WriteLine(String.Format("{0}: Error: Expected: {1}, but got {2}", GetTestName(), expected, v));
-        return 1;
-    }
-    private static int AssertEq(float expected, float v)
-    {
-        if (expected == v) return 0;
+        if (expected != null && expected.Equals(v)) return 0;
         Console.WriteLine(String.Format("{0}: Error: Expected: {1}, but got {2}", GetTestName(), expected, v));
         return 1;
     }
@@ -31,11 +19,9 @@ public unsafe class TestConfigFile
     private static int RunTestsConfigFile(ConfigFile.Config* config)
     {
         int errors = 0;
-        errors += AssertEq(1, ConfigFile.GetInt(config, "test.my_int", -1));
-        errors += AssertEq(2.0f, ConfigFile.GetFloat(config, "test.my_float", -1.0f));
-        errors += AssertEq("hello", ConfigFile.GetString(config, "test.my_string", "wrong"));
-
-        Console.WriteLine(String.Format("{0}: ConfigFile Test: errors {1}", GetTestName(), errors));
+        errors += AssertEq<int>(1, ConfigFile.GetInt(config, "test.my_int", -1));
+        errors += AssertEq<float>(2.0f, ConfigFile.GetFloat(config, "test.my_float", -1.0f));
+        errors += AssertEq<string>("hello", ConfigFile.GetString(config, "test.my_string", "wrong"));
         return errors; // return != 0 for failure
     }
 
