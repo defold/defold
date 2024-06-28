0577accdea8f3e14f671d23071170e19a5a40cc9
JCash <mwesterdahl76@gmail.com>
2024-06-28 11:50:19 +0200

    run python3 on ci

--- ci/ci.sh
+++ ci/ci.sh
@@ -38,9 +38,9 @@ echo "PATH=" $PATH
 echo "Calling ci.py with args: $@"
 # # -u to run python unbuffered to guarantee that output ends up in the correct order in logs
 if [[ "${OSTYPE}" == "linux-gnu" ]]; then
-    xvfb-run --auto-servernum python -u ./ci/ci.py "$@"
+    xvfb-run --auto-servernum python3 -u ./ci/ci.py "$@"
 else
-    python -u ./ci/ci.py "$@"
+    python3 -u ./ci/ci.py "$@"
 fi
 
 echo -e "ci.sh finished\n-----------------\n\n\n\n\n"
