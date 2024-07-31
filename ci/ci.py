66c559083e0888425f5337b0992a7076a9714572
Björn Ritzl <bjorn.ritzl@gmail.com>
2024-07-31 07:56:42 +0200

    Update ci.py

--- ci/ci.py
+++ ci/ci.py
@@ -160,8 +160,8 @@ def install(args):
             "libgl1-mesa-dev",
             "libglw1-mesa-dev",
             "lib32z1",
-            "libc6-dev-i386",       # cross-compile to a 32 bit target on a 64 bit host
-            "gcc-multilib",
+            "libc6-dev-i386",       # cross-compile to a 32 bit target on a 64 bit host (LuaJIT)
+            "gcc-multilib",         # cross-compile to a 32 bit target on a 64 bit host (LuaJIT)
             "openssl",
             "tofrodos",
             "tree",
