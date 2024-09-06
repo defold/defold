9f15befb8464147dddebdfd2c33d6811cdbf91ec
JCash <mwesterdahl76@gmail.com>
2024-09-06 09:09:49 +0200

    Added extra check for existance of jni.h

--- build_tools/waf_dynamo.py
+++ build_tools/waf_dynamo.py
@@ -1914,6 +1914,12 @@ def detect(conf):
             conf.env['LIB_JNI'] = ['jni']
             conf.env['LIB_JNI_NOASAN'] = ['jni_noasan']
 
+            # if the jdk doesn't have the jni.h
+            jni_path = os.path.join(conf.env['INCLUDES_JDK'][0], 'jni.h')
+            if not os.path.exists(jni_path):
+                Logs.info("Failed to find jni.h at %s" % jni_path)
+                sys.exit(1)
+
     conf.load('waf_csharp')
 
     if Options.options.generate_compile_commands:
