bf660c43fbe64d99a923b02d623a50d715f5cb8b
JCash <mwesterdahl76@gmail.com>
2024-09-23 10:06:58 +0200

    removed lib reference to openal32

--- build_tools/waf_dynamo.py
+++ build_tools/waf_dynamo.py
@@ -1909,7 +1909,7 @@ def detect(conf):
     if platform in ('x86_64-win32','win32'):
         conf.env['LINKFLAGS_SOUND']     = ['ole32.lib'] # cocreateinstance in device_wasapi.cpp
         conf.env['LINKFLAGS_DINPUT']    = ['dinput8.lib', 'dxguid.lib', 'xinput9_1_0.lib']
-        conf.env['LINKFLAGS_APP']       = ['user32.lib', 'shell32.lib', 'openal32.lib', 'dbghelp.lib'] + conf.env['LINKFLAGS_DINPUT']
+        conf.env['LINKFLAGS_APP']       = ['user32.lib', 'shell32.lib', 'dbghelp.lib'] + conf.env['LINKFLAGS_DINPUT']
 
     conf.env['STLIB_EXTENSION'] = 'extension'
     conf.env['STLIB_SCRIPT'] = 'script'
