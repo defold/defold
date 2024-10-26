b5bb59990f37a6d589b00bc24f5f9f245cb391a5
JCash <mwesterdahl76@gmail.com>
2024-10-26 15:19:34 +0200

    Use llvm 16 on CI for linux

--- ci/ci.py
+++ ci/ci.py
@@ -207,11 +207,10 @@ def build_engine(platform, channel, with_valgrind = False, with_asan = False, wi
                 with_vanilla_lua = False, skip_tests = False, skip_build_tests = False, skip_codesign = True,
                 skip_docs = False, skip_builtins = False, archive = False):
 
+    install_sdk = 'install_sdk'
     # for some platforms, we use the locally installed platform sdk
-    if platform in ('x86_64-macos', 'arm64-macos', 'arm64-ios', 'x86_64-ios', 'js-web', 'wasm-web'):
+    if platform in ('x86_64-macos', 'arm64-macos', 'x86_64-linux', 'arm64-linux', 'arm64-ios', 'x86_64-ios', 'js-web', 'wasm-web'):
         install_sdk = ''
-    else:
-        install_sdk = 'install_sdk'
 
     args = ('python scripts/build.py distclean %s install_ext check_sdk' % install_sdk).split()
 
