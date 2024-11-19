069cc5f1a17b99f82c95a2a25a7b46d97a6fb8ee
Jhonny Göransson <jhonny.goransson@gmail.com>
2024-11-19 15:21:20 +0100

    Use correct dpi value in glfwGetDisplayScaleFactor

--- external/glfw/js/library_glfw.js
+++ external/glfw/js/library_glfw.js
@@ -765,6 +765,9 @@ var LibraryGLFW = {
 
   glfwOpenWindowHint: function(target, hint) {
     GLFW.params[target] = hint;
+
+    GLFW.dpi = 1;
+
     // if display._high_dpi flag is on in game.project
     // we get information about the current pixel ratio from browser
     if (target == 0x00020019) { //GLFW_WINDOW_HIGH_DPI
@@ -1127,7 +1130,7 @@ var LibraryGLFW = {
   },
 
   glfwGetDisplayScaleFactor: function() {
-    return 1;
+    return GLFW.dpi;
   }
 };
 
