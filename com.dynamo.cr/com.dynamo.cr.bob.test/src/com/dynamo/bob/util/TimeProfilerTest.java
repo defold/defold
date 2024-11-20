a93151477f28e2205bb012078b295f981261f56c
Vlad Protsenko <vlad@refold.io>
2024-11-20 08:51:27 +0100

    Improve tests

--- com.dynamo.cr/com.dynamo.cr.bob.test/src/com/dynamo/bob/util/TimeProfilerTest.java
+++ com.dynamo.cr/com.dynamo.cr.bob.test/src/com/dynamo/bob/util/TimeProfilerTest.java
@@ -16,7 +16,15 @@ public class TimeProfilerTest {
     public void testBuildReportGeneration() throws Exception {
         var cwd = System.getProperty("user.dir");
         var jsonReport = Files.createTempFile("time-profiler-test", "report.json");
+        var jsonTimeReport = jsonReport.getParent().resolve(jsonReport.getFileName().toString().replace("report.json", "report_time.json"));
         var htmlReport = Files.createTempFile("time-profiler-test", "report.html");
+        var htmlTimeReport = htmlReport.getParent().resolve(htmlReport.getFileName().toString().replace("report.html", "report_time.html"));
+
+        assertFalse(Files.exists(htmlTimeReport));
+        assertFalse(Files.exists(jsonTimeReport));
+        assertEquals(0, Files.size(jsonReport));
+        assertEquals(0, Files.size(htmlReport));
+
         var result = Bob.invoke(null, new ConsoleProgress(), null, new String[]{
                 "--root", cwd + "/test/time_profiler_project",
                 "--build-report-json", jsonReport.toString(),
@@ -28,17 +36,19 @@ public class TimeProfilerTest {
                 "--archive",
                 "distclean", "resolve", "build", "bundle"
         });
+
         assertTrue(result.success);
+        assertTrue(Files.exists(htmlTimeReport));
+        assertTrue(Files.exists(jsonTimeReport));
+        assertTrue(Files.size(jsonReport) > 0);
+        assertTrue(Files.size(htmlReport) > 0);
+
         var jsonReportData = new ObjectMapper().readValue(jsonReport.toFile(), Map.class);
         assertNotNull(jsonReportData.get("build_options"));
         assertFalse(((List<?>) jsonReportData.get("resources")).isEmpty());
 
-        var jsonTimeReport = jsonReport.getParent().resolve(jsonReport.getFileName().toString().replace("report.json", "report_time.json"));
         var jsonTimeReportData = new ObjectMapper().readValue(jsonTimeReport.toFile(), Map.class);
         assertFalse(((List<?>) jsonTimeReportData.get("data")).isEmpty());
-
-        var htmlTimeReport = htmlReport.getParent().resolve(htmlReport.getFileName().toString().replace("report.html", "report_time.html"));
-        assertTrue(Files.exists(htmlTimeReport));
     }
 
     @Test
@@ -46,6 +56,8 @@ public class TimeProfilerTest {
         var cwd = System.getProperty("user.dir");
         var jsonReport1 = Files.createTempFile("time-profiler-test", "report.json");
         var jsonReport2 = Files.createTempFile("time-profiler-test", "report.json");
+        assertEquals(0, Files.size(jsonReport1));
+        assertEquals(0, Files.size(jsonReport2));
         var result1 = Bob.invoke(null, new ConsoleProgress(), null, new String[]{
                 "--root", cwd + "/test/time_profiler_project",
                 "--build-report-json", jsonReport1.toString(),
@@ -58,7 +70,7 @@ public class TimeProfilerTest {
                 "distclean", "resolve", "build"
         });
         assertTrue(result2.success);
-        assertTrue(Files.exists(jsonReport1));
-        assertTrue(Files.exists(jsonReport2));
+        assertTrue(Files.size(jsonReport1) > 0);
+        assertTrue(Files.size(jsonReport2) > 0);
     }
 }
