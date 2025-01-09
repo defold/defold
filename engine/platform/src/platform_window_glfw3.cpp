bdca0b2f6f6b733ed65a28b17b67ddc4cc130630
Jhonny Göransson <jhonny.goransson@gmail.com>
2025-01-09 11:28:32 +0100

    Compile fix

--- engine/platform/src/platform_window_glfw3.cpp
+++ engine/platform/src/platform_window_glfw3.cpp
@@ -293,7 +293,7 @@ namespace dmPlatform
         if (res == PLATFORM_RESULT_OK)
         {
             FocusWindowNative(window);
-            SetWindowsIconNative(HWindow window);
+            SetWindowsIconNative(window);
 
         #ifdef __MACH__
             // Set size from settings
