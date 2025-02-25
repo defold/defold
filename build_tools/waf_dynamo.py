6dc318b91a3205872d2edc3feb1edce06acaaa5a
Kharkunov Eugene <eugene.kharkunov@gmail.com>
2025-02-25 18:36:40 +0200

    Build both graphics libraries for arm64 linux. Make opengles as default library for custom arm64 builds

--- build_tools/waf_dynamo.py
+++ build_tools/waf_dynamo.py
@@ -77,8 +77,6 @@ def platform_supports_feature(platform, feature, data):
         return platform in ['x86_64-win32']
     if feature == 'opengl_compute':
         return platform not in ['js-web', 'wasm-web', 'x86_64-ios', 'arm64-ios', 'arm64-macos', 'x86_64-macos']
-    if feature == 'opengl':
-        return platform not in ['arm64-linux']
     if feature == 'opengles':
         return platform in ['arm64-linux']
     if feature == 'webgpu':
