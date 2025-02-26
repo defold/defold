8d544ffd1d8d56e14e07d5c33e6c0c1c77a47ee1
Konstantinos Kaloutas <sprocketc@gmail.com>
2025-02-26 17:51:36 +0200

    use a popup and fix multiple issues

--- editor/src/clj/editor/color_dropper.clj
+++ editor/src/clj/editor/color_dropper.clj
@@ -17,21 +17,19 @@
             [editor.handler :as handler]
             [editor.ui :as ui])
   (:import [javafx.geometry Rectangle2D]
-           [javafx.scene Cursor Scene]
+           [javafx.scene Cursor]
            [javafx.scene.canvas Canvas GraphicsContext]
            [javafx.scene.image PixelReader WritableImage]
-           [javafx.scene.input KeyCode KeyEvent MouseEvent]
+           [javafx.scene.input KeyEvent MouseEvent]
            [javafx.scene.layout StackPane]
            [javafx.scene.paint Color]
            [javafx.scene.shape Circle]
            [javafx.scene.robot Robot]
-           [javafx.stage Screen Stage StageStyle Modality]
-           [java.awt MouseInfo]))
+           [javafx.stage Popup Screen]))
 
 (set! *warn-on-reflection* true)
 
 (g/defnode ColorDropper
-  (property canvas Canvas)
   (property color Color)
   (property size g/Any)
   (property image WritableImage))
@@ -55,63 +53,58 @@
 (defn- in-bounds?
   [view-node x y]
   (let [{:keys [width height]} (g/node-value view-node :size)]
-    (and (< 0 x width)
-         (< 0 y height))))
+    (and (<= 0 x width)
+         (<= 0 y height))))
 
-(defn mouse-move-handler!
-  [view-node ^Stage stage ^Canvas canvas ^MouseEvent e]
+(def pixel-range (range -4 5))
+(def pixel-size 10)
+(def center 5)
+(def diameter (* pixel-size (count pixel-range)))
+(def radius (/ diameter 2))
+
+(defn- mouse-move-handler!
+  [view-node ^Canvas canvas ^MouseEvent e]
   (.consume e)
-  (when (in-bounds? view-node (.getX e) (.getY e))
-    (let [^GraphicsContext graphics-context (.getGraphicsContext2D canvas)
-          ^WritableImage image (g/node-value view-node :image)
-          ^PixelReader pixel-reader (.getPixelReader image)
-          mouse-x (.getX e)
-          mouse-y (.getY e)
-          delta-x (- (.getScreenX e) mouse-x)
-          delta-y (- (.getScreenY e) mouse-y)
-          adjusted-x (+ mouse-x delta-x)
-          adjusted-y (+ mouse-y delta-y)
-          pixel-range (range -4 5)
-          pixel-size 10
-          half-pixel-size (/ pixel-size 2)
-          color (.getColor pixel-reader adjusted-x adjusted-y)
-          radius (/ (* pixel-size (count pixel-range)) 2)
-          mask (doto (Circle.)
-                 (.setCenterX (+ mouse-x))
-                 (.setCenterY (+ mouse-y))
-                 (.setRadius radius))]
-      (g/set-property! view-node :color color)
-      (.clearRect graphics-context 0 0 (.getWidth canvas) (.getHeight canvas))
-
-      (doseq [x-range pixel-range
-              y-range pixel-range
-              :let [x (+ mouse-x (* x-range pixel-size) (- half-pixel-size))
-                    y (+ mouse-y (* y-range pixel-size) (- half-pixel-size))
-                    color (.getColor pixel-reader (+ adjusted-x x-range) (+ adjusted-y y-range))]]
-        (when (in-bounds? view-node x y)
-          (paint-pixel! graphics-context x y pixel-size color)))
-
-      (highlight-pixel! graphics-context (- mouse-x half-pixel-size) (- mouse-y half-pixel-size) pixel-size)
-
-      (.setClip canvas mask))))
+  (let [^GraphicsContext graphics-context (.getGraphicsContext2D canvas)
+        ^WritableImage image (g/node-value view-node :image)
+        ^PixelReader pixel-reader (.getPixelReader image)
+        mouse-x (.getX e)
+        mouse-y (.getY e)
+        delta-x (- (.getScreenX e) mouse-x)
+        delta-y (- (.getScreenY e) mouse-y)
+        adjusted-x (+ mouse-x delta-x)
+        adjusted-y (+ mouse-y delta-y)
+        color (.getColor pixel-reader adjusted-x adjusted-y)
+        mask (doto (Circle.)
+               (.setCenterX (+ mouse-x))
+               (.setCenterY (+ mouse-y))
+               (.setRadius radius))]
+    (g/set-property! view-node :color color)
+    (.clearRect graphics-context (- mouse-x radius) (- mouse-y radius) diameter diameter)
+    (.setClip canvas mask)
+  
+    (doseq [x-range pixel-range
+            y-range pixel-range
+            :let [x (+ mouse-x (* x-range pixel-size) (- center))
+                  y (+ mouse-y (* y-range pixel-size) (- center))]]
+      (when (in-bounds? view-node x y)
+        (->> (.getColor pixel-reader (+ adjusted-x x-range) (+ adjusted-y y-range))
+             (paint-pixel! graphics-context x y pixel-size))))
+  
+    (doto graphics-context
+      (.setStroke Color/GRAY)
+      (.strokeOval (- mouse-x radius) (- mouse-y radius) diameter diameter)
+      (highlight-pixel! (- mouse-x center) (- mouse-y center) pixel-size))))
 
 (defn- apply-and-deactivate!
-  [view-node ^Stage stage pick-fn]
-  (.close stage)
-  (when pick-fn
-    (pick-fn (g/node-value view-node :color))))
-
-(defn- key-pressed-handler!
-  [view-node ^Stage stage pick-fn ^KeyEvent event]
-  (.consume event)
-  (condp = (.getCode event)
-    KeyCode/ENTER (apply-and-deactivate! view-node stage pick-fn)
-    KeyCode/ESCAPE (.close stage)
-    nil))
+  [view-node ^Popup popup pick-fn]
+  (.hide popup)
+  (pick-fn (g/node-value view-node :color))
+  (ui/user-data! (ui/main-scene) ::ui/refresh-requested? true))
 
 (defn- get-size []
   (reduce (fn [size ^Screen screen]
-            (let [screen-bounds (.getBounds screen)]
+            (let [screen-bounds (.getVisualBounds screen)]
               (-> size
                   (update :width + (.getWidth screen-bounds))
                   (update :height + (.getHeight screen-bounds)))))
@@ -129,44 +122,37 @@
 (defn make-color-dropper! [graph]
   (g/make-node! graph ColorDropper))
 
-(defn- create-stage! [width height]
-   (doto (Stage.)
-    (.initModality Modality/APPLICATION_MODAL)
-    (.initStyle StageStyle/TRANSPARENT)
-    (.initOwner (ui/main-stage))
+(defn- create-popup! ^Popup
+  [width height]
+   (doto (Popup.)
     (.setX 0)
     (.setY 0)
     (.setWidth width)
-    (.setHeight height)
-    (.setAlwaysOnTop true)
-    (.setResizable false)))
+    (.setHeight height)))
 
-(defn activate!
+(defn- activate!
   [view-node pick-fn]
   (let [size (get-size)
-        stage (create-stage! (:width size) (:height size))
-        pane (doto (StackPane.)
-               (.setStyle "-fx-background-color: transparent;"))
-        canvas (doto (Canvas.)
-                 (.setCursor Cursor/NONE))
-        scene (doto (Scene. pane)
-                (.setFill Color/TRANSPARENT))]
+        popup (create-popup! (:width size) (:height size))
+        pane (StackPane.)
+        canvas (Canvas.)]
     (g/set-property! view-node :size size)
-    (ui/add-child! pane canvas)
-    (.setScene stage scene)
-    (.bind (.widthProperty canvas) (.widthProperty pane))
-    (.bind (.heightProperty canvas) (.heightProperty pane))
-    
-    (capture! view-node)
+    (.add (.getContent popup) pane)
+    (.addListener (.focusedProperty pane) (ui/change-listener _ _ _ (capture! view-node)))
+    (.show popup (ui/main-stage))
 
     (doto pane
       (.requestFocus)
-      (.addEventHandler KeyEvent/ANY (ui/event-handler event (.consume event)))
-      (.addEventHandler KeyEvent/KEY_PRESSED (ui/event-handler event (key-pressed-handler! view-node stage pick-fn event)))
-      (.addEventHandler MouseEvent/MOUSE_MOVED (ui/event-handler event (mouse-move-handler! view-node stage canvas event)))
-      (.addEventHandler MouseEvent/MOUSE_PRESSED (ui/event-handler event (apply-and-deactivate! view-node stage pick-fn))))
-    
-    (.show stage)))
+      (ui/add-child! canvas)
+      (.setStyle "-fx-background-color: transparent;")
+      (.setCursor Cursor/NONE)
+      (.addEventHandler KeyEvent/ANY (ui/event-handler event (.hide popup)))
+      (.addEventHandler MouseEvent/MOUSE_MOVED (ui/event-handler event (mouse-move-handler! view-node canvas event)))
+      (.addEventHandler MouseEvent/MOUSE_PRESSED (ui/event-handler event (apply-and-deactivate! view-node popup pick-fn))))
+
+    (doto canvas
+      (.setWidth (:width size))
+      (.setHeight (:height size)))))
 
 (handler/defhandler :color-dropper :global
   (active? [color-dropper evaluation-context user-data] true)
