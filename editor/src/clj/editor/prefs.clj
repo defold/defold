4961ab041d02804e9d75849feaaf9c47b0ad520b
Konstantinos <sprocketc@gmail.com>
2025-03-24 16:21:07 +0200

    Introduce code zoom on scroll preference (#10373)
    * Introduce code zoom on mouse wheel pref
    
    * remove empty line
    
    * handle review suggestions

--- editor/src/clj/editor/prefs.clj
+++ editor/src/clj/editor/prefs.clj
@@ -82,6 +82,7 @@
            {:custom-editor {:type :string}
             :open-file {:type :string :default "{file}"}
             :open-file-at-line {:type :string :default "{file}:{line}" :label "Open File at Line"}
+            :zoom-on-scroll {:type :boolean :default true}
             :font {:type :object
                    :properties
                    {:name {:type :string :default "Dejavu Sans Mono"}
