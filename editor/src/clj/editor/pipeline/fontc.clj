7ac5b625f71f6c5c75103cf7d99e48ad6fe5e5ed
JCash <mwesterdahl76@gmail.com>
2025-03-27 18:59:03 +0100

    review fix

--- editor/src/clj/editor/pipeline/fontc.clj
+++ editor/src/clj/editor/pipeline/fontc.clj
@@ -178,9 +178,7 @@
   ; We can't know if it's only one glyph. And chances are it's a dynamic font
   (if (<= (count semi-glyphs) 1)
     false
-    (let [base-advance (if-let [first-glyph (first semi-glyphs)]
-                       (:advance first-glyph)
-                       0)]
+    (let [base-advance (:advance (first semi-glyphs))]
       (every? #(= base-advance (:advance %)) semi-glyphs))))
 
 (def ^:private positive-glyph-extent-pairs-xf (comp (map pair) (filter (comp positive-wh? :glyph-wh second))))
