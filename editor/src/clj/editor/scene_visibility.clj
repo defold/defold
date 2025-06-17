7c58f7c27ba59dce533a156c6a51a98307c47539
Konstantinos Kaloutas <sprocketc@gmail.com>
2025-06-17 17:20:33 +0300

    filter out grid toggle if there is no grid attached to the scene

--- editor/src/clj/editor/scene_visibility.clj
+++ editor/src/clj/editor/scene_visibility.clj
@@ -396,8 +396,10 @@
   (run [scene-visibility] (toggle-tag-visibility! scene-visibility :outline)))
 
 (handler/defhandler :scene.visibility.toggle-grid :workbench
-  (active? [scene-visibility evaluation-context]
-           (g/node-value scene-visibility :active-scene-resource-node evaluation-context))
+  (active? [app-view scene-visibility evaluation-context]
+           (and (g/node-value scene-visibility :active-scene-resource-node evaluation-context)
+                (when-let [active-view (g/node-value app-view :active-view evaluation-context)]
+                  (some? (g/node-value active-view :grid evaluation-context)))))
   (run [scene-visibility] (toggle-tag-visibility! scene-visibility :grid))
   (state [scene-visibility]
          (not (:grid (g/node-value scene-visibility :filtered-renderable-tags)))))
