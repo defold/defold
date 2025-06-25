b9ddea12d44fba6377c7871295c506483833ec35
Sam Magnuson <smagnuso@gmail.com>
2025-06-25 09:25:35 -0700

    export malloc/free so that the host can access them

--- build_tools/waf_dynamo.py
+++ build_tools/waf_dynamo.py
@@ -592,7 +592,7 @@ def default_flags(self):
         emflags_link = [
             'DISABLE_EXCEPTION_CATCHING=1',
             'ALLOW_UNIMPLEMENTED_SYSCALLS=0',
-            'EXPORTED_RUNTIME_METHODS=["ccall","UTF8ToString","callMain","HEAPU8","stringToNewUTF8"]',
+            'EXPORTED_RUNTIME_METHODS=["ccall","UTF8ToString","callMain","HEAPU8","stringToNewUTF8","_malloc","_free"]',
             'EXPORTED_FUNCTIONS=_main,_malloc,_free',
             'ERROR_ON_UNDEFINED_SYMBOLS=1',
             'INITIAL_MEMORY=33554432',
