6a0c93e52ec71921d6e243e64dceb0492f0be04e
AGulev <me@agulev.com>
2025-07-15 17:53:23 +0200

    fix "." path

--- com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/Project.java
+++ com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/Project.java
@@ -2262,7 +2262,7 @@ public class Project {
         TimeProfiler.start("findResourcePathsByExtension");
         TimeProfiler.addData("path", _path);
         TimeProfiler.addData("ext", ext);
-
+        _path = ".".equals(_path) ? "" : _path;
         final String path = Project.stripLeadingSlash(_path);
 
         // Initialize and cache all paths only once
