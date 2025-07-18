46c6d8d27ad9c0f38e6f3c34bcfd6c92735d23e3
JCash <mwesterdahl76@gmail.com>
2025-07-18 16:09:34 +0200

    Cleanup of library names and build configurations

--- build_tools/waf_dynamo.py
+++ build_tools/waf_dynamo.py
@@ -2035,9 +2035,10 @@ def detect(conf):
     conf.env['STLIB_CRASH_NULL'] = 'crashext_null'
 
     conf.env['STLIB_PROFILE'] = ['profile']
-    conf.env['STLIB_PROFILE_BASIC'] = ['profile_basic']
-    conf.env['STLIB_PROFILE_REMOTERY'] = ['profile_remotery', 'remotery']
     conf.env['STLIB_PROFILE_NULL'] = ['profile_null']
+    conf.env['STLIB_PROFILER_BASIC'] = ['profiler_basic']
+    conf.env['STLIB_PROFILER_REMOTERY'] = ['profiler_remotery']
+    conf.env['STLIB_PROFILER_NULL'] = ['profiler_null']
 
     conf.env['DEFINES_PROFILE_NULL'] = ['DM_PROFILE_NULL']
     conf.env['STLIB_PROFILE_NULL_NOASAN'] = ['profile_null_noasan']
