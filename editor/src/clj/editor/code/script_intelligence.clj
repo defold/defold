37c7e0f410a2affb6daeb454546682c2789e9a9f
vlaaad <vlad@refold.io>
2025-09-04 14:02:11 +0200

    Disallow non-top-level go.property calls in scripts (#11152)
    * Disallow non-top-level go.property calls in scripts
    
    Fixes #11147
    
    * Implement top-level go.property validation in Bob
    
    * Add bob test
    
    * Refactor bob parser and use it from the editor too
    
    * Review fixes

--- editor/src/clj/editor/code/script_intelligence.clj
+++ editor/src/clj/editor/code/script_intelligence.clj
@@ -15,13 +15,14 @@
 (ns editor.code.script-intelligence
   (:require [dynamo.graph :as g]
             [editor.graph-util :as gu]
+            [editor.lua :as lua]
             [schema.core :as s]))
 
 (g/deftype ScriptCompletions {s/Str [{s/Keyword s/Any}]})
 
 (g/defnk produce-lua-completions
   [lua-completions]
-  (apply (partial merge-with into) lua-completions))
+  (lua/combine-completions (apply (partial merge-with into) lua-completions)))
 
 (g/defnode ScriptIntelligenceNode
   (input lua-completions ScriptCompletions :array :substitute gu/array-subst-remove-errors)
