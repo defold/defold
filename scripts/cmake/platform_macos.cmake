24d03e6f183b1b9f7cbed46d01ce593ed58f655e
JCash <mwesterdahl76@gmail.com>
2025-09-07 15:05:36 +0200

    add macos sdk from prepackaged folder

--- scripts/cmake/platform_macos.cmake
+++ scripts/cmake/platform_macos.cmake
@@ -7,12 +7,6 @@ else()
   set(_DEFOLD_TARGET_ARCH "x86_64")
 endif()
 
-# Resolve SDK sysroot and min version if available
-set(_DEFOLD_SYSROOT "${CMAKE_OSX_SYSROOT}")
-if(NOT _DEFOLD_SYSROOT AND DEFINED ENV{SDKROOT})
-  set(_DEFOLD_SYSROOT "$ENV{SDKROOT}")
-endif()
-
 # Since it's a cmake feature, let's check it first if it's overridden
 set(_DEFOLD_MACOS_MIN "${CMAKE_OSX_DEPLOYMENT_TARGET}")
 if(NOT _DEFOLD_MACOS_MIN)
@@ -32,12 +26,11 @@ add_compile_options(
   -target ${_DEFOLD_TARGET_ARCH}-apple-darwin19
 )
 
-if(_DEFOLD_SYSROOT)
-  add_compile_options(-isysroot ${_DEFOLD_SYSROOT})
+if(_XCODE_TOOLCHAIN)
   # Add libc++ include dir and disable default stdlib includes for C++
   add_compile_options(
     $<$<COMPILE_LANGUAGE:CXX>:-isystem>
-    $<$<COMPILE_LANGUAGE:CXX>:${_DEFOLD_SYSROOT}/usr/include/c++/v1>
+    $<$<COMPILE_LANGUAGE:CXX>:${CMAKE_OSX_SYSROOT}/usr/include/c++/v1>
   )
 endif()
 
@@ -49,18 +42,18 @@ add_compile_options(
 
 # Link options
 set(_DEFOLD_LINK_OPTS
-  -stdlib=libc++
-  -mmacosx-version-min=${_DEFOLD_MACOS_MIN}
-
-  -target ${_DEFOLD_TARGET_ARCH}-apple-darwin19
+    -stdlib=libc++
+    -mmacosx-version-min=${_DEFOLD_MACOS_MIN}
+    -target ${_DEFOLD_TARGET_ARCH}-apple-darwin19
+    
+    # Add common macOS frameworks (use linker-prefixed form to avoid token grouping issues)
+    -Wl,-framework,AppKit
+    -Wl,-framework,Carbon
+    -Wl,-weak_framework,Foundation
 )
-if(_DEFOLD_SYSROOT)
-  list(APPEND _DEFOLD_LINK_OPTS -isysroot ${_DEFOLD_SYSROOT})
+if(_XCODE_TOOLCHAIN)
+  list(APPEND _DEFOLD_LINK_OPTS -isysroot ${CMAKE_OSX_SYSROOT})
 endif()
 
 add_link_options(${_DEFOLD_LINK_OPTS})
 
-# Add common macOS frameworks
-add_link_options(-Wl,-weak_framework,Foundation)
-add_link_options(-framework AppKit)
-add_link_options(-framework Carbon)
