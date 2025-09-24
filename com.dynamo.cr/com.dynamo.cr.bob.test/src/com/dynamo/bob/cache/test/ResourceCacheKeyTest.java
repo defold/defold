3b2c53eabbfbe9749609f47784f9fdf78662d84c
Björn Ritzl <bjorn.ritzl@gmail.com>
2025-09-24 08:42:19 +0200

    Update ResourceCacheKeyTest.java

--- com.dynamo.cr/com.dynamo.cr.bob.test/src/com/dynamo/bob/cache/test/ResourceCacheKeyTest.java
+++ com.dynamo.cr/com.dynamo.cr.bob.test/src/com/dynamo/bob/cache/test/ResourceCacheKeyTest.java
@@ -284,6 +284,7 @@ public class ResourceCacheKeyTest {
                 "build-server",
                 "build-server-header",
                 "bundle-format",
+                "bundle-module",
                 "bundle-output",
                 "certificate",
                 "debug",
