edc94f8002e1c5b78dd3a9e9dec9526760322c66
JCash <mwesterdahl76@gmail.com>
2025-10-15 16:39:03 +0200

    cleanup

--- build_tools/waf_csharp.py
+++ build_tools/waf_csharp.py
@@ -22,83 +22,6 @@ from BuildUtility import BuildUtility, BuildUtilityException, create_build_utili
 import run
 import sdk
 
-DOTNET_VERSION=9
-
-def sortversions(versions):
-    """Return a list of semantic version strings sorted in ascending order."""
-
-    if not versions:
-        return []
-
-    def _version_key(value):
-        parts = []
-        for token in re.split(r"(\\d+)|([^.]+)", value):
-            if not token or token == '.':
-                continue
-            # trick to make integer vs string comparisons, by making them a separate tuple
-            # this means strings (e.g. "1-rc", will be sorted before actual integers)
-            # this is ok as we're really only using the major version in this
-            if token.isdigit():
-                parts.append((1, int(token)))
-            else:
-                parts.append((0, token.lower()))
-        return parts
-
-    return sorted(versions, key=_version_key)
-
-
-def _find_best_version_from_folder(base_folder, major_version):
-
-    print("MAWE _find_best_version_from_folder:", base_folder)
-
-    if not base_folder or not os.path.isdir(base_folder):
-        print(f"MAWE not is dir {base_folder}")
-        return None
-
-    candidates = []
-    for entry in os.listdir(base_folder):
-        print("MAWE entry:", entry)
-        entry_path = os.path.join(base_folder, entry)
-        if not os.path.isdir(entry_path):
-            continue
-        candidates.append(entry)
-
-    if not candidates:
-        print(f"MAWE no candidates")
-        return None
-
-    print("MAWE ***********************************************")
-    print("MAWE UNSORTED")
-    for x in candidates:
-        print("MAWE: ", x)
-    print("MAWE ***********************************************")
-
-    candidates = sortversions(candidates)
-    candidates.reverse()
-
-    print("MAWE ***********************************************")
-    print("MAWE SORTED")
-    for x in candidates:
-        print("MAWE: ", x)
-    print("MAWE ***********************************************")
-
-    for version in candidates:
-        tokens = version.split('.')
-        print("MAWE KEY:", tokens)
-        major = tokens[0]
-        try:
-            major = int(major)
-        except:
-            continue
-
-        if major > major_version:
-            continue
-
-        return version
-
-    return None
-
-
 # For properties, see https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-publish
 Task.task_factory('csproj_stlib', '${DOTNET} publish -c ${MODE} -o ${BUILD_DIR} -v q --artifacts-path ${ARTIFACTS_PATH} -r ${RUNTIME} -p:PublishAot=true -p:NativeLib=Static -p:PublishTrimmed=true -p:IlcDehydrate=false ${SRC[0].abspath()}',
                       color='BROWN',
@@ -214,15 +137,6 @@ def _get_dotnet_nuget_path():
             nuget_path = os.path.expanduser('~/.nuget/packages')
     return nuget_path
 
-def _skip_dotnet(self):
-    conf.env.DOTNET_VERSION = None
-    conf.env.DOTNET_AOT_VERSION = None
-    conf.env.DOTNET_SDK = None
-    print("*************************************************************")
-    print("Dotnet configuration does not work. Skipping!")
-    print("*************************************************************")
-
-
 def configure(conf):
     platform = getattr(Options.options, 'platform', sdk.get_host_platform())
     if platform == '':
@@ -260,15 +174,11 @@ def configure(conf):
         if not os.path.exists(conf.env.NUGET_PACKAGES):
             conf.fatal("Couldn't find C# nuget packages: '%s'" % conf.env.NUGET_PACKAGES)
 
-    print(f"Nuget path: {nuget_path}")
-    for x in os.listdir(nuget_path):
-        print("MAWE x", x, os.path.isdir(os.path.join(nuget_path, x)))
-
     print("Setting DOTNET_SDK", conf.env.DOTNET_SDK)
     print("Setting DOTNET_VERSION", conf.env.DOTNET_VERSION)
 
+    # Note, we can't check the path here, in case this is a clean install, and we haven't downloaded any packages with nuget yet
     aot_base = _get_dotnet_aot_base_with_version(nuget_path, dotnet_platform, conf.env.DOTNET_VERSION)
-    # Note, we can't check it here, in case this is a clean install, and we haven't downloaded any packages with nuget yet
 
     if build_util.get_target_os() in ('win32'):
         pass
