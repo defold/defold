b0d5247ef647de45c80c6d4281acf5e2eada5eff
JCash <mwesterdahl76@gmail.com>
2025-10-31 17:22:29 +0100

    Removed old dynamic gui resource profile reporting

--- engine/gui/src/gui.cpp
+++ engine/gui/src/gui.cpp
@@ -1145,21 +1145,6 @@ namespace dmGui
         scene->m_DynamicTextures.Clear();
     }
 
-    void IterateDynamicTextures(dmhash_t gui_res_id, HScene scene, FDynamicTextturesIterator callback, void* user_ctx)
-    {
-        dmHashTable64<TextureInfo>::Iterator dynamic_textures_iter = scene->m_DynamicTextures.GetIterator();
-        while(dynamic_textures_iter.Next())
-        {
-            const TextureInfo texture = dynamic_textures_iter.GetValue();
-            uint32_t size = texture.m_OriginalWidth * texture.m_OriginalHeight * dmImage::BytesPerPixel(texture.m_ImageType);
-            bool result = callback(gui_res_id, dynamic_textures_iter.GetKey(), size, user_ctx);
-            if (!result)
-            {
-                break;
-            }
-        }
-    }
-
     static uint16_t GetLayerIndex(HScene scene, InternalNode* node)
     {
         if (node->m_Node.m_LayerHash == DEFAULT_LAYER && node->m_ParentIndex != INVALID_INDEX)
