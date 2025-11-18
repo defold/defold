3e81fc945cf1acf8ecd21cbddec72258e0aab916
AGulev <me@agulev.com>
2025-11-18 19:15:07 +0100

    windows experemental fix

--- share/ext/astcenc/build_astcenc.sh
+++ share/ext/astcenc/build_astcenc.sh
@@ -22,6 +22,10 @@ SOURCE_DIR=${PWD}/source
 SHA1=30aabb3f42406df45a910d8496f9bee17eeba9bb
 BUILD_DIR=${PWD}/build/${PLATFORM}
 
+unset CFLAGS
+unset CXXFLAGS
+unset CPPFLAGS
+
 . ../common.sh
 if [ -z "$PLATFORM" ]; then
     echo "No platform specified!"
@@ -33,44 +37,56 @@ OSX_MIN_SDK_VERSION=$VERSION_MACOSX_MIN
 
 CONFIG=Release
 
-CMAKE_FLAGS="-DASTCENC_CLI=OFF ${CMAKE_FLAGS}"
-CMAKE_FLAGS="-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded ${CMAKE_FLAGS}"
+CMAKE_FLAGS=()
+CMAKE_GENERATOR_ARGS=()
+
+CMAKE_FLAGS+=(-DASTCENC_CLI=OFF)
+CMAKE_FLAGS+=(-DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded)
 # The default Unix Makefile/Ninja generators build Debug unless build type is set explicitly.
 # Our macOS and Linux builds use single-config generators, so make sure we get an optimized lib.
 if [ "$PLATFORM" != "x86_64-win32" ]; then
-    CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=${CONFIG} ${CMAKE_FLAGS}"
+    CMAKE_FLAGS+=(-DCMAKE_BUILD_TYPE=${CONFIG})
 fi
 
 case $PLATFORM in
     arm64-macos)
-        CMAKE_FLAGS="-DCMAKE_OSX_ARCHITECTURES=arm64 ${CMAKE_FLAGS}"
-        CMAKE_FLAGS="-DCMAKE_OSX_DEPLOYMENT_TARGET=${OSX_MIN_SDK_VERSION} ${CMAKE_FLAGS}"
-        CMAKE_FLAGS="-DASTCENC_ISA_NEON=ON -DASTCENC_PACKAGE=arm64 ${CMAKE_FLAGS}"
+        CMAKE_FLAGS+=(-DCMAKE_OSX_ARCHITECTURES=arm64)
+        CMAKE_FLAGS+=(-DCMAKE_OSX_DEPLOYMENT_TARGET=${OSX_MIN_SDK_VERSION})
+        CMAKE_FLAGS+=(-DASTCENC_ISA_NEON=ON)
+        CMAKE_FLAGS+=(-DASTCENC_PACKAGE=arm64)
         ;;
     x86_64-macos)
-        CMAKE_FLAGS="-DCMAKE_OSX_ARCHITECTURES=x86_64 ${CMAKE_FLAGS}"
-        CMAKE_FLAGS="-DCMAKE_OSX_DEPLOYMENT_TARGET=${OSX_MIN_SDK_VERSION} ${CMAKE_FLAGS}"
-        CMAKE_FLAGS="-DASTCENC_ISA_SSE41=ON -DASTCENC_ISA_NATIVE=OFF -DASTCENC_PACKAGE=x64 ${CMAKE_FLAGS}"
+        CMAKE_FLAGS+=(-DCMAKE_OSX_ARCHITECTURES=x86_64)
+        CMAKE_FLAGS+=(-DCMAKE_OSX_DEPLOYMENT_TARGET=${OSX_MIN_SDK_VERSION})
+        CMAKE_FLAGS+=(-DASTCENC_ISA_SSE41=ON)
+        CMAKE_FLAGS+=(-DASTCENC_ISA_NATIVE=OFF)
+        CMAKE_FLAGS+=(-DASTCENC_PACKAGE=x64)
         ;;
     arm64-linux)
-        CMAKE_FLAGS="-DARCH=aarch64 -DASTCENC_ISA_NEON=ON -DASTCENC_ISA_SVE_128=ON -DASTCENC_ISA_SVE_256=ON -DASTCENC_PACKAGE=arm64 ${CMAKE_FLAGS}"
+        CMAKE_FLAGS+=(-DARCH=aarch64)
+        CMAKE_FLAGS+=(-DASTCENC_ISA_NEON=ON)
+        CMAKE_FLAGS+=(-DASTCENC_ISA_SVE_128=ON)
+        CMAKE_FLAGS+=(-DASTCENC_ISA_SVE_256=ON)
+        CMAKE_FLAGS+=(-DASTCENC_PACKAGE=arm64)
         # Need to setup clang on linux
         cmi_setup_cc $PLATFORM
         ;;
     x86_64-linux)
-        CMAKE_FLAGS="-DASTCENC_ISA_AVX2=ON -DASTCENC_ISA_SSE41=ON -DASTCENC_ISA_SSE2=ON -DASTCENC_ISA_NATIVE=OFF -DASTCENC_PACKAGE=x64 ${CMAKE_FLAGS}"
+        CMAKE_FLAGS+=(-DASTCENC_ISA_AVX2=ON)
+        CMAKE_FLAGS+=(-DASTCENC_ISA_SSE41=ON)
+        CMAKE_FLAGS+=(-DASTCENC_ISA_SSE2=ON)
+        CMAKE_FLAGS+=(-DASTCENC_ISA_NATIVE=OFF)
+        CMAKE_FLAGS+=(-DASTCENC_PACKAGE=x64)
         # Need to setup clang on linux
         cmi_setup_cc $PLATFORM
         ;;
     x86_64-win32)
-
-        # NOTE! For windows we need to use the exact same version of the toolset as we use for the engine
-        #       However, I could not get the cmake setup to work, so I just installed the same version via the visual studio installer.
-        #       Leaving the almost-working-code here for posterity..
-        #
-        # eval $(python ${DYNAMO_HOME}/../../build_tools/set_sdk_vars.py VERSION_WINDOWS_MSVC_2022)
-        # MSVC_CL_EXE=$(cygpath -u "${DYNAMO_HOME}/ext/SDKs/Win32/MicrosoftVisualStudio14.0/VC/Tools/MSVC/${VERSION_WINDOWS_MSVC_2022}/bin/Hostx64/x64/cl.exe")
-        # CMAKE_FLAGS="-DCMAKE_C_COMPILER=${MSVC_CL_EXE} -DCMAKE_CXX_COMPILER=${MSVC_CL_EXE} $CMAKE_FLAGS"
+        CMAKE_GENERATOR_ARGS=(-G "Visual Studio 17 2022" -A x64 -T ClangCL)
+        CMAKE_FLAGS+=(-DASTCENC_ISA_AVX2=ON)
+        CMAKE_FLAGS+=(-DASTCENC_ISA_SSE41=ON)
+        CMAKE_FLAGS+=(-DASTCENC_ISA_SSE2=ON)
+        CMAKE_FLAGS+=(-DASTCENC_ISA_NATIVE=OFF)
+        CMAKE_FLAGS+=(-DASTCENC_PACKAGE=x64)
         ;;
 esac
 
@@ -84,7 +100,7 @@ mkdir -p ${BUILD_DIR}
 
 pushd $BUILD_DIR
 
-cmake ${CMAKE_FLAGS} $SOURCE_DIR
+cmake "${CMAKE_GENERATOR_ARGS[@]}" "${CMAKE_FLAGS[@]}" "$SOURCE_DIR"
 cmake --build . --config $CONFIG -j 8
 
 LIB_NAME=
@@ -104,7 +120,7 @@ case $PLATFORM in
         LIB_NAME=libastcenc-avx2-static.a
         ;;
     x86_64-win32)
-        LIB_NAME=$CONFIG/astcenc-native-static.lib
+        LIB_NAME=$CONFIG/astcenc-avx2-static.lib
         LIB_EXT=.lib
         ;;
     *)
