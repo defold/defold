006f14da3d3153b219102a4b5c1df2d15e892276
AGulev <me@agulev.com>
2025-11-26 14:41:13 +0100

    Reapply "algo optimisations"
    This reverts commit d53152d1e7c2ca1c60f2a46df733c4b2ab1c65fc.

--- com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/util/PackedResources.java
+++ com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/util/PackedResources.java
@@ -2,6 +2,9 @@ package com.dynamo.bob.util;
 
 import com.dynamo.bob.Bob;
 import com.dynamo.bob.Platform;
+import com.dynamo.bob.pipeline.ModelImporterJni;
+import com.dynamo.bob.pipeline.ShadercJni;
+import com.dynamo.bob.pipeline.TexcLibraryJni;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,6 +21,12 @@ public class PackedResources {
 
     private static void runUnpackAllLibsAsync(Platform platform) throws IOException {
         TimeProfiler.start("runUnpackAllLibsAsync");
+        // unpacking for these happens in static initializers
+        new TexcLibraryJni();
+        new ShadercJni();
+        new ModelImporterJni();
+
+        // regular libs
         String platformPair = platform.getPair();
         File targetDir = new File(Bob.getRootFolder(), platformPair);
 
