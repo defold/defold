a844006cc6cdb9654875c88f78005bb2e88b12d3
AGulev <me@agulev.com>
2025-12-18 19:44:52 +0100

    fix issue with http server on windows

--- engine/dlib/scripts/run_http_server_standalone.py
+++ engine/dlib/scripts/run_http_server_standalone.py
@@ -17,14 +17,13 @@ import urllib, urllib.request, time, atexit, os, sys, configparser, tempfile
 
 server_sockets = None
 
-os.system('scripts/start_http_server.sh')
-
 server_config_path = "test_http_server.cfg"
 if os.path.exists(server_config_path):
     os.unlink(server_config_path)
 
 start = time.time()
-timeout = 8
+timeout = 30 if sys.platform == 'win32' else 15
+os.system('scripts/start_http_server.sh')
 while True:
     if time.time() - start > timeout:
         error('HTTP server failed to start within ' + timeout + ' seconds')
