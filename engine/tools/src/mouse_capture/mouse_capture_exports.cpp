7f4676064d328b5c86347be9fb7768c72021bba4
Joseph Ferano <joseph@ferano.io>
2026-02-26 14:12:29 +0700

    Forgot file

---
+++ engine/tools/src/mouse_capture/mouse_capture_exports.cpp
@@ -0,0 +1,24 @@
+#include <dmsdk/dlib/shared_library.h>
+#include "mouse_capture.h"
+
+extern "C" {
+    DM_DLLEXPORT void* MouseCapture_CreateContext() {
+        return dmMouseCapture::CreateContext();
+    }
+
+    DM_DLLEXPORT void MouseCapture_DestroyContext(void* context) {
+        dmMouseCapture::DestroyContext((dmMouseCapture::HContext)context);
+    }
+
+    DM_DLLEXPORT bool MouseCapture_StartCapture(void* context, WindowHandle window) {
+        return dmMouseCapture::StartCapture((dmMouseCapture::HContext)context, window);
+    }
+
+    DM_DLLEXPORT void MouseCapture_StopCapture(void* context) {
+        dmMouseCapture::StopCapture((dmMouseCapture::HContext)context);
+    }
+
+    DM_DLLEXPORT bool MouseCapture_PollDelta(void* context, dmMouseCapture::MouseDelta* delta) {
+        return dmMouseCapture::PollDelta((dmMouseCapture::HContext)context, delta);
+    }
+}
