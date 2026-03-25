cd253fffb2c04afa5aa9758bdf9cf3b60f5018ed
Jhonny Göransson <jhonny.goransson@gmail.com>
2026-03-25 18:57:38 +0100

    Increase number of texture units per draw from 8 to 16 (#12081)
    * Change from 8 to 16 texture units + add test
    
    * Add new property hashes to gamesys

--- engine/render/src/dmsdk/render/render.h
+++ engine/render/src/dmsdk/render/render.h
@@ -152,7 +152,7 @@ namespace dmRender
 
 
     /*#
-     * The maximum number of textures the render object can hold (currently 8)
+     * The maximum number of textures the render object can hold (16)
      * @constant
      * @name dmRender::RenderObject::MAX_TEXTURE_COUNT
      */
@@ -184,7 +184,7 @@ namespace dmRender
         RenderObject();
         void Init();
 
-        static const uint32_t MAX_TEXTURE_COUNT       = 8;
+        static const uint32_t MAX_TEXTURE_COUNT       = 16;
         static const uint32_t MAX_VERTEX_BUFFER_COUNT = 3;
 
         HNamedConstantBuffer            m_ConstantBuffer;
