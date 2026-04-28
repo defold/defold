6e31e32a90d74afe6c5a6b7064ee5e0ac4ff668d
AGulev <me@agulev.com>
2026-04-28 17:04:36 +0200

    faster defold sdk build

--- ci/ci.py
+++ ci/ci.py
@@ -429,7 +429,7 @@ def release(channel):
     call(cmd)
 
 def build_sdk(channel):
-    args = "python scripts/build.py install_ext build_sdk".split()
+    args = "python scripts/build.py install_release_dependencies build_sdk".split()
     opts = []
     opts.append("--channel=%s" % channel)
 
