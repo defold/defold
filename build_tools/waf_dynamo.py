587972e89554f21ea6be01ebf0b3e98c13e2aaac
Mathias Westerdahl <mwesterdahl76@gmail.com>
2026-05-01 09:39:57 +0200

    Added initial version of find_feature_files and modified condition_variable files to support this system

--- build_tools/waf_dynamo.py
+++ build_tools/waf_dynamo.py
@@ -12,7 +12,7 @@
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-import os, sys, subprocess, shutil, re, socket, stat, glob, zipfile, tempfile, configparser, shlex
+import os, sys, subprocess, shutil, re, socket, stat, glob, zipfile, tempfile, configparser, shlex, json
 from waflib.Configure import conf
 from waflib import Utils, Build, Options, Task, Logs
 from waflib.TaskGen import extension, feature, after, before, task_gen
@@ -94,6 +94,132 @@ def platform_setup_vars(ctx, build_util):
 
 def transform_runnable_path(platform, path):
     return waf_dynamo_vendor.transform_runnable_path(platform, path)
+
+def get_repo_root():
+    return os.path.dirname(script_dir)
+
+def get_platform_roots(platform):
+    config_path = os.path.join(get_repo_root(), '.defold-platforms')
+    if not os.path.exists(config_path):
+        return []
+    with open(config_path, 'r') as f:
+        platforms = json.load(f)
+    platform_config = platforms.get(platform, {})
+    roots = []
+    if 'root' in platform_config:
+        roots.append(platform_config['root'])
+    roots.extend(platform_config.get('roots', []))
+    return roots
+
+def get_platform_file_tags(platform):
+    target = platform
+    if '-' in platform:
+        target = platform.split('-')[-1]
+
+    tags = []
+    def append_tag(tag):
+        if tag and tag not in tags:
+            tags.append(tag)
+
+    append_tag(target)
+
+    if target == 'nx64':
+        append_tag('switch')
+        append_tag('nintendo')
+    elif target in ('ps4', 'ps5'):
+        append_tag('playstation')
+        append_tag('sony')
+    elif target == 'xbone':
+        append_tag('xbox')
+        append_tag('microsoft')
+        append_tag('win32')
+
+    if target in ('macos', 'ios'):
+        append_tag('darwin')
+        append_tag('apple')
+        append_tag('posix')
+    elif target == 'android':
+        append_tag('posix')
+    elif target == 'linux':
+        append_tag('posix')
+
+    return tags
+
+def find_platform_file(bld, platform, path, public_fallback = True):
+    repo_root = get_repo_root()
+    base_path = os.path.relpath(bld.path.abspath(), repo_root)
+    for root in get_platform_roots(platform):
+        absolute_path = os.path.join(root, base_path, path)
+        if os.path.exists(absolute_path):
+            node = bld.root.find_node(absolute_path)
+            if node:
+                return node
+            return absolute_path
+
+    if public_fallback:
+        return bld.path.find_node(path)
+    return None
+
+def source_file_path(source):
+    if hasattr(source, 'abspath'):
+        return source.abspath()
+    return source
+
+def remove_source_files(sources, remove_sources):
+    remove_paths = set(source_file_path(x) for x in remove_sources)
+    return [x for x in sources if source_file_path(x) not in remove_paths]
+
+def find_feature_files(bld, feature_name, platform):
+    """Return (selected_files, feature_files) for feature_name.
+
+    Rules:
+    * feature_files contains <feature>.<ext> and <feature>_*.ext when found.
+    * selected_files contains all <feature>.<ext> core files when found.
+    * selected_files contains all matching <feature>_<tag>.ext files, using platform tags then default.
+    * platform roots are searched before the public repo.
+    * missing feature files or missing selected files fail the build.
+    """
+    files = []
+    feature_files = []
+
+    def find_file(path, public_fallback = True):
+        return find_platform_file(bld, platform, path, public_fallback)
+
+    def append_file(files, node):
+        if node and source_file_path(node) not in [source_file_path(x) for x in files]:
+            files.append(node)
+
+    feature_base, extension = os.path.splitext(feature_name)
+    extensions = ['.cpp', '.c', '.cc', '.cxx', '.mm', '.m']
+    if extension:
+        extensions = [extension]
+
+    feature_patterns = []
+    for extension in extensions:
+        feature_patterns += [feature_base + extension,
+                             feature_base + '_*' + extension]
+    for node in bld.path.ant_glob(feature_patterns):
+        append_file(feature_files, node)
+
+    for extension in extensions:
+        node = find_file(feature_base + extension)
+        if node:
+            append_file(files, node)
+            append_file(feature_files, node)
+
+    for tag in get_platform_file_tags(platform) + ['default']:
+        for extension in extensions:
+            node = find_file('%s_%s%s' % (feature_base, tag, extension))
+            if node:
+                append_file(files, node)
+                append_file(feature_files, node)
+
+    if not feature_files:
+        bld.fatal('Could not find any source files for feature %s' % feature_name)
+    if not files:
+        bld.fatal('Could not find selected source files for feature %s on platform %s' % (feature_name, platform))
+
+    return files, feature_files
 
 def platform_glfw_version(platform):
     if platform in ['x86_64-macos', 'arm64-macos', 'x86_64-win32', 'win32', 'x86_64-linux', 'arm64-linux']:
