69b70a218a5f6753ba3d6af2b1c55625687681bd
Mathias Westerdahl <mwesterdahl76@gmail.com>
2026-05-06 11:00:10 +0200

    Don't unpack all tools when running "--version"

--- com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/Bob.java
+++ com.dynamo.cr/com.dynamo.cr.bob/src/com/dynamo/bob/Bob.java
@@ -639,6 +639,10 @@ public class Bob {
             if (cmd == null) { // nothing to do: requested to print help
                 return new InvocationResult(true, Collections.emptyList());
             }
+            if (cmd.hasOption("version")) {
+                System.out.println(String.format("bob.jar version: %s  sha1: %s  built: %s", EngineVersion.version, EngineVersion.sha1, EngineVersion.timestamp));
+                return new InvocationResult(true, Collections.emptyList());
+            }
             String buildDirectory = getOptionsValue(cmd, 'o', "build/default");
             String rootDirectory = getOptionsValue(cmd, 'r', cwd);
 
@@ -672,11 +676,6 @@ public class Bob {
 
             try {
                 TimeProfiler.start("ParseCommandLine");
-                if (cmd.hasOption("version")) {
-                    System.out.println(String.format("bob.jar version: %s  sha1: %s  built: %s", EngineVersion.version, EngineVersion.sha1, EngineVersion.timestamp));
-                    return new InvocationResult(true, Collections.emptyList());
-                }
-
                 if (cmd.hasOption("debug") && cmd.hasOption("variant")) {
                     System.out.println("-d (--debug) option is deprecated and can't be set together with option --variant");
                     throw new OptionValidationException(1);
