c1aa8c63c1b92706c3d1fa9208af8384d2b5af27
AGulev <me@agulev.com>
2026-05-26 19:03:24 +0200

    review fix

--- engine/dlib/src/dmsdk/dlib/jobsystem.h
+++ engine/dlib/src/dmsdk/dlib/jobsystem.h
@@ -210,7 +210,7 @@ enum JobSystemResult JobSystemPushJob(HJobContext context, HJob job);
  * @name JobSystemCancelJob
  * @param context [type:HJobContext] the job system context
  * @param job [type:HJob] the job to cancel
- * @return result [type:JobSystemResult] Returns JOBSYSTEM_RESULT_OK if finished, JOBSYSTEM_RESULT_CANCELED if canceled, or JOBSYSTEM_RESULT_PENDING if the job (or any child) is still in flight
+ * @return result [type:JobSystemResult] Returns JOBSYSTEM_RESULT_CANCELED if canceled, JOBSYSTEM_RESULT_PENDING if the job (or any child) is still in flight, or JOBSYSTEM_RESULT_INVALID_HANDLE if the handle is invalid
  * @examples
  * How to wait until a job has been cancelled or finished
  * ```c
@@ -220,6 +220,8 @@ enum JobSystemResult JobSystemPushJob(HJobContext context, HJob job);
  *     dmTime::Sleep(1000);
  *     jr = JobSystemCancelJob(job_context, hjob);
  * }
+ * // If the loop observed PENDING before this point, INVALID_HANDLE means the
+ * // job was flushed by JobSystemUpdate() between polls.
  * ```
  */
 enum JobSystemResult JobSystemCancelJob(HJobContext context, HJob job);
