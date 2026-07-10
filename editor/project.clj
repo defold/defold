d59e190d9fbe2fb6229c4ffca0d7db4f07d48d16
vlaaad <vlad@refold.io>
2026-07-10 08:36:34 +0200

    Fix dependency fetch timeout issue (#12718)
    * Fix depepdency fetch timeout issue
    
    Fix #12603
    
    * Bump fetches per host to 4

--- editor/project.clj
+++ editor/project.clj
@@ -161,8 +161,8 @@
 
   :jvm-opts          ["-Djna.nosys=true"
                       "-Djava.net.preferIPv4Stack=true"
-                      "-Ddefold.library.connectTimeoutMillis=2000"
-                      "-Ddefold.library.hostProbeTimeoutMillis=5000"
+                      "-Ddefold.library.connectTimeoutMillis=10000"
+                      "-Ddefold.library.hostProbeTimeoutMillis=10000"
                       "-Dfile.encoding=UTF-8"
                       ;; hide warnings about illegal reflective access by jogl
                       "--add-opens=java.base/java.lang=ALL-UNNAMED"
