b8e3dbdbe76706875016e732fe3b2a21072d345a
AGulev <me@agulev.com>
2026-07-20 15:43:49 +0200

    add test

--- engine/engine/src/test/test_engine.cpp
+++ engine/engine/src/test/test_engine.cpp
@@ -626,6 +626,13 @@ TEST_F(EngineTest, ISSUE_12362)
     ASSERT_EQ(0, Launch(DM_ARRAY_SIZE(argv), (char**)argv, 0, 0, 0));
 }
 
+TEST_F(EngineTest, ISSUE_12703)
+{
+    char project_path[256];
+    const char* argv[] = {"test_engine", "--config=bootstrap.main_collection=/issue-12703/issue-12703.collectionc", "--config=dmengine.unload_builtins=0", MAKE_PATH(project_path, "/game.projectc")};
+    ASSERT_EQ(0, Launch(DM_ARRAY_SIZE(argv), (char**)argv, 0, 0, 0));
+}
+
 TEST_F(EngineTest, ModelComponent)
 {
     char project_path[256];
