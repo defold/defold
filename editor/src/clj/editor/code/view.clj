d7a91a135bece71356b40df939c7e8cc53676db1
Joseph Ferano <joseph@ferano.io>
2026-09-03 15:10:20 +0700

    Restore syntax validation before syntax-dependent editor actions

--- editor/src/clj/editor/code/view.clj
+++ editor/src/clj/editor/code/view.clj
@@ -1838,8 +1838,11 @@
     (g/user-data! resource-node :syntax-info syntax-info)
     syntax-info))
 
-(defn- get-current-syntax-info [resource-node]
-  (or (g/user-data resource-node :syntax-info) []))
+(defn- get-cursor-ranges-syntax-info [view-node cursor-ranges evaluation-context]
+  (get-valid-syntax-info
+    (get-property view-node :resource-node evaluation-context)
+    (get-property view-node :canvas-repaint-info evaluation-context)
+    (transduce (map #(inc (.-row (data/cursor-range-end %)))) max 1 cursor-ranges)))
 
 (defn- syntax-scope-before-cursor [view-node ^Cursor cursor evaluation-context]
   (if-let [syntax-info (coll/not-empty
@@ -2106,7 +2109,7 @@
                          (vec (sort-by first splices)))
                        splices)
          syntax-info (g/with-auto-evaluation-context evaluation-context
-                       (get-current-syntax-info (get-property view-node :resource-node evaluation-context)))
+                       (get-cursor-ranges-syntax-info view-node replacement-cursor-ranges evaluation-context))
          props (data/replace-typed-chars indent-level-pattern indent-string grammar syntax-info lines regions layout all-splices)]
      (when (some? props)
        (hide-hover! view-node)
@@ -2435,7 +2438,7 @@
         (data/delete (get-property view-node :lines evaluation-context)
                      (get-property view-node :grammar evaluation-context)
                      (prefs/get prefs [:code :auto-closing-parens])
-                     (get-current-syntax-info (get-property view-node :resource-node evaluation-context))
+                     (get-cursor-ranges-syntax-info view-node cursor-ranges evaluation-context)
                      cursor-ranges
                      (get-property view-node :regions evaluation-context)
                      (get-property view-node :layout evaluation-context)
@@ -2476,14 +2479,15 @@
   (hide-suggestions! view-node)
   (set-properties! view-node nil
                    (g/with-auto-evaluation-context evaluation-context
-                     (data/indent (get-property view-node :indent-level-pattern evaluation-context)
-                                  (get-property view-node :indent-string evaluation-context)
-                                  (get-property view-node :grammar evaluation-context)
-                                  (get-current-syntax-info (get-property view-node :resource-node evaluation-context))
-                                  (get-property view-node :lines evaluation-context)
-                                  (get-property view-node :cursor-ranges evaluation-context)
-                                  (get-property view-node :regions evaluation-context)
-                                  (get-property view-node :layout evaluation-context)))))
+                     (let [cursor-ranges (get-property view-node :cursor-ranges evaluation-context)]
+                       (data/indent (get-property view-node :indent-level-pattern evaluation-context)
+                                    (get-property view-node :indent-string evaluation-context)
+                                    (get-property view-node :grammar evaluation-context)
+                                    (get-cursor-ranges-syntax-info view-node cursor-ranges evaluation-context)
+                                    (get-property view-node :lines evaluation-context)
+                                    cursor-ranges
+                                    (get-property view-node :regions evaluation-context)
+                                    (get-property view-node :layout evaluation-context))))))
 
 (defn- deindent! [view-node]
   (hide-hover! view-node)
@@ -2582,7 +2586,10 @@
                                 (get-property view-node :cursor-ranges evaluation-context)
                                 (get-property view-node :regions evaluation-context)
                                 (get-property view-node :layout evaluation-context)
-                                (get-current-syntax-info (get-property view-node :resource-node evaluation-context))
+                                (get-cursor-ranges-syntax-info
+                                  view-node
+                                  (get-property view-node :cursor-ranges evaluation-context)
+                                  evaluation-context)
                                 typed)))
         (hide-hover! view-node)
         (if (and show-suggestions (implies-completions? view-node))
@@ -2858,15 +2865,16 @@
   (hide-suggestions! view-node)
   (set-properties! view-node nil
                    (g/with-auto-evaluation-context evaluation-context
-                     (data/paste (get-property view-node :indent-level-pattern evaluation-context)
-                                 (get-property view-node :indent-string evaluation-context)
-                                 (get-property view-node :grammar evaluation-context)
-                                 (get-current-syntax-info (get-property view-node :resource-node evaluation-context))
-                                 (get-property view-node :lines evaluation-context)
-                                 (get-property view-node :cursor-ranges evaluation-context)
-                                 (get-property view-node :regions evaluation-context)
-                                 (get-property view-node :layout evaluation-context)
-                                 clipboard))))
+                     (let [cursor-ranges (get-property view-node :cursor-ranges evaluation-context)]
+                       (data/paste (get-property view-node :indent-level-pattern evaluation-context)
+                                   (get-property view-node :indent-string evaluation-context)
+                                   (get-property view-node :grammar evaluation-context)
+                                   (get-cursor-ranges-syntax-info view-node cursor-ranges evaluation-context)
+                                   (get-property view-node :lines evaluation-context)
+                                   cursor-ranges
+                                   (get-property view-node :regions evaluation-context)
+                                   (get-property view-node :layout evaluation-context)
+                                   clipboard)))))
 
 (defn split-selection-into-lines! [view-node]
   (hide-hover! view-node)
@@ -2974,14 +2982,15 @@
   (run [view-node]
     (set-properties! view-node nil
                      (g/with-auto-evaluation-context evaluation-context
-                       (data/reindent (get-property view-node :indent-level-pattern evaluation-context)
-                                      (get-property view-node :indent-string evaluation-context)
-                                      (get-property view-node :grammar evaluation-context)
-                                      (get-current-syntax-info (get-property view-node :resource-node evaluation-context))
-                                      (get-property view-node :lines evaluation-context)
-                                      (get-property view-node :cursor-ranges evaluation-context)
-                                      (get-property view-node :regions evaluation-context)
-                                      (get-property view-node :layout evaluation-context))))))
+                       (let [cursor-ranges (get-property view-node :cursor-ranges evaluation-context)]
+                         (data/reindent (get-property view-node :indent-level-pattern evaluation-context)
+                                        (get-property view-node :indent-string evaluation-context)
+                                        (get-property view-node :grammar evaluation-context)
+                                        (get-cursor-ranges-syntax-info view-node cursor-ranges evaluation-context)
+                                        (get-property view-node :lines evaluation-context)
+                                        cursor-ranges
+                                        (get-property view-node :regions evaluation-context)
+                                        (get-property view-node :layout evaluation-context)))))))
 
 (handler/defhandler :code.convert-indentation :code-view
   (label [user-data]
