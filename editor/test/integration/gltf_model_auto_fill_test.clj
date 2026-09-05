36054157d026fe31a974831afc0934065514d1f1
Mathias Westerdahl <mwesterdahl76@gmail.com>
2026-09-05 14:22:40 +0200

    liocense text fix

--- editor/test/integration/gltf_model_auto_fill_test.clj
+++ editor/test/integration/gltf_model_auto_fill_test.clj
@@ -5,7 +5,7 @@
 ;; this file except in compliance with the License.
 ;;
 ;; You may obtain a copy of the License, together with FAQs at
-;; https://www.defold.com/license
+;; https:;;www.defold.com/license
 ;;
 ;; Unless required by applicable law or agreed to in writing, software distributed
 ;; under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
