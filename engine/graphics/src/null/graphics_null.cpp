67b14d9ff5b2d4d4084dcd65c6c367bded924d53
Jhonny Göransson <jhonny.goransson@gmail.com>
2026-09-21 13:31:18 +0200

    Let null adapter accept MSL

--- engine/graphics/src/null/graphics_null.cpp
+++ engine/graphics/src/null/graphics_null.cpp
@@ -1035,13 +1035,16 @@ namespace dmGraphics
             case ShaderDesc::SHADER_TYPE_VERTEX:
                 return language == ShaderDesc::LANGUAGE_GLSL_SM330 ||
                        language == ShaderDesc::LANGUAGE_GLES_SM300 ||
+                       language == ShaderDesc::LANGUAGE_MSL_22 ||
                        language == ShaderDesc::LANGUAGE_SPIRV;
             case ShaderDesc::SHADER_TYPE_FRAGMENT:
                 return language == ShaderDesc::LANGUAGE_GLSL_SM330 ||
                        language == ShaderDesc::LANGUAGE_GLES_SM300 ||
+                       language == ShaderDesc::LANGUAGE_MSL_22 ||
                        language == ShaderDesc::LANGUAGE_SPIRV;
             case ShaderDesc::SHADER_TYPE_COMPUTE:
                 return language == ShaderDesc::LANGUAGE_GLSL_SM430 ||
+                       language == ShaderDesc::LANGUAGE_MSL_22 ||
                        language == ShaderDesc::LANGUAGE_SPIRV;
         }
     #endif
